97dfdef66e447328d82c111b05a59cdbd9bf2fba
Mark Raasveldt <mark.raasveldt@gmail.com>
2018-10-23 11:37:59 -0300

    --ignore-last-format option for format.py to always format everything

--- format.py
+++ format.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
-import os, time
+import os, time, sys
 
 last_format_file = '.last_format'
+ignore_last_format = False
 cpp_format_command = 'clang-format -i -sort-includes=${SORT_INCLUDES} -style=file "${FILE}"'
 sql_format_command = 'pg_format "${FILE}" -o "${FILE}.out" && mv "${FILE}.out" "${FILE}"'
 extensions = ['.cpp', '.c', '.hpp', '.h', '.cc', '.hh', '.sql']
+
+for arg in sys.argv:
+	if arg == '--ignore-last-format':
+		ignore_last_format = True
 
 format_commands = {
 	'.cpp': cpp_format_command,
@@ -20,16 +25,17 @@ format_commands = {
 
 last_format_time = 0
 
-if os.path.exists(last_format_file):
-	with open(last_format_file, 'r') as f:
-		try:
-			last_format_time = float(f.read())
-		except:
-			pass
+if not ignore_last_format:
+	if os.path.exists(last_format_file):
+		with open(last_format_file, 'r') as f:
+			try:
+				last_format_time = float(f.read())
+			except:
+				pass
 
 
-if last_format_time > 0:
-	print('Last format: %s' % (time.ctime(last_format_time)))
+	if last_format_time > 0:
+		print('Last format: %s' % (time.ctime(last_format_time)))
 
 
 time.ctime(os.path.getmtime('tools/sqlite3_api_wrapper/include/sqlite3.h'))
@@ -40,7 +46,7 @@ def format_directory(directory, sort_includes=True):
 	for f in files:
 		full_path = os.path.join(directory, f)
 		if os.path.isdir(full_path):
-			format_directory(full_path)
+			format_directory(full_path, sort_includes)
 		else:
 			# don't format TPC-H constants
 			if 'tpch_constants.hpp' in full_path:
@@ -61,8 +67,10 @@ format_directory('src')
 format_directory('test')
 format_directory('third_party/dbgen')
 format_directory('third_party/sqlsmith', False)
+format_directory('third_party/tpce-tool', False)
 format_directory('tools')
 
 # write the last modified time
-with open(last_format_file, 'w+') as f:
-	f.write(str(time.time()))
+if not ignore_last_format:
+	with open(last_format_file, 'w+') as f:
+		f.write(str(time.time()))
