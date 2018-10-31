dc1ccba7d89455d1aea4a8396c7e7f9767250342
Mark Raasveldt <mark.raasveldt@gmail.com>
2018-10-31 08:39:45 -0300

    Use subprocess module to allow for Ctrl+C of python process

--- benchmark/run_benchmarks_forever.py
+++ benchmark/run_benchmarks_forever.py
@@ -1,13 +1,16 @@
 
-import os, sys, time
+import os, sys, time, subprocess
 
 # move to the base directory
 path = os.path.dirname(sys.argv[0])
 if len(path) > 0:
-	os.chdir(path)
+    os.chdir(path)
 os.chdir("..")
 
 while True:
-	if os.system('python run_benchmarks.py') == 0:
-		os.system('python benchmark_html.py')
-	time.sleep(60)
+    proc = subprocess.Popen('python', 'benchmark/run_benchmarks.py')
+    proc.wait()
+    if proc.returncode == 0:
+        proc = subprocess.Popen('python', 'benchmark/benchmark_html.py')
+        proc.wait()
+    time.sleep(60)
