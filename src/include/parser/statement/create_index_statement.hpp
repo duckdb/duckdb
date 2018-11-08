7d035bb42cce8212d14586e8e3170e92d619ed7d
Pedro Holanda <pedroholanda@gmail.com>
2018-11-08 17:10:05 +0100

    Create Index Parsing

---
+++ src/include/parser/statement/create_index_statement.hpp
@@ -0,0 +1,40 @@
+//===----------------------------------------------------------------------===//
+//
+//                         DuckDB
+//
+// parser/statement/create_index_statement.hpp
+//
+// Author: Pedro Holanda
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "parser/column_definition.hpp"
+
+#include "parser/parsed_data.hpp"
+#include "parser/sql_statement.hpp"
+
+namespace duckdb {
+
+    class CreateIndexStatement : public SQLStatement {
+    public:
+        CreateIndexStatement()
+                : SQLStatement(StatementType::CREATE_INDEX),
+                  info(make_unique<CreateIndexInformation>()){};
+        virtual ~CreateIndexStatement() {
+        }
+
+        virtual std::string ToString() const {
+            return "CREATE INDEX";
+        }
+        virtual void Accept(SQLNodeVisitor *v) {
+//            v->Visit(*this);
+        }
+
+        std::unique_ptr<CreateIndexInformation> info;
+    };
+
+} // namespace duckdb
+//
+
