c1b92c22aca59b5b203aa4cb686e956e84f60937
diegomestre2 <diegogomest@gmail.com>
2018-12-06 16:32:31 +0100

    More cleaning up for storage

--- src/common/fstream_util.cpp
+++ src/common/fstream_util.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 using namespace duckdb;
 
-fstream FstreamUtil::OpenFile(const string &file_path) {
+fstream FstreamUtil::OpenFile(const string &file_path, ios_base::openmode mode) {
+
 	fstream new_file;
-	new_file.open(file_path, ios::in | ios::out | ios::binary);
+	new_file.open(file_path, mode);
 	if (!new_file.good()) {
-		throw IOException("Could not open File!");
+		throw IOException("Could not open File!" + file_path);
 	}
 
 	return new_file;
+}
+
+void FstreamUtil::CloseFile(fstream &file) {
+	file.close();
+	// check the success of the write
+	if (file.fail()) {
+		throw IOException("Failed to close the file!");
+	}
 }
 
 size_t FstreamUtil::GetFileSize(fstream &file) {
@@ -18,10 +27,10 @@ size_t FstreamUtil::GetFileSize(fstream &file) {
 	return file.tellg();
 }
 
-unique_ptr<char[]> FstreamUtil::ReadBinary(fstream &file) {
+data_ptr FstreamUtil::ReadBinary(fstream &file) {
 	size_t file_size = GetFileSize(file);
 	file.seekg(0, ios::beg);
-	auto result = unique_ptr<char[]>(new char[file_size]);
+	auto result = data_ptr(new char[file_size]);
 	file.read(result.get(), file_size);
 
 	return result;
