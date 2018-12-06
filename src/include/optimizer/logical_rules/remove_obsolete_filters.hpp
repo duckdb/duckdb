9db68ecc19f5f47e694d75c4d3484f3b68d19ad2
Pedro Holanda <pedroholanda@gmail.com>
2018-12-06 17:35:33 +0100

    Obsolete filters removal optimization

---
+++ src/include/optimizer/logical_rules/remove_obsolete_filters.hpp
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// optimizer/logical_rules/remove_obsolete_filters.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "optimizer/rule.hpp"
+
+namespace duckdb {
+    class RemoveObsoleteFilterRule : public Rule {
+    public:
+        RemoveObsoleteFilterRule();
+
+        unique_ptr<LogicalOperator> Apply(Rewriter &rewriter, LogicalOperator &op_root, vector<AbstractOperator> &bindings,
+                                          bool &fixed_point);
+    };
+} // namespace duckdb
