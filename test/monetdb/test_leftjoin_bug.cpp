e782613e62a07420bc834ca16e69c73a0fd24f24
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-02-25 15:41:55 +0100

    Add additional tests ported from MonetDB

--- test/monetdb/test_leftjoin_bug.cpp
+++ test/monetdb/test_leftjoin_bug.cpp
@@ -12,11 +12,54 @@ TEST_CASE("MonetDB Test: leftjoin.Bug-3981.sql", "[monetdb]") {
 	DuckDBConnection con(db);
 	con.EnableQueryVerification();
 
-	// LEFT OUTER JOIN on arbitrary expression (not simple LEFT-RIGHT comparison) not supported currently
 	result = con.Query(
 	    "SELECT * FROM ( SELECT 'apple' as fruit UNION ALL SELECT 'banana' ) a JOIN ( SELECT 'apple' as fruit UNION "
 	    "ALL SELECT 'banana' ) b ON a.fruit=b.fruit LEFT JOIN ( SELECT 1 as isyellow ) c ON b.fruit='banana';");
 	REQUIRE(CHECK_COLUMN(result, 0, {"apple", "banana"}));
 	REQUIRE(CHECK_COLUMN(result, 1, {"apple", "banana"}));
 	REQUIRE(CHECK_COLUMN(result, 2, {Value(), 1}));
+}
+
+TEST_CASE("MonetDB Test: null_matches_in_outer.Bug-6398.sql", "[monetdb]") {
+	unique_ptr<DuckDBResult> result;
+	DuckDB db(nullptr);
+	DuckDBConnection con(db);
+	con.EnableQueryVerification();
+
+	REQUIRE_NO_FAIL(con.Query("create table \"E\" (\"intCol\" bigint, \"stringCol\" varchar);"));
+
+	REQUIRE_NO_FAIL(con.Query("insert into \"E\" values (0, 'zero');"));
+	REQUIRE_NO_FAIL(con.Query("insert into \"E\" values (1, 'one');"));
+	REQUIRE_NO_FAIL(con.Query("insert into \"E\" values (2, 'two');"));
+	REQUIRE_NO_FAIL(con.Query("insert into \"E\" values (null, null);"));
+
+	REQUIRE_NO_FAIL(con.Query("create table \"I\" (\"intCol\" bigint, \"stringCol\" varchar);"));
+
+	REQUIRE_NO_FAIL(con.Query("insert into \"I\" values (2, 'due');"));
+	REQUIRE_NO_FAIL(con.Query("insert into \"I\" values (4, 'quattro');"));
+	REQUIRE_NO_FAIL(con.Query("insert into \"I\" values (null, 'this is not null')"));
+
+	result = con.Query("select * from \"E\" left outer join \"I\" on \"E\".\"intCol\" = \"I\".\"intCol\" or "
+	                   "(\"E\".\"intCol\" is null and  \"I\".\"intCol\" is null) ORDER BY 1;");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value(), 0, 1, 2}));
+	REQUIRE(CHECK_COLUMN(result, 1, {Value(), "zero", "one", "two"}));
+	REQUIRE(CHECK_COLUMN(result, 2, {Value(), Value(), Value(), 2}));
+	REQUIRE(CHECK_COLUMN(result, 3, {"this is not null", Value(), Value(), "due"}));
+}
+
+TEST_CASE("MonetDB Test: outerjoin_project.Bug-3725.sql", "[monetdb]") {
+	unique_ptr<DuckDBResult> result;
+	DuckDB db(nullptr);
+	DuckDBConnection con(db);
+	con.EnableQueryVerification();
+
+	return;
+
+	REQUIRE_NO_FAIL(con.Query("create table a (a integer);"));
+	REQUIRE_NO_FAIL(con.Query("create table b (a integer);"));
+	REQUIRE_NO_FAIL(con.Query("insert into a values (1);"));
+
+	result = con.Query("select * from a left join (select a, 20 from b) as x using (a);");
+	REQUIRE(CHECK_COLUMN(result, 0, {1}));
+	REQUIRE(CHECK_COLUMN(result, 1, {Value()}));
 }
