b60902a3e862d8ed876b94bebb47dd3723b979bc
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-02-25 17:32:39 +0100

    Move WindowSegmentTree to separate file

--- src/include/execution/operator/aggregate/physical_window.hpp
+++ src/include/execution/operator/aggregate/physical_window.hpp
@@ -46,29 +46,4 @@ public:
 	ChunkCollection window_results;
 };
 
-class WindowSegmentTree {
-public:
-	WindowSegmentTree(ExpressionType window_type, TypeId payload_type, ChunkCollection *input)
-	    : aggregate(Value()), n_aggregated(0), window_type(window_type), payload_type(payload_type), input_ref(input) {
-		ConstructTree();
-	}
-	Value Compute(size_t start, size_t end);
-
-private:
-	void ConstructTree();
-	void WindowSegmentValue(size_t l_idx, size_t begin, size_t end);
-	void AggregateInit();
-	Value AggegateFinal();
-
-	Value aggregate;
-	size_t n_aggregated; // for sum
-	ExpressionType window_type;
-	TypeId payload_type;
-	unique_ptr<char[]> levels_flat_native;
-	vector<size_t> levels_flat_start;
-
-	const size_t fanout = 64; // this should cleanly divide STANDARD_VECTOR_SIZE
-	ChunkCollection *input_ref;
-};
-
 } // namespace duckdb
