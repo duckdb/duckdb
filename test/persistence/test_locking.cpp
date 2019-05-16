20cd84c9b10c4a3ff97318aa9d28d60d5ca1ecae
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-05-16 19:24:53 +0200

    Fix cross process communication in locking test

--- test/persistence/test_locking.cpp
+++ test/persistence/test_locking.cpp
@@ -14,9 +14,8 @@ using namespace std;
 #define BOOL_COUNT 3
 
 TEST_CASE("Test write lock with multiple processes", "[persistence]") {
-	// shared memory to keep track of which processes are done
-	bool *finished = (bool *)mmap(NULL, sizeof(bool) * BOOL_COUNT, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
-	memset(finished, 0, BOOL_COUNT * sizeof(bool));
+	uint64_t *count = (uint64_t *)mmap(NULL, sizeof(uint64_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+	*count = 0;
 
 	string dbdir = TestCreatePath("writelocktest");
 	DeleteDatabase(dbdir);
@@ -29,31 +28,33 @@ TEST_CASE("Test write lock with multiple processes", "[persistence]") {
 		DuckDB db(dbdir);
 		Connection con(db);
 		// opened db for writing
-		finished[0] = true;
 		// insert some values
+		(*count)++;
 		con.Query("CREATE TABLE a(i INTEGER)");
 		con.Query("INSERT INTO a VALUES(42)");
-		// wait for parent to finish
-		while (!finished[1]);
-		finished[2] = true;
-		// exit
-		exit(0);
+		while(true) {
+			con.Query("SELECT * FROM a");
+			usleep(100);
+		}
 	} else if (pid > 0) {
 		unique_ptr<DuckDB> db;
 		// parent process
-		// wait for child to open db for writing
-		while(!finished[0]);
+		// sleep a bit to wait for child process
+		while(*count == 0) {
+			usleep(100);
+		}
 		// try to open db for writing, this should fail
 		REQUIRE_THROWS(db = make_unique<DuckDB>(dbdir));
-		// now kill the child
-		finished[1] = true;
+		// kill the child
+		if (kill(pid, SIGKILL) != 0) {
+			FAIL();
+		}
 	}
 }
 
 TEST_CASE("Test read lock with multiple processes", "[persistence]") {
-	// shared memory to keep track of which processes are done
-	bool *finished = (bool *)mmap(NULL, sizeof(bool) * BOOL_COUNT, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
-	memset(finished, 0, BOOL_COUNT * sizeof(bool));
+	uint64_t *count = (uint64_t *)mmap(NULL, sizeof(uint64_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+	*count = 0;
 
 	string dbdir = TestCreatePath("readlocktest");
 	DeleteDatabase(dbdir);
@@ -74,21 +75,21 @@ TEST_CASE("Test read lock with multiple processes", "[persistence]") {
 		// open db for reading
 		DuckDB db(dbdir, &config);
 		Connection con(db);
-		// opened db for reading
-		finished[0] = true;
+
+		(*count)++;
 		// query some values
-		con.Query("SELECT * FROM a");
 		con.Query("SELECT i+2 FROM a");
-		// wait for parent to finish
-		while (!finished[1]);
-		finished[2] = true;
-		// exit
-		exit(0);
+		while(true) {
+			usleep(100);
+			con.Query("SELECT * FROM a");
+		}
 	} else if (pid > 0) {
 		unique_ptr<DuckDB> db;
 		// parent process
-		// wait for child to open db for writing
-		while(!finished[0]);
+		// sleep a bit to wait for child process
+		while(*count == 0) {
+			usleep(100);
+		}
 		// try to open db for writing, this should fail
 		REQUIRE_THROWS(db = make_unique<DuckDB>(dbdir));
 		// but opening db for reading should work
@@ -96,8 +97,10 @@ TEST_CASE("Test read lock with multiple processes", "[persistence]") {
 		// we can query the database
 		Connection con(*db);
 		REQUIRE_NO_FAIL(con.Query("SELECT * FROM a"));
-		// now kill the child
-		finished[1] = true;
+		// kill the child
+		if (kill(pid, SIGKILL) != 0) {
+			FAIL();
+		}
 	}
 }
 
