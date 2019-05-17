534b06c97d6cdb7baf27d8cf61a17de33ae26145
Pedro Holanda <pedroholanda@gmail.com>
2019-05-17 10:34:20 +0200

    Removing order index

--- test/optimizer/test_index_scan_optimizer.cpp
+++ test/optimizer/test_index_scan_optimizer.cpp
@@ -18,19 +18,12 @@ TEST_CASE("Test Index Scan Optimizer for Integers", "[index-optimizer-int]") {
 	for (int idx = 0; idx < 1; idx++) {
 		REQUIRE_NO_FAIL(con.Query("CREATE TABLE integers(i " + int_types[idx] + ")"));
 		//! Checking Order Index
-		con.Query("CREATE INDEX i_index ON integers using order_index(i)");
+		con.Query("CREATE INDEX i_index ON integers(i)");
 		// Checking if Optimizer is using index in simple case
 		auto tree = helper.ParseLogicalTree("SELECT i FROM integers where i > CAST(10 AS " + int_types[idx] + ")");
 		IndexScan index_scan;
 		auto plan = index_scan.Optimize(move(tree));
 		REQUIRE(plan->children[0]->type == LogicalOperatorType::INDEX_SCAN);
 		con.Query("DROP INDEX i_index");
-
-		//! Checking ART
-		con.Query("CREATE INDEX i_index ON integers using art(i)");
-		// Checking if Optimizer is using index in simple case
-		tree = helper.ParseLogicalTree("SELECT i FROM integers where i = 10");
-		plan = index_scan.Optimize(move(tree));
-		REQUIRE(plan->children[0]->type == LogicalOperatorType::INDEX_SCAN);
 	}
 }
