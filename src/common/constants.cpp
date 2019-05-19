0985eb6409e620b8036be38c96fd17f37f51f88b
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-05-19 10:39:47 +0200

    Place definitions of constants in cpp files

--- src/common/constants.cpp
+++ src/common/constants.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 namespace duckdb {
 
-column_t COLUMN_IDENTIFIER_ROW_ID = (column_t)-1;
-sel_t ZERO_VECTOR[STANDARD_VECTOR_SIZE] = {0};
+const index_t INVALID_INDEX = (index_t)-1;
+const column_t COLUMN_IDENTIFIER_ROW_ID = (column_t)-1;
+const sel_t ZERO_VECTOR[STANDARD_VECTOR_SIZE] = {0};
 
 } // namespace duckdb
