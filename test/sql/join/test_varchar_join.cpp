67a259b683374caa6b3953491c83d8f70e669ba7
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-06-06 12:01:06 +0200

    More tests for nulls in joins

--- test/sql/join/test_varchar_join.cpp
+++ test/sql/join/test_varchar_join.cpp
@@ -12,9 +12,18 @@ TEST_CASE("Test joins on VARCHAR columns with NULL values", "[join]") {
 	Connection con(db);
 	con.EnableQueryVerification();
 
+	result = con.Query("select * from (select NULL::varchar as b) sq1, (select 'asdf' as b) sq2 where sq1.b = sq2.b;");
+	REQUIRE(CHECK_COLUMN(result, 0, {}));
+
 	result = con.Query("select * from (select 42 as a, NULL::varchar as b) sq1, (select 42 as a, 'asdf' as b) sq2 where sq1.b <> sq2.b;");
 	REQUIRE(CHECK_COLUMN(result, 0, {}));
 
 	result = con.Query("select * from (select 42 as a, NULL::varchar as b) sq1, (select 42 as a, 'asdf' as b) sq2 where sq1.a=sq2.a and sq1.b <> sq2.b;");
+	REQUIRE(CHECK_COLUMN(result, 0, {}));
+
+	result = con.Query("select * from (select 42 as a, 'asdf' as b) sq2, (select 42 as a, NULL::varchar as b) sq1 where sq1.b <> sq2.b;");
+	REQUIRE(CHECK_COLUMN(result, 0, {}));
+
+	result = con.Query("select * from (select 42 as a, 'asdf' as b) sq2, (select 42 as a, NULL::varchar as b) sq1 where sq1.a=sq2.a and sq1.b <> sq2.b;");
 	REQUIRE(CHECK_COLUMN(result, 0, {}));
 }
