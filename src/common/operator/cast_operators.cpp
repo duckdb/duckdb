15ddeac0f927ade76d55bcc93b0530949541ef36
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-06-13 17:00:48 +0200

    Rework casting to/from integer values

--- src/common/operator/cast_operators.cpp
+++ src/common/operator/cast_operators.cpp
@@ -12,30 +12,41 @@ using namespace std;
 
 namespace duckdb {
 
-template <class SRC, class DST> static DST cast_with_overflow_check(SRC value) {
+template <class SRC, class DST> static bool try_cast_with_overflow_check(SRC value, DST &result) {
 	if (value < MinimumValue<DST>() || value > MaximumValue<DST>()) {
-		throw ValueOutOfRangeException((int64_t)value, GetTypeId<SRC>(), GetTypeId<DST>());
+		return false;
 	}
-	return (DST)value;
+	result = (DST) value;
+	return true;
 }
 
-template <class SRC> static uint64_t cast_to_uint64_overflow_check(SRC value) {
-	if (value < 0) {
-		throw ValueOutOfRangeException((int64_t)value, GetTypeId<SRC>(), TypeId::POINTER);
+template <class SRC, class DST> static DST cast_with_overflow_check(SRC value) {
+	DST result;
+	if (!try_cast_with_overflow_check<SRC, DST>(value, result)) {
+		throw ValueOutOfRangeException((int64_t) value, GetTypeId<SRC>(), GetTypeId<DST>());
 	}
-	return (uint64_t)value;
-}
-
-template <class DST> static DST cast_from_uint64_overflow_check(uint64_t value) {
-	if (value > (uint64_t)MaximumValue<DST>()) {
-		throw ValueOutOfRangeException((int64_t)value, TypeId::POINTER, GetTypeId<DST>());
-	}
-	return (DST)value;
+	return result;
 }
 
 //===--------------------------------------------------------------------===//
 // Numeric -> int8_t casts
 //===--------------------------------------------------------------------===//
+template <> bool TryCast::Operation(int16_t left, int8_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(int32_t left, int8_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(int64_t left, int8_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(float left, int8_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(double left, int8_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+
 template <> int8_t Cast::Operation(int16_t left) {
 	return cast_with_overflow_check<int16_t, int8_t>(left);
 }
@@ -45,8 +56,8 @@ template <> int8_t Cast::Operation(int32_t left) {
 template <> int8_t Cast::Operation(int64_t left) {
 	return cast_with_overflow_check<int64_t, int8_t>(left);
 }
-template <> int8_t Cast::Operation(uint64_t left) {
-	return cast_from_uint64_overflow_check<int8_t>(left);
+template <> int8_t Cast::Operation(float left) {
+	return cast_with_overflow_check<float, int8_t>(left);
 }
 template <> int8_t Cast::Operation(double left) {
 	return cast_with_overflow_check<double, int8_t>(left);
@@ -54,14 +65,27 @@ template <> int8_t Cast::Operation(double left) {
 //===--------------------------------------------------------------------===//
 // Numeric -> int16_t casts
 //===--------------------------------------------------------------------===//
+template <> bool TryCast::Operation(int32_t left, int16_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(int64_t left, int16_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(float left, int16_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(double left, int16_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+
 template <> int16_t Cast::Operation(int32_t left) {
 	return cast_with_overflow_check<int32_t, int16_t>(left);
 }
 template <> int16_t Cast::Operation(int64_t left) {
 	return cast_with_overflow_check<int64_t, int16_t>(left);
 }
-template <> int16_t Cast::Operation(uint64_t left) {
-	return cast_from_uint64_overflow_check<int16_t>(left);
+template <> int16_t Cast::Operation(float left) {
+	return cast_with_overflow_check<float, int16_t>(left);
 }
 template <> int16_t Cast::Operation(double left) {
 	return cast_with_overflow_check<double, int16_t>(left);
@@ -69,11 +93,21 @@ template <> int16_t Cast::Operation(double left) {
 //===--------------------------------------------------------------------===//
 // Numeric -> int32_t casts
 //===--------------------------------------------------------------------===//
+template <> bool TryCast::Operation(int64_t left, int32_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(float left, int32_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(double left, int32_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+
 template <> int32_t Cast::Operation(int64_t left) {
 	return cast_with_overflow_check<int64_t, int32_t>(left);
 }
-template <> int32_t Cast::Operation(uint64_t left) {
-	return (int32_t)cast_from_uint64_overflow_check<uint64_t>(left);
+template <> int32_t Cast::Operation(float left) {
+	return cast_with_overflow_check<float, int32_t>(left);
 }
 template <> int32_t Cast::Operation(double left) {
 	return cast_with_overflow_check<double, int32_t>(left);
@@ -81,96 +115,128 @@ template <> int32_t Cast::Operation(double left) {
 //===--------------------------------------------------------------------===//
 // Numeric -> int64_t casts
 //===--------------------------------------------------------------------===//
-template <> int64_t Cast::Operation(uint64_t left) {
-	return cast_from_uint64_overflow_check<uint64_t>(left);
+template <> bool TryCast::Operation(float left, int64_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+template <> bool TryCast::Operation(double left, int64_t &result) {
+	return try_cast_with_overflow_check(left, result);
+}
+
+template <> int64_t Cast::Operation(float left) {
+	return cast_with_overflow_check<float, int64_t>(left);
 }
 template <> int64_t Cast::Operation(double left) {
 	return cast_with_overflow_check<double, int64_t>(left);
-}
-//===--------------------------------------------------------------------===//
-// Numeric -> uint64_t casts
-//===--------------------------------------------------------------------===//
-template <> uint64_t Cast::Operation(int8_t left) {
-	return cast_to_uint64_overflow_check<int8_t>(left);
-}
-template <> uint64_t Cast::Operation(int16_t left) {
-	return cast_to_uint64_overflow_check<int16_t>(left);
-}
-template <> uint64_t Cast::Operation(int32_t left) {
-	return cast_to_uint64_overflow_check<int32_t>(left);
-}
-template <> uint64_t Cast::Operation(int64_t left) {
-	return cast_to_uint64_overflow_check<int64_t>(left);
-}
-template <> uint64_t Cast::Operation(double left) {
-	return cast_to_uint64_overflow_check<double>(left);
 }
 
 //===--------------------------------------------------------------------===//
 // Cast String -> Numeric
 //===--------------------------------------------------------------------===//
+template<class T>
+static T try_cast_string(const char *left) {
+	T result;
+	if (!TryCast::Operation<const char*, T>(left, result)) {
+		throw ConversionException("Could not convert string '%s' to numeric", left);
+	}
+	return result;
+}
+
+template<class T>
+static bool TryIntegerCast(const char *buf, T &result) {
+	if (!*buf) {
+		return false;
+	}
+	int negative = *buf == '-';
+	size_t pos = negative;
+
+	result = 0;
+	if (!negative) {
+		while(buf[pos]) {
+			if (!std::isdigit(buf[pos])) {
+				return false;
+			}
+			T digit = buf[pos++] - '0';
+			if (result > (MaximumValue<T>() - digit) / 10) {
+				return false;
+			}
+			result = result * 10 + digit;
+		}
+	} else {
+		while(buf[pos]) {
+			if (!std::isdigit(buf[pos])) {
+				return false;
+			}
+			T digit = buf[pos++] - '0';
+			if (result < (MinimumValue<T>() + digit) / 10) {
+				return false;
+			}
+			result = result * 10 - digit;
+		}
+	}
+	return true;
+}
+
+template <> bool TryCast::Operation(const char *left, bool &result) {
+	if (left[0] == 't' || left[0] == 'T') {
+		result = true;
+	} else if (left[0] == 'f' || left[0] == 'F') {
+		result = false;
+	} else {
+		return false;
+	}
+	return true;
+}
+template <> bool TryCast::Operation(const char *left, int8_t &result) {
+	return TryIntegerCast<int8_t>(left, result);
+}
+template <> bool TryCast::Operation(const char *left, int16_t &result) {
+	return TryIntegerCast<int16_t>(left, result);
+}
+template <> bool TryCast::Operation(const char *left, int32_t &result) {
+	return TryIntegerCast<int32_t>(left, result);
+}
+template <> bool TryCast::Operation(const char *left, int64_t &result) {
+	return TryIntegerCast<int64_t>(left, result);
+}
+template <> bool TryCast::Operation(const char *left, float &result) {
+	// FIXME: don't use stod but implement own function
+	try {
+		result = (float)stod(left, NULL);
+		return true;
+	} catch (...) {
+		return false;
+	}
+}
+template <> bool TryCast::Operation(const char *left, double &result) {
+	// FIXME: don't use stod but implement own function
+	try {
+		result = stod(left, NULL);
+		return true;
+	} catch (...) {
+		return false;
+	}
+}
 
 template <> bool Cast::Operation(const char *left) {
-	if (left[0] == 't' || left[0] == 'T') {
-		return true;
-	} else if (left[0] == 'f' || left[0] == 'F') {
-		return false;
-	} else {
-		throw ConversionException("Could not convert string '%s' to boolean", left);
-	}
+	return try_cast_string<bool>(left);
 }
-
 template <> int8_t Cast::Operation(const char *left) {
-	int64_t value = Cast::Operation<const char *, int64_t>(left);
-	if (in_bounds<int8_t>(value))
-		return (int8_t)value;
-	throw ValueOutOfRangeException(value, TypeId::VARCHAR, TypeId::TINYINT);
+	return try_cast_string<int8_t>(left);
 }
-
 template <> int16_t Cast::Operation(const char *left) {
-	int64_t value = Cast::Operation<const char *, int64_t>(left);
-	if (in_bounds<int16_t>(value))
-		return (int16_t)value;
-	throw ValueOutOfRangeException(value, TypeId::VARCHAR, TypeId::SMALLINT);
+	return try_cast_string<int16_t>(left);
 }
-
-template <> int Cast::Operation(const char *left) {
-	int64_t value = Cast::Operation<const char *, int64_t>(left);
-	if (in_bounds<int32_t>(value))
-		return (int32_t)value;
-	throw ValueOutOfRangeException(value, TypeId::VARCHAR, TypeId::INTEGER);
+template <> int32_t Cast::Operation(const char *left) {
+	return try_cast_string<int32_t>(left);
 }
-
 template <> int64_t Cast::Operation(const char *left) {
-	try {
-		return stoll(left, NULL, 10);
-	} catch (...) {
-		throw ConversionException("Could not convert string '%s' to numeric", left);
-	}
+	return try_cast_string<int64_t>(left);
 }
-
-template <> uint64_t Cast::Operation(const char *left) {
-	try {
-		return stoull(left, NULL, 10);
-	} catch (...) {
-		throw ConversionException("Could not convert string '%s' to numeric", left);
-	}
-}
-
 template <> float Cast::Operation(const char *left) {
-	try {
-		return (float)stod(left, NULL);
-	} catch (...) {
-		throw ConversionException("Could not convert string '%s' to numeric", left);
-	}
+	return try_cast_string<float>(left);
 }
-
 template <> double Cast::Operation(const char *left) {
-	try {
-		return stod(left, NULL);
-	} catch (...) {
-		throw ConversionException("Could not convert string '%s' to numeric", left);
-	}
+	return try_cast_string<double>(left);
 }
 
 //===--------------------------------------------------------------------===//
---
+++ test/common/test_cast.cpp
@@ -0,0 +1,147 @@
+#include "catch.hpp"
+#include "common/operator/cast_operators.hpp"
+
+#include <vector>
+
+using namespace duckdb;
+using namespace std;
+
+template<class SRC, class DST>
+static void TestNumericCast(vector<SRC> &working_values, vector<SRC> &broken_values) {
+	DST result;
+	for(auto value : working_values) {
+		REQUIRE_NOTHROW(Cast::Operation<SRC, DST>(value) == (DST) value);
+		REQUIRE(TryCast::Operation<SRC, DST>(value, result));
+		REQUIRE(result == (DST) value);
+	}
+	for(auto value : broken_values) {
+		REQUIRE_THROWS(Cast::Operation<SRC, DST>(value));
+		REQUIRE(!TryCast::Operation<SRC, DST>(value, result));
+	}
+}
+
+template<class DST>
+static void TestStringCast(vector<string> &working_values, vector<DST> &expected_values, vector<string> &broken_values) {
+	DST result;
+	for(index_t i = 0; i < working_values.size(); i++) {
+		auto &value = working_values[i];
+		auto expected_value = expected_values[i];
+		REQUIRE_NOTHROW(Cast::Operation<const char*, DST>(value.c_str()) == expected_value);
+		REQUIRE(TryCast::Operation<const char*, DST>(value.c_str(), result));
+		REQUIRE(result == expected_value);
+
+		// cast back and compare
+		REQUIRE(Cast::Operation<DST, string>(result) == value);
+	}
+	for(auto &value : broken_values) {
+		REQUIRE_THROWS(Cast::Operation<const char*, DST>(value.c_str()));
+		REQUIRE(!TryCast::Operation<const char*, DST>(value.c_str(), result));
+	}
+}
+
+TEST_CASE("Test casting to boolean", "[cast]") {
+	vector<string> working_values     = { "true", "false", "TRUE", "FALSE", "T", "F" };
+	vector<int8_t> expected_values    = { true, false, true, false, true, false };
+	vector<string> broken_values = { "1", "blabla", "", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa" };
+
+	bool result;
+	for(index_t i = 0; i < working_values.size(); i++) {
+		auto &value = working_values[i];
+		auto expected_value = expected_values[i];
+		REQUIRE_NOTHROW(Cast::Operation<const char*, bool>(value.c_str()) == expected_value);
+		REQUIRE(TryCast::Operation<const char*, bool>(value.c_str(), result));
+		REQUIRE(result == expected_value);
+	}
+	for(auto &value : broken_values) {
+		REQUIRE_THROWS(Cast::Operation<const char*, bool>(value.c_str()));
+		REQUIRE(!TryCast::Operation<const char*, bool>(value.c_str(), result));
+	}
+}
+
+TEST_CASE("Test casting to int8_t", "[cast]") {
+	// int16_t -> int8_t
+	vector<int16_t> working_values_int16     = { 10, -10, 127, -127 };
+	vector<int16_t> broken_values_int16 = { 128, -128, 1000, -1000 };
+	TestNumericCast<int16_t, int8_t>(working_values_int16, broken_values_int16);
+	// int32_t -> int8_t
+	vector<int32_t> working_values_int32     = { 10, -10, 127, -127 };
+	vector<int32_t> broken_values_int32 = { 128, -128, 1000000, -1000000 };
+	TestNumericCast<int32_t, int8_t>(working_values_int32, broken_values_int32);
+	// int64_t -> int8_t
+	vector<int64_t> working_values_int64     = { 10, -10, 127, -127 };
+	vector<int64_t> broken_values_int64 = { 128, -128, 10000000000, -10000000000 };
+	TestNumericCast<int64_t, int8_t>(working_values_int64, broken_values_int64);
+	// float -> int8_t
+	vector<float> working_values_float = { 10, -10, 127, -127, 1.3, -2.7 };
+	vector<float> broken_values_float = { 128, -128, 10000000000, -10000000000, 1e30f, -1e30f };
+	TestNumericCast<float, int8_t>(working_values_float, broken_values_float);
+	// double -> int8_t
+	vector<double> working_values_double = { 10, -10, 127, -127, 1.3, -2.7 };
+	vector<double> broken_values_double = { 128, -128, 10000000000, -10000000000, 1e100, -1e100 };
+	TestNumericCast<double, int8_t>(working_values_double, broken_values_double);
+	// string -> int8_t
+	vector<string> working_values_str     = { "10", "-10", "127", "-127" };
+	vector<int8_t> expected_values_str    = { 10, -10, 127, -127 };
+	vector<string> broken_values_str = { "128", "-128", "10000000000000000000000000000000000000000000000000000000000000", "1.3", "aaaa", "19A", "" };
+	TestStringCast<int8_t>(working_values_str, expected_values_str, broken_values_str);
+}
+
+TEST_CASE("Test casting to int16_t", "[cast]") {
+	// int32_t -> int16_t
+	vector<int32_t> working_values_int32     = { 10, -10, 127, -127, 32767, -32767 };
+	vector<int32_t> broken_values_int32 = { 32768, -32768, 1000000, -1000000 };
+	TestNumericCast<int32_t, int16_t>(working_values_int32, broken_values_int32);
+	// int64_t -> int16_t
+	vector<int64_t> working_values_int64     = { 10, -10, 127, -127,  32767, -32767 };
+	vector<int64_t> broken_values_int64 = { 32768, -32768, 10000000000, -10000000000 };
+	TestNumericCast<int64_t, int16_t>(working_values_int64, broken_values_int64);
+	// float -> int16_t
+	vector<float> working_values_float     = { 10.0f, -10.0f, 32767.0f, -32767.0f, 1.3f, -2.7f };
+	vector<float> broken_values_float = { 32768.0f, -32768.0f, 10000000000.0f, -10000000000.0f, 1e30f, -1e30f };
+	TestNumericCast<float, int16_t>(working_values_float, broken_values_float);
+	// double -> int16_t
+	vector<double> working_values_double     = { 10, -10, 32767, -32767, 1.3, -2.7 };
+	vector<double> broken_values_double = { 32768, -32768, 10000000000, -10000000000, 1e100, -1e100 };
+	TestNumericCast<double, int16_t>(working_values_double, broken_values_double);
+	// string -> int16_t
+	vector<string> working_values_str     = { "10", "-10", "32767", "-32767" };
+	vector<int16_t> expected_values_str    = { 10, -10, 32767, -32767 };
+	vector<string> broken_values_str = { "32768", "-32768", "10000000000000000000000000000000000000000000000000000000000000", "1.3", "aaaa", "19A", "" };
+	TestStringCast<int16_t>(working_values_str, expected_values_str, broken_values_str);
+}
+
+TEST_CASE("Test casting to int32_t", "[cast]") {
+	// int64_t -> int32_t
+	vector<int64_t> working_values_int64     = { 10, -10, 127, -127,  32767, -32767, 2147483647, -2147483647 };
+	vector<int64_t> broken_values_int64 = { 2147483648, -2147483648, 10000000000, -10000000000 };
+	TestNumericCast<int64_t, int32_t>(working_values_int64, broken_values_int64);
+	// float -> int32_t
+	vector<float> working_values_float     = { 10.0f, -10.0f, 2000000000.0f, -2000000000.0f, 1.3f, -2.7f };
+	vector<float> broken_values_float = { 3000000000.0f, -3000000000.0f, 10000000000.0f, -10000000000.0f, 1e30f, -1e30f };
+	TestNumericCast<float, int32_t>(working_values_float, broken_values_float);
+	// double -> int32_t
+	vector<double> working_values_double     = { 10, -10, 32767, -32767, 1.3, -2.7, 2147483647, -2147483647 };
+	vector<double> broken_values_double = { 2147483648, -2147483648, 10000000000, -10000000000, 1e100, -1e100 };
+	TestNumericCast<double, int32_t>(working_values_double, broken_values_double);
+	// string -> int32_t
+	vector<string> working_values_str     = { "10", "-10", "2147483647", "-2147483647" };
+	vector<int32_t> expected_values_str    = { 10, -10, 2147483647, -2147483647 };
+	vector<string> broken_values_str = { "2147483648", "-2147483648", "10000000000000000000000000000000000000000000000000000000000000", "1.3", "aaaa", "19A", "" };
+	TestStringCast<int32_t>(working_values_str, expected_values_str, broken_values_str);
+}
+
+TEST_CASE("Test casting to int64_t", "[cast]") {
+	// float -> int64_t
+	vector<float> working_values_float     = { 10.0f, -10.0f, 32767.0f, -32767.0f, 1.3, -2.7, 2000000000.0f, -2000000000.0f, 4000000000000000000.0f, -4000000000000000000.0f };
+	vector<float> broken_values_float = { 20000000000000000000.0f, -20000000000000000000.0f, 1e30f, -1e30f };
+	TestNumericCast<float, int64_t>(working_values_float, broken_values_float);
+	// double -> int64_t
+	vector<double> working_values_double     = { 10, -10, 32767, -32767, 1.3, -2.7, 2147483647, -2147483647, 4611686018427387904, -4611686018427387904 };
+	vector<double> broken_values_double = { 18446744073709551616.0, -18446744073709551616.0, 1e100, -1e100 };
+	TestNumericCast<double, int64_t>(working_values_double, broken_values_double);
+	// string -> int64_t
+	vector<string> working_values_str     = { "10", "-10", "9223372036854775807", "-9223372036854775807" };
+	vector<int64_t> expected_values_str    = { 10, -10, 9223372036854775807LL, -9223372036854775807LL };
+	vector<string> broken_values_str = { "9223372036854775808", "-9223372036854775808", "10000000000000000000000000000000000000000000000000000000000000", "1.3", "aaaa", "19A", ""};
+	TestStringCast<int64_t>(working_values_str, expected_values_str, broken_values_str);
+}
