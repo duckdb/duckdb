29e86bbd83b04345dd089acc2bf65534ab8eabcb
Hannes Mühleisen <hannes@muehleisen.org>
2019-06-27 10:38:27 +0200

    third attempt in binary upload: WebDAV

--- tools/upload-s3.py
+++ tools/upload-s3.py
@@ -1,7 +1,6 @@
 import subprocess
 import sys
 import os
-import ftplib
 
 if (len(sys.argv) < 3):
     print("Usage: [prefix] [filename1] [filename2] ... ")
@@ -16,30 +15,42 @@ prefix = sys.argv[1].strip()
 # Hannes controls this web server
 # Files are served at https://download.duckdb.org/...
 
-secret_key=os.getenv('FTP_PASSWORD')
+secret_key=os.getenv('DAV_PASSWORD')
 if secret_key is None:
-    print("Can't find FTP_PASSWORD in env ")
+    print("Can't find DAV_PASSWORD in env ")
     exit(2)
-
-ftp = ftplib.FTP_TLS('wp10635776.server-he.de','ftp10635776-duckdb',secret_key)
-ftp.set_debuglevel(1)
-ftp.set_pasv(True)
 
 
 files = sys.argv[2:]
 basenames = [os.path.basename(x) for x in files]
-
 
 basenames_set = set(basenames)
 if len(basenames) != len(basenames_set):
     print("Name conflict")
     exit(3)
 
-folder = 'rev/%s/%s' % (git_rev_hash(), prefix)
 
+git_hash = git_rev_hash()
+
+folder = 'rev/%s/%s' % (git_hash, prefix)
+
+def curlcmd(cmd, path):
+    p = subprocess.Popen(['curl','--retry', '10'] + cmd + ['http://duckdb:%s@dav10635776.mywebdav.de/duckdb-download/%s' % (secret_key, path)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate()
+    rc = p.returncode
+
+    if (p.returncode != 0):
+        print(err)
+        exit(4)
+
+# create dirs, no recursive create supported by webdav
+f_p = ''
+for p in folder.split('/'):
+    f_p += p + '/'
+    curlcmd(['-X','MKCOL'], f_p)
+    
 for f in files:
     base = os.path.basename(f)
     key = '%s/%s' % (folder, base)
     print("%s\t->\thttps://download.duckdb.org/%s " % (f, key))
-
-    subprocess.call(['curl', '-s','--ftp-create-dirs', '--ftp-pasv','--ftp-method', 'multicwd','-m','60', '--retry', '10', '-T', f, 'ftp://ftp10635776-duckdb:%s@wp10635776.server-he.de/%s' % (secret_key, key)])
+    curlcmd(['-T',f], key)
