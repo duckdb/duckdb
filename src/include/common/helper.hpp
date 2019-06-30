e4c0b734aa580064dc57501114f857aaab2a7039
Uwe L. Korn <uwelk@xhochy.com>
2019-06-30 18:11:44 +0200

    Support building with -std=c++14

--- src/include/common/helper.hpp
+++ src/include/common/helper.hpp
@@ -19,7 +19,7 @@
 #endif
 
 namespace duckdb {
-#ifndef _MSC_VER
+#if !defined(_MSC_VER) && (__cplusplus < 201402L)
 template <typename T, typename... Args> unique_ptr<T> make_unique(Args &&... args) {
 	return unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
