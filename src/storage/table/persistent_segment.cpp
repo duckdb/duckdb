09d95ffd77b9eb8f4de173d15d67ee200d018eb3
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-07-09 11:44:13 +0200

    Revert "Start of separation of Transient and Persistent chunks in storage"
    This reverts commit 208954b3fcb4827af87ca29d8d73318f3f7baa05.

--- src/storage/table/persistent_segment.cpp
+++
@@ -1,8 +0,0 @@
-#include "storage/table/persistent_segment.hpp"
-
-using namespace duckdb;
-using namespace std;
-
-PersistentSegment::PersistentSegment(BlockManager &manager, block_id_t block_id, index_t offset, index_t count, index_t start) :
-    ColumnSegment(ColumnSegmentType::PERSISTENT, start, count), manager(manager), block_id(block_id), offset(offset) {
-}
--- src/storage/table/transient_segment.cpp
+++
@@ -1,9 +0,0 @@
-#include "storage/table/transient_segment.hpp"
-
-using namespace duckdb;
-using namespace std;
-
-TransientSegment::TransientSegment(index_t start)
-    : ColumnSegment(ColumnSegmentType::TRANSIENT, start, 0), block(INVALID_BLOCK) {
-
-}
