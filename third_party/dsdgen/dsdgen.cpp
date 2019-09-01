0a9b87e811c6bdc1438ac6f85a3c8610cab27950
Hannes Mühleisen <hannes@muehleisen.org>
2019-09-01 16:48:10 +0200

    closing appenders in tpcds dbgen

--- third_party/dsdgen/dsdgen.cpp
+++ third_party/dsdgen/dsdgen.cpp
@@ -78,6 +78,7 @@ void dbgen(double flt_scale, DuckDB &db, string schema, string suffix) {
 	// flush any incomplete chunks
 	for (int table_id = tmin; table_id < tmax; table_id++) {
 		append_info[table_id]->appender.Flush();
+		append_info[table_id]->connection.CloseAppender();
 	}
 }
 
