be83738e50a1f881d965e27c89b67efdd5a3e68f
Hannes Mühleisen <hannes@muehleisen.org>
2019-10-29 09:35:42 +0100

    added explicit check for exception support

--- src/include/duckdb.hpp
+++ src/include/duckdb.hpp
@@ -8,6 +8,10 @@
 
 #pragma once
 
+#ifndef __cpp_exceptions
+	#error "DuckDB requires C++ Exception support (e.g. -fexceptions in gcc)"
+#endif
+
 #include "main/connection.hpp"
 #include "main/database.hpp"
 #include "main/query_result.hpp"
