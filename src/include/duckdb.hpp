49dd85d8d67dfbfddcd7dbb10726f665f591990e
Hannes Mühleisen <hannes@muehleisen.org>
2019-10-31 15:09:34 +0100

    :/

--- src/include/duckdb.hpp
+++ src/include/duckdb.hpp
@@ -8,10 +8,6 @@
 
 #pragma once
 
-#if !defined(__cpp_exceptions)
-	#error "DuckDB requires C++ Exception support(-fexceptions in gcc)"
-#endif
-
 #include "main/connection.hpp"
 #include "main/database.hpp"
 #include "main/query_result.hpp"
