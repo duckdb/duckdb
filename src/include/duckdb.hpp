5f8472ac424c25859b3621567a0e4ba2be345553
Hannes Mühleisen <hannes@muehleisen.org>
2019-11-05 12:39:33 +0100

    no defines check because gcc dropped the ball

--- src/include/duckdb.hpp
+++ src/include/duckdb.hpp
@@ -8,10 +8,6 @@
 
 #pragma once
 
-#if !defined(__cpp_exceptions) || !defined(__cpp_rtti)
-	#error "DuckDB requires C++ Exceptions and Run-Time Type Information (-fexceptions -frtti in gcc)"
-#endif
-
 #include "main/connection.hpp"
 #include "main/database.hpp"
 #include "main/query_result.hpp"
