5234a153bed375e8f153d247d652d7dacfe855ca
Hannes Mühleisen <hannes@muehleisen.org>
2019-11-11 15:10:06 +0100

    test case for multi read only db

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,1 +1,1 @@
-install.packages(c("DBI", "DBItest", "testthat", "dbplyr", "RSQLite"), repos=c("http://cran.rstudio.com/"))
+install.packages(c("DBI", "DBItest", "testthat", "dbplyr", "RSQLite", "callr"), repos=c("http://cran.rstudio.com/"))
