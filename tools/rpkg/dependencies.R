478c73c611384680f2c9efd305f2a377d2ec725b
Hannes Mühleisen <hannes@muehleisen.org>
2019-12-06 12:46:59 +0100

    forcing binary packages to install on windows because rsqlite depends on boost. cautionary tale.

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,1 +1,6 @@
-install.packages(c("DBI", "DBItest", "testthat", "dbplyr", "RSQLite", "callr", "nycflights13"), repos=c("http://cran.rstudio.com/"))
+type <- "both"
+if (.Platform$OS.type == "windows") {
+	message("Forcing binary package installs on Windows :/")
+	type <- "binary"
+}
+install.packages(c("DBI", "DBItest", "testthat", "dbplyr", "RSQLite", "callr", "nycflights13"), repos=c("http://cran.rstudio.com/"), type=type)
