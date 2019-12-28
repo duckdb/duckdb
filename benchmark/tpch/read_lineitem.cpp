a0c404d1ad0ec5ef679d72bd45f5694a97952658
Mark Raasveldt <mark.raasveldt@gmail.com>
2019-12-28 10:55:24 +0100

    Add benchmark for reading a CSV with multi-byte delimiters

--- benchmark/tpch/read_lineitem.cpp
+++ benchmark/tpch/read_lineitem.cpp
@@ -46,6 +46,44 @@ string BenchmarkInfo() override {
 }
 FINISH_BENCHMARK(ReadLineitemCSV)
 
+DUCKDB_BENCHMARK(ReadLineitemCSVUnicode, "[csv]")
+int64_t count = 0;
+void Load(DuckDBBenchmarkState *state) override {
+	// load the data into the tpch schema
+	state->conn.Query("CREATE SCHEMA tpch");
+	tpch::dbgen(SF, state->db, "tpch");
+	// create the CSV file
+	auto result = state->conn.Query("COPY tpch.lineitem TO 'lineitem_unicode.csv' DELIMITER '🦆' HEADER");
+	assert(result->success);
+	count = result->collection.chunks[0]->data[0].GetValue(0).GetNumericValue();
+	// delete the database
+	state->conn.Query("DROP SCHEMA tpch CASCADE");
+	// create the empty schema to load into
+	tpch::dbgen(0, state->db);
+}
+string GetQuery() override {
+	return "COPY lineitem FROM 'lineitem_unicode.csv' DELIMITER '🦆' HEADER";
+}
+void Cleanup(DuckDBBenchmarkState *state) override {
+	state->conn.Query("DROP TABLE lineitem");
+	tpch::dbgen(0, state->db);
+}
+string VerifyResult(QueryResult *result) override {
+	if (!result->success) {
+		return result->error;
+	}
+	auto &materialized = (MaterializedQueryResult &)*result;
+	auto expected_count = materialized.collection.chunks[0]->data[0].GetValue(0).GetNumericValue();
+	if (expected_count != count) {
+		return StringUtil::Format("Count mismatch, expected %lld elements but got %lld", count, expected_count);
+	}
+	return string();
+}
+string BenchmarkInfo() override {
+	return "Read the lineitem table from SF 0.1 from CSV format";
+}
+FINISH_BENCHMARK(ReadLineitemCSVUnicode)
+
 DUCKDB_BENCHMARK(WriteLineitemCSV, "[csv]")
 void Load(DuckDBBenchmarkState *state) override {
 	// load the data into the tpch schema
