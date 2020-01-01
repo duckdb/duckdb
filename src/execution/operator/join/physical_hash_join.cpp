d9a40fa733c9b7365653cb5d4c4cfd7fd382e14c
Mark Raasveldt <mark.raasveldt@gmail.com>
2020-01-01 22:35:09 +0100

    Add cache after hash table probe that re-probes and caches if there are too few matches to avoid outputting small chunks

--- src/execution/operator/join/physical_hash_join.cpp
+++ src/execution/operator/join/physical_hash_join.cpp
@@ -13,6 +13,7 @@ public:
 	}
 
 	bool initialized;
+	DataChunk cached_chunk;
 	DataChunk join_keys;
 	unique_ptr<JoinHashTable::ScanStructure> scan_structure;
 };
@@ -26,38 +27,39 @@ PhysicalHashJoin::PhysicalHashJoin(LogicalOperator &op, unique_ptr<PhysicalOpera
 	children.push_back(move(right));
 }
 
-void PhysicalHashJoin::GetChunkInternal(ClientContext &context, DataChunk &chunk, PhysicalOperatorState *state_) {
+void PhysicalHashJoin::BuildHashTable(ClientContext &context, PhysicalOperatorState *state_) {
 	auto state = reinterpret_cast<PhysicalHashJoinState *>(state_);
-	if (!state->initialized) {
+
+	// build the HT
+	auto right_state = children[1]->GetOperatorState();
+	auto types = children[1]->GetTypes();
+
+	DataChunk right_chunk;
+	right_chunk.Initialize(types);
+
+	state->join_keys.Initialize(hash_table->condition_types);
+	while (true) {
+		// get the child chunk
+		children[1]->GetChunk(context, right_chunk, right_state.get());
+		if (right_chunk.size() == 0) {
+			break;
+		}
+		// resolve the join keys for the right chunk
+		state->rhs_executor.Execute(right_chunk, state->join_keys);
+
 		// build the HT
-		auto right_state = children[1]->GetOperatorState();
-		auto types = children[1]->GetTypes();
-
-		DataChunk right_chunk;
-		right_chunk.Initialize(types);
-
-		state->join_keys.Initialize(hash_table->condition_types);
-		while (true) {
-			// get the child chunk
-			children[1]->GetChunk(context, right_chunk, right_state.get());
-			if (right_chunk.size() == 0) {
-				break;
-			}
-			// resolve the join keys for the right chunk
-			state->rhs_executor.Execute(right_chunk, state->join_keys);
-
-			// build the HT
-			hash_table->Build(state->join_keys, right_chunk);
-		}
-
-		if (hash_table->size() == 0 &&
-		    (hash_table->join_type == JoinType::INNER || hash_table->join_type == JoinType::SEMI)) {
-			// empty hash table with INNER or SEMI join means empty result set
-			return;
-		}
-
-		state->initialized = true;
+		hash_table->Build(state->join_keys, right_chunk);
 	}
+
+	if (hash_table->size() == 0 &&
+		(hash_table->join_type == JoinType::INNER || hash_table->join_type == JoinType::SEMI)) {
+		// empty hash table with INNER or SEMI join means empty result set
+		return;
+	}
+}
+
+void PhysicalHashJoin::ProbeHashTable(ClientContext &context, DataChunk &chunk, PhysicalOperatorState *state_) {
+	auto state = reinterpret_cast<PhysicalHashJoinState *>(state_);
 	if (state->child_chunk.size() > 0 && state->scan_structure) {
 		// still have elements remaining from the previous probe (i.e. we got
 		// >1024 elements in the previous probe)
@@ -124,4 +126,44 @@ void PhysicalHashJoin::GetChunkInternal(ClientContext &context, DataChunk &chunk
 
 unique_ptr<PhysicalOperatorState> PhysicalHashJoin::GetOperatorState() {
 	return make_unique<PhysicalHashJoinState>(children[0].get(), children[1].get(), conditions);
+}
+
+void PhysicalHashJoin::GetChunkInternal(ClientContext &context, DataChunk &chunk, PhysicalOperatorState *state_) {
+	auto state = reinterpret_cast<PhysicalHashJoinState *>(state_);
+	if (!state->initialized) {
+		state->cached_chunk.Initialize(types);
+		BuildHashTable(context, state_);
+		state->initialized = true;
+	}
+	do {
+		ProbeHashTable(context, chunk, state);
+		if (chunk.size() == 0) {
+			if (state->cached_chunk.size() > 0) {
+				// finished probing but cached data remains, return cached chunk
+				for(index_t col_idx = 0; col_idx < chunk.column_count; col_idx++) {
+					state->cached_chunk.data[col_idx].Move(chunk.data[col_idx]);
+				}
+				chunk.sel_vector = state->cached_chunk.sel_vector;
+				state->cached_chunk.Reset();
+			}
+			return;
+		} else if (chunk.size() < 64) {
+			// small chunk: add it to chunk cache and continue
+			state->cached_chunk.Append(chunk);
+			if (state->cached_chunk.size() >= (1024 - 64)) {
+				// chunk cache full: return it
+				for(index_t col_idx = 0; col_idx < chunk.column_count; col_idx++) {
+					state->cached_chunk.data[col_idx].Move(chunk.data[col_idx]);
+				}
+				chunk.sel_vector = state->cached_chunk.sel_vector;
+				state->cached_chunk.Reset();
+				return;
+			} else {
+				// chunk cache not full: probe again
+				chunk.Reset();
+			}
+		} else {
+			return;
+		}
+	} while(true);
 }
