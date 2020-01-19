7e8502b08ca456af09f5ddbbdda0cbb9e99182ba
Hannes Mühleisen <hannes@muehleisen.org>
2020-01-19 17:58:31 +0100

    Switching to stock SQLite shell (#388)
    We now build a statically linked command line interface `duckdb_cli` built from the stock SQLite shell.c sources.

--- src/function/table/sqlite_functions.cpp
+++ src/function/table/sqlite_functions.cpp
@@ -1,4 +1,10 @@
 #include "duckdb/function/table/sqlite_functions.hpp"
+#include "duckdb/parser/parsed_data/create_view_info.hpp"
+#include "duckdb/parser/query_node/select_node.hpp"
+#include "duckdb/parser/expression/star_expression.hpp"
+#include "duckdb/parser/tableref/table_function_ref.hpp"
+#include "duckdb/parser/expression/function_expression.hpp"
+#include "duckdb/catalog/catalog.hpp"
 
 using namespace std;
 
@@ -14,6 +20,22 @@ void BuiltinFunctions::RegisterSQLiteFunctions() {
 	                           SQLType::VARCHAR, SQLType(SQLTypeId::BOOLEAN)},
 	                          {"cid", "name", "type", "notnull", "dflt_value", "pk"}, pragma_table_info_init,
 	                          pragma_table_info, nullptr));
+
+	CreateViewInfo info;
+	info.schema = DEFAULT_SCHEMA;
+	info.view_name = "sqlite_master";
+	info.replace = true;
+
+	auto select = make_unique<SelectNode>();
+	select->select_list.push_back(make_unique<StarExpression>());
+	vector<unique_ptr<ParsedExpression>> children;
+
+	auto function = make_unique<FunctionExpression>(DEFAULT_SCHEMA, "sqlite_master", children);
+	auto function_expr = make_unique<TableFunctionRef>();
+	function_expr->function = move(function);
+	select->from_table = move(function_expr);
+	info.query = move(select);
+//	catalog.CreateView(transaction, &info);
 }
 
 } // namespace duckdb
