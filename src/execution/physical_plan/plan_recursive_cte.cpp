cdaa0ee2739027f7c46c219f50745cdeb79eabe2
Denis Hirn <denis.hirn@uni-tuebingen.de>
2020-01-28 11:41:14 +0100

    Use ChunkScan instead of CTEScan

--- src/execution/physical_plan/plan_recursive_cte.cpp
+++ src/execution/physical_plan/plan_recursive_cte.cpp
@@ -30,10 +30,9 @@ unique_ptr<PhysicalOperator> PhysicalPlanGenerator::CreatePlan(LogicalRecursiveC
 unique_ptr<PhysicalOperator> PhysicalPlanGenerator::CreatePlan(LogicalCTERef &op) {
     assert(op.children.size() == 0);
 
-    auto chunk_scan = make_unique<PhysicalCTEScan>(op.types, PhysicalOperatorType::CTE_SCAN);
+    auto chunk_scan = make_unique<PhysicalChunkScan>(op.types, PhysicalOperatorType::CHUNK_SCAN);
 
     auto cte = rec_ctes.find(op.table_index);
     chunk_scan->collection = cte->second.first.get();
-    chunk_scan->remote_iteration = cte->second.second.get();
     return move(chunk_scan);
 }
