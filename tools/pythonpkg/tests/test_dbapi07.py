01e46e02344ed943b91c71f14f192c0acd48a50e
Hannes Mühleisen <hannes@muehleisen.org>
2020-03-08 09:46:07 +0100

    fixing datetime in fetchall()/fetchone()

--- tools/pythonpkg/tests/test_dbapi07.py
+++ tools/pythonpkg/tests/test_dbapi07.py
@@ -1,9 +1,16 @@
 # timestamp ms precision
 
 import numpy 
-
+from datetime import datetime
 
 class TestNumpyTimestampMilliseconds(object):
     def test_numpy_timestamp(self, duckdb_cursor):
-        res = duckdb_cursor.execute("SELECT TIMESTAMP '2020-01-10 00:00:00.500' as test_time").fetchnumpy()
-        assert res['test_time'] == numpy.datetime64('2020-01-10 00:00:00.500')
+        res = duckdb_cursor.execute("SELECT TIMESTAMP '2019-11-26 21:11:42.501' as test_time").fetchnumpy()
+        assert res['test_time'] == numpy.datetime64('2019-11-26 21:11:42.501')
+
+
+class TestTimestampMilliseconds(object):
+    def test_numpy_timestamp(self, duckdb_cursor):
+        res = duckdb_cursor.execute("SELECT TIMESTAMP '2019-11-26 21:11:42.501' as test_time").fetchone()[0]
+        print(res)
+        assert res == datetime.strptime('2019-11-26 21:11:42.501', '%Y-%m-%d %H:%M:%S.%f')
