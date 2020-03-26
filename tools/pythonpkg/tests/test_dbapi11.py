5b8744bbe033e094d7b8a210e6f08308c4e4e579
Hannes Mühleisen <hannes@muehleisen.org>
2020-03-26 18:38:44 +0100

    damn cursors

--- tools/pythonpkg/tests/test_dbapi11.py
+++ tools/pythonpkg/tests/test_dbapi11.py
@@ -18,22 +18,19 @@ class TestReadOnly(object):
         os.close(fd)
         os.remove(db)
 
+        # this is forbidden
         check_exception(lambda :duckdb.connect(":memory:", True))
 
-        # this is forbidden
-
-        con_rw = duckdb.connect(db, False).cursor()
-        con_rw.execute("create table a (i integer)")
-        con_rw.execute("insert into a values (42)")
+        con_rw = duckdb.connect(db, False)
+        con_rw.cursor().execute("create table a (i integer)")
+        con_rw.cursor().execute("insert into a values (42)")
         con_rw.close()
 
-
-        con_ro = duckdb.connect(db, True).cursor()
-        con_ro.execute("select * from a").fetchall()
+        con_ro = duckdb.connect(db, True)
+        con_ro.cursor().execute("select * from a").fetchall()
         check_exception(lambda : con_ro.execute("delete from a"))
         con_ro.close()
 
-
-        con_rw = duckdb.connect(db, False).cursor()
-        con_rw.execute("drop table a")
+        con_rw = duckdb.connect(db, False)
+        con_rw.cursor().execute("drop table a")
         con_rw.close()
