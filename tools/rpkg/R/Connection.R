a99b1ac6bf2a60f5b38f5e798b95445214281f90
Hannes Mühleisen <hannes@muehleisen.org>
2020-03-28 23:11:15 +0100

    Adding auto-create feature to R client: duckdb.read.csv/read_csv_duckdb (#479)
    * support for read_csv_duckdb (alias: duckdb.read.csv) to automagically create a schema from a CSV file and import said file

--- tools/rpkg/R/Connection.R
+++ tools/rpkg/R/Connection.R
@@ -298,3 +298,55 @@ setMethod("dbRollback", "duckdb_connection",
             dbExecute(conn, SQL("ROLLBACK"))
             invisible(TRUE)
           })
+
+
+read_csv_duckdb <- duckdb.read.csv <- function(conn, files, tablename, header=TRUE, na.strings="", nrow.check=500, 
+                                               delim=",", quote="\"", col.names=NULL, lower.case.names=FALSE, sep=delim, transaction=TRUE, ...){
+  
+  if (length(na.strings)>1) stop("na.strings must be of length 1")
+  if (!missing(sep)) delim <- sep
+
+  headers <- lapply(files, utils::read.csv, sep=delim, na.strings=na.strings, quote=quote, nrows=nrow.check, header=header, ...)
+  if (length(files)>1){
+    nn <- sapply(headers, ncol)
+    if (!all(nn==nn[1])) stop("Files have different numbers of columns")
+    nms <- sapply(headers, names)
+    if(!all(nms==nms[, 1])) stop("Files have different variable names")
+    types <- sapply(headers, function(df) sapply(df, dbDataType, dbObj=conn))
+    if(!all(types==types[, 1])) stop("Files have different variable types")
+  }
+  
+  if (transaction) {
+     dbBegin(conn)
+     on.exit(tryCatch(dbRollback(conn), error=function(e){}))
+  }
+
+  tablename <- dbQuoteIdentifier(conn, tablename)
+
+  if (!dbExistsTable(conn, tablename)) {
+    if(lower.case.names) names(headers[[1]]) <- tolower(names(headers[[1]]))
+    if(!is.null(col.names)) {
+      if (lower.case.names) {
+        warning("Ignoring lower.case.names parameter as overriding col.names are supplied.")
+      }
+      col.names <- as.character(col.names)
+      if (length(unique(col.names)) != length(names(headers[[1]]))) {
+        stop("You supplied ", length(unique(col.names)), " unique column names, but file has ", 
+          length(names(headers[[1]])), " columns.")
+      }
+      names(headers[[1]]) <-  col.names
+    }
+    dbWriteTable(conn, tablename, headers[[1]][FALSE, ,drop=FALSE])
+  }
+  
+  for(i in seq_along(files)) {
+    thefile <- dbQuoteString(conn, encodeString(normalizePath(files[i])))
+    dbExecute(conn, sprintf("COPY %s FROM %s (DELIMITER %s, QUOTE %s, HEADER %s, NULL %s)", tablename, thefile, dbQuoteString(conn, delim), dbQuoteString(conn,quote), tolower(header), dbQuoteString(conn, na.strings[1])))
+  }
+  dbGetQuery(conn, paste("SELECT COUNT(*) FROM", tablename))[[1]]
+ 
+  if (transaction) {
+    dbCommit(conn)
+    on.exit(NULL)
+  }
+}
