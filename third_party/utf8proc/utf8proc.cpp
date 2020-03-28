d1192a661ad2d882272f426314c9daf43ac97071
Hannes Mühleisen <hannes@muehleisen.org>
2020-03-28 08:10:45 +0100

    static utf8proc for windows

--- third_party/utf8proc/utf8proc.cpp
+++ third_party/utf8proc/utf8proc.cpp
@@ -1,14 +1,32 @@
 #include "utf8proc.hpp"
+#define UTF8PROC_STATIC
 #include "utf8proc.h"
 
 using namespace duckdb;
 using namespace std;
+
+// This function efficiently checks if a string is valid UTF8.
+// It was originally written by Sjoerd Mullender.
+
+// Here is the table that makes it work:
+
+// B 		= Number of Bytes in UTF8 encoding
+// C_MIN 	= First Unicode code point
+// C_MAX 	= Last Unicode code point
+// B1 		= First Byte Prefix
+
+// 	B	C_MIN		C_MAX		B1
+//	1	U+000000	U+00007F		0xxxxxxx
+//	2	U+000080	U+0007FF		110xxxxx
+//	3	U+000800	U+00FFFF		1110xxxx
+//	4	U+010000	U+10FFFF		11110xxx
 
 UnicodeType Utf8Proc::Analyze(const char *s, size_t len) {
 	UnicodeType type = UnicodeType::ASCII;
 	char c;
 	for (size_t i = 0; i < len; i++) {
 		c = s[i];
+		// 1 Byte / ASCII
 		if ((c & 0x80) == 0)
 			continue;
 		type = UnicodeType::UNICODE;
