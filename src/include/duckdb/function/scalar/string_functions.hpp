aecb19866d90697cd46c74681843b4bb7cc7b288
Tiago Kepe <tiagokepe@gmail.com>
2020-04-07 14:13:10 -0300

    Removed bind info structures of old contains functions.

--- src/include/duckdb/function/scalar/string_functions.hpp
+++ src/include/duckdb/function/scalar/string_functions.hpp
@@ -73,27 +73,6 @@ struct ContainsFun {
 	static void RegisterFunction(BuiltinFunctions &set);
 };
 
-struct ContainsKMPBindData : public FunctionData {
-	ContainsKMPBindData(std::unique_ptr<vector<uint32_t>> table, std::unique_ptr<string_t> pattern);
-	~ContainsKMPBindData();
-
-	std::unique_ptr<vector<uint32_t>> kmp_table;
-	std::unique_ptr<string_t> pattern;
-
-	unique_ptr<FunctionData> Copy() override;
-};
-
-struct ContainsBMBindData : public FunctionData {
-	ContainsBMBindData(std::unique_ptr<unordered_map<char, uint32_t>> table, std::unique_ptr<string_t> pattern);
-	~ContainsBMBindData();
-
-	std::unique_ptr<unordered_map<char, uint32_t>> bm_table;
-	std::unique_ptr<string_t> pattern;
-
-	unique_ptr<FunctionData> Copy() override;
-};
-
-
 struct RegexpMatchesBindData : public FunctionData {
 	RegexpMatchesBindData(std::unique_ptr<re2::RE2> constant_pattern, string range_min, string range_max,
 	                      bool range_success);
