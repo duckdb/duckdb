4a35047d1f4cd1ef9a3e279d86e635ad66d9633e
Tiago Kepe <tiagokepe@gmail.com>
2020-04-14 18:57:11 -0300

    Formatting...

--- src/optimizer/rule/like_optimizations.cpp
+++ src/optimizer/rule/like_optimizations.cpp
@@ -14,41 +14,41 @@ LikeOptimizationRule::LikeOptimizationRule(ExpressionRewriter &rewriter) : Rule(
 	auto func = make_unique<FunctionExpressionMatcher>();
 	func->matchers.push_back(make_unique<ConstantExpressionMatcher>());
 	func->matchers.push_back(make_unique<ExpressionMatcher>());
-    func->policy = SetMatcher::Policy::SOME;
-    // we only match on LIKE ("~~")
-    func->function = make_unique<SpecificFunctionMatcher>("~~");
+	func->policy = SetMatcher::Policy::SOME;
+	// we only match on LIKE ("~~")
+	func->function = make_unique<SpecificFunctionMatcher>("~~");
 	root = move(func);
 }
 
 unique_ptr<Expression> LikeOptimizationRule::Apply(LogicalOperator &op, vector<Expression *> &bindings,
-                                                     bool &changes_made) {
+                                                   bool &changes_made) {
 	auto root = (BoundFunctionExpression *)bindings[0];
 	auto constant_expr = (BoundConstantExpression *)bindings[1];
-    assert(root->children.size() == 2);
-    if(constant_expr->value.is_null) {
-    	return make_unique<BoundConstantExpression>(Value(root->return_type));
-    }
+	assert(root->children.size() == 2);
+	if (constant_expr->value.is_null) {
+		return make_unique<BoundConstantExpression>(Value(root->return_type));
+	}
 
 	// the constant_expr is a scalar expression that we have to fold
-	if(!constant_expr->IsFoldable()) {
-    	return move(root->Copy());
+	if (!constant_expr->IsFoldable()) {
+		return move(root->Copy());
 	}
 
 	auto constant_value = ExpressionExecutor::EvaluateScalar(*constant_expr);
-    assert(constant_value.type == constant_expr->return_type);
+	assert(constant_value.type == constant_expr->return_type);
 	string patt_str = string(((string_t)constant_value.str_value).GetData());
 
-	if( std::regex_match(patt_str, std::regex("[^%_]*[%]+")) ) {
+	if (std::regex_match(patt_str, std::regex("[^%_]*[%]+"))) {
 		//^^^^^^^^^^^^^^^Prefix LIKE pattern : [^%_]*[%]+, ignoring undescore
 
 		return move(ApplyRule(root, StringFunctionsType::PREFIX, patt_str));
 
-	} else if( std::regex_match(patt_str, std::regex("[%]+[^%_]*")) ) {
+	} else if (std::regex_match(patt_str, std::regex("[%]+[^%_]*"))) {
 		//^^^^^^^^^^^^^^^^^^^^^^^Suffix LIKE pattern: [%]+[^%_]*, ignoring undescore
 
 		return move(ApplyRule(root, StringFunctionsType::SUFFIX, patt_str));
 
-	} else if( std::regex_match(patt_str, std::regex("[%]+[^%_]*[%]+")) ) {
+	} else if (std::regex_match(patt_str, std::regex("[%]+[^%_]*[%]+"))) {
 		//^^^^^^^^^^^^^^^^^^^^^Contains LIKE pattern: [%]+[^%_]*[%]+, ignoring undescore
 
 		return move(ApplyRule(root, StringFunctionsType::CONTAINS, patt_str));
@@ -57,8 +57,8 @@ unique_ptr<Expression> LikeOptimizationRule::Apply(LogicalOperator &op, vector<E
 	return nullptr;
 }
 
-unique_ptr<Expression> LikeOptimizationRule::ApplyRule(BoundFunctionExpression *expr,
-													   StringFunctionsType func_type, string pattern) {
+unique_ptr<Expression> LikeOptimizationRule::ApplyRule(BoundFunctionExpression *expr, StringFunctionsType func_type,
+                                                       string pattern) {
 	ScalarFunction function = GetScalarFunction(func_type);
 
 	// replace LIKE by an optimized function
