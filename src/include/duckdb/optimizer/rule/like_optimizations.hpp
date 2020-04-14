46cc668ea90324ef6a6056cb5568ab7e9fb3429b
Tiago Kepe <tiagokepe@gmail.com>
2020-04-14 18:46:45 -0300

    Implemented Like Optimization Rules.

---
+++ src/include/duckdb/optimizer/rule/like_optimizations.hpp
@@ -0,0 +1,29 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/optimizer/rule/like_optimizations.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "duckdb/optimizer/rule.hpp"
+#include "duckdb/function/scalar/string_functions.hpp"
+
+
+namespace duckdb {
+
+// The Like Optimization rule rewrites LIKE to optimized scalar functions (e.g.: prefix, suffix, and contains)
+class LikeOptimizationRule : public Rule {
+public:
+	LikeOptimizationRule(ExpressionRewriter &rewriter);
+
+	unique_ptr<Expression> Apply(LogicalOperator &op, vector<Expression *> &bindings, bool &changes_made) override;
+
+	unique_ptr<Expression> ApplyRule(BoundFunctionExpression *expr, StringFunctionsType funct_type, string pattern);
+
+	ScalarFunction GetScalarFunction(StringFunctionsType type);
+};
+
+} // namespace duckdb
