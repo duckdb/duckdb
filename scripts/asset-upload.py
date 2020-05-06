4c8b5bc9b8bd0aab7ff33504e1f966335356b4f7
Hannes Mühleisen <hannes@muehleisen.org>
2020-05-06 10:13:53 +0200

    not re-creating the release

--- scripts/asset-upload.py
+++ scripts/asset-upload.py
@@ -1,48 +1,86 @@
-import requests, json, os, sys, mimetypes
+import json, os, sys, mimetypes
+import urllib.request
+import json
+
+api_url = 'https://api.github.com/repos/cwida/duckdb/'
+tag = 'master-builds'
 
 if (len(sys.argv) < 2):
     print("Usage: [filename1] [filename2] ... ")
     exit(1)
-
-api_url = 'https://api.github.com/repos/cwida/duckdb/'
-
-tag = 'master-builds'
 
 # this essentially should run on release tag builds to fill up release assets and master
 # so check travis tag?
 
 # TODO check TRAVIS_TAG
 
-token = os.getenv("GH_TOKEN", "")
-if token == "":
-	raise ValueError('need a GitHub token in GH_TOKEN')
-header = {'Authorization': 'token ' + token}
-
-
 branch = os.getenv("TRAVIS_BRANCH", "")
 if branch != "ghassets":
 	print("Only running on ghassets branch for now. Exiting.")
 	exit(0)
 
-sha = os.getenv("TRAVIS_COMMIT", "")
-if sha == "":
-	raise ValueError('need a commit ID in TRAVIS_COMMIT')
+# sha = os.getenv("TRAVIS_COMMIT", "")
+# if sha == "":
+# 	raise ValueError('need a commit ID in TRAVIS_COMMIT')
+sha = 'ba75d81601913782d28a3878707d135319f38bdd'
+
+token = os.getenv("GH_TOKEN", "")
+if token == "":
+	raise ValueError('need a GitHub token in GH_TOKEN')
+
+def gh_api(suburl, payload={}, filename='', method='GET'):
+	url = api_url + suburl
+	headers = {
+		"Content-Type": "application/json",
+		'Authorization': 'token ' + token
+	}
+
+	body_data = b''
+	if len(payload) > 0:
+		method = 'POST'
+		body_data = json.dumps(payload).encode("utf-8")
+		headers["Content-Length"] = len(body_data)
+
+	if len(filename) > 0:
+		method = 'POST'
+		body_data = open(filename, 'rb')
+
+		mime_type = mimetypes.guess_type(local_filename)[0]
+		if mime_type is None:
+			mime_type = "application/octet-stream"
+		headers["Content-Type"] = mime_type
+		headers["Content-Length"] = os.path.getsize(local_filename)
+
+		url = suburl # cough
+
+	req = urllib.request.Request(url, body_data, headers)
+	req.get_method = lambda: method
+	try:
+		raw_resp = urllib.request.urlopen(req).read().decode()
+	except urllib.error.HTTPError as e:
+		raw_resp = e.read().decode() # gah
+
+	if (method != 'DELETE'):
+		return json.loads(raw_resp)
+	else:
+		return {}
+
 
 # find out if commit exists in the first place
-resp = requests.get(api_url +'commits/%s' % sha, headers=header).json()
-if 'sha' not in resp:
+if 'sha' not in gh_api('commits/%s' % sha):
 	raise ValueError('commit %s not found' % sha)
 
 # check if tag exists with the correct hash already, if not, recreate tag & release
-resp = requests.get(api_url +'git/ref/tags/%s' % tag, headers=header).json()
-if 'object' not in resp or 'sha' not in resp['object'] or resp['object']['sha'] != sha:
+resp = gh_api('git/ref/tags/%s' % tag)
+if 'object' not in resp or 'sha' not in resp['object'] : # or resp['object']['sha'] != sha
 	print("re-creating tag & release")
 	# clean up, delete release if exists and tag
 	# this creates a potential race condition, use travis stages?
-	response_json = requests.get(api_url +'releases/tags/%s' % tag, headers=header).json()
-	if "id" in response_json:
-		requests.delete(api_url +'releases/%s' % response_json["id"], headers=header)
-	requests.delete(api_url +'git/refs/tags/%s' % tag, headers=header)
+	resp = gh_api('releases/tags/%s' % tag)
+	if "id" in resp:
+		gh_api('releases/%s' % resp["id"], method='DELETE')
+
+	gh_api('git/refs/tags/%s' % tag, method='DELETE')
 
 	payload = {
 	  "tag_name": tag,
@@ -52,15 +90,22 @@ if 'object' not in resp or 'sha' not in resp['object'] or resp['object']['sha'] 
 	  "draft": False,
 	  "prerelease": True
 	}
-	response_json = requests.post(api_url +'releases', json=payload, headers=header).json()
+	resp = gh_api('releases', payload=payload)
+	print(resp)
+
+resp = gh_api('releases/tags/%s' % tag)
+if 'id' not in resp or 'upload_url' not in resp:
+	raise ValueError('release does not exist for tag ' % tag)
 
 # double-check that release exists and has correct sha
-resp = requests.get(api_url +'releases/tags/%s' % tag, headers=header).json()
-if 'id' not in resp or 'upload_url' not in resp or 'target_commitish' not in resp or resp['target_commitish'] != sha:
-	raise ValueError('release does not point to requested commit %s' % sha)
+# disabled to not spam people watching releases
+# if 'id' not in resp or 'upload_url' not in resp or 'target_commitish' not in resp or resp['target_commitish'] != sha:
+# 	raise ValueError('release does not point to requested commit %s' % sha)
+
+# TODO this could be a paged response!
+assets = gh_api('releases/%s/assets' % resp['id'])
 
 upload_url = resp['upload_url'].split('{')[0] # gah
-
 files = sys.argv[1:]
 for filename in files:
 	if '=' in filename:
@@ -71,13 +116,12 @@ for filename in files:
 		local_filename = filename
 		asset_filename = os.path.basename(filename)
 
-	mime_type = mimetypes.guess_type(local_filename)[0]
-	if mime_type is None:
-		mime_type = "application/octet-stream"
+	# delete if present
+	for asset in assets:
+		if asset['name'] == asset_filename:
+			gh_api('releases/assets/%s' % asset['id'], method='DELETE')
 
-	header["Content-Type"] = mime_type
-	with open(local_filename, 'rb') as f:
-	    resp = requests.post(upload_url + '?name=%s' % asset_filename, data=f, headers=header).json()
-	    if 'id' not in resp:
-	    	raise ValueError('upload failed :/ ' + str(resp))
-	    print(resp['browser_download_url'])
+    resp = gh_api(upload_url + '?name=%s' % asset_filename, filename=local_filename)
+    if 'id' not in resp:
+    	raise ValueError('upload failed :/ ' + str(resp))
+    print(resp['browser_download_url'])
