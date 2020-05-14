f0c1af9350f06dbef9994dc95335ddf7373147ac
Tiago Kepe <tiagokepe@gmail.com>
2020-05-14 16:59:27 -0300

    Removed wrong checking.

--- src/execution/expression_executor/execute_cast.cpp
+++ src/execution/expression_executor/execute_cast.cpp
@@ -19,8 +19,7 @@ void ExpressionExecutor::Execute(BoundCastExpression &expr, ExpressionState *sta
 	auto child_state = state->child_states[0].get();
 
 	Execute(*expr.child, child_state, sel, count, child);
-	if (expr.source_type == expr.target_type ||
-		(expr.source_type == SQLType::VARCHAR && expr.target_type == SQLType::BLOB)) {
+	if (expr.source_type == expr.target_type) {
 		// NOP cast
 		result.Reference(child);
 	} else {
