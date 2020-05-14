15b2961f8373bcd4fe85affb48ebc2202d8fbb52
Tiago Kepe <tiagokepe@gmail.com>
2020-05-14 17:08:08 -0300

    Improving test cases for BLOB.

--- test/sql/blob/test_blob.cpp
+++ test/sql/blob/test_blob.cpp
@@ -7,22 +7,46 @@
 using namespace duckdb;
 using namespace std;
 
+TEST_CASE("Cast BLOB values", "[blob]") {
+	unique_ptr<QueryResult> result;
+	DuckDB db(nullptr);
+	Connection con(db);
+
+	// Test BLOB to VARCHAR -> CastFromBlob
+	result = con.Query("SELECT 'a'::BYTEA::VARCHAR");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("\\x61")}));
+
+	// Test VARCHAR to BLOB -> nop cast
+	result = con.Query("SELECT 'a'::VARCHAR::BYTEA");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("a")}));
+
+	REQUIRE_FAIL(con.Query("SELECT 1::BYTEA"));
+	REQUIRE_FAIL(con.Query("SELECT 1.0::BYTEA"));
+
+    // numeric -> bytea, not valid/implemented casts
+	vector<string> types = {"tinyint", "smallint", "integer", "bigint", "decimal"};
+	for (auto &type : types) {
+		REQUIRE_FAIL(con.Query("SELECT 1::"+ type + "::BYTEA"));
+	}
+}
+
 TEST_CASE("Insert BLOB values from string", "[blob]") {
 	unique_ptr<QueryResult> result;
 	DuckDB db(nullptr);
 	Connection con(db);
 
-	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BLOB);"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BYTEA);"));
 	// insert BLOB from string
 	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('aaaaaaaaaa')"));
-	// sizes: 10, 100, 1000, 10000
+	// sizes: 22, 202, 20002, 20002 -> double plus two due to hexadecimal representation
 	for (idx_t i = 0; i < 3; i++) {
+		// The concat function casts BLOB to VARCHAR,resulting in a hex string
 		REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs SELECT b||b||b||b||b||b||b||b||b||b FROM blobs "
 								  "WHERE LENGTH(b)=(SELECT MAX(LENGTH(b)) FROM blobs)"));
 	}
 
 	result = con.Query("SELECT LENGTH(b) FROM blobs ORDER BY 1");
-	REQUIRE(CHECK_COLUMN(result, 0, {10, 100, 1000, 10000}));
+	REQUIRE(CHECK_COLUMN(result, 0, {10*2 + 2, 100*2 + 2, 1000*2 + 2, 10000*2 + 2}));
 }
 
 TEST_CASE("Insert BLOB values", "[blob]") {
@@ -30,11 +54,11 @@ TEST_CASE("Insert BLOB values", "[blob]") {
 	DuckDB db(nullptr);
 	Connection con(db);
 
-	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BLOB);"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BYTEA);"));
 	// insert BLOB
-	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('aaaaaaaaaa'::BLOB)"));
+	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('aaaaaaaaaa'::BYTEA)"));
 	// insert BLOB with “non-printable” octets
-	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('\153\154\155 \052\251\124'::BLOB)"));
+	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('\153\154\155 \052\251\124'::BYTEA)"));
 
 	// insert BLOB with “non-printable” octets, but now using VARCHAR string (should fail)
 	REQUIRE_FAIL(con.Query("INSERT INTO blobs VALUES ('\153\154\155 \052\251\124'::VARCHAR)"));
@@ -48,16 +72,21 @@ TEST_CASE("Select BLOB values", "[blob]") {
 	DuckDB db(nullptr);
 	Connection con(db);
 
-	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BLOB);"));
-	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('aaaaaaaaaa'::BLOB)"));
-	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('\153\154\155 \052\251\124'::BLOB)"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BYTEA);"));
+	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('a a'::BYTEA)"));
+	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('\153\154\155 \052\251\124'::BYTEA)"));
 
-	result = con.Query("SELECT count(*) FROM blobs");
-	REQUIRE(CHECK_COLUMN(result, 0, {2}));
+	result = con.Query("SELECT * FROM blobs");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("a a"), Value::BLOB("\153\154\155 \052\251\124")}));
 
 	//BLOB with “non-printable” octets
-	REQUIRE_NO_FAIL(con.Query("SELECT 'abc \201'::BLOB;"));
-	REQUIRE_NO_FAIL(con.Query("SELECT 'abc \153\154\155 \052\251\124'::BLOB;"));
+	REQUIRE_NO_FAIL(con.Query("SELECT 'abc \201'::BYTEA;"));
+	result = con.Query("SELECT 'abc \201'::BYTEA;");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("abc \201")}));
+
+	REQUIRE_NO_FAIL(con.Query("SELECT 'abc \153\154\155 \052\251\124'::BYTEA;"));
+	result = con.Query("SELECT 'abc \153\154\155 \052\251\124'::BYTEA;");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("abc \153\154\155 \052\251\124")}));
 
 	//now VARCHAR with “non-printable” octets, should fail
 	REQUIRE_FAIL(con.Query("SELECT 'abc \201'::VARCHAR;"));
@@ -69,10 +98,19 @@ TEST_CASE("Test BLOB with PreparedStatement from a file", "[blob]") {
 	unique_ptr<QueryResult> result_other;
 	DuckDB db(nullptr);
 	Connection con(db);
-	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BLOB);"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BYTEA);"));
+
+	string blob_file_path = TestCreatePath("blob_file.txt");
+    ofstream ofs_blob_file(blob_file_path, std::ofstream::out | std::ofstream::app);
+    // Insert all ASCII chars from 1 to 255, avoiding only the '\0' char
+	char ch = '\1';
+	for(idx_t i = 0; i < 255; ++i, ++ch) {
+    	ofs_blob_file << ch;
+	}
+	ofs_blob_file.close();
 
 	// DuckDB readme file
-	ifstream ifs("README.md", ifstream::binary);
+	ifstream ifs(blob_file_path, ifstream::binary);
 	REQUIRE(ifs.is_open());
 
 	// get pointer to associated buffer object
@@ -83,53 +121,71 @@ TEST_CASE("Test BLOB with PreparedStatement from a file", "[blob]") {
 	file_buf->pubseekpos (0, ifs.in);
 
 	// allocate memory to contain file data
-	char* buffer=new char[size + 1];
+	unique_ptr<char[]> buffer(new char[size + 1]);
 
 	// get file data
-	file_buf->sgetn (buffer,size);
+	file_buf->sgetn (buffer.get(), size);
 
 	ifs.close();
 
-	buffer[size] = '\0';
-	string str_buffer(buffer);
-	delete[] buffer;
+	string str_buffer(buffer.get(), size);
 
-	unique_ptr<PreparedStatement> ps = con.Prepare("INSERT INTO blobs VALUES (?::BLOB)");
+	unique_ptr<PreparedStatement> ps = con.Prepare("INSERT INTO blobs VALUES (?::BYTEA)");
 	ps->Execute(str_buffer);
 	REQUIRE(ps->success);
 	ps.reset();
-//	result = con.Query("SELECT LENGTH(b) FROM blobs");
-//	REQUIRE(CHECK_COLUMN(result, 0, {size}));
+
+	result = con.Query("SELECT OCTET_LENGTH(b) FROM blobs");
+	REQUIRE(CHECK_COLUMN(result, 0, {255}));
+
 	result = con.Query("SELECT count(b) FROM blobs");
 	REQUIRE(CHECK_COLUMN(result, 0, {1}));
+
+	ch = '\1';
+	for(idx_t i = 0; i < 255; ++i, ++ch) {
+    	buffer[i] = ch;
+	}
+	string blob_str(buffer.get(), 255);
+
+	result = con.Query("SELECT b FROM blobs");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB(blob_str)}));
+
+	TestDeleteFile(blob_file_path);
 }
 
 TEST_CASE("BLOB with Functions", "[blob]") {
 	unique_ptr<QueryResult> result;
 	DuckDB db(nullptr);
 	Connection con(db);
-	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BLOB);"));
-	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('a'::BLOB)"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE blobs (b BYTEA);"));
+	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('a'::BYTEA)"));
 
-	// concat BLOBs -----------------------------------------------------------------------
-	//FIXME: this is failing because of UTFVerify() methods that call string_t::Verify()
-//	REQUIRE_NO_FAIL(con.Query("SELECT 'abc '::BLOB || '\153\154\155 \052\251\124'::BLOB)"));
+	// conventional concat, (cast BLOB to VARCHAR generating a hex string)
+	result = con.Query("SELECT b || 'ZZ'::BYTEA FROM blobs");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("\\x615A5A")}));
 
-	result = con.Query("SELECT b || 'ZZ'::BLOB FROM blobs");
+	REQUIRE_NO_FAIL(con.Query("SELECT 'abc '::BYTEA || '\153\154\155 \052\251\124'::BYTEA"));
+	result = con.Query("SELECT 'abc '::BYTEA || '\153\154\155 \052\251\124'::BYTEA");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("\\x616263206B6C6D202AA954")}));
+
+	// specialized concat BLOB
+	result = con.Query("SELECT concat_blob(b, 'ZZ'::BYTEA) FROM blobs");
 	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("aZZ")}));
 
-	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('abc \153\154\155 \052\251\124'::BLOB)"));
+	result = con.Query("SELECT concat_blob('abc '::BYTEA, '\153\154\155 \052\251\124'::BYTEA)");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::BLOB("abc \153\154\155 \052\251\124")}));
 
-	result = con.Query("SELECT count(*) FROM blobs");
+
+	REQUIRE_NO_FAIL(con.Query("INSERT INTO blobs VALUES ('abc \153\154\155 \052\251\124'::BYTEA)"));
+
+	result = con.Query("SELECT COUNT(*) FROM blobs");
 	REQUIRE(CHECK_COLUMN(result, 0, {2}));
 
 	// octet_length
-	result = con.Query("SELECT octet_length(b) FROM blobs");
+	result = con.Query("SELECT OCTET_LENGTH(b) FROM blobs");
 	REQUIRE(CHECK_COLUMN(result, 0, {1, 11}));
 
-	//this should fail because LENGTH only supports UTF8 strings
-	//FIXME: this causes a stack-buffer-overflow in duckdb/third_party/utf8proc/utf8proc.cpp:354
-//	REQUIRE_FAIL(con.Query("SELECT length(T.b) FROM (SELECT 'abc \153\154\155 \052\251\124'::BLOB as b) AS T"));
-//	REQUIRE_FAIL(con.Query("SELECT length(b) FROM blobs"));
-
+	// length
+	result = con.Query("SELECT LENGTH(b) FROM blobs");
+	REQUIRE(CHECK_COLUMN(result, 0, {1*2 + 2, 11*2 + 2}));
 }
