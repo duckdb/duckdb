54584f553b59cd1fe5f26b37233b232cc0cd85d2
Hannes Mühleisen <hannes@muehleisen.org>
2020-06-03 08:49:05 +0200

    removed dbgen from parquet tests for windows

--- extension/parquet/test/parquet_test.cpp
+++ extension/parquet/test/parquet_test.cpp
@@ -4,7 +4,7 @@
 #include "duckdb.hpp"
 #include "test_helpers.hpp"
 #include "duckdb/common/types/timestamp.hpp"
-#include "dbgen.hpp"
+//#include "dbgen.hpp"
 
 using namespace duckdb;
 
@@ -15,6 +15,71 @@ TEST_CASE("Test basic parquet reading", "[parquet]") {
 	Connection con(db);
 
 	con.EnableQueryVerification();
+
+	SECTION("Exception on missing file") {
+		REQUIRE_THROWS(con.Query("SELECT * FROM parquet_scan('does_not_exist')"));
+	}
+
+	SECTION("alltypes_plain.parquet") {
+		auto result = con.Query("SELECT * FROM parquet_scan('extension/parquet/test/alltypes_plain.parquet')");
+		REQUIRE(CHECK_COLUMN(result, 0, {4, 5, 6, 7, 2, 3, 0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 1, {true, false, true, false, true, false, true, false}));
+		REQUIRE(CHECK_COLUMN(result, 2, {0, 1, 0, 1, 0, 1, 0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 3, {0, 1, 0, 1, 0, 1, 0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 4, {0, 1, 0, 1, 0, 1, 0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 5, {0, 10, 0, 10, 0, 10, 0, 10}));
+		REQUIRE(CHECK_COLUMN(result, 6, {0.0, 1.1, 0.0, 1.1, 0.0, 1.1, 0.0, 1.1}));
+		REQUIRE(CHECK_COLUMN(result, 7, {0.0, 10.1, 0.0, 10.1, 0.0, 10.1, 0.0, 10.1}));
+		REQUIRE(CHECK_COLUMN(
+		    result, 8,
+		    {"03/01/09", "03/01/09", "04/01/09", "04/01/09", "02/01/09", "02/01/09", "01/01/09", "01/01/09"}));
+		REQUIRE(CHECK_COLUMN(result, 9, {"0", "1", "0", "1", "0", "1", "0", "1"}));
+
+		REQUIRE(CHECK_COLUMN(result, 10,
+		                     {Value::BIGINT(Timestamp::FromString("2009-03-01 00:00:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-03-01 00:01:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-04-01 00:00:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-04-01 00:01:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-02-01 00:00:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-02-01 00:01:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-01-01 00:00:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-01-01 00:01:00"))}));
+	}
+
+	SECTION("alltypes_plain.snappy.parquet") {
+		auto result = con.Query("SELECT * FROM parquet_scan('extension/parquet/test/alltypes_plain.snappy.parquet')");
+		REQUIRE(CHECK_COLUMN(result, 0, {6, 7}));
+		REQUIRE(CHECK_COLUMN(result, 1, {true, false}));
+		REQUIRE(CHECK_COLUMN(result, 2, {0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 3, {0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 4, {0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 5, {0, 10}));
+		REQUIRE(CHECK_COLUMN(result, 6, {0.0, 1.1}));
+		REQUIRE(CHECK_COLUMN(result, 7, {0.0, 10.1}));
+		REQUIRE(CHECK_COLUMN(result, 8, {"04/01/09", "04/01/09"}));
+		REQUIRE(CHECK_COLUMN(result, 9, {"0", "1"}));
+		REQUIRE(CHECK_COLUMN(result, 10,
+		                     {Value::BIGINT(Timestamp::FromString("2009-04-01 00:00:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-04-01 00:01:00"))}));
+	}
+
+	SECTION("alltypes_dictionary.parquet") {
+		auto result = con.Query("SELECT * FROM parquet_scan('extension/parquet/test/alltypes_dictionary.parquet')");
+
+		REQUIRE(CHECK_COLUMN(result, 0, {0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 1, {true, false}));
+		REQUIRE(CHECK_COLUMN(result, 2, {0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 3, {0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 4, {0, 1}));
+		REQUIRE(CHECK_COLUMN(result, 5, {0, 10}));
+		REQUIRE(CHECK_COLUMN(result, 6, {0.0, 1.1}));
+		REQUIRE(CHECK_COLUMN(result, 7, {0.0, 10.1}));
+		REQUIRE(CHECK_COLUMN(result, 8, {"01/01/09", "01/01/09"}));
+		REQUIRE(CHECK_COLUMN(result, 9, {"0", "1"}));
+		REQUIRE(CHECK_COLUMN(result, 10,
+		                     {Value::BIGINT(Timestamp::FromString("2009-01-01 00:00:00")),
+		                      Value::BIGINT(Timestamp::FromString("2009-01-01 00:01:00"))}));
+	}
 
 	SECTION("userdata1.parquet") {
 
@@ -139,18 +204,18 @@ TEST_CASE("Test basic parquet reading", "[parquet]") {
 		REQUIRE(CHECK_COLUMN(result, 1, {"1E+02"}));
 	}
 }
-
-TEST_CASE("Test TPCH SF1 from parquet file", "[parquet][.]") {
-	DuckDB db(nullptr);
-	db.LoadExtension<ParquetExtension>();
-	Connection con(db);
-
-	auto result = con.Query("SELECT * FROM "
-	                        "parquet_scan('extension/parquet/test/lineitem-sf1.snappy.parquet') limit 10");
-	result->Print();
-
-	con.Query("CREATE VIEW lineitem AS SELECT * FROM "
-	          "parquet_scan('extension/parquet/test/lineitem-sf1.snappy.parquet')");
-	result = con.Query(tpch::get_query(1));
-	COMPARE_CSV(result, tpch::get_answer(1, 1), true);
-}
+//
+// TEST_CASE("Test TPCH SF1 from parquet file", "[parquet][.]") {
+//	DuckDB db(nullptr);
+//	db.LoadExtension<ParquetExtension>();
+//	Connection con(db);
+//
+//	auto result = con.Query("SELECT * FROM "
+//	                        "parquet_scan('extension/parquet/test/lineitem-sf1.snappy.parquet') limit 10");
+//	result->Print();
+//
+//	con.Query("CREATE VIEW lineitem AS SELECT * FROM "
+//	          "parquet_scan('extension/parquet/test/lineitem-sf1.snappy.parquet')");
+//	result = con.Query(tpch::get_query(1));
+//	COMPARE_CSV(result, tpch::get_answer(1, 1), true);
+//}
