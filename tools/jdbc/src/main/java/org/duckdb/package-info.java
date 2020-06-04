728b6996c10c280db13556dc156641429f2f010f
Hannes Mühleisen <hannes@muehleisen.org>
2020-06-04 14:33:21 +0200

    added package info so we can run javadoc

---
+++ tools/jdbc/src/main/java/org/duckdb/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The DuckDB JDBC driver. Connect like so:
+ * 
+ * <pre>
+ * Class.forName("org.duckdb.DuckDBDriver");
+ * Connection conn = DriverManager.getConnection("jdbc:duckdb:");
+ * </pre>
+ */
+package org.duckdb;
