da9c3af4287673211d821d66f8f807efeded0da2
Hannes Mühleisen <hannes@muehleisen.org>
2020-06-14 18:37:12 +0200

    Adding Parquet and ICU support to shell (#676)
    * adding parquet & icu extensions to sqlite api wrapper and thus shell
    * adding read_parquet alias to parquet_scan
    * don't write collations to WAL

--- tools/sqlite3_api_wrapper/sqlite3_api_wrapper.cpp
+++ tools/sqlite3_api_wrapper/sqlite3_api_wrapper.cpp
@@ -68,6 +68,14 @@ int sqlite3_open(const char *filename, /* Database filename (UTF-8) */
 	return sqlite3_open_v2(filename, ppDb, 0, NULL);
 }
 
+#ifdef BUILD_ICU_EXTENSION
+#include "icu-extension.hpp"
+#endif
+
+#ifdef BUILD_PARQUET_EXTENSION
+#include "parquet-extension.hpp"
+#endif
+
 int sqlite3_open_v2(const char *filename, /* Database filename (UTF-8) */
                     sqlite3 **ppDb,       /* OUT: SQLite db handle */
                     int flags,            /* Flags */
@@ -90,6 +98,13 @@ int sqlite3_open_v2(const char *filename, /* Database filename (UTF-8) */
 			}
 			pDb->db = make_unique<DuckDB>(filename, &config);
 			pDb->con = make_unique<Connection>(*pDb->db);
+
+#ifdef BUILD_ICU_EXTENSION
+			pDb->db->LoadExtension<ICUExtension>();
+#endif
+#ifdef BUILD_PARQUET_EXTENSION
+			pDb->db->LoadExtension<ParquetExtension>();
+#endif
 		} catch (std::exception &ex) {
 			if (pDb) {
 				pDb->last_error = ex.what();
