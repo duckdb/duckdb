f4e1f4b5a420c0169dbbef8b9ecf572bd4d094a5
Tiago Kepe <tiagokepe@gmail.com>
2020-06-17 12:27:15 -0300

    Created method CreateFunction with SQLTypes as arguments.

--- src/function/udf_function.cpp
+++ src/function/udf_function.cpp
@@ -1,6 +1,15 @@
 #include "duckdb/function/udf_function.hpp"
 
-namespace duckdb {
+using namespace duckdb;
 
 UDFWrapper::UDFWrapper(ClientContext &context): _context(context) {
+}
+
+void UDFWrapper::CreateFunction(string name, vector<SQLType> args, SQLType ret_type, void *udf_func) {
+	// switch on return type
+	switch(ret_type.id) {
+	case SQLTypeId::INTEGER:
+		CreateFunctionInitial<int>(name, args, ret_type, udf_func);
+		break;
+	}
 }
