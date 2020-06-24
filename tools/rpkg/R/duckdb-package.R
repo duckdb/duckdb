2470d37b2dc75e6d1ecf22639905337b131db516
pat-s <patrick.schratz@gmail.com>
2020-06-24 14:44:55 +0200

    automate useDynLib construction via Rcpp

---
+++ tools/rpkg/R/duckdb-package.R
@@ -0,0 +1,9 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @useDynLib duckdb, .registration = TRUE
+## usethis namespace: end
+NULL
