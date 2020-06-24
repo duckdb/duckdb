c1a290e8e36a2c991735adc22568e31ac0962464
pat-s <patrick.schratz@gmail.com>
2020-06-24 15:16:56 +0200

    remove Rcpp dep

--- tools/rpkg/R/duckdb-package.R
+++ tools/rpkg/R/duckdb-package.R
@@ -1,9 +1,4 @@
 ## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
-
-## usethis namespace: start
 #' @useDynLib duckdb, .registration = TRUE
 ## usethis namespace: end
 NULL
