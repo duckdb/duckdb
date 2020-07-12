42597d6cfe139a1a0f300b3de83a8f6da6c9cc75
Tiago Kepe <tiagokepe@gmail.com>
2020-07-12 10:26:56 -0300

    Additional comments on the vectorized UDF test functions.

--- test/sql/udf_function/udf_functions_to_test.hpp
+++ test/sql/udf_function/udf_functions_to_test.hpp
@@ -54,6 +54,9 @@ inline string_t udf_varchar(string_t a, string_t b, string_t c) {return c;}
 
 // Vectorized UDF Functions -------------------------------------------------------------------
 
+/*
+ * This vectorized function is a unary one that copies input values to the result vector
+ */
 template<typename TYPE>
 static void udf_unary_function(DataChunk &input, ExpressionState &state, Vector &result) {
 	assert(input.column_count() == 1);
@@ -92,6 +95,9 @@ static void udf_unary_function(DataChunk &input, ExpressionState &state, Vector 
 	}
 }
 
+/*
+ * This vectorized function is a binary one that copies values from the second input vector to the result vector
+ */
 template<typename TYPE>
 static void udf_binary_function(DataChunk &input, ExpressionState &state, Vector &result) {
 	assert(input.column_count() == 2);
@@ -131,6 +137,9 @@ static void udf_binary_function(DataChunk &input, ExpressionState &state, Vector
 	}
 }
 
+/*
+ * This vectorized function is a binary one that copies values from the third input vector to the result vector
+ */
 template<typename TYPE>
 static void udf_ternary_function(DataChunk &input, ExpressionState &state, Vector &result) {
 	assert(input.column_count() == 3);
