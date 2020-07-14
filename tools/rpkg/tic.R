8ea8f95b02bf03802c0143b1e1d9c98baa8230b0
pat-s <patrick.schratz@gmail.com>
2020-07-14 11:47:18 +0200

    trying

--- tools/rpkg/tic.R
+++ tools/rpkg/tic.R
@@ -1,2 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
+# if (Sys.info()["sysname"] == "Windows") {
+#   do_package_checks(args = c("--no-manual", "--no-multiarch", "--no-byte-compile"))
+# }
 do_package_checks()
