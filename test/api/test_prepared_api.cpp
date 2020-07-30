b0d72a03715aa054c4a780982220ee879674ce31
Mark Raasveldt <mark.raasveldt@gmail.com>
2020-07-30 13:51:04 +0200

    Convert all prepared tests, and move udf_function tests to api

--- test/api/test_prepared_api.cpp
+++ test/api/test_prepared_api.cpp
@@ -320,3 +320,106 @@ TEST_CASE("Test BLOB with PreparedStatement", "[api]") {
 
 	blob_chars.reset();
 }
+
+TEST_CASE("PREPARE for INSERT with dates", "[prepared]") {
+	unique_ptr<QueryResult> result;
+	DuckDB db(nullptr);
+	Connection con(db);
+
+	// prepared DATE insert
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE dates(d DATE)"));
+	REQUIRE_NO_FAIL(con.Query("PREPARE s1 AS INSERT INTO dates VALUES ($1)"));
+	REQUIRE_NO_FAIL(con.Query("EXECUTE s1 (DATE '1992-01-01')"));
+
+	result = con.Query("SELECT * FROM dates");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::DATE(1992, 1, 1)}));
+
+	REQUIRE_NO_FAIL(con.Query("DELETE FROM dates"));
+
+	auto prepared = con.Prepare("INSERT INTO dates VALUES ($1)");
+	REQUIRE_NO_FAIL(prepared->Execute(Value::DATE(1992, 1, 3)));
+
+	result = con.Query("SELECT * FROM dates");
+	REQUIRE(CHECK_COLUMN(result, 0, {Value::DATE(1992, 1, 3)}));
+}
+
+TEST_CASE("PREPARE multiple statements", "[prepared]") {
+	unique_ptr<QueryResult> result;
+	DuckDB db(nullptr);
+	Connection con(db);
+
+	string query = "SELECT $1::INTEGER; SELECT $1::INTEGER;";
+	// cannot prepare multiple statements like this
+	auto prepared = con.Prepare(query);
+	REQUIRE(!prepared->success);
+	// we can use ExtractStatements to execute the individual statements though
+	auto statements = con.ExtractStatements(query);
+	for (auto &statement : statements) {
+		string stmt = query.substr(statement->stmt_location, statement->stmt_length);
+		prepared = con.Prepare(stmt);
+		REQUIRE(prepared->success);
+
+		result = prepared->Execute(1);
+		REQUIRE(CHECK_COLUMN(result, 0, {1}));
+	}
+}
+
+static unique_ptr<QueryResult> TestExecutePrepared(Connection &con, string query) {
+	auto prepared = con.Prepare(query);
+	return prepared->Execute();
+}
+
+TEST_CASE("Prepare all types of statements", "[prepared]") {
+	unique_ptr<QueryResult> result;
+	DuckDB db(nullptr);
+	Connection con(db);
+	auto &fs = db.GetFileSystem();
+
+	string csv_path = TestCreatePath("prepared_files");
+	if (fs.DirectoryExists(csv_path)) {
+		fs.RemoveDirectory(csv_path);
+	}
+
+	// TRANSACTION
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "BEGIN TRANSACTION"));
+	// SELECT
+	result = TestExecutePrepared(con, "SELECT 42");
+	REQUIRE(CHECK_COLUMN(result, 0, {42}));
+	// CREATE_SCHEMA
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "CREATE SCHEMA test"));
+	// CREATE_TABLE
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "CREATE TABLE test.a(i INTEGER)"));
+	// CREATE_TABLE
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "CREATE TABLE b(i INTEGER)"));
+	// CREATE_INDEX
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "CREATE INDEX i_index ON test.a(i)"));
+	// CREATE_VIEW
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "CREATE VIEW v1 AS SELECT * FROM test.a WHERE i=2"));
+	// CREATE_SEQUENCE
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "CREATE SEQUENCE seq"));
+	// PRAGMA
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "PRAGMA table_info('b')"));
+	// EXPLAIN
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "EXPLAIN SELECT 42"));
+	// COPY
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "COPY test.a TO '" + csv_path + "'"));
+	// INSERT
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "INSERT INTO test.a VALUES (1), (2), (3)"));
+	// UPDATE
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "UPDATE test.a SET i=i+1"));
+	// DELETE
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "DELETE FROM test.a WHERE i<4"));
+	// PREPARE
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "PREPARE p1 AS SELECT * FROM test.a"));
+	// EXECUTE
+	result = TestExecutePrepared(con, "EXECUTE p1");
+	REQUIRE(CHECK_COLUMN(result, 0, {4}));
+	// DROP
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "DROP SEQUENCE seq"));
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "DROP VIEW v1"));
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "DROP TABLE test.a CASCADE"));
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "DROP SCHEMA test CASCADE"));
+
+	// TRANSACTION
+	REQUIRE_NO_FAIL(TestExecutePrepared(con, "COMMIT"));
+}
