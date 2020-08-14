a2c4a58b7a92ab023a3b353ed970004f32f1423d
Kirill Müller <krlmlr+r@mailbox.org>
2020-08-14 05:50:03 +0200

    Remove src_duckdb()

--- tools/rpkg/tests/testthat.R
+++ tools/rpkg/tests/testthat.R
@@ -1,31 +1,4 @@
 library(testthat)
 library(DBI)
 
-# the easy part
 test_check("duckdb")
-
-dbplyr_tests <- function() {
-  if (!identical(Sys.getenv("NOT_CRAN"), "true")) {
-    return()
-  }
-  # the hacky part
-  library("dbplyr")
-
-  # pull dbplyr sources to harvest test cases
-  zipfile <- tempfile()
-  all_pkgs <- available.packages()
-  version_latest <- all_pkgs[all_pkgs[, "Package"] == "dbplyr", "Version"]
-  url <- sprintf("https://github.com/tidyverse/dbplyr/archive/v%s.zip", version_latest)
-  download.file(url, zipfile)
-  dbplyr_src <- tempdir()
-  unzip(zipfile, exdir = dbplyr_src)
-  tests <- Sys.glob(file.path(dbplyr_src, "dbplyr*", "tests"))
-  setwd(tests)
-
-  options(duckdb.debug = T)
-  test_register_src("duckdb", duckdb::src_duckdb())
-
-  # TODO fix excluded test cases
-  test_check("dbplyr", stop_on_failure = TRUE, , invert = T, filter = "(verb-joins|verb-mutate)")
-}
-# dbplyr_tests()
