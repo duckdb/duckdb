54748cd9dd38b6733a19778a495422ec369cd84b
Mark Raasveldt <mark.raasveldt@gmail.com>
2020-08-20 14:23:55 +0200

    Also write a load.sql file in EXPORT DATABASE

--- src/execution/operator/persistent/physical_export.cpp
+++ src/execution/operator/persistent/physical_export.cpp
@@ -3,6 +3,7 @@
 #include "duckdb/transaction/transaction.hpp"
 #include "duckdb/common/file_system.hpp"
 #include "duckdb/catalog/catalog_entry/schema_catalog_entry.hpp"
+#include "duckdb/common/string_util.hpp"
 
 #include <algorithm>
 #include <sstream>
@@ -15,6 +16,46 @@ static void WriteCatalogEntries(stringstream& ss, vector<CatalogEntry*> &entries
 		ss << entry->ToSQL() << std::endl;
 	}
 	ss << std::endl;
+}
+
+static void WriteStringStreamToFile(FileSystem &fs, stringstream& ss, string path) {
+	auto ss_string = ss.str();
+	auto handle = fs.OpenFile(path, FileFlags::FILE_FLAGS_WRITE | FileFlags::FILE_FLAGS_FILE_CREATE_NEW, FileLockType::WRITE_LOCK);
+	fs.Write(*handle, (void *)ss_string.c_str(), ss_string.size());
+	handle.reset();
+
+}
+
+static void WriteValueAsSQL(stringstream &ss, Value &val) {
+	if (val.type().IsNumeric()) {
+		ss << val.ToString();
+	} else {
+		ss << "'" << val.ToString() << "'";
+	}
+}
+
+static void WriteCopyStatement(FileSystem &fs, stringstream &ss, TableCatalogEntry *table, CopyInfo &info) {
+	string table_file_path;
+	ss << "COPY ";
+	if (table->schema->name != DEFAULT_SCHEMA) {
+		table_file_path = fs.JoinPath(info.file_path, StringUtil::Format("%s.%s.csv", table->schema->name, table->name));
+		ss << table->schema->name << ".";
+	} else {
+		table_file_path = fs.JoinPath(info.file_path, StringUtil::Format("%s.csv", table->name));
+	}
+	ss << table->name << " FROM '" << table_file_path << "' (";
+	// write the copy options
+	ss << "FORMAT '" << info.format << "'";
+	for(auto &info : info.options) {
+		ss << ", " << info.first << " ";
+		if (info.second.size() == 1) {
+			WriteValueAsSQL(ss, info.second[0]);
+		} else {
+			// FIXME handle multiple options
+			throw NotImplementedException("FIXME: serialize list of options");
+		}
+	}
+	ss << ");" << std::endl;
 }
 
 void PhysicalExport::GetChunkInternal(ExecutionContext &context, DataChunk &chunk, PhysicalOperatorState *state) {
@@ -50,7 +91,7 @@ void PhysicalExport::GetChunkInternal(ExecutionContext &context, DataChunk &chun
 		});
 	});
 
-	// now write the schema.sql file
+	// write the schema.sql file
 	// export order is SCHEMA -> SEQUENCE -> TABLE -> VIEW -> INDEX
 
 	stringstream ss;
@@ -60,15 +101,15 @@ void PhysicalExport::GetChunkInternal(ExecutionContext &context, DataChunk &chun
 	WriteCatalogEntries(ss, views);
 	WriteCatalogEntries(ss, indexes);
 
-	auto schema_str = ss.str();
-	// write the final schema to the file
-	auto schema_file = fs.JoinPath(info->file_path, "schema.sql");
-	auto handle = fs.OpenFile(schema_file, FileFlags::FILE_FLAGS_WRITE | FileFlags::FILE_FLAGS_FILE_CREATE_NEW, FileLockType::WRITE_LOCK);
-	fs.Write(*handle, (void *)schema_str.c_str(), schema_str.size());
-	handle.reset();
+	WriteStringStreamToFile(fs, ss, fs.JoinPath(info->file_path, "schema.sql"));
 
-
-
+	// write the load.sql file
+	// for every table, we write COPY INTO statement with the specified options
+	stringstream load_ss;
+	for(auto &table : tables) {
+		WriteCopyStatement(fs, load_ss, (TableCatalogEntry *) table, *info);
+	}
+	WriteStringStreamToFile(fs, load_ss, fs.JoinPath(info->file_path, "load.sql"));
 	state->finished = true;
 }
 
