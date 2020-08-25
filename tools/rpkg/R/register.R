f59554506611baeb3fe3889ce5821709a1fffc28
Kirill Müller <krlmlr+r@mailbox.org>
2020-08-25 13:02:22 +0200

    Document duckdb_register() and duckdb_unregister() separately

--- tools/rpkg/R/register.R
+++ tools/rpkg/R/register.R
@@ -1,22 +1,38 @@
-#' Register a R data.frame as a virtual table (view) in DuckDB without copying the data
-#' @rdname duckdb_connection
+#' Register a data frame as a virtual table
+#'
+#' `duckdb_register()` registers a data frame as a virtual table (view)
+#'  in a DuckDB connection.
+#'  No data is copied.
+#'
+#' `duckdb_unregister()` unregisters a previously registered data frame.
 #' @param conn A DuckDB connection, created by `dbConnect()`.
-#' @param name The name for the virtual table that is registered
+#' @param name The name for the virtual table that is registered or unregistered
 #' @param df A `data.frame` with the data for the virtual table
+#' @return These functions are called for their side effect.
 #' @export
+#' @examples
+#' con <- dbConnect(duckdb())
+#'
+#' data <- data.frame(a = 1:3, b = letters[1:3])
+#'
+#' duckdb_register(con, "data", data)
+#' dbReadTable(con, "data")
+#'
+#' duckdb_unregister(con, "data")
+#' try(dbReadTable(con, "data"))
+#'
+#' dbDisconnect(con)
 duckdb_register <- function(conn, name, df) {
   stopifnot(dbIsValid(conn))
   .Call(duckdb_register_R, conn@conn_ref, as.character(name), as.data.frame(df))
   invisible(TRUE)
 }
 
-#' Unregister a virtual table referring to a data.frame
-#' @rdname duckdb_connection
-#' @param conn A DuckDB connection, created by `dbConnect()`.
-#' @param name The name for the virtual table previously registered using `duckdb_register()`.
+#' @rdname duckdb_register
 #' @export
 duckdb_unregister <- function(conn, name) {
   stopifnot(dbIsValid(conn))
   .Call(duckdb_unregister_R, conn@conn_ref, as.character(name))
   invisible(TRUE)
 }
+
