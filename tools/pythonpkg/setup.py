ca5ec440c1d1022f66bb677d41e1d8c56e7f8d77
Mark Raasveldt <mark.raasveldt@gmail.com>
2020-08-27 16:46:36 +0200

    Maybe like this?

--- tools/pythonpkg/setup.py
+++ tools/pythonpkg/setup.py
@@ -74,7 +74,7 @@ if len(existing_duckdb_dir) == 0:
 
         (source_list, include_list, original_sources, githash) = package_build.build_package(os.path.join(script_path, 'duckdb'))
 
-        duckdb_sources = [package_build.get_relative_path(script_path, x) for x in source_list]
+        duckdb_sources = [os.path.sep.join(package_build.get_relative_path(script_path, x).split('/')) for x in source_list]
         duckdb_sources.sort()
 
         original_sources = [os.path.join('duckdb', x) for x in original_sources]
