12fa618130b77ceb242475ccb7d85957661fb59d
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-20 07:46:14 +0200

    Move make_context() to helper file

--- tools/rpkg/tests/testthat/test-DBItest.R
+++ tools/rpkg/tests/testthat/test-DBItest.R
@@ -1,4 +1,3 @@
-DBItest::make_context(duckdb::duckdb(), list(debug = F), tweaks = DBItest::tweaks(omit_blob_tests = TRUE, temporary_tables = FALSE, timestamp_cast = function(x) sprintf("CAST('%s' AS TIMESTAMP)", x), date_cast = function(x) sprintf("CAST('%s' AS DATE)", x), time_cast = function(x) sprintf("CAST('%s' AS TIME)", x)), name = "duckdb")
 DBItest::test_all(c(
   "package_name", # wontfix
   "constructor", # wontfix
