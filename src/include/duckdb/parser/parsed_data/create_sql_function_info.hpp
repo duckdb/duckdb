d88ec1fbd15a08d4cf1812ef3be53798471c789a
Laurens Kuiper <laurens.kuiper@cwi.nl>
2020-10-30 10:46:43 +0100

    init

---
+++ src/include/duckdb/parser/parsed_data/create_sql_function_info.hpp
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/parser/parsed_data/create_sql_function_info.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "duckdb/parser/parsed_data/create_function_info.hpp"
+
+namespace duckdb {
+
+struct CreateSQLFunctionInfo : public CreateFunctionInfo {
+    CreateSQLFunctionInfo() : CreateFunctionInfo(CatalogType::SCALAR_FUNCTION_ENTRY) {
+    }
+
+    vector<unique_ptr<ParsedExpression>> arguments;
+    unique_ptr<ParsedExpression> function;
+};
+
+} // namespace duckdb
