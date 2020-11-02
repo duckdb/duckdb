2c325de1066715fdff5d1d6a9bb7accb6c92f68b
Laurens Kuiper <lnkuiper@github.com>
2020-11-02 17:04:09 +0100

    function arguments as dummy NULL columns in binder

--- src/planner/binder/statement/bind_create_function.cpp
+++ src/planner/binder/statement/bind_create_function.cpp
@@ -1,5 +1,4 @@
 #include "duckdb/planner/binder.hpp"
-#include "duckdb/parser/parsed_expression_iterator.hpp"
 
 namespace duckdb {
 
@@ -9,11 +8,22 @@ unique_ptr<BoundCreateFunctionInfo> Binder::BindCreateFunctionInfo(unique_ptr<Cr
     auto result = make_unique<BoundCreateFunctionInfo>(move(info));
     result->schema = BindSchema(*result->base);
 
-	// check whether an unknown parameter is used in the function
-    ParsedExpressionIterator::EnumerateChildren(*base.function, [&](const ParsedExpression &child) {
-      // TODO: check whether an unknown parameter is used in base.function
+	// arguments become dummy column names
+    vector<string> dummy_column_names;
+    vector<LogicalType> dummy_column_types;
+    for (auto &arg : base.arguments) {
+		string arg_str = arg->ToString();
+        if (arg->expression_class != ExpressionClass::COLUMN_REF)
+			throw BinderException("Invalid parameter \"%s\"", arg_str);
+		dummy_column_names.push_back(arg_str);
+		dummy_column_types.push_back(LogicalType::SQLNULL);
+    }
 
-	});
+    // check whether an unknown parameter is used in the function
+	ExpressionBinder binder(*this, context);
+	bind_context.AddGenericBinding(-1, "0_macro_arguments", dummy_column_names, dummy_column_types);
+	binder.Bind(base.function, 0, true);
+
     return result;
 }
 
