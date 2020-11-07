3138a0b12145d00072f94b2e241fe5a7c9121169
Gabor Szarnyas <szarnyasg@gmail.com>
2020-11-07 19:30:08 +0100

    Add benchmark files

--- benchmark/trainbenchmark/schema.sql
+++ benchmark/trainbenchmark/schema.sql
@@ -3,37 +3,37 @@ CREATE TABLE Route (
   active int,
   entry int,
   exit int,
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Region (
   id int NOT NULL,
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Segment (
   id int NOT NULL,
   length int NOT NULL DEFAULT 1,
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Sensor (
   id int NOT NULL,
   region int NOT NULL, -- inverse of the sensors edge
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Semaphore (
   id int NOT NULL,
   segment int NOT NULL, -- inverse of the semaphores edge
   signal int NOT NULL,
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Switch (
   id int NOT NULL,
   currentPosition int NOT NULL,
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE SwitchPosition (
@@ -41,13 +41,13 @@ CREATE TABLE SwitchPosition (
   route int, -- inverse of the follows edge
   target int,
   position int NOT NULL,
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE TrackElement (
   id int NOT NULL,
   region int NOT NULL, -- inverse of the elements edge
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 );
 
 --
@@ -57,17 +57,17 @@ CREATE TABLE TrackElement (
 CREATE TABLE connectsTo (
   TrackElement1_id int NOT NULL,
   TrackElement2_id int NOT NULL,
-  PRIMARY KEY  (TrackElement1_id, TrackElement2_id)
+  PRIMARY KEY (TrackElement1_id, TrackElement2_id)
 );
 
 CREATE TABLE monitoredBy (
   TrackElement_id int NOT NULL,
   Sensor_id int NOT NULL,
-  PRIMARY KEY  (TrackElement_id, Sensor_id)
+  PRIMARY KEY (TrackElement_id, Sensor_id)
 );
 
 CREATE TABLE requires (
   Route_id int NOT NULL,
   Sensor_id int NOT NULL,
-  PRIMARY KEY  (Route_id, Sensor_id)
+  PRIMARY KEY (Route_id, Sensor_id)
 );
