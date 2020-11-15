0231339474d092d37ea2a7ae9a729529c8672889
Hannes Mühleisen <hannes@muehleisen.org>
2020-11-15 10:08:47 +0100

    need clone depth

--- scripts/node_build.sh
+++ scripts/node_build.sh
@@ -4,6 +4,17 @@ source scripts/install_node.sh $1
 cd tools/nodejs
 make clean
 ./configure
+
+# figure out the version number from the tag
+export LASTTAG=`git tag -l --sort tag | tail -n 1`
+export LASTVER=`echo $LASTTAG | tr -d "v"`
+npm version $LASTVER
+# TODO when we're on the tag build, dont do this
+export MASTERDIST=`git log $LASTTAG..HEAD --pretty=oneline | wc -l | tr -d " "`
+npm version prerelease --preid=$MASTERDIST
+npm publish --tag next --dry-run
+
+
 npm install --build-from-source
 npm test
 export PATH=node_modules/node-pre-gyp/bin:$PATH
