798d8fd662fdb054cf3be0bca33d02990afa359d
Hannes Mühleisen <hannes@muehleisen.org>
2020-11-15 11:28:18 +0100

    script updates

--- scripts/node_build.sh
+++ scripts/node_build.sh
@@ -5,20 +5,6 @@ cd tools/nodejs
 make clean
 ./configure
 
-# figure out the version number from the tag
-
-git describe --tags --long  
-
-export LASTVER=`git describe --tags --abbrev=0 | tr -d "v"`
-export DIST=`git describe --tags --long | cut -f2 -d-`
-
-# set version to lastver
-npm version $LASTVER
-# TODO when we're on the tag build, dont do this
-npm version prerelease --preid="dev"$DIST
-npm publish --tag next --dry-run
-
-
 npm install --build-from-source
 npm test
 export PATH=node_modules/node-pre-gyp/bin:$PATH
---
+++ scripts/node_version.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+cd tools/nodejs
+
+# figure out the version number from the tag
+
+export TAG=''
+# for master do prereleases
+if [[ "$GITHUB_REF" =~ ^refs/tags/v.+$ ]] ; then
+	# proper release
+	npm version `echo $GITHUB_REF | sed 's|refs/tags/v||'`
+else 
+	git describe --tags --long  
+
+	export VER=`git describe --tags --abbrev=0 | tr -d "v"`
+	export DIST=`git describe --tags --long | cut -f2 -d-`
+
+	# set version to lastver
+	npm version $VER
+	# TODO when we're on the tag build, dont do this
+	npm version prerelease --preid="dev"$DIST
+	export TAG='--tag next'
+fi
+
+# upload to npm, maybe
+if [[ "$GITHUB_REF" =~ ^(refs/heads/master|refs/tags/v.+)$ && "$1" = "upload" ]] ; then
+	npm publish --dry-run $TAG
+fi
