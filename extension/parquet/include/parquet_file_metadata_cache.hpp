750a19c31daf59f5a620165c1f19b84de54dca9b
Hannes Mühleisen <hannes@muehleisen.org>
2020-11-20 14:30:39 +0100

    moving parquet metadata cache to database instance and secure with lock against race condition

--- extension/parquet/include/parquet_file_metadata_cache.hpp
+++ extension/parquet/include/parquet_file_metadata_cache.hpp
@@ -13,16 +13,18 @@
 namespace duckdb {
 
 //! ParquetFileMetadataCache
-class ParquetFileMetadataCache : public ObjectCache {
+class ParquetFileMetadataCache : public ObjectCacheEntry {
 public:
-	ParquetFileMetadataCache() : metadata(nullptr) {}
+	ParquetFileMetadataCache() : metadata(nullptr) {
+	}
 	ParquetFileMetadataCache(std::unique_ptr<parquet::format::FileMetaData> file_metadata, time_t r_time)
-	    : metadata(std::move(file_metadata)), read_time(r_time) {}
+	    : metadata(std::move(file_metadata)), read_time(r_time) {
+	}
 
 	~ParquetFileMetadataCache() override = default;
 
 	//! Parquet file metadata
-	std::unique_ptr<parquet::format::FileMetaData> metadata;
+	std::unique_ptr<const parquet::format::FileMetaData> metadata;
 
 	//! read time
 	time_t read_time;
