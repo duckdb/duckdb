0482ebb3770ed852ca043e878164f4a2a067c5ac
Hannes Mühleisen <hannes@muehleisen.org>
2020-11-21 08:34:14 +0100

    Making physical window a sink so it can pull data in parallel (#1131)

--- src/include/duckdb/execution/operator/aggregate/physical_window.hpp
+++ src/include/duckdb/execution/operator/aggregate/physical_window.hpp
@@ -9,23 +9,32 @@
 #pragma once
 
 #include "duckdb/common/types/chunk_collection.hpp"
-#include "duckdb/execution/physical_operator.hpp"
+#include "duckdb/execution/physical_sink.hpp"
 
 namespace duckdb {
 
 //! PhysicalWindow implements window functions
-class PhysicalWindow : public PhysicalOperator {
+class PhysicalWindow : public PhysicalSink {
 public:
 	PhysicalWindow(vector<LogicalType> types, vector<unique_ptr<Expression>> select_list,
 	               PhysicalOperatorType type = PhysicalOperatorType::WINDOW);
 
 	void GetChunkInternal(ExecutionContext &context, DataChunk &chunk, PhysicalOperatorState *state) override;
+	unique_ptr<PhysicalOperatorState> GetOperatorState() override;
 
-	//! The projection list of the SELECT statement (that contains aggregates)
-	vector<unique_ptr<Expression>> select_list;
+	// sink stuff
+	void Sink(ExecutionContext &context, GlobalOperatorState &state, LocalSinkState &lstate, DataChunk &input) override;
+	void Combine(ExecutionContext &context, GlobalOperatorState &state, LocalSinkState &lstate) override;
+	void Finalize(Pipeline &pipeline, ClientContext &context, unique_ptr<GlobalOperatorState> gstate) override;
+
+	unique_ptr<LocalSinkState> GetLocalSinkState(ExecutionContext &context) override;
+	unique_ptr<GlobalOperatorState> GetGlobalState(ClientContext &context) override;
+
+	string ParamsToString() const override;
 
 public:
-	unique_ptr<PhysicalOperatorState> GetOperatorState() override;
+	//! The projection list of the WINDOW statement (may contain aggregates)
+	vector<unique_ptr<Expression>> select_list;
 };
 
 } // namespace duckdb
