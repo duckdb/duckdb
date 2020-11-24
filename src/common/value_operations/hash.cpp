5da1967ef7fe5960791192c51963694f33e9e3e9
Mark Raasveldt <mark.raasveldt@gmail.com>
2020-11-24 10:28:56 +0100

    Add value hash implementation for list/struct values

--- src/common/value_operations/hash.cpp
+++ src/common/value_operations/hash.cpp
@@ -33,8 +33,22 @@ hash_t ValueOperations::Hash(const Value &op) {
 		return duckdb::Hash(op.value_.interval);
 	case PhysicalType::VARCHAR:
 		return duckdb::Hash(op.str_value.c_str());
+	case PhysicalType::LIST: {
+		hash_t hash = 0;
+		for(auto &entry : op.list_value) {
+			hash ^= ValueOperations::Hash(entry);
+		}
+		return hash;
+	}
+	case PhysicalType::STRUCT: {
+		hash_t hash = 0;
+		for(auto &entry : op.struct_value) {
+			hash ^= ValueOperations::Hash(entry.second);
+		}
+		return hash;
+	}
 	default:
-		throw NotImplementedException("Unimplemented type for value hash");
+		throw InternalException("Unimplemented type for value hash");
 	}
 }
 
