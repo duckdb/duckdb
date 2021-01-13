31aaa8e5867d9f9465f5bd04f2e68ae8469b28ee
AzimAfroozeh <afroozeh3@gmail.com>
2021-01-13 15:43:59 +0100

    fix bug
    Signed-off-by: AzimAfroozeh <afroozeh3@gmail.com>

--- src/optimizer/statistics/expression/propagate_function.cpp
+++ src/optimizer/statistics/expression/propagate_function.cpp
@@ -5,9 +5,6 @@ namespace duckdb {
 
 unique_ptr<BaseStatistics> StatisticsPropagator::PropagateExpression(BoundFunctionExpression &func,
                                                                      unique_ptr<Expression> *expr_ptr) {
-	if (!func.function.statistics) {
-		return nullptr;
-	}
 	vector<unique_ptr<BaseStatistics>> stats;
 	stats.reserve(func.children.size());
 	for (idx_t i = 0; i < func.children.size(); i++) {
