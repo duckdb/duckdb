637cbdba3591be2924f8602343ecbe444859772a
Pedro Holanda <pedroholanda@gmail.com>
2021-01-19 18:59:23 +0100

    Fix build

--- src/include/duckdb/function/aggregate_function.hpp
+++ src/include/duckdb/function/aggregate_function.hpp
@@ -12,6 +12,7 @@
 #include "duckdb/function/function.hpp"
 #include "duckdb/storage/statistics/base_statistics.hpp"
 #include "duckdb/storage/statistics/node_statistics.hpp"
+#include "duckdb/planner/expression.hpp"
 
 namespace duckdb {
 
