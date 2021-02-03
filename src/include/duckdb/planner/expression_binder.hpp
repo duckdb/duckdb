00e943bb6d1b74720e552f196ea4db7d6dc12141
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-02-03 10:54:44 +0100

    Add support for aliases in the HAVING clause (fixes issue #1358)

--- src/include/duckdb/planner/expression_binder.hpp
+++ src/include/duckdb/planner/expression_binder.hpp
@@ -13,6 +13,7 @@
 #include "duckdb/parser/parsed_expression.hpp"
 #include "duckdb/parser/tokens.hpp"
 #include "duckdb/planner/expression.hpp"
+#include "duckdb/common/unordered_map.hpp"
 
 namespace duckdb {
 
@@ -58,7 +59,7 @@ public:
 	string Bind(unique_ptr<ParsedExpression> *expr, idx_t depth, bool root_expression = false);
 
 	// Bind table names to ColumnRefExpressions
-	static void BindTableNames(Binder &binder, ParsedExpression &expr);
+	static void BindTableNames(Binder &binder, ParsedExpression &expr, unordered_map<string, idx_t> *alias_map = nullptr);
 	static unique_ptr<Expression> PushCollation(ClientContext &context, unique_ptr<Expression> source, string collation,
 	                                            bool equality_only = false);
 	static void TestCollation(ClientContext &context, string collation);
