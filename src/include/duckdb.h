0aebfb1f5e678097aedd5c4d73632934c6ca6924
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-02-16 15:56:52 +0100

    Inline winapi.hpp in duckdb.h

--- src/include/duckdb.h
+++ src/include/duckdb.h
@@ -10,7 +10,15 @@
 
 #pragma once
 
-#include "duckdb/common/winapi.hpp"
+#ifdef _WIN32
+#ifdef DUCKDB_BUILD_LIBRARY
+#define DUCKDB_API __declspec(dllexport)
+#else
+#define DUCKDB_API __declspec(dllimport)
+#endif
+#else
+#define DUCKDB_API
+#endif
 
 #include <stdbool.h>
 #include <stdint.h>
