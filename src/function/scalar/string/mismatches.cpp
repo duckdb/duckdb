4d99fb954fe403ba846c66cfe49f072d4a15bebc
Simon, Reinhard <reinhard.simon@wur.nl>
2021-02-25 20:54:03 +0100

    formatting ctd

--- src/function/scalar/string/mismatches.cpp
+++ src/function/scalar/string/mismatches.cpp
@@ -16,7 +16,6 @@ static int64_t MismatchesScalarFunction(Vector &result, const string_t str, stri
 	if (str_len < 1) {
 		throw InvalidInputException("Mismatch Function: Strings are too short!");
 	}
-		
 
 	idx_t mismatches = 0;
 	auto str_str = str.GetDataUnsafe();
@@ -35,27 +34,20 @@ static void MismatchesFunction(DataChunk &args, ExpressionState &state, Vector &
 	auto &tgt_vec = args.data[1];
 
 	BinaryExecutor::Execute<string_t, string_t, int64_t>(
-	str_vec, tgt_vec, result, args.size(),
-		[&](string_t str, string_t tgt) { return MismatchesScalarFunction(result, str, tgt); });
+	    str_vec, tgt_vec, result, args.size(),
+	    [&](string_t str, string_t tgt) { return MismatchesScalarFunction(result, str, tgt); });
 }
 
 void MismatchesFun::RegisterFunction(BuiltinFunctions &set) {
 	ScalarFunctionSet mismatches("mismatches");
-	mismatches.AddFunction(
-		ScalarFunction("mismatches", {
-			LogicalType::VARCHAR, 
-			LogicalType::VARCHAR
-		}, 
-	LogicalType::BIGINT, MismatchesFunction)); // Pointer to function implementation
+	mismatches.AddFunction(ScalarFunction("mismatches", {LogicalType::VARCHAR, LogicalType::VARCHAR},
+	                                      LogicalType::BIGINT,
+	                                      MismatchesFunction)); // Pointer to function implementation
 	set.AddFunction(mismatches);
 
 	ScalarFunctionSet hamming("hamming");
-	hamming.AddFunction(
-		ScalarFunction("mismatches", {
-			LogicalType::VARCHAR, 
-			LogicalType::VARCHAR
-		}, 
-	LogicalType::BIGINT, MismatchesFunction)); // Pointer to function implementation
+	hamming.AddFunction(ScalarFunction("mismatches", {LogicalType::VARCHAR, LogicalType::VARCHAR}, LogicalType::BIGINT,
+	                                   MismatchesFunction)); // Pointer to function implementation
 	set.AddFunction(hamming);
 }
 
