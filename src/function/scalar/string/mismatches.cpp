041ff9d7653108ce519b86eeb6d980d535f945f4
Simon, Reinhard <reinhard.simon@wur.nl>
2021-02-26 09:26:23 +0100

    adding tests for two columns

--- src/function/scalar/string/mismatches.cpp
+++ src/function/scalar/string/mismatches.cpp
@@ -8,13 +8,13 @@ namespace duckdb {
 
 static int64_t MismatchesScalarFunction(Vector &result, const string_t str, string_t tgt) {
 	idx_t str_len = str.GetSize();
-	idx_t tgt_len = tgt.GetSize();
+	idx_t tgt_len = tgt.GetSize(); 
 
 	if (str_len != tgt_len) {
 		throw InvalidInputException("Mismatch Function: Strings must be of equal length!");
 	}
 	if (str_len < 1) {
-		throw InvalidInputException("Mismatch Function: Strings are too short!");
+		throw InvalidInputException("Mismatch Function: Strings must be of length > 0!");
 	}
 
 	idx_t mismatches = 0;
