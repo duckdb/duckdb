d25d9eb95ec6a21c3774b7d6b1d1528e700dd4da
Hannes Mühleisen <hannes@muehleisen.org>
2021-03-16 11:33:28 +0100

    grr

--- third_party/hyperloglog/hyperloglog.cpp
+++ third_party/hyperloglog/hyperloglog.cpp
@@ -191,7 +191,7 @@ struct hllhdr {
     uint8_t encoding;   /* HLL_DENSE or HLL_SPARSE. */
     uint8_t notused[3]; /* Reserved for future use, must be zero. */
     uint8_t card[8];    /* Cached cardinality, little endian. */
-    uint8_t registers[]; /* Data bytes. */
+    uint8_t registers[1]; /* Data bytes. */
 };
 
 /* The cached cardinality MSB is used to signal validity of the cached value. */
