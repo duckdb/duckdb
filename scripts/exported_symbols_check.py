0e5f423c8b59ae5a224a9b7d07743d5ac9e5451e
Hannes Mühleisen <hannes@muehleisen.org>
2021-03-17 09:29:21 +0100

    moving tdigest to third_party

--- scripts/exported_symbols_check.py
+++ scripts/exported_symbols_check.py
@@ -6,7 +6,7 @@ if len(sys.argv) < 2 or not os.path.isfile(sys.argv[1]):
 	print("Usage: [libduckdb dynamic library file, release build]")
 	exit(1)
 
-res = subprocess.run('nm -g -U -j -C'.split(' ') + [sys.argv[1]], check=True, capture_output=True)
+res = subprocess.run('nm -g -C -P'.split(' ') + [sys.argv[1]], check=True, capture_output=True)
 if res.returncode != 0:
 	raise ValueError('Failed to run `nm`') 
 
@@ -15,18 +15,30 @@ culprits = []
 for symbol in res.stdout.decode('utf-8').split('\n'):
 	if len(symbol.strip()) == 0:
 		continue
+	if '@@GLIBC' in symbol:
+		continue
+	if '@@CXXABI' in symbol:
+		continue
+	if symbol.endswith(' U'): # undefined because dynamic linker
+		continue
+	if symbol.endswith(' U 0 0'): # undefined because dynamic linker
+		continue
 	if 'duckdb::' in symbol:
+		continue
+	if 'std::' in symbol:
 		continue
 	if 'duckdb_miniz::' in symbol:
 		continue
 	if 'duckdb_fmt::' in symbol:
+		continue
+	if 'duckdb_hll::' in symbol:
 		continue
 	if symbol.startswith('_duckdb_'):
 		continue
 	if symbol.startswith('duckdb_'):
 		continue
 	# not so sure about that one
-	if symbol.startswith('_utf8proc_'):
+	if 'utf8proc_' in symbol:
 		continue
 
 	culprits.append(symbol)
