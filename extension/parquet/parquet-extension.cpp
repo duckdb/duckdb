1a5633ace8f20abb1dbbe3e5109951b4db02fd8e
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-03-26 15:26:24 +0100

    Add support for adding replacement scans generically and in extensions

--- extension/parquet/parquet-extension.cpp
+++ extension/parquet/parquet-extension.cpp
@@ -16,6 +16,11 @@
 #include "duckdb/parser/parsed_data/create_copy_function_info.hpp"
 #include "duckdb/parser/parsed_data/create_table_function_info.hpp"
 
+#include "duckdb/main/config.hpp"
+#include "duckdb/parser/expression/constant_expression.hpp"
+#include "duckdb/parser/expression/function_expression.hpp"
+#include "duckdb/parser/tableref/table_function_ref.hpp"
+
 #include "duckdb/storage/statistics/base_statistics.hpp"
 
 #include "duckdb/main/client_context.hpp"
@@ -379,6 +384,17 @@ unique_ptr<LocalFunctionData> ParquetWriteInitializeLocal(ClientContext &context
 	return make_unique<ParquetWriteLocalState>();
 }
 
+unique_ptr<TableFunctionRef> ParquetScanReplacement(const string &table_name, void *data) {
+	if (!StringUtil::EndsWith(table_name, ".parquet")) {
+		return nullptr;
+	}
+	auto table_function = make_unique<TableFunctionRef>();
+	vector<unique_ptr<ParsedExpression>> children;
+	children.push_back(make_unique<ConstantExpression>(Value(table_name)));
+	table_function->function = make_unique<FunctionExpression>("parquet_scan", children);
+	return table_function;
+}
+
 void ParquetExtension::Load(DuckDB &db) {
 	ParquetScanFunction scan_fun;
 	CreateTableFunctionInfo cinfo(scan_fun);
@@ -407,6 +423,9 @@ void ParquetExtension::Load(DuckDB &db) {
 	catalog.CreateTableFunction(context, &cinfo);
 	catalog.CreateTableFunction(context, &pq_scan);
 	con.Commit();
+
+	auto &config = DBConfig::GetConfig(*db.instance);
+	config.replacement_scans.push_back(ReplacementScan(ParquetScanReplacement));
 }
 
 } // namespace duckdb
