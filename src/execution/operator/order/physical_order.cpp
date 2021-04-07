e34defc2c85c2530a24c4787652b21d9fd3656b8
Laurens Kuiper <laurens.kuiper@cwi.nl>
2021-04-07 11:07:41 +0200

    fix text and compiler warning

--- src/execution/operator/order/physical_order.cpp
+++ src/execution/operator/order/physical_order.cpp
@@ -220,10 +220,6 @@ unique_ptr<LocalSinkState> PhysicalOrder::GetLocalSinkState(ExecutionContext &co
 
 void PhysicalOrder::Sink(ExecutionContext &context, GlobalOperatorState &gstate_p, LocalSinkState &lstate_p,
                          DataChunk &input) {
-	if (input.size() == 0) {
-		return;
-	}
-
 	auto &gstate = (OrderGlobalState &)gstate_p;
 	auto &lstate = (OrderLocalState &)lstate_p;
 	const auto &sorting_state = *gstate.sorting_state;
@@ -283,7 +279,6 @@ void PhysicalOrder::Sink(ExecutionContext &context, GlobalOperatorState &gstate_
 		var_block.SerializeVector(sort.data[sort_col], sort.size(), *lstate.sel_ptr, input.size(), 0,
 		                          lstate.key_locations, nullptr);
 	}
-	lstate.sort.Reset();
 
 	// compute entry sizes of payload columns if there are variable size columns
 	if (payload_state.HAS_VARIABLE_SIZE) {
@@ -848,17 +843,7 @@ static void Scan(ClientContext &context, DataChunk &chunk, PhysicalOrderOperator
 	const data_ptr_t payl_dataptr = state.payload_handle->node->buffer;
 	if (payload_state.HAS_VARIABLE_SIZE) {
 		const idx_t *offsets = (idx_t *)state.offsets_handle->node->buffer;
-		// fixed-size inner loop to allow unrolling
-		idx_t i;
-		for (i = 0; i + 7 < next; i += 8) {
-			for (idx_t j = 0; j < 8; j++) {
-				state.validitymask_locations[i + j] = payl_dataptr + offsets[Load<idx_t>(sort_dataptr)];
-				state.key_locations[i + j] = state.validitymask_locations[i + j] + payload_state.VALIDITYMASK_SIZE;
-				sort_dataptr += sorting_state.ENTRY_SIZE;
-			}
-		}
-		// finishing up
-		for (; i < next; i++) {
+		for (idx_t i = 0; i < next; i++) {
 			state.validitymask_locations[i] = payl_dataptr + offsets[Load<idx_t>(sort_dataptr)];
 			state.key_locations[i] = state.validitymask_locations[i] + payload_state.VALIDITYMASK_SIZE;
 			sort_dataptr += sorting_state.ENTRY_SIZE;
