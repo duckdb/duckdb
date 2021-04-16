eabe99dcd0f6713bb7a378629d794e1405e5bc89
Richard Wesley <hawkfish@electricfish.com>
2021-04-16 14:32:06 -0700

    Issue #1560: VARCHAR slicing
    Add support for VARCHAR slicing.

--- src/function/scalar/nested/array_slice.cpp
+++ src/function/scalar/nested/array_slice.cpp
@@ -5,6 +5,7 @@
 #include "duckdb/common/types/chunk_collection.hpp"
 #include "duckdb/common/types/data_chunk.hpp"
 #include "duckdb/common/pair.hpp"
+#include "duckdb/function/scalar/string_functions.hpp"
 
 namespace duckdb {
 
@@ -28,6 +29,11 @@ int64_t ValueLength(const list_entry_t &value) {
 	return value.length;
 }
 
+template <>
+int64_t ValueLength(const string_t &value) {
+	return LengthFun::Length<string_t, int64_t>(value);
+}
+
 template <typename INPUT_TYPE, typename INDEX_TYPE>
 bool ClampIndex(INDEX_TYPE &index, const INPUT_TYPE &value) {
 	const auto offset = ValueOffset<INPUT_TYPE, INDEX_TYPE>(value);
@@ -44,17 +50,7 @@ bool ClampIndex(INDEX_TYPE &index, const INPUT_TYPE &value) {
 }
 
 template <typename INPUT_TYPE, typename INDEX_TYPE>
-void TruncateValue(INPUT_TYPE &value, INDEX_TYPE begin, INDEX_TYPE end) {
-}
-
-template <>
-void TruncateValue(list_entry_t &list, int64_t begin, int64_t end) {
-	list.offset += begin;
-	list.length = end - begin;
-}
-
-template <typename INPUT_TYPE, typename INDEX_TYPE>
-static bool SliceValue(INPUT_TYPE &value, INDEX_TYPE begin, INDEX_TYPE end, bool begin_valid, bool end_valid) {
+static bool ClampSlice(const INPUT_TYPE &value, INDEX_TYPE &begin, INDEX_TYPE &end, bool begin_valid, bool end_valid) {
 	// Clamp offsets
 	begin = begin_valid ? begin : 0;
 	end = end_valid ? end : ValueLength<INPUT_TYPE, INDEX_TYPE>(value);
@@ -63,62 +59,77 @@ static bool SliceValue(INPUT_TYPE &value, INDEX_TYPE begin, INDEX_TYPE end, bool
 	}
 	end = MaxValue<INDEX_TYPE>(begin, end);
 
-	// Trim down the range
-	TruncateValue(value, begin, end);
-
 	return true;
 }
 
 template <typename INPUT_TYPE, typename INDEX_TYPE>
-static void ExecuteSlice(Vector &result, Vector &l, Vector &b, Vector &e, const idx_t count) {
+INPUT_TYPE SliceValue(Vector &result, INPUT_TYPE input, INDEX_TYPE begin, INDEX_TYPE end) {
+	return input;
+}
+
+template <>
+list_entry_t SliceValue(Vector &result, list_entry_t input, int64_t begin, int64_t end) {
+	input.offset += begin;
+	input.length = end - begin;
+	return input;
+}
+
+template <>
+string_t SliceValue(Vector &result, string_t input, int64_t begin, int64_t end) {
+	// one-based - zero has strange semantics
+	return SubstringFun::SubstringScalarFunction(result, input, begin + 1, end - begin);
+}
+
+template <typename INPUT_TYPE, typename INDEX_TYPE>
+static void ExecuteSlice(Vector &result, Vector &s, Vector &b, Vector &e, const idx_t count) {
 	if (result.GetVectorType() == VectorType::CONSTANT_VECTOR) {
 		auto rdata = ConstantVector::GetData<INPUT_TYPE>(result);
-		auto ldata = ConstantVector::GetData<INPUT_TYPE>(l);
+		auto sdata = ConstantVector::GetData<INPUT_TYPE>(s);
 		auto bdata = ConstantVector::GetData<INDEX_TYPE>(b);
 		auto edata = ConstantVector::GetData<INDEX_TYPE>(e);
 
-		auto list = ldata[0];
+		auto sliced = sdata[0];
 		auto begin = bdata[0];
 		auto end = edata[0];
 
-		auto lvalid = !ConstantVector::IsNull(l);
+		auto svalid = !ConstantVector::IsNull(s);
 		auto bvalid = !ConstantVector::IsNull(b);
 		auto evalid = !ConstantVector::IsNull(e);
 
 		// Try to slice
-		if (!lvalid || !SliceValue(list, begin, end, bvalid, evalid)) {
+		if (!svalid || !ClampSlice(sliced, begin, end, bvalid, evalid)) {
 			ConstantVector::SetNull(result, true);
 		} else {
-			rdata[0] = list;
+			rdata[0] = SliceValue<INPUT_TYPE, INDEX_TYPE>(result, sliced, begin, end);
 		}
 	} else {
-		VectorData ldata, bdata, edata;
+		VectorData sdata, bdata, edata;
 
-		l.Orrify(count, ldata);
+		s.Orrify(count, sdata);
 		b.Orrify(count, bdata);
 		e.Orrify(count, edata);
 
 		auto rdata = FlatVector::GetData<INPUT_TYPE>(result);
-		auto rmask = FlatVector::Validity(result);
+		auto &rmask = FlatVector::Validity(result);
 
 		for (idx_t i = 0; i < count; ++i) {
-			auto lidx = ldata.sel->get_index(i);
+			auto sidx = sdata.sel->get_index(i);
 			auto bidx = bdata.sel->get_index(i);
 			auto eidx = edata.sel->get_index(i);
 
-			auto list = ((INPUT_TYPE *)ldata.data)[lidx];
+			auto sliced = ((INPUT_TYPE *)sdata.data)[sidx];
 			auto begin = ((INDEX_TYPE *)bdata.data)[bidx];
 			auto end = ((INDEX_TYPE *)edata.data)[eidx];
 
-			auto lvalid = ldata.validity.RowIsValid(lidx);
+			auto svalid = sdata.validity.RowIsValid(sidx);
 			auto bvalid = bdata.validity.RowIsValid(bidx);
 			auto evalid = edata.validity.RowIsValid(eidx);
 
 			// Try to slice
-			if (!lvalid || !SliceValue(list, begin, end, bvalid, evalid)) {
+			if (!svalid || !ClampSlice(sliced, begin, end, bvalid, evalid)) {
 				rmask.SetInvalid(i);
 			} else {
-				rdata[i] = list;
+				rdata[i] = SliceValue<INPUT_TYPE, INDEX_TYPE>(result, sliced, begin, end);
 			}
 		}
 	}
@@ -127,7 +138,6 @@ static void ExecuteSlice(Vector &result, Vector &l, Vector &b, Vector &e, const 
 }
 
 static void ArraySliceFunction(DataChunk &args, ExpressionState &state, Vector &result) {
-	D_ASSERT(result.GetType().child_types().size() == 1);
 	D_ASSERT(args.ColumnCount() == 3);
 	D_ASSERT(args.data.size() == 3);
 	auto count = args.size();
@@ -139,17 +149,20 @@ static void ArraySliceFunction(DataChunk &args, ExpressionState &state, Vector &
 		}
 	}
 
-	Vector &l = args.data[0];
+	Vector &s = args.data[0];
 	Vector &b = args.data[1];
 	Vector &e = args.data[2];
 
 	switch (result.GetType().id()) {
 	case LogicalTypeId::LIST:
 		// Share the value dictionary as we are just going to slice it
-		ListVector::ReferenceEntry(result, l);
-		ExecuteSlice<list_entry_t, int64_t>(result, l, b, e, count);
+		D_ASSERT(result.GetType().child_types().size() == 1);
+		ListVector::ReferenceEntry(result, s);
+		ExecuteSlice<list_entry_t, int64_t>(result, s, b, e, count);
 		break;
 	case LogicalTypeId::VARCHAR:
+		ExecuteSlice<string_t, int64_t>(result, s, b, e, count);
+		break;
 	default:
 		throw NotImplementedException("Specifier type not implemented");
 	}
@@ -157,18 +170,23 @@ static void ArraySliceFunction(DataChunk &args, ExpressionState &state, Vector &
 
 static unique_ptr<FunctionData> ArraySliceBind(ClientContext &context, ScalarFunction &bound_function,
                                                vector<unique_ptr<Expression>> &arguments) {
-	if (arguments[0]->return_type.id() != LogicalTypeId::LIST) {
-		throw BinderException("ARRAY_SLICE can only operate on LISTs");
+
+	switch (arguments[0]->return_type.id()) {
+	case LogicalTypeId::LIST:
+	case LogicalTypeId::VARCHAR:
+		// The result is the same type
+		bound_function.return_type = arguments[0]->return_type;
+		break;
+	default:
+		throw BinderException("ARRAY_SLICE can only operate on LISTs snd VARCHARs");
 	}
 
-	// The result is the same type
-	bound_function.return_type = arguments[0]->return_type;
 	return make_unique<VariableReturnBindData>(bound_function.return_type);
 }
 
 void ArraySliceFun::RegisterFunction(BuiltinFunctions &set) {
 	// the arguments and return types are actually set in the binder function
-	ScalarFunction fun("array_slice", {LogicalType::ANY, LogicalType::BIGINT, LogicalType::BIGINT}, LogicalType::LIST,
+	ScalarFunction fun("array_slice", {LogicalType::ANY, LogicalType::BIGINT, LogicalType::BIGINT}, LogicalType::ANY,
 	                   ArraySliceFunction, false, ArraySliceBind);
 	fun.varargs = LogicalType::ANY;
 	set.AddFunction(fun);
