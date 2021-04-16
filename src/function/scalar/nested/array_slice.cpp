64338a2182e1065d032a54d5b45f04c3e702e2a1
Richard Wesley <hawkfish@electricfish.com>
2021-04-16 12:18:52 -0700

    Issue #1560: LIST array slice
    Prep work for string variant.

--- src/function/scalar/nested/array_slice.cpp
+++ src/function/scalar/nested/array_slice.cpp
@@ -8,36 +8,125 @@
 
 namespace duckdb {
 
-template <typename T>
-static bool ClampOffset(T &offset, const list_entry_t &list_entry) {
-	if (offset < 0) {
-		if ((idx_t)-offset > list_entry.length) {
+template <typename INPUT_TYPE, typename INDEX_TYPE>
+INDEX_TYPE ValueOffset(const INPUT_TYPE &value) {
+	return 0;
+}
+
+template <>
+int64_t ValueOffset(const list_entry_t &value) {
+	return value.offset;
+}
+
+template <typename INPUT_TYPE, typename INDEX_TYPE>
+INDEX_TYPE ValueLength(const INPUT_TYPE &value) {
+	return 0;
+}
+
+template <>
+int64_t ValueLength(const list_entry_t &value) {
+	return value.length;
+}
+
+template <typename INPUT_TYPE, typename INDEX_TYPE>
+bool ClampIndex(INDEX_TYPE &index, const INPUT_TYPE &value) {
+	const auto offset = ValueOffset<INPUT_TYPE, INDEX_TYPE>(value);
+	const auto length = ValueLength<INPUT_TYPE, INDEX_TYPE>(value);
+	if (index < 0) {
+		if (-index > length) {
 			return false;
 		}
-		offset = list_entry.offset + list_entry.length + offset;
+		index = offset + length + index;
+	} else if (index > length) {
+		return false;
 	}
 	return true;
 }
 
-template <typename T>
-static bool SliceList(list_entry_t &list_entry, T begin, T end, bool list_valid, bool begin_valid, bool end_valid) {
+template <typename INPUT_TYPE, typename INDEX_TYPE>
+void TruncateValue(INPUT_TYPE &value, INDEX_TYPE begin, INDEX_TYPE end) {
+}
+
+template <>
+void TruncateValue(list_entry_t &list, int64_t begin, int64_t end) {
+	list.offset += begin;
+	list.length = end - begin;
+}
+
+template <typename INPUT_TYPE, typename INDEX_TYPE>
+static bool SliceValue(INPUT_TYPE &value, INDEX_TYPE begin, INDEX_TYPE end, bool begin_valid, bool end_valid) {
 	// Clamp offsets
 	begin = begin_valid ? begin : 0;
-	end = end_valid ? end : list_entry.length;
-	if (!ClampOffset(begin, list_entry) || !ClampOffset(end, list_entry) || !list_valid) {
+	end = end_valid ? end : ValueLength<INPUT_TYPE, INDEX_TYPE>(value);
+	if (!ClampIndex(begin, value) || !ClampIndex(end, value)) {
 		return false;
 	}
-	end = MaxValue<T>(begin, end);
+	end = MaxValue<INDEX_TYPE>(begin, end);
 
 	// Trim down the range
-	list_entry.offset += begin;
-	list_entry.length = end - begin;
+	TruncateValue(value, begin, end);
 
 	return true;
+}
+
+template <typename INPUT_TYPE, typename INDEX_TYPE>
+static void ExecuteSlice(Vector &result, Vector &l, Vector &b, Vector &e, const idx_t count) {
+	if (result.GetVectorType() == VectorType::CONSTANT_VECTOR) {
+		auto rdata = ConstantVector::GetData<INPUT_TYPE>(result);
+		auto ldata = ConstantVector::GetData<INPUT_TYPE>(l);
+		auto bdata = ConstantVector::GetData<INDEX_TYPE>(b);
+		auto edata = ConstantVector::GetData<INDEX_TYPE>(e);
+
+		auto list = ldata[0];
+		auto begin = bdata[0];
+		auto end = edata[0];
+
+		auto lvalid = !ConstantVector::IsNull(l);
+		auto bvalid = !ConstantVector::IsNull(b);
+		auto evalid = !ConstantVector::IsNull(e);
+
+		// Try to slice
+		if (!lvalid || !SliceValue(list, begin, end, bvalid, evalid)) {
+			ConstantVector::SetNull(result, true);
+		} else {
+			rdata[0] = list;
+		}
+	} else {
+		VectorData ldata, bdata, edata;
+
+		l.Orrify(count, ldata);
+		b.Orrify(count, bdata);
+		e.Orrify(count, edata);
+
+		auto rdata = FlatVector::GetData<INPUT_TYPE>(result);
+		auto rmask = FlatVector::Validity(result);
+
+		for (idx_t i = 0; i < count; ++i) {
+			auto lidx = ldata.sel->get_index(i);
+			auto bidx = bdata.sel->get_index(i);
+			auto eidx = edata.sel->get_index(i);
+
+			auto list = ((INPUT_TYPE *)ldata.data)[lidx];
+			auto begin = ((INDEX_TYPE *)bdata.data)[bidx];
+			auto end = ((INDEX_TYPE *)edata.data)[eidx];
+
+			auto lvalid = ldata.validity.RowIsValid(lidx);
+			auto bvalid = bdata.validity.RowIsValid(bidx);
+			auto evalid = edata.validity.RowIsValid(eidx);
+
+			// Try to slice
+			if (!lvalid || !SliceValue(list, begin, end, bvalid, evalid)) {
+				rmask.SetInvalid(i);
+			} else {
+				rdata[i] = list;
+			}
+		}
+	}
+
+	result.Verify(count);
 }
 
 static void ArraySliceFunction(DataChunk &args, ExpressionState &state, Vector &result) {
-	D_ASSERT(result.GetType().id() == LogicalTypeId::LIST);
 	D_ASSERT(result.GetType().child_types().size() == 1);
 	D_ASSERT(args.ColumnCount() == 3);
 	D_ASSERT(args.data.size() == 3);
@@ -50,67 +139,20 @@ static void ArraySliceFunction(DataChunk &args, ExpressionState &state, Vector &
 		}
 	}
 
-	Vector &list = args.data[0];
-	Vector &begins = args.data[1];
-	Vector &ends = args.data[2];
+	Vector &l = args.data[0];
+	Vector &b = args.data[1];
+	Vector &e = args.data[2];
 
-	// Share the value dictionary as we are just going to slice it
-	ListVector::ReferenceEntry(result, list);
-
-	if (result.GetVectorType() == VectorType::CONSTANT_VECTOR) {
-		auto result_data = ConstantVector::GetData<list_entry_t>(result);
-		auto list_data = ConstantVector::GetData<list_entry_t>(list);
-		auto begin_data = ConstantVector::GetData<int64_t>(begins);
-		auto end_data = ConstantVector::GetData<int64_t>(ends);
-
-		auto list_entry = list_data[0];
-		auto begin = begin_data[0];
-		auto end = end_data[0];
-
-		auto list_valid = !ConstantVector::IsNull(list);
-		auto begin_valid = !ConstantVector::IsNull(begins);
-		auto end_valid = !ConstantVector::IsNull(ends);
-
-		// Clamp offsets
-		if (!SliceList(list_entry, begin, end, list_valid, begin_valid, end_valid)) {
-			ConstantVector::SetNull(result, true);
-		} else {
-			result_data[0] = list_entry;
-		}
-	} else {
-		VectorData list_data, begins_data, ends_data;
-
-		list.Orrify(count, list_data);
-		begins.Orrify(count, begins_data);
-		ends.Orrify(count, ends_data);
-
-		auto result_data = FlatVector::GetData<list_entry_t>(result);
-		auto result_mask = FlatVector::Validity(result);
-
-		for (idx_t i = 0; i < count; ++i) {
-			auto list_index = list_data.sel->get_index(i);
-			auto begins_index = begins_data.sel->get_index(i);
-			auto ends_index = ends_data.sel->get_index(i);
-
-			auto list_entry = ((list_entry_t *)list_data.data)[list_index];
-			auto begin = ((int64_t *)begins_data.data)[begins_index];
-			auto end = ((int64_t *)ends_data.data)[ends_index];
-
-			auto list_valid = list_data.validity.RowIsValid(list_index);
-			auto begin_valid = begins_data.validity.RowIsValid(begins_index);
-			auto end_valid = ends_data.validity.RowIsValid(ends_index);
-
-			// Clamp offsets
-			if (!SliceList(list_entry, begin, end, list_valid, begin_valid, end_valid)) {
-				result_mask.SetInvalid(i);
-				continue;
-			}
-
-			result_data[i] = list_entry;
-		}
+	switch (result.GetType().id()) {
+	case LogicalTypeId::LIST:
+		// Share the value dictionary as we are just going to slice it
+		ListVector::ReferenceEntry(result, l);
+		ExecuteSlice<list_entry_t, int64_t>(result, l, b, e, count);
+		break;
+	case LogicalTypeId::VARCHAR:
+	default:
+		throw NotImplementedException("Specifier type not implemented");
 	}
-
-	result.Verify(count);
 }
 
 static unique_ptr<FunctionData> ArraySliceBind(ClientContext &context, ScalarFunction &bound_function,
