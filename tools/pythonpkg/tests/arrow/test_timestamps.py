9c9bf9661ec8e94a7c362cc1975b372a8d89f1b0
Pedro Holanda <pedroholanda@gmail.com>
2021-04-26 10:18:23 +0200

    Adding test for #868

---
+++ tools/pythonpkg/tests/arrow/test_timestamps.py
@@ -0,0 +1,20 @@
+import duckdb
+import os
+import sys
+import datetime
+try:
+    import pyarrow as pa
+    can_run = True
+except:
+    can_run = False
+
+# class TestArrowReads(object):
+#     def test_multiple_queries_same_relation(self, duckdb_cursor):
+#         if not can_run:
+#             return
+data = (pa.array([datetime.datetime.now()], type=pa.timestamp('ns')),pa.array([datetime.datetime.now()], type=pa.timestamp('us')),pa.array([datetime.datetime.now()], pa.timestamp('ms')),pa.array([datetime.datetime.now()], pa.timestamp('s')))
+print (data)
+arrow_table = pa.Table.from_arrays([data[0],data[1],data[2],data[3]],['a','b','c','d'])
+print (arrow_table)
+rel = duckdb.from_arrow_table(arrow_table).project('a,b,c,d').arrow()
+rel.validate(full=True)
