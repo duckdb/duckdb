2634e69e40a821acc601d36d8b824bef032782b5
Diego Gomes Tome <diegogomest@gmail.com>
2021-04-26 09:57:09 +0200

    uncommenting branch

--- src/execution/physical_plan/plan_comparison_join.cpp
+++ src/execution/physical_plan/plan_comparison_join.cpp
@@ -115,14 +115,14 @@ unique_ptr<PhysicalOperator> PhysicalPlanGenerator::CreatePlan(LogicalComparison
 			                                      op.left_projection_map, op.right_projection_map, tbl_scan.column_ids,
 			                                      right_index, true, op.estimated_cardinality);
 		}
-		/* 	// equality join with dense keys: Make perfect hash table joins
-		    auto stats = reinterpret_cast<NumericStatistics *>(op.join_stats[0].get()); // lhs stats
-		    auto join_keys_range = stats->max - stats->min;                             // Join Keys Range
-		    if (join_keys_range < PERFECT_HASH_THRESHOLD) {
-		        return make_unique<PhysicalPerfectHashJoin>(op, move(left), move(right), move(op.conditions),
-		   op.join_type, op.left_projection_map, op.right_projection_map, move(op.delim_types),
-		   op.estimated_cardinality);
-		    } */
+		// equality join with dense keys: Make perfect hash table joins
+		auto stats = reinterpret_cast<NumericStatistics *>(op.join_stats[0].get()); // lhs stats
+		auto join_keys_range = stats->max - stats->min;                             // Join Keys Range
+		if (join_keys_range < PERFECT_HASH_THRESHOLD) {
+			return make_unique<PhysicalPerfectHashJoin>(op, move(left), move(right), move(op.conditions), op.join_type,
+			                                            op.left_projection_map, op.right_projection_map,
+			                                            move(op.delim_types), op.estimated_cardinality);
+		}
 
 		// equality join: use hash join
 		plan = make_unique<PhysicalHashJoin>(op, move(left), move(right), move(op.conditions), op.join_type,
