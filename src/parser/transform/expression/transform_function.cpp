8cfea2ea45977f116af960850c0621d4cef0bd10
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-04-27 15:22:57 +0200

    Create an ARRAY_CONSTRUCTOR operator instead of a list_value function in the parser

--- src/parser/transform/expression/transform_function.cpp
+++ src/parser/transform/expression/transform_function.cpp
@@ -212,8 +212,11 @@ unique_ptr<ParsedExpression> Transformer::TransformFuncCall(duckdb_libpgquery::P
 		expr->else_expr = move(children[2]);
 		return move(expr);
 	}
-
-	else if (lowercase_name == "ifnull") {
+	else if (lowercase_name == "construct_array") {
+		auto construct_array = make_unique<OperatorExpression>(ExpressionType::ARRAY_CONSTRUCTOR);
+		construct_array->children = move(children);
+		return move(construct_array);
+	} else if (lowercase_name == "ifnull") {
 		if (children.size() != 2) {
 			throw ParserException("Wrong number of arguments to IFNULL.");
 		}
