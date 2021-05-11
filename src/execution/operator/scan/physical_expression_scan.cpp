1f504ffa4ec4c223c6326a362b68f0177984352d
Richard Wesley <hawkfish@electricfish.com>
2021-05-11 09:24:02 -0700

    Issue #1511: Make GetChunk cost
    Remove unnecessary lock on thread-local object.

--- src/execution/operator/scan/physical_expression_scan.cpp
+++ src/execution/operator/scan/physical_expression_scan.cpp
@@ -10,9 +10,6 @@ public:
 	    : PhysicalOperatorState(op, child), expression_index(0) {
 	}
 
-	//! Thread safety
-	mutex lock;
-
 	//! The current position in the scan
 	idx_t expression_index;
 
@@ -27,19 +24,15 @@ void PhysicalExpressionScan::GetChunkInternal(ExecutionContext &context, DataChu
 		return;
 	}
 
-	{
-		lock_guard<mutex> slock(state->lock);
-		if (state->expression_index == 0) {
-			// first run, fetch the chunk from the child
-			D_ASSERT(children.size() == 1);
-			children[0]->GetChunk(context, state->child_chunk, state->child_state.get());
-			if (state->child_chunk.size() == 0) {
-				return;
-			}
+	if (state->expression_index == 0) {
+		// first run, fetch the chunk from the child
+		D_ASSERT(children.size() == 1);
+		children[0]->GetChunk(context, state->child_chunk, state->child_state.get());
+		if (state->child_chunk.size() == 0) {
+			return;
 		}
 	}
 	// now execute the expressions of the nth expression list for the child chunk list
-	// These are scanned in parallel, so the mutability is not an issue?
 	state->executor = make_unique<ExpressionExecutor>(expressions[state->expression_index]);
 	state->executor->Execute(state->child_chunk, chunk);
 
