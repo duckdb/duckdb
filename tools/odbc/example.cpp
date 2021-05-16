192f88ba67b11ceedcf85788ac2e851446679a62
Hannes Mühleisen <hannes@muehleisen.org>
2021-05-16 23:22:12 +0200

    more types for prepared statement binding

--- tools/odbc/example.cpp
+++ tools/odbc/example.cpp
@@ -2,89 +2,73 @@
 #include <sql.h>
 #include <sqlext.h>
 
-/*
- * see Retrieving ODBC Diagnostics
- * for a definition of extract_error().
- */
-void extract_error(const char *fn, SQLHANDLE handle, SQLSMALLINT type) {
-	SQLINTEGER i = 0;
-	SQLINTEGER native;
-	SQLCHAR state[7];
-	SQLCHAR text[256];
-	SQLSMALLINT len;
-	SQLRETURN ret;
+#include <assert.h>
 
-	fprintf(stderr,
-	        "\n"
-	        "The driver reported the following diagnostics whilst running "
-	        "%s\n\n",
-	        fn);
-
-	do {
-		ret = SQLGetDiagRec(type, handle, ++i, state, &native, text, sizeof(text), &len);
-		if (SQL_SUCCEEDED(ret))
-			printf("%s:%d:%d:%s\n", state, i, native, text);
-	} while (ret == SQL_SUCCESS);
-}
+#define SUCCESS(x)                                                                                                     \
+	do {                                                                                                               \
+		SQLRETURN retval = (x);                                                                                        \
+		if (retval != SQL_SUCCESS) {                                                                                   \
+			fprintf(stderr, "Runtime error: %s returned %d at %s:%d\n", #x, retval, __FILE__, __LINE__);               \
+			return -1;                                                                                                 \
+		}                                                                                                              \
+	} while (0)
 
 // param example: "Driver=/Users/hannes/source/duckdb/build/debug/tools/odbc/libduckdb_odbc.dylib;Database=:memory:"
 int main(int argc, const char **argv) {
 	SQLHENV env;
 	SQLHDBC dbc;
 	SQLHSTMT stmt;
-	SQLRETURN ret;
 
 	/* Allocate an environment handle */
-	SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env);
+	SUCCESS(SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env));
 
 	/* We want ODBC 3 support */
-	SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, (void *)SQL_OV_ODBC3, 0);
+	SUCCESS(SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, (void *)SQL_OV_ODBC3, 0));
 
 	/* Allocate a connection handle */
-	SQLAllocHandle(SQL_HANDLE_DBC, env, &dbc);
+	SUCCESS(SQLAllocHandle(SQL_HANDLE_DBC, env, &dbc));
 
-	/* Connect to the DSN mydsn */
-	/* You will need to change mydsn to one you have created */
-	/* and tested */
-	SQLDriverConnect(dbc, NULL, (SQLCHAR *)argv[1], SQL_NTS, NULL, 0, NULL, SQL_DRIVER_COMPLETE);
+	SUCCESS(SQLDriverConnect(dbc, NULL, (SQLCHAR *)argv[1], SQL_NTS, NULL, 0, NULL, SQL_DRIVER_COMPLETE));
 
 	/* Allocate a statement handle */
-	SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
-	/* Retrieve a list of tables */
+	SUCCESS(SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt));
 
-	SQLExecDirect(stmt, (SQLCHAR *)"create table a (i integer)", SQL_NTS);
-	SQLExecDirect(stmt, (SQLCHAR *)"insert into a values (42), (43)", SQL_NTS);
+	SUCCESS(SQLExecDirect(stmt, (SQLCHAR *)"CREATE TABLE a (i INTEGER)", SQL_NTS));
+	SUCCESS(SQLExecDirect(stmt, (SQLCHAR *)"INSERT INTO a VALUES (42), (43)", SQL_NTS));
 
-	SQLSMALLINT columns; /* number of columns in result-set */
+	SUCCESS(SQLPrepare(stmt, (SQLCHAR *)"SELECT i one, i+1 two, i+2 three FROM a WHERE i > ?", SQL_NTS));
+	int64_t param = 41;
+	SUCCESS(SQLBindParameter(stmt, 1, SQL_PARAM_INPUT, SQL_C_SBIGINT, SQL_UNKNOWN_TYPE, 0, 0, &param, 0, nullptr));
+	SUCCESS(SQLExecute(stmt));
 
-	// SQLBindCol?
+	SQLSMALLINT columns = -1; /* number of columns in result-set */
 
-	SQLPrepare(stmt, (SQLCHAR *)"SELECT i, i+1, i+2 FROM a", SQL_NTS);
-	SQLExecute(stmt);
-
-	SQLNumResultCols(stmt, &columns);
-
-	printf("num_cols=%d\n", columns);
+	SUCCESS(SQLNumResultCols(stmt, &columns));
+	for (SQLUSMALLINT i = 1; i <= columns; i++) {
+		SQLCHAR colname[100];
+		SQLSMALLINT namelength;
+		SUCCESS(SQLDescribeCol(stmt, i, colname, 100, &namelength, nullptr, nullptr, nullptr, nullptr));
+		printf("%s\t", colname);
+	}
+	printf("\n");
 	/* Loop through the rows in the result-set */
 	while (SQLFetch(stmt) == SQL_SUCCESS) {
-		SQLUSMALLINT i;
 		/* Loop through the columns */
-		for (i = 1; i <= columns; i++) {
+		for (SQLUSMALLINT i = 1; i <= columns; i++) {
 			SQLLEN indicator;
 			int int_res;
+
 			/* retrieve column data as a string */
-			ret = SQLGetData(stmt, i, SQL_C_SLONG, &int_res, sizeof(int), &indicator);
-			if (ret == SQL_SUCCESS) {
-				/* Handle null columns */
-				// if (indicator == SQL_NULL_DATA) strcpy(buf, "NULL");
-				printf("  Column %u : %d\n", i, int_res);
-			}
+			SUCCESS(SQLGetData(stmt, i, SQL_C_SLONG, &int_res, sizeof(int), &indicator));
+			printf("%d\t", int_res);
 		}
+		printf("\n");
 	}
 
-	SQLFreeHandle(SQL_HANDLE_STMT, &stmt);
-	SQLFreeHandle(SQL_HANDLE_DBC, &dbc);
-	SQLFreeHandle(SQL_HANDLE_ENV, &env);
+	SUCCESS(SQLFreeStmt(stmt, SQL_CLOSE));
 
-	printf("OK\n");
+	SUCCESS(SQLFreeHandle(SQL_HANDLE_STMT, stmt));
+	// TODO why do those fail?
+	//	SUCCESS(SQLFreeHandle(SQL_HANDLE_DBC, dbc));
+	//	SUCCESS(SQLFreeHandle(SQL_HANDLE_ENV, env));
 }
