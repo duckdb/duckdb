837404939903a8a0ded040e51bca359772df0021
Andre Kohn <kohn.a@outlook.com>
2021-05-17 18:01:54 +0200

    Fix formatting

--- tools/arrow_abi_test/abi_test.cc
+++ tools/arrow_abi_test/abi_test.cc
@@ -24,7 +24,7 @@ static arrow::Result<std::shared_ptr<arrow::Array>> GenI32Seq(int32_t n, int32_t
 	for (int32_t i = 0; i < n; ++i) {
 		builder.Append(next++).ok();
 	}
-    REQUIRE(builder.length() == n);
+	REQUIRE(builder.length() == n);
 	return builder.Finish();
 }
 constexpr size_t ARRAY_SIZE = 1024;
@@ -75,7 +75,7 @@ TEST_CASE("Test random integers") {
 		int32_t start_a = 0, start_b = 0xFFFF, start_c = 0xFFFFF;
 
 		DYNAMIC_SECTION("N=" << n) {
-		    int32_t next_a = start_a, next_b = start_b, next_c = start_c;
+			int32_t next_a = start_a, next_b = start_b, next_c = start_c;
 
 			std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
 			while (n > 0) {
@@ -83,36 +83,36 @@ TEST_CASE("Test random integers") {
 				REQUIRE_RESULT(auto a, GenI32Seq(here, next_a));
 				REQUIRE_RESULT(auto b, GenI32Seq(here, next_b));
 				REQUIRE_RESULT(auto c, GenI32Seq(here, next_c));
-                next_a += here;
-                next_b += here;
-                next_c += here;
+				next_a += here;
+				next_b += here;
+				next_c += here;
 				batches.push_back(arrow::RecordBatch::Make(schema, here, {a, b, c}));
-                n -= here;
+				n -= here;
 			}
 
 			SECTION("Batch reader") {
 				REQUIRE_RESULT(auto reader, arrow::RecordBatchReader::Make(batches, schema));
 
-		        int32_t expected_a = start_a, expected_b = start_b, expected_c = start_c;
+				int32_t expected_a = start_a, expected_b = start_b, expected_c = start_c;
 
-                for (auto next = reader->Next(); next.ok() && next.ValueUnsafe(); next = reader->Next()) {
-                    auto& batch = *next.ValueUnsafe();
-                    REQUIRE(batch.num_columns() == 3);
+				for (auto next = reader->Next(); next.ok() && next.ValueUnsafe(); next = reader->Next()) {
+					auto &batch = *next.ValueUnsafe();
+					REQUIRE(batch.num_columns() == 3);
 
-                    auto a_array = batch.column(0);
-                    auto b_array = batch.column(1);
-                    auto c_array = batch.column(2);
-                    REQUIRE(a_array->type_id() == arrow::Type::INT32);
-                    REQUIRE(b_array->type_id() == arrow::Type::INT32);
-                    REQUIRE(c_array->type_id() == arrow::Type::INT32);
+					auto a_array = batch.column(0);
+					auto b_array = batch.column(1);
+					auto c_array = batch.column(2);
+					REQUIRE(a_array->type_id() == arrow::Type::INT32);
+					REQUIRE(b_array->type_id() == arrow::Type::INT32);
+					REQUIRE(c_array->type_id() == arrow::Type::INT32);
 
-                    for (int64_t i = 0; i < batch.num_rows(); ++i) {
-                        INFO(a_array->ToString());
-                        REQUIRE(reinterpret_cast<const arrow::Int32Array*>(a_array.get())->Value(i) == expected_a++);
-                        REQUIRE(reinterpret_cast<const arrow::Int32Array*>(b_array.get())->Value(i) == expected_b++);
-                        REQUIRE(reinterpret_cast<const arrow::Int32Array*>(c_array.get())->Value(i) == expected_c++);
-                    }
-                }
+					for (int64_t i = 0; i < batch.num_rows(); ++i) {
+						INFO(a_array->ToString());
+						REQUIRE(reinterpret_cast<const arrow::Int32Array *>(a_array.get())->Value(i) == expected_a++);
+						REQUIRE(reinterpret_cast<const arrow::Int32Array *>(b_array.get())->Value(i) == expected_b++);
+						REQUIRE(reinterpret_cast<const arrow::Int32Array *>(c_array.get())->Value(i) == expected_c++);
+					}
+				}
 			}
 
 			SECTION("DuckDB scan") {
@@ -128,6 +128,6 @@ TEST_CASE("Test random integers") {
 				// XXX Never gets here atm
 				(void)result;
 			}
-        }
+		}
 	}
 }
