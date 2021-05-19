91fa1529db2ebcd63da50701d280e3af4c623946
Hannes Mühleisen <hannes@muehleisen.org>
2021-05-19 11:32:22 +0200

    added arrow dep

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,1 +1,1 @@
-install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat"), repos="https://cloud.r-project.org/")
+install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "arrow"), repos="https://cloud.r-project.org/")
