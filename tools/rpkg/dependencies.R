d90c6adaa1375855d45a9c653c829bd72d7f3d5f
Hannes Mühleisen <hannes@muehleisen.org>
2021-05-19 16:04:23 +0200

    some deps for arrow

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,3 +1,4 @@
 install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat"), repos="https://cloud.r-project.org/")
 # arrow nightly for now
+install.packages(c("bit64", "cpp11"), repos="https://cloud.r-project.org/")
 install.packages("arrow", repos = "https://arrow-r-nightly.s3.amazonaws.com")
