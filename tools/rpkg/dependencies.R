32027a6fa14be652f1e4da837c5fd7b714abdc8c
Hannes Mühleisen <hannes@muehleisen.org>
2021-05-24 15:33:47 +0200

    magrittr dep yay

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,3 +1,3 @@
 repo <- "https://cloud.r-project.org/"
 install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat"), repos=repo)
-install.packages("testthat", type="source", repos=repo)
+install.packages(c("testthat", "magrittr"), type="source", repos=repo)
