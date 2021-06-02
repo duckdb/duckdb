f86fd89f6c604ca9f80b08f832a341773bc2cfaa
Hannes Mühleisen <hannes@muehleisen.org>
2021-06-02 15:25:08 +0200

    more nanodbc tests passing

--- tools/odbc/duckdb_odbc.cpp
+++ tools/odbc/duckdb_odbc.cpp
@@ -27,8 +27,21 @@ struct OdbcHandleDbc : public OdbcHandle {
 	unique_ptr<Connection> conn;
 };
 
+struct OdbcBoundCol {
+	OdbcBoundCol() : type(SQL_UNKNOWN_TYPE), ptr(nullptr), len(0), strlen_or_ind(nullptr) {};
+
+	bool IsBound() {
+		return ptr != nullptr;
+	}
+
+	SQLSMALLINT type;
+	SQLPOINTER ptr;
+	SQLLEN len;
+	SQLLEN *strlen_or_ind;
+};
+
 struct OdbcHandleStmt : public OdbcHandle {
-	OdbcHandleStmt(OdbcHandleDbc *dbc_p) : OdbcHandle(OdbcHandleType::STMT), dbc(dbc_p) {
+	OdbcHandleStmt(OdbcHandleDbc *dbc_p) : OdbcHandle(OdbcHandleType::STMT), dbc(dbc_p), rows_fetched_ptr(nullptr) {
 		assert(dbc_p);
 		assert(dbc_p->conn);
 	};
@@ -38,6 +51,7 @@ struct OdbcHandleStmt : public OdbcHandle {
 	unique_ptr<QueryResult> res;
 	unique_ptr<DataChunk> chunk;
 	vector<Value> params;
+	vector<OdbcBoundCol> bound_cols;
 	bool open;
 	row_t chunk_row;
 	SQLULEN *rows_fetched_ptr;
@@ -153,10 +167,15 @@ SQLRETURN SQLSetStmtAttr(SQLHSTMT StatementHandle, SQLINTEGER Attribute, SQLPOIN
 	}
 	case SQL_ATTR_ROW_ARRAY_SIZE: {
 		// this should be 1 (for now!)
+		// TODO allow fetch to put more rows in bound cols
+		auto new_size = (SQLULEN)ValuePtr;
+		if (new_size != 1) {
+			return SQL_ERROR;
+		}
 		return SQL_SUCCESS;
 	}
 	case SQL_ATTR_ROWS_FETCHED_PTR: {
-		stmt->rows_fetched_ptr = Load<SQLULEN *>((data_ptr_t)ValuePtr);
+		stmt->rows_fetched_ptr = (SQLULEN *)ValuePtr;
 		;
 		// fetch needs to set this now
 		return SQL_SUCCESS;
@@ -268,6 +287,8 @@ SQLRETURN SQLPrepare(SQLHSTMT StatementHandle, SQLCHAR *StatementText, SQLINTEGE
 		stmt->res.reset();
 		stmt->chunk.reset();
 		stmt->params.resize(0);
+		stmt->bound_cols.resize(0);
+
 		string query = TextLength == SQL_NTS ? string((const char *)StatementText)
 		                                     : string((const char *)StatementText, (size_t)TextLength);
 		stmt->stmt = stmt->dbc->conn->Prepare(query);
@@ -275,6 +296,8 @@ SQLRETURN SQLPrepare(SQLHSTMT StatementHandle, SQLCHAR *StatementText, SQLINTEGE
 			return SQL_ERROR;
 		}
 		stmt->params.resize(stmt->stmt->n_param);
+		stmt->bound_cols.resize(stmt->stmt->ColumnCount());
+
 		return SQL_SUCCESS;
 	});
 }
@@ -301,6 +324,7 @@ SQLRETURN SQLBindParameter(SQLHSTMT StatementHandle, SQLUSMALLINT ParameterNumbe
 			string param = BufferLength == SQL_NTS ? string((const char *)ParameterValuePtr)
 			                                       : string((const char *)ParameterValuePtr, (size_t)BufferLength);
 			res = Value(param);
+			break;
 		}
 		case SQL_C_TINYINT:
 		case SQL_C_STINYINT:
@@ -353,7 +377,9 @@ SQLRETURN SQLExecute(SQLHSTMT StatementHandle) {
 		stmt->chunk.reset();
 		stmt->chunk_row = -1;
 		stmt->open = false;
-		stmt->rows_fetched_ptr = nullptr;
+		if (stmt->rows_fetched_ptr) {
+			*stmt->rows_fetched_ptr = 0;
+		}
 		stmt->res = stmt->stmt->Execute(stmt->params);
 		if (!stmt->res->success) {
 			return SQL_ERROR;
@@ -394,16 +420,23 @@ SQLRETURN SQLGetData(SQLHSTMT StatementHandle, SQLUSMALLINT Col_or_Param_Num, SQ
 		if (!stmt->chunk) {
 			return SQL_ERROR;
 		}
-		switch (TargetType) {
-		case SQL_C_SLONG: {
-			auto val = stmt->chunk->GetValue(Col_or_Param_Num - 1, stmt->chunk_row).GetValue<int>();
-			assert(BufferLength >= sizeof(int));
-			Store<int>(val, (data_ptr_t)TargetValuePtr);
+		auto val = stmt->chunk->GetValue(Col_or_Param_Num - 1, stmt->chunk_row);
+		if (val.is_null) {
+			if (!StrLen_or_IndPtr) {
+				return SQL_ERROR;
+			}
+			*StrLen_or_IndPtr = SQL_NULL_DATA;
 			return SQL_SUCCESS;
 		}
+
+		switch (TargetType) {
+		case SQL_C_SLONG:
+			assert(BufferLength >= sizeof(int));
+			Store<int>(val.GetValue<int>(), (data_ptr_t)TargetValuePtr);
+			return SQL_SUCCESS;
+
 		case SQL_CHAR: {
-			auto val = stmt->chunk->GetValue(Col_or_Param_Num - 1, stmt->chunk_row).GetValue<string>();
-			auto out_len = snprintf((char *)TargetValuePtr, BufferLength, "%s", val.c_str());
+			auto out_len = snprintf((char *)TargetValuePtr, BufferLength, "%s", val.GetValue<string>().c_str());
 			if (StrLen_or_IndPtr) {
 				*StrLen_or_IndPtr = out_len;
 			}
@@ -418,8 +451,19 @@ SQLRETURN SQLGetData(SQLHSTMT StatementHandle, SQLUSMALLINT Col_or_Param_Num, SQ
 
 SQLRETURN SQLBindCol(SQLHSTMT StatementHandle, SQLUSMALLINT ColumnNumber, SQLSMALLINT TargetType,
                      SQLPOINTER TargetValuePtr, SQLLEN BufferLength, SQLLEN *StrLen_or_IndPtr) {
-	// TODO
-	throw new std::runtime_error("SQLBindCol");
+	return WithStatementPrepared(StatementHandle, [&](OdbcHandleStmt *stmt) {
+		auto col_nr_internal = ColumnNumber - 1;
+		if (col_nr_internal >= stmt->bound_cols.size()) {
+			stmt->bound_cols.resize(col_nr_internal);
+		}
+
+		stmt->bound_cols[col_nr_internal].type = TargetType;
+		stmt->bound_cols[col_nr_internal].ptr = TargetValuePtr;
+		stmt->bound_cols[col_nr_internal].len = BufferLength;
+		stmt->bound_cols[col_nr_internal].strlen_or_ind = StrLen_or_IndPtr;
+
+		return SQL_SUCCESS;
+	});
 }
 
 SQLRETURN SQLFetch(SQLHSTMT StatementHandle) {
@@ -445,13 +489,34 @@ SQLRETURN SQLFetch(SQLHSTMT StatementHandle) {
 			stmt->chunk_row = -1;
 		}
 		if (stmt->rows_fetched_ptr) {
-			*stmt->rows_fetched_ptr = 1;
+			(*stmt->rows_fetched_ptr)++;
 		}
 		stmt->chunk_row++;
+
+		// now fill buffers in fetch if set
+		// TODO actually vectorize this
+		for (idx_t col_idx = 0; col_idx < stmt->stmt->ColumnCount(); col_idx++) {
+			auto bound_buf = stmt->bound_cols[col_idx];
+			if (bound_buf.IsBound()) {
+				if (!SQL_SUCCEEDED(SQLGetData(StatementHandle, col_idx + 1, bound_buf.type, bound_buf.ptr,
+				                              bound_buf.len, bound_buf.strlen_or_ind))) {
+					return SQL_ERROR;
+				}
+			}
+		}
+
 		assert(stmt->chunk);
 		assert(stmt->chunk_row < stmt->chunk->size());
 		return SQL_SUCCESS;
 	});
+}
+
+SQLRETURN SQLFetchScroll(SQLHSTMT StatementHandle, SQLSMALLINT FetchOrientation, SQLLEN FetchOffset) {
+
+	if (FetchOrientation != SQL_FETCH_NEXT) {
+		return SQL_ERROR;
+	}
+	return SQLFetch(StatementHandle);
 }
 
 SQLRETURN SQLDescribeCol(SQLHSTMT StatementHandle, SQLUSMALLINT ColumnNumber, SQLCHAR *ColumnName,
@@ -579,6 +644,10 @@ SQLRETURN SQLGetInfo(SQLHDBC ConnectionHandle, SQLUSMALLINT InfoType, SQLPOINTER
 			SQLFreeHandle(SQL_HANDLE_STMT, stmt);
 			return SQL_ERROR;
 		}
+		if (!SQL_SUCCEEDED(SQLFetch(stmt))) {
+			SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+			return SQL_ERROR;
+		}
 		if (!SQL_SUCCEEDED(SQLGetData(stmt, 1, SQL_C_CHAR, InfoValuePtr, BufferLength, (SQLLEN *)StringLengthPtr))) {
 			SQLFreeHandle(SQL_HANDLE_STMT, stmt);
 			return SQL_ERROR;
@@ -586,6 +655,24 @@ SQLRETURN SQLGetInfo(SQLHDBC ConnectionHandle, SQLUSMALLINT InfoType, SQLPOINTER
 		SQLFreeHandle(SQL_HANDLE_STMT, stmt);
 		return SQL_SUCCESS;
 	}
+	case SQL_NON_NULLABLE_COLUMNS:
+		// TODO assert buffer length >= sizeof(SQLUSMALLINT)
+		Store<SQLUSMALLINT>(SQL_NNC_NON_NULL, (data_ptr_t)InfoValuePtr);
+		return SQL_SUCCESS;
+
+	case SQL_ODBC_INTERFACE_CONFORMANCE:
+		// TODO assert buffer length >= sizeof(SQLUINTEGER)
+
+		Store<SQLUINTEGER>(SQL_OIC_CORE, (data_ptr_t)InfoValuePtr);
+
+		return SQL_SUCCESS;
+
+	case SQL_CREATE_TABLE:
+		// TODO assert buffer length >= sizeof(SQLUINTEGER)
+
+		Store<SQLUINTEGER>(SQL_CT_CREATE_TABLE, (data_ptr_t)InfoValuePtr);
+
+		return SQL_SUCCESS;
 
 	default:
 		return SQL_ERROR;
@@ -607,6 +694,88 @@ SQLRETURN SQLColumns(SQLHSTMT StatementHandle, SQLCHAR *CatalogName, SQLSMALLINT
                      SQLSMALLINT NameLength2, SQLCHAR *TableName, SQLSMALLINT NameLength3, SQLCHAR *ColumnName,
                      SQLSMALLINT NameLength4) {
 	return WithStatement(StatementHandle, [&](OdbcHandleStmt *stmt) { return SQL_ERROR; });
+}
+
+// https://docs.microsoft.com/en-us/sql/odbc/reference/syntax/sqltables-function
+SQLRETURN SQLTables(SQLHSTMT StatementHandle, SQLCHAR *CatalogName, SQLSMALLINT NameLength1, SQLCHAR *SchemaName,
+                    SQLSMALLINT NameLength2, SQLCHAR *TableName, SQLSMALLINT NameLength3, SQLCHAR *TableType,
+                    SQLSMALLINT NameLength4) {
+
+	// TODO make this NTS business a function
+	string catalog_name = NameLength1 == SQL_NTS ? string((const char *)CatalogName)
+	                                             : string((const char *)CatalogName, (size_t)NameLength1);
+
+	string schema_name = NameLength2 == SQL_NTS ? string((const char *)SchemaName)
+	                                            : string((const char *)SchemaName, (size_t)NameLength2);
+
+	string table_name =
+	    NameLength3 == SQL_NTS ? string((const char *)TableName) : string((const char *)TableName, (size_t)NameLength3);
+
+	string table_type = NameLength4 == SQL_NTS ? string((const char *)CatalogName)
+	                                           : string((const char *)CatalogName, (size_t)NameLength4);
+
+	// special cases
+	if (catalog_name == string(SQL_ALL_CATALOGS) && NameLength2 == 0 && NameLength2 == 0) {
+		if (!SQL_SUCCEEDED(SQLExecDirect(StatementHandle,
+		                                 (SQLCHAR *)"SELECT '' \"TABLE_CAT\", NULL \"TABLE_SCHEM\", NULL "
+		                                            "\"TABLE_NAME\", NULL \"TABLE_TYPE\" , NULL \"REMARKS\"",
+		                                 SQL_NTS))) {
+			return SQL_ERROR;
+		}
+		return SQL_SUCCESS;
+	}
+
+	if (schema_name == string(SQL_ALL_SCHEMAS) && NameLength1 == 0 && NameLength3 == 0) {
+		if (!SQL_SUCCEEDED(
+		        SQLExecDirect(StatementHandle,
+		                      (SQLCHAR *)"SELECT '' \"TABLE_CAT\", schema_name \"TABLE_SCHEM\", NULL \"TABLE_NAME\", "
+		                                 "NULL \"TABLE_TYPE\" , NULL \"REMARKS\" FROM information_schema.schemata",
+		                      SQL_NTS))) {
+			return SQL_ERROR;
+		}
+		return SQL_SUCCESS;
+	}
+
+	if (table_type == string(SQL_ALL_TABLE_TYPES) && NameLength1 == 0 && NameLength2 == 0 && NameLength3 == 0) {
+		return SQL_ERROR; // TODO
+	}
+
+	// TODO make this a nice template? also going to use this for SQLColumns etc.
+
+	if (!SQL_SUCCEEDED(SQLPrepare(
+	        StatementHandle,
+	        (SQLCHAR
+	             *)"SELECT table_catalog \"TABLE_CAT\", table_schema \"TABLE_SCHEM\", table_name \"TABLE_NAME\", CASE "
+	               "WHEN table_type='BASE TABLE' THEN 'TABLE' ELSE table_type END \"TABLE_TYPE\" , '' \"REMARKS\"  "
+	               "FROM information_schema.tables WHERE table_schema LIKE ? AND table_name LIKE ? and table_type = ?",
+	        SQL_NTS))) {
+		return SQL_ERROR;
+	}
+
+	if (!SQL_SUCCEEDED(SQLBindParameter(StatementHandle, 1, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_UNKNOWN_TYPE, 0, 0,
+	                                    SchemaName, NameLength2, nullptr))) {
+		return SQL_ERROR;
+	}
+
+	if (!SQL_SUCCEEDED(SQLBindParameter(StatementHandle, 2, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_UNKNOWN_TYPE, 0, 0,
+	                                    TableName, NameLength3, nullptr))) {
+		return SQL_ERROR;
+	}
+
+	if (!SQL_SUCCEEDED(SQLBindParameter(StatementHandle, 3, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_UNKNOWN_TYPE, 0, 0,
+	                                    TableType, NameLength4, nullptr))) {
+		return SQL_ERROR;
+	}
+
+	if (!SQL_SUCCEEDED(SQLExecute(StatementHandle))) {
+		return SQL_ERROR;
+	}
+
+	return SQL_SUCCESS;
+}
+
+SQLRETURN SQLCancel(SQLHSTMT StatementHandle) {
+	return WithStatement(StatementHandle, [&](OdbcHandleStmt *stmt) { return SQL_SUCCESS; });
 }
 
 } // extern "C"
