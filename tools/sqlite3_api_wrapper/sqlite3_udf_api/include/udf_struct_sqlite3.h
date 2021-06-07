0daa1cf05dab5d30139daaae2f7d2b9dd07f7b01
Tiago Kepe <tiagokepe@gmail.com>
2021-06-07 13:07:34 -0300

    Formatting missing changes.

--- tools/sqlite3_api_wrapper/sqlite3_udf_api/include/udf_struct_sqlite3.h
+++ tools/sqlite3_api_wrapper/sqlite3_udf_api/include/udf_struct_sqlite3.h
@@ -19,8 +19,8 @@ struct sqlite3_value {
 	union MemValue {
 		double r;  /* Real value used when MEM_Real is set in flags */
 		int64_t i; /* Integer value used when MEM_Int is set in flags */
-		// int nZero;          /* Extra zero bytes when MEM_Zero and MEM_Blob set */
-		// const char *zPType; /* Pointer type when MEM_Term|MEM_Subtype|MEM_Null */
+		           // int nZero;          /* Extra zero bytes when MEM_Zero and MEM_Blob set */
+		           // const char *zPType; /* Pointer type when MEM_Term|MEM_Subtype|MEM_Null */
 	} u;
 	SQLiteTypeValue type;
 
@@ -32,16 +32,16 @@ struct FuncDef {
 	//   i8 nArg;             /* Number of arguments.  -1 means unlimited */
 	//   u32 funcFlags;       /* Some combination of SQLITE_FUNC_* */
 	void *pUserData; /* User data parameter */
-	//   FuncDef *pNext;      /* Next function with same name */
-	//   void (*xSFunc)(sqlite3_context*,int,sqlite3_value**); /* func or agg-step */
-	//   void (*xFinalize)(sqlite3_context*);                  /* Agg finalizer */
-	//   void (*xValue)(sqlite3_context*);                     /* Current agg value */
-	//   void (*xInverse)(sqlite3_context*,int,sqlite3_value**); /* inverse agg-step */
-	//   const char *zName;   /* SQL name of the function. */
-	//   union {
-	//     FuncDef *pHash;      /* Next with a different name but the same hash */
-	//     FuncDestructor *pDestructor;   /* Reference counted destructor function */
-	//   } u;
+	                 //   FuncDef *pNext;      /* Next function with same name */
+	                 //   void (*xSFunc)(sqlite3_context*,int,sqlite3_value**); /* func or agg-step */
+	                 //   void (*xFinalize)(sqlite3_context*);                  /* Agg finalizer */
+	                 //   void (*xValue)(sqlite3_context*);                     /* Current agg value */
+	                 //   void (*xInverse)(sqlite3_context*,int,sqlite3_value**); /* inverse agg-step */
+	                 //   const char *zName;   /* SQL name of the function. */
+	                 //   union {
+	                 //     FuncDef *pHash;      /* Next with a different name but the same hash */
+	                 //     FuncDestructor *pDestructor;   /* Reference counted destructor function */
+	                 //   } u;
 };
 
 struct sqlite3_context {
@@ -51,7 +51,7 @@ struct sqlite3_context {
 	                      //   Vdbe *pVdbe;            /* The VM that owns this context */
 	                      //   int iOp;                /* Instruction number of OP_Function */
 	int isError;          /* Error code returned by the function. */
-	//   u8 skipFlag;            /* Skip accumulator loading if true */
-	//   u8 argc;                /* Number of arguments */
-	//   sqlite3_value *argv[1]; /* Argument set */
+	                      //   u8 skipFlag;            /* Skip accumulator loading if true */
+	                      //   u8 argc;                /* Number of arguments */
+	                      //   sqlite3_value *argv[1]; /* Argument set */
 };
