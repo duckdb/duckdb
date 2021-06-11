651d7eeb9de31ea718feeb0361d6145c617a6b9f
Tiago Kepe <tiagokepe@gmail.com>
2021-06-11 08:44:31 -0300

    Casting Timestamp types to VARCHAR.

--- src/common/vector_operations/vector_cast.cpp
+++ src/common/vector_operations/vector_cast.cpp
@@ -502,6 +502,10 @@ static void TimestampCastSwitch(Vector &source, Vector &result, idx_t count) {
 static void TimestampNsCastSwitch(Vector &source, Vector &result, idx_t count) {
 	// now switch on the result type
 	switch (result.GetType().id()) {
+	case LogicalTypeId::VARCHAR:
+		// timestamp (ns) to varchar
+		VectorStringCast<timestamp_t, duckdb::CastFromTimestampNS>(source, result, count);
+		break;
 	case LogicalTypeId::TIMESTAMP:
 		// timestamp (ns) to timestamp (us)
 		UnaryExecutor::Execute<timestamp_t, timestamp_t, duckdb::CastTimestampNsToUs>(source, result, count);
@@ -515,6 +519,10 @@ static void TimestampNsCastSwitch(Vector &source, Vector &result, idx_t count) {
 static void TimestampMsCastSwitch(Vector &source, Vector &result, idx_t count) {
 	// now switch on the result type
 	switch (result.GetType().id()) {
+	case LogicalTypeId::VARCHAR:
+		// timestamp (ms) to varchar
+		VectorStringCast<timestamp_t, duckdb::CastFromTimestampMS>(source, result, count);
+		break;
 	case LogicalTypeId::TIMESTAMP:
 		// timestamp (ms) to timestamp (us)
 		UnaryExecutor::Execute<timestamp_t, timestamp_t, duckdb::CastTimestampMsToUs>(source, result, count);
@@ -528,6 +536,10 @@ static void TimestampMsCastSwitch(Vector &source, Vector &result, idx_t count) {
 static void TimestampSecCastSwitch(Vector &source, Vector &result, idx_t count) {
 	// now switch on the result type
 	switch (result.GetType().id()) {
+	case LogicalTypeId::VARCHAR:
+		// timestamp (sec) to varchar
+		VectorStringCast<timestamp_t, duckdb::CastFromTimestampSec>(source, result, count);
+		break;
 	case LogicalTypeId::TIMESTAMP:
 		// timestamp (s) to timestamp (us)
 		UnaryExecutor::Execute<timestamp_t, timestamp_t, duckdb::CastTimestampSecToUs>(source, result, count);
