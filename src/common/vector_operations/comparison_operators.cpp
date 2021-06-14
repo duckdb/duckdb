d5d2194d3ba61482252883124702d3b4070d7211
Richard Wesley <hawkfish@electricfish.com>
2021-06-14 12:16:45 -0700

    Issue #1791: Nested row comparisons
    Implement (in)equality boolean comparison functions
    for nested types

--- src/common/vector_operations/comparison_operators.cpp
+++ src/common/vector_operations/comparison_operators.cpp
@@ -11,6 +11,124 @@
 
 namespace duckdb {
 
+struct ComparisonSelector {
+	template <typename OP>
+	static idx_t Select(Vector &left, Vector &right, const SelectionVector *sel, idx_t count, SelectionVector *true_sel,
+	                    SelectionVector *false_sel) {
+		throw NotImplementedException("Unknown comparison operation!");
+	}
+};
+
+template <>
+inline idx_t ComparisonSelector::Select<duckdb::Equals>(Vector &left, Vector &right, const SelectionVector *sel,
+                                                        idx_t count, SelectionVector *true_sel,
+                                                        SelectionVector *false_sel) {
+	return VectorOperations::Equals(left, right, sel, count, true_sel, false_sel);
+}
+
+template <>
+inline idx_t ComparisonSelector::Select<duckdb::NotEquals>(Vector &left, Vector &right, const SelectionVector *sel,
+                                                           idx_t count, SelectionVector *true_sel,
+                                                           SelectionVector *false_sel) {
+	return VectorOperations::NotEquals(left, right, sel, count, true_sel, false_sel);
+}
+
+template <>
+inline idx_t ComparisonSelector::Select<duckdb::GreaterThan>(Vector &left, Vector &right, const SelectionVector *sel,
+                                                             idx_t count, SelectionVector *true_sel,
+                                                             SelectionVector *false_sel) {
+	return VectorOperations::GreaterThan(left, right, sel, count, true_sel, false_sel);
+}
+
+template <>
+inline idx_t ComparisonSelector::Select<duckdb::GreaterThanEquals>(Vector &left, Vector &right,
+                                                                   const SelectionVector *sel, idx_t count,
+                                                                   SelectionVector *true_sel,
+                                                                   SelectionVector *false_sel) {
+	return VectorOperations::GreaterThanEquals(left, right, sel, count, true_sel, false_sel);
+}
+
+template <>
+inline idx_t ComparisonSelector::Select<duckdb::LessThan>(Vector &left, Vector &right, const SelectionVector *sel,
+                                                          idx_t count, SelectionVector *true_sel,
+                                                          SelectionVector *false_sel) {
+	return VectorOperations::LessThan(left, right, sel, count, true_sel, false_sel);
+}
+
+template <>
+inline idx_t ComparisonSelector::Select<duckdb::LessThanEquals>(Vector &left, Vector &right, const SelectionVector *sel,
+                                                                idx_t count, SelectionVector *true_sel,
+                                                                SelectionVector *false_sel) {
+	return VectorOperations::LessThanEquals(left, right, sel, count, true_sel, false_sel);
+}
+
+static idx_t ComparesNotNull(ValidityMask &vleft, ValidityMask &vright, ValidityMask &vresult, idx_t count,
+                             SelectionVector &not_null) {
+	idx_t valid = 0;
+	for (idx_t i = 0; i < count; ++i) {
+		if (vleft.RowIsValid(i) && vright.RowIsValid(i)) {
+			not_null.set_index(valid++, i);
+		} else {
+			vresult.SetInvalid(i);
+		}
+	}
+	return valid;
+}
+
+template <typename OP>
+static void NestedComparisonExecutor(Vector &left, Vector &right, Vector &result, idx_t count) {
+	const auto left_constant = left.GetVectorType() == VectorType::CONSTANT_VECTOR;
+	const auto right_constant = right.GetVectorType() == VectorType::CONSTANT_VECTOR;
+
+	if ((left_constant && ConstantVector::IsNull(left)) || (right_constant && ConstantVector::IsNull(right))) {
+		// either left or right is constant NULL: result is constant NULL
+		result.SetVectorType(VectorType::CONSTANT_VECTOR);
+		ConstantVector::SetNull(result, true);
+		return;
+	}
+
+	if (left_constant && right_constant) {
+		// both sides are constant, and neither is NULL so just compare one element.
+		result.SetVectorType(VectorType::CONSTANT_VECTOR);
+		SelectionVector true_sel(1);
+		auto match_count = ComparisonSelector::Select<OP>(left, right, nullptr, 1, &true_sel, nullptr);
+		auto result_data = ConstantVector::GetData<bool>(result);
+		result_data[0] = match_count > 0;
+		return;
+	}
+
+	result.SetVectorType(VectorType::FLAT_VECTOR);
+	auto result_data = FlatVector::GetData<bool>(result);
+	auto &validity = FlatVector::Validity(result);
+
+	VectorData leftv, rightv;
+	left.Orrify(count, leftv);
+	right.Orrify(count, rightv);
+
+	SelectionVector true_sel(count);
+	SelectionVector false_sel(count);
+
+	idx_t match_count = 0;
+	if (leftv.validity.AllValid() && rightv.validity.AllValid()) {
+		match_count = ComparisonSelector::Select<OP>(left, right, nullptr, count, &true_sel, &false_sel);
+	} else {
+		SelectionVector not_null(count);
+		count = ComparesNotNull(leftv.validity, rightv.validity, validity, count, not_null);
+		match_count = ComparisonSelector::Select<OP>(left, right, &not_null, count, &true_sel, &false_sel);
+	}
+
+	for (idx_t i = 0; i < match_count; ++i) {
+		const auto idx = true_sel.get_index(i);
+		result_data[idx] = true;
+	}
+
+	const idx_t no_match_count = count - match_count;
+	for (idx_t i = 0; i < no_match_count; ++i) {
+		const auto idx = false_sel.get_index(i);
+		result_data[idx] = false;
+	}
+}
+
 struct ComparisonExecutor {
 private:
 	template <class T, class OP>
@@ -66,6 +184,11 @@ public:
 			break;
 		case PhysicalType::VARCHAR:
 			TemplatedExecute<string_t, OP>(left, right, result, count);
+			break;
+		case PhysicalType::LIST:
+		case PhysicalType::MAP:
+		case PhysicalType::STRUCT:
+			NestedComparisonExecutor<OP>(left, right, result, count);
 			break;
 		default:
 			throw InvalidTypeException(left.GetType(), "Invalid type for comparison");
