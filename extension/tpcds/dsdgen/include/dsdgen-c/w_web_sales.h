0c06beec411ed021fc6d791e2ce0d595382ca28c
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-06-16 14:31:29 +0200

    Format fix

--- extension/tpcds/dsdgen/include/dsdgen-c/w_web_sales.h
+++ extension/tpcds/dsdgen/include/dsdgen-c/w_web_sales.h
@@ -71,8 +71,8 @@ struct W_WEB_SALES_TBL {
 #define WS_WHOLESALE_MAX "100.00"
 #define WS_COUPON_MAX    "0.50"
 #define WS_GIFT_PCT                                                                                                    \
-	7                        /* liklihood that a purchase is shipped to someone else                                   \
-	                          */
+	7 /* liklihood that a purchase is shipped to someone else                                                          \
+	   */
 #define WS_ITEMS_PER_ORDER 9 /* number of lineitems in an order */
 #define WS_MIN_SHIP_DELAY  1 /* time between order date and ship date */
 #define WS_MAX_SHIP_DELAY  120
