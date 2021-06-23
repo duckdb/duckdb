744a2b96c23f6457125676459909fdca110ad995
Tiago Kepe <tiagokepe@gmail.com>
2021-06-23 08:01:23 -0300

    Moved implementation to the source file.

--- tools/odbc/include/statement_functions.hpp
+++ tools/odbc/include/statement_functions.hpp
@@ -4,52 +4,8 @@
 
 namespace duckdb {
 
-static SQLRETURN PrepareStmt(SQLHSTMT StatementHandle, SQLCHAR *StatementText, SQLINTEGER TextLength) {
-	return WithStatement(StatementHandle, [&](OdbcHandleStmt *stmt) {
-		if (stmt->stmt) {
-			stmt->stmt.reset();
-		}
-		if (stmt->res) {
-			stmt->res.reset();
-		}
-		if (stmt->chunk) {
-			stmt->chunk.reset();
-		}
-		stmt->params.resize(0);
-		stmt->bound_cols.resize(0);
+SQLRETURN PrepareStmt(SQLHSTMT StatementHandle, SQLCHAR *StatementText, SQLINTEGER TextLength);
 
-		auto query = OdbcUtils::ReadString(StatementText, TextLength);
-		stmt->stmt = stmt->dbc->conn->Prepare(query);
-		if (!stmt->stmt->success) {
-			return SQL_ERROR;
-		}
-		stmt->params.resize(stmt->stmt->n_param);
-		stmt->bound_cols.resize(stmt->stmt->ColumnCount());
-		return SQL_SUCCESS;
-	});
-}
-
-static SQLRETURN ExecuteStmt(SQLHSTMT StatementHandle) {
-	return WithStatement(StatementHandle, [&](OdbcHandleStmt *stmt) {
-		if (stmt->res) {
-			stmt->res.reset();
-		}
-		if (stmt->chunk) {
-			stmt->chunk.reset();
-		}
-
-		stmt->chunk_row = -1;
-		stmt->open = false;
-		if (stmt->rows_fetched_ptr) {
-			*stmt->rows_fetched_ptr = 0;
-		}
-		stmt->res = stmt->stmt->Execute(stmt->params);
-		if (!stmt->res->success) {
-			return SQL_ERROR;
-		}
-		stmt->open = true;
-		return SQL_SUCCESS;
-	});
-}
+SQLRETURN ExecuteStmt(SQLHSTMT StatementHandle);
 
 } // namespace duckdb
---
+++ tools/odbc/statement_functions.cpp
@@ -0,0 +1,49 @@
+#include "statement_functions.hpp"
+
+SQLRETURN duckdb::PrepareStmt(SQLHSTMT StatementHandle, SQLCHAR *StatementText, SQLINTEGER TextLength) {
+	return WithStatement(StatementHandle, [&](OdbcHandleStmt *stmt) {
+		if (stmt->stmt) {
+			stmt->stmt.reset();
+		}
+		if (stmt->res) {
+			stmt->res.reset();
+		}
+		if (stmt->chunk) {
+			stmt->chunk.reset();
+		}
+		stmt->params.resize(0);
+		stmt->bound_cols.resize(0);
+
+		auto query = OdbcUtils::ReadString(StatementText, TextLength);
+		stmt->stmt = stmt->dbc->conn->Prepare(query);
+		if (!stmt->stmt->success) {
+			return SQL_ERROR;
+		}
+		stmt->params.resize(stmt->stmt->n_param);
+		stmt->bound_cols.resize(stmt->stmt->ColumnCount());
+		return SQL_SUCCESS;
+	});
+}
+
+SQLRETURN duckdb::ExecuteStmt(SQLHSTMT StatementHandle) {
+	return WithStatement(StatementHandle, [&](OdbcHandleStmt *stmt) {
+		if (stmt->res) {
+			stmt->res.reset();
+		}
+		if (stmt->chunk) {
+			stmt->chunk.reset();
+		}
+
+		stmt->chunk_row = -1;
+		stmt->open = false;
+		if (stmt->rows_fetched_ptr) {
+			*stmt->rows_fetched_ptr = 0;
+		}
+		stmt->res = stmt->stmt->Execute(stmt->params);
+		if (!stmt->res->success) {
+			return SQL_ERROR;
+		}
+		stmt->open = true;
+		return SQL_SUCCESS;
+	});
+}
