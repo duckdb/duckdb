ed3f5d49f47eac21293691d1633b7b30af9bcce7
Elliana May <me@mause.me>
2021-07-07 14:55:30 +0800

    Use empty fixture

--- tools/pythonpkg/tests/api/test_dbapi10.py
+++ tools/pythonpkg/tests/api/test_dbapi10.py
@@ -5,5 +5,5 @@ class TestCursorDescription(object):
         description = duckdb_cursor.execute("SELECT * FROM integers").description
         assert description == [('i', None, None, None, None, None, None)]
 
-    def test_none_description(self, duckdb_cursor):
-        assert duckdb_cursor.description is None
+    def test_none_description(self, duckdb_empty_cursor):
+        assert duckdb_empty_cursor.description is None
