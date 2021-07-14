a99b2a6b2eaf951e687840863e59e41f8565b36b
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-07-14 13:48:57 +0200

    Initial config implementation

--- src/main/duckdb-c.cpp
+++ src/main/duckdb-c.cpp
@@ -4,6 +4,7 @@
 #include "duckdb/common/arrow.hpp"
 #include "duckdb/common/vector_operations/vector_operations.hpp"
 #include "duckdb/main/appender.hpp"
+#include "duckdb/main/config.hpp"
 
 #include "duckdb.h"
 #include "duckdb.hpp"
@@ -36,6 +37,64 @@ struct DatabaseData {
 	DuckDB *database;
 };
 } // namespace duckdb
+
+// config
+duckdb_state duckdb_create_config(duckdb_config *out_config) {
+	if (!out_config) {
+		return DuckDBError;
+	}
+	DBConfig *config;
+	try {
+		config = new DBConfig();
+	} catch (...) {
+		return DuckDBError;
+	}
+	*out_config = (duckdb_config)config;
+	return DuckDBSuccess;
+}
+
+size_t duckdb_config_count() {
+	return DBConfig::GetOptionCount();
+}
+
+duckdb_state duckdb_get_config_flag(size_t index, char **out_name, char **out_description) {
+	auto option = DBConfig::GetOptionByIndex(index);
+	if (!option) {
+		return DuckDBError;
+	}
+	if (out_name) {
+		*out_name = option->name;
+	}
+	if (out_description) {
+		*out_description = option->description;
+	}
+	return DuckDBSuccess;
+}
+
+duckdb_state duckdb_set_config(duckdb_config config, const char *name, const char *option) {
+	if (!config) {
+		return DuckDBError;
+	}
+	auto option = DBConfig::GetOptionByName(name);
+	if (!option) {
+		return DuckDBError;
+	}
+	auto db_config = (DBConfig *) config;
+	config->
+
+}
+
+void duckdb_destroy_config(duckdb_config *config) {
+	if (!config) {
+		return;
+	}
+	if (*config) {
+		auto db_config = (DBConfig *) *config;
+		delete db_config;
+		*db_config = nullptr;
+	}
+}
+
 duckdb_state duckdb_open(const char *path, duckdb_database *out) {
 	auto wrapper = new DatabaseData();
 	try {
