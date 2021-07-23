872b9338ae4fb013e3124db564293fede97dd699
Tiago Kepe <tiagokepe@gmail.com>
2021-07-23 17:21:19 -0300

    Formating odbc fetcher files.

--- tools/odbc/duckdb_odbc.cpp
+++ tools/odbc/duckdb_odbc.cpp
@@ -5,10 +5,10 @@
 
 duckdb::OdbcHandleStmt::OdbcHandleStmt(OdbcHandleDbc *dbc_p)
     : OdbcHandle(OdbcHandleType::STMT), dbc(dbc_p), rows_fetched_ptr(nullptr), row_count(0) {
-    D_ASSERT(dbc_p);
-    D_ASSERT(dbc_p->conn);
+	D_ASSERT(dbc_p);
+	D_ASSERT(dbc_p->conn);
 
-    odbc_fetcher = make_unique<OdbcFetch>();
+	odbc_fetcher = make_unique<OdbcFetch>();
 }
 
 duckdb::OdbcHandleStmt::~OdbcHandleStmt() = default;
--- tools/odbc/include/odbc_fetch.hpp
+++ tools/odbc/include/odbc_fetch.hpp
@@ -9,30 +9,26 @@ namespace duckdb {
 
 class OdbcHandleStmt;
 
-enum class FetchOrientation : uint8_t {
-    COLUMN = 0,
-    ROW = 1
-};
+enum class FetchOrientation : uint8_t { COLUMN = 0, ROW = 1 };
 
 class OdbcFetch {
 public:
-    FetchOrientation orientation;
-    SQLULEN rows_to_fetch;
-   	SQLPOINTER row_length;
-    SQLUSMALLINT *row_status_buff;
+	FetchOrientation orientation;
+	SQLULEN rows_to_fetch;
+	SQLPOINTER row_length;
+	SQLUSMALLINT *row_status_buff;
 
 public:
-    OdbcFetch(): orientation(FetchOrientation::COLUMN), rows_to_fetch(1) {
-    }
-    ~OdbcFetch();
+	OdbcFetch() : orientation(FetchOrientation::COLUMN), rows_to_fetch(1) {
+	}
+	~OdbcFetch();
 
-    SQLRETURN Fetch(SQLHSTMT statement_handle, OdbcHandleStmt *stmt);
+	SQLRETURN Fetch(SQLHSTMT statement_handle, OdbcHandleStmt *stmt);
 
 private:
-    SQLRETURN ColumnWise(SQLHSTMT statement_handle, OdbcHandleStmt *stmt);
+	SQLRETURN ColumnWise(SQLHSTMT statement_handle, OdbcHandleStmt *stmt);
 
-    SQLRETURN RowWise(SQLHSTMT statement_handle, OdbcHandleStmt *stmt);
-
+	SQLRETURN RowWise(SQLHSTMT statement_handle, OdbcHandleStmt *stmt);
 };
 } // namespace duckdb
 
