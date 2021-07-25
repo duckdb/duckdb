719b2547bcbc929f719ac745269f243f5a92f6c2
Tiago Kepe <tiagokepe@gmail.com>
2021-07-25 13:20:21 -0300

    Adjusting ODBC CMakeLists.txt

--- tools/odbc/statement.cpp
+++ tools/odbc/statement.cpp
@@ -187,7 +187,7 @@ SQLRETURN SQLColAttribute(SQLHSTMT statement_handle, SQLUSMALLINT column_number,
 		}
 		case SQL_DESC_OCTET_LENGTH:
 			// 0 DuckDB doesn't provide octet length
-			if(numeric_attribute_ptr) {
+			if (numeric_attribute_ptr) {
 				*numeric_attribute_ptr = 0;
 			}
 			return SQL_SUCCESS;
