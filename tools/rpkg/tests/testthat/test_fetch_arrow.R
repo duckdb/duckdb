9e02c75f6212ce739f2dbcb5beccebf745fd3c40
Jonathan Keane <jkeane@gmail.com>
2021-08-03 10:26:56 -0500

    Check if parquet is installed as a proxy for fully-functional arrow

--- tools/rpkg/tests/testthat/test_fetch_arrow.R
+++ tools/rpkg/tests/testthat/test_fetch_arrow.R
@@ -1,17 +1,19 @@
 library("testthat")
 library("DBI")
 
+skip_on_os("windows")
+skip_if_not_installed("arrow", "4.0.1")
+# Skip if parquet is not a capabiltiy as an indicator that Arrow is fully installed.
+full_arrow <- "parquet" %in% names(arrow::arrow_info()$capabilities) && arrow::arrow_info()$capabilities["parquet"]
+skip_if_not(full_arrow, message = "The installed Arrow is not fully featured, skipping Arrow integration tests")
 
 test_that("duckdb_fetch_arrow() test table over vector size", {
-    skip_on_os("windows")
-    skip_if_not_installed("arrow", "4.0.1")
-
     con <- dbConnect(duckdb::duckdb())
     dbExecute(con, paste0("CREATE TABLE test (a  INTEGER)"))
     for (value in 1:10000){
-      dbExecute(con, sprintf("INSERT INTO  test VALUES(%d);", value)) 
+      dbExecute(con, sprintf("INSERT INTO  test VALUES(%d);", value))
     }
-    dbExecute(con, "INSERT INTO  test VALUES(NULL);") 
+    dbExecute(con, "INSERT INTO  test VALUES(NULL);")
     arrow_table <- duckdb::duckdb_fetch_arrow(dbSendQuery(con, "SELECT * FROM test", arrow=TRUE))
     duckdb::duckdb_register_arrow(con, "testarrow", arrow_table)
 
@@ -22,9 +24,6 @@ test_that("duckdb_fetch_arrow() test table over vector size", {
 })
 
 test_that("duckdb_fetch_arrow() empty table", {
-    skip_on_os("windows")
-    skip_if_not_installed("arrow", "4.0.1")
-
     con <- dbConnect(duckdb::duckdb())
     dbExecute(con, paste0("CREATE TABLE test (a  INTEGER)"))
 
@@ -38,13 +37,10 @@ test_that("duckdb_fetch_arrow() empty table", {
 })
 
 test_that("duckdb_fetch_arrow() table with only nulls", {
-    skip_on_os("windows")
-    skip_if_not_installed("arrow", "4.0.1")
-
     con <- dbConnect(duckdb::duckdb())
     dbExecute(con, paste0("CREATE TABLE test (a  INTEGER)"))
 
-    dbExecute(con, "INSERT INTO  test VALUES(NULL);") 
+    dbExecute(con, "INSERT INTO  test VALUES(NULL);")
     arrow_table <- duckdb::duckdb_fetch_arrow(dbSendQuery(con, "SELECT * FROM test", arrow=TRUE))
     duckdb::duckdb_register_arrow(con, "testarrow", arrow_table)
 
@@ -55,14 +51,11 @@ test_that("duckdb_fetch_arrow() table with only nulls", {
 })
 
 test_that("duckdb_fetch_arrow() table with prepared statement", {
-    skip_on_os("windows")
-    skip_if_not_installed("arrow", "4.0.1")
-
     con <- dbConnect(duckdb::duckdb())
     dbExecute(con, paste0("CREATE TABLE test (a  INTEGER)"))
     dbExecute(con, paste0("PREPARE s1 AS INSERT INTO test VALUES ($1), ($2 / 2)"))
     for (value in 1:5000){
-      dbExecute(con, sprintf("EXECUTE s1 (%d, %d);", value,value*2)) 
+      dbExecute(con, sprintf("EXECUTE s1 (%d, %d);", value,value*2))
     }
     arrow_table <- duckdb::duckdb_fetch_arrow(dbSendQuery(con, "SELECT * FROM test", arrow=TRUE))
     duckdb::duckdb_register_arrow(con, "testarrow", arrow_table)
