e59855d033ab1761a38dbf97f8d0d741fba74595
Laurens Kuiper <laurens.kuiper@cwi.nl>
2021-08-13 15:33:22 +0200

    let's try a simpler single-threaded unload approach first

--- src/include/duckdb/storage/buffer_manager.hpp
+++ src/include/duckdb/storage/buffer_manager.hpp
@@ -123,11 +123,7 @@ private:
 	//! The temporary id used for managed buffers
 	atomic<block_id_t> temporary_id;
 	//!
-	mutex memory_full_lock;
-	//!
 	mutex unload_lock;
-	//! When we hit the memory limit we try to unload until current_memory/maximum_memory = unload_until
-	atomic<double> unload_until_fraction;
 
 	//! Lock for current_memory (used for verification)
 	mutex current_memory_lock;
