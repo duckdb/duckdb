6cb254a9127a28e69a329ac9a6b285e303bfac7d
Laurens Kuiper <laurens.kuiper@cwi.nl>
2021-08-18 16:46:05 +0200

    remove single-threaded write again

--- src/storage/buffer_manager.cpp
+++ src/storage/buffer_manager.cpp
@@ -46,12 +46,6 @@ unique_ptr<BufferHandle> BlockHandle::Load(shared_ptr<BlockHandle> &handle) {
 
 	auto &buffer_manager = BufferManager::GetBufferManager(handle->db);
 	auto &block_manager = BlockManager::GetBlockManager(handle->db);
-
-	if (!buffer_manager.io_lock.try_lock()) {
-		buffer_manager.io_lock.lock();
-	}
-	buffer_manager.io_lock.unlock();
-
 	if (handle->block_id < MAXIMUM_BLOCK) {
 		auto block = make_unique<Block>(Allocator::Get(handle->db), handle->block_id);
 		block_manager.Read(*block);
@@ -350,99 +344,34 @@ void BufferManager::Unpin(shared_ptr<BlockHandle> &handle) {
 }
 
 bool BufferManager::EvictBlocks(idx_t extra_memory, idx_t memory_limit) {
-	PurgeQueue();
 #ifdef DEBUG
 	VerifyCurrentMemory();
 #endif
-	current_memory += extra_memory;
-	if (current_memory < memory_limit) {
-		return true;
-	}
-
-	if (!writer_lock.try_lock()) {
-		// memory is full, wait until there is space
-		lock_guard<mutex> wait_until_space(memory_full_lock);
-		return true;
-	}
-
-	// memory is full and this thread is the writer
-	memory_full_lock.lock();
-	bool mf_locked = true;
-
-	// unload until we have some room
-	vector<shared_ptr<BlockHandle>> handles_to_unload;
-	idx_t memory_to_free = 0;
-
 	unique_ptr<BufferEvictionNode> node;
-	while (current_memory - memory_to_free > 0.8 * memory_limit) {
-		// lock so the other threads have to wait until there is space
-		if (!mf_locked && current_memory > memory_limit) {
-			memory_full_lock.lock();
-			mf_locked = true;
-		}
-
+	current_memory += extra_memory;
+	while (current_memory > memory_limit) {
 		// get a block to unpin from the queue
 		if (!queue->q.try_dequeue(node)) {
-			// we cannot unload
 			current_memory -= extra_memory;
-			break;
+			return false;
 		}
-		// get a reference to the underlying block pointer
+		// try to get a reference to the underlying block pointer
 		auto handle = node->TryGetBlockHandle();
 		if (!handle) {
+			// we did not get a reference: the node was redundant
 			continue;
 		}
 		// we might be able to free this block: grab the mutex and check if we can free it
-		handle->lock.lock();
-		if (!node->CanUnload(*handle)) {
+		lock_guard<mutex> lock(handle->lock);
+		if (!handle->CanUnload()) {
 			// something changed in the mean-time, bail out
-			handle->lock.unlock();
 			continue;
 		}
 		// hooray, we can unload the block
-		// add the block to the blocks that will be unloaded
-		memory_to_free += handle->memory_usage;
-		handles_to_unload.push_back(move(handle));
-		// if we can unload 5% of memory, do it
-		if (memory_to_free >= 0.05 * memory_limit) {
-			lock_guard<mutex> lock(io_lock);
-			for (auto &h : handles_to_unload) {
-				h->Unload();
-			}
-			for (auto &h : handles_to_unload) {
-				h->lock.unlock();
-			}
-			handles_to_unload.clear();
-			if (mf_locked && current_memory <= memory_limit) {
-				memory_full_lock.unlock();
-				mf_locked = false;
-			}
-		}
+		// release the memory and mark the block as unloaded
+		handle->Unload();
 	}
-	// unload remaining blocks
-	{
-		lock_guard<mutex> lock(io_lock);
-		for (auto &h : handles_to_unload) {
-			h->Unload();
-		}
-		for (auto &h : handles_to_unload) {
-			h->lock.unlock();
-		}
-		if (mf_locked && current_memory <= memory_limit) {
-			memory_full_lock.unlock();
-			mf_locked = false;
-		}
-	}
-	// unlock io lock again
-	writer_lock.unlock();
-	if (mf_locked) {
-		// we could not free up enough space
-		memory_full_lock.unlock();
-		return false;
-	} else {
-		// we freed up enough space!
-		return true;
-	}
+	return true;
 }
 
 void BufferManager::PurgeQueue() {
