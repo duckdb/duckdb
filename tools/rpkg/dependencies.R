e6edab84bb5571a9c86b8e2618bccc5e5bb762db
Hannes Mühleisen <hannes@muehleisen.org>
2021-08-30 15:01:03 +0200

    mc

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,1 +1,1 @@
-install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow"), repos="https://cloud.r-project.org/", type="source")
+install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow"), repos="https://cloud.r-project.org/", type="source", Ncpus=parallel::detectCores())
