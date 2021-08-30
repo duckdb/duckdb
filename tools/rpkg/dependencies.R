dbc9456708bf031b057548167b935b382f5a1b4d
Hannes Mühleisen <hannes@muehleisen.org>
2021-08-30 15:10:25 +0200

    speed up arrow install grr

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,1 +1,2 @@
-install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow"), repos="https://cloud.r-project.org/", type="source", Ncpus=parallel::detectCores())
+install.packages(c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11"), repos="https://cloud.r-project.org/", type="source", Ncpus=parallel::detectCores())
+install.packages("arrow", repos = c("https://arrow-r-nightly.s3.amazonaws.com", "https://cloud.r-project.org/"))
