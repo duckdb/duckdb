081544a156c7a3608008bcb91a0833eb7c344241
Pedro Holanda <pedroholanda@gmail.com>
2021-09-01 13:59:59 +0200

    I guess the description cant be empty

--- scripts/regression_test.py
+++ scripts/regression_test.py
@@ -89,6 +89,9 @@ def regression_test(threshold):
         else:
             os.system("echo \"REGRESSION_STATE=failure\" >> $GITHUB_ENV")
 
+        if description == '':
+            description = "No Regression or Speed Up."
+            
         os.system("echo \"REGRESSION_DESCRIPTION="+description+"\" >> $GITHUB_ENV")
 
 regression_test(0.1)
