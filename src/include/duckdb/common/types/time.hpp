8d97a0f6f5d788f63d8953e7232a390bf58028aa
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-09-13 13:49:10 +0200

    Format fix

--- src/include/duckdb/common/types/time.hpp
+++ src/include/duckdb/common/types/time.hpp
@@ -29,8 +29,8 @@ public:
 	DUCKDB_API static dtime_t FromTime(int32_t hour, int32_t minute, int32_t second, int32_t microseconds = 0);
 
 	//! Extract the time from a given timestamp object
-	DUCKDB_API static void Convert(dtime_t time, int32_t &out_hour, int32_t &out_min, int32_t &out_sec, 
-				       int32_t &out_micros);
+	DUCKDB_API static void Convert(dtime_t time, int32_t &out_hour, int32_t &out_min, int32_t &out_sec,
+	                               int32_t &out_micros);
 
 	DUCKDB_API static string ConversionError(const string &str);
 	DUCKDB_API static string ConversionError(string_t str);
