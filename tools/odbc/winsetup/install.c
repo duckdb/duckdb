60e10e79a568931b6051f48920415389737e3833
Tiago Kepe <tiagokepe@gmail.com>
2021-10-02 10:08:22 -0300

    ODBC: added LICENSE file into winsetup.

--- tools/odbc/winsetup/install.c
+++ tools/odbc/winsetup/install.c
@@ -1,9 +1,5 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0.  If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * Copyright 1997 - July 2008 CWI, August 2008 - 2021 MonetDB B.V.
+/* Copyright 1997 - July 2008 CWI, August 2008 - 2021 MonetDB B.V.
+ * All Rights Reserved. Use of this source code is governed by a MPL/2.0 license that can be found in the LICENSE file.
  */
 
 #include <windows.h>
