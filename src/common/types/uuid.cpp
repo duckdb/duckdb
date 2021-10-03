be6d766486be45b1f5b9280a739bedfc4913e49f
wangfenjin <wangfenj@gmail.com>
2021-10-03 12:14:05 +0800

    fix order by issue; add more tests
    Change-Id: I6f2bf3d6f0adbbccade1d1787337deb86522b99e

--- src/common/types/uuid.cpp
+++ src/common/types/uuid.cpp
@@ -51,19 +51,41 @@ bool UUID::FromString(string str, hugeint_t &result) {
 		}
 		count++;
 	}
+	// Flip the first bit to make `order by uuid` same as `order by uuid::varchar`
+	result.upper ^= (int64_t(1) << 63);
 	return count == 32;
 }
 
-void UUID::ToString(hugeint_t input, char *buff) {
-	std::snprintf(buff, 36, "%08" PRIx64 "-%04" PRIx64 "-%04" PRIx64 "-%04" PRIx64 "-%011" PRIx64,
-	              input.upper >> 32 & 0xFFFFFFFF, input.upper >> 16 & 0xFFFF, input.upper & 0xFFFF,
-	              input.lower >> 48 & 0xFFFF, input.lower >> 4 & 0xFFFFFFFFFFF);
-	// snprintf always append null termintor at the end
-	// which requires buff size be 37
-	// but our buffer size is 36
-	char last[2];
-	std::snprintf(last, 2, "%01" PRIx64, input.lower & 0xF);
-	buff[35] = last[0];
+void UUID::ToString(hugeint_t input, char *buf) {
+	auto byte_to_hex = [](char byte_val, char *buf, idx_t &pos) {
+		static char const HEX_DIGITS[] = "0123456789abcdef";
+		buf[pos++] = HEX_DIGITS[(byte_val >> 4) & 0xf];
+		buf[pos++] = HEX_DIGITS[byte_val & 0xf];
+	};
+
+	// Flip back before convert to string
+	int64_t upper = input.upper ^ (int64_t(1) << 63);
+	idx_t pos = 0;
+	byte_to_hex(upper >> 56 & 0xFF, buf, pos);
+	byte_to_hex(upper >> 48 & 0xFF, buf, pos);
+	byte_to_hex(upper >> 40 & 0xFF, buf, pos);
+	byte_to_hex(upper >> 32 & 0xFF, buf, pos);
+	buf[pos++] = '-';
+	byte_to_hex(upper >> 24 & 0xFF, buf, pos);
+	byte_to_hex(upper >> 16 & 0xFF, buf, pos);
+	buf[pos++] = '-';
+	byte_to_hex(upper >> 8 & 0xFF, buf, pos);
+	byte_to_hex(upper & 0xFF, buf, pos);
+	buf[pos++] = '-';
+	byte_to_hex(input.lower >> 56 & 0xFF, buf, pos);
+	byte_to_hex(input.lower >> 48 & 0xFF, buf, pos);
+	buf[pos++] = '-';
+	byte_to_hex(input.lower >> 40 & 0xFF, buf, pos);
+	byte_to_hex(input.lower >> 32 & 0xFF, buf, pos);
+	byte_to_hex(input.lower >> 24 & 0xFF, buf, pos);
+	byte_to_hex(input.lower >> 16 & 0xFF, buf, pos);
+	byte_to_hex(input.lower >> 8 & 0xFF, buf, pos);
+	byte_to_hex(input.lower & 0xFF, buf, pos);
 }
 
 } // namespace duckdb
