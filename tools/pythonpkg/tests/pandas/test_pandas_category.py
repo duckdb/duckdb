5a918144eee4cff2c3a1f41513a36bbea2421e8f
Pedro Holanda <pedroholanda@gmail.com>
2021-10-08 11:00:40 +0200

    More on codecov

--- tools/pythonpkg/tests/pandas/test_pandas_category.py
+++ tools/pythonpkg/tests/pandas/test_pandas_category.py
@@ -1,6 +1,7 @@
 import duckdb
 import pandas as pd
 import numpy
+import pytest
 
 def check_category_equal(category):
     df_in = pd.DataFrame({
@@ -15,17 +16,28 @@ def check_create_table(category):
     conn.execute ("PRAGMA enable_verification")
     df_in = pd.DataFrame({
     'x': pd.Categorical(category, ordered=True),
+    'y': pd.Categorical(category, ordered=True)
     })
+
+    df_out = duckdb.query_df(df_in, "data", "SELECT * FROM data").df()
+    assert df_in.equals(df_out)
+
     conn.execute("CREATE TABLE t1 AS SELECT * FROM df_in")
     conn.execute("CREATE TABLE t2 AS SELECT * FROM df_in")
 
     # Do a insert to trigger string -> cat 
-    conn.execute("INSERT INTO t1 VALUES ('2')")
+    conn.execute("INSERT INTO t1 VALUES ('2','2')")
 
-    res = conn.execute("SELECT * FROM t1 where x = '1'").fetchall()
+    res = conn.execute("SELECT x FROM t1 where x = '1'").fetchall()
     assert res == [('1',)]
 
     res =  conn.execute("SELECT t1.x FROM t1 inner join t2 on (t1.x = t2.x)").fetchall()
+    assert res == conn.execute("SELECT x FROM t1").fetchall()
+    
+    # Can't compare different ENUMs
+    with pytest.raises(Exception):
+        conn.execute("SELECT * FROM t1 inner join t2 on (t1.x = t2.y)").fetchall()
+    
     assert res == conn.execute("SELECT x FROM t1").fetchall()
     # Triggering the cast with ENUM as a src
     conn.execute("ALTER TABLE t1 ALTER x SET DATA TYPE VARCHAR")
