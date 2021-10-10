8b1eb8adeb8d7b58779a964effc7a4c474f13a40
Jarrad Whitaker <akdor1154@gmail.com>
2021-10-11 00:14:52 +1100

    add autogen header and regenerate stubs

--- scripts/generate_python_stubs.sh
+++ scripts/generate_python_stubs.sh
@@ -19,3 +19,19 @@ stubgen \
 # We need this while `duckdb` is a single file module and not a package.
 # If `duckdb` becomes a proper package, this can be removed.
 mv "${OUTPUT_DIR}/duckdb.pyi" "${OUTPUT_DIR}/__init__.pyi"
+
+add_header() (
+	{ set +x; } 2>/dev/null
+	cat - "$1" > "$1.tmp" <<EOF
+# AUTOMATICALLY GENERATED FILE
+# to regenerate, run scripts/generate_python_stubs.sh
+EOF
+	mv "$1.tmp" "$1"
+)
+
+find "${OUTPUT_DIR}" -name "*.pyi" -print |
+	while read pyi_name
+	do
+		add_header "${pyi_name}"
+	done
+
