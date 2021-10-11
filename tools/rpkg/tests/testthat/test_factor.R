8aed295921e65ea4320348026e74f461708bc043
Hannes Mühleisen <hannes@muehleisen.org>
2021-10-11 14:27:21 +0200

    fixing dbWriteTable

--- tools/rpkg/tests/testthat/test_factor.R
+++ tools/rpkg/tests/testthat/test_factor.R
@@ -4,13 +4,17 @@ library("testthat")
 test_that("factors can be round tripped", {
   con <- dbConnect(duckdb::duckdb())
 
-  df0 <- data.frame(f=as.factor(c('a', 'b', NA)))
+  df0 <- data.frame(
+    a=c(1,2, 3), 
+    f=as.factor(c('a', 'b', NA)), 
+    x=c('Hello', 'World', 'Etc'), 
+    stringsAsFactors=FALSE)
+  
   duckdb::duckdb_register(con, "df0", df0)
   df1 <- dbReadTable(con, "df0")
   expect_equal(df0, df1)
 
   dbWriteTable(con, "df1", df0)
   df2 <- dbReadTable(con, "df1")
-  # This becomes a string because we cannot do a CREATE TABLE (a ENUM)
-  expect_equal(as.character(df0$f), df2$f)
+  expect_equal(df0, df2)
 })
