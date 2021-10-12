c641c7226ad0ab74fc9996180e58f9f8e2d0d226
Pedro Holanda <pedroholanda@gmail.com>
2021-10-12 13:14:33 +0200

    Adding dependency on Enum Types -> Tables

--- src/planner/binder/statement/bind_create_table.cpp
+++ src/planner/binder/statement/bind_create_table.cpp
@@ -163,6 +163,11 @@ unique_ptr<BoundCreateTableInfo> Binder::BindCreateTableInfo(unique_ptr<CreateIn
 	// bind collations to detect any unsupported collation errors
 	for (auto &column : base.columns) {
 		ExpressionBinder::TestCollation(context, StringType::GetCollation(column.type));
+		BindLogicalType(context, column.type);
+		if (column.type.id() == LogicalTypeId::ENUM) {
+			// We add a catalog dependency
+			result->dependencies.insert(EnumType::GetCatalog(column.type));
+		}
 	}
 	this->allow_stream_result = false;
 	return result;
