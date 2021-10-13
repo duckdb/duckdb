d8beba9dcd4d78f3342a7469ae2b8f0131779305
Jarrad Whitaker <akdor1154@gmail.com>
2021-10-14 09:27:22 +1100

    more test changes

--- tools/pythonpkg/tests/stubs/test_stubs.py
+++ tools/pythonpkg/tests/stubs/test_stubs.py
@@ -6,32 +6,16 @@ import contextlib
 import os
 from typing import *
 
-MYPY_INI = '''
-[mypy]
-[mypy-pandas]
-ignore_missing_imports = True
-'''
-
-@contextlib.contextmanager
-def temp_mypy_ini(contents: str) -> Generator[str, None, None]:
-	with tempfile.NamedTemporaryFile('wt', suffix='mypy.ini', delete=False) as mypy_ini:
-		mypy_ini.write(contents)
-	mypy_ini_filename = mypy_ini.name
-	try:
-		yield mypy_ini_filename
-	finally:
-		os.remove(mypy_ini_filename)
-
+MYPY_INI_PATH = os.path.join(os.path.dirname(__file__), 'mypy.ini')
 
 def test_stubs():
 	# just run stubtest
 
-	with temp_mypy_ini(MYPY_INI) as ini_path:
-		assert (
-			subprocess.run([
-				sys.executable, '-m', 'mypy.stubtest',
-				'duckdb',
-				'--mypy-config-file', ini_path,
-			]).returncode
-			== 0
-		), 'Stubtest failed! Check stdout.'
+	assert (
+		subprocess.run([
+			sys.executable, '-m', 'mypy.stubtest',
+			'duckdb',
+			'--mypy-config-file', MYPY_INI_PATH,
+		]).returncode
+		== 0
+	), 'Stubtest failed! Check stdout.'
