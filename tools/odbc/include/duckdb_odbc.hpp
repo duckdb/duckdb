f76b44967aefa477443d4a63476321bbc68c25f2
Tiago Kepe <tiagokepe@gmail.com>
2021-10-27 19:28:18 -0700

    ODBC: removed ODBC functions from header and added stubs for ODBC descriptor handle.

--- tools/odbc/include/duckdb_odbc.hpp
+++ tools/odbc/include/duckdb_odbc.hpp
@@ -6,93 +6,19 @@
 
 #include "duckdb/common/windows.hpp"
 
-#include <sql.h>
 #include <sqltypes.h>
 #include <sqlext.h>
 
-extern "C" {
-// handles
-SQLRETURN SQLAllocHandle(SQLSMALLINT handle_type, SQLHANDLE input_handle, SQLHANDLE *output_handle_ptr);
-SQLRETURN SQLFreeHandle(SQLSMALLINT handle_type, SQLHANDLE handle);
-
-// attributes
-SQLRETURN SQLGetConnectAttr(SQLHDBC connection_handle, SQLINTEGER attribute, SQLPOINTER value_ptr,
-                            SQLINTEGER buffer_length, SQLINTEGER *string_length_ptr);
-SQLRETURN SQLSetEnvAttr(SQLHENV environment_handle, SQLINTEGER attribute, SQLPOINTER value_ptr,
-                        SQLINTEGER string_length);
-SQLRETURN SQLSetConnectAttr(SQLHDBC connection_handle, SQLINTEGER attribute, SQLPOINTER value_ptr,
-                            SQLINTEGER string_length);
-SQLRETURN SQLSetStmtAttr(SQLHSTMT statement_handle, SQLINTEGER attribute, SQLPOINTER value_ptr,
-                         SQLINTEGER string_length);
-
-// connections
-SQLRETURN SQLDriverConnect(SQLHDBC connection_handle, SQLHWND window_handle, SQLCHAR *in_connection_string,
-                           SQLSMALLINT string_length1, SQLCHAR *out_connection_string, SQLSMALLINT buffer_length,
-                           SQLSMALLINT *string_length2_ptr, SQLUSMALLINT driver_completion);
-SQLRETURN SQLConnect(SQLHDBC connection_handle, SQLCHAR *server_name, SQLSMALLINT name_length1, SQLCHAR *user_name,
-                     SQLSMALLINT name_length2, SQLCHAR *authentication, SQLSMALLINT name_length3);
-
-SQLRETURN SQLGetInfo(SQLHDBC connection_handle, SQLUSMALLINT info_type, SQLPOINTER info_value_ptr,
-                     SQLSMALLINT buffer_length, SQLSMALLINT *string_length_ptr);
-SQLRETURN SQLEndTran(SQLSMALLINT handle_type, SQLHANDLE handle, SQLSMALLINT completion_type);
-SQLRETURN SQLDisconnect(SQLHDBC connection_handle);
-
-// statements
-SQLRETURN SQLTables(SQLHSTMT statement_handle, SQLCHAR *catalog_name, SQLSMALLINT name_length1, SQLCHAR *schema_name,
-                    SQLSMALLINT name_length2, SQLCHAR *table_name, SQLSMALLINT name_length3, SQLCHAR *table_type,
-                    SQLSMALLINT name_length4);
-SQLRETURN SQLColumns(SQLHSTMT statement_handle, SQLCHAR *catalog_name, SQLSMALLINT name_length1, SQLCHAR *schema_name,
-                     SQLSMALLINT name_length2, SQLCHAR *table_name, SQLSMALLINT name_length3, SQLCHAR *column_name,
-                     SQLSMALLINT name_length4);
-
-SQLRETURN SQLPrepare(SQLHSTMT statement_handle, SQLCHAR *statement_text, SQLINTEGER text_length);
-SQLRETURN SQLExecDirect(SQLHSTMT statement_handle, SQLCHAR *statement_text, SQLINTEGER text_length);
-SQLRETURN SQLFreeStmt(SQLHSTMT statement_handle, SQLUSMALLINT option);
-SQLRETURN SQLDescribeParam(SQLHSTMT statement_handle, SQLUSMALLINT parameter_number, SQLSMALLINT *data_type_ptr,
-                           SQLULEN *parameter_size_ptr, SQLSMALLINT *decimal_digits_ptr, SQLSMALLINT *nullable_ptr);
-SQLRETURN SQLDescribeCol(SQLHSTMT statement_handle, SQLUSMALLINT column_number, SQLCHAR *column_name,
-                         SQLSMALLINT buffer_length, SQLSMALLINT *name_length_ptr, SQLSMALLINT *data_type_ptr,
-                         SQLULEN *column_size_ptr, SQLSMALLINT *decimal_digits_ptr, SQLSMALLINT *nullable_ptr);
-SQLRETURN SQLColAttribute(SQLHSTMT statement_handle, SQLUSMALLINT column_number, SQLUSMALLINT field_identifier,
-                          SQLPOINTER character_attribute_ptr, SQLSMALLINT buffer_length, SQLSMALLINT *string_length_ptr,
-                          SQLLEN *numeric_attribute_ptr);
-SQLRETURN SQLFetchScroll(SQLHSTMT statement_handle, SQLSMALLINT fetch_orientation, SQLLEN fetch_offset);
-SQLRETURN SQLRowCount(SQLHSTMT statement_handle, SQLLEN *row_count_ptr);
-
-SQLRETURN SQLNumResultCols(SQLHSTMT statement_handle, SQLSMALLINT *column_count_ptr);
-
-SQLRETURN SQLBindCol(SQLHSTMT statement_handle, SQLUSMALLINT column_number, SQLSMALLINT target_type,
-                     SQLPOINTER target_value_ptr, SQLLEN buffer_length, SQLLEN *str_len_or_ind_ptr);
-
-SQLRETURN SQLPutData(SQLHSTMT statement_handle, SQLPOINTER data_ptr, SQLLEN str_len_or_ind_ptr);
-
-SQLRETURN SQLCancel(SQLHSTMT statement_handle);
-
-SQLRETURN SQLNumParams(SQLHSTMT statement_handle, SQLSMALLINT *parameter_count_ptr);
-
-SQLRETURN SQLParamData(SQLHSTMT statement_handle, SQLPOINTER *value_ptr_ptr);
-SQLRETURN SQLMoreResults(SQLHSTMT statement_handle);
-
-// diagnostics
-SQLRETURN SQLGetDiagField(SQLSMALLINT handle_type, SQLHANDLE handle, SQLSMALLINT rec_number,
-                          SQLSMALLINT diag_identifier, SQLPOINTER diag_info_ptr, SQLSMALLINT buffer_length,
-                          SQLSMALLINT *string_length_ptr);
-SQLRETURN SQLGetDiagRec(SQLSMALLINT handle_type, SQLHANDLE handle, SQLSMALLINT rec_number, SQLCHAR *sql_state,
-                        SQLINTEGER *native_error_ptr, SQLCHAR *message_text, SQLSMALLINT buffer_length,
-                        SQLSMALLINT *text_length_ptr);
-
-// api info
-SQLRETURN SQLGetFunctions(SQLHDBC connection_handle, SQLUSMALLINT function_id, SQLUSMALLINT *supported_ptr);
-SQLRETURN SQLGetTypeInfo(SQLHSTMT statement_handle, SQLSMALLINT data_type);
-
-} // extern "C"
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 namespace duckdb {
 
 class OdbcFetch;
 class ParameterWrapper;
 
-enum OdbcHandleType { ENV, DBC, STMT };
+enum OdbcHandleType { ENV, DBC, STMT, DESC };
 struct OdbcHandle {
 	explicit OdbcHandle(OdbcHandleType type_p) : type(type_p) {};
 	OdbcHandleType type;
@@ -148,6 +74,8 @@ struct OdbcBoundCol {
 	SQLLEN *strlen_or_ind;
 };
 
+struct OdbcHandleDesc;
+
 struct OdbcHandleStmt : public OdbcHandle {
 	explicit OdbcHandleStmt(OdbcHandleDbc *dbc_p);
 	~OdbcHandleStmt();
@@ -165,6 +93,19 @@ struct OdbcHandleStmt : public OdbcHandle {
 	vector<std::string> error_messages;
 	// fetcher
 	unique_ptr<OdbcFetch> odbc_fetcher;
+
+	OdbcHandleDesc *apd;
+	OdbcHandleDesc *ipd;
+	OdbcHandleDesc *ard;
+	OdbcHandleDesc *ird;
+};
+
+struct OdbcHandleDesc : public OdbcHandle {
+	//! https://docs.microsoft.com/en-us/sql/odbc/reference/develop-app/descriptors?view=sql-server-ver15
+	//TODO requires full implmentation
+	explicit OdbcHandleDesc(): OdbcHandle(OdbcHandleType::DESC) {
+	};
+	~OdbcHandleDesc();
 };
 
 struct OdbcUtils {
