6c512f92ed4263bb4ac061cdc9babb81b755b222
David Korczynski <david@adalogics.com>
2021-11-03 22:15:42 +0000

    fix lint

--- test/ossfuzz/parse_fuzz_test.cpp
+++ test/ossfuzz/parse_fuzz_test.cpp
@@ -1,12 +1,11 @@
 #include "duckdb/parser/parser.hpp"
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
-{
-    std::string input(reinterpret_cast<const char*>(data), size);
-    duckdb::Parser parser;
-    try {
-       parser.ParseQuery(input);
-    } catch (std::exception &e) {}
-
-    return 0;
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+	std::string input(reinterpret_cast<const char *>(data), size);
+	duckdb::Parser parser;
+	try {
+		parser.ParseQuery(input);
+	} catch (std::exception &e) {
+	}
+	return 0;
 }
