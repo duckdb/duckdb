8d5d5ff17b2199b32c8752421012b9672ab06cac
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-11-16 10:36:17 +0100

    Tie in SET to settings

--- src/execution/operator/helper/physical_set.cpp
+++ src/execution/operator/helper/physical_set.cpp
@@ -8,13 +8,39 @@ namespace duckdb {
 
 void PhysicalSet::GetData(ExecutionContext &context, DataChunk &chunk, GlobalSourceState &gstate,
                           LocalSourceState &lstate) const {
-	D_ASSERT(scope == SetScope::GLOBAL || scope == SetScope::SESSION);
+	auto option = DBConfig::GetOptionByName(name);
+	if (!option) {
+		throw CatalogException("unrecognized configuration parameter \"%s\"", name);
+	}
+	SetScope variable_scope = scope;
+	if (variable_scope == SetScope::AUTOMATIC) {
+		if (option->set_local) {
+			variable_scope = SetScope::SESSION;
+		} else {
+			D_ASSERT(option->set_global);
+			variable_scope = SetScope::GLOBAL;
+		}
+	}
 
-	auto normalized_name = ValidateInput(context);
-	if (scope == SetScope::GLOBAL) {
-		DBConfig::GetConfig(context.client).set_variables[normalized_name] = value;
-	} else {
-		ClientConfig::GetConfig(context.client).set_variables[normalized_name] = value;
+	Value input = value.CastAs(option->parameter_type);
+	switch(variable_scope) {
+	case SetScope::GLOBAL: {
+		if (!option->set_global) {
+			throw CatalogException("option \"%s\" cannot be set globally", name);
+		}
+		auto &db = DatabaseInstance::GetDatabase(context.client);
+		auto &config = DBConfig::GetConfig(context.client);
+		option->set_global(&db, config, input);
+		break;
+	}
+	case SetScope::SESSION:
+		if (!option->set_local) {
+			throw CatalogException("option \"%s\" cannot be set locally", name);
+		}
+		option->set_local(context.client, input);
+		break;
+	default:
+		throw InternalException("Unsupported SetScope for variable");
 	}
 }
 
