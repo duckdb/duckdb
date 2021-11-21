5f40d90b8d9740e03bd2dfdac61252f1d4584f60
chenxi8611 <chenxi8611@gmail.com>
2021-11-21 14:05:00 +0800

    fix format-check error

--- src/include/duckdb/execution/operator/helper/physical_limit.hpp
+++ src/include/duckdb/execution/operator/helper/physical_limit.hpp
@@ -19,21 +19,21 @@ public:
 	PhysicalLimit(vector<LogicalType> types, idx_t limit, idx_t offset, unique_ptr<Expression> limit_expression,
 	              unique_ptr<Expression> offset_expression, idx_t estimated_cardinality)
 	    : PhysicalOperator(PhysicalOperatorType::LIMIT, move(types), estimated_cardinality), is_limit_percent(false),
-	      limit_value(limit), limit_percent(100.0), offset_value(offset), limit_expression(move(limit_expression)),
+	      limit_value(limit), offset_value(offset), limit_expression(move(limit_expression)),
 	      offset_expression(move(offset_expression)) {
 	}
 
 	PhysicalLimit(vector<LogicalType> types, double limit_percent, idx_t offset,
 	              unique_ptr<Expression> limit_expression, unique_ptr<Expression> offset_expression,
 	              idx_t estimated_cardinality)
-	    : PhysicalOperator(PhysicalOperatorType::LIMIT, move(types), estimated_cardinality), limit_value(INVALID_INDEX), is_limit_percent(true),
-	      limit_percent(limit_percent), offset_value(offset), limit_expression(move(limit_expression)),
-	      offset_expression(move(offset_expression)) {
+	    : PhysicalOperator(PhysicalOperatorType::LIMIT, move(types), estimated_cardinality),
+	      is_limit_percent(true), limit_percent(limit_percent), offset_value(offset),
+	      limit_expression(move(limit_expression)), offset_expression(move(offset_expression)) {
 	}
 
-	idx_t limit_value;
-	bool is_limit_percent;
-	double limit_percent;
+	idx_t limit_value = INVALID_INDEX;
+	bool is_limit_percent = false;
+	double limit_percent = 100.0;
 	idx_t limit_count = INVALID_INDEX;
 	idx_t offset_value;
 	unique_ptr<Expression> limit_expression;
