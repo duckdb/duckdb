2c96e473a7f9bf3aee1e02ee8b7de3f133d9147d
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-11-28 12:15:19 +0100

    pip install awscli

--- scripts/extension-upload.sh
+++ scripts/extension-upload.sh
@@ -6,5 +6,5 @@ do
 	ext=`basename $f .duckdb_extension`
 	echo $ext
 	gzip -k -f $f
-   	aws s3 cp $f.gz s3://duckdb-extensions/`git log -1 --format=%h`/$1/$ext.duckdb_extension.gz --acl public-read  
+	aws s3 cp $f.gz s3://duckdb-extensions/`git log -1 --format=%h`/$1/$ext.duckdb_extension.gz --acl public-read
 done
