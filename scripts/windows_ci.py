fe7a44f9fd6cc00055e9a52b23050024eddbc535
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-11-28 13:54:32 +0100

    small is defined in windows.h (yay)

--- scripts/windows_ci.py
+++ scripts/windows_ci.py
@@ -8,7 +8,11 @@ with open(common_path, 'r') as f:
 
 text = text.replace('#pragma once', '''#pragma once
 
+#ifdef _WIN32
+#ifdef DUCKDB_MAIN_LIBRARY
 #include "duckdb/common/windows.hpp"
+#endif
+#endif
 ''')
 
 with open(common_path, 'w+') as f:
