88b1f39b0dc0e51d7e75050610a1d7ff730e867c
Diego Gomes Tome <diegogomest@gmail.com>
2021-11-29 14:33:26 +0100

    Fix for thread sanatizer and tpch

--- src/parallel/pipeline.cpp
+++ src/parallel/pipeline.cpp
@@ -146,7 +146,8 @@ void Pipeline::Finalize(Event &event) {
 	D_ASSERT(ready);
 	try {
 		auto sink_state = sink->Finalize(*this, event, executor.context, *sink->sink_state);
-		executor.context.config.has_primary_key = sink->sink_state->has_primary_key;
+		auto primary_key_flag = sink->sink_state->has_primary_key.load(); // atomically load
+		executor.context.config.has_primary_key.store(primary_key_flag);  // atomically store it
 		sink->sink_state->state = sink_state;
 	} catch (Exception &ex) { // LCOV_EXCL_START
 		executor.PushError(ex.type, ex.what());
