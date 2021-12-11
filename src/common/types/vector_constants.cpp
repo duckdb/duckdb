6b717af9356096e4947bc95905b987640a18a10a
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-12-11 11:51:34 +0100

    Actually use Wexit-time-destructors, and disable in certain cases

--- src/common/types/vector_constants.cpp
+++ src/common/types/vector_constants.cpp
@@ -2,8 +2,17 @@
 
 namespace duckdb {
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wexit-time-destructors"
+#endif
+
 const SelectionVector ConstantVector::ZERO_SELECTION_VECTOR = SelectionVector((sel_t *)ConstantVector::ZERO_VECTOR);
 const SelectionVector FlatVector::INCREMENTAL_SELECTION_VECTOR;
 const sel_t ConstantVector::ZERO_VECTOR[STANDARD_VECTOR_SIZE] = {0};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 } // namespace duckdb
