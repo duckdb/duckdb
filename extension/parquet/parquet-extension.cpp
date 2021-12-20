42581365935100270d1abe6a76c5f639cfb5431f
Hannes Mühleisen <hannes@muehleisen.org>
2021-12-20 09:24:51 +0100

    format

--- extension/parquet/parquet-extension.cpp
+++ extension/parquet/parquet-extension.cpp
@@ -139,7 +139,7 @@ public:
 			FileSystem &fs = FileSystem::GetFileSystem(context);
 			for (idx_t file_idx = 1; file_idx < bind_data.files.size(); file_idx++) {
 				auto &file_name = bind_data.files[file_idx];
-				auto metadata = (ParquetFileMetadataCache*)(cache.Get(file_name).get());
+				auto metadata = (ParquetFileMetadataCache *)(cache.Get(file_name).get());
 				auto handle = fs.OpenFile(file_name, FileFlags::FILE_FLAGS_READ, FileSystem::DEFAULT_LOCK,
 				                          FileSystem::DEFAULT_COMPRESSION, FileSystem::GetFileOpener(context));
 				// but we need to check if the metadata cache entries are current
