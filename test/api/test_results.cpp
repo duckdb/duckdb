c2f5038f9c5d24b2f6c9e6374aeab30060642e0c
Aneesh Makala <makalaaneesh18@gmail.com>
2022-01-05 12:56:38 +0100

    test case

--- test/api/test_results.cpp
+++ test/api/test_results.cpp
@@ -190,3 +190,25 @@ TEST_CASE("Test UUID", "[api][uuid]") {
 	}
 	REQUIRE(row_count == 2);
 }
+
+
+TEST_CASE("Test ARRAY_AGG with OrderBy", "[api][array_agg]") {
+	DuckDB db(nullptr);
+	Connection con(db);
+
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE t2 (a INT, b INT, c INT)"));
+	REQUIRE_NO_FAIL(con.Query("INSERT INTO t2 VALUES (1,1,1), (1,2,2), (2,1,3), (2,2,4)"));
+
+	idx_t row_count = 0;
+	auto result = con.Query("select a,array_agg(c order by b) from t2 group by a");
+	for (auto &name : (*result).names) {
+		printf("%s\n", name.c_str());
+	}
+
+	// for (auto &row : *result) {
+	// 	auto uuid = row.GetValue<string>(0);
+	// 	REQUIRE(uuid == "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
+	// 	row_count++;
+	// }
+	// REQUIRE(row_count == 2);
+}
