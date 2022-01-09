909728bfcea996a091a17aaac0264835ed1a19cf
Jens <jens-hofer@gmx.de>
2022-01-10 00:22:44 +0100

    Run make format-fix

--- tools/jdbc/src/jni/duckdb_java.cpp
+++ tools/jdbc/src/jni/duckdb_java.cpp
@@ -180,8 +180,8 @@ JNIEXPORT jobject JNICALL Java_org_duckdb_DuckDBNative_duckdb_1jdbc_1execute(JNI
 				    Value::BIGINT(env->CallLongMethod(param, env->GetMethodID(long_class, "longValue", "()J"))));
 				continue;
 			} else if (env->IsInstanceOf(param, timestamp_class)) {
-				duckdb_params.push_back(
-				    Value::TIMESTAMP((timestamp_t)env->CallLongMethod(param, env->GetMethodID(timestamp_class, "getMicrosEpoch", "()J"))));
+				duckdb_params.push_back(Value::TIMESTAMP((timestamp_t)env->CallLongMethod(
+				    param, env->GetMethodID(timestamp_class, "getMicrosEpoch", "()J"))));
 				continue;
 			} else if (env->IsInstanceOf(param, float_class)) {
 				duckdb_params.push_back(
@@ -331,7 +331,7 @@ JNIEXPORT jobjectArray JNICALL Java_org_duckdb_DuckDBNative_duckdb_1jdbc_1fetch(
 			constlen_data = env->NewDirectByteBuffer(FlatVector::GetData(vec), row_count * sizeof(double));
 			break;
 		case LogicalTypeId::TIMESTAMP:
-            constlen_data = env->NewDirectByteBuffer(FlatVector::GetData(vec), row_count * sizeof(timestamp_t));
+			constlen_data = env->NewDirectByteBuffer(FlatVector::GetData(vec), row_count * sizeof(timestamp_t));
 			break;
 		case LogicalTypeId::TIME:
 		case LogicalTypeId::DATE:
