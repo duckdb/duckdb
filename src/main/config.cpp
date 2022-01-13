671e9da9217f302c2522bb03935dc82d720afdb6
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-01-13 19:28:00 +0100

    GetOptionByName needs to be case insensitive too

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -83,8 +83,10 @@ ConfigurationOption *DBConfig::GetOptionByIndex(idx_t target_index) {
 }
 
 ConfigurationOption *DBConfig::GetOptionByName(const string &name) {
+	auto lname = StringUtil::Lower(name);
 	for (idx_t index = 0; internal_options[index].name; index++) {
-		if (internal_options[index].name == name) {
+		D_ASSERT(StringUtil::Lower(internal_options[index].name) == string(internal_options[index].name));
+		if (internal_options[index].name == lname) {
 			return internal_options + index;
 		}
 	}
