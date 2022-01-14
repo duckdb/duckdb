0e7d21099f9b5be41d2f4f8ef20ccb29664ad60a
Tiago Kepe <tiagokepe@gmail.com>
2022-01-14 08:49:24 -0300

    ODBC: added cursor.cpp into CMakeLists.

--- tools/odbc/cursor.cpp
+++ tools/odbc/cursor.cpp
@@ -2,21 +2,20 @@
 #include "statement_functions.hpp"
 
 SQLRETURN SQL_API SQLCloseCursor(SQLHSTMT statement_handle) {
-    return duckdb::WithStatement(statement_handle, [&](duckdb::OdbcHandleStmt *stmt) -> SQLRETURN {
-        return duckdb::CloseStmt(stmt);
-    });
+	return duckdb::WithStatement(statement_handle,
+	                             [&](duckdb::OdbcHandleStmt *stmt) -> SQLRETURN { return duckdb::CloseStmt(stmt); });
 }
 
 SQLRETURN SQL_API SQLSetCursorName(SQLHSTMT statement_handle, SQLCHAR *cursor_name, SQLSMALLINT name_length) {
-    return duckdb::WithStatement(statement_handle, [&](duckdb::OdbcHandleStmt *stmt) -> SQLRETURN {
-        stmt->error_messages.emplace_back("SQLSetCursorName does not supported.");
-        return SQL_ERROR;
-    });
+	return duckdb::WithStatement(statement_handle, [&](duckdb::OdbcHandleStmt *stmt) -> SQLRETURN {
+		stmt->error_messages.emplace_back("SQLSetCursorName does not supported.");
+		return SQL_ERROR;
+	});
 }
 
 SQLRETURN SQL_API SQLGetCursorName(SQLHSTMT statement_handle, SQLCHAR *cursor_name, SQLSMALLINT name_length) {
-    return duckdb::WithStatement(statement_handle, [&](duckdb::OdbcHandleStmt *stmt) -> SQLRETURN {
-        stmt->error_messages.emplace_back("SQLGetCursorName does not supported.");
-        return SQL_ERROR;
-    });
+	return duckdb::WithStatement(statement_handle, [&](duckdb::OdbcHandleStmt *stmt) -> SQLRETURN {
+		stmt->error_messages.emplace_back("SQLGetCursorName does not supported.");
+		return SQL_ERROR;
+	});
 }
