3159955bdada412902f96e0cd3e4b9e37422d7f9
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-19 21:08:44 +0100

    Keep installing binary packages on Windows

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,5 +1,10 @@
-options(HTTPUserAgent = sprintf("R/4.1.0 R (4.1.0 %s)", paste(R.version$platform, R.version$arch, R.version$os)))
-install.packages(
-  c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow", "covr"),
-  repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"
-)
+local({
+  pkg <- c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow", "covr"),
+
+  if (.Platform$OS.type == "unix") {
+    options(HTTPUserAgent = sprintf("R/4.1.0 R (4.1.0 %s)", paste(R.version$platform, R.version$arch, R.version$os)))
+    install.packages(pkg, repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
+  } else {
+    install.packages(pkg, repos = "https://cloud.r-project.org", pkgType = "binary")
+  }
+})
