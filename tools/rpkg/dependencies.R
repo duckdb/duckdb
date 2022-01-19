24d7d45ef17e3456174a990a1f81604c8e429afe
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-19 05:36:48 +0100

    Add coverage check

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,5 +1,5 @@
 options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os))
 install.packages(
-  c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow"),
+  c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow", "covr"),
   repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"
 )
