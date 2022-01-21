a8994790c86e1e8c8d76769778ee3bfbeca898d3
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-21 06:17:24 +0100

    Use patched version of covr

--- tools/rpkg/dependencies.R
+++ tools/rpkg/dependencies.R
@@ -1,9 +1,11 @@
 local({
-  pkg <- c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow", "covr")
+  pkg <- c("DBI", "callr", "DBItest", "dbplyr", "nycflights13", "testthat", "bit64", "cpp11", "arrow", "covr", "remotes")
 
   if (.Platform$OS.type == "unix") {
     options(HTTPUserAgent = sprintf("R/4.1.0 R (4.1.0 %s)", paste(R.version$platform, R.version$arch, R.version$os)))
     install.packages(pkg, repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
+    # https://github.com/r-lib/covr/pull/499
+    remotes::install_github("r-lib/covr@f-497-relative-path")
   } else {
     install.packages(pkg, repos = "https://cloud.r-project.org", pkgType = "binary")
   }
