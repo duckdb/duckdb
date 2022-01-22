6e277ec105548004d982e37724a418d3f58d7006
Tiago Kepe <tiagokepe@gmail.com>
2022-01-22 07:04:25 -0300

    ODBC: formating OdbcUtils.

--- tools/odbc/include/odbc_utils.hpp
+++ tools/odbc/include/odbc_utils.hpp
@@ -12,19 +12,23 @@ using std::vector;
 
 namespace duckdb {
 struct OdbcUtils {
-	public:
+public:
 	static string ReadString(const SQLPOINTER ptr, const SQLSMALLINT len);
 	static void WriteString(const string &s, SQLCHAR *out_buf, SQLSMALLINT buf_len, SQLSMALLINT *out_len);
 
 	static SQLRETURN SetStringValueLength(const string &val_str, SQLLEN *str_len_or_ind_ptr);
-	static SQLRETURN SetStringAndLength(vector<string> &error_messages, const string &val_str, SQLPOINTER target_value_ptr, SQLSMALLINT buffer_length, SQLSMALLINT *str_len_or_ind_ptr);
+	static SQLRETURN SetStringAndLength(vector<string> &error_messages, const string &val_str,
+	                                    SQLPOINTER target_value_ptr, SQLSMALLINT buffer_length,
+	                                    SQLSMALLINT *str_len_or_ind_ptr);
 
 	static string GetStringAsIdentifier(const string str);
-	static string ParseStringFilter(const string filter_name, const string filter_value, SQLUINTEGER sql_attr_metadata_id, const string coalesce_str = "");
+	static string ParseStringFilter(const string filter_name, const string filter_value,
+	                                SQLUINTEGER sql_attr_metadata_id, const string coalesce_str = "");
 
-	static string GetQueryDuckdbTables(const string schema_filter, const string table_filter, const string table_type_filter);
-	static string GetQueryDuckdbColumns(const string catalog_filter, const string schema_filter, const string table_filter, const string column_filter);
-
+	static string GetQueryDuckdbTables(const string schema_filter, const string table_filter,
+	                                   const string table_type_filter);
+	static string GetQueryDuckdbColumns(const string catalog_filter, const string schema_filter,
+	                                    const string table_filter, const string column_filter);
 };
 } // namespace duckdb
 #endif
--- tools/odbc/odbc_utils.cpp
+++ tools/odbc/odbc_utils.cpp
@@ -29,8 +29,9 @@ SQLRETURN OdbcUtils::SetStringValueLength(const string &val_str, SQLLEN *str_len
 	return SQL_ERROR;
 }
 
-
-SQLRETURN OdbcUtils::SetStringAndLength(vector<string> &error_messages, const string &val_str, SQLPOINTER target_value_ptr, SQLSMALLINT buffer_length, SQLSMALLINT *str_len_or_ind_ptr) {
+SQLRETURN OdbcUtils::SetStringAndLength(vector<string> &error_messages, const string &val_str,
+                                        SQLPOINTER target_value_ptr, SQLSMALLINT buffer_length,
+                                        SQLSMALLINT *str_len_or_ind_ptr) {
 	if (!target_value_ptr) {
 		return OdbcUtils::SetStringValueLength(val_str, reinterpret_cast<SQLLEN *>(str_len_or_ind_ptr));
 	}
@@ -59,7 +60,7 @@ string OdbcUtils::GetStringAsIdentifier(const string str) {
 	string str_ret = nullptr;
 
 	std::regex regex_str_quoted("^(\".*\"|\'.*\')$");
-	
+
 	if (std::regex_match(str, regex_str_quoted)) {
 		// removing quotes
 		str_ret = str_ret.substr(1, str_ret.size() - 2);
@@ -74,7 +75,8 @@ string OdbcUtils::GetStringAsIdentifier(const string str) {
 	return str_ret;
 }
 
-string OdbcUtils::ParseStringFilter(const string filter_name, const string filter_value, SQLUINTEGER sql_attr_metadata_id, const string coalesce_str) {
+string OdbcUtils::ParseStringFilter(const string filter_name, const string filter_value,
+                                    SQLUINTEGER sql_attr_metadata_id, const string coalesce_str) {
 	string filter;
 	if (filter_value.empty()) {
 		if (coalesce_str.empty()) {
@@ -82,7 +84,7 @@ string OdbcUtils::ParseStringFilter(const string filter_name, const string filte
 		} else {
 			filter = "COALESCE(" + filter_name + ",'" + coalesce_str + "') LIKE '" + coalesce_str + "'";
 		}
-	} else  if (sql_attr_metadata_id == SQL_TRUE) {
+	} else if (sql_attr_metadata_id == SQL_TRUE) {
 		filter = filter_name + "=" + OdbcUtils::GetStringAsIdentifier(filter_value);
 	} else {
 		filter = filter_name + " LIKE '" + filter_value + "'";
@@ -90,84 +92,90 @@ string OdbcUtils::ParseStringFilter(const string filter_name, const string filte
 	return filter;
 }
 
-string OdbcUtils::GetQueryDuckdbColumns(const string catalog_filter, const string schema_filter, const string table_filter, const string column_filter) {
-	string sql_duckdb_columns = "SELECT "
-									"NULL \"TABLE_CAT\", "
-									"schema_name \"TABLE_SCHEM\", "
-									"table_name \"TABLE_NAME\", "
-									"column_name \"COLUMN_NAME\", "
-									"data_type_id \"DATA_TYPE\", "
-									"data_type \"TYPE_NAME\", "
-									"CASE "
-										"WHEN data_type='DATE' THEN 12 "
-										"WHEN data_type='TIME' THEN 15 "
-										"WHEN data_type LIKE 'TIMESTAMP%' THEN 26 "
-										"WHEN data_type='CHAR' OR data_type='BOOLEAN' THEN 1 "
-										"WHEN data_type='VARCHAR' OR data_type='BLOB' THEN character_maximum_length "
-										"WHEN data_type LIKE '%INT%' THEN numeric_precision "
-										"WHEN data_type like 'DECIMAL%' THEN numeric_precision "
-										"WHEN data_type='FLOAT' OR data_type='DOUBLE' THEN numeric_precision "
-										"ELSE NULL "
-									"END as \"COLUMN_SIZE\", "
-									"CASE "
-										"WHEN data_type='DATE' THEN 4 "
-										"WHEN data_type='TIME' THEN 8 "
-										"WHEN data_type LIKE 'TIMESTAMP%' THEN 8 "
-										"WHEN data_type='CHAR' OR data_type='BOOLEAN' THEN 1 "
-										"WHEN data_type='VARCHAR' OR data_type='BLOB' THEN 16 "
-										"WHEN data_type LIKE '%TINYINT' THEN 1 "
-										"WHEN data_type LIKE '%SMALLINT' THEN 2 "
-										"WHEN data_type LIKE '%INTEGER' THEN 4 "
-										"WHEN data_type LIKE '%BIGINT' THEN 8 "
-										"WHEN data_type='HUGEINT' THEN 16 "
-										"WHEN data_type='FLOAT' THEN 4 "
-										"WHEN data_type='DOUBLE' THEN 8 "
-										"ELSE NULL "
-									"END as \"BUFFER_LENGTH\", "
-									"numeric_scale \"DECIMAL_DIGITS\", "
-									"numeric_precision_radix \"NUM_PREC_RADIX\", "
-									"CASE is_nullable "
-										"WHEN false THEN 0 "
-										"WHEN true THEN 1 "
-										"ELSE 2 "
-									"END as \"NULLABLE\", "
-									"NULL \"REMARKS\", "
-									"column_default \"COLUMN_DEF\", "
-									"data_type_id  \"SQL_DATA_TYPE\", "
-									"CASE "
-										"WHEN data_type='DATE' OR data_type='TIME' OR data_type LIKE 'TIMESTAMP%' THEN data_type_id "
-										"ELSE NULL "
-									"END as \"SQL_DATETIME_SUB\", "
-									"CASE " 
-										"WHEN data_type='%CHAR' OR data_type='BLOB' THEN character_maximum_length "
-										"ELSE NULL "
-									"END as \"CHAR_OCTET_LENGTH\", "
-									"column_index as \"ORDINAL_POSITION\", "
-									"CASE is_nullable "
-										"WHEN false THEN 'NO' "
-										"WHEN true THEN 'YES' "
-										"ELSE '' "
-									"END as \"IS_NULLABLE\" "
-								"FROM duckdb_columns";
+string OdbcUtils::GetQueryDuckdbColumns(const string catalog_filter, const string schema_filter,
+                                        const string table_filter, const string column_filter) {
+	string sql_duckdb_columns =
+	    "SELECT "
+	    "NULL \"TABLE_CAT\", "
+	    "schema_name \"TABLE_SCHEM\", "
+	    "table_name \"TABLE_NAME\", "
+	    "column_name \"COLUMN_NAME\", "
+	    "data_type_id \"DATA_TYPE\", "
+	    "data_type \"TYPE_NAME\", "
+	    "CASE "
+	    "WHEN data_type='DATE' THEN 12 "
+	    "WHEN data_type='TIME' THEN 15 "
+	    "WHEN data_type LIKE 'TIMESTAMP%' THEN 26 "
+	    "WHEN data_type='CHAR' OR data_type='BOOLEAN' THEN 1 "
+	    "WHEN data_type='VARCHAR' OR data_type='BLOB' THEN character_maximum_length "
+	    "WHEN data_type LIKE '%INT%' THEN numeric_precision "
+	    "WHEN data_type like 'DECIMAL%' THEN numeric_precision "
+	    "WHEN data_type='FLOAT' OR data_type='DOUBLE' THEN numeric_precision "
+	    "ELSE NULL "
+	    "END as \"COLUMN_SIZE\", "
+	    "CASE "
+	    "WHEN data_type='DATE' THEN 4 "
+	    "WHEN data_type='TIME' THEN 8 "
+	    "WHEN data_type LIKE 'TIMESTAMP%' THEN 8 "
+	    "WHEN data_type='CHAR' OR data_type='BOOLEAN' THEN 1 "
+	    "WHEN data_type='VARCHAR' OR data_type='BLOB' THEN 16 "
+	    "WHEN data_type LIKE '%TINYINT' THEN 1 "
+	    "WHEN data_type LIKE '%SMALLINT' THEN 2 "
+	    "WHEN data_type LIKE '%INTEGER' THEN 4 "
+	    "WHEN data_type LIKE '%BIGINT' THEN 8 "
+	    "WHEN data_type='HUGEINT' THEN 16 "
+	    "WHEN data_type='FLOAT' THEN 4 "
+	    "WHEN data_type='DOUBLE' THEN 8 "
+	    "ELSE NULL "
+	    "END as \"BUFFER_LENGTH\", "
+	    "numeric_scale \"DECIMAL_DIGITS\", "
+	    "numeric_precision_radix \"NUM_PREC_RADIX\", "
+	    "CASE is_nullable "
+	    "WHEN false THEN 0 "
+	    "WHEN true THEN 1 "
+	    "ELSE 2 "
+	    "END as \"NULLABLE\", "
+	    "NULL \"REMARKS\", "
+	    "column_default \"COLUMN_DEF\", "
+	    "data_type_id  \"SQL_DATA_TYPE\", "
+	    "CASE "
+	    "WHEN data_type='DATE' OR data_type='TIME' OR data_type LIKE 'TIMESTAMP%' THEN data_type_id "
+	    "ELSE NULL "
+	    "END as \"SQL_DATETIME_SUB\", "
+	    "CASE "
+	    "WHEN data_type='%CHAR' OR data_type='BLOB' THEN character_maximum_length "
+	    "ELSE NULL "
+	    "END as \"CHAR_OCTET_LENGTH\", "
+	    "column_index as \"ORDINAL_POSITION\", "
+	    "CASE is_nullable "
+	    "WHEN false THEN 'NO' "
+	    "WHEN true THEN 'YES' "
+	    "ELSE '' "
+	    "END as \"IS_NULLABLE\" "
+	    "FROM duckdb_columns";
 
-	sql_duckdb_columns += " WHERE " + catalog_filter + " AND " + schema_filter + " AND " + table_filter + " AND " + column_filter;
+	sql_duckdb_columns +=
+	    " WHERE " + catalog_filter + " AND " + schema_filter + " AND " + table_filter + " AND " + column_filter;
 	sql_duckdb_columns += " ORDER BY \"TABLE_CAT\", \"TABLE_SCHEM\", \"TABLE_NAME\", \"ORDINAL_POSITION\"";
 	return sql_duckdb_columns;
 }
 
-string OdbcUtils::GetQueryDuckdbTables(const string schema_filter, const string table_filter, const string table_type_filter) {
+string OdbcUtils::GetQueryDuckdbTables(const string schema_filter, const string table_filter,
+                                       const string table_type_filter) {
 	string sql_duckdb_tables = "SELECT "
-									"table_catalog::VARCHAR \"TABLE_CAT\", "
-									"table_schema \"TABLE_SCHEM\", "
-									"table_name \"TABLE_NAME\", "
-									"CASE "
-	               						"WHEN table_type='BASE TABLE' "
-										"THEN 'TABLE' "
-										"ELSE table_type "
-									"END \"TABLE_TYPE\" ,"
-									"'' \"REMARKS\"  "
-	               				"FROM information_schema.tables "
-								"WHERE " + schema_filter + " AND " + table_filter + " AND table_type IN (" + table_type_filter + ") "
-				   				"ORDER BY TABLE_TYPE, TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME";
+	                           "table_catalog::VARCHAR \"TABLE_CAT\", "
+	                           "table_schema \"TABLE_SCHEM\", "
+	                           "table_name \"TABLE_NAME\", "
+	                           "CASE "
+	                           "WHEN table_type='BASE TABLE' "
+	                           "THEN 'TABLE' "
+	                           "ELSE table_type "
+	                           "END \"TABLE_TYPE\" ,"
+	                           "'' \"REMARKS\"  "
+	                           "FROM information_schema.tables "
+	                           "WHERE " +
+	                           schema_filter + " AND " + table_filter + " AND table_type IN (" + table_type_filter +
+	                           ") "
+	                           "ORDER BY TABLE_TYPE, TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME";
 	return sql_duckdb_tables;
 }
