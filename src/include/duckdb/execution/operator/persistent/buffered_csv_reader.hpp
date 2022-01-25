009cc057ccc3d5081db11653663312de1923e54b
Jim <jimstam@hotmail.com>
2022-01-25 13:07:25 +0100

    Formatting fixes

--- src/include/duckdb/execution/operator/persistent/buffered_csv_reader.hpp
+++ src/include/duckdb/execution/operator/persistent/buffered_csv_reader.hpp
@@ -99,8 +99,8 @@ struct BufferedCSVReaderOptions {
 	std::map<LogicalTypeId, StrpTimeFormat> date_format = {{LogicalTypeId::DATE, {}}, {LogicalTypeId::TIMESTAMP, {}}};
 	//! Whether or not a type format is specified
 	std::map<LogicalTypeId, bool> has_format = {{LogicalTypeId::DATE, false}, {LogicalTypeId::TIMESTAMP, false}};
-    //! Whether the file should be overridden when query fails
-    bool overwrite = true;
+	//! Whether the file should be overridden when query fails
+	bool overwrite = true;
 
 	void SetDelimiter(const string &delimiter);
 
