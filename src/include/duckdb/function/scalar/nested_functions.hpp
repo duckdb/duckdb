fe1420b32be231465926ccbef6186477fab5dd52
Tania Bogatsch <tania.bogatsch@tu-ilmenau.de>
2022-02-24 18:24:37 +0100

    implemented list_position

--- src/include/duckdb/function/scalar/nested_functions.hpp
+++ src/include/duckdb/function/scalar/nested_functions.hpp
@@ -62,6 +62,11 @@ struct ListContainsFun {
 	static void RegisterFunction(BuiltinFunctions &set);
 };
 
+struct ListPositionFun {
+	static ScalarFunction GetFunction();
+	static void RegisterFunction(BuiltinFunctions &set);
+};
+
 struct CardinalityFun {
 	static void RegisterFunction(BuiltinFunctions &set);
 };
