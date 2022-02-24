7df1858773ea5bb96c0d96a46945f2dcdc0d64a0
Tiago Kepe <tiagokepe@gmail.com>
2022-02-24 19:11:59 -0300

    ODBC: created OdbcException.

---
+++ tools/odbc/include/odbc_exception.hpp
@@ -0,0 +1,34 @@
+#ifndef ODBC_EXCEPTION_HPP
+#define ODBC_EXCEPTION_HPP
+
+#include "odbc_diagnostic.hpp"
+#include "sqlext.h"
+#include "sqltypes.h"
+
+#include <stdexcept>
+#include <string>
+
+namespace duckdb {
+struct OdbcException : public std::exception {
+public:
+	explicit OdbcException(const std::string &msg);
+	explicit OdbcException(const std::string &msg, const std::string &sqlstate="", const std::string &server_name="", SQLINTEGER col_number=SQL_NO_COLUMN_NUMBER, SQLINTEGER sql_native=0, SQLLEN row_number=SQL_NO_ROW_NUMBER);
+	explicit OdbcException(const duckdb::DiagRecord &diag_rec): diag_record(diag_rec) {
+	};
+
+	void SetComponent(const std::string &comp);
+	void SetSqlReturn(SQLRETURN ret);
+	void SetMessage(const std::string &message);
+
+	const std::string GetMessage();
+	const std::string GetComponent();
+	const SQLRETURN GetSqlReturn();
+	const DiagRecord GetDiagRecord();
+
+private:
+	std::string component;
+	duckdb::DiagRecord diag_record;
+	SQLRETURN sql_ret = SQL_ERROR;
+};
+} // namespace duckdb
+#endif
