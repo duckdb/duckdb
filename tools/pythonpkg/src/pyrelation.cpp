3872147e1427ee7b0b10368d1b84695b57d1cc6d
Alan Du <alanhdu@gmail.com>
2022-02-24 22:40:43 -0500

    Revert "Don't leak memory in duckdb.query_df"
    This reverts commit f108ff504bfdab3d420f3aadd48e76d10d55148d.

--- tools/pythonpkg/src/pyrelation.cpp
+++ tools/pythonpkg/src/pyrelation.cpp
@@ -404,10 +404,7 @@ unique_ptr<DuckDBPyResult> DuckDBPyRelation::Execute() {
 
 unique_ptr<DuckDBPyResult> DuckDBPyRelation::QueryDF(py::object df, const string &view_name, const string &sql_query,
                                                      DuckDBPyConnection *conn) {
-	auto relation = conn->FromDF(std::move(df));
-	auto result = relation->Query(view_name, sql_query);
-	conn->UnregisterPythonObject(relation->GetAlias());
-	return result;
+	return conn->FromDF(std::move(df))->Query(view_name, sql_query);
 }
 
 void DuckDBPyRelation::InsertInto(const string &table) {
