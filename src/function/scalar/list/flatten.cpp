1d9bf6301671a9b0f97a858d9d434dddff4e8f2f
usurai <crowdwei@gmail.com>
2022-02-26 00:37:15 +0800

    Fix flatten vector invariant

--- src/function/scalar/list/flatten.cpp
+++ src/function/scalar/list/flatten.cpp
@@ -7,31 +7,44 @@ namespace duckdb {
 
 void ListFlattenFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	D_ASSERT(args.ColumnCount() == 1);
-	Vector &input = args.data[0];
 
+	Vector &input = args.data[0];
 	if (input.GetType().id() == LogicalTypeId::SQLNULL) {
 		result.Reference(input);
 		return;
 	}
-	auto &child_vector = ListVector::GetEntry(input);
 
 	idx_t count = args.size();
+
 	VectorData list_data;
 	input.Orrify(count, list_data);
 	auto list_entries = (list_entry_t *)list_data.data;
 
-	auto child_size = ListVector::GetListSize(input);
-	VectorData child_data;
-	child_vector.Orrify(child_size, child_data);
-	auto child_entries = (list_entry_t *)child_data.data;
+	auto &child_vector = ListVector::GetEntry(input);
 
 	result.SetVectorType(VectorType::FLAT_VECTOR);
 	auto result_entries = FlatVector::GetData<list_entry_t>(result);
 	auto &result_validity = FlatVector::Validity(result);
 
-	if (child_vector.GetType().id() != LogicalTypeId::SQLNULL) {
-		ListVector::ReferenceEntry(result, child_vector);
-	}
+    if (child_vector.GetType().id() == LogicalTypeId::SQLNULL) {
+        auto result_entries = FlatVector::GetData<list_entry_t>(result);
+        for (idx_t i = 0; i < count; i++) {
+            auto list_index = list_data.sel->get_index(i);
+            if (!list_data.validity.RowIsValid(list_index)) {
+                result_validity.SetInvalid(i);
+                continue;
+            }
+            result_entries[i].offset = 0;
+            result_entries[i].length = 0;
+        }
+        return;
+    }
+
+	auto child_size = ListVector::GetListSize(input);
+	VectorData child_data;
+	child_vector.Orrify(child_size, child_data);
+	auto child_entries = (list_entry_t *)child_data.data;
+	auto &data_vector = ListVector::GetEntry(child_vector);
 
 	idx_t offset = 0;
 	for (idx_t i = 0; i < count; i++) {
@@ -42,11 +55,12 @@ void ListFlattenFunction(DataChunk &args, ExpressionState &state, Vector &result
 		}
 		auto list_entry = list_entries[list_index];
 
+        idx_t source_offset = 0;
 		// Find first valid child list entry to get offset
 		for (idx_t j = 0; j < list_entry.length; j++) {
 			auto child_list_index = child_data.sel->get_index(list_entry.offset + j);
 			if (child_data.validity.RowIsValid(child_list_index)) {
-				offset = child_entries[child_list_index].offset;
+				source_offset = child_entries[child_list_index].offset;
 				break;
 			}
 		}
@@ -57,10 +71,12 @@ void ListFlattenFunction(DataChunk &args, ExpressionState &state, Vector &result
 			auto child_list_index = child_data.sel->get_index(list_entry.offset + j);
 			if (child_data.validity.RowIsValid(child_list_index)) {
 				auto child_entry = child_entries[child_list_index];
-				length = child_entry.offset + child_entry.length - offset;
+				length = child_entry.offset + child_entry.length - source_offset;
 				break;
 			}
 		}
+        ListVector::Append(result, data_vector, source_offset + length, source_offset);
+
 		result_entries[i].offset = offset;
 		result_entries[i].length = length;
 		offset += length;
@@ -101,10 +117,12 @@ static unique_ptr<BaseStatistics> ListFlattenStats(ClientContext &context, Bound
 		return nullptr;
 	}
 	auto &list_stats = (ListStatistics &)*child_stats[0];
-	if (!list_stats.child_stats) {
+	if (!list_stats.child_stats || list_stats.child_stats->type == LogicalTypeId::SQLNULL) {
 		return nullptr;
 	}
+
 	auto child_copy = list_stats.child_stats->Copy();
+	child_copy->validity_stats = make_unique<ValidityStatistics>(true);
 	return child_copy;
 }
 
