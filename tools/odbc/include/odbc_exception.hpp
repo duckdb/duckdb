bf94f858ba16b8119b392d95b81929fdab4765ad
Tiago Kepe <tiagokepe@gmail.com>
2022-02-28 18:16:01 -0300

    ODBC: removing unused/commented code.

--- tools/odbc/include/odbc_exception.hpp
+++ tools/odbc/include/odbc_exception.hpp
@@ -14,16 +14,6 @@ public:
 	explicit OdbcException(const std::string &comp, SQLRETURN ret, const DiagRecord &diag_rec)
 	    : component(comp), sql_ret(ret), diag_record(diag_rec) {};
 
-	// explicit OdbcException(const std::string &msg, const std::string &sqlstate="", const std::string &server_name="",
-	// SQLINTEGER col_number=SQL_NO_COLUMN_NUMBER, SQLINTEGER sql_native=0, SQLLEN row_number=SQL_NO_ROW_NUMBER);
-	// explicit OdbcException(const duckdb::DiagRecord &diag_rec): diag_record(diag_rec) {
-	// };
-
-	// void SetComponent(const std::string &comp);
-	// void SetSqlReturn(SQLRETURN ret);
-	// void SetMessage(const std::string &message);
-
-	// const std::string GetMessage();
 	const std::string GetComponent();
 	const SQLRETURN GetSqlReturn();
 	const DiagRecord GetDiagRecord();
