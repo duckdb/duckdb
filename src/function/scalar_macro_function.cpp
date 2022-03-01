fdc80612f528dae0a2d4f752dd735980b561c34e
hmb1 <henryb@hush.com>
2022-03-01 19:19:54 +0000

    fix OSX regression

--- src/function/scalar_macro_function.cpp
+++ src/function/scalar_macro_function.cpp
@@ -25,8 +25,8 @@ ScalarMacroFunction::ScalarMacroFunction(void) : MacroFunction(MacroType::SCALAR
 }
 
 unique_ptr<MacroFunction> ScalarMacroFunction::Copy() {
-	auto result = make_unique<ScalarMacroFunction>(move(expression->Copy()));
-	// result->expression=expression->Copy();
+	auto result = make_unique<ScalarMacroFunction>();
+	result->expression=expression->Copy();
 	CopyProperties(*result);
 
 	return move(result);
