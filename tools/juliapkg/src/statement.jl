514c5cb1fab6c626ada6e9dc650bd44f1fbe26a9
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-03-01 17:03:12 +0100

    Clean up

--- tools/juliapkg/src/statement.jl
+++ tools/juliapkg/src/statement.jl
@@ -1,31 +1,3 @@
-# # prepare given sql statement
-# function _Stmt(db::DB, sql::AbstractString)
-#     handle = Ref{duckdb_prepared_statement}()
-#     sqliteprepare(db, sql, handle, Ref{duckdb_prepared_statement}())
-#     return _Stmt(handle[])
-# end
-#
-# """
-#     SQLite.Stmt(db, sql) => SQL.Stmt
-#
-# Prepares an optimized internal representation of SQL statement in
-# the context of the provided SQLite3 `db` and constructs the `SQLite.Stmt`
-# Julia object that holds a reference to the prepared statement.
-#
-# *Note*: the `sql` statement is not actually executed, but only compiled
-# (mainly for usage where the same statement is executed multiple times
-# with different parameters bound as values).
-#
-# Internally `SQLite.Stmt` constructor creates the [`SQLite._Stmt`](@ref) object that is managed by `db`.
-# `SQLite.Stmt` references the `SQLite._Stmt` by its unique id.
-#
-# The `SQLite.Stmt` will be automatically closed/shutdown when it goes out of scope
-# (i.e. the end of the Julia session, end of a function call wherein it was created, etc.).
-# One can also call `DBInterface.close!(stmt)` to immediately close it.
-#
-# All prepared statements of a given DB connection are also automatically closed when the
-# DB is disconnected or when [`SQLite.finalize_statements!`](@ref) is explicitly called.
-# """
 mutable struct Stmt <: DBInterface.Statement
     con::Connection
     handle::duckdb_prepared_statement
@@ -56,42 +28,6 @@ end
 
 DBInterface.getconnection(stmt::Stmt) = stmt.con
 
-# function bind! end
-#
-# function bind!(stmt::_Stmt, params::DBInterface.NamedStatementParams)
-#     nparams = sqlite3_bind_parameter_count(stmt.handle)
-#     (nparams <= length(params)) || throw(SQLiteException("values should be provided for all query placeholders"))
-#     for i in 1:nparams
-#         name = unsafe_string(sqlite3_bind_parameter_name(stmt.handle, i))
-#         isempty(name) && throw(SQLiteException("nameless parameters should be passed as a Vector"))
-#         # name is returned with the ':', '@' or '$' at the start
-#         sym = Symbol(name[2:end])
-#         haskey(params, sym) || throw(SQLiteException("`$name` not found in values keyword arguments to bind to sql statement"))
-#         bind!(stmt, i, params[sym])
-#     end
-# end
-
-# function bind!(stmt::_Stmt, values::DBInterface.PositionalStatementParams)
-#     nparams = sqlite3_bind_parameter_count(stmt.handle)
-#     (nparams == length(values)) || throw(SQLiteException("values should be provided for all query placeholders"))
-#     for i in 1:nparams
-#         @inbounds bind!(stmt, i, values[i])
-#     end
-# end
-
-# bind!(stmt::Stmt, values::DBInterface.StatementParams) = bind!(_stmt(stmt), values)
-#
-# bind!(stmt::Union{_Stmt, Stmt}; kwargs...) = bind!(stmt, kwargs.data)
-
-# Binding parameters to SQL statements
-# function bind!(stmt::_Stmt, name::AbstractString, val::Any)
-#     i::Int = sqlite3_bind_parameter_index(stmt.handle, name)
-#     if i == 0
-#         throw(SQLiteException("SQL parameter $name not found in $stmt"))
-#     end
-#     return bind!(stmt, i, val)
-# end
-
 duckdb_bind_internal(stmt::Stmt, i::Integer, val::AbstractFloat) = duckdb_bind_double(stmt.handle, i, Float64(val));
 duckdb_bind_internal(stmt::Stmt, i::Integer, val::Bool) = duckdb_bind_boolean(stmt.handle, i, val);
 duckdb_bind_internal(stmt::Stmt, i::Integer, val::Int8) = duckdb_bind_int8(stmt.handle, i, val);
@@ -114,7 +50,6 @@ duckdb_bind_internal(stmt::Stmt, i::Integer, val::WeakRefString{UInt8}) =
 function duckdb_bind_internal(stmt::Stmt, i::Integer, val::Any)
 	println(val);
     throw(NotImplementedException("unsupported type for bind"))
-    # bind!(stmt, i, sqlserialize(val))
 end
 
 function BindParameters(stmt::Stmt, params::DBInterface.StatementParams)
