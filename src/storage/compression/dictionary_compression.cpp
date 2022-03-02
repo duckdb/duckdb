1e9d46051bbaa1df70f44604dfa8819769653122
Sam Ansmink <samansmink@hotmail.com>
2022-03-02 17:44:13 +0100

    feat: new string hash function, explicit switch fallthroughs

--- src/storage/compression/dictionary_compression.cpp
+++ src/storage/compression/dictionary_compression.cpp
@@ -10,56 +10,9 @@
 
 namespace duckdb {
 
-// https://github.com/martinus/robin-hood-hashing/releases/tag/3.11.5
-inline size_t HashBytes(void const *ptr, size_t len) noexcept {
-	static constexpr uint64_t M = UINT64_C(0xc6a4a7935bd1e995);
-	static constexpr uint64_t SEED = UINT64_C(0xe17a1465);
-	static constexpr unsigned int R = 47;
-
-	auto const *const data64 = static_cast<uint64_t const *>(ptr);
-	uint64_t h = SEED ^ (len * M);
-
-	size_t const n_blocks = len / 8;
-	for (size_t i = 0; i < n_blocks; ++i) {
-		auto k = Load<uint64_t>(reinterpret_cast<const_data_ptr_t>(data64 + i));
-
-		k *= M;
-		k ^= k >> R;
-		k *= M;
-
-		h ^= k;
-		h *= M;
-	}
-
-	auto const *const data8 = reinterpret_cast<uint8_t const *>(data64 + n_blocks);
-	switch (len & 7U) {
-	case 7:
-		h ^= static_cast<uint64_t>(data8[6]) << 48U;
-	case 6:
-		h ^= static_cast<uint64_t>(data8[5]) << 40U;
-	case 5:
-		h ^= static_cast<uint64_t>(data8[4]) << 32U;
-	case 4:
-		h ^= static_cast<uint64_t>(data8[3]) << 24U;
-	case 3:
-		h ^= static_cast<uint64_t>(data8[2]) << 16U;
-	case 2:
-		h ^= static_cast<uint64_t>(data8[1]) << 8U;
-	case 1:
-		h ^= static_cast<uint64_t>(data8[0]);
-		h *= M;
-	default:
-		break;
-	}
-	h ^= h >> R;
-	h *= M;
-	h ^= h >> R;
-	return static_cast<size_t>(h);
-}
-
 struct StringHash {
 	std::size_t operator()(const string_t &k) const {
-		return HashBytes(k.GetDataUnsafe(), k.GetSize());
+		return Hash(k.GetDataUnsafe(), k.GetSize());
 	}
 };
 
