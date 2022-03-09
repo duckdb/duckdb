8fa404af3f1d710c48ee96dc9e6e758deaf901ab
Xi Chen <90917509+chenxi8611@users.noreply.github.com>
2022-03-10 00:48:31 +0800

    Foreignkey (#2)
    restore original code

--- src/include/duckdb/storage/data_table.hpp
+++ src/include/duckdb/storage/data_table.hpp
@@ -227,8 +227,7 @@ private:
 	//! Verify constraints with a chunk from the Append containing all columns of the table
 	void VerifyAppendConstraints(TableCatalogEntry &table, ClientContext &context, DataChunk &chunk);
 	//! Verify constraints with a chunk from the Update containing only the specified column_ids
-	void VerifyUpdateConstraints(TableCatalogEntry &table, ClientContext &context, DataChunk &chunk,
-	                             const vector<column_t> &column_ids, Vector &row_ids, idx_t count);
+	void VerifyUpdateConstraints(TableCatalogEntry &table, DataChunk &chunk, const vector<column_t> &column_ids);
 	//! Verify constraints with a chunk from the Delete containing all columns of the table
 	void VerifyDeleteConstraints(TableCatalogEntry &table, ClientContext &context, DataChunk &chunk);
 
