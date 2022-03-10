f97eb50d6c7480b0ab5a769552d7559723144fe5
Sam Ansmink <samansmink@hotmail.com>
2022-03-10 17:33:57 +0100

    feat: 64bit md5 functions

--- src/function/scalar/string/md5.cpp
+++ src/function/scalar/string/md5.cpp
@@ -18,10 +18,35 @@ struct MD5Operator {
 	}
 };
 
+template <bool lower>
+struct MD5NumberOperator {
+	template <class INPUT_TYPE, class RESULT_TYPE>
+	static RESULT_TYPE Operation(INPUT_TYPE input) {
+		data_t digest[MD5Context::MD5_HASH_LENGTH_BINARY];
+
+		MD5Context context;
+		context.Add(input);
+		context.Finish(digest);
+		return *reinterpret_cast<uint64_t*>(&digest[lower ? 8 : 0]);
+	}
+};
+
 static void MD5Function(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto &input = args.data[0];
 
 	UnaryExecutor::ExecuteString<string_t, string_t, MD5Operator>(input, result, args.size());
+}
+
+static void MD5UpperFunction(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &input = args.data[0];
+
+	UnaryExecutor::Execute<string_t, uint64_t, MD5NumberOperator<false>>(input, result, args.size());
+}
+
+static void MD5LowerFunction(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &input = args.data[0];
+
+	UnaryExecutor::Execute<string_t, uint64_t, MD5NumberOperator<true>>(input, result, args.size());
 }
 
 void MD5Fun::RegisterFunction(BuiltinFunctions &set) {
@@ -29,6 +54,16 @@ void MD5Fun::RegisterFunction(BuiltinFunctions &set) {
 	                               {LogicalType::VARCHAR}, // argument list
 	                               LogicalType::VARCHAR,   // return type
 	                               MD5Function));          // pointer to function implementation
+
+	set.AddFunction(ScalarFunction("md5_number_upper",        // name of the function
+	                               {LogicalType::VARCHAR}, // argument list
+	                               LogicalType::UBIGINT,  // return type
+	                               MD5UpperFunction));          // pointer to function implementation
+
+	set.AddFunction(ScalarFunction("md5_number_lower",                  // name of the function
+	                               {LogicalType::VARCHAR}, // argument list
+	                               LogicalType::UBIGINT,   // return type
+	                               MD5LowerFunction));          // pointer to function implementation
 }
 
 } // namespace duckdb
