d74892ddacf62b874185b4acdf2e583447a29bcc
Reijo Sund <reijo.sund@uef.fi>
2022-03-11 17:26:56 +0200

    Format fix

--- tools/rpkg/src/register.cpp
+++ tools/rpkg/src/register.cpp
@@ -238,20 +238,20 @@ unique_ptr<TableFunctionRef> duckdb::ArrowScanReplacement(const string &table_na
 	static_cast<cpp11::sexp>(conn->db_eptr).attr("_registered_arrow_" + name) = R_NilValue;
 }
 
-[[cpp11::register]] void rapi_register_parquet(duckdb::conn_eptr_t conn, std::string name, std::string path, bool binary_as_string) {
-  if (!conn || !conn->conn) {
-    cpp11::stop("rapi_register_parquet: Invalid connection");
-  }
-  if (name.empty()) {
-    cpp11::stop("rapi_register_parquet: Name cannot be empty");
-  }
-  vector<Value> params;
-  params.emplace_back(path);
-  named_parameter_map_t named_parameters({{"binary_as_string", Value::BOOLEAN(binary_as_string)}});
-  try {
-    conn->conn->TableFunction("parquet_scan", params, named_parameters)
-    ->CreateView(name, true, true);
-  } catch (std::exception &e) {
-    cpp11::stop("rapi_register_parquet: Failed to register parquet: %s", e.what());
-  }
+[[cpp11::register]] void rapi_register_parquet(duckdb::conn_eptr_t conn, std::string name, std::string path,
+                                               bool binary_as_string) {
+	if (!conn || !conn->conn) {
+		cpp11::stop("rapi_register_parquet: Invalid connection");
+	}
+	if (name.empty()) {
+		cpp11::stop("rapi_register_parquet: Name cannot be empty");
+	}
+	vector<Value> params;
+	params.emplace_back(path);
+	named_parameter_map_t named_parameters({{"binary_as_string", Value::BOOLEAN(binary_as_string)}});
+	try {
+		conn->conn->TableFunction("parquet_scan", params, named_parameters)->CreateView(name, true, true);
+	} catch (std::exception &e) {
+		cpp11::stop("rapi_register_parquet: Failed to register parquet: %s", e.what());
+	}
 }
