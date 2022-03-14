d6485ac6a861c5adc9dd6e85788a68be938846a0
Tmonster <tom@ebergen.com>
2022-03-14 16:48:05 +0100

    hopefully last pull request notes

--- src/execution/operator/persistent/physical_insert.cpp
+++ src/execution/operator/persistent/physical_insert.cpp
@@ -15,7 +15,6 @@ namespace duckdb {
 class InsertGlobalState : public GlobalSinkState {
 public:
 	InsertGlobalState() : insert_count(0), returned_chunk_count(0) {
-		return_chunk_collection = ChunkCollection();
 	}
 
 	mutex lock;
