80c26079dae8e6c49dd331fc6d4ef6fcac3bf862
Pedro Holanda <pedroholanda@gmail.com>
2022-03-15 19:57:50 +0100

    Commited wrong version of the update test

--- tools/pythonpkg/tests/fast/pandas/test_pandas_update.py
+++ tools/pythonpkg/tests/fast/pandas/test_pandas_update.py
@@ -1,12 +1,12 @@
 import duckdb
 import pandas as pd
 
-class TestPandasUnregister(object):
-	def test_pandas_unregister1(self, duckdb_cursor):
+class TestPandasUpdateList(object):
+	def test_pandas_update_list(self, duckdb_cursor):
 		duckdb_cursor = duckdb.connect(':memory:')
 		duckdb_cursor.execute('create table t (l int[])')
 		duckdb_cursor.execute('insert into t values ([1, 2]), ([3,4])')
 		duckdb_cursor.execute('update t set l = [5, 6]')
-		assertduckdb_cursor.execute('select * from t').fetchdf()['l']  == 0
+		assert duckdb_cursor.execute('select * from t').fetchdf()['l'].tolist()  == [[5, 6], [5, 6]]
 
 
