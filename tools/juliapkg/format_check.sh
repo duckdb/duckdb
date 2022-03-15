da923679c69fea99d822e41e9448d8b5d266635c
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-03-15 18:29:47 +0100

    format_check update

--- tools/juliapkg/format_check.sh
+++ tools/juliapkg/format_check.sh
@@ -1,5 +1,10 @@
 set -e
 
+if [[ $(git diff) ]]; then
+  echo "There are already differences prior to the format! Commit your changes prior to running format_check.sh"
+  exit 1
+fi
+
 ./format.sh
 if [[ $(git diff) ]]; then
     echo "Julia format found differences:"
