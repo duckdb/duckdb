8ba4d31ca3d9699c1a1110995d3972725f91da23
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-04-04 17:07:38 +0200

    Rebind parameters: Python, R and Julia test cases

---
+++ tools/pythonpkg/tests/fast/test_ambiguous_prepare.py
@@ -0,0 +1,12 @@
+import duckdb
+import pandas as pd
+import pytest
+
+class TestAmbiguousPrepare(object):
+    def test_bool(self, duckdb_cursor):
+        conn = duckdb.connect()
+        res = conn.execute("select ?, ?, ?", (True, 42, [1, 2, 3])).fetchall()
+        assert res[0][0] == True
+        assert res[0][1] == 42
+        assert res[0][2] == [1, 2, 3]
+
