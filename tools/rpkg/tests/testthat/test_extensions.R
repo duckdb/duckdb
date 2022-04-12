81f49d898755fe4c405638eb95a69edf277b6804
Sam Ansmink <samansmink@hotmail.com>
2022-04-12 10:55:35 +0200

    fixed path for R extension test

--- tools/rpkg/tests/testthat/test_extensions.R
+++ tools/rpkg/tests/testthat/test_extensions.R
@@ -4,9 +4,8 @@ library("testthat")
 test_that("extensions can be loaded", {
   con <- dbConnect(duckdb::duckdb())
   on.exit(dbDisconnect(con, shutdown = TRUE))
-
   matches = list.files(pattern='*.duckdb_extension', recursive=FALSE, full.names=TRUE)
-  matches <- c(matches, list.files('/tmp/*/*.duckdb_extension', recursive=FALSE, full.names=TRUE))
+  matches <- c(matches, list.files(path='/tmp/duckdb_extensions', pattern='*.duckdb_extension', recursive=TRUE, full.names=TRUE))
 
   require_variable <- Sys.getenv("DUCKDB_R_TEST_EXTENSION_REQUIRED")
 
