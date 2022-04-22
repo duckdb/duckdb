d29001df1fe1e575dc2a295fc6cd56094cbbbdcf
Tishj <t_b@live.nl>
2022-04-22 13:49:39 +0200

    fixed a typo in a 'value.hpp' comment

--- src/include/duckdb/common/types/value.hpp
+++ src/include/duckdb/common/types/value.hpp
@@ -147,7 +147,7 @@ public:
 	DUCKDB_API static Value LIST(LogicalType child_type, vector<Value> values);
 	//! Create an empty list with the specified child-type
 	DUCKDB_API static Value EMPTYLIST(LogicalType child_type);
-	//! Creat a map value from a (key, value) pair
+	//! Create a map value from a (key, value) pair
 	DUCKDB_API static Value MAP(Value key, Value value);
 
 	//! Create a blob Value from a data pointer and a length: no bytes are interpreted
