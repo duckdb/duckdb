6fc9074744acea72cce3a8aba7dfa33248be8eb3
Tishj <t_b@live.nl>
2022-05-09 12:48:43 +0200

    map_from_entries functional

--- src/function/scalar/map/map_from_entries.cpp
+++ src/function/scalar/map/map_from_entries.cpp
@@ -10,7 +10,7 @@ namespace duckdb {
 static void MapFromEntriesFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	D_ASSERT(result.GetType().id() == LogicalTypeId::MAP);
 
-	//! Otherwise if its not a constant vector, this breaks the optimizer
+	// ! Otherwise if its not a constant vector, this breaks the optimizer
 	result.SetVectorType(VectorType::CONSTANT_VECTOR);
 	for (idx_t i = 0; i < args.ColumnCount(); i++) {
 		if (args.data[i].GetVectorType() != VectorType::CONSTANT_VECTOR) {
@@ -18,22 +18,32 @@ static void MapFromEntriesFunction(DataChunk &args, ExpressionState &state, Vect
 		}
 	}
 
+	//Get the arguments vector
+	auto& array = args.data[0];
+	idx_t pair_amount = ListVector::GetListSize(array);
+	auto& entries = ListVector::GetEntry(array);
+
+	//Prepare the result vectors
+	result.Resize(0, pair_amount);
 	auto &child_entries = StructVector::GetEntries(result);
 	D_ASSERT(child_entries.size() == 2);
 	auto &key_vector = child_entries[0];
 	auto &value_vector = child_entries[1];
 
-	idx_t pair_amount = 2;
-
-	key_vector->Resize(0, pair_amount);
-	value_vector->Resize(0, pair_amount);
-
+	auto key_data = FlatVector::GetData<list_entry_t>(*key_vector);
+	auto value_data = FlatVector::GetData<list_entry_t>(*value_vector);
+	//Transform to mapped values
+	key_data[0].offset = 0;
+	value_data[0].offset = 0;
 	for (idx_t i = 0; i < pair_amount; i++) {
-		auto data = args.data[0].GetValue(i);
-		auto children = ListValue::GetChildren(data);
-		key_vector->SetValue(i, children[0]);
-		value_vector->SetValue(i, children[1]);
+		auto element = entries.GetValue(i);
+		D_ASSERT(element.type().id() == LogicalTypeId::STRUCT);
+		auto& key_value = StructValue::GetChildren(element);
+		ListVector::PushBack(*key_vector, key_value[0]);
+		ListVector::PushBack(*value_vector, key_value[1]);
 	}
+	key_data[0].length = pair_amount;
+	value_data[0].length = pair_amount;
 
 	result.Verify(args.size());
 }
@@ -57,8 +67,8 @@ static unique_ptr<FunctionData> MapFromEntriesBind(ClientContext &context, Scala
 	if (children.size() != 2) {
 		throw Exception("Struct should only contain 2 fields, a key and a value");
 	}
-	child_types.push_back(make_pair("key", children[0].second));
-	child_types.push_back(make_pair("value", children[1].second));
+	child_types.push_back(make_pair("key", LogicalType::LIST(children[0].second)));
+	child_types.push_back(make_pair("value", LogicalType::LIST(children[1].second)));
 
 	//! this is more for completeness reasons
 	bound_function.return_type = LogicalType::MAP(move(child_types));
