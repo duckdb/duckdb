d922a8796e3bf1ebeba30dcac13ddc084d382162
Pedro Ferreira <pedrotadim@outlook.com>
2022-05-10 11:43:53 +0200

    Code review fixes

--- src/optimizer/rule/equal_or_null_simplification.cpp
+++ src/optimizer/rule/equal_or_null_simplification.cpp
@@ -8,61 +8,105 @@ namespace duckdb {
 
 EqualOrNullSimplification::EqualOrNullSimplification(ExpressionRewriter &rewriter) : Rule(rewriter) {
 	// match on OR conjunction
-	root = make_unique<ExpressionMatcher>();
-	root->expr_type = make_unique<SpecificExpressionTypeMatcher>(ExpressionType::CONJUNCTION_OR);
+	auto op = make_unique<ConjunctionExpressionMatcher>();
+	op->expr_type = make_unique<SpecificExpressionTypeMatcher>(ExpressionType::CONJUNCTION_OR);
+	op->policy = SetMatcher::Policy::SOME;
+
+	// equi comparison on one side
+	auto equal_child = make_unique<ComparisonExpressionMatcher>();
+	equal_child->expr_type = make_unique<SpecificExpressionTypeMatcher>(ExpressionType::COMPARE_EQUAL);
+	equal_child->policy = SetMatcher::Policy::SOME;
+	op->matchers.push_back(move(equal_child));
+
+	// AND conjuction on the other
+	auto and_child = make_unique<ConjunctionExpressionMatcher>();
+	and_child->expr_type = make_unique<SpecificExpressionTypeMatcher>(ExpressionType::CONJUNCTION_AND);
+	and_child->policy = SetMatcher::Policy::SOME;
+
+	// IS NULL tests inside AND
+	auto isnull_child = make_unique<ExpressionMatcher>();
+	isnull_child->expr_type = make_unique<SpecificExpressionTypeMatcher>(ExpressionType::OPERATOR_IS_NULL);
+	// I could try to use std::make_unique for a copy, but it's available from C++14 only
+	auto isnull_child2 = make_unique<ExpressionMatcher>();
+	isnull_child2->expr_type = make_unique<SpecificExpressionTypeMatcher>(ExpressionType::OPERATOR_IS_NULL);
+	and_child->matchers.push_back(move(isnull_child));
+	and_child->matchers.push_back(move(isnull_child2));
+
+	op->matchers.push_back(move(and_child));
+	root = move(op);
 }
 
 // a=b OR (a IS NULL AND b IS NULL) to a IS NOT DISTINCT FROM b
-static unique_ptr<Expression> TryRewriteEqualOrIsNull(const Expression *e1, const Expression *e2) {
-	if (e1->type == ExpressionType::COMPARE_EQUAL && e2->type == ExpressionType::CONJUNCTION_AND &&
-	    ((BoundConjunctionExpression *)e2)->children.size() == 2) {
-		const auto equal_comp = (BoundComparisonExpression *)e1;
-		const auto a_exp = equal_comp->left.get(), b_exp = equal_comp->right.get();
-		bool valid = true, a_is_null_found = false, b_is_null_found = false;
+static unique_ptr<Expression> TryRewriteEqualOrIsNull(const Expression *equal_expr, const Expression *and_expr) {
+	if (equal_expr->type != ExpressionType::COMPARE_EQUAL || and_expr->type != ExpressionType::CONJUNCTION_AND) {
+		return nullptr;
+	}
 
-		// Make sure on the AND conjuction the relevant conditions appear
-		for (const auto &item : ((BoundConjunctionExpression *)e2)->children) {
-			const auto next_exp = item.get();
+	const auto equal_cast = (BoundComparisonExpression *)equal_expr;
+	const auto and_cast = (BoundConjunctionExpression *)and_expr;
 
-			if (next_exp->type == ExpressionType::OPERATOR_IS_NULL) {
-				const auto child = ((BoundOperatorExpression *)next_exp)->children[0].get();
-				if (Expression::Equals(child, a_exp)) {
-					a_is_null_found = true;
-				} else if (Expression::Equals(child, b_exp)) {
-					b_is_null_found = true;
-				} else {
-					valid = false;
-					break;
-				}
+	if (and_cast->children.size() != 2) {
+		return nullptr;
+	}
+
+	// Make sure on the AND conjuction the relevant conditions appear
+	const auto a_exp = equal_cast->left.get();
+	const auto b_exp = equal_cast->right.get();
+	bool valid = true;
+	bool a_is_null_found = false;
+	bool b_is_null_found = false;
+
+	for (const auto &item : and_cast->children) {
+		const auto next_exp = item.get();
+
+		if (next_exp->type == ExpressionType::OPERATOR_IS_NULL) {
+			const auto next_exp_cast = (BoundOperatorExpression *)next_exp;
+			const auto child = next_exp_cast->children[0].get();
+
+			// Test for equality on both 'a' and 'b' expressions
+			if (Expression::Equals(child, a_exp)) {
+				a_is_null_found = true;
+			} else if (Expression::Equals(child, b_exp)) {
+				b_is_null_found = true;
 			} else {
 				valid = false;
 				break;
 			}
+		} else {
+			valid = false;
+			break;
 		}
-		if (valid && a_is_null_found && b_is_null_found) {
-			return make_unique<BoundComparisonExpression>(ExpressionType::COMPARE_NOT_DISTINCT_FROM,
-			                                              move(equal_comp->left), move(equal_comp->right));
-		}
+	}
+	if (valid && a_is_null_found && b_is_null_found) {
+		return make_unique<BoundComparisonExpression>(ExpressionType::COMPARE_NOT_DISTINCT_FROM, move(equal_cast->left),
+		                                              move(equal_cast->right));
 	}
 	return nullptr;
 }
 
 unique_ptr<Expression> EqualOrNullSimplification::Apply(LogicalOperator &op, vector<Expression *> &bindings,
                                                         bool &changes_made, bool is_root) {
-	const auto or_exp = bindings[0];
+	const Expression *or_exp = bindings[0];
 
-	if (((BoundConjunctionExpression *)or_exp)->children.size() == 2) {
-		const auto or_cast = (BoundConjunctionExpression *)or_exp;
-		const auto left_exp = or_cast->children[0].get(), right_exp = or_cast->children[1].get();
-		// Test for: a=b OR (a IS NULL AND b IS NULL)
-		auto one_try = TryRewriteEqualOrIsNull(left_exp, right_exp);
-		if (one_try != nullptr) {
-			return one_try;
-		}
-		// Test for: (a IS NULL AND b IS NULL) OR a=b
-		return TryRewriteEqualOrIsNull(right_exp, left_exp);
+	if (or_exp->type != ExpressionType::CONJUNCTION_OR) {
+		return nullptr;
 	}
-	return nullptr;
+
+	const auto or_exp_cast = (BoundConjunctionExpression *)or_exp;
+
+	if (or_exp_cast->children.size() != 2) {
+		return nullptr;
+	}
+
+	const auto left_exp = or_exp_cast->children[0].get();
+	const auto right_exp = or_exp_cast->children[1].get();
+	// Test for: a=b OR (a IS NULL AND b IS NULL)
+	auto first_try = TryRewriteEqualOrIsNull(left_exp, right_exp);
+	if (first_try) {
+		return first_try;
+	}
+	// Test for: (a IS NULL AND b IS NULL) OR a=b
+	return TryRewriteEqualOrIsNull(right_exp, left_exp);
 }
 
 } // namespace duckdb
