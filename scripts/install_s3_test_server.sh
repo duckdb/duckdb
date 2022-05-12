7f5789d0076e31f450f1bbc8553b555a29686c86
Sam Ansmink <samansmink@hotmail.com>
2022-05-12 17:59:55 +0200

    cleanup, add ninja to httpfs build

--- scripts/install_s3_test_server.sh
+++ scripts/install_s3_test_server.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# Note: needs sudo for editing hosts file
+# Note: needs sudo
 
-# Old s3rver config
-#npm install -g s3rver
-#echo '127.0.0.1 test-bucket.s3.s3rver-endpoint.com' >> /etc/hosts
-#echo '127.0.0.1 s3.s3rver-endpoint.com' >> /etc/hosts
-
-sudo apt-get install -y docker.io
+apt-get install -y docker.io
 docker --version
 echo '127.0.0.1 duckdb-minio.com' >> /etc/hosts
 echo '127.0.0.1 test-bucket.duckdb-minio.com' >> /etc/hosts
