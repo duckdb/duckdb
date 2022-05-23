afb63f610ccc140a933148768d6a5678f7168358
Sam Ansmink <samansmink@hotmail.com>
2022-05-23 13:58:40 +0200

    revert python to before RTLD_GLOBAL fix

--- tools/pythonpkg/clean.py
+++ tools/pythonpkg/clean.py
@@ -3,7 +3,7 @@ import duckdb, shutil, os
 # try:
 base_dir = duckdb.__file__.rsplit(os.path.sep, 1)[0]
 for fpath in os.listdir(base_dir):
-    if '_duckdb_extension' in fpath:
+    if 'duckdb' in fpath:
         full_path = os.path.join(base_dir, fpath)
         if os.path.isfile(full_path):
             os.remove(full_path)
