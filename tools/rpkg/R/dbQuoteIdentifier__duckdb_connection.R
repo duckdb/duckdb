b04ca1aa76e951c90dfb8c6370f93aee21678900
Hadley Wickham <h.wickham@gmail.com>
2022-05-26 09:24:02 -0500

    Update caching strategy & tests
    And quote all keywords, not just reserved words

--- tools/rpkg/R/dbQuoteIdentifier__duckdb_connection.R
+++ tools/rpkg/R/dbQuoteIdentifier__duckdb_connection.R
@@ -7,7 +7,7 @@ dbQuoteIdentifier__duckdb_connection <- function(conn, x, ...) {
   }
 
   x <- enc2utf8(x)
-  needs_escape <- !grepl("^[a-zA-Z0-9_]+$", x) | tolower(x) %in% reserved_words(conn)
+  needs_escape <- !grepl("^[a-zA-Z0-9_]+$", x) | tolower(x) %in% the$reserved_words
   x[needs_escape] <- paste0('"', gsub('"', '""', x[needs_escape]), '"')
 
   SQL(x, names = names(x))
@@ -19,14 +19,3 @@ setMethod("dbQuoteIdentifier", signature("duckdb_connection"), dbQuoteIdentifier
 setMethod("dbQuoteIdentifier", signature("duckdb_connection", "character"), dbQuoteIdentifier__duckdb_connection)
 setMethod("dbQuoteIdentifier", signature("duckdb_connection", "SQL"), dbQuoteIdentifier__duckdb_connection)
 setMethod("dbQuoteIdentifier", signature("duckdb_connection", "Id"), dbQuoteIdentifier__duckdb_connection)
-
-reserved_words <- function(con) {
-  if (is.null(the$reserved_words)) {
-    res <- dbGetQuery(con,
-      "SELECT * FROM duckdb_keywords() WHERE keyword_category<>'unreserved';"
-    )
-    the$reserved_words <- res[[1]]
-  }
-
-  the$reserved_words
-}
