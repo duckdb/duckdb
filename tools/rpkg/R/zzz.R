d5d96101871beaab16e2af850acbd2ccc4550927
Hadley Wickham <h.wickham@gmail.com>
2022-05-26 10:22:06 -0500

    Try avoid S4 dispatch in .onLoad

--- tools/rpkg/R/zzz.R
+++ tools/rpkg/R/zzz.R
@@ -8,8 +8,12 @@
   s3_register("dbplyr::sql_escape_datetime", "duckdb_connection")
   s3_register("dplyr::tbl", "duckdb_connection")
 
-  con <- dbConnect(duckdb())
-  on.exit(dbDisconnect(con, shutdown = TRUE))
-  res <- dbGetQuery(con, "SELECT * FROM duckdb_keywords();")
-  the$reserved_words <- res[[1]]
+  con <- dbConnect__duckdb_driver(duckdb())
+  on.exit(dbDisconnect__duckdb_connection(con, shutdown = TRUE), add = TRUE)
+
+  rs <- dbSendQuery__duckdb_connection_character(con, "SELECT * FROM duckdb_keywords();")
+  on.exit(dbClearResult__duckdb_result(rs), add = TRUE)
+  the$reserved_words <- dbFetch__duckdb_result(rs)[[1]]
+
+  invisible()
 }
