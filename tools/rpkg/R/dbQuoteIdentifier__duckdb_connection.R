fce3a6aa6cd4766a012d2fa276096da8da95c7b2
Hadley Wickham <h.wickham@gmail.com>
2022-05-28 15:38:11 -0500

    Memomise on first use

--- tools/rpkg/R/dbQuoteIdentifier__duckdb_connection.R
+++ tools/rpkg/R/dbQuoteIdentifier__duckdb_connection.R
@@ -11,7 +11,7 @@ dbQuoteIdentifier__duckdb_connection <- function(conn, x, ...) {
   }
 
   x <- enc2utf8(x)
-  needs_escape <- !grepl("^[a-zA-Z0-9_]+$", x) | tolower(x) %in% the$reserved_words
+  needs_escape <- !grepl("^[a-zA-Z0-9_]+$", x) | tolower(x) %in% reserved_words()
   x[needs_escape] <- paste0('"', gsub('"', '""', x[needs_escape]), '"')
 
   SQL(x, names = names(x))
@@ -23,3 +23,15 @@ setMethod("dbQuoteIdentifier", signature("duckdb_connection"), dbQuoteIdentifier
 setMethod("dbQuoteIdentifier", signature("duckdb_connection", "character"), dbQuoteIdentifier__duckdb_connection)
 setMethod("dbQuoteIdentifier", signature("duckdb_connection", "SQL"), dbQuoteIdentifier__duckdb_connection)
 setMethod("dbQuoteIdentifier", signature("duckdb_connection", "Id"), dbQuoteIdentifier__duckdb_connection)
+
+reserved_words <- function() {
+  if (is.null(the$reserved_words)) {
+    con <- dbConnect__duckdb_driver(duckdb())
+    on.exit(dbDisconnect__duckdb_connection(con, shutdown = TRUE), add = TRUE)
+
+    rs <- dbSendQuery__duckdb_connection_character(con, "SELECT * FROM duckdb_keywords();")
+    on.exit(dbClearResult__duckdb_result(rs), add = TRUE)
+    the$reserved_words <- dbFetch__duckdb_result(rs)[[1]]
+  }
+  the$reserved_words
+}
