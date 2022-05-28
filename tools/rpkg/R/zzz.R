5250943c97c7125ce1ccc2be1596ce76e490b51b
Hadley Wickham <h.wickham@gmail.com>
2022-05-28 15:38:11 -0500

    Memomise on first use

--- tools/rpkg/R/zzz.R
+++ tools/rpkg/R/zzz.R
@@ -8,12 +8,5 @@
   s3_register("dbplyr::sql_escape_datetime", "duckdb_connection")
   s3_register("dplyr::tbl", "duckdb_connection")
 
-  con <- dbConnect__duckdb_driver(duckdb())
-  on.exit(dbDisconnect__duckdb_connection(con, shutdown = TRUE), add = TRUE)
-
-  rs <- dbSendQuery__duckdb_connection_character(con, "SELECT * FROM duckdb_keywords();")
-  on.exit(dbClearResult__duckdb_result(rs), add = TRUE)
-  the$reserved_words <- dbFetch__duckdb_result(rs)[[1]]
-
   invisible()
 }
