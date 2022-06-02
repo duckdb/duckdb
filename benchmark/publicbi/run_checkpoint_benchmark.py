40cd39641cacd51d6116e4be689856d1a0ad5a7b
Jim Stam <jimstam@hotmail.com>
2022-06-02 11:39:17 +0200

    Benchmark 9, with autocheckpoint

--- benchmark/publicbi/run_checkpoint_benchmark.py
+++ benchmark/publicbi/run_checkpoint_benchmark.py
@@ -51,6 +51,7 @@ def run_checkpoint(subdir, subset, rle_sorting):
 
     # Set sorting PRAGMA and force compression
     # duckdb_conn.execute("pragma force_compression='rle'")
+    duckdb_conn.execute("pragma wal_autocheckpoint='1TB'")
     duckdb_conn.execute(f"pragma force_compression_sorting='{rle_sorting}'")
 
     # Load in the tables and checkpoint
