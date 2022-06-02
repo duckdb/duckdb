1c7c14868e91d49f1ca3d4c9d4b5f4489ed1d1a8
Pedro Holanda <pedroholanda@gmail.com>
2022-06-02 14:56:03 +0200

    Adding other tpch queries

--- tools/pythonpkg/tests/fast/substrait/test_ibis_tpch.py
+++ tools/pythonpkg/tests/fast/substrait/test_ibis_tpch.py
@@ -1,11 +1,14 @@
-from ibis_substrait.compiler.core import SubstraitCompiler
-
-import ibis
-from ibis.backends.base import BaseBackend
-from ibis.backends.duckdb.datatypes import parse_type
-from ibis_tpch_util import get_tpch_query
+try:
+    from ibis_substrait.compiler.core import SubstraitCompiler
+    import ibis
+    from ibis.backends.base import BaseBackend
+    from ibis.backends.duckdb.datatypes import parse_type
+    can_run = True
+except:
+    can_run = False
 
 import duckdb
+from ibis_tpch_util import get_tpch_query
 
 def unbound_from_duckdb(table):  # noqa: D103
     return ibis.table(
@@ -53,15 +56,52 @@ def tpch_execute_ibis_to_duck_query(duck_con, query_number):
         proto = compiler.compile(tpch_query)
     except Exception:
         raise ValueError("can't compile")
-    result = duck_con.con.from_substrait(proto.SerializeToString()).df()
-    # proto_bytes = get_query_binary(query_number)
-    # result = duckdb_cursor.from_substrait(proto_bytes).df().sort_index(ascending=False, axis=1)
+    result = duck_con.con.from_substrait(proto.SerializeToString()).df().sort_index(ascending=False, axis=1)
     query = duck_con.con.execute("select query from tpch_queries() where query_nr="+str(query_number)).fetchone()[0]
-    answer = duck_con.con.execute(query).df()
-    print (result)
-    print (answer)
+    answer = duck_con.con.execute(query).df().sort_index(ascending=False, axis=1)
+    if not (result.equals(answer)):
+        print (query_number)
+        print (result)
+        print (answer)
     assert result.equals(answer)
 
 def test_ibis_to_duck_substrait(duckdb_cursor):
+    if not can_run:
+        return
     duck_con = TPCHBackend(fname="")
-    tpch_execute_ibis_to_duck_query(duck_con,1)
+    # can't compile
+    skip = [2]
+    # Scalar Function with name any does not exist!
+    skip.append(4)
+    # can't compile
+    skip.append(7)
+    # can't compile
+    skip.append(8)
+    # INTERNAL Error: Substrait type not yet supported
+    skip.append(9)
+    # Result does not match
+    skip.append(11)
+    # Scalar Function with name not does not exist!
+    skip.append(13)
+    # can't compile
+    skip.append(14)
+    # can't compile
+    skip.append(15)
+    # can't compile
+    skip.append(16)
+    # can't compile
+    skip.append(17)
+    # can't compile
+    skip.append(18)
+    # can't compile
+    skip.append(20)
+    # Scalar Function with name any does not exist!
+    skip.append(21)
+    # Scalar Function with name any does not exist!
+    skip.append(22)
+    for i in range(1,23):
+        if skip.count(i) > 0:
+            continue
+        tpch_execute_ibis_to_duck_query(duck_con,i)
+
+# tpch_execute_ibis_to_duck_query(duck_con,11)
