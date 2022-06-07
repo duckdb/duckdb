829a4deffe514584eff4d1b1cea73a9067c32656
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-06-07 10:31:55 +0200

    Julia Package History: v0.2.8

--- tools/juliapkg/test/runtests.jl
+++ tools/juliapkg/test/runtests.jl
@@ -4,13 +4,16 @@ using DuckDB
 #include("../src/DuckDB.jl")
 
 @testset "DB Connection" begin
-    con = DuckDB.connect(":memory:")
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
     @test isa(con, Base.RefValue{Ptr{Nothing}})
     DuckDB.disconnect(con)
+    DuckDB.close(db)
 end
 
 @testset "Test README" begin
-    con = DuckDB.connect(":memory:")
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
     res = DuckDB.execute(con, "CREATE TABLE integers(date DATE, jcol INTEGER)")
     res = DuckDB.execute(
         con,
@@ -20,18 +23,22 @@ end
     @test isa(DuckDB.toDataFrame(res), DataFrame)
     @test isa(DuckDB.toDataFrame(con, "SELECT * FROM integers"), DataFrame)
     DuckDB.disconnect(con)
+    DuckDB.close(db)
 end
 
 @testset "HUGE Int test" begin
-  con = DuckDB.connect(":memory:")  
-  res = DuckDB.execute(con,"CREATE TABLE huge(id INTEGER,data HUGEINT);")
-  res = DuckDB.execute(con,"INSERT INTO huge VALUES (1,NULL), (2, 1761718171), (3, 171661889178);")
-  res = DuckDB.toDataFrame(con,"SELECT * FROM huge")
-  DuckDB.disconnect(con)  
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
+    res = DuckDB.execute(con,"CREATE TABLE huge(id INTEGER,data HUGEINT);")
+    res = DuckDB.execute(con,"INSERT INTO huge VALUES (1,NULL), (2, 1761718171), (3, 171661889178);")
+    res = DuckDB.toDataFrame(con,"SELECT * FROM huge")
+    DuckDB.disconnect(con)
+    DuckDB.close(db)  
 end
 
 @testset "Interval type" begin
-    con = DuckDB.connect(":memory:")
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
     res = DuckDB.execute(con, "CREATE TABLE interval(interval INTERVAL);")
     res = DuckDB.execute(
         con,
@@ -46,10 +53,12 @@ INSERT INTO interval VALUES
     res = DuckDB.toDataFrame(con, "SELECT * FROM interval;")
     @test isa(res, DataFrame)
     DuckDB.disconnect(con)
+    DuckDB.close(db)
 end
 
-@testset "Interval type" begin
-    con = DuckDB.connect(":memory:")
+@testset "Timestamp" begin
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
     res = DuckDB.execute(con, "CREATE TABLE timestamp(timestamp TIMESTAMP , data INTEGER);")
     res = DuckDB.execute(
         con,
@@ -59,10 +68,12 @@ end
     res = DuckDB.toDataFrame(res)
     @test isa(res, DataFrame)
     DuckDB.disconnect(con)
+    DuckDB.close(db)
 end
 
 @testset "Items table" begin
-    con = DuckDB.connect(":memory:")
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
     res = DuckDB.execute(
         con,
         "CREATE TABLE items(item VARCHAR, value DECIMAL(10,2), count INTEGER);",
@@ -92,9 +103,24 @@ end
 @testset "Query CSV and output DataFrame" begin
     df = DataFrame(a=1:100, b=1:100)
     CSV.write("test_dataframe.csv", df)
-
-    con = DuckDB.connect(":memory:")
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
     df1 = DuckDB.toDataFrame(con, "SELECT * FROM 'test_dataframe.csv';")
-    
     @test df == df1
+    DuckDB.disconnect(con)
+    DuckDB.close(db)
+end
+
+@testset "Export and Query Parquet" begin
+    db = DuckDB.open(":memory:")
+    con = DuckDB.connect(db)
+    res = DuckDB.execute(con, "CREATE TABLE integers(date DATE, jcol INTEGER)")
+    res = DuckDB.execute(con,
+        "INSERT INTO integers VALUES ('2021-09-27', 4), ('2021-09-28', 6), ('2021-09-29', 8)")
+    res = DuckDB.execute(con, "COPY (SELECT * FROM integers) TO 'test.parquet' (FORMAT 'parquet');")
+    df1 = DuckDB.toDataFrame(con, "SELECT * FROM integers;")
+    df2 = DuckDB.toDataFrame(con, "SELECT * FROM 'test.parquet';")
+    @test df1 == df2
+    DuckDB.disconnect(con)
+    DuckDB.close(db)
 end
