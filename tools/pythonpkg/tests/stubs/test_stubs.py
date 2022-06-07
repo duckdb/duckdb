309fb771c9b004c6cba88f5151b75f135f36b7d5
Sam Ansmink <samansmink@hotmail.com>
2022-06-07 13:16:29 +0200

    fix stub test from pedro

--- tools/pythonpkg/tests/stubs/test_stubs.py
+++ tools/pythonpkg/tests/stubs/test_stubs.py
@@ -9,6 +9,8 @@ from typing import *
 MYPY_INI_PATH = os.path.join(os.path.dirname(__file__), 'mypy.ini')
 
 def test_stubs():
+	# The test has false positives on the following keywords
+	skip_stubs_errors = ['pybind11_module_local', 'git_revision']
 	# just run stubtest
 	stubs = subprocess.run([
 			sys.executable, '-m', 'mypy.stubtest',
@@ -17,9 +19,16 @@ def test_stubs():
 		], stdout=subprocess.PIPE)
 	if (stubs.returncode != 0):
 		errors = stubs.stdout.decode('utf-8').split('error')
+		broken_stubs = []
 		for error in errors:
-			if error != '' and 'pybind11_module_local' not in error:
-				print(errors)
+			add_error = True
+			for skip in skip_stubs_errors:
+				if error == '' or skip in error:
+					add_error = False
+					break
+			if (add_error):
+				broken_stubs.append(error)
+			if len(broken_stubs) > 0:
+				print("Stubs must be updated, either add them to skip_stubs_errors or update __init__.pyi accordingly")
+				print(broken_stubs)
 				assert(0)
-
-
