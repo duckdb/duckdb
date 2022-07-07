8781b91852c29c713b18f8c8bc0292d01b3027dd
Jim Stam <jimstam@hotmail.com>
2022-07-07 10:41:25 +0200

    Bash to sh

--- tools/pythonpkg/clean.sh
+++ tools/pythonpkg/clean.sh
@@ -3,7 +3,7 @@
 rm -rf .eggs .pytest_cache build dist duckdb.egg-info duckdb.cpp duckdb.hpp parquet-extension.cpp parquet-extension.hpp duckdb duckdb_tarball
 rm -f sources.list includes.list githash.list
 
-while [[ $# -ge 1 ]]
+while [ $# -ge 1 ]
 do
 key="$1"
   case $key in
