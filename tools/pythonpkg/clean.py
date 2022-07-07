7e3244c1b1a3cd556583110f3da86a4e7c2274a0
Jim Stam <jimstam@hotmail.com>
2022-07-07 09:58:33 +0200

    Add --force to clean script

--- tools/pythonpkg/clean.py
+++ tools/pythonpkg/clean.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 import shutil
 import sys
+import argparse
 
 # avoid importing from the current directory
 sys.path = sys.path[1:]
@@ -18,15 +19,23 @@ while 'duckdb' in pathlib.PurePath(next_dir).name:
 if 'duckdb' not in base_dir:
     raise Exception("Failed to find DuckDB path to delete")
 
-answer = ""
-while answer not in ["y", "yes", "n", "no"]:
-    print("The following directory and all files in it will be deleted:")
-    print(base_dir)
-    answer = input(f"Delete directory and all files in it? (y/n): ")
-    if answer.lower() in ["y", "yes"]:
-        shutil.rmtree(base_dir)
-    elif answer.lower() in ["n", "no"]:
-        print("Aborting clean.py")
-        quit()
-    else:
-        print("Please indicate if you wish to continue deletion (y) or abort (n)")
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--force', help="force deletion of folders", action='store_true')
+args = parser.parse_args()
+
+if args.force:
+    print(f"Deleting {base_dir}")
+    shutil.rmtree(base_dir)
+else:
+    answer = ""
+    while answer not in ["y", "yes", "n", "no"]:
+        print("The following directory and all files in it will be deleted:")
+        print(base_dir)
+        answer = input(f"Delete directory and all files in it? (y/n): ")
+        if answer.lower() in ["y", "yes"]:
+            shutil.rmtree(base_dir)
+        elif answer.lower() in ["n", "no"]:
+            print("Aborting clean.py")
+            quit()
+        else:
+            print("Please indicate if you wish to continue deletion (y) or abort (n)")
--- tools/pythonpkg/clean.sh
+++ tools/pythonpkg/clean.sh
@@ -2,4 +2,16 @@
 
 rm -rf .eggs .pytest_cache build dist duckdb.egg-info duckdb.cpp duckdb.hpp parquet-extension.cpp parquet-extension.hpp duckdb duckdb_tarball
 rm -f sources.list includes.list githash.list
+
+while [[ $# -ge 1 ]]
+do
+key="$1"
+  case $key in
+    -f | --force)
+    python3 clean.py -f
+        exit 1
+        ;;
+  esac
+shift
+done
 python3 clean.py
