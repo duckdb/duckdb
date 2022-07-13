1a02208343dd7f69e23efd1470da924790844d5e
Tishj <t_b@live.nl>
2022-07-13 12:49:15 +0200

    made the extra includes in setup.py more general, removed misleading, non-functional 'include_directories' from pythonpkg/CMakeLists

--- tools/pythonpkg/setup.py
+++ tools/pythonpkg/setup.py
@@ -154,17 +154,22 @@ class get_numpy_include(object):
         import numpy
         return numpy.get_include()
 
+#TODO: add missing third party includes
+# Add paths that should be included when building the python client here
+def extra_python_includes():
+	extra_includes = []
+	extra_includes += [os.path.join(script_path, '..', '..', 'third_party', 'fmt', 'include')] #third_party/fmt/include
+	return extra_includes
 
 extra_files = []
 header_files = []
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 main_include_path = os.path.join(script_path, 'src', 'include')
-#TODO: add all the third party include paths
-third_party_fmt_include_path = os.path.join(script_path, '..', '..', 'third_party', 'fmt', 'include')
 main_source_path = os.path.join(script_path, 'src')
 main_source_files = ['duckdb_python.cpp'] + [os.path.join('src', x) for x in os.listdir(main_source_path) if '.cpp' in x]
-include_directories = [main_include_path, third_party_fmt_include_path, get_numpy_include(), get_pybind_include(), get_pybind_include(user=True)]
+include_directories = [main_include_path, get_numpy_include(), get_pybind_include(), get_pybind_include(user=True)]
+include_directories += extra_python_includes()
 
 if len(existing_duckdb_dir) == 0:
     # no existing library supplied: compile everything from source
