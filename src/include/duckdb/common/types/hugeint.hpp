0b2064458a8466e6cd882f6b5951255e03a185be
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-07-15 17:37:51 +0200

    More fixes + fix test cases

--- src/include/duckdb/common/types/hugeint.hpp
+++ src/include/duckdb/common/types/hugeint.hpp
@@ -53,10 +53,10 @@ public:
 	}
 
 	static void NegateInPlace(hugeint_t &input) {
-		input.lower = NumericLimits<uint64_t>::Maximum() - input.lower + 1;
-		if (input.upper == NumericLimits<int64_t>::Minimum()) {
+		if (input.upper == NumericLimits<int64_t>::Minimum() && input.lower == 0) {
 			throw OutOfRangeException("HUGEINT is out of range");
 		}
+		input.lower = NumericLimits<uint64_t>::Maximum() - input.lower + 1;
 		input.upper = -1 - input.upper + (input.lower == 0);
 	}
 	static hugeint_t Negate(hugeint_t input) {
