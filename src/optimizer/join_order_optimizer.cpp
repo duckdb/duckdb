54d61a4c10043515aaf1f971206a10d0d166828c
Laurens Kuiper <laurens.kuiper@cwi.nl>
2022-07-22 14:28:05 +0200

    undo change

--- src/optimizer/join_order_optimizer.cpp
+++ src/optimizer/join_order_optimizer.cpp
@@ -212,7 +212,7 @@ static unique_ptr<JoinNode> CreateJoinTree(JoinRelationSet *set, NeighborInfo *i
 		expected_cardinality = MaxValue(left->cardinality, right->cardinality);
 	}
 	// cost is expected_cardinality plus the cost of the previous plans
-	idx_t cost = expected_cardinality + left->cost + right->cost;
+	idx_t cost = expected_cardinality;
 	return make_unique<JoinNode>(set, info, left, right, expected_cardinality, cost);
 }
 
