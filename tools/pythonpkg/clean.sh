902ab711b2c22c7dacec2b9ab10f6d9d7740c350
Jim Stam <jimstam@hotmail.com>
2022-07-26 09:27:49 +0200

    Use pip uninstall

--- tools/pythonpkg/clean.sh
+++ tools/pythonpkg/clean.sh
@@ -3,15 +3,4 @@
 rm -rf .eggs .pytest_cache build dist duckdb.egg-info duckdb.cpp duckdb.hpp parquet-extension.cpp parquet-extension.hpp duckdb duckdb_tarball
 rm -f sources.list includes.list githash.list
 
-while [ $# -ge 1 ]
-do
-key="$1"
-  case $key in
-    -f | --force)
-    python3 clean.py -f
-        exit 0
-        ;;
-  esac
-shift
-done
-python3 clean.py
+python3 -m pip uninstall duckdb
