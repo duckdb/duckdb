30e7a2f353e028866468960eec4241c474a7a180
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-07-26 14:37:33 +0200

    Checksum should not use hash function

--- src/common/checksum.cpp
+++ src/common/checksum.cpp
@@ -3,13 +3,17 @@
 
 namespace duckdb {
 
+hash_t Checksum(uint64_t x) {
+	return x * UINT64_C(0xbf58476d1ce4e5b9);
+}
+
 uint64_t Checksum(uint8_t *buffer, size_t size) {
 	uint64_t result = 5381;
 	uint64_t *ptr = (uint64_t *)buffer;
 	size_t i;
-	// for efficiency, we first hash uint64_t values
+	// for efficiency, we first checksum uint64_t values
 	for (i = 0; i < size / 8; i++) {
-		result ^= Hash(ptr[i]);
+		result ^= Checksum(ptr[i]);
 	}
 	if (size - i * 8 > 0) {
 		// the remaining 0-7 bytes we hash using a string hash
