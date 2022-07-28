8ce828591e6e5fbf9c603817b76c048619bbe83d
Sam Ansmink <samansmink@hotmail.com>
2022-07-28 13:35:39 +0200

    fix: oops

--- scripts/raspberry-pi-cmake-toolchain.cmake
+++ scripts/raspberry-pi-cmake-toolchain.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 # Define the cross compiler locations
-SET(CMAKE_C_COMPILER ${DUCKDB_RPI_TOOLCHAIsN_PREFIX}/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_C_COMPILER ${DUCKDB_RPI_TOOLCHAIN_PREFIX}/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER ${DUCKDB_RPI_TOOLCHAIN_PREFIX}/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
 # Define the sysroot path for the RaspberryPi distribution in our tools folder 
 SET(CMAKE_FIND_ROOT_PATH ${DUCKDB_RPI_TOOLCHAIN_PREFIX}/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/)
