60f7300774710b885c161babe3fbf34ee60810a1
Sam Ansmink <samansmink@hotmail.com>
2022-07-28 14:17:00 +0200

    ci: trying this

--- scripts/aarch64-cmake-toolchain.cmake
+++ scripts/aarch64-cmake-toolchain.cmake
@@ -2,9 +2,15 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 SET(CMAKE_SYSTEM_VERSION 1)
+
+if(NOT DUCKDB_AARCH64_TOOLCHAIN_GCC_VERSION)
+    set(DUCKDB_AARCH64_TOOLCHAIN_GCC_VERSION 8)
+endif()
+
 # Define the cross compiler locations
-set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc-8")
-set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++-8")
+set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc-${DUCKDB_AARCH64_TOOLCHAIN_GCC_VERSION}")
+set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++-${DUCKDB_AARCH64_TOOLCHAIN_GCC_VERSION}")
+
 # Define the sysroot path for the RaspberryPi distribution in our tools folder
 SET(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
 # Use our definitions for compiler tools
