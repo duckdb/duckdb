10ffa8f9b619bcd48785d278815e978c1e0c3ab5
Yves <yves@motherduck.com>
2022-07-28 18:19:47 -0400

    [api] -> [serialization]

--- test/api/test_plan_serialization.cpp
+++ test/api/test_plan_serialization.cpp
@@ -48,43 +48,43 @@ static void test_helper(string sql) {
 	printf("Optimized plan:\n%s\n", optimized_plan->ToString().c_str());
 }
 
-TEST_CASE("Test plan serialization", "[api]") {
-	test_helper("SELECT last_name,COUNT(*), min(first_name), max(first_name) FROM "
-	            "parquet_scan('data/parquet-testing/userdata1.parquet') GROUP BY last_name");
+TEST_CASE("Test plan serialization", "[serialization]") {
+	test_helper(
+	    "SELECT last_name,COUNT(*) FROM parquet_scan('data/parquet-testing/userdata1.parquet') GROUP BY last_name");
 }
 
-TEST_CASE("Test logical_dummy_scan", "[api]") {
+TEST_CASE("Test logical_dummy_scan", "[serialization]") {
 	test_helper("SELECT [1, 2, 3]");
 }
 
-TEST_CASE("Test logical_unnest", "[api]") {
+TEST_CASE("Test logical_unnest", "[serialization]") {
 	test_helper("SELECT UNNEST([1, 2, 3])");
 }
 
-TEST_CASE("Test bound_constant_expression", "[api]") {
+TEST_CASE("Test bound_constant_expression", "[serialization]") {
 	test_helper("SELECT 42 as i");
 }
 
-TEST_CASE("Test logical_window", "[api]") {
+TEST_CASE("Test logical_window", "[serialization]") {
 	test_helper("SELECT * FROM (SELECT 42 as i) WINDOW w AS (partition by i)");
 }
 
-TEST_CASE("Test logical_set", "[api]") {
+TEST_CASE("Test logical_set", "[serialization]") {
 	test_helper("SET memory_limit='10GB'");
 }
 
-TEST_CASE("Test logical_sample", "[api]") {
+TEST_CASE("Test logical_sample", "[serialization]") {
 	test_helper("SELECT * FROM (SELECT 42 as i) USING SAMPLE RESERVOIR(20%)");
 }
 
-TEST_CASE("Test logical_limit_percent", "[api]") {
+TEST_CASE("Test logical_limit_percent", "[serialization]") {
 	test_helper("SELECT 42 LIMIT 35%");
 }
 
-TEST_CASE("Test logical_limit", "[api]") {
+TEST_CASE("Test logical_limit", "[serialization]") {
 	test_helper("SELECT 42 LIMIT 1");
 }
 
-TEST_CASE("Test logical_join", "[api]") {
+TEST_CASE("Test logical_join", "[serialization]") {
 	test_helper("SELECT * FROM (SELECT 42 as i), (SELECT 42 as j) WHERE i = j");
 }
