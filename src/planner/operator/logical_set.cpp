9af294cb4ea71d27e541e683835ff73c9525bee3
stephaniewang <qw79@cornell.edu>
2022-07-28 20:10:58 -0400

    nit

--- src/planner/operator/logical_set.cpp
+++ src/planner/operator/logical_set.cpp
@@ -14,7 +14,6 @@ unique_ptr<LogicalOperator> LogicalSet::Deserialize(ClientContext &context, Logi
 	auto value = Value::Deserialize(reader.GetSource());
 	auto scope = reader.ReadRequired<SetScope>();
 	return make_unique<LogicalSet>(name, value, scope);
-	;
 }
 
 } // namespace duckdb
