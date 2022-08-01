30cc414dd88b062c951ee2f9fce882204a31b262
stephaniewang <qw79@cornell.edu>
2022-08-01 14:32:05 -0400

    add logical_delim_join

--- src/planner/operator/logical_delim_join.cpp
+++ src/planner/operator/logical_delim_join.cpp
@@ -3,12 +3,17 @@
 namespace duckdb {
 
 void LogicalDelimJoin::Serialize(FieldWriter &writer) const {
-	throw NotImplementedException(LogicalOperatorToString(type));
+	writer.WriteField(join_type);
+	writer.WriteSerializableList(duplicate_eliminated_columns);
 }
 
 unique_ptr<LogicalOperator> LogicalDelimJoin::Deserialize(ClientContext &context, LogicalOperatorType type,
                                                           FieldReader &reader) {
-	throw NotImplementedException(LogicalOperatorToString(type));
+	auto join_type = reader.ReadRequired<JoinType>();
+	auto duplicate_eliminated_columns = reader.ReadRequiredSerializableList<Expression>(context);
+	auto result = make_unique<LogicalDelimJoin>(join_type);
+	result->duplicate_eliminated_columns = move(duplicate_eliminated_columns);
+	return result;
 }
 
 } // namespace duckdb
