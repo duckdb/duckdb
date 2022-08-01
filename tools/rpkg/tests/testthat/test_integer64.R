a897973a0623fd171ed3b875c4f09365c2310724
Kirill Müller <krlmlr+r@mailbox.org>
2022-08-01 15:56:25 +0200

    Restore skip_if_not_installed()

--- tools/rpkg/tests/testthat/test_integer64.R
+++ tools/rpkg/tests/testthat/test_integer64.R
@@ -1,11 +1,13 @@
 # this tests both retrieval and scans
 test_that("we can roundtrip an integer64", {
-    con <- dbConnect(duckdb::duckdb(bigint="integer64"))
-    on.exit(dbDisconnect(con, shutdown = TRUE))
-    df <- data.frame(a = bit64::as.integer64(42), b = bit64::as.integer64(-42), c = bit64::as.integer64(NA))
+  skip_if_not_installed("bit64")
 
-    duckdb::duckdb_register(con, "df", df)
+  con <- dbConnect(duckdb::duckdb(bigint="integer64"))
+  on.exit(dbDisconnect(con, shutdown = TRUE))
+  df <- data.frame(a = bit64::as.integer64(42), b = bit64::as.integer64(-42), c = bit64::as.integer64(NA))
 
-    res <- dbReadTable(con, "df")
-    expect_identical(df, res)
+  duckdb::duckdb_register(con, "df", df)
+
+  res <- dbReadTable(con, "df")
+  expect_identical(df, res)
 })
