bc9d2bc6567d3a0f09fecb1386c9ff30b1f162ee
Kirill Müller <krlmlr+r@mailbox.org>
2022-08-01 10:23:22 +0200

    Avoid attaching package during tests

--- tools/rpkg/tests/testthat/test_integer64.R
+++ tools/rpkg/tests/testthat/test_integer64.R
@@ -1,15 +1,8 @@
-library("DBI")
-library("testthat")
-
-skip_if_not_installed("bit64")
-
-library("bit64")
-
 # this tests both retrieval and scans
 test_that("we can roundtrip an integer64", {
     con <- dbConnect(duckdb::duckdb(bigint="integer64"))
     on.exit(dbDisconnect(con, shutdown = TRUE))
-    df <- data.frame(a=as.integer64(42), b=as.integer64(-42), c=as.integer64(NA))
+    df <- data.frame(a = bit64::as.integer64(42), b = bit64::as.integer64(-42), c = bit64::as.integer64(NA))
 
     duckdb::duckdb_register(con, "df", df)
 
