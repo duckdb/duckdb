63172fc0207b422cb95a87804c3297707078bd15
Elliana May <me@mause.me>
2022-08-02 22:35:59 +0800

    feat: add project links to setup.py metadata

--- tools/pythonpkg/setup.py
+++ tools/pythonpkg/setup.py
@@ -298,4 +298,10 @@ setup(
     maintainer = "Hannes Muehleisen",
     maintainer_email = "hannes@cwi.nl",
     cmdclass={"build_ext": build_ext},
+    project_urls={
+        "Documentation": "https://duckdb.org/docs/api/python/overview",
+        "Source": "https://github.com/duckdb/duckdb/blob/master/tools/pythonpkg",
+        "Issues": "https://github.com/duckdb/duckdb/issues",
+        "Changelog": "https://github.com/duckdb/duckdb/releases",
+    },
 )
