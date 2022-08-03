91d30ef2d7181b7929d6d404e790df2f9537dba4
Eero <eero.lihavainen@gmail.com>
2022-08-03 12:56:45 +0300

    fix test

--- tools/pythonpkg/tests/fast/arrow/test_arrow_fetch_recordbatch.py
+++ tools/pythonpkg/tests/fast/arrow/test_arrow_fetch_recordbatch.py
@@ -113,5 +113,5 @@ class TestArrowFetchRecordBatch(object):
         duckdb_cursor.execute("CREATE table t as select 'foo' as a;")
         query = duckdb_cursor.execute("SELECT cast(a as double) FROM t")
         record_batch_reader = query.fetch_record_batch(1024)
-        with pytest.raises(RuntimeError, match='Conversion Error'):
+        with pytest.raises(OSError, match='Conversion Error'):
             record_batch_reader.read_next_batch()
