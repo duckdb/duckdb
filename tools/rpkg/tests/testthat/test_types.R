2bca667c4cdfd85a4d1e285f42e85265f302860f
Kirill Müller <krlmlr+r@mailbox.org>
2022-08-06 17:54:02 +0200

    Move

--- tools/rpkg/tests/testthat/test_types.R
+++ tools/rpkg/tests/testthat/test_types.R
@@ -1,7 +1,8 @@
 test_that("test_all_types() output", {
-  local_edition(3)
   con <- dbConnect(duckdb::duckdb())
   on.exit(dbDisconnect(con, shutdown = TRUE))
+
+  local_edition(3)
 
   expect_snapshot({
     unclass(dbGetQuery(con, "SELECT * EXCLUDE (timestamp_tz, time_tz, uuid, interval, json, map) FROM test_all_types()"))
