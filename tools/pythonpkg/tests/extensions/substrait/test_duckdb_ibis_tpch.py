f8ff6085bb897e40dca15730c0b09c5829f84ea7
Pedro Holanda <pedroholanda@gmail.com>
2022-08-09 20:27:45 +0200

    importorskip and updating errors

--- tools/pythonpkg/tests/extensions/substrait/test_duckdb_ibis_tpch.py
+++ tools/pythonpkg/tests/extensions/substrait/test_duckdb_ibis_tpch.py
@@ -1,95 +1,90 @@
 import duckdb
 import pytest
 
-try:
-    from ibis_substrait.compiler.decompile import decompile
-    from ibis_substrait.proto.substrait import plan_pb2
-    can_run = True
 
-    def duck_to_ibis(duck_con, query_number):
-        # Gets TPC-H Query for DuckDB
-        query = duck_con.execute("select query from tpch_queries() where query_nr="+str(query_number)).fetchone()[0]
-        try:
-            # Compiles TPC-H in DuckDB to substrait proto
-            proto = duck_con.get_substrait(query).fetchone()[0]
-        except Exception as err:
-            raise ValueError("DuckDB Compilation: " + str(err))
-        try:
-            # Executed Ibis' substrait ```proto``` in Ibis
-            plan = plan_pb2.Plan()
-            plan.ParseFromString(proto)
-            (result,) = decompile(plan)
-        except Exception as err:
-            raise ValueError("Ibis Consumption: " + str(err))
+decompile = pytest.importorskip('ibis_substrait.compiler.decompile')
+plan_pb2 = pytest.importorskip('ibis_substrait.proto.substrait')
 
-except:
-    can_run = False
+
+def duck_to_ibis(duck_con, query_number):
+    # Gets TPC-H Query for DuckDB
+    query = duck_con.execute("select query from tpch_queries() where query_nr="+str(query_number)).fetchone()[0]
+    try:
+        # Compiles TPC-H in DuckDB to substrait proto
+        proto = duck_con.get_substrait(query).fetchone()[0]
+    except Exception as err:
+        raise ValueError("DuckDB Compilation: " + str(err))
+    try:
+        # Executed Ibis' substrait ```proto``` in Ibis
+        plan = plan_pb2.plan_pb2.Plan()
+        plan.ParseFromString(proto)
+        (result,) = decompile.decompile(plan)
+    except Exception as err:
+        raise ValueError("Ibis Consumption: " + str(err))
 
 def execute_substrait_ibis_to_duck(require, query_number):
-    if not can_run:
-        return
     connection = require('substrait', 'test.db')
     if not connection:
         return
     duck_to_ibis(connection,query_number)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_01(require):
     execute_substrait_ibis_to_duck(require,1)
     
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_02(require):
     execute_substrait_ibis_to_duck(require,2)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_03(require):
     execute_substrait_ibis_to_duck(require,3)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_04(require):
     execute_substrait_ibis_to_duck(require,4)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_05(require):
     execute_substrait_ibis_to_duck(require,5)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_06(require):
     execute_substrait_ibis_to_duck(require,6)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_07(require):
     execute_substrait_ibis_to_duck(require,7)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_08(require):
     execute_substrait_ibis_to_duck(require,8)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_09(require):
     execute_substrait_ibis_to_duck(require,9)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_10(require):
     execute_substrait_ibis_to_duck(require,10)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_11(require):
     execute_substrait_ibis_to_duck(require,11)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_12(require):
     execute_substrait_ibis_to_duck(require,12)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_13(require):
     execute_substrait_ibis_to_duck(require,13)
     
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_14(require):
     execute_substrait_ibis_to_duck(require,14)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_15(require):
     execute_substrait_ibis_to_duck(require,15)
 
@@ -97,19 +92,19 @@ def test_query_substrait_ibis_to_duck_15(require):
 def test_query_substrait_ibis_to_duck_16(require):
     execute_substrait_ibis_to_duck(require,16)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_17(require):
     execute_substrait_ibis_to_duck(require,17)
     
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_18(require):
     execute_substrait_ibis_to_duck(require,18)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="getattr(): attribute name must be string")
 def test_query_substrait_ibis_to_duck_19(require):
     execute_substrait_ibis_to_duck(require,19)
 
-@pytest.mark.xfail(reason="Ibis Consumption: unknown field type when decompiling: <class 'substrait.type_pb2.VarChar'>")
+@pytest.mark.xfail(reason="pop from an empty deque")
 def test_query_substrait_ibis_to_duck_20(require):
     execute_substrait_ibis_to_duck(require,20)
 
--- tools/pythonpkg/tests/extensions/substrait/test_ibis_duckdb_tpch.py
+++ tools/pythonpkg/tests/extensions/substrait/test_ibis_duckdb_tpch.py
@@ -1,83 +1,77 @@
 import duckdb
 import pytest
 
-try:
-    from ibis_substrait.compiler.core import SubstraitCompiler
-    import ibis
-    from ibis.backends.base import BaseBackend
-    from ibis.backends.duckdb.datatypes import parse_type
-    from ibis_tpch_util import get_tpch_query
-
-    can_run = True
-
-    def unbound_from_duckdb(table):  # noqa: D103
-        return ibis.table(
-            list(zip(table.columns, map(parse_type, table.dtypes))), name=table.alias
-        )
-
-    class TPCHBackend(BaseBackend):  # noqa: D101
-        def __init__(self,duck_con,  scale_factor=0.1):  # noqa: D107
-            self.con = duck_con
-            try:
-                self.con.table('lineitem')
-            except:
-                self.con.execute(f"CALL dbgen(sf={scale_factor})")
-
-            _tables = self.con.execute("PRAGMA show_tables").fetchall()
-            _tables = map(lambda x: x[0], _tables)
-
-            self.tables = {
-                table.alias: unbound_from_duckdb(table)
-                for table in map(
-                    self.con.table,
-                    _tables,
-                )
-            }
-
-        def table(self, table):  # noqa: D102
-            return self.tables.get(table)
-
-        def current_database(self):  # noqa: D102
-            ...
-
-        def list_databases(self):  # noqa: D102
-            ...
-
-        def list_tables(self):  # noqa: D102
-            ...
-
-        def version(self):  # noqa: D102
-            return "awesome"
+SubstraitCompiler = pytest.importorskip('ibis_substrait.compiler.core')
+ibis = pytest.importorskip('ibis')
+BaseBackend = pytest.importorskip('ibis.backends.base')
+parse_type = pytest.importorskip('ibis.backends.duckdb.datatypes')
+get_tpch_query = pytest.importorskip('ibis_tpch_util')
 
 
-    def ibis_to_duck(duck_con, query_number):
-        # Gets TPC-H Query for IBIS
-        tpch_query = get_tpch_query(query_number)(duck_con)
-        # Ibis Substrait Compiler (i.e., Producer)
-        compiler = SubstraitCompiler()
+def unbound_from_duckdb(table):  # noqa: D103
+    return ibis.table(
+        list(zip(table.columns, map(parse_type.parse_type, table.dtypes))), name=table.alias
+    )
+
+class TPCHBackend(BaseBackend.BaseBackend):  # noqa: D101
+    def __init__(self,duck_con,  scale_factor=0.1):  # noqa: D107
+        self.con = duck_con
         try:
-            # Compiles TPC-H in Ibis to substrait proto
-            proto = compiler.compile(tpch_query)
-        except Exception as err:
-            raise ValueError("Ibis Compilation: " + str(err))
-        try:
-            # Executed Ibis' substrait ```proto``` in DuckDB
-            result = duck_con.con.from_substrait(proto.SerializeToString())
-        except Exception as err:
-            raise ValueError("DuckDB Consumption: " + str(err))
-        
-        # Result Checking
-        result = result.df().sort_index(ascending=False, axis=1)
-        query = duck_con.con.execute("select query from tpch_queries() where query_nr="+str(query_number)).fetchone()[0]
-        answer = duck_con.con.execute(query).df().sort_index(ascending=False, axis=1)
-        assert result.equals(answer)
+            self.con.table('lineitem')
+        except:
+            self.con.execute(f"CALL dbgen(sf={scale_factor})")
 
-except:
-    can_run = False
+        _tables = self.con.execute("PRAGMA show_tables").fetchall()
+        _tables = map(lambda x: x[0], _tables)
+
+        self.tables = {
+            table.alias: unbound_from_duckdb(table)
+            for table in map(
+                self.con.table,
+                _tables,
+            )
+        }
+
+    def table(self, table):  # noqa: D102
+        return self.tables.get(table)
+
+    def current_database(self):  # noqa: D102
+        ...
+
+    def list_databases(self):  # noqa: D102
+        ...
+
+    def list_tables(self):  # noqa: D102
+        ...
+
+    def version(self):  # noqa: D102
+        return "awesome"
+
+
+def ibis_to_duck(duck_con, query_number):
+    # Gets TPC-H Query for IBIS
+    tpch_query = get_tpch_query.get_tpch_query(query_number)(duck_con)
+    # Ibis Substrait Compiler (i.e., Producer)
+    compiler = SubstraitCompiler.SubstraitCompiler()
+    try:
+        # Compiles TPC-H in Ibis to substrait proto
+        proto = compiler.compile(tpch_query)
+    except Exception as err:
+        raise ValueError("Ibis Compilation: " + str(err))
+    try:
+        # Executed Ibis' substrait ```proto``` in DuckDB
+        result = duck_con.con.from_substrait(proto.SerializeToString())
+    except Exception as err:
+        raise ValueError("DuckDB Consumption: " + str(err))
+    
+    # Result Checking
+    result = result.df().sort_index(ascending=False, axis=1)
+    query = duck_con.con.execute("select query from tpch_queries() where query_nr="+str(query_number)).fetchone()[0]
+    answer = duck_con.con.execute(query).df().sort_index(ascending=False, axis=1)
+    assert result.equals(answer)
+
 
 def run_query(require, query_number):
-    if not can_run:
-        return
     connection = require('substrait', 'test.db')
     if not connection:
         return
