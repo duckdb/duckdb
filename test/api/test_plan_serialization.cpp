b48ce607b0433551d295785dcd18c4c04975e25a
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-08-16 21:11:05 +0200

    Unit tests for SELECT queries are not required anymore

--- test/api/test_plan_serialization.cpp
+++ test/api/test_plan_serialization.cpp
@@ -63,71 +63,12 @@ static void test_helper(string sql, vector<string> fixtures = vector<string>()) 
 	}
 }
 
-TEST_CASE("Test plan serialization", "[serialization]") {
-	test_helper(
-	    "SELECT last_name,COUNT(*) FROM parquet_scan('data/parquet-testing/userdata1.parquet') GROUP BY last_name");
-}
-
-TEST_CASE("Test logical_dummy_scan", "[serialization]") {
-	test_helper("SELECT [1, 2, 3]");
-}
-
-TEST_CASE("Test logical_unnest", "[serialization]") {
-	test_helper("SELECT UNNEST([1, 2, 3])");
-}
-
-TEST_CASE("Test bound_constant_expression", "[serialization]") {
-	test_helper("SELECT 42 as i");
-}
-
-TEST_CASE("Test logical_window", "[serialization]") {
-	test_helper("SELECT * FROM (SELECT 42 as i) WINDOW w AS (partition by i)");
-}
-
 TEST_CASE("Test logical_set", "[serialization]") {
 	test_helper("SET memory_limit='10GB'");
 }
 
-TEST_CASE("Test logical_sample", "[serialization]") {
-	test_helper("SELECT * FROM (SELECT 42 as i) USING SAMPLE RESERVOIR(20%)");
-}
-
-TEST_CASE("Test logical_limit_percent", "[serialization]") {
-	test_helper("SELECT 42 LIMIT 35%");
-}
-
-TEST_CASE("Test logical_limit", "[serialization]") {
-	test_helper("SELECT 42 LIMIT 1");
-}
-
-TEST_CASE("Test logical_comparison_join", "[serialization]") {
-	test_helper("SELECT * FROM (SELECT 42 as i), (SELECT 42 as j) WHERE i = j");
-}
-
-TEST_CASE("Test logical_any_join", "[serialization]") {
-	test_helper("SELECT * FROM (SELECT 42 as i), (SELECT 42 as j) WHERE i = j OR i > 1");
-}
-
 TEST_CASE("Test logical_show", "[serialization]") {
 	test_helper("SHOW SELECT 42");
-}
-
-TEST_CASE("Test bound_comparison_expression", "[serialization]") {
-	test_helper("SELECT COUNT(*) FILTER (WHERE i = 1) FROM (SELECT 42 as i)");
-}
-
-TEST_CASE("Test logical_filter", "[serialization]") {
-	test_helper("SELECT COUNT(*) FROM (SELECT 42 as i) HAVING COUNT(*) >= 1");
-}
-
-TEST_CASE("Test logical_expression_get", "[serialization]") {
-	test_helper("SELECT * FROM (VALUES\n"
-	            "  ([1]),\n"
-	            "  ([NULL]),\n"
-	            "  ([]),\n"
-	            "  ([9,10,11]),\n"
-	            "  (NULL)\n"
-	            "  ) lv(pk);");
 }
 
 TEST_CASE("Test logical_explain", "[serialization]") {
@@ -146,25 +87,8 @@ TEST_CASE("Test insert_into", "[serialization]") {
 	test_helper("INSERT INTO tbl VALUES(1)", {"CREATE TABLE tbl (foo INTEGER)"});
 }
 
-TEST_CASE("Test logical_distinct", "[serialization]") {
-	test_helper("SELECT DISTINCT(i) FROM (SELECT 42 as i)");
-}
-
 TEST_CASE("Test logical_delete", "[serialization]") {
 	test_helper("DELETE FROM tbl", {"CREATE TABLE tbl (foo INTEGER)"});
-}
-
-TEST_CASE("Test logical_cteref", "[serialization]") {
-	test_helper("with cte1 as (select 42), cte2 as (select * from cte1) select * FROM cte2");
-}
-
-// TODO(stephwang): check why we lost x<3 expression in filter
-TEST_CASE("Test logical_recursive_cte", "[serialization]") {
-	test_helper("with recursive t as (select 1 as x union all select x+1 from t where x < 3) select * from t");
-}
-
-TEST_CASE("Test logical_cross_product", "[serialization]") {
-	test_helper("SELECT * FROM (SELECT 42 as i) CROSS JOIN (SELECT 42 as j)");
 }
 
 // TODO: only select for now
@@ -179,10 +103,6 @@ TEST_CASE("Test logical_cross_product", "[serialization]") {
 // TEST_CASE("Test logical_create_view", "[serialization]") {
 //	test_helper("CREATE VIEW test_view AS (SELECT 42)");
 //}
-
-TEST_CASE("Test logical_top_n", "[serialization]") {
-	test_helper("SELECT * FROM tbl ORDER BY foo LIMIT 1", {"CREATE TABLE tbl (foo INTEGER)"});
-}
 
 TEST_CASE("Test logical_update", "[serialization]") {
 	test_helper("UPDATE tbl SET foo=42", {"CREATE TABLE tbl (foo INTEGER)"});
