e3d8e6d02e41473f84567962ee6f339c33a3f4ce
Boaz Leskes <b.leskes@gmail.com>
2022-08-18 09:07:36 +0200

    Update src/include/duckdb/planner/logical_operator.hpp
    fix typo
    
    Co-authored-by: Elliana May <me@mause.me>

--- src/include/duckdb/planner/logical_operator.hpp
+++ src/include/duckdb/planner/logical_operator.hpp
@@ -24,7 +24,7 @@ namespace duckdb {
 class FieldWriter;
 class FieldReader;
 
-//! The current version of the plan serialization format. Exposed via by @Serializer & @Desializer
+//! The current version of the plan serialization format. Exposed via by @Serializer & @Deserializer
 //! to be used by various Operator to know what format to read and write.
 extern const uint64_t PLAN_SERIALIZATION_VERSION;
 
