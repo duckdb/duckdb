3448d10ba6262990fbd182f90994aa12c15c66af
Hannes Mühleisen <hannes@muehleisen.org>
2022-08-27 08:04:16 +0200

    more cross compile fun

--- scripts/install_unixodbc.sh
+++ scripts/install_unixodbc.sh
@@ -6,6 +6,5 @@ cd build/unixodbc
 mkdir sources build
 curl http://www.unixodbc.org/unixODBC-2.3.11.tar.gz | tar xvz -C sources --strip-components 1
 cd sources
-export CFLAGS="-arch x86_64 -arch arm64"
 ./configure --prefix `cd ../build; pwd` --disable-debug --disable-dependency-tracking --enable-static --enable-gui=no $@
 make -j install
