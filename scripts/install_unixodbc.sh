efae59b472f9e0e2d41d929f2005c06bdacb74a5
Hannes Mühleisen <hannes@muehleisen.org>
2022-08-27 07:33:21 +0200

    need to pass host flag it seems

--- scripts/install_unixodbc.sh
+++ scripts/install_unixodbc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 rm -rf build/unixodbc
 mkdir -p build/unixodbc
 cd build/unixodbc
@@ -6,5 +7,5 @@ mkdir sources build
 curl http://www.unixodbc.org/unixODBC-2.3.11.tar.gz | tar xvz -C sources --strip-components 1
 cd sources
 export CFLAGS="-arch x86_64 -arch arm64"
-./configure --prefix `cd ../build; pwd` --disable-debug --disable-dependency-tracking --enable-static --enable-gui=no
+./configure --prefix `cd ../build; pwd` --disable-debug --disable-dependency-tracking --enable-static --enable-gui=no $@
 make -j install
