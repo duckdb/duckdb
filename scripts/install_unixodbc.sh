d154c531bd69dbb023744ddff31207b12842c7b4
Hannes Mühleisen <hannes@muehleisen.org>
2022-08-27 07:25:25 +0200

    adding z option

--- scripts/install_unixodbc.sh
+++ scripts/install_unixodbc.sh
@@ -3,7 +3,7 @@ rm -rf build/unixodbc
 mkdir -p build/unixodbc
 cd build/unixodbc
 mkdir sources build
-curl http://www.unixodbc.org/unixODBC-2.3.11.tar.gz | tar xv -C sources --strip-components 1
+curl http://www.unixodbc.org/unixODBC-2.3.11.tar.gz | tar xvz -C sources --strip-components 1
 cd sources
 export CFLAGS="-arch x86_64 -arch arm64"
 ./configure --prefix `cd ../build; pwd` --disable-debug --disable-dependency-tracking --enable-static --enable-gui=no
