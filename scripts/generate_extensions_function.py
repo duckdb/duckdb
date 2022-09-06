5d3c4face485e1e0bd52d78317497fd218e51058
Pedro Holanda <pedroholanda@gmail.com>
2022-09-06 13:32:38 +0200

    Unify extensions file, add CI to validate that extension_functions.hpp is up to date

--- scripts/generate_extensions_function.py
+++ scripts/generate_extensions_function.py
@@ -1,10 +1,20 @@
 import os
 import csv
+import re
+import argparse
 
 os.chdir(os.path.dirname(__file__))
 
+parser = argparse.ArgumentParser(description='Generates/Validates extension_functions.hpp file')
+
+parser.add_argument('--validate', action=argparse.BooleanOptionalAction,
+                    help='If set  will validate that extension_functions.hpp is up to date, otherwise it generates the extension_functions.hpp file.')
+
+
+args = parser.parse_args()
+
 functions = {}
-reader = csv.reader(open(os.path.join("..",'extension.csv')))
+reader = csv.reader(open(os.path.join("..",'extensions.csv')))
 # This skips the first row (i.e., the header) of the CSV file.
 next(reader)
 
@@ -12,7 +22,7 @@ get_func = "select distinct on(function_name) function_name from duckdb_function
 duckdb_path = os.path.join("..",'build', 'release', 'duckdb')
 base_functions = os.popen(f'{duckdb_path} -csv -c "{get_func}" ').read().split("\n")[1:-1]
 
-base_functions = {x for x in base_functions} 
+base_functions = {x for x in base_functions}
 
 function_map = {}
 for row in reader:
@@ -21,33 +31,43 @@ for row in reader:
     install = f"INSTALL {extension_name};"
     load = f"LOAD {extension_name};"
     extension_functions = os.popen(f'{duckdb_path} -csv -c "{install}{load}{get_func}" ').read().split("\n")[1:-1]
-    extension_functions = {x for x in extension_functions} 
+    extension_functions = {x for x in extension_functions}
     extension_functions = extension_functions.difference(base_functions)
     for extension_function in extension_functions:
         function_map[extension_function] = extension_name
 
-# Generate Header
-file = open(os.path.join("..","src","include","extension_functions.hpp"),'w')
-file.write("//===----------------------------------------------------------------------===//\n") 
-file.write("//                         DuckDB\n") 
-file.write("//\n") 
-file.write("// extension_functions.hpp\n") 
-file.write("//\n") 
-file.write("//\n") 
-file.write("//===----------------------------------------------------------------------===//\n") 
-file.write("\n") 
-file.write("#pragma once\n") 
-file.write("\n") 
-file.write("#include \"duckdb/common/unordered_map.hpp\"") 
-file.write("\n\n") 
-file.write("namespace duckdb { \n\n") 
+if args.validate:
+    cur_function_map = {}
+    file = open(os.path.join("..","src","include","extension_functions.hpp"),'r')
+    pattern = re.compile("{\"(.*?)\", \"(.*?)\"},")
+    for line in file:
+        if pattern.match(line):
+            split_line = line.split("\"")
+            cur_function_map[split_line[1]] = split_line[3]
+    assert cur_function_map == function_map
+else:
+    # Generate Header
+    file = open(os.path.join("..","src","include","extension_functions.hpp"),'w')
+    file.write("//===----------------------------------------------------------------------===//\n")
+    file.write("//                         DuckDB\n")
+    file.write("//\n")
+    file.write("// extension_functions.hpp\n")
+    file.write("//\n")
+    file.write("//\n")
+    file.write("//===----------------------------------------------------------------------===//\n")
+    file.write("\n")
+    file.write("#pragma once\n")
+    file.write("\n")
+    file.write("#include \"duckdb/common/unordered_map.hpp\"")
+    file.write("\n\n")
+    file.write("namespace duckdb { \n\n")
 
-file.write("static const unordered_map <std::string,std::string> extension_functions = {") 
-for function_name in function_map:
-    file.write("{")
-    file.write(f"\"{function_name}\", \"{function_map[function_name]}\"") 
-    file.write("}, \n")
-file.write("}; \n")
-file.write("}\n")     
+    file.write("static const unordered_map <std::string,std::string> extension_functions = {\n")
+    for function_name in function_map:
+        file.write("{")
+        file.write(f"\"{function_name}\", \"{function_map[function_name]}\"")
+        file.write("}, \n")
+    file.write("}; \n")
+    file.write("}\n")
 
-file.close()
+    file.close()
