2e934b6aced517ef2697e2c030fcc6f259d905c8
taofengliu <taofengliu@outlook.com>
2022-09-14 20:46:50 +0800

    fix #4703

--- src/catalog/catalog_entry/table_catalog_entry.cpp
+++ src/catalog/catalog_entry/table_catalog_entry.cpp
@@ -441,7 +441,16 @@ unique_ptr<CatalogEntry> TableCatalogEntry::RemoveColumn(ClientContext &context,
 		return make_unique<TableCatalogEntry>(catalog, schema, (BoundCreateTableInfo *)bound_create_info.get(),
 		                                      storage);
 	}
-	auto new_storage = make_shared<DataTable>(context, *storage, removed_index);
+
+	// We need to skip generated columns because they are not in 'storage'
+	column_t storage_index = removed_index;
+	for (column_t i = 0; i < removed_index; i++) {
+		if (columns[i].Generated()) {
+			storage_index--;
+		}
+	}
+
+	auto new_storage = make_shared<DataTable>(context, *storage, storage_index);
 	return make_unique<TableCatalogEntry>(catalog, schema, (BoundCreateTableInfo *)bound_create_info.get(),
 	                                      new_storage);
 }
