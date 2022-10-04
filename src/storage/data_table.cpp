c3a98fc328b0dbcb7d74e26e6ff0070c17f64d13
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-10-04 11:14:35 +0200

    entry_offset in list

--- src/storage/data_table.cpp
+++ src/storage/data_table.cpp
@@ -528,7 +528,8 @@ void DataTable::ScanTableSegment(idx_t row_start, idx_t count, const std::functi
 	CreateIndexScanState state;
 
 	InitializeScanWithOffset(state, column_ids, row_start, row_start + count);
-	auto row_start_aligned = state.table_state.row_group_state.row_group->start + state.table_state.row_group_state.vector_index * STANDARD_VECTOR_SIZE;
+	auto row_start_aligned = state.table_state.row_group_state.row_group->start +
+	                         state.table_state.row_group_state.vector_index * STANDARD_VECTOR_SIZE;
 
 	idx_t current_row = row_start_aligned;
 	while (current_row < end) {
