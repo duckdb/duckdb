f1543cd2e4ab4da7b97ae75f24ba702dc2427c73
Tishj <t_b@live.nl>
2022-10-05 10:26:36 +0200

    actually committing the new files might help

---
+++ src/include/duckdb/storage/compression/chimp/byte_reader.hpp
@@ -0,0 +1,170 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/storage/compression/chimp/byte_reader.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <stdint.h>
+#include <iostream>
+#include "duckdb/common/fast_mem.hpp"
+
+namespace duckdb_chimp {
+
+// This class reads arbitrary amounts of bits from a buffer
+// If 41 bits are requested (5 bytes + 1 bit), we will read 6 bytes and increment the byte index by 6
+// With the assumption that the remainder of the last byte read is zero-initialized
+class ByteReader {
+private:
+	static constexpr uint8_t final_shifts[8] = {0, 7, 6, 5, 4, 3, 2, 1};
+
+public:
+	ByteReader() : buffer(nullptr), index(0) {
+	}
+
+public:
+	void SetStream(uint8_t *buffer) {
+		this->buffer = buffer;
+		index = 0;
+	}
+
+	template <class T>
+	T ReadValue() {
+		throw std::runtime_error("fixme read value");
+		//		uint64_t bytes = 0;
+		//		const uint8_t bytes_to_read = (SIZE >> 3) + ((SIZE & 7) != 0);
+		//		duckdb::FastMemcpy(&bytes, (void*)(buffer + index), bytes_to_read);
+		//		T result = (T)bytes;
+		//		index += bytes_to_read;
+		//		return result;
+	}
+
+	template <>
+	uint8_t ReadValue<uint8_t>() {
+		auto result = duckdb::Load<uint8_t>(buffer + index);
+		index++;
+		return result;
+	}
+	template <>
+	uint16_t ReadValue<uint16_t>() {
+		auto result = duckdb::Load<uint16_t>(buffer + index);
+		index += 2;
+		return result;
+	}
+	template <>
+	uint32_t ReadValue<uint32_t>() {
+		auto result = duckdb::Load<uint32_t>(buffer + index);
+		index += 4;
+		return result;
+	}
+	template <>
+	uint64_t ReadValue<uint64_t>() {
+		auto result = duckdb::Load<uint64_t>(buffer + index);
+		index += 8;
+		return result;
+	}
+
+	template <class T, uint8_t SIZE>
+	T ReadValue() {
+		return ReadValue<T>(SIZE);
+	}
+
+	template <class T>
+	T ReadValue(const uint8_t &size) {
+		T result = 0;
+		switch (size) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			result = duckdb::Load<uint8_t>(buffer + index);
+			index++;
+			return result;
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+			result = duckdb::Load<uint16_t>(buffer + index);
+			index += 2;
+			return result;
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+			memcpy(&result, (void *)(buffer + index), 3);
+			index += 3;
+			return result;
+		case 25:
+		case 26:
+		case 27:
+		case 28:
+		case 29:
+		case 30:
+		case 31:
+		case 32:
+			result = duckdb::Load<uint32_t>(buffer + index);
+			index += 4;
+			return result;
+		case 33:
+		case 34:
+		case 35:
+		case 36:
+		case 37:
+		case 38:
+		case 39:
+		case 40:
+			memcpy(&result, (void *)(buffer + index), 5);
+			index += 5;
+			return result;
+		case 41:
+		case 42:
+		case 43:
+		case 44:
+		case 45:
+		case 46:
+		case 47:
+		case 48:
+			memcpy(&result, (void *)(buffer + index), 6);
+			index += 6;
+			return result;
+		case 49:
+		case 50:
+		case 51:
+		case 52:
+		case 53:
+		case 54:
+		case 55:
+		case 56:
+			memcpy(&result, (void *)(buffer + index), 7);
+			index += 7;
+			return result;
+		default:
+			result = duckdb::Load<uint64_t>(buffer + index);
+			//			memcpy(&result, (void *)(buffer + index), 8);
+			index += 8;
+			return result;
+		}
+	}
+
+private:
+private:
+	uint8_t *buffer;
+	uint32_t index;
+};
+
+} // namespace duckdb_chimp
---
+++ src/include/duckdb/storage/compression/chimp/leading_zero_buffer.hpp
@@ -0,0 +1,159 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/storage/compression/chimp/leading_zero_buffer.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <stdint.h>
+#include <cstring>
+#ifdef DEBUG
+#include <vector>
+#endif
+
+namespace duckdb_chimp {
+
+#define BLOCK_IDX ((counter >> 3) * (LEADING_ZERO_BLOCK_BIT_SIZE / 8))
+
+//! This class is in charge of storing the leading_zero_bits, which are of a fixed size
+//! These are packed together so that the rest of the data can be byte-aligned
+//! The leading zero bit data is read from left to right
+template <bool EMPTY>
+class LeadingZeroBuffer {
+public:
+	static constexpr uint32_t CHIMP_GROUP_SIZE = 1024;
+	static constexpr uint32_t LEADING_ZERO_BITS_SIZE = 3;
+	static constexpr uint32_t LEADING_ZERO_BLOCK_SIZE = 8;
+	static constexpr uint32_t LEADING_ZERO_BLOCK_BIT_SIZE = LEADING_ZERO_BLOCK_SIZE * LEADING_ZERO_BITS_SIZE;
+	static constexpr uint32_t MAX_LEADING_ZERO_BLOCKS = CHIMP_GROUP_SIZE / LEADING_ZERO_BLOCK_SIZE;
+	static constexpr uint32_t MAX_BITS_USED_BY_ZERO_BLOCKS = MAX_LEADING_ZERO_BLOCKS * LEADING_ZERO_BLOCK_BIT_SIZE;
+	static constexpr uint32_t MAX_BYTES_USED_BY_ZERO_BLOCKS = MAX_BITS_USED_BY_ZERO_BLOCKS / 8;
+
+	// Add an extra byte to prevent heap buffer overflow on the last group, because we'll be addressing 4 bytes each
+	static constexpr uint32_t BUFFER_SIZE =
+	    MAX_BYTES_USED_BY_ZERO_BLOCKS + (sizeof(uint32_t) - (LEADING_ZERO_BLOCK_BIT_SIZE / 8));
+
+	static constexpr uint32_t leading_zero_masks[8] = {
+	    7,        // 0b 00000000 00000000 00000000 00000111,
+	    56,       // 0b 00000000 00000000 00000000 00111000,
+	    448,      // 0b 00000000 00000000 00000001 11000000,
+	    3584,     // 0b 00000000 00000000 00001110 00000000,
+	    28672,    // 0b 00000000 00000000 01110000 00000000,
+	    229376,   // 0b 00000000 00000011 10000000 00000000,
+	    1835008,  // 0b 00000000 00011100 00000000 00000000,
+	    14680064, // 0b 00000000 11100000 00000000 00000000,
+	};
+
+	// We're not using the last byte (the most significant) of the 4 bytes we're accessing
+	static constexpr uint8_t leading_zero_shifts[8] = {0, 3, 6, 9, 12, 15, 18, 21};
+
+	template <typename T>
+	const T Load(const uint8_t *ptr) {
+		T ret;
+		memcpy(&ret, ptr, sizeof(ret));
+		return ret;
+	}
+
+public:
+	LeadingZeroBuffer() : current(0), counter(0), buffer(nullptr) {
+	}
+	void SetBuffer(uint8_t *buffer) {
+		// Set the internal buffer, when inserting this should be BUFFER_SIZE bytes in length
+		// This buffer does not need to be zero-initialized for inserting
+		this->buffer = buffer;
+		this->counter = 0;
+	}
+	void Flush() {
+		if ((counter & 7) != 0) {
+			FlushBuffer();
+		}
+	}
+
+	uint64_t BitsWritten() const {
+		return counter * 3;
+	}
+
+	// Reset the counter, but don't replace the buffer
+	void Reset() {
+		this->counter = 0;
+		current = 0;
+// if (!EMPTY && buffer) {
+//	buffer[0] = 0;
+//	buffer[1] = 0;
+//	buffer[2] = 0;
+// }
+#ifdef DEBUG
+		flags.clear();
+#endif
+	}
+
+public:
+#ifdef DEBUG
+	uint8_t ExtractValue(uint32_t value, uint8_t index) {
+		return (value & leading_zero_masks[index]) >> leading_zero_shifts[index];
+	}
+#endif
+
+	void FlushBuffer() {
+		if (EMPTY) {
+			return;
+		}
+		const auto buffer_idx = BLOCK_IDX;
+		std::memcpy((void *)(buffer + buffer_idx), (uint8_t *)&current, 3);
+#ifdef DEBUG
+		// Verify that the bits are copied correctly
+
+		uint32_t temp_value = 0;
+		std::memcpy((uint8_t *)&temp_value, (void *)(buffer + buffer_idx), 3);
+		for (size_t i = 0; i < flags.size(); i++) {
+			assert(flags[i] == ExtractValue(temp_value, i));
+		}
+		flags.clear();
+#endif
+	}
+
+	void Insert(const uint8_t &value) {
+		if (!EMPTY) {
+#ifdef DEBUG
+			flags.push_back(value);
+#endif
+			current |= (value & 7) << leading_zero_shifts[counter & 7];
+#ifdef DEBUG
+			// Verify that the bits are serialized correctly
+			assert(flags[counter & 7] == ExtractValue(current, counter & 7));
+#endif
+
+			if ((counter & (LEADING_ZERO_BLOCK_SIZE - 1)) == 7) {
+				FlushBuffer();
+				current = 0;
+			}
+		}
+		counter++;
+	}
+
+	__attribute__((no_sanitize("alignment"))) inline uint8_t Extract() {
+		const auto buffer_idx = BLOCK_IDX;
+		auto const temp = Load<uint32_t>(buffer + buffer_idx);
+
+		const uint8_t result = (temp & leading_zero_masks[counter & 7]) >> leading_zero_shifts[counter & 7];
+		counter++;
+		return result;
+	}
+	size_t BlockCount() const {
+		return (counter >> 3) + ((counter & 7) != 0);
+	}
+
+private:
+private:
+	uint32_t current;
+	uint32_t counter = 0; // block_index * 8
+	uint8_t *buffer;
+#ifdef DEBUG
+	std::vector<uint8_t> flags;
+#endif
+};
+
+} // namespace duckdb_chimp
