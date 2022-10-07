1f9cdac353083249951c39bc955e79036c9baebf
Laurens Kuiper <laurens.kuiper@cwi.nl>
2022-10-07 17:18:20 +0200

    more CI debugging

--- extension/jemalloc/jemalloc/include/jemalloc/internal/jemalloc_internal_defs.h
+++ extension/jemalloc/jemalloc/include/jemalloc/internal/jemalloc_internal_defs.h
@@ -326,7 +326,7 @@ namespace duckdb_jemalloc {
 /* #undef JEMALLOC_PURGE_MADVISE_DONTNEED_ZEROS */
 
 /* Defined if madvise(2) is available but MADV_FREE is not (x86 Linux only). */
-/* #undef JEMALLOC_DEFINE_MADVISE_FREE */
+#undef JEMALLOC_DEFINE_MADVISE_FREE
 
 /*
  * Defined if MADV_DO[NT]DUMP is supported as an argument to madvise.
