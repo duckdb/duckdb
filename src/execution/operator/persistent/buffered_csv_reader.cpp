5b92307a0cf46a800e0b13390539b908d14cdee0
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-10-10 11:20:30 +0200

    We should only do this in the multi-column case

--- src/execution/operator/persistent/buffered_csv_reader.cpp
+++ src/execution/operator/persistent/buffered_csv_reader.cpp
@@ -1935,11 +1935,11 @@ bool BufferedCSVReader::AddRow(DataChunk &insert_chunk, idx_t &column) {
 	linenr++;
 
 	if (row_empty) {
-		if (mode == ParserMode::PARSING) {
-			FlatVector::SetNull(parse_chunk.data[0], parse_chunk.size(), false);
-		}
 		row_empty = false;
 		if (sql_types.size() != 1) {
+			if (mode == ParserMode::PARSING) {
+				FlatVector::SetNull(parse_chunk.data[0], parse_chunk.size(), false);
+			}
 			column = 0;
 			return false;
 		}
