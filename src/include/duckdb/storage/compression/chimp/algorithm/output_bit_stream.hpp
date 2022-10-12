d001655fbcb6d64149c08bf0b48a0053eeb2bd3c
Tishj <t_b@live.nl>
2022-10-12 10:42:37 +0200

    added missing curly braces

--- src/include/duckdb/storage/compression/chimp/algorithm/output_bit_stream.hpp
+++ src/include/duckdb/storage/compression/chimp/algorithm/output_bit_stream.hpp
@@ -68,23 +68,31 @@ public:
 	template <class T>
 	void WriteRemainder(T value, uint8_t i) {
 		if (sizeof(T) * 8 > 32) {
-			if (i == 64)
+			if (i == 64) {
 				WriteToStream(((uint64_t)value >> 56) & 0xFF);
-			if (i > 55)
+			}
+			if (i > 55) {
 				WriteToStream(((uint64_t)value >> 48) & 0xFF);
-			if (i > 47)
+			}
+			if (i > 47) {
 				WriteToStream(((uint64_t)value >> 40) & 0xFF);
-			if (i > 39)
+			}
+			if (i > 39) {
 				WriteToStream(((uint64_t)value >> 32) & 0xFF);
+			}
 		}
-		if (i > 31)
+		if (i > 31) {
 			WriteToStream((value >> 24) & 0xFF);
-		if (i > 23)
+		}
+		if (i > 23) {
 			WriteToStream((value >> 16) & 0xFF);
-		if (i > 15)
+		}
+		if (i > 15) {
 			WriteToStream((value >> 8) & 0xFF);
-		if (i > 7)
+		}
+		if (i > 7) {
 			WriteToStream(value);
+		}
 	}
 
 	template <class T, uint8_t VALUE_SIZE>
