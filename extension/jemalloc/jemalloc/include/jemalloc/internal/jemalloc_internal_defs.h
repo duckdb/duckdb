ddaee6f065ea731aa633498489367969201d1985
Laurens Kuiper <laurens.kuiper@cwi.nl>
2022-11-03 14:38:40 +0100

    remove more platform-specific gunk

--- extension/jemalloc/jemalloc/include/jemalloc/internal/jemalloc_internal_defs.h
+++ extension/jemalloc/jemalloc/include/jemalloc/internal/jemalloc_internal_defs.h
@@ -440,7 +440,7 @@ namespace duckdb_jemalloc {
 /*
  * Defined if strerror_r returns char * if _GNU_SOURCE is defined.
  */
-#define JEMALLOC_STRERROR_R_RETURNS_CHAR_WITH_GNU_SOURCE
+//#define JEMALLOC_STRERROR_R_RETURNS_CHAR_WITH_GNU_SOURCE
 
 /* Performs additional safety checks when defined. */
 /* #undef JEMALLOC_OPT_SAFETY_CHECKS */
