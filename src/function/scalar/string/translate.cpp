f7ffcded73ec8cbd25c4d7fee226682e204c3cb4
zhouliqi <zhouliqi1204@gmail.com>
2022-11-05 11:09:31 +0800

    fix tidy-check error

--- src/function/scalar/string/translate.cpp
+++ src/function/scalar/string/translate.cpp
@@ -3,6 +3,8 @@
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/vector_operations/vector_operations.hpp"
 #include "duckdb/common/vector_operations/ternary_executor.hpp"
+#include "utf8proc.hpp"
+#include "utf8proc_wrapper.hpp"
 
 #include <string.h>
 #include <ctype.h>
