e42eaeaf9531a2b8dfb40dfbf2f0f6786f2e500c
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-11-06 10:00:12 +0100

    More null byte tests, avoid using null byte as place-holder character in LIKE

--- src/function/scalar/string/chr.cpp
+++ src/function/scalar/string/chr.cpp
@@ -8,11 +8,11 @@ struct ChrOperator {
 	template <class TA, class TR>
 	static inline TR Operation(const TA &input) {
 		char c[5] = {'\0', '\0', '\0', '\0', '\0'};
-		int utf8_bytes = 4;
+		int utf8_bytes;
 		if (input < 0 || !Utf8Proc::CodepointToUtf8(input, utf8_bytes, &c[0])) {
 			throw InvalidInputException("Invalid UTF8 Codepoint %d", input);
 		}
-		return string_t(&c[0]);
+		return string_t(&c[0], utf8_bytes);
 	}
 };
 
