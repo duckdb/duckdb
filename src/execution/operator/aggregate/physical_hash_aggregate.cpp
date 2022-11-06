47625996e08d9caf4a45da4e9586a4883c3e74ea
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-11-06 15:18:57 +0100

    Insert next event before scheduling tasks

--- src/execution/operator/aggregate/physical_hash_aggregate.cpp
+++ src/execution/operator/aggregate/physical_hash_aggregate.cpp
@@ -690,6 +690,10 @@ public:
 
 public:
 	void Schedule() override {
+		//! Now that all tables are combined, it's time to do the distinct aggregations
+		auto new_event = make_shared<HashDistinctAggregateFinalizeEvent>(op, gstate, *pipeline, client);
+		this->InsertEvent(move(new_event));
+
 		vector<unique_ptr<Task>> tasks;
 		for (idx_t i = 0; i < op.groupings.size(); i++) {
 			auto &grouping = op.groupings[i];
@@ -706,10 +710,6 @@ public:
 
 		D_ASSERT(!tasks.empty());
 		SetTasks(move(tasks));
-
-		//! Now that all tables are combined, it's time to do the distinct aggregations
-		auto new_event = make_shared<HashDistinctAggregateFinalizeEvent>(op, gstate, *pipeline, client);
-		this->InsertEvent(move(new_event));
 	}
 };
 
