5b005c52bdf3baa218413d44687709df7cf8f40d
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-11-08 18:44:56 +0100

    Fuzzer 34: system functions should also no longer bind the client context themselves

--- src/function/scalar/system/system_functions.cpp
+++ src/function/scalar/system/system_functions.cpp
@@ -10,39 +10,14 @@
 namespace duckdb {
 
 // current_query
-struct SystemBindData : public FunctionData {
-	ClientContext &context;
-
-	explicit SystemBindData(ClientContext &context) : context(context) {
-	}
-
-	unique_ptr<FunctionData> Copy() const override {
-		return make_unique<SystemBindData>(context);
-	}
-	bool Equals(const FunctionData &other_p) const override {
-		return true;
-	}
-
-	static SystemBindData &GetFrom(ExpressionState &state) {
-		auto &func_expr = (BoundFunctionExpression &)state.expr;
-		return (SystemBindData &)*func_expr.bind_info;
-	}
-};
-
-unique_ptr<FunctionData> BindSystemFunction(ClientContext &context, ScalarFunction &bound_function,
-                                            vector<unique_ptr<Expression>> &arguments) {
-	return make_unique<SystemBindData>(context);
-}
-
 static void CurrentQueryFunction(DataChunk &input, ExpressionState &state, Vector &result) {
-	auto &info = SystemBindData::GetFrom(state);
-	Value val(info.context.GetCurrentQuery());
+	Value val(state.GetContext().GetCurrentQuery());
 	result.Reference(val);
 }
 
 // current_schema
 static void CurrentSchemaFunction(DataChunk &input, ExpressionState &state, Vector &result) {
-	Value val(ClientData::Get(SystemBindData::GetFrom(state).context).catalog_search_path->GetDefault());
+	Value val(ClientData::Get(state.GetContext()).catalog_search_path->GetDefault());
 	result.Reference(val);
 }
 
@@ -58,7 +33,7 @@ static void CurrentSchemasFunction(DataChunk &input, ExpressionState &state, Vec
 	}
 	auto implicit_schemas = *ConstantVector::GetData<bool>(input.data[0]);
 	vector<Value> schema_list;
-	auto &catalog_search_path = ClientData::Get(SystemBindData::GetFrom(state).context).catalog_search_path;
+	auto &catalog_search_path = ClientData::Get(state.GetContext()).catalog_search_path;
 	vector<string> search_path = implicit_schemas ? catalog_search_path->Get() : catalog_search_path->GetSetPaths();
 	std::transform(search_path.begin(), search_path.end(), std::back_inserter(schema_list),
 	               [](const string &s) -> Value { return Value(s); });
@@ -69,7 +44,7 @@ static void CurrentSchemasFunction(DataChunk &input, ExpressionState &state, Vec
 
 // txid_current
 static void TransactionIdCurrent(DataChunk &input, ExpressionState &state, Vector &result) {
-	auto &transaction = Transaction::GetTransaction(SystemBindData::GetFrom(state).context);
+	auto &transaction = Transaction::GetTransaction(state.GetContext());
 	auto val = Value::BIGINT(transaction.start_time);
 	result.Reference(val);
 }
@@ -83,14 +58,13 @@ static void VersionFunction(DataChunk &input, ExpressionState &state, Vector &re
 void SystemFun::RegisterFunction(BuiltinFunctions &set) {
 	auto varchar_list_type = LogicalType::LIST(LogicalType::VARCHAR);
 
-	ScalarFunction current_query("current_query", {}, LogicalType::VARCHAR, CurrentQueryFunction, BindSystemFunction);
+	ScalarFunction current_query("current_query", {}, LogicalType::VARCHAR, CurrentQueryFunction);
 	current_query.side_effects = FunctionSideEffects::HAS_SIDE_EFFECTS;
 	set.AddFunction(current_query);
+	set.AddFunction(ScalarFunction("current_schema", {}, LogicalType::VARCHAR, CurrentSchemaFunction));
 	set.AddFunction(
-	    ScalarFunction("current_schema", {}, LogicalType::VARCHAR, CurrentSchemaFunction, BindSystemFunction));
-	set.AddFunction(ScalarFunction("current_schemas", {LogicalType::BOOLEAN}, varchar_list_type, CurrentSchemasFunction,
-	                               BindSystemFunction));
-	set.AddFunction(ScalarFunction("txid_current", {}, LogicalType::BIGINT, TransactionIdCurrent, BindSystemFunction));
+	    ScalarFunction("current_schemas", {LogicalType::BOOLEAN}, varchar_list_type, CurrentSchemasFunction));
+	set.AddFunction(ScalarFunction("txid_current", {}, LogicalType::BIGINT, TransactionIdCurrent));
 	set.AddFunction(ScalarFunction("version", {}, LogicalType::VARCHAR, VersionFunction));
 	set.AddFunction(ExportAggregateFunction::GetCombine());
 	set.AddFunction(ExportAggregateFunction::GetFinalize());
