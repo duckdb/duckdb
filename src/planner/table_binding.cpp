b4877dd85ef587b3dbdd679b19d769cf4df26467
Tishj <t_b@live.nl>
2022-11-10 16:03:01 +0100

    remove unintended change

--- src/planner/table_binding.cpp
+++ src/planner/table_binding.cpp
@@ -94,21 +94,15 @@ TableBinding::TableBinding(const string &alias, vector<LogicalType> types_p, vec
 	}
 }
 
-static void BakeTableName(ParsedExpression &expr, const string &table_name, const string &column_name) {
+static void BakeTableName(ParsedExpression &expr, const string &table_name) {
 	if (expr.type == ExpressionType::COLUMN_REF) {
 		auto &colref = (ColumnRefExpression &)expr;
 		D_ASSERT(!colref.IsQualified());
 		auto &col_names = colref.column_names;
-		if (colref.GetName() == column_name) {
-			throw BinderException("Query could not complete because generated column '%s' has been turned into a self "
-			                      "referencing expression",
-			                      column_name);
-		}
 		col_names.insert(col_names.begin(), table_name);
 	}
-	ParsedExpressionIterator::EnumerateChildren(expr, [&](const ParsedExpression &child) {
-		BakeTableName((ParsedExpression &)child, table_name, column_name);
-	});
+	ParsedExpressionIterator::EnumerateChildren(
+	    expr, [&](const ParsedExpression &child) { BakeTableName((ParsedExpression &)child, table_name); });
 }
 
 unique_ptr<ParsedExpression> TableBinding::ExpandGeneratedColumn(const string &column_name) {
@@ -122,9 +116,8 @@ unique_ptr<ParsedExpression> TableBinding::ExpandGeneratedColumn(const string &c
 	auto column_index = GetBindingIndex(column_name);
 	D_ASSERT(table_entry->columns[column_index].Generated());
 	// Get a copy of the generated column
-	// FIXME: this does not respect query-level column aliases
 	auto expression = table_entry->columns[column_index].GeneratedExpression().Copy();
-	BakeTableName(*expression, alias, column_name);
+	BakeTableName(*expression, alias);
 	return (expression);
 }
 
