8fda83b16485f8628edce5c96e61786900a50234
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-11-11 16:43:15 +0100

    Box renderer: only show the x rows dialog if we have 10 or more rows

--- src/common/box_renderer.cpp
+++ src/common/box_renderer.cpp
@@ -500,8 +500,8 @@ void BoxRenderer::RenderRowCount(string row_count_str, string shown_str, const s
 	}
 	auto minimum_length = row_count_str.size() + column_count_str.size() + 6;
 	bool render_rows_and_columns = total_length >= minimum_length &&
-	                               ((has_hidden_columns && row_count > 0) || (row_count > 1 && column_count > 1));
-	bool render_rows = total_length >= minimum_row_length && row_count != 1;
+	                               ((has_hidden_columns && row_count > 0) || (row_count >= 10 && column_count > 1));
+	bool render_rows = total_length >= minimum_row_length && (row_count == 0 || row_count >= 10);
 	bool render_anything = true;
 	if (!render_rows && !render_rows_and_columns) {
 		render_anything = false;
