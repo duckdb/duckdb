6f859c01f45eb939d941b549d9cc76218cbab32f
Tishj <t_b@live.nl>
2022-11-13 18:35:55 +0100

    check for nan to prevent undefined behavior sanitizer issue

--- src/planner/operator/logical_limit_percent.cpp
+++ src/planner/operator/logical_limit_percent.cpp
@@ -20,7 +20,7 @@ unique_ptr<LogicalOperator> LogicalLimitPercent::Deserialize(LogicalDeserializat
 
 idx_t LogicalLimitPercent::EstimateCardinality(ClientContext &context) {
 	auto child_cardinality = LogicalOperator::EstimateCardinality(context);
-	if (limit_percent < 0 || limit_percent > 100) {
+	if ((limit_percent < 0 || limit_percent > 100) || isnan(limit_percent)) {
 		return child_cardinality;
 	}
 	return idx_t(child_cardinality * (limit_percent / 100.0));
