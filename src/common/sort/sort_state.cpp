24cdb8558c34d1545dacdaf0ff20697321b6082d
Laurens Kuiper <laurens.kuiper@cwi.nl>
2022-11-16 15:58:24 +0100

    free blocks a bit earlier in the sort code

--- src/common/sort/sort_state.cpp
+++ src/common/sort/sort_state.cpp
@@ -267,10 +267,12 @@ unique_ptr<RowDataBlock> LocalSortState::ConcatenateBlocks(RowDataCollection &ro
 	auto new_block_handle = buffer_manager->Pin(new_block->block);
 	data_ptr_t new_block_ptr = new_block_handle.Ptr();
 	// Copy the data of the blocks into a single block
-	for (auto &block : row_data.blocks) {
+	for (idx_t i = 0; i < row_data.blocks.size(); i++) {
+		auto &block = row_data.blocks[i];
 		auto block_handle = buffer_manager->Pin(block->block);
 		memcpy(new_block_ptr, block_handle.Ptr(), block->count * entry_size);
 		new_block_ptr += block->count * entry_size;
+		block.reset();
 	}
 	row_data.blocks.clear();
 	row_data.count = 0;
