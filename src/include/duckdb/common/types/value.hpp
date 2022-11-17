73a6fd3fde71094c71cb673c12a966a7ca9e5cdf
Tishj <t_b@live.nl>
2022-11-17 13:45:22 +0100

    remove Value::SetNull function

--- src/include/duckdb/common/types/value.hpp
+++ src/include/duckdb/common/types/value.hpp
@@ -69,10 +69,6 @@ public:
 		return is_null;
 	}
 
-	inline void SetNull() {
-		is_null = true;
-	}
-
 	//! Create the lowest possible value of a given type (numeric only)
 	DUCKDB_API static Value MinimumValue(const LogicalType &type);
 	//! Create the highest possible value of a given type (numeric only)
