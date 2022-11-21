81e84be2e837f7459e2a4463b4bfd9578bff45c8
Tishj <t_b@live.nl>
2022-11-21 13:15:28 +0100

    remove leftovers of removed changes

--- tools/pythonpkg/src/include/duckdb_python/pyrelation.hpp
+++ tools/pythonpkg/src/include/duckdb_python/pyrelation.hpp
@@ -227,7 +227,6 @@ private:
 	string GenerateExpressionList(const string &function_name, const vector<string> &aggregated_columns,
 	                              const string &groups = "", const string &function_parameter = "",
 	                              const string &projected_columns = "", const string &window_function = "");
-
 };
 
 } // namespace duckdb
