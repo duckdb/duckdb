9b3daa58151dfa4ee48756b931a3913b6b4e4647
Tom Ebergen <tom@ebergen.com>
2022-11-21 14:14:10 +0100

    again a CI fix/check

--- benchmark/h2oai/join/queries/q02.sql
+++ benchmark/h2oai/join/queries/q02.sql
@@ -1,1 +1,1 @@
-CREATE TABLE ans AS SELECT x.*, medium.id1 AS medium_id1, medium.id4 AS medium_id4, medium.id5 AS medium_id5, v2 FROM x JOIN medium USING (id2);
+CREATE TEMP TABLE ans AS SELECT x.*, medium.id1 AS medium_id1, medium.id4 AS medium_id4, medium.id5 AS medium_id5, v2 FROM x JOIN medium USING (id2);
