489959ec6fbe52b88d37d209e7f1384ae1818b5a
Sam Ansmink <samansmink@hotmail.com>
2022-11-24 19:43:41 +0100

    format

--- tools/nodejs/src/connection.cpp
+++ tools/nodejs/src/connection.cpp
@@ -436,7 +436,8 @@ Napi::Value Connection::RegisterBuffer(const Napi::CallbackInfo &info) {
 
 	if (info.Length() > 2) {
 		if (!info[2].IsBoolean()) {
-			Napi::TypeError::New(env, "Parameter 3 is of unexpected type. Expected boolean").ThrowAsJavaScriptException();
+			Napi::TypeError::New(env, "Parameter 3 is of unexpected type. Expected boolean")
+			    .ThrowAsJavaScriptException();
 			return env.Null();
 		}
 		force_register = info[2].As<Napi::Boolean>().Value();
@@ -455,7 +456,8 @@ Napi::Value Connection::RegisterBuffer(const Napi::CallbackInfo &info) {
 	for (uint64_t ipc_idx = 0; ipc_idx < array.Length(); ipc_idx++) {
 		Napi::Value v = array[ipc_idx];
 		if (!v.IsObject()) {
-			Napi::TypeError::New(env, "Parameter 2 contains unexpected type at index " + std::to_string(ipc_idx)).ThrowAsJavaScriptException();
+			Napi::TypeError::New(env, "Parameter 2 contains unexpected type at index " + std::to_string(ipc_idx))
+			    .ThrowAsJavaScriptException();
 			return env.Null();
 		}
 		Napi::Uint8Array arr = v.As<Napi::Uint8Array>();
