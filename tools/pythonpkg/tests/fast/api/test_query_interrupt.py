bc9afc6e00476873dba76c094596bf27783ed33f
Tishj <t_b@live.nl>
2022-11-24 22:12:41 +0100

    add passing test for query interruption

---
+++ tools/pythonpkg/tests/fast/api/test_query_interrupt.py
@@ -0,0 +1,29 @@
+import duckdb
+import time
+import pytest
+
+import threading
+
+try:
+  import thread
+except ImportError:  # for Python 3 compat
+  import _thread as thread
+
+def send_keyboard_interrupt():
+    # Wait a couple seconds
+    time.sleep(2)
+    # Send an interrupt to the main thread
+    thread.interrupt_main()
+
+class TestQueryInterruption(object):
+    def test_query_interruption(self):
+        con = duckdb.connect()
+        thread = threading.Thread(target=send_keyboard_interrupt)
+        # Start the thread
+        thread.start()
+        try:
+            res = con.execute('select count(*) from range(100000000000000)').fetchall()
+        except RuntimeError:
+            assert True
+        # If this is not reached, this test will hang forever
+        # indicating that the query interruption functionality is broken
