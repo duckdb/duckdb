01d4fc98d13c4719a17bcc57b81f440d169835e1
Laurens Kuiper <laurens.kuiper@cwi.nl>
2022-11-29 10:19:46 +0100

    went a bit overboard with the DUCKDB_API apparently

--- src/include/duckdb/common/file_opener.hpp
+++ src/include/duckdb/common/file_opener.hpp
@@ -18,9 +18,9 @@ class Value;
 //! Abstract type that provide client-specific context to FileSystem.
 class FileOpener {
 public:
-	DUCKDB_API virtual ~FileOpener() {};
+	virtual ~FileOpener() {};
 
-	DUCKDB_API virtual bool TryGetCurrentSetting(const string &key, Value &result) = 0;
+	virtual bool TryGetCurrentSetting(const string &key, Value &result) = 0;
 
 	DUCKDB_API static FileOpener *Get(ClientContext &context);
 };
