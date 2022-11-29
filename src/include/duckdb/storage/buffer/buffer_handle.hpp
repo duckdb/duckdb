80721419c32d07b8f11fce1a0a4d06f28a3ea9ed
Tishj <t_b@live.nl>
2022-11-29 15:47:07 +0100

    reverted changes to buffer_handle

--- src/include/duckdb/storage/buffer/buffer_handle.hpp
+++ src/include/duckdb/storage/buffer/buffer_handle.hpp
@@ -14,30 +14,39 @@ namespace duckdb {
 class BlockHandle;
 class FileBuffer;
 
-//! Handle for an allocated buffer, created from the BufferManager
 class BufferHandle {
-protected:
-	DUCKDB_API BufferHandle() {
-	}
-	virtual DUCKDB_API ~BufferHandle() {
-	}
+public:
+	DUCKDB_API BufferHandle();
+	DUCKDB_API BufferHandle(shared_ptr<BlockHandle> handle, FileBuffer *node);
+	DUCKDB_API ~BufferHandle();
 	// disable copy constructors
 	BufferHandle(const BufferHandle &other) = delete;
 	BufferHandle &operator=(const BufferHandle &) = delete;
+	//! enable move constructors
+	DUCKDB_API BufferHandle(BufferHandle &&other) noexcept;
+	DUCKDB_API BufferHandle &operator=(BufferHandle &&) noexcept;
 
 public:
 	//! Returns whether or not the BufferHandle is valid.
-	virtual DUCKDB_API bool IsValid() const = 0;
+	DUCKDB_API bool IsValid() const;
 	//! Returns a pointer to the buffer data. Handle must be valid.
-	virtual DUCKDB_API data_ptr_t Ptr() const = 0;
+	DUCKDB_API data_ptr_t Ptr() const;
 	//! Returns a pointer to the buffer data. Handle must be valid.
-	virtual DUCKDB_API data_ptr_t Ptr() = 0;
+	DUCKDB_API data_ptr_t Ptr();
 	//! Gets the underlying file buffer. Handle must be valid.
-	virtual DUCKDB_API FileBuffer &GetFileBuffer() = 0;
+	DUCKDB_API FileBuffer &GetFileBuffer();
 	//! Destroys the buffer handle
-	virtual DUCKDB_API void Destroy() = 0;
-	//! Get the internal block handle
-	virtual const shared_ptr<BlockHandle> &GetBlockHandle() const = 0;
+	DUCKDB_API void Destroy();
+
+	const shared_ptr<BlockHandle> &GetBlockHandle() const {
+		return handle;
+	}
+
+private:
+	//! The block handle
+	shared_ptr<BlockHandle> handle;
+	//! The managed buffer node
+	FileBuffer *node;
 };
 
 } // namespace duckdb
