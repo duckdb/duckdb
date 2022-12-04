477bfb6d356c6ea13d1da2cb1f1387d9835c99c2
Tishj <t_b@live.nl>
2022-12-04 20:34:18 +0100

    make format master

--- tools/pythonpkg/src/include/duckdb_python/pyrelation.hpp
+++ tools/pythonpkg/src/include/duckdb_python/pyrelation.hpp
@@ -68,12 +68,11 @@ public:
 	                                                bool file_row_number, bool filename, bool hive_partitioning,
 	                                                DuckDBPyConnection *conn = nullptr);
 
-	static unique_ptr<DuckDBPyRelation>
-	FromParquets(const vector<string> &file_globs, bool binary_as_string, bool file_row_number, bool filename,
-	             bool hive_partitioning, DuckDBPyConnection *conn = nullptr);
+	static unique_ptr<DuckDBPyRelation> FromParquets(const vector<string> &file_globs, bool binary_as_string,
+	                                                 bool file_row_number, bool filename, bool hive_partitioning,
+	                                                 DuckDBPyConnection *conn = nullptr);
 
-	static unique_ptr<DuckDBPyRelation>
-	FromSubstrait(py::bytes &proto, DuckDBPyConnection *conn = nullptr);
+	static unique_ptr<DuckDBPyRelation> FromSubstrait(py::bytes &proto, DuckDBPyConnection *conn = nullptr);
 
 	static unique_ptr<DuckDBPyRelation> GetSubstrait(const string &query, DuckDBPyConnection *conn = nullptr);
 
