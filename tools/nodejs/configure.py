3a2c5b26f11e900f191f7089a93e206e23621a8c
Mark Raasveldt <mark.raasveldt@gmail.com>
2022-12-13 17:56:01 +0100

    Add caching to filelist to hopefully speed up CI builds

--- tools/nodejs/configure.py
+++ tools/nodejs/configure.py
@@ -1,37 +1,86 @@
 import os
 import sys
-import sys
+import pickle
+
+# list of extensions to bundle
+extensions = ['parquet', 'icu', 'json']
 
 # path to target
 basedir = os.getcwd()
 target_dir = os.path.join(basedir, 'src', 'duckdb')
 gyp_in = os.path.join(basedir, 'binding.gyp.in')
 gyp_out = os.path.join(basedir, 'binding.gyp')
+cache_file = os.path.join(basedir, 'filelist.cache')
 
 # path to package_build.py
 os.chdir(os.path.join('..', '..'))
 scripts_dir = 'scripts'
-# list of extensions to bundle
-extensions = ['parquet']
 
 sys.path.append(scripts_dir)
 import package_build
 
-(source_list, include_list, original_sources) = package_build.build_package(target_dir, extensions, False)
+if 'DUCKDB_NODE_BUILD_CACHE' in os.environ and os.path.isfile(cache_file):
+    with open(cache_file, 'rb') as f:
+        cache = pickle.load(f)
+    source_list = cache['source_list']
+    include_list = cache['include_list']
+    library_text = cache['library_text']
+elif 'DUCKDB_NODE_BINDIR' in os.environ:
+    def find_library_path(libdir, libname):
+        flist = os.listdir(libdir)
+        for fname in flist:
+            fpath = os.path.join(libdir, fname)
+            if os.path.isfile(fpath) and libname in fname:
+                return fpath
+        raise Exception(f"Failed to find library {libname} in {libdir}")
+    # existing build
+    existing_duckdb_dir = os.environ['DUCKDB_NODE_BINDIR']
+    cflags = os.environ['DUCKDB_NODE_CFLAGS']
+    libraries = os.environ['DUCKDB_NODE_LIBS'].split(' ')
 
-# # the list of all source files (.cpp files) that have been copied into the `duckdb_source_copy` directory
-# print(source_list)
-# # the list of all include files
-# print(include_list)
+    include_directories = [os.path.join('..', '..', include) for include in package_build.third_party_includes()]
+    include_list = package_build.includes(extensions)
+
+    result_libraries = package_build.get_libraries(existing_duckdb_dir, libraries, extensions)
+    libraries = []
+    for (libdir, libname) in result_libraries:
+        if libdir is None:
+            continue
+        libraries.append(find_library_path(libdir, libname))
+
+    libs = ',\n                 '.join(['"' + x + '"' for x in libraries])
+    source_list = []
+    library_text = f'''"libraries": [
+                 {libs}
+            ]
+    '''
+    if 'DUCKDB_NODE_BUILD_CACHE' in os.environ:
+        cache = {
+            'source_list': source_list,
+            'include_list': include_list,
+            'library_text': library_text
+        }
+        with open(cache_file, 'wb+') as f:
+            pickle.dump(cache, f)
+else:
+    # fresh build - copy over all of the files
+    (source_list, include_list, original_sources) = package_build.build_package(target_dir, extensions, False)
+
+    # # the list of all source files (.cpp files) that have been copied into the `duckdb_source_copy` directory
+    # print(source_list)
+    # # the list of all include files
+    # print(include_list)
+    source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x) for x in source_list]
+    include_list = [os.path.join('src', 'duckdb', x) for x in include_list]
+    library_text = ''
+
 
 with open(gyp_in, 'r') as f:
-	text = f.read()
-
-source_list = [os.path.relpath(x, basedir) if os.path.isabs(x) else os.path.join('src', x) for x in source_list]
-include_list = [os.path.join('src', 'duckdb', x) for x in include_list]
+    text = f.read()
 
 text = text.replace('${SOURCE_FILES}', ',\n                '.join(['"' + x + '"' for x in source_list]))
 text = text.replace('${INCLUDE_FILES}', ',\n                '.join(['"' + x + '"' for x in include_list]))
+text = text.replace('${LIBRARY_FILES}', library_text)
 
 with open(gyp_out, 'w+') as f:
-	f.write(text)
+    f.write(text)
