fb9fcaf51f4ccfcfdb2b59a1f2ee36a2da64e1e3
Laurens Kuiper <laurens.kuiper@cwi.nl>
2023-01-24 09:33:48 +0100

    more fixes to json_contains #5960

--- extension/json/json_functions/json_contains.cpp
+++ extension/json/json_functions/json_contains.cpp
@@ -3,16 +3,18 @@
 namespace duckdb {
 
 static inline bool JSONContains(yyjson_val *haystack, yyjson_val *needle);
+static inline bool JSONFuzzyEquals(yyjson_val *haystack, yyjson_val *needle);
 
-static inline bool JSONArrayContainsArray(yyjson_val *haystack, yyjson_val *needle) {
+static inline bool JSONArrayFuzzyEquals(yyjson_val *haystack, yyjson_val *needle) {
 	D_ASSERT(yyjson_get_tag(haystack) == (YYJSON_TYPE_ARR | YYJSON_SUBTYPE_NONE) &&
 	         yyjson_get_tag(needle) == (YYJSON_TYPE_ARR | YYJSON_SUBTYPE_NONE));
+
 	size_t needle_idx, needle_max, haystack_idx, haystack_max;
 	yyjson_val *needle_child, *haystack_child;
 	yyjson_arr_foreach(needle, needle_idx, needle_max, needle_child) {
 		bool found = false;
 		yyjson_arr_foreach(haystack, haystack_idx, haystack_max, haystack_child) {
-			if (JSONContains(haystack_child, needle_child)) {
+			if (JSONFuzzyEquals(haystack_child, needle_child)) {
 				found = true;
 				break;
 			}
@@ -24,58 +26,84 @@ static inline bool JSONArrayContainsArray(yyjson_val *haystack, yyjson_val *need
 	return true;
 }
 
-static inline bool JSONObjectContainsObject(yyjson_val *haystack, yyjson_val *needle) {
+static inline bool JSONObjectFuzzyEquals(yyjson_val *haystack, yyjson_val *needle) {
 	D_ASSERT(yyjson_get_tag(haystack) == (YYJSON_TYPE_OBJ | YYJSON_SUBTYPE_NONE) &&
 	         yyjson_get_tag(needle) == (YYJSON_TYPE_OBJ | YYJSON_SUBTYPE_NONE));
+
 	size_t idx, max;
 	yyjson_val *key, *needle_child;
 	yyjson_obj_foreach(needle, idx, max, key, needle_child) {
 		auto haystack_child = yyjson_obj_get(haystack, yyjson_get_str(key));
-		if (!haystack_child || !JSONContains(haystack_child, needle_child)) {
+		if (!haystack_child || !JSONFuzzyEquals(haystack_child, needle_child)) {
 			return false;
 		}
 	}
 	return true;
 }
 
+static inline bool JSONFuzzyEquals(yyjson_val *haystack, yyjson_val *needle) {
+	D_ASSERT(haystack && needle);
+
+	// Strict equality
+	if (unsafe_yyjson_equals(haystack, needle)) {
+		return true;
+	}
+
+	auto haystack_tag = yyjson_get_tag(needle);
+	if (haystack_tag != yyjson_get_tag(haystack)) {
+		return false;
+	}
+
+	// Fuzzy equality (contained in)
+	switch (haystack_tag) {
+	case YYJSON_TYPE_ARR | YYJSON_SUBTYPE_NONE:
+		return JSONArrayFuzzyEquals(haystack, needle);
+	case YYJSON_TYPE_OBJ | YYJSON_SUBTYPE_NONE:
+		return JSONObjectFuzzyEquals(haystack, needle);
+	default:
+		return false;
+	}
+}
+
+static inline bool JSONArrayContains(yyjson_val *haystack_array, yyjson_val *needle) {
+	D_ASSERT(yyjson_get_tag(haystack_array) == (YYJSON_TYPE_ARR | YYJSON_SUBTYPE_NONE));
+
+	size_t idx, max;
+	yyjson_val *child_haystack;
+	yyjson_arr_foreach(haystack_array, idx, max, child_haystack) {
+		if (JSONContains(child_haystack, needle)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+static inline bool JSONObjectContains(yyjson_val *haystack_object, yyjson_val *needle) {
+	D_ASSERT(yyjson_get_tag(haystack_object) == (YYJSON_TYPE_OBJ | YYJSON_SUBTYPE_NONE));
+
+	size_t idx, max;
+	yyjson_val *key, *child_haystack;
+	yyjson_obj_foreach(haystack_object, idx, max, key, child_haystack) {
+		if (JSONContains(child_haystack, needle)) {
+			return true;
+		}
+	}
+	return false;
+}
+
 static inline bool JSONContains(yyjson_val *haystack, yyjson_val *needle) {
-	if (yyjson_equals(haystack, needle)) {
+	if (JSONFuzzyEquals(haystack, needle)) {
 		return true;
 	}
 
 	switch (yyjson_get_tag(haystack)) {
-	case YYJSON_TYPE_ARR | YYJSON_SUBTYPE_NONE: {
-		if (yyjson_get_tag(needle) == (YYJSON_TYPE_ARR | YYJSON_SUBTYPE_NONE) &&
-		    JSONArrayContainsArray(haystack, needle)) {
-			return true;
-		}
-		size_t idx, max;
-		yyjson_val *child_haystack;
-		yyjson_arr_foreach(haystack, idx, max, child_haystack) {
-			if (JSONContains(child_haystack, needle)) {
-				return true;
-			}
-		}
-		break;
-	}
-	case YYJSON_TYPE_OBJ | YYJSON_SUBTYPE_NONE: {
-		if (yyjson_get_tag(needle) == (YYJSON_TYPE_OBJ | YYJSON_SUBTYPE_NONE) &&
-		    JSONObjectContainsObject(haystack, needle)) {
-			return true;
-		}
-		size_t idx, max;
-		yyjson_val *key, *child_haystack;
-		yyjson_obj_foreach(haystack, idx, max, key, child_haystack) {
-			if (JSONContains(child_haystack, needle)) {
-				return true;
-			}
-		}
-		break;
-	}
+	case YYJSON_TYPE_ARR | YYJSON_SUBTYPE_NONE:
+		return JSONArrayContains(haystack, needle);
+	case YYJSON_TYPE_OBJ | YYJSON_SUBTYPE_NONE:
+		return JSONObjectContains(haystack, needle);
 	default:
-		break;
+		return false;
 	}
-	return false;
 }
 
 static void JSONContainsFunction(DataChunk &args, ExpressionState &state, Vector &result) {
