e765a70ced2b3cbfdaaf488a0b803644cae16574
Sam Ansmink <samansmink@hotmail.com>
2023-01-30 18:06:06 +0100

    revert this script

--- scripts/extension-upload.sh
+++ scripts/extension-upload.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
-# Usage: ./extension-upload.sh <architecture> <commithash or version_tag> <s3_bucket> <skip_signing>
+# Usage: ./extension-upload.sh <architecture> <commithash or version_tag>
 
 set -e
 
-SHOULD_SIGN=${4:-true}
-S3_BUCKET=${3:-"duckdb-extensions"}
-
-if [ "$SHOULD_SIGN" = true ] ; then
-    echo "creating key"
-    echo "$DUCKDB_EXTENSION_SIGNING_PK" > private.pem
-fi
+echo "$DUCKDB_EXTENSION_SIGNING_PK" > private.pem
 
 FILES="build/release/extension/*/*.duckdb_extension"
 for f in $FILES
 do
 	ext=`basename $f .duckdb_extension`
 	echo $ext
-
-	if [ "$SHOULD_SIGN" = true ] ; then
-	    echo "signing it"
-      # calculate SHA256 hash of extension binary
-      openssl dgst -binary -sha256 $f > $f.hash
-      # encrypt hash with extension signing private key to create signature
-      openssl pkeyutl -sign -in $f.hash -inkey private.pem -pkeyopt digest:sha256 -out $f.sign
-      # append signature to extension binary
-      cat $f.sign >> $f
-  fi
-
+	# calculate SHA256 hash of extension binary
+	openssl dgst -binary -sha256 $f > $f.hash
+	# encrypt hash with extension signing private key to create signature
+	openssl pkeyutl -sign -in $f.hash -inkey private.pem -pkeyopt digest:sha256 -out $f.sign
+	# append signature to extension binary
+  cat $f.sign >> $f
   # compress extension binary
 	gzip < $f > "$f.gz"
 	# upload compressed extension binary to S3
-	aws s3 cp $f.gz s3://$S3_BUCKET/$2/$1/$ext.duckdb_extension.gz --acl public-read
+	aws s3 cp $f.gz s3://duckdb-extensions/$2/$1/$ext.duckdb_extension.gz --acl public-read
 done
 
-if [ "$SHOULD_SIGN" = true ] ; then
-  rm private.pem
-fi
+rm private.pem
