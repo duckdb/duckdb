87fcaef92b272577de560b87982dbb75d1086ef3
Tishj <t_b@live.nl>
2023-02-02 20:04:00 +0100

    add dependency on 'xlrd' in read_excel test

--- tools/pythonpkg/tests/fast/api/test_read_excel.py
+++ tools/pythonpkg/tests/fast/api/test_read_excel.py
@@ -1,6 +1,8 @@
 import pytest
 import duckdb
 pandas = pytest.importorskip("pandas", minversion="1.5.3")
+# To parse the excel files, pandas uses 'xlrd'
+excel = pytest.importorskip("xlrd")
 import os
 
 def ExpectedExampleOutput():
