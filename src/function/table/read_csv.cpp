5ab5ad84098f9671628f9e2c65b3ca4adafa1955
Pedro Holanda <pedroholanda@gmail.com>
2023-02-09 17:28:23 +0100

    Fixing race condition by holding current file path in csv parallel global state

--- src/function/table/read_csv.cpp
+++ src/function/table/read_csv.cpp
@@ -15,11 +15,12 @@
 
 namespace duckdb {
 
-unique_ptr<CSVFileHandle> ReadCSV::OpenCSV(const BufferedCSVReaderOptions &options, ClientContext &context) {
+unique_ptr<CSVFileHandle> ReadCSV::OpenCSV(const string &file_path, FileCompressionType compression,
+                                           ClientContext &context) {
 	auto &fs = FileSystem::GetFileSystem(context);
 	auto opener = FileSystem::GetFileOpener(context);
-	auto file_handle = fs.OpenFile(options.file_path.c_str(), FileFlags::FILE_FLAGS_READ, FileLockType::NO_LOCK,
-	                               options.compression, opener);
+	auto file_handle =
+	    fs.OpenFile(file_path.c_str(), FileFlags::FILE_FLAGS_READ, FileLockType::NO_LOCK, compression, opener);
 	return make_unique<CSVFileHandle>(std::move(file_handle));
 }
 
@@ -248,7 +249,7 @@ public:
 	                       vector<string> &files_path_p, idx_t system_threads_p, idx_t buffer_size_p,
 	                       idx_t rows_to_skip, bool force_parallelism_p)
 	    : file_handle(std::move(file_handle_p)), system_threads(system_threads_p), buffer_size(buffer_size_p),
-	      force_parallelism(force_parallelism_p) {
+	      force_parallelism(force_parallelism_p), current_file_path(files_path_p[0]) {
 		for (idx_t i = 0; i < rows_to_skip; i++) {
 			file_handle->ReadLine();
 		}
@@ -315,6 +316,7 @@ private:
 
 	//! The index of the next file to read (i.e. current file + 1)
 	idx_t file_index = 1;
+	string current_file_path;
 
 	//! Mutex to lock when getting next batch of bytes (Parallel Only)
 	mutex main_mutex;
@@ -406,8 +408,8 @@ unique_ptr<CSVBufferRead> ParallelCSVGlobalState::Next(ClientContext &context, R
 	if (!current_buffer) {
 		// This means we are done with the current file, we need to go to the next one (if exists).
 		if (file_index < bind_data.files.size()) {
-			bind_data.options.file_path = bind_data.files[file_index++];
-			file_handle = ReadCSV::OpenCSV(bind_data.options, context);
+			current_file_path = bind_data.files[file_index++];
+			file_handle = ReadCSV::OpenCSV(current_file_path, bind_data.options.compression, context);
 			current_csv_position = 0;
 			current_buffer = make_shared<CSVBuffer>(context, buffer_size, *file_handle, current_csv_position);
 			next_buffer = current_buffer->Next(*file_handle, buffer_size, current_csv_position);
@@ -455,7 +457,7 @@ static unique_ptr<GlobalTableFunctionState> ParallelCSVInitGlobal(ClientContext 
 	unique_ptr<CSVFileHandle> file_handle;
 
 	bind_data.options.file_path = bind_data.files[0];
-	file_handle = ReadCSV::OpenCSV(bind_data.options, context);
+	file_handle = ReadCSV::OpenCSV(bind_data.options.file_path, bind_data.options.compression, context);
 	idx_t rows_to_skip =
 	    bind_data.options.skip_rows + (bind_data.options.has_header && bind_data.options.header ? 1 : 0);
 	return make_unique<ParallelCSVGlobalState>(context, std::move(file_handle), bind_data.files,
