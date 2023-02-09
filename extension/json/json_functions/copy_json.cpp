a1e5c84562140dbc3e4325d39cd7d4080e2e424e
Laurens Kuiper <laurens.kuiper@cwi.nl>
2023-02-09 13:37:36 +0100

    date/timestamp formatting for JSON copy FROM and TO

--- extension/json/json_functions/copy_json.cpp
+++ extension/json/json_functions/copy_json.cpp
@@ -4,6 +4,7 @@
 #include "duckdb/function/built_in_functions.hpp"
 #include "duckdb/function/copy_function.hpp"
 #include "duckdb/main/client_context.hpp"
+#include "duckdb/parser/expression/constant_expression.hpp"
 #include "duckdb/parser/expression/function_expression.hpp"
 #include "duckdb/parser/query_node/select_node.hpp"
 #include "duckdb/planner/binder.hpp"
@@ -16,15 +17,44 @@ namespace duckdb {
 static BoundStatement CopyToJSONPlan(Binder &binder, CopyStatement &stmt) {
 	auto stmt_copy = stmt.Copy();
 	auto &copy = (CopyStatement &)*stmt_copy;
-
 	auto &select_stmt = (SelectNode &)*copy.select_statement;
+	auto &info = *copy.info;
+
+	// strftime if the user specified a format TODO: deal with date/timestamp within nested types
+	auto date_it = info.options.find("dateformat");
+	auto timestamp_it = info.options.find("timestampformat");
+
+	// Bind the select statement of the original to resolve the types
+	auto dummy_binder = Binder::CreateBinder(binder.context, &binder, true);
+	auto bound_original = dummy_binder->Bind(*stmt.select_statement);
+	D_ASSERT(bound_original.types.size() == select_stmt.select_list.size());
+	const idx_t num_cols = bound_original.types.size();
+
+	// This loop also makes sure the columns have an alias (needed for struct_pack)
+	vector<unique_ptr<ParsedExpression>> strftime_children;
+	for (idx_t i = 0; i < num_cols; i++) {
+		strftime_children.clear();
+		auto &col = select_stmt.select_list[i];
+		auto name = col->GetName();
+		if (bound_original.types[i] == LogicalTypeId::DATE && date_it != info.options.end()) {
+			strftime_children.emplace_back(std::move(col));
+			strftime_children.emplace_back(make_unique<ConstantExpression>(date_it->second.back()));
+			col = make_unique<FunctionExpression>("strftime", std::move(strftime_children));
+		} else if (bound_original.types[i] == LogicalTypeId::TIMESTAMP && timestamp_it != info.options.end()) {
+			strftime_children.emplace_back(std::move(col));
+			strftime_children.emplace_back(make_unique<ConstantExpression>(timestamp_it->second.back()));
+			col = make_unique<FunctionExpression>("strftime", std::move(strftime_children));
+		}
+		col->alias = name;
+	}
+
+	// Now create the struct_pack/to_json to create a JSON object per row
 	vector<unique_ptr<ParsedExpression>> struct_pack_child;
-	// TODO: strptime for date format! (need to get this to work for inserts too)
 	struct_pack_child.emplace_back(make_unique<FunctionExpression>("struct_pack", std::move(select_stmt.select_list)));
 	select_stmt.select_list.clear();
 	select_stmt.select_list.emplace_back(make_unique<FunctionExpression>("to_json", std::move(struct_pack_child)));
 
-	auto &info = *copy.info;
+	// Now we can just use the CSV writer
 	info.format = "csv";
 	info.options["quote"] = {""};
 	info.options["escape"] = {""};
