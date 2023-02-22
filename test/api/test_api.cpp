6eb23f4864ddb0d551f8d4f96eecb1dc08ed4fa6
Sam Ansmink <samansmink@hotmail.com>
2023-02-22 11:28:01 +0100

    disable Operator Cache for pull

--- test/api/test_api.cpp
+++ test/api/test_api.cpp
@@ -554,3 +554,43 @@ TEST_CASE("Issue #4583: Catch Insert/Update/Delete errors", "[api]") {
 	result = con.SendQuery("SELECT MIN(c0) FROM t0;");
 	REQUIRE(CHECK_COLUMN(result, 0, {1}));
 }
+
+TEST_CASE("Issue #6284: CachingPhysicalOperator in pull causes issues", "[api]") {
+
+	DBConfig config;
+	config.options.maximum_threads = 8;
+	DuckDB db(nullptr, &config);
+	Connection con(db);
+
+	REQUIRE_NO_FAIL(con.Query("select setseed(0.1); CREATE TABLE T0 AS SELECT DISTINCT (RANDOM()*9999999)::BIGINT record_nb, 0.0 x_0, 1.0 y_0 FROM range(1000000) tbl"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE T1 AS SELECT record_nb, 0.0 x_1, 1.0 y_1 FROM T0"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE T2 AS SELECT record_nb, 0.0 x_2, 1.0 y_2 FROM T0"));
+	REQUIRE_NO_FAIL(con.Query("CREATE TABLE T3 AS SELECT record_nb, 0.0 x_3, 1.0 y_3 FROM T0"));
+	auto result = con.SendQuery(R"(
+        SELECT T0.record_nb,
+            T1.x_1 x_1,
+            T1.y_1 y_1,
+            T2.x_2 x_2,
+            T2.y_2 y_2,
+            T3.x_3 x_3,
+            T3.y_3 y_3
+         FROM T0
+           INNER JOIN T1 on T0.record_nb = T1.record_nb
+           INNER JOIN T2 on T0.record_nb = T2.record_nb
+           INNER JOIN T3 on T0.record_nb = T3.record_nb
+    )");
+
+	idx_t count = 0;
+	while(true) {
+		auto chunk = result->Fetch();
+		if (!chunk) {
+			break;
+		}
+		if (chunk->size() == 0) {
+			break;
+		}
+		count += chunk->size();
+	}
+
+	REQUIRE(951446 - count == 0);
+}
