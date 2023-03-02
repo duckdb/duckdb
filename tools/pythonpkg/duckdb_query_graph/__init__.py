3b26951af713b598c151a2b6b2fc7fe489440e6d
Dominik Moritz <domoritz@gmail.com>
2023-03-02 16:54:11 -0500

    Add missing escape for "

--- tools/pythonpkg/duckdb_query_graph/__init__.py
+++ tools/pythonpkg/duckdb_query_graph/__init__.py
@@ -29,7 +29,7 @@ def generate_html(graph_json, include_meta_info):
 		css += f.read() + "\n"
 	css += "</style>"
 
-	graph_json = graph_json.replace('\n', ' ').replace("'", "\\'").replace("\\n", "\\\\n")
+	graph_json = graph_json.replace('\n', ' ').replace("'", "\\'").replace('"', '\\"').replace("\\n", "\\\\n")
 
 	chart_script = """<script>
 var graph_json = '${GRAPH_JSON}';
