17a287e94d4fc9ef45836abdc0c268bf1c5daa80
Tishj <t_b@live.nl>
2023-03-04 12:15:30 +0100

    fix windows issue hopefully?

--- tools/pythonpkg/pyduckdb/filesystem.py
+++ tools/pythonpkg/pyduckdb/filesystem.py
@@ -8,6 +8,15 @@ class ModifiedMemoryFileSystem(MemoryFileSystem):
 	# defer to the original implementation that doesn't hardcode the protocol
 	_strip_protocol = classmethod(AbstractFileSystem._strip_protocol.__func__)
 
+	# Add this manually because it's apparently missing on windows???
+	def unstrip_protocol(self, name):
+		"""Format FS-specific path to generic, including protocol"""
+		protos = (self.protocol,) if isinstance(self.protocol, str) else self.protocol
+		for protocol in protos:
+			if name.startswith(f"{protocol}://"):
+				return name
+		return f"{protos[0]}://{name}"
+
 	def add_file(self, object, filename):
 		if (isinstance(object, io.BytesIO)):
 			with self.open(filename, 'wb') as f:
