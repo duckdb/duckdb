e787d8ce4727882716f08d24f0c6b9cf09b43018
Tishj <t_b@live.nl>
2023-03-06 14:00:55 +0100

    add missing fiel

---
+++ tools/pythonpkg/src/include/duckdb_python/filesystem_object.hpp
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb_python/filesystem_object.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+#include "duckdb_python/registered_py_object.hpp"
+#include "duckdb_python/pyfilesystem.hpp"
+
+namespace duckdb {
+
+class FileSystemObject : public RegisteredObject {
+public:
+	explicit FileSystemObject(py::object fs, const string &filename)
+	    : RegisteredObject(std::move(fs)), filename(filename) {
+	}
+	virtual ~FileSystemObject() {
+		py::gil_scoped_acquire acquire;
+		// Assert that the 'obj' is a filesystem
+		D_ASSERT(py::isinstance<ModifiedMemoryFileSystem>(obj));
+		obj.attr("delete")(filename);
+	}
+
+	string filename;
+};
+
+} // namespace duckdb
