23abec8451a6e639bc5fe3b07d47d514392c9291
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-03-12 10:58:53 +0100

    All nested casts with JSON working

--- src/include/duckdb/common/optional_ptr.hpp
+++ src/include/duckdb/common/optional_ptr.hpp
@@ -1,41 +1,45 @@
 //===----------------------------------------------------------------------===//
- //                         DuckDB
- //
- // duckdb/common/optional_ptr.hpp
- //
- //
- //===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/common/optional_ptr.hpp
+//
+//
+//===----------------------------------------------------------------------===//
 
- #pragma once
+#pragma once
 
- #include "duckdb/common/exception.hpp"
+#include "duckdb/common/exception.hpp"
 
- namespace duckdb {
+namespace duckdb {
 
- template<class T>
- class optional_ptr {
- public:
- 	optional_ptr() : ptr(nullptr) {}
- 	optional_ptr(T *ptr_p) : ptr(ptr_p) {} // NOLINT: allow implicit creation from pointer
+template <class T>
+class optional_ptr {
+public:
+	optional_ptr() : ptr(nullptr) {
+	}
+	optional_ptr(T *ptr_p) : ptr(ptr_p) { // NOLINT: allow implicit creation from pointer
+	}
+	optional_ptr(const unique_ptr<T> &ptr_p) : ptr(ptr_p.get()) { // NOLINT: allow implicit creation from unique pointer
+	}
 
+	operator bool() const {
+		return ptr;
+	}
+	T &operator*() {
+		if (!ptr) {
+			throw InternalException("Attempting to dereference an optional pointer that is not set");
+		}
+		return *ptr;
+	}
+	T *operator->() {
+		if (!ptr) {
+			throw InternalException("Attempting to call a method on an optional pointer that is not set");
+		}
+		return ptr;
+	}
 
-     operator bool() const {
-         return ptr;
-     }
- 	T& operator*() {
- 		if (!ptr) {
- 			throw InternalException("Attempting to dereference an optional pointer of type \"%s\" that is not set", typeid(T).name());
- 		}
-        return *ptr;
- 	}
-     T* operator->() {
- 		if (!ptr) {
- 			throw InternalException("Attempting to call a method on an optional pointer of type \"%s\" that is not set", typeid(T).name());
- 		}
-        return ptr;
-     }
- private:
- 	T *ptr;
- };
+private:
+	T *ptr;
+};
 
- }
+} // namespace duckdb
