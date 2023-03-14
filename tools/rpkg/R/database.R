b6ab35df7d23c0893918e16898bd3f5d2ea7120c
Tom Ebergen <tom@ebergen.com>
2023-03-14 14:21:27 +0100

    can't seem to exit R cleanly with overloaded aggregate function finalize pointer

---
+++ tools/rpkg/R/database.R
@@ -0,0 +1,14 @@
+
+
+
+#' Override the finalize call of the duckdb sum function so that
+#' if the result is not set, set it to 0
+#' @param conn A DuckDB connection, created by `dbConnect()`.
+#' @return a boolean if the overwrite was successful or not
+#' @noRd
+#' @examples
+#' conn <- dbConnect(duckdb::duckdb())
+#' duckdb_set_sum_default_to_zero(conn)
+duckdb_set_sum_default_to_zero <- function(conn) {
+  rapi_set_sum_default_to_zero(conn@conn_ref)
+}
