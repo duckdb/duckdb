02d302765b341a6ba0603bb236f49d8281344a61
Tishj <t_b@live.nl>
2023-03-23 14:24:08 +0100

    clean old attempts

--- tools/pythonpkg/src/include/duckdb_python/conversions/pyconnection_default.hpp
+++ tools/pythonpkg/src/include/duckdb_python/conversions/pyconnection_default.hpp
@@ -41,38 +41,3 @@ struct is_holder_type<DuckDBPyConnection, std::shared_ptr<DuckDBPyConnection>> :
 
 } // namespace detail
 } // namespace PYBIND11_NAMESPACE
-
-// namespace pybind11 {
-// namespace detail {
-
-// template<>
-// struct type_caster<shared_ptr<DuckDBPyConnection>>
-//{
-//	using type = DuckDBPyConnection;
-//	PYBIND11_TYPE_CASTER(shared_ptr<type>, const_name("default_connection_holder"));
-
-//	using BaseCaster = copyable_holder_caster<type, shared_ptr<type>>;
-
-//	bool load(pybind11::handle src, bool b)
-//	{
-//		if (py::none().is(src)) {
-//			value = DuckDBPyConnection::DefaultConnection();
-//			return true;
-//		}
-//		BaseCaster bc;
-
-//		return bc.load(src, b);
-//	}
-
-//	static handle cast(shared_ptr<type> base,
-//					   return_value_policy rvp,
-//					   handle h)
-//	{
-//		return BaseCaster::cast(base, rvp, h);
-//	}
-//};
-
-// template <>
-// struct is_holder_type<DuckDBPyConnection, std::shared_ptr<DuckDBPyConnection>> : std::true_type
-//{
-//};
