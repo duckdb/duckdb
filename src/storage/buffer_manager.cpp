b1caf7619e9594afd725e7024a8188f65406937a
Tishj <t_b@live.nl>
2023-04-04 14:56:35 +0200

    move buffer_pool_reservation code out of buffer_manager.cpp

--- src/storage/buffer_manager.cpp
+++ src/storage/buffer_manager.cpp
@@ -10,33 +10,6 @@
 
 namespace duckdb {
 
-BufferPoolReservation::BufferPoolReservation(BufferPoolReservation &&src) noexcept {
-	size = src.size;
-	src.size = 0;
-}
-
-BufferPoolReservation &BufferPoolReservation::operator=(BufferPoolReservation &&src) noexcept {
-	size = src.size;
-	src.size = 0;
-	return *this;
-}
-
-BufferPoolReservation::~BufferPoolReservation() {
-	D_ASSERT(size == 0);
-}
-
-void BufferPoolReservation::Resize(atomic<idx_t> &counter, idx_t new_size) {
-	int64_t delta = (int64_t)new_size - size;
-	D_ASSERT(delta > 0 || (int64_t)counter >= -delta);
-	counter += delta;
-	size = new_size;
-}
-
-void BufferPoolReservation::Merge(BufferPoolReservation &&src) {
-	size += src.size;
-	src.size = 0;
-}
-
 struct BufferAllocatorData : PrivateAllocatorData {
 	explicit BufferAllocatorData(BufferManager &manager) : manager(manager) {
 	}
