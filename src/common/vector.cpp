7dd7585b9559292892c6d6121c3146c29d278e4f
Tishj <t_b@live.nl>
2023-04-04 17:08:02 +0200

    added missing parameters to exception

--- src/common/vector.cpp
+++ src/common/vector.cpp
@@ -6,7 +6,7 @@ void AssertIndexInBounds(idx_t index, idx_t size) {
 	if (index < size) {
 		return;
 	}
-	throw InternalException("Attempted to access index %ld within vector of size %ld");
+	throw InternalException("Attempted to access index %ld within vector of size %ld", index, size);
 }
 
 } // namespace duckdb
