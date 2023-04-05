790a7ed7aa21a9ff3d03cf2805e3c5a11ecc488a
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-04-05 10:53:51 +0200

    Wasm loadable extensions: Trigger static_assert on usage of ExtensionDirectory

--- src/main/extension/extension_install.cpp
+++ src/main/extension/extension_install.cpp
@@ -39,6 +39,9 @@ const vector<string> ExtensionHelper::PathComponents() {
 }
 
 string ExtensionHelper::ExtensionDirectory(DBConfig &config, FileSystem &fs, FileOpener *opener) {
+#ifdef WASM_LOADABLE_EXTENSIONS
+	static_assertion(0, "ExtensionDirectory functionality is not supported in duckdb-wasm");
+#endif
 	string extension_directory;
 	if (!config.options.extension_directory.empty()) { // create the extension directory if not present
 		extension_directory = config.options.extension_directory;
