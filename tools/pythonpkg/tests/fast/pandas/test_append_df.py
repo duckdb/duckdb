b9930c9d882b0919da0cf05f100b6c710c1ea1de
Tishj <t_b@live.nl>
2023-04-07 17:05:22 +0200

    add ArrowPandas to a couple more tests

--- tools/pythonpkg/tests/fast/pandas/test_append_df.py
+++ tools/pythonpkg/tests/fast/pandas/test_append_df.py
@@ -1,11 +1,13 @@
 import duckdb
-import pandas as pd
+import pytest
+from conftest import NumpyPandas, ArrowPandas
 
 class TestAppendDF(object):
 
-    def test_df_to_table_append(self, duckdb_cursor):
+    @pytest.mark.parametrize('pandas', [NumpyPandas(), ArrowPandas()])
+    def test_df_to_table_append(self, duckdb_cursor, pandas):
         conn = duckdb.connect()
         conn.execute("Create table integers (i integer)")
-        df_in = pd.DataFrame({'numbers': [1,2,3,4,5],})
+        df_in = pandas.DataFrame({'numbers': [1,2,3,4,5],})
         conn.append('integers',df_in)
         assert conn.execute('select count(*) from integers').fetchone()[0] == 5
