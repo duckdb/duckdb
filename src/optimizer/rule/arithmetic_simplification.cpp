24f89894ad90c1c2fcd056225799e57466568a85
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-04-08 15:31:17 +0200

    Rework division operator to be identical to python's - / is floating point division, // is integer division

--- src/optimizer/rule/arithmetic_simplification.cpp
+++ src/optimizer/rule/arithmetic_simplification.cpp
@@ -14,7 +14,7 @@ ArithmeticSimplificationRule::ArithmeticSimplificationRule(ExpressionRewriter &r
 	op->matchers.push_back(make_uniq<ExpressionMatcher>());
 	op->policy = SetMatcher::Policy::SOME;
 	// we only match on simple arithmetic expressions (+, -, *, /)
-	op->function = make_uniq<ManyFunctionMatcher>(unordered_set<string> {"+", "-", "*", "/"});
+	op->function = make_uniq<ManyFunctionMatcher>(unordered_set<string> {"+", "-", "*", "//"});
 	// and only with numeric results
 	op->type = make_uniq<IntegerTypeMatcher>();
 	op->matchers[0]->type = make_uniq<IntegerTypeMatcher>();
