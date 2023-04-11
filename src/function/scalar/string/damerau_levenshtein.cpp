003d88e5359e37e1a49adb414fcd6013fd5601ad
ADBond <48208438+ADBond@users.noreply.github.com>
2023-04-11 17:31:57 +0100

    rename vars, some comments, tidy

--- src/function/scalar/string/damerau_levenshtein.cpp
+++ src/function/scalar/string/damerau_levenshtein.cpp
@@ -1,9 +1,5 @@
-#include "duckdb/common/string_util.hpp"
-#include "duckdb/common/vector_operations/vector_operations.hpp"
 #include "duckdb/function/scalar/string_functions.hpp"
 
-#include <algorithm>
-#include <ctype.h>
 #include <map>
 
 namespace duckdb {
@@ -39,57 +35,65 @@ static idx_t DamerauLevenshteinDistance(const string_t &source, const string_t &
 	std::vector<std::vector<idx_t>> distance(source_len + 2, std::vector<idx_t>(target_len + 2, inf));
 	// keeps track of the largest string indices of source string matching each character
 	// same as DA in LW paper
-	std::map<char, idx_t> largest_source_index_matching;
+	std::map<char, idx_t> largest_source_chr_matching;
 
+	// initialise row/column corresponding to zero-length strings
+	// partial string -> empty requires a deletion for each character
 	for (idx_t source_idx = 0; source_idx <= source_len; source_idx++) {
 		distance[source_idx + 1][1] = source_idx * cost_deletion;
 	}
+	// and empty -> partial string means simply inserting characters
 	for (idx_t target_idx = 1; target_idx <= target_len; target_idx++) {
 		distance[1][target_idx + 1] = target_idx * cost_insertion;
 	}
+	// loop through string indices - these are offset by 2 from distance indices
 	for (idx_t source_idx = 0; source_idx < source_len; source_idx++) {
 		// keeps track of the largest string indices of target string matching current source character
 		// same as DB in LW paper
-		idx_t largest_target_index_matching;
-		largest_target_index_matching = 0;
+		idx_t largest_target_chr_matching;
+		largest_target_chr_matching = 0;
 		for (idx_t target_idx = 0; target_idx < target_len; target_idx++) {
-			idx_t ii, jj;
-			// cost associated to diagnoal shift in distance matrix
-			// d in LW paper
+			// correspond to i1 and j1 in LW paper respectively
+			idx_t largest_source_chr_matching_target;
+			idx_t largest_target_chr_matching_source;
+			// cost associated to diagnanl shift in distance matrix
+			// corresponds to d in LW paper
 			uint8_t cost_diagonal_shift;
-			ii = largest_source_index_matching[target_str[target_idx]];
-			jj = largest_target_index_matching;
+			largest_source_chr_matching_target = largest_source_chr_matching[target_str[target_idx]];
+			largest_target_chr_matching_source = largest_target_chr_matching;
 			// if characters match, diagonal move costs nothing and we update our largest target index
 			// otherwise move is substitution and costs as such
 			if (source_str[source_idx] == target_str[target_idx]) {
 				cost_diagonal_shift = 0;
-				largest_target_index_matching = target_idx + 1;
+				largest_target_chr_matching = target_idx + 1;
 			} else {
 				cost_diagonal_shift = cost_substitution;
 			}
-			distance[source_idx + 2][target_idx + 2] =
-			    MinValue(distance[source_idx + 1][target_idx + 1] + cost_diagonal_shift,
-			             MinValue(distance[source_idx + 2][target_idx + 1] + cost_insertion,
-			                      MinValue(distance[source_idx + 1][target_idx + 2] + cost_deletion,
-			                               distance[ii][jj] + (source_idx - ii) * cost_deletion + cost_transposition +
-			                                   (target_idx - jj) * cost_insertion)));
+			distance[source_idx + 2][target_idx + 2] = MinValue(
+			    distance[source_idx + 1][target_idx + 1] + cost_diagonal_shift,
+			    MinValue(distance[source_idx + 2][target_idx + 1] + cost_insertion,
+			             MinValue(distance[source_idx + 1][target_idx + 2] + cost_deletion,
+			                      distance[largest_source_chr_matching_target][largest_target_chr_matching_source] +
+			                          (source_idx - largest_source_chr_matching_target) * cost_deletion +
+			                          cost_transposition +
+			                          (target_idx - largest_target_chr_matching_source) * cost_insertion)));
 		}
-		largest_source_index_matching[source_str[source_idx]] = source_idx + 1;
+		largest_source_chr_matching[source_str[source_idx]] = source_idx + 1;
 	}
 	return distance[source_len + 1][target_len + 1];
 }
 
-static int64_t DamerauLevenshteinScalarFunction(Vector &result, const string_t str, const string_t tgt) {
-	return (int64_t)DamerauLevenshteinDistance(str, tgt);
+static int64_t DamerauLevenshteinScalarFunction(Vector &result, const string_t source, const string_t target) {
+	return (int64_t)DamerauLevenshteinDistance(source, target);
 }
 
 static void DamerauLevenshteinFunction(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &str_vec = args.data[0];
-	auto &tgt_vec = args.data[1];
+	auto &source_vec = args.data[0];
+	auto &target_vec = args.data[1];
 
 	BinaryExecutor::Execute<string_t, string_t, int64_t>(
-	    str_vec, tgt_vec, result, args.size(),
-	    [&](string_t str, string_t tgt) { return DamerauLevenshteinScalarFunction(result, str, tgt); });
+	    source_vec, target_vec, result, args.size(),
+	    [&](string_t source, string_t target) { return DamerauLevenshteinScalarFunction(result, source, target); });
 }
 
 void DamerauLevenshteinFun::RegisterFunction(BuiltinFunctions &set) {
