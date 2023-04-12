77c1cb1b59666f6d457b83d3f4defb1fb7111cb5
DouEnergy <douenergy@protonmail.com>
2023-04-13 01:11:04 +0800

    array_reverse function

--- src/catalog/default/default_functions.cpp
+++ src/catalog/default/default_functions.cpp
@@ -91,6 +91,8 @@ static DefaultMacro internal_macros[] = {
 	{DEFAULT_SCHEMA, "array_push_back", {"arr", "e", nullptr}, "list_concat(arr, list_value(e))"},
 	{DEFAULT_SCHEMA, "array_push_front", {"arr", "e", nullptr}, "list_concat(list_value(e), arr)"},
 	{DEFAULT_SCHEMA, "generate_subscripts", {"arr", "dim", nullptr}, "unnest(generate_series(1, array_length(arr, dim)))"},
+	{DEFAULT_SCHEMA, "array_reverse", {"arr", nullptr},"( SELECT coalesce(list(elm), arr) FROM ( SELECT generate_subscripts(arr, 1) as g, arr[g] as elm ORDER BY g DESC) )"},
+	{DEFAULT_SCHEMA, "list_reverse", {"arr", nullptr},"array_reverse(arr)"},
 	{DEFAULT_SCHEMA, "fdiv", {"x", "y", nullptr}, "floor(x/y)"},
 	{DEFAULT_SCHEMA, "fmod", {"x", "y", nullptr}, "(x-y*floor(x/y))"},
 	{DEFAULT_SCHEMA, "count_if", {"l", nullptr}, "sum(if(l, 1, 0))"},
