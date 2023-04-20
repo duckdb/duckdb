217c707b0853d6d0ab1e9355fde8fcf8e3befdd2
Sam Arch <samarch@Sams-MacBook-Pro-5.local>
2023-04-20 09:29:33 -0400

    Added scratchpad

--- scratchpad.sql
+++ scratchpad.sql
@@ -14,23 +14,126 @@ SELECT *
             FROM (SELECT 142 k) t3(k), 
                  (SELECT k) t4(l)) t2(j);
 
-Bind SELECT_STMT;
-Bind FROM_CLAUSE;
-Bind JoinRef
-Bind Left Side of JoinRef
-Bind TableRef
+-- Bind SELECT_STMT;
+-- Bind FROM_CLAUSE;
+-- Bind JoinRef
+-- Bind Left Side of JoinRef
+-- Bind TableRef
 
-LateralBinder :: ExpressionBinder
+-- LateralBinder :: ExpressionBinder
 
-Bind Right Side of JoinRef
+-- Bind Right Side of JoinRef
 
 
 SELECT * FROM (SELECT 42) t(i)
-WHERE i IN (SELECT * FROM (SELECT 42 k) t3(k)
+WHERE i IN (SELECT l FROM (SELECT 42) t(l) ,
+                          (SELECT i * 2) t2(j),
+                          (SELECT i + j) t3(k)
             WHERE k IN (SELECT * FROM (SELECT 42 l) t4(l) WHERE i-k = 0));
 
+
+-- Apr 4th '23 (2023-04-01): SQL Commands Test on sambinder: --
+
+-- FAIL --
 SELECT *
-    FROM (SELECT 42) t(i),           
-         (SELECT *              
-            FROM (SELECT 142 k) t3(k),           
-                 (SELECT 1 WHERE i+k=0) t4(l));
+     FROM (SELECT 42) t(i),           
+          (SELECT *              
+               FROM (SELECT 142 k) t3(k),           
+                    (SELECT 1 WHERE i+k=0) t4(l));
+
+-- SUCCESS --
+SELECT * 
+     FROM (SELECT 42) t1(i), 
+          (SELECT 22) t2(j), 
+          (SELECT 1 WHERE i+j=64) t3(l);
+
+-- SUCCESS --
+SELECT * 
+     FROM (SELECT 42) t1(i), 
+          (SELECT 22) t2(j), 
+          (SELECT 1 WHERE i=64) t3(l);
+
+-- SUCCESS --
+SELECT * 
+     FROM (SELECT 42) t1(i), 
+          (SELECT 22) t2(j), 
+          (SELECT i+j WHERE i+j=64) t3(l);
+
+-- SUCCESS --
+SELECT * 
+     FROM (SELECT 42) t(i), 
+          (SELECT i * 2) t2(j),    
+          (SELECT i + j) t3(k);
+
+-- SUCCESS --
+SELECT * 
+     FROM (SELECT i,j,k 
+          FROM (SELECT 42) t(i), 
+               (SELECT i * 2) t2(j),
+               (SELECT i + j) t3(k));
+
+-- SUCCESS --
+EXPLAIN SELECT * 
+     FROM (SELECT i,j,k 
+          FROM (SELECT 42) t(i), 
+               (SELECT i * 2) t2(j), 
+               (SELECT i + j) t3(k));
+
+-- FAIL --
+SELECT * 
+     FROM (SELECT 42) t(i), 
+          (SELECT * 
+               FROM (SELECT 142 k) t3(k), 
+                    (SELECT 1 WHERE i+k=0) t4(l));
+-- SUCCESS --
+SELECT * 
+     FROM (SELECT * 
+               FROM (SELECT 42) t1(i), 
+                    (SELECT 22) t2(j), 
+                    (SELECT 1 WHERE i+j=64) t3(l));
+
+-- FAIL --
+SELECT * 
+     FROM (SELECT * 
+          FROM (SELECT 42) t(i), 
+               (SELECT * 
+                    FROM (SELECT 142 k) t3(k), 
+                         (SELECT 1 WHERE i+k=0) t4(l)));
+
+SELECT *
+FROM (SELECT *
+      FROM (SELECT 42) t(i),
+           (SELECT *
+            FROM (SELECT 142 k) t3(k),
+                 (SELECT 1 WHERE i=0) t4(l)));
+
+
+SELECT *
+FROM (SELECT 42) t(i),
+     (SELECT *
+      FROM (SELECT 142 k) t3(k),
+           (SELECT *
+               FROM (SELECT 242 l) t4(l),
+                    (SELECT 1 WHERE i+l+k=0) t5(m)));
+
+
+SELECT *
+    FROM (SELECT 42) t(i)
+    WHERE i IN (SELECT l
+                FROM (SELECT 42) t(l) ,
+                     (SELECT i * 2) t2(j),
+                     (SELECT i + j) t3(k)
+                WHERE k IN (SELECT l
+                                  FROM (SELECT 42 l) t4(l),
+                                       (SELECT l+5) t5(n)
+                                  WHERE i-k IN (SELECT * FROM (SELECT i+5))
+            ));
+
+SELECT *
+FROM (SELECT 42) t(i)
+WHERE i IN (SELECT k
+            FROM (SELECT 42) t(k)
+            WHERE k IN (SELECT l
+                        FROM (SELECT 42 l) t4(l)
+                        WHERE i-k IN (SELECT * FROM (SELECT i+5))
+            ));
