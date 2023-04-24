7a8263b19e2fbc6784ffbb1ff1dc294daa55347e
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-04-24 16:27:22 +0800

    Create CStackObject.h

---
+++ src/include/duckdb/optimizer/cascade/common/CStackObject.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		CStackObject.h
+//
+//	@doc:
+//		Class of all objects that must reside on the stack;
+//		e.g., auto objects;
+//---------------------------------------------------------------------------
+#ifndef GPOS_CStackObject_H
+#define GPOS_CStackObject_H
+
+#include "gpos/types.h"
+
+namespace gpos
+{
+//---------------------------------------------------------------------------
+//	@class:
+//		CStackObject
+//
+//	@doc:
+//		Constructor tests stack layout to ensure object is allocated on stack;
+//		constructor is protected to prevent direct instantiation of class;
+//
+//---------------------------------------------------------------------------
+class CStackObject
+{
+protected:
+	CStackObject();
+};
+}  // namespace gpos
+
+#endif
