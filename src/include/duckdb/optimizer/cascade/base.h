614bd427016909d9498f1f1c8d8050c0a85cb23e
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-04-24 15:31:13 +0800

    Create base.h

---
+++ src/include/duckdb/optimizer/cascade/base.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		base.h
+//
+//	@doc:
+//		Collection of commonly used OS abstraction primitives;
+//		Most files should be fine including only this one from the GPOS folder;
+//---------------------------------------------------------------------------
+#ifndef GPOS_base_H
+#define GPOS_base_H
+
+#include "gpos/assert.h"
+#include "gpos/error/CErrorHandler.h"
+#include "gpos/error/CException.h"
+#include "gpos/error/IErrorContext.h"
+#include "gpos/error/ILogger.h"
+#include "gpos/memory/CMemoryPool.h"
+#include "gpos/task/ITask.h"
+#include "gpos/types.h"
+
+#endif
