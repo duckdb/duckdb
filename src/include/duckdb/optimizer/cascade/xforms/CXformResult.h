258705a4e9295277b7d7ea3826222eb41192a763
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-04-25 21:52:09 +0800

    Create CXformResult.h

---
+++ src/include/duckdb/optimizer/cascade/xforms/CXformResult.h
@@ -0,0 +1,73 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		CXformResult.h
+//
+//	@doc:
+//		Result container for all transformations
+//---------------------------------------------------------------------------
+#ifndef GPOPT_CXformResult_H
+#define GPOPT_CXformResult_H
+
+#include "duckdb/optimizer/cascade/base.h"
+
+#include "duckdb/optimizer/cascade/operators/CExpression.h"
+
+namespace gpopt
+{
+using namespace gpos;
+
+//---------------------------------------------------------------------------
+//	@class:
+//		CXformResult
+//
+//	@doc:
+//		result container
+//
+//---------------------------------------------------------------------------
+class CXformResult : public CRefCount
+{
+private:
+	// set of alternatives
+	CExpressionArray *m_pdrgpexpr;
+
+	// cursor for retrieval
+	ULONG m_ulExpr;
+
+	// private copy ctor
+	CXformResult(const CXformResult &);
+
+public:
+	// ctor
+	explicit CXformResult(CMemoryPool *);
+
+	// dtor
+	~CXformResult();
+
+	// accessor
+	inline CExpressionArray *
+	Pdrgpexpr() const
+	{
+		return m_pdrgpexpr;
+	}
+
+	// add alternative
+	void Add(CExpression *pexpr);
+
+	// retrieve next alternative
+	CExpression *PexprNext();
+
+	// print function
+	virtual IOstream &OsPrint(IOstream &os) const;
+
+};	// class CXformResult
+
+// shorthand for printing
+inline IOstream &
+operator<<(IOstream &os, CXformResult &xfres)
+{
+	return xfres.OsPrint(os);
+}
+
+}  // namespace gpopt
+
+#endif
