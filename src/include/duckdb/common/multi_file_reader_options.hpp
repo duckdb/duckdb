56109bcd4d3ab0cce0b66d5405d103c0da00cbf4
Lars Verdoes <lars@MacBook-Pro.localdomain>
2023-04-26 15:47:46 +0200

    use regex for detection

--- src/include/duckdb/common/multi_file_reader_options.hpp
+++ src/include/duckdb/common/multi_file_reader_options.hpp
@@ -27,66 +27,66 @@ struct MultiFileReaderOptions {
 	DUCKDB_API static MultiFileReaderOptions Deserialize(Deserializer &source);
 	DUCKDB_API void AddBatchInfo(BindInfo &bind_info) const;
 
-	// static bool AutoDetectHivePartitioning(const vector<string> &files) {
-
-	// 	if (files.empty()) {
-	// 		return false;
-	// 	}
-	// 	duckdb_re2::RE2 regex("[\\/\\\\]([^\\/\\?\\\\]+)=([^\\/\\n\\?\\\\]+)");
-	// 	const auto partitions = HivePartitioning::Parse(files.front(), regex);
-	// 	for (auto &f : files) {
-	// 		auto scheme = HivePartitioning::Parse(f, regex);
-	// 		if (scheme.size() != partitions.size()) {
-	// 			return false;
-	// 		}
-	// 		for (auto &i : scheme) {
-	// 			if (partitions.find(i.first) == partitions.end()) {
-	// 				return false;
-	// 			}
-	// 		}
-	// 	}
-	// 	return true;
-	// }
-
 	static bool AutoDetectHivePartitioning(const vector<string> &files) {
+
 		if (files.empty()) {
 			return false;
 		}
-		std::unordered_set<string> uset;
-		idx_t splits_size;
-		{
-			//	front file
-			auto splits = StringUtil::Split(files.front(), "/");
-			splits_size = splits.size();
-			if (splits.size() < 2) {
+		duckdb_re2::RE2 regex("[\\/\\\\]([^\\/\\?\\\\]+)=([^\\/\\n\\?\\\\]+)");
+		const auto partitions = HivePartitioning::Parse(files.front(), regex);
+		for (auto &f : files) {
+			auto scheme = HivePartitioning::Parse(f, regex);
+			if (scheme.size() != partitions.size()) {
 				return false;
 			}
-			for (auto it = splits.begin(); it != std::prev(splits.end()); it++) {
-				auto part = StringUtil::Split(*it, "=");
-				if (part.size() == 2) {
-					uset.insert(part.front());
-				}
-			}
-		}
-		if (uset.empty()) {
-			return false;
-		}
-		for (auto &file : files) {
-			auto splits = StringUtil::Split(file, "/");
-			if (splits.size() != splits_size) {
-				return false;
-			}
-			for (auto it = splits.begin(); it != std::prev(splits.end()); it++) {
-				auto part = StringUtil::Split(*it, "=");
-				if (part.size() == 2) {
-					if (uset.find(part.front()) == uset.end()) {
-						return false;
-					}
+			for (auto &i : scheme) {
+				if (partitions.find(i.first) == partitions.end()) {
+					return false;
 				}
 			}
 		}
 		return true;
 	}
+
+	// static bool AutoDetectHivePartitioning(const vector<string> &files) {
+	// 	if (files.empty()) {
+	// 		return false;
+	// 	}
+	// 	std::unordered_set<string> uset;
+	// 	idx_t splits_size;
+	// 	{
+	// 		//	front file
+	// 		auto splits = StringUtil::Split(files.front(), "/");
+	// 		splits_size = splits.size();
+	// 		if (splits.size() < 2) {
+	// 			return false;
+	// 		}
+	// 		for (auto it = splits.begin(); it != std::prev(splits.end()); it++) {
+	// 			auto part = StringUtil::Split(*it, "=");
+	// 			if (part.size() == 2) {
+	// 				uset.insert(part.front());
+	// 			}
+	// 		}
+	// 	}
+	// 	if (uset.empty()) {
+	// 		return false;
+	// 	}
+	// 	for (auto &file : files) {
+	// 		auto splits = StringUtil::Split(file, "/");
+	// 		if (splits.size() != splits_size) {
+	// 			return false;
+	// 		}
+	// 		for (auto it = splits.begin(); it != std::prev(splits.end()); it++) {
+	// 			auto part = StringUtil::Split(*it, "=");
+	// 			if (part.size() == 2) {
+	// 				if (uset.find(part.front()) == uset.end()) {
+	// 					return false;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	return true;
+	// }
 };
 
 } // namespace duckdb
