7d2a05aebfe268480bb159c7c141f48595ae525f
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-04-27 15:52:34 +0800

    Add files via upload

---
+++ src/include/duckdb/optimizer/cascade/error/CErrorHandler.h
@@ -0,0 +1,51 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		CErrorHandler.h
+//
+//	@doc:
+//		Error handler base class;
+//---------------------------------------------------------------------------
+#ifndef GPOS_CErrorHandler_H
+#define GPOS_CErrorHandler_H
+
+#include "duckdb/optimizer/cascade/assert.h"
+#include "duckdb/optimizer/cascade/error/CException.h"
+#include "duckdb/optimizer/cascade/types.h"
+
+namespace gpos
+{
+// fwd declarations
+class CMemoryPool;
+
+//---------------------------------------------------------------------------
+//	@class:
+//		CErrorHandler
+//
+//	@doc:
+//		Error handler to be installed inside a worker;
+//
+//---------------------------------------------------------------------------
+class CErrorHandler
+{
+private:
+	// private copy ctor
+	CErrorHandler(const CErrorHandler &);
+
+public:
+	// ctor
+	CErrorHandler()
+	{
+	}
+
+	// dtor
+	virtual ~CErrorHandler()
+	{
+	}
+
+	// process error
+	virtual void Process(CException exception) = 0;
+
+};	// class CErrorHandler
+}  // namespace gpos
+
+#endif
