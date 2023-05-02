937b1caa4dee5e24a88e14edf19af194880c6c31
Dylan Scott <dscott@hex.tech>
2023-05-02 14:53:06 -0700

    refactor to use UnaryTimestampFunction

--- extension/icu/icu-datepart.cpp
+++ extension/icu/icu-datepart.cpp
@@ -254,7 +254,7 @@ struct ICUDatePart : public ICUDateFunc {
 				                                                         return info.adapters[0](calendar, micros);
 			                                                         } else {
 				                                                         mask.SetInvalid(idx);
-				                                                         return RESULT_TYPE(0);
+				                                                         return RESULT_TYPE();
 			                                                         }
 		                                                         });
 	}
@@ -361,29 +361,6 @@ struct ICUDatePart : public ICUDateFunc {
 		}
 
 		result.Verify(count);
-	}
-
-	template <typename INPUT_TYPE>
-	static void NameFunction(DataChunk &args, ExpressionState &state, Vector &result) {
-		using bind_t = BindAdapterData<string_t>;
-		D_ASSERT(args.ColumnCount() == 1);
-		auto &date_arg = args.data[0];
-
-		auto &func_expr = (BoundFunctionExpression &)state.expr;
-		auto &info = (bind_t &)*func_expr.bind_info;
-		CalendarPtr calendar_ptr(info.calendar->clone());
-		auto calendar = calendar_ptr.get();
-
-		UnaryExecutor::ExecuteWithNulls<INPUT_TYPE, string_t>(date_arg, result, args.size(),
-		                                                      [&](INPUT_TYPE input, ValidityMask &mask, idx_t idx) {
-			                                                      if (Timestamp::IsFinite(input)) {
-				                                                      const auto micros = SetTime(calendar, input);
-				                                                      return info.adapters[0](calendar, micros);
-			                                                      } else {
-				                                                      mask.SetInvalid(idx);
-				                                                      return string_t();
-			                                                      }
-		                                                      });
 	}
 
 	template <typename BIND_TYPE>
@@ -522,7 +499,8 @@ struct ICUDatePart : public ICUDateFunc {
 
 	template <typename INPUT_TYPE>
 	static ScalarFunction GetMonthNameFunction(const LogicalType &temporal_type) {
-		return ScalarFunction({temporal_type}, LogicalType::VARCHAR, NameFunction<INPUT_TYPE>, BindMonthName);
+		return ScalarFunction({temporal_type}, LogicalType::VARCHAR, UnaryTimestampFunction<INPUT_TYPE, string_t>,
+		                      BindMonthName);
 	}
 	static void AddMonthNameFunctions(const string &name, ClientContext &context) {
 		auto &catalog = Catalog::GetSystemCatalog(context);
@@ -540,7 +518,8 @@ struct ICUDatePart : public ICUDateFunc {
 
 	template <typename INPUT_TYPE>
 	static ScalarFunction GetDayNameFunction(const LogicalType &temporal_type) {
-		return ScalarFunction({temporal_type}, LogicalType::VARCHAR, NameFunction<INPUT_TYPE>, BindDayName);
+		return ScalarFunction({temporal_type}, LogicalType::VARCHAR, UnaryTimestampFunction<INPUT_TYPE, string_t>,
+		                      BindDayName);
 	}
 	static void AddDayNameFunctions(const string &name, ClientContext &context) {
 		auto &catalog = Catalog::GetSystemCatalog(context);
