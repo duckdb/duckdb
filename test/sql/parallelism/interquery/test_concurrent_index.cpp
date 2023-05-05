e44a81fe78595ddc2d51dd607681a00e875125dc
Tania Bogatsch <44262898+taniabogatsch@users.noreply.github.com>
2023-05-05 16:39:20 +0200

    enable tests again, close #6189

--- test/sql/parallelism/interquery/test_concurrent_index.cpp
+++ test/sql/parallelism/interquery/test_concurrent_index.cpp
@@ -81,8 +81,7 @@ static void append_to_integers(DuckDB *db, idx_t threadnr) {
 }
 
 TEST_CASE("Concurrent writes during index creation", "[index][.]") {
-	// FIXME: this breaks sporadically on CI
-	return;
+
 	duckdb::unique_ptr<QueryResult> result;
 	DuckDB db(nullptr);
 	Connection con(db);
@@ -355,8 +354,7 @@ static void join_integers(Connection *con, bool *index_join_success, idx_t threa
 }
 
 TEST_CASE("Concurrent appends during index join", "[interquery][.]") {
-	// FIXME: this test occassionally fails in the CI, likely due to a race condition in the index code
-	return;
+
 	duckdb::unique_ptr<QueryResult> result;
 	DuckDB db(nullptr);
 	Connection con(db);
