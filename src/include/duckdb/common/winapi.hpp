367595af51b459d07121647bc2de1beabc836e0c
Hannes Mühleisen <hannes@muehleisen.org>
2023-05-08 15:27:42 +0200

    so no declspec

--- src/include/duckdb/common/winapi.hpp
+++ src/include/duckdb/common/winapi.hpp
@@ -9,7 +9,7 @@
 #pragma once
 
 #ifndef DUCKDB_API
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(__MINGW32__)
 #if defined(DUCKDB_BUILD_LIBRARY) && !defined(DUCKDB_BUILD_LOADABLE_EXTENSION)
 #define DUCKDB_API __declspec(dllexport)
 #else
@@ -21,7 +21,7 @@
 #endif
 
 #ifndef DUCKDB_EXTENSION_API
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(__MINGW32__)
 #ifdef DUCKDB_BUILD_LOADABLE_EXTENSION
 #define DUCKDB_EXTENSION_API __declspec(dllexport)
 #else
