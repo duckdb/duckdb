94828f01c2a5805e391b4cf2b8cbca83999fe43c
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-05-14 09:27:31 +0200

    Add encoding tests

--- tools/pythonpkg/tests/fast/test_windows_abs_path.py
+++ tools/pythonpkg/tests/fast/test_windows_abs_path.py
@@ -3,6 +3,33 @@ import pytest
 import os
 
 class TestWindowsAbsPath(object):
+    def test_windows_path_accent(self):
+        if os.name != 'nt':
+            return
+        current_directory = os.getcwd()
+        test_dir = os.path.join(current_directory, 'tést')
+        dbpath = os.path.join(test_dir, 'test.db')
+        try:
+            os.mkdir(test_dir)
+        except FileExistsError:
+            pass
+        con = duckdb.connect(dbpath)
+        con.execute("CREATE OR REPLACE TABLE int AS SELECT * FROM range(10) t(i)")
+        res = con.execute("SELECT COUNT(*) FROM int").fetchall()
+        assert res[0][0] == 10
+        del res
+        del con
+
+        os.chdir('tést')
+        dbpath = os.path.join('..',  dbpath)
+        con = duckdb.connect(dbpath)
+        res = con.execute("SELECT COUNT(*) FROM int").fetchall()
+        assert res[0][0] == 10
+        del res
+        del con
+
+        os.chdir('..')
+
     def test_windows_abs_path(self):
         if os.name != 'nt':
             return
