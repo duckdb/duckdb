5aa2a32b8fac81ce8c470ed4e3e00dbb065fd062
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-05-16 19:20:22 +0800

    Add files via upload

--- src/optimizer/cascade/Cascade.cpp
+++ src/optimizer/cascade/Cascade.cpp
@@ -2,38 +2,43 @@
 
 #include "duckdb/optimizer/cascade/search/CSearchStage.h"
 #include "duckdb/optimizer/cascade/memory/CAutoMemoryPool.h"
+#include "duckdb/optimizer/cascade/base/CQueryContext.h"
+#include "duckdb/optimizer/cascade/engine/CEngine.h"
 
 #define AUTO_MEM_POOL(amp) CAutoMemoryPool amp(CAutoMemoryPool::ElcExc)
 
-Cascade::Optimize(unique_ptr<LogialOperator> plan) {
-	AUTO_MEM_POOL(amp);
-	CMemoryPool *mp = amp.Pmp();
-	CSearchStageArray *search_strategy_arr = LoadSearchStrategy(mp, optimizer_search_strategy_path);
-	
-	// pdrgpul: the array of query output column reference id
-	// pdrgpmdname: the array of output column names
-	CQueryContext *pqc = CQueryContext::PqcGenerate(mp, plan, pdrgpul, pdrgpmdname, true);
-	
-	CEngine eng(mp);
-	eng.Init(pqc, search_stage_array);
-	eng.Optimize();
+namespace duckdb {
+	gpopt::CExpression* Cascade::Optimize(unique_ptr<LogicalOperator> plan) {
+		AUTO_MEM_POOL(amp);
+		CMemoryPool *mp = amp.Pmp();
+		gpopt::CSearchStageArray *search_strategy_arr = LoadSearchStrategy(mp);
+		
+		gpopt::CExpression* pexpr = adaptator(plan);
+		// pdrgpul: the array of query output column reference id
+		// pdrgpmdname: the array of output column names
+		gpopt::CQueryContext *pqc = gpopt::CQueryContext::PqcGenerate(mp, pexpr, pdrgpul, pdrgpmdname, true);
+		
+		gpopt::CEngine eng(mp);
+		eng.Init(pqc, search_strategy_arr);
+		eng.Optimize();
 
-	GPOS_CHECK_ABORT;
+		GPOS_CHECK_ABORT;
 
-	CExpression *pexprPlan = eng.PexprExtractPlan();
-	(void) pexprPlan->PrppCompute(mp, pqc->Prpp());
+		gpopt::CExpression *pexprPlan = eng.PexprExtractPlan();
+		(void) pexprPlan->PrppCompute(mp, pqc->Prpp());
 
-	CheckCTEConsistency(mp, pexprPlan);
+		CheckCTEConsistency(mp, pexprPlan);
 
-	PrintQueryOrPlan(mp, pexprPlan);
+		PrintQueryOrPlan(mp, pexprPlan);
 
-	GPOS_CHECK_ABORT;
+		GPOS_CHECK_ABORT;
 
-	return pexprPlan;
-}
+		return pexprPlan;
+	}
 
-CSearchStageArray* COptTasks::LoadSearchStrategy(CMemoryPool *mp, char *path)
-{
-	CSearchStageArray *search_strategy_arr = NULL;
-	return search_strategy_arr;
+	gpopt::CSearchStageArray* LoadSearchStrategy(CMemoryPool *mp)
+	{
+		gpopt::CSearchStageArray *search_strategy_arr = NULL;
+		return search_strategy_arr;
+	}
 }
