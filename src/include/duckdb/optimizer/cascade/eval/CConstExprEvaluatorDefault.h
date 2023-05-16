3c43f05ba30e0ed50157bd0a82e526625b6dc281
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-05-16 19:02:49 +0800

    Add files via upload

---
+++ src/include/duckdb/optimizer/cascade/eval/CConstExprEvaluatorDefault.h
@@ -0,0 +1,55 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		CConstExprEvaluatorDefault.h
+//
+//	@doc:
+//		Dummy implementation of the constant expression evaluator
+//
+//	@owner:
+//
+//
+//	@test:
+//
+//---------------------------------------------------------------------------
+#ifndef GPOPT_CConstExprEvaluatorDefault_H
+#define GPOPT_CConstExprEvaluatorDefault_H
+
+#include "duckdb/optimizer/cascade/base.h"
+
+#include "duckdb/optimizer/cascade/eval/IConstExprEvaluator.h"
+
+namespace gpopt
+{
+//---------------------------------------------------------------------------
+//	@class:
+//		CConstExprEvaluatorDefault
+//
+//	@doc:
+//		Constant expression evaluator default implementation for the case when
+//		no database instance is available
+//
+//---------------------------------------------------------------------------
+class CConstExprEvaluatorDefault : public IConstExprEvaluator
+{
+private:
+	// private copy ctor
+	CConstExprEvaluatorDefault(const CConstExprEvaluatorDefault &);
+
+public:
+	// ctor
+	CConstExprEvaluatorDefault() : IConstExprEvaluator()
+	{
+	}
+
+	// dtor
+	virtual ~CConstExprEvaluatorDefault();
+
+	// Evaluate the given expression and return the result as a new expression
+	virtual CExpression *PexprEval(CExpression *pexpr);
+
+	// Returns true iff the evaluator can evaluate constant expressions
+	virtual BOOL FCanEvalExpressions();
+};
+}  // namespace gpopt
+
+#endif
---
+++ src/include/duckdb/optimizer/cascade/eval/IConstExprEvaluator.h
@@ -0,0 +1,54 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		IConstExprEvaluator.h
+//
+//	@doc:
+//		Interface for constant expression evaluator in the optimizer
+//
+//	@owner:
+//
+//
+//	@test:
+//
+//---------------------------------------------------------------------------
+
+#ifndef GPOPT_IConstExprEvaluator_H
+#define GPOPT_IConstExprEvaluator_H
+
+#include "duckdb/optimizer/cascade/base.h"
+#include "duckdb/optimizer/cascade/common/CRefCount.h"
+
+namespace gpopt
+{
+using namespace gpos;
+
+class CExpression;	// forward declaration
+
+//---------------------------------------------------------------------------
+//	@class:
+//		IConstExprEvaluator
+//
+//	@doc:
+//		Interface to access the underlying evaluator of constant expressions
+//		(expressions that can be evaluated independent of the contents of the
+//		database)
+//
+//---------------------------------------------------------------------------
+class IConstExprEvaluator : public CRefCount
+{
+public:
+	// dtor
+	virtual ~IConstExprEvaluator()
+	{
+	}
+
+	// evaluate the given expression and return the result as a new expression
+	// caller takes ownership of returned expression
+	virtual CExpression *PexprEval(CExpression *pexpr) = 0;
+
+	// returns true iff the evaluator can evaluate constant expressions without subqueries
+	virtual BOOL FCanEvalExpressions() = 0;
+};
+}  // namespace gpopt
+
+#endif
