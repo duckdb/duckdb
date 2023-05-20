4ef95f9b15ff6334e8dcaddde4284f590b22b657
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-05-20 16:51:57 +0800

    Add files via upload

--- src/optimizer/cascade/Cascade.cpp
+++ src/optimizer/cascade/Cascade.cpp
@@ -1,44 +1,105 @@
 #include "duckdb/optimizer/cascade/Cascade.h"
-
+#include "duckdb/optimizer/cascade/base/CAutoOptCtxt.h"
+#include "duckdb/optimizer/cascade/memory/CMemoryPoolManager.h"
 #include "duckdb/optimizer/cascade/search/CSearchStage.h"
 #include "duckdb/optimizer/cascade/memory/CAutoMemoryPool.h"
 #include "duckdb/optimizer/cascade/base/CQueryContext.h"
 #include "duckdb/optimizer/cascade/engine/CEngine.h"
+#include "duckdb/optimizer/cascade/task/CWorkerPoolManager.h"
+#include "duckdb/optimizer/cascade/error/CMessageRepository.h"
+#include "duckdb/optimizer/cascade/task/CAutoTaskProxy.h"
 
 #define AUTO_MEM_POOL(amp) CAutoMemoryPool amp(CAutoMemoryPool::ElcExc)
 
 namespace duckdb {
-	gpopt::CExpression* Cascade::Optimize(unique_ptr<LogicalOperator> plan) {
+	using namespace gpos;
+	using namespace gpmd;
+	using namespace gpopt;
+
+	unique_ptr<PhysicalOperator> Cascade::Optimize(unique_ptr<LogicalOperator> plan)
+	{
+		if (GPOS_OK != CMemoryPoolManager::Init())
+		{
+			return NULL;
+		}
+		if (GPOS_OK != CWorkerPoolManager::Init())
+		{
+			CMemoryPoolManager::GetMemoryPoolMgr()->Shutdown();
+			return NULL;
+		}
+		if (GPOS_OK != CMessageRepository::Init())
+		{
+			CWorkerPoolManager::WorkerPoolManager()->Shutdown();
+			CMemoryPoolManager::GetMemoryPoolMgr()->Shutdown();
+			return NULL;
+		}
+		/*
+		if (GPOS_OK != gpos::CCacheFactory::Init())
+		{
+			return;
+		}
+		*/
+		CWorkerPoolManager *pwpm = CWorkerPoolManager::WorkerPoolManager();
+		// check if worker pool is initialized
+		if (NULL == pwpm)
+		{
+			return NULL;
+		}
+		void* pvStackStart = &pwpm;
+		// put worker to stack - main thread has id '0'
+		CWorker wrkr(GPOS_WORKER_STACK_SIZE, (ULONG_PTR) pvStackStart);
 		AUTO_MEM_POOL(amp);
-		CMemoryPool *mp = amp.Pmp();
-		gpopt::CSearchStageArray *search_strategy_arr = LoadSearchStrategy(mp);
-		
-		gpopt::CExpression* pexpr = adaptator(plan);
+		CMemoryPool* mp = amp.Pmp();
+		CAutoTaskProxy atp(mp, pwpm, true);
+		CTask *ptsk = atp.Create(NULL, NULL);
+		// init TLS
+		ptsk->GetTls().Reset(mp);
+		atp.Execute(ptsk);
+		CSearchStageArray* search_strategy_arr = LoadSearchStrategy(mp);
+		CMDProviderArray* relcache_provider = GPOS_NEW(mp) CMDProviderArray(mp);
+		CMDAccessor mda(mp, NULL, NULL, relcache_provider);
+		CAutoOptCtxt aoc(mp, &mda, (IConstExprEvaluator*)NULL, (COptimizerConfig*)NULL);
+		CExpression* pexpr = Orca2Duck(std::move(plan));
+		ULongPtrArray* pdrgpul = GPOS_NEW(mp) ULongPtrArray(mp);
+		ULONG x = 0;
+		pdrgpul->Append(&x);
+		CMDNameArray* pdrgpmdname = GPOS_NEW(mp) CMDNameArray(mp);
+		WCHAR str[] = L"A";
+		CWStringConst* cwstr = GPOS_NEW(mp) CWStringConst(str);
+		CMDName* mdname = GPOS_NEW(mp) CMDName(mp, cwstr);
+		pdrgpmdname->Append(mdname);
 		// pdrgpul: the array of query output column reference id
 		// pdrgpmdname: the array of output column names
-		gpopt::CQueryContext *pqc = gpopt::CQueryContext::PqcGenerate(mp, pexpr, pdrgpul, pdrgpmdname, true);
-		
-		gpopt::CEngine eng(mp);
+		CQueryContext* pqc = CQueryContext::PqcGenerate(mp, pexpr, pdrgpul, pdrgpmdname, true);
+		CEngine eng(mp);
 		eng.Init(pqc, search_strategy_arr);
 		eng.Optimize();
 
 		GPOS_CHECK_ABORT;
 
-		gpopt::CExpression *pexprPlan = eng.PexprExtractPlan();
+		CExpression* pexprPlan = eng.PexprExtractPlan();
 		(void) pexprPlan->PrppCompute(mp, pqc->Prpp());
 
-		CheckCTEConsistency(mp, pexprPlan);
+		//CheckCTEConsistency(mp, pexprPlan);
 
-		PrintQueryOrPlan(mp, pexprPlan);
+		//PrintQueryOrPlan(mp, pexprPlan);
 
 		GPOS_CHECK_ABORT;
-
-		return pexprPlan;
+		unique_ptr<PhysicalOperator> final = Duck2Orca(pexprPlan);
+		return final;
 	}
 
-	gpopt::CSearchStageArray* LoadSearchStrategy(CMemoryPool *mp)
+	CSearchStageArray* Cascade::LoadSearchStrategy(CMemoryPool *mp)
 	{
-		gpopt::CSearchStageArray *search_strategy_arr = NULL;
+		CSearchStageArray *search_strategy_arr = NULL;
 		return search_strategy_arr;
+	}
+
+	CExpression* Cascade::Orca2Duck(unique_ptr<LogicalOperator> plan) {
+		return NULL;
+	}
+
+	unique_ptr<PhysicalOperator> Cascade::Duck2Orca(CExpression* Plan) {
+		return NULL;
 	}
 }
---
+++ src/optimizer/cascade/utils.cpp
@@ -0,0 +1,165 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		utils.cpp
+//
+//	@doc:
+//		Various utilities which are not necessarily gpos specific
+//---------------------------------------------------------------------------
+#include "duckdb/optimizer/cascade/utils.h"
+#include "duckdb/optimizer/cascade/types.h"
+
+// using 16 addresses a line fits exactly into 80 characters
+#define GPOS_MEM_BPL 16
+
+// number of stack frames to search for addresses
+#define GPOS_SEARCH_STACK_FRAMES 16
+
+using namespace gpos;
+
+//---------------------------------------------------------------------------
+//	GPOS versions of standard streams
+//	Do not reference std::(w)cerr/(w)cout directly;
+//---------------------------------------------------------------------------
+COstreamBasic gpos::oswcerr(&std::wcerr);
+COstreamBasic gpos::oswcout(&std::wcout);
+
+//---------------------------------------------------------------------------
+//	@function:
+//		gpos::Print
+//
+//	@doc:
+//		Print wide-character string
+//
+//---------------------------------------------------------------------------
+void gpos::Print(WCHAR *wsz)
+{
+	std::wcout << wsz;
+}
+
+//---------------------------------------------------------------------------
+//	@function:
+//		gpos::HexDump
+//
+//	@doc:
+//		Generic memory dumper; produces regular hex dump
+//
+//---------------------------------------------------------------------------
+IOstream & gpos::HexDump(IOstream &os, const void *pv, ULLONG size)
+{
+	for (ULONG i = 0; i < 1 + (size / GPOS_MEM_BPL); i++)
+	{
+		// starting address of line
+		BYTE *buf = ((BYTE *) pv) + (GPOS_MEM_BPL * i);
+		os << (void *) buf << "  ";
+		os << COstream::EsmHex;
+		// individual bytes
+		for (ULONG j = 0; j < GPOS_MEM_BPL; j++)
+		{
+			if (buf[j] < 16)
+			{
+				os << "0";
+			}
+			os << (ULONG) buf[j] << " ";
+			// separator in middle of line
+			if (j + 1 == GPOS_MEM_BPL / 2)
+			{
+				os << "- ";
+			}
+		}
+		// blank between hex and text dump
+		os << " ";
+		// text representation
+		for (ULONG j = 0; j < GPOS_MEM_BPL; j++)
+		{
+			// print only 'visible' characters
+			if (buf[j] >= 0x20 && buf[j] <= 0x7f)
+			{
+				// cast to CHAR to avoid stream from (mis-)interpreting BYTE
+				os << (CHAR) buf[j];
+			}
+			else
+			{
+				os << ".";
+			}
+		}
+		os << COstream::EsmDec << std::endl;
+	}
+	return os;
+}
+
+//---------------------------------------------------------------------------
+//	@function:
+//		gpos::HashByteArray
+//
+//	@doc:
+//		Generic hash function for an array of BYTEs
+//		Taken from D. E. Knuth;
+//
+//---------------------------------------------------------------------------
+ULONG gpos::HashByteArray(const BYTE *byte_array, ULONG size)
+{
+	ULONG hash = size;
+	for (ULONG i = 0; i < size; ++i)
+	{
+		BYTE b = byte_array[i];
+		hash = ((hash << 5) ^ (hash >> 27)) ^ b;
+	}
+	return hash;
+}
+
+//---------------------------------------------------------------------------
+//	@function:
+//		gpos::CombineHashes
+//
+//	@doc:
+//		Combine ULONG-based hash values
+//
+//---------------------------------------------------------------------------
+ULONG gpos::CombineHashes(ULONG hash1, ULONG hash2)
+{
+	ULONG hashes[2];
+	hashes[0] = hash1;
+	hashes[1] = hash2;
+	return HashByteArray((BYTE *) hashes, GPOS_ARRAY_SIZE(hashes) * sizeof(hashes[0]));
+}
+
+
+//---------------------------------------------------------------------------
+//	@function:
+//		gpos::Add
+//
+//	@doc:
+//		Add two unsigned long long values, throw an exception if overflow occurs,
+//
+//---------------------------------------------------------------------------
+ULLONG gpos::Add(ULLONG first, ULLONG second)
+{
+	if (first > gpos::ullong_max - second)
+	{
+		// if addition result overflows, we have (a + b > gpos::ullong_max),
+		// then we need to check for  (a > gpos::ullong_max - b)
+		GPOS_RAISE(CException::ExmaSystem, CException::ExmiOverflow);
+	}
+	ULLONG res = first + second;
+	return res;
+}
+
+//---------------------------------------------------------------------------
+//	@function:
+//		gpos::Multiply
+//
+//	@doc:
+//		Multiply two unsigned long long values, throw an exception if overflow occurs,
+//
+//---------------------------------------------------------------------------
+ULLONG gpos::Multiply(ULLONG first, ULLONG second)
+{
+	if (0 < second && first > gpos::ullong_max / second)
+	{
+		// if multiplication result overflows, we have (a * b > gpos::ullong_max),
+		// then we need to check for  (a > gpos::ullong_max / b)
+		GPOS_RAISE(CException::ExmaSystem, CException::ExmiOverflow);
+	}
+	ULLONG res = first * second;
+	return res;
+}
