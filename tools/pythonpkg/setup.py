593a23f4a2b8ffc5402e337ca5f142bd7bc88fb1
Tishj <t_b@live.nl>
2023-05-23 13:11:37 +0200

    not functional - the package likely needs to be built separately from 'duckdb', since it depends on the 'libduckdb.dylib' to already be built

--- tools/pythonpkg/setup.py
+++ tools/pythonpkg/setup.py
@@ -7,7 +7,8 @@ import platform
 import multiprocessing.pool
 from glob import glob
 
-from setuptools import setup, Extension
+import subprocess
+from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext as _build_ext
 
 
@@ -285,11 +286,7 @@ setup(
     long_description = 'See here for an introduction: https://duckdb.org/docs/api/python/overview',
     license='MIT',
     data_files = data_files,
-    packages=[
-        'adbc_driver_duckdb',
-        'pyduckdb',
-        'duckdb-stubs'
-    ],
+    packages=find_packages(),
     include_package_data=True,
     setup_requires=setup_requires + ["setuptools_scm<7.0.0", 'pybind11>=2.6.0'],
     use_scm_version = setuptools_scm_conf,
@@ -310,3 +307,10 @@ setup(
         "Changelog": "https://github.com/duckdb/duckdb/releases",
     },
 )
+
+from pathlib import Path
+source_root = Path(__file__).parent
+adbc_driver_duckdb = source_root.joinpath('adbc_driver_duckdb/setup.py')
+
+# Execute the setup.py of adbc_driver_duckdb submodule
+subprocess.check_call(['python3', adbc_driver_duckdb, 'install'])
