da6d7a1cba4e65a35d4a80543d6437e8865efc5e
Tishj <t_b@live.nl>
2023-05-29 07:17:20 +0200

    fix bad_function_call on linux

--- tools/pythonpkg/src/include/duckdb_python/pyrelation.hpp
+++ tools/pythonpkg/src/include/duckdb_python/pyrelation.hpp
@@ -49,8 +49,8 @@ public:
 	using config_modify_func_t = std::function<void(ClientConfig &config)>;
 
 public:
-	ScopedConfigSetting(ClientConfig &config, config_modify_func_t set, config_modify_func_t unset)
-	    : config(config), set(std::move(set)), unset(std::move(unset)) {
+	ScopedConfigSetting(ClientConfig &config, config_modify_func_t set_f, config_modify_func_t unset_f)
+	    : config(config), set(std::move(set_f)), unset(std::move(unset_f)) {
 		set(config);
 	}
 	~ScopedConfigSetting() {
