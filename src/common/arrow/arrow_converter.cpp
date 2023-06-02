071fa0c1f709507812bb9beafbcd36ddb00ddd0b
Pedro Holanda <pedroholanda@gmail.com>
2023-06-02 10:01:47 +0200

    Thijs' feeback

--- src/common/arrow/arrow_converter.cpp
+++ src/common/arrow/arrow_converter.cpp
@@ -135,7 +135,7 @@ void SetArrowFormat(DuckDBArrowSchemaHolder &root_holder, ArrowSchema &child, co
 		child.format = "tsu:";
 		break;
 	case LogicalTypeId::TIMESTAMP_TZ: {
-		string format = "tsu:" + options.timezone;
+		string format = "tsu:" + options.time_zone;
 		auto format_ptr = make_unsafe_uniq_array<char>(format.size() + 1);
 		for (size_t i = 0; i < format.size(); i++) {
 			format_ptr[i] = format[i];
