b43cbe328bcdb06847b77d8c19af19ef5932657a
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-06-06 16:11:22 +0200

    OssFuzz fixes

--- src/common/exception_format_value.cpp
+++ src/common/exception_format_value.cpp
@@ -86,6 +86,10 @@ string ExceptionFormatValue::Format(const string &msg, std::vector<ExceptionForm
 		return duckdb_fmt::vsprintf(msg, duckdb_fmt::basic_format_args<duckdb_fmt::printf_context>(
 		                                     format_args.data(), static_cast<int>(format_args.size())));
 	} catch (std::exception &ex) {
+		// work-around for oss-fuzz limiting memory which causes issues here
+		if (StringUtil::Contains(ex.what(), "fuzz mode")) {
+			throw Exception(msg);
+		}
 		throw InternalException(std::string("Primary exception: ") + msg +
 		                        "\nSecondary exception in ExceptionFormatValue: " + ex.what());
 	}
