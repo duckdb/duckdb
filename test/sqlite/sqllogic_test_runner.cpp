eafdf1af665e405abaff69f8a4d1699835005dd9
Denis Hirn <denis.hirn@uni-tuebingen.de>
2023-06-07 08:23:00 +0200

    Only skip tests with require alternativeverify, when in alternative verify mode

--- test/sqlite/sqllogic_test_runner.cpp
+++ test/sqlite/sqllogic_test_runner.cpp
@@ -518,7 +518,9 @@ void SQLLogicTestRunner::ExecuteFile(string script) {
 			} else if (param == "skip_reload") {
 				skip_reload = true;
 			} else if (param == "noalternativeverify") {
+#ifdef DUCKDB_ALTERNATIVE_VERIFY
 				return;
+#endif
 			} else {
 				auto result = ExtensionHelper::LoadExtension(*db, param);
 				if (result == ExtensionLoadResult::LOADED_EXTENSION) {
