d417ca153d8072aa7fbc69d2f3945ab8a06be7f7
stephaniewang <qw79@cornell.edu>
2023-06-07 17:51:30 -0400

    feat: update parser exception handling for extensions

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,18 +1,17 @@
 #include "duckdb/parser/parser.hpp"
 
-#include "duckdb/parser/transformer.hpp"
 #include "duckdb/parser/parsed_data/create_table_info.hpp"
+#include "duckdb/parser/parser_extension.hpp"
+#include "duckdb/parser/query_error_context.hpp"
+#include "duckdb/parser/query_node/select_node.hpp"
 #include "duckdb/parser/statement/create_statement.hpp"
 #include "duckdb/parser/statement/extension_statement.hpp"
 #include "duckdb/parser/statement/select_statement.hpp"
 #include "duckdb/parser/statement/update_statement.hpp"
-#include "duckdb/parser/query_node/select_node.hpp"
 #include "duckdb/parser/tableref/expressionlistref.hpp"
-#include "postgres_parser.hpp"
-#include "duckdb/parser/query_error_context.hpp"
-#include "duckdb/parser/parser_extension.hpp"
-
+#include "duckdb/parser/transformer.hpp"
 #include "parser/parser.hpp"
+#include "postgres_parser.hpp"
 
 namespace duckdb {
 
@@ -138,39 +137,78 @@ void Parser::ParseQuery(const string &query) {
 	}
 	{
 		PostgresParser::SetPreserveIdentifierCase(options.preserve_identifier_case);
-		PostgresParser parser;
-		parser.Parse(query);
-		if (parser.success) {
-			if (!parser.parse_tree) {
-				// empty statement
-				return;
-			}
-
-			// if it succeeded, we transform the Postgres parse tree into a list of
-			// SQLStatements
-			transformer.TransformParseTree(parser.parse_tree, statements);
-		} else {
-			parser_error = QueryErrorContext::Format(query, parser.error_message, parser.error_location - 1);
-		}
-	}
-	if (!parser_error.empty()) {
-		if (options.extensions) {
-			for (auto &ext : *options.extensions) {
-				D_ASSERT(ext.parse_function);
-				auto result = ext.parse_function(ext.parser_info.get(), query);
-				if (result.type == ParserExtensionResultType::PARSE_SUCCESSFUL) {
-					auto statement = make_uniq<ExtensionStatement>(ext, std::move(result.parse_data));
-					statement->stmt_length = query.size();
-					statement->stmt_location = 0;
-					statements.push_back(std::move(statement));
+		bool parsing_failed = false;
+		// Creating a new scope to prevent multiple PostgresParser destructors being called
+		// which led to some memory issues
+		{
+			PostgresParser parser;
+			parser.Parse(query);
+			if (parser.success) {
+				if (!parser.parse_tree) {
+					// empty statement
 					return;
 				}
-				if (result.type == ParserExtensionResultType::DISPLAY_EXTENSION_ERROR) {
-					throw ParserException(result.error);
+
+				// if it succeeded, we transform the Postgres parse tree into a list of
+				// SQLStatements
+				transformer.TransformParseTree(parser.parse_tree, statements);
+			} else {
+				parsing_failed = true;
+			}
+		}
+		// if DuckDB fails to parse the entire sql string, break the string down into individual statements
+		// so that parser extensions can help parse some statements
+		if (parsing_failed) {
+			vector<string> query_statements;
+			auto tokens = Tokenize(query);
+			auto next_statement_start = 0;
+			for (size_t i = 1; i < tokens.size(); ++i) {
+				auto &t_prev = tokens[i - 1];
+				auto &t = tokens[i];
+				if (t_prev.type == SimplifiedTokenType::SIMPLIFIED_TOKEN_OPERATOR) {
+					if (StringUtil::Contains(query.substr(t_prev.start, t.start - t_prev.start), ";")) {
+						query_statements.emplace_back(
+						    query.substr(next_statement_start, t.start - next_statement_start));
+						next_statement_start = tokens[i].start;
+					}
+				}
+			}
+			query_statements.emplace_back(query.substr(next_statement_start, query.size() - next_statement_start));
+			for (auto const &s : query_statements) {
+				PostgresParser another_parser;
+				another_parser.Parse(s);
+				// first see if DuckDB can parse this individual query statement
+				if (another_parser.success) {
+					if (!another_parser.parse_tree) {
+						// empty statement
+						return;
+					}
+
+					transformer.TransformParseTree(another_parser.parse_tree, statements);
+				} else {
+					// let the extension parse the statement which DuckDB failed to parse
+					if (options.extensions) {
+						for (auto &ext : *options.extensions) {
+							D_ASSERT(ext.parse_function);
+							auto result = ext.parse_function(ext.parser_info.get(), s);
+							if (result.type == ParserExtensionResultType::PARSE_SUCCESSFUL) {
+								auto statement = make_uniq<ExtensionStatement>(ext, std::move(result.parse_data));
+								statement->stmt_length = s.size();
+								statement->stmt_location = 0;
+								statements.push_back(std::move(statement));
+							}
+							if (result.type == ParserExtensionResultType::DISPLAY_EXTENSION_ERROR) {
+								throw ParserException(result.error);
+							} else if (result.type == ParserExtensionResultType::DISPLAY_ORIGINAL_ERROR) {
+								parser_error = QueryErrorContext::Format(query, another_parser.error_message,
+								                                         another_parser.error_location - 1);
+								throw ParserException(parser_error);
+							}
+						}
+					}
 				}
 			}
 		}
-		throw ParserException(parser_error);
 	}
 	if (!statements.empty()) {
 		auto &last_statement = statements.back();
