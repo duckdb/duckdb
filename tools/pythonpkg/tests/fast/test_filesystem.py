7d2370b1eb47375cc100bc65c75919a9b7f02d55
Lars Verdoes <lars@MacBook-Pro.localdomain>
2023-06-09 11:02:07 +0200

    reset to original

--- tools/pythonpkg/tests/fast/test_filesystem.py
+++ tools/pythonpkg/tests/fast/test_filesystem.py
@@ -196,7 +196,7 @@ class TestPythonFilesystem:
             '/root/a=1/data_0.csv'
         ).read() == b'1\n'
 
-    def test_read_hive_partition(self, duckdb_cursor: DuckDBPyConnection, memory: AbstractFileSystem):
+def test_read_hive_partition(self, duckdb_cursor: DuckDBPyConnection, memory: AbstractFileSystem):
         duckdb_cursor.register_filesystem(memory)
 
         with memory.open('/root/a=1/data_0.csv', 'wb') as fh:
@@ -204,4 +204,4 @@ class TestPythonFilesystem:
 
         duckdb_cursor.execute('''SELECT * FROM read_csv_auto('memory://root/*/*.csv', HIVE_PARTITIONING = 1);''')
 
-        assert duckdb_cursor.fetchall() == [(1, 1)]
+        assert duckdb_cursor.fetchall() == [(1, '1')]
