78d447c2a28abfd26d7d4fc43adbaded16eb3dc4
Richard Wesley <hawkfish@electricfish.com>
2023-06-14 11:18:47 -0700

    Issue #7879: Missing JDBC TIMESTAMP_TZ
    CR feedback.

--- tools/jdbc/src/test/java/org/duckdb/test/TestDuckDBJDBC.java
+++ tools/jdbc/src/test/java/org/duckdb/test/TestDuckDBJDBC.java
@@ -509,6 +509,29 @@ public class TestDuckDBJDBC {
 		stmt.close();
 		conn.close();
 	}
+	
+	public static void test_timestamptz_as_long() throws Exception {
+		Connection conn = DriverManager.getConnection("jdbc:duckdb:");
+		Statement stmt = conn.createStatement();
+
+		ResultSet rs;
+
+		stmt.execute("SET CALENDAR='gregorian'");
+		stmt.execute("SET TIMEZONE='America/Los_Angeles'");
+		stmt.execute("CREATE TABLE t (id INT, t1 TIMESTAMPTZ)");
+		stmt.execute("INSERT INTO t (id, t1) VALUES (1, '2022-01-01T12:11:10Z')");
+		stmt.execute("INSERT INTO t (id, t1) VALUES (2, '2022-01-01T12:11:11Z')");
+
+		rs = stmt.executeQuery("SELECT * FROM t ORDER BY id");
+		rs.next();
+		assertEquals(rs.getLong(2), 1641039070000000L);
+		rs.next();
+		assertEquals(rs.getLong(2), 1641039071000000L);
+
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
 
     public static void test_consecutive_timestamps() throws Exception {
     	long expected = 986860800000L;
@@ -523,7 +546,7 @@ public class TestDuckDBJDBC {
         }
     }
 
-	public static void test_read_timestamp_tz() throws Exception {
+	public static void test_throw_wrong_datatype() throws Exception {
 		Connection conn = DriverManager.getConnection("jdbc:duckdb:");
 		Statement stmt = conn.createStatement();
 		ResultSet rs;
@@ -533,9 +556,12 @@ public class TestDuckDBJDBC {
 
 		rs = stmt.executeQuery("SELECT * FROM t");
 		rs.next();
-		
-		//	This succeeds now,
-		rs.getTimestamp(2);
+
+		try {
+			rs.getShort(2);
+			fail();
+		} catch (IllegalArgumentException e) {
+		}
 
 		rs.close();
 		stmt.close();
