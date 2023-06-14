761d40d78847f0db6d892ef58361214a9d6a54d4
dtenwolde <dljtw98@gmail.com>
2023-06-14 14:22:56 +0200

    Simplified transform drop property graph to just dropInfo

--- src/parser/transform/statement/transform_drop_property_graph.cpp
+++ src/parser/transform/statement/transform_drop_property_graph.cpp
@@ -1,16 +1,16 @@
-#include "duckdb/parser/statement/drop_property_graph_statement.hpp"
+#include "duckdb/parser/statement/drop_statement.hpp"
 #include "duckdb/parser/transformer.hpp"
 
 namespace duckdb {
 unique_ptr<SQLStatement> Transformer::TransformDropPropertyGraph(duckdb_libpgquery::PGNode *node) {
 	auto stmt = (duckdb_libpgquery::PGDropPropertyGraphStmt *)(node);
-	auto result = make_uniq<DropPropertyGraphStatement>();
+	auto result = make_uniq<DropStatement>();
 	auto &info = *result->info.get();
 	D_ASSERT(stmt);
 	auto pg_tableref = TransformQualifiedName(stmt->name);
 
 	info.name = pg_tableref.name;
-	info.type = CatalogType::DATABASE_ENTRY;
+	info.type = CatalogType::TABLE_ENTRY;
 	return result;
 }
 
