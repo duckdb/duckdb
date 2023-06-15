7b8d4cebf47b4c6b9897640bff6c241420b2c170
Sam Ansmink <samansmink@hotmail.com>
2023-06-15 14:39:42 +0200

    update outdated extension hashes

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -8,7 +8,7 @@
 duckdb_extension_load(sqlite_scanner
     DONT_LINK
     GIT_URL https://github.com/duckdblabs/sqlite_scanner
-    GIT_TAG e607f30160260a5d3152087e001967ece39c36c0
+    GIT_TAG 05dd50b3fc91ee4974964086efbc16be74fa115f
 )
 duckdb_extension_load(postgres_scanner
     DONT_LINK
@@ -18,18 +18,18 @@ duckdb_extension_load(postgres_scanner
 duckdb_extension_load(spatial
     DONT_LINK
     GIT_URL https://github.com/duckdblabs/duckdb_spatial.git
-    GIT_TAG eaa44f9733b7f4a9f53791536ed8219ef39813f5
+    GIT_TAG f577b9441793f9170403e489f5d3587e023a945f
 )
 
 if (NOT WIN32)
     duckdb_extension_load(arrow
         DONT_LINK
         GIT_URL https://github.com/duckdblabs/arrow
-        GIT_TAG c80462e30b463c2391e033fe11d86668a5ac44c3
+        GIT_TAG 1a43a5513b96e4c6ffd92026775ffeb648e71dac
     )
     duckdb_extension_load(substrait
         DONT_LINK
         GIT_URL https://github.com/duckdblabs/substrait
-        GIT_TAG 48d64b27c7a6985d6f6c3e65044038544608c03b
+        GIT_TAG a049baa6ad42faeee3ab71abcec349bd24abed14
     )
 endif()
