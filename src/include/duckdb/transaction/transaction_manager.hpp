f84ae17629dae58a147e002e9b0d44c6e672367c
Jacob <535707+jkub@users.noreply.github.com>
2023-06-17 23:02:16 -0700

    format

--- src/include/duckdb/transaction/transaction_manager.hpp
+++ src/include/duckdb/transaction/transaction_manager.hpp
@@ -39,7 +39,7 @@ public:
 	virtual void RollbackTransaction(Transaction *transaction) = 0;
 
 	virtual void Checkpoint(ClientContext &context, bool force = false,
-		std::function<void()> on_checkpoint = nullptr) = 0;
+	                        std::function<void()> on_checkpoint = nullptr) = 0;
 
 	static TransactionManager &Get(AttachedDatabase &db);
 
