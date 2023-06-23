34391dc2823d19b6d2b9c5d07289819ad99c029b
Sam Ansmink <samansmink@hotmail.com>
2023-06-23 11:15:29 +0200

    add vcpkg config file

---
+++ .github/config/vcpkg_out_of_tree_extensions.cmake
@@ -0,0 +1,18 @@
+#
+# This is the DuckDB extension config for extensions that require VCPKG as it will run on the CI.
+#
+# to build duckdb with this configuration run:
+#   EXTENSION_CONFIGS=.github/config/vcpkg_out_of_tree_extensions.cmake make
+#
+
+duckdb_extension_load(azure
+    LOAD_TESTS
+    GIT_URL https://github.com/samansmink/azure-extension
+    GIT_TAG d692c1c5f6bb43af34525feff502c60a4413c519
+)
+
+duckdb_extension_load(aws
+    LOAD_TESTS
+    GIT_URL https://github.com/samansmink/aws
+    GIT_TAG c7a91ff4af3d17e0317359de6b2aad4dd1d01dbb
+)
