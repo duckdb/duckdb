22e1f03be31d76537c9bdbe21bc2c975678f3686
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-06-23 15:26:59 +0200

    Yolo

--- src/main/appender.cpp
+++ src/main/appender.cpp
@@ -16,6 +16,7 @@
 namespace duckdb {
 
 // random comment
+// second comment
 BaseAppender::BaseAppender(Allocator &allocator, AppenderType type_p)
     : allocator(allocator), column(0), appender_type(type_p) {
 }
