1160dade572377fdd4d44d98c1376156bb24ac7c
Sam Ansmink <samansmink@hotmail.com>
2023-06-23 16:15:56 +0200

    print produced dependency list for nicer debuggin

--- scripts/merge_vcpkg_deps.py
+++ scripts/merge_vcpkg_deps.py
@@ -18,5 +18,10 @@ data = {
     "description": f"Auto-generated vcpkg.json for combined DuckDB extension build",
     "dependencies": deduplicated_dependencies
 }
+
+# Print output
+print("Writing to 'build/vcpkg_merged_manifest/vcpkg.json': ")
+print(data["dependencies"])
+
 with open('build/vcpkg_merged_manifest/vcpkg.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
