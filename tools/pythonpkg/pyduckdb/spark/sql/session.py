ffe36fb003ce258cf46cc65a2a3f7b8a87d706f6
Tishj <t_b@live.nl>
2023-06-23 15:55:28 +0200

    SparkSession + Builder minimally working

--- tools/pythonpkg/pyduckdb/spark/sql/session.py
+++ tools/pythonpkg/pyduckdb/spark/sql/session.py
@@ -1,6 +1,27 @@
+from typing_extensions import Self
+
+
 class SparkSession:
 	def __init__(self):
 		self.name = "session"
+		self._master = 'master'
+
+	class Builder:
+		def __init__(self):
+			self.name = "builder"
+
+		def master(self, name: str) -> Self:
+			self._master = name
+			return self
+
+		def appName(self, name: str) -> Self:
+			self._appName = name
+			return self
+
+		def getOrCreate(self) -> "SparkSession":
+			return SparkSession()
+
+	builder = Builder()
 
 __all__ = [
 	"SparkSession"
