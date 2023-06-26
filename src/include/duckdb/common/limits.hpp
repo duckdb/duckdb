2c0e1d0fd11fc8e3203462a260cd1f8cc8a3b9ca
Nick Gerrets <nickgerrets@gmail.com>
2023-06-26 10:10:40 +0200

    changed IsSigned & Digits in limits to be constexpr

--- src/include/duckdb/common/limits.hpp
+++ src/include/duckdb/common/limits.hpp
@@ -25,8 +25,8 @@ struct NumericLimits {
 	static constexpr T Maximum() {
 		return std::numeric_limits<T>::max();
 	};
-	DUCKDB_API static bool IsSigned();
-	DUCKDB_API static idx_t Digits();
+	DUCKDB_API static constexpr bool IsSigned();
+	DUCKDB_API static constexpr idx_t Digits();
 };
 
 template <>
@@ -37,10 +37,10 @@ struct NumericLimits<int8_t> {
 	static constexpr int8_t Maximum() {
 		return std::numeric_limits<int8_t>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return true;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 3;
 	}
 };
@@ -52,10 +52,10 @@ struct NumericLimits<int16_t> {
 	static constexpr int16_t Maximum() {
 		return std::numeric_limits<int16_t>::max();
 	}
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return true;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 5;
 	}
 };
@@ -68,10 +68,10 @@ struct NumericLimits<int32_t> {
 	static constexpr int32_t Maximum() {
 		return std::numeric_limits<int32_t>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return true;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 10;
 	}
 };
@@ -84,10 +84,10 @@ struct NumericLimits<int64_t> {
 	static constexpr int64_t Maximum() {
 		return std::numeric_limits<int64_t>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return true;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 19;
 	}
 };
@@ -99,10 +99,10 @@ struct NumericLimits<hugeint_t> {
 	static constexpr hugeint_t Maximum() {
 		return {std::numeric_limits<int64_t>::max(), std::numeric_limits<uint64_t>::max()};
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return true;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 39;
 	}
 };
@@ -115,10 +115,10 @@ struct NumericLimits<uint8_t> {
 	static constexpr uint8_t Maximum() {
 		return std::numeric_limits<uint8_t>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return false;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 3;
 	}
 };
@@ -131,10 +131,10 @@ struct NumericLimits<uint16_t> {
 	static constexpr uint16_t Maximum() {
 		return std::numeric_limits<uint16_t>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return false;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 5;
 	}
 };
@@ -146,10 +146,10 @@ struct NumericLimits<uint32_t> {
 	static constexpr uint32_t Maximum() {
 		return std::numeric_limits<uint32_t>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return false;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 10;
 	}
 };
@@ -161,10 +161,10 @@ struct NumericLimits<uint64_t> {
 	static constexpr uint64_t Maximum() {
 		return std::numeric_limits<uint64_t>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return false;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 20;
 	}
 };
@@ -177,10 +177,10 @@ struct NumericLimits<float> {
 	static constexpr float Maximum() {
 		return std::numeric_limits<float>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return true;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 127;
 	}
 };
@@ -192,10 +192,10 @@ struct NumericLimits<double> {
 	static constexpr double Maximum() {
 		return std::numeric_limits<double>::max();
 	};
-	static bool IsSigned() {
+	static constexpr bool IsSigned() {
 		return true;
 	}
-	static idx_t Digits() {
+	static constexpr idx_t Digits() {
 		return 250;
 	}
 };
