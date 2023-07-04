c5c26b1c30982ea991f3066e5c1bd8c8d3b69270
Sam Ansmink <samansmink@hotmail.com>
2023-07-04 17:38:07 +0200

    more ci fixups

--- .github/config/vcpkg_extensions.cmake
+++ .github/config/vcpkg_extensions.cmake
@@ -8,11 +8,11 @@
 duckdb_extension_load(azure
     LOAD_TESTS
     GIT_URL https://github.com/samansmink/azure-extension
-    GIT_TAG 8441330242fa8b84fbaebbe478c46b46db408bf8
+    GIT_TAG 8b6f1062988cce067a54c9f1a93bf0874531a675
 )
 
 duckdb_extension_load(aws
     LOAD_TESTS
     GIT_URL https://github.com/samansmink/aws
-    GIT_TAG c7a91ff4af3d17e0317359de6b2aad4dd1d01dbb
+    GIT_TAG ff226f67941d71f9e20eaba031d1088366be75b1
 )
