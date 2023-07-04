92af5afe2c4db9b672b310468f897a03a1cb88de
Tishj <t_b@live.nl>
2023-07-04 14:50:52 +0200

    define some methods

--- tools/pythonpkg/src/include/duckdb_python/expression/pyexpression.hpp
+++ tools/pythonpkg/src/include/duckdb_python/expression/pyexpression.hpp
@@ -13,32 +13,48 @@
 #include "duckdb/common/string.hpp"
 #include "duckdb/parser/parsed_expression.hpp"
 #include "duckdb/parser/expression/constant_expression.hpp"
+#include "duckdb_python/python_conversion.hpp"
+#include "duckdb_python/pyconnection/pyconnection.hpp"
 
 namespace duckdb {
 
 struct DuckDBPyExpression {
 public:
-	explicit DuckDBPyExpression(unique_ptr<ParsedExpression> expr) : expression(std::move(expr)) {
-	}
+	explicit DuckDBPyExpression(unique_ptr<ParsedExpression> expr);
 
 public:
 	static void Initialize(py::handle &m);
 
-	string ToSQL();
+	string Type() const;
 
-	py::str Type();
+	string ToString() const;
+	void Print() const;
 
-	string ToString();
-	void Print();
+public:
+	const ParsedExpression &GetExpression() const;
 
 private:
 private:
 	unique_ptr<ParsedExpression> expression;
 };
 
+struct PythonValue : py::object {
+public:
+	PythonValue(const py::object &o) : py::object(o, borrowed_t {}) {
+	}
+	using py::object::object;
+
+public:
+	static bool check_(const py::handle &object) { // NOLINT
+		auto &import_cache = *DuckDBPyConnection::ImportCache();
+		return py::isinstance(object, import_cache.pyduckdb().value());
+	}
+};
+
 struct PyConstantExpression : public DuckDBPyExpression {
 public:
-	explicit PyConstantExpression(Value value) : DuckDBPyExpression(make_uniq<ConstantExpression>(std::move(value))) {
+	explicit PyConstantExpression(const PythonValue &value)
+	    : DuckDBPyExpression(make_uniq<ConstantExpression>(TransformPythonValue(value))) {
 	}
 };
 
--- tools/pythonpkg/src/pyexpression/initialize.cpp
+++ tools/pythonpkg/src/pyexpression/initialize.cpp
@@ -3,10 +3,25 @@
 namespace duckdb {
 
 void DuckDBPyExpression::Initialize(py::handle &m) {
-	auto expression_module = py::class_<DuckDBPyExpression>(m, "Expression", py::module_local());
+	auto expression = py::class_<DuckDBPyExpression>(m, "Expression", py::module_local());
 
+	const char *docs;
 	auto constant_expression =
 	    py::class_<PyConstantExpression, DuckDBPyExpression>(m, "ConstantExpression", py::module_local());
+	constant_expression.def(py::init<const PythonValue &>(), py::arg("value"));
+
+	docs = R"(
+    Print the stringified version of the expression.
+	)";
+	expression.def("show", &DuckDBPyExpression::Print, docs);
+
+	docs = R"(
+        Return the stringified version of the expression.
+
+        Returns:
+            str: The string representation.
+    )";
+	expression.def("__repr__", &DuckDBPyExpression::ToString, docs);
 }
 
 } // namespace duckdb
