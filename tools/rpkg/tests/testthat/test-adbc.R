6acdeb2bf35f460a0c97713036cf3ee3f7d46bd2
Dewey Dunnington <dewey@voltrondata.com>
2023-07-06 16:26:26 -0300

    add adbcdrivermanager integration

---
+++ tools/rpkg/tests/testthat/test-adbc.R
@@ -0,0 +1,44 @@
+test_that("ADBC driver can create databases, connections, and statements", {
+  drv <- duckdb_adbc()
+  expect_s3_class(drv, "duckdb_driver_adbc")
+
+  db <- adbcdrivermanager::local_adbc(
+    adbcdrivermanager::adbc_database_init(duckdb_adbc())
+  )
+  expect_s3_class(db, "duckdb_database_adbc")
+
+  con <- adbcdrivermanager::local_adbc(
+    adbcdrivermanager::adbc_connection_init(db)
+  )
+  expect_s3_class(con, "duckdb_connection_adbc")
+
+  stmt <- adbcdrivermanager::local_adbc(
+    adbcdrivermanager::adbc_statement_init(con)
+  )
+  expect_s3_class(stmt, "duckdb_statement_adbc")
+
+  stream <- adbcdrivermanager::read_adbc(con, "SELECT 1 as one;")
+  expect_identical(as.data.frame(stream), data.frame(one = 1L))
+})
+
+test_that("DuckDB ADBC classes can be used in adbc_* S4 object slots", {
+  setClass("SomeClass",
+    slots = list(
+      driver = "adbc_driver",
+      database = "adbc_database",
+      connection = "adbc_connection",
+      statement = "adbc_statement"
+    )
+  )
+  on.exit(removeClass("SomeClass"))
+
+  setMethod("initialize", "SomeClass", function(.Object, driver) {
+    .Object@driver <- driver
+    .Object@database <- adbcdrivermanager::adbc_database_init(.Object@driver)
+    .Object@connection <- adbcdrivermanager::adbc_connection_init(.Object@database)
+    .Object@statement <- adbcdrivermanager::adbc_statement_init(.Object@connection)
+    .Object
+  })
+
+  expect_s4_class(new("SomeClass", duckdb_adbc()), "SomeClass")
+})
