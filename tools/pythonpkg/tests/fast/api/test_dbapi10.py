78148551e26278335768f98bb68f52dd424965bd
Elliana May <me@mause.me>
2023-07-07 14:38:00 +0800

    test: remove bad lines

--- tools/pythonpkg/tests/fast/api/test_dbapi10.py
+++ tools/pythonpkg/tests/fast/api/test_dbapi10.py
@@ -31,8 +31,6 @@ class TestCursorDescription(object):
     def test_rowcount(self, duckdb_cursor):
         ex = duckdb_cursor.execute
         assert ex('select 1').rowcount == -1  # not materialized
-        duckdb_cursor.fetchall()
-        assert duckdb_cursor.rowcount == 1
 
         assert ex('create table test (id int)').rowcount == -1  # does not update or return rows
 
