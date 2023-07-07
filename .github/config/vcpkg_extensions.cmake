832f53e1a593ef182b99b3596fff4790b63f8844
Sam Ansmink <samansmink@hotmail.com>
2023-07-07 09:15:25 +0200

    disable iceberg

--- .github/config/vcpkg_extensions.cmake
+++ .github/config/vcpkg_extensions.cmake
@@ -17,8 +17,9 @@ duckdb_extension_load(aws
     GIT_TAG 617a4b1456eec1dee3d668f9ce005a1de9ef21c8
 )
 
-duckdb_extension_load(iceberg
-    LOAD_TESTS
-    GIT_URL https://github.com/duckdblabs/duckdb_iceberg
-    GIT_TAG a2668326dbc402b463ba0d80daacff87eb1ee59b
-)
+# Disabled for now, has various issues that need addressing first
+#duckdb_extension_load(iceberg
+#    LOAD_TESTS
+#    GIT_URL https://github.com/duckdblabs/duckdb_iceberg
+#    GIT_TAG a2668326dbc402b463ba0d80daacff87eb1ee59b
+#)
