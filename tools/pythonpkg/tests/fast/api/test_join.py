d4e5b9a6b0e74197be2be76823c0be76d178f318
Tishj <t_b@live.nl>
2023-07-07 10:42:44 +0200

    assign a uuid as default alias if none is provided

--- tools/pythonpkg/tests/fast/api/test_join.py
+++ tools/pythonpkg/tests/fast/api/test_join.py
@@ -18,6 +18,16 @@ class TestJoin(object):
         rel1 = con.sql("SELECT 1 AS col1, 2 AS col2")
         rel2 = con.sql("SELECT 1 AS col1, 3 AS col3")
 
+        rel = rel1.join(rel2, 'col1')
+        res = rel.fetchall()
+        assert res == [(1, 2, 3)]
+
+    def test_relational_join_alias_collision(self):
+        con = duckdb.connect()
+
+        rel1 = con.sql("SELECT 1 AS col1, 2 AS col2").set_alias('a')
+        rel2 = con.sql("SELECT 1 AS col1, 3 AS col3").set_alias('a')
+
         with pytest.raises(duckdb.InvalidInputException, match='Both relations have the same alias'):
             rel = rel1.join(rel2, 'col1')
 
