583aad7e42cf50215621e7c6f0c2b5046fd81bd0
Tishj <t_b@live.nl>
2023-07-12 14:38:38 +0200

    ran 'black' on 'sql/types.py', explicitly skipped the read_csv test for now

--- tools/pythonpkg/pyduckdb/spark/tests/basic/test_dataframe_read.py
+++ tools/pythonpkg/pyduckdb/spark/tests/basic/test_dataframe_read.py
@@ -1,6 +1,6 @@
 import pytest
 
 class TestSparkSession(object):
+	@pytest.mark.skip("Reading CSV to DataFrame is not implemented yet")
 	def test_read_csv_basic(self, spark):
 		pass
-		#spark.read.csv()
