4b322cad107f3e6b7a68a1e9dd159c2b197a8689
Nick Gerrets <nickgerrets@gmail.com>
2023-07-12 11:05:55 +0200

    fixed assertion

--- src/include/duckdb/common/bitpacking.hpp
+++ src/include/duckdb/common/bitpacking.hpp
@@ -169,7 +169,7 @@ private:
 		if (bitwidth < sizeof(T) * 8 && bitwidth != 0) {
 			if (NumericLimits<T>::IsSigned()) {
 				D_ASSERT(max_value <= (T(1) << (bitwidth - 1)) - 1);
-				D_ASSERT(min_value >= (-1 * ((T(1) << (bitwidth - 1)) - 1) - 1));
+				D_ASSERT(min_value >= (T(-1) * ((T(1) << (bitwidth - 1)) - 1) - 1));
 			} else {
 				D_ASSERT(max_value <= (T(1) << (bitwidth)) - 1);
 			}
