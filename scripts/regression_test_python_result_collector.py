5869930f68b13460c2ce66b982552795476a1821
Tishj <t_b@live.nl>
2023-07-15 22:32:14 +0200

    adapt the regressioon test

--- scripts/regression_test_python_result_collector.py
+++ scripts/regression_test_python_result_collector.py
@@ -23,6 +23,7 @@ if out_file is not None:
 
 con = duckdb.connect()
 
+# Every column in 'test_all_types', written out explicitly so future additions don't cause unexpected breakages
 columns = [
 	'bool',
 	'tinyint',
@@ -69,6 +70,7 @@ columns = [
 	'map'
 ]
 
+# These don't convert correctly (yet) so we exclude them from this test
 excluded = [
 	'date',
 	'timestamp_s',
@@ -80,16 +82,41 @@ excluded = [
 	'timestamp_array',
 	'timestamptz_array',
 ]
-
 columns = [col for col in columns if col not in excluded]
 
-projection = ", ".join(columns)
+# These types are more complex, and that's why the result collection times are longer for them
+complex_types = [
+	col for col in columns if any([x in col for x in [
+		'array',
+		'map',
+		'struct',
+		'uuid'
+	]])
+]
 
-con.execute(f"""
-	create table tbl as select
-		{projection} from (select * from test_all_types() limit 1 offset 1),
-		range(1500000)
-""")
+test_tables = {
+	'regular_types': (
+		[
+			x for x in columns if x not in complex_types
+		],
+		10_000_000
+	),
+	'complex_types': (
+		complex_types,
+		1_500_000
+	)
+}
+
+
+for table_name, data in test_tables.items():
+	list_of_columns, table_size = data
+	projection = ", ".join(list_of_columns)
+	con.execute(f"""
+		create table {table_name} as select
+			{projection} from (select * from test_all_types() limit 1 offset 1),
+			range({table_size})
+	""")
+
 
 result_collectors = {
 	"native": "fetchall",
@@ -99,21 +126,25 @@ result_collectors = {
 }
 
 for name, result_collector in result_collectors.items():
-	for col in columns:
-		times = []
-		for i in range(10):
-			rel = con.table('tbl')[col]
-			start = time.time()
-			res = getattr(rel, result_collector)()
-			del(res)
-			end = time.time()
-			diff = end - start
-			times.append(diff)
-			timing = f"{name}_{col}\t{i}\t{diff}"
-			if out_file is not None:
-				f.write(timing)
-				f.write('\n')
-			else:
-				print(timing)
-		average = mean(times)
-		print(f"\t{name}\t{col}\taverage:\t{average}")
+	for table_name, data in test_tables.items():
+		list_of_columns, _ = data
+		print(table_name)
+		print(list_of_columns)
+		for col in list_of_columns:
+			times = []
+			for i in range(10):
+				rel = con.table(table_name)[col]
+				start = time.time()
+				res = getattr(rel, result_collector)()
+				del(res)
+				end = time.time()
+				diff = end - start
+				times.append(diff)
+				timing = f"{name}_{col}\t{i}\t{diff}"
+				if out_file is not None:
+					f.write(timing)
+					f.write('\n')
+				else:
+					print(timing)
+			average = mean(times)
+			print(f"\t{name}\t{col}\taverage:\t{average}")
