86965f27bc255d50681a96ca9008024932cc106e
Tishj <t_b@live.nl>
2023-07-15 21:37:10 +0200

    update the way the examples are imported

--- tools/pythonpkg/tests/fast/test_examples.py
+++ tools/pythonpkg/tests/fast/test_examples.py
@@ -1,13 +1,24 @@
-import pytest
-
-import subprocess
 import os
+import sys
+import importlib
 
 class TestExamples(object):
 	def test_examples(self):
-		current_dir = os.path.dirname(os.path.abspath(__file__))
-		relative_path = "../../../../examples/python/duckdb-python.py"
-		absolute_path = os.path.abspath(os.path.join(current_dir, relative_path))
-		result = subprocess.run(['python3', absolute_path])
-		assert result.returncode == 0
-		assert not result.stderr
+		# In CI runs the structure is slightly different, hence the two different paths
+		relative_paths = [
+			'../../../../',
+			'../../../../../'
+		]
+		script_dir = os.path.dirname(os.path.abspath(__file__))
+		# Append the folder of the module to the paths
+		paths = [
+			os.path.join(path, 'examples/python') for path in relative_paths
+		]
+		# Prepend the directory of the script to the paths
+		paths = [
+			os.path.abspath(os.path.join(script_dir, path)) for path in paths
+		]
+		sys.path.extend(paths)
+		
+		# Import the examples module, which subsequently runs
+		import duckdb_python
