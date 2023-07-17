df46cd9012918559d2d025c34d64bcba7abc48fd
Tishj <t_b@live.nl>
2023-07-17 15:11:26 +0200

    deal with cibuildwheel issues

--- tools/pythonpkg/tests/fast/test_examples.py
+++ tools/pythonpkg/tests/fast/test_examples.py
@@ -3,22 +3,27 @@ import sys
 import importlib
 
 class TestExamples(object):
-	def test_examples(self):
-		# In CI runs the structure is slightly different, hence the two different paths
-		relative_paths = [
-			'../../../../',
-			'../../../../../'
-		]
-		script_dir = os.path.dirname(os.path.abspath(__file__))
-		# Append the folder of the module to the paths
-		paths = [
-			os.path.join(path, 'examples/python') for path in relative_paths
-		]
-		# Prepend the directory of the script to the paths
-		paths = [
-			os.path.abspath(os.path.join(script_dir, path)) for path in paths
-		]
-		sys.path.extend(paths)
-		
-		# Import the examples module, which subsequently runs
-		import duckdb_python
+    def test_examples(self):
+        relative_paths = [
+            '../../../../',
+            ''
+        ]
+        if 'CIBW_ROOT_DIR' in os.environ:
+            # This is being called from the CI by cibuildhweel
+            # the project+tests have been copied to another location, this variable contains the working dir where cibuildwheel was called from
+            root_dir = os.getenv('CIBW_ROOT_DIR')
+        else:
+            root_dir = os.path.dirname(os.path.abspath(__file__))
+        # Append the folder of the module to the paths
+        paths = [
+            os.path.join(path, 'examples/python') for path in relative_paths
+        ]
+        # Prepend the directory of the script to the paths
+        paths = [
+            os.path.abspath(os.path.join(root_dir, path)) for path in paths
+        ]
+        print(paths)
+        sys.path.extend(paths)
+
+        # Import the examples module, which subsequently runs
+        import duckdb_python
