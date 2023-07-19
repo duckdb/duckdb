f978d634028a6edb625e7bbc5f6d6bbbee7c58c5
taniabogatsch <tani.bogatsch@gmail.com>
2023-07-19 09:51:09 +0200

    better struct init

--- src/include/duckdb/common/vector_operations/aggregate_executor.hpp
+++ src/include/duckdb/common/vector_operations/aggregate_executor.hpp
@@ -18,10 +18,10 @@ namespace duckdb {
 // structs
 struct AggregateInputData;
 struct FrameBounds {
-	FrameBounds() {};
+	FrameBounds() : start(0), end(0) {};
 	FrameBounds(idx_t start, idx_t end) : start(start), end(end) {};
-	idx_t start;
-	idx_t end;
+	idx_t start = 0;
+	idx_t end = 0;
 };
 
 class AggregateExecutor {
