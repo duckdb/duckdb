d4f9aed58c1ab1324b622788c4591fcdabcb405f
maiadegraaf <68693691+maiadegraaf@users.noreply.github.com>
2023-07-20 14:05:32 +0200

    Finish cursor_commit.cpp

--- tools/odbc/test/tests/cursor_commit.cpp
+++ tools/odbc/test/tests/cursor_commit.cpp
@@ -7,7 +7,12 @@ using namespace odbc_test;
 /**
 	 * Execute a query that generates a result set
  */
-static void BasicCursorCommitTest(HSTMT &hstmt) {
+static void SimpleCursorCommitTest(SQLHANDLE dbc) {
+	HSTMT hstmt = SQL_NULL_HSTMT;
+
+	// Allocate a statement handle
+	EXECUTE_AND_CHECK("SQLAllocHandle (HSTMT)", SQLAllocHandle, SQL_HANDLE_STMT, dbc, &hstmt);
+
 	EXECUTE_AND_CHECK("SQLSetStmtAttr", SQLSetStmtAttr, hstmt, SQL_ATTR_CURSOR_TYPE, ConvertToSQLPOINTER(SQL_CURSOR_STATIC), SQL_IS_UINTEGER);
 
 	EXECUTE_AND_CHECK("SQLExecDirect", SQLExecDirect, hstmt,
@@ -27,28 +32,87 @@ static void BasicCursorCommitTest(HSTMT &hstmt) {
 		REQUIRE(STR_EQUAL(buf, ConvertToCString(i)));
 	}
 
+	SQLRETURN ret = SQLFetchScroll(hstmt, SQL_FETCH_NEXT, 0);
+	REQUIRE(ret == SQL_NO_DATA);
+
 	EXECUTE_AND_CHECK("SQLFreeStmt (HSTMT)", SQLFreeStmt, hstmt, SQL_CLOSE);
+	EXECUTE_AND_CHECK("SQLFreeHandle (HSTMT)", SQLFreeHandle, SQL_HANDLE_STMT, hstmt);
+}
+
+static void PreparedCursorCommitTest(SQLHANDLE dbc) {
+	HSTMT hstmt = SQL_NULL_HSTMT;
+
+	// Allocate a statement handle
+	EXECUTE_AND_CHECK("SQLAllocHandle (HSTMT)", SQLAllocHandle, SQL_HANDLE_STMT, dbc, &hstmt);
+
+	// Try to commit without an open query
+	EXECUTE_AND_CHECK("SQLEndTran", SQLEndTran, SQL_HANDLE_DBC, dbc, SQL_COMMIT);
+
+	// Prepare a statement
+	EXECUTE_AND_CHECK("SQLPrepare", SQLPrepare, hstmt, ConvertToSQLCHAR("SELECT ?::BOOL"), SQL_NTS);
+
+	// Commit with a prepared statement
+	EXECUTE_AND_CHECK("SQLEndTran", SQLEndTran, SQL_HANDLE_DBC, dbc, SQL_COMMIT);
+
+	EXECUTE_AND_CHECK("SQLFreeStmt (HSTMT)", SQLFreeStmt, hstmt, SQL_CLOSE);
+	EXECUTE_AND_CHECK("SQLFreeHandle (HSTMT)", SQLFreeHandle, SQL_HANDLE_STMT, hstmt);
+}
+
+static void MultipleHSTMTTest(SQLHANDLE dbc) {
+	HSTMT hstmt1 = SQL_NULL_HSTMT;
+	HSTMT hstmt2 = SQL_NULL_HSTMT;
+
+	// Allocate a statement handles
+	EXECUTE_AND_CHECK("SQLAllocHandle (HSTMT)", SQLAllocHandle, SQL_HANDLE_STMT, dbc, &hstmt1);
+	EXECUTE_AND_CHECK("SQLAllocHandle (HSTMT)", SQLAllocHandle, SQL_HANDLE_STMT, dbc, &hstmt2);
+
+	// Execute queries on both statement handles
+	EXECUTE_AND_CHECK("SQLExecDirect", SQLExecDirect, hstmt1,
+	                  ConvertToSQLCHAR("SELECT g FROM generate_series(1,3) g(g)"),
+	                  SQL_NTS);
+	EXECUTE_AND_CHECK("SQLExecDirect", SQLExecDirect, hstmt2,
+	                  ConvertToSQLCHAR("SELECT g FROM generate_series(1,3) g(g)"),
+	                  SQL_NTS);
+
+	// Free first statement handle
+	EXECUTE_AND_CHECK("SQLFreeStmt (HSTMT)", SQLFreeStmt, hstmt1, SQL_CLOSE);
+	EXECUTE_AND_CHECK("SQLFreeHandle (HSTMT)", SQLFreeHandle, SQL_HANDLE_STMT, hstmt1);
+
+	// Commit test after the first handle is released
+	EXECUTE_AND_CHECK("SQLEndTran", SQLEndTran, SQL_HANDLE_DBC, dbc, SQL_COMMIT);
+
+	SQLINTEGER  buf;
+	SQLLEN buf_len;
+
+	EXECUTE_AND_CHECK("SQLBindCol", SQLBindCol, hstmt2, 1, SQL_C_SLONG, &buf, sizeof(buf), &buf_len);
+
+	// Fetch from the second statement handle
+	for (int i = 1; i < 4; i++) {
+		EXECUTE_AND_CHECK("SQLFetchScroll", SQLFetchScroll, hstmt2, SQL_FETCH_NEXT, 0);
+		REQUIRE(buf_len == sizeof(buf));
+		REQUIRE(buf == i);
+	}
+
+	SQLRETURN ret = SQLFetchScroll(hstmt2, SQL_FETCH_NEXT, 0);
+	REQUIRE(ret == SQL_NO_DATA);
+
+	// Free second statement handle
+	EXECUTE_AND_CHECK("SQLFreeStmt (HSTMT)", SQLFreeStmt, hstmt2, SQL_CLOSE);
+	EXECUTE_AND_CHECK("SQLFreeHandle (HSTMT)", SQLFreeHandle, SQL_HANDLE_STMT, hstmt2);
 }
 
 TEST_CASE("Test Cursor Commit", "[odbc]") {
 	SQLHANDLE env;
 	SQLHANDLE dbc;
 
-	HSTMT hstmt = SQL_NULL_HSTMT;
-
 	// Connect to the database using SQLConnect
 	CONNECT_TO_DATABASE(env, dbc);
 
 	EXECUTE_AND_CHECK("SQLSetConnectAttr", SQLSetConnectAttr, dbc, SQL_ATTR_AUTOCOMMIT, (SQLPOINTER)SQL_AUTOCOMMIT_OFF, SQL_IS_INTEGER);
 
-	// Allocate a statement handle
-	EXECUTE_AND_CHECK("SQLAllocHandle (HSTMT)", SQLAllocHandle, SQL_HANDLE_STMT, dbc, &hstmt);
-
-	BasicCursorCommitTest(hstmt);
-
-	// Free the statement handle
-	EXECUTE_AND_CHECK("SQLFreeStmt (HSTMT)", SQLFreeStmt, hstmt, SQL_CLOSE);
-	EXECUTE_AND_CHECK("SQLFreeHandle (HSTMT)", SQLFreeHandle, SQL_HANDLE_STMT, hstmt);
+	SimpleCursorCommitTest(dbc);
+	PreparedCursorCommitTest(dbc);
+	MultipleHSTMTTest(dbc);
 
 	DISCONNECT_FROM_DATABASE(env, dbc);
 }
