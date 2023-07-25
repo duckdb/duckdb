3fbf3931dccbb3368276529f4c2e2281d5f07f8a
Pedro Holanda <pedroholanda@gmail.com>
2023-07-25 14:43:51 +0200

    Add sample read test

--- tools/pythonpkg/tests/fast/test_adbc.py
+++ tools/pythonpkg/tests/fast/test_adbc.py
@@ -1,7 +1,7 @@
 import duckdb
 import pytest
 import sys
-
+import datetime
 
 adbc_driver_manager = pytest.importorskip('adbc_driver_manager.dbapi')
 adbc_driver_manager_lib = pytest.importorskip('adbc_driver_manager._lib')
@@ -10,6 +10,8 @@ pyarrow = pytest.importorskip('pyarrow')
 
 
 def test_insertion():
+    if sys.platform.startswith("win"):
+        pytest.xfail("Not supported on Windows")
     con = adbc_driver_manager.connect(driver=duckdb.duckdb.__file__, entrypoint="duckdb_adbc_init")
 
     table = pyarrow.table(
@@ -43,19 +45,48 @@ def test_insertion():
         assert cursor.fetch_arrow_table().to_pydict() == {'count_star()': [8]}
 
 
-# def test_read():
-#     con = adbc_driver_manager.connect(driver='/Users/holanda/Documents/Projects/duckdb/build/debug/src/libduckdb.dylib', entrypoint="duckdb_adbc_init")
-
-#     table = pyarrow.table(
-#             [
-#                 [1, 2, 3, 4],
-#                 ["a", "b", None, "d"],
-#             ],
-#             names=["ints", "strs"],
-#         )
-#     reader = table.to_reader()
-
-#     with con.cursor() as cursor:
-#         cursor.adbc_ingest("ingest", reader,"create")
-#         cursor.execute("SELECT * FROM ingest")
-#         assert cursor.fetch_arrow_table() == table
+def test_read():
+    if sys.platform.startswith("win"):
+        pytest.xfail("Not supported on Windows")
+    con = adbc_driver_manager.connect(driver=duckdb.duckdb.__file__, entrypoint="duckdb_adbc_init")
+    with con.cursor() as cursor:
+        cursor.execute("SELECT * FROM 'data/category.csv'")
+        assert cursor.fetch_arrow_table().to_pydict() == {
+            'CATEGORY_ID': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            'NAME': [
+                'Action',
+                'Animation',
+                'Children',
+                'Classics',
+                'Comedy',
+                'Documentary',
+                'Drama',
+                'Family',
+                'Foreign',
+                'Games',
+                'Horror',
+                'Music',
+                'New',
+                'Sci-Fi',
+                'Sports',
+                'Travel',
+            ],
+            'LAST_UPDATE': [
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+                datetime.datetime(2006, 2, 15, 4, 46, 27),
+            ],
+        }
