0b3f8f537ce6e682d1f5e222ed00761d4f925eac
Sam Ansmink <samansmink@hotmail.com>
2023-07-25 22:23:55 +0200

    format

--- scripts/apply_extension_patches.py
+++ scripts/apply_extension_patches.py
@@ -12,15 +12,15 @@ patches = glob.glob(patch_pattern)
 
 # Exit if no patches are found
 if not patches:
-  error_message = (
-    f"\nERROR: Extension patching enabled, but no patches found in '{directory}'. "
-    "Please make sure APPLY_PATCHES is only enabled when there are actually patches present. "
-    "See .github/patches/extensions/README.md for more details.\n"
-  )
-  sys.stderr.write(error_message)
-  sys.exit(1)
+    error_message = (
+        f"\nERROR: Extension patching enabled, but no patches found in '{directory}'. "
+        "Please make sure APPLY_PATCHES is only enabled when there are actually patches present. "
+        "See .github/patches/extensions/README.md for more details.\n"
+    )
+    sys.stderr.write(error_message)
+    sys.exit(1)
 
 # Apply each patch file using git apply
 for patch in patches:
-  print(f"Applying patch: {patch}")
-  subprocess.run(["git", "apply", "--ignore-space-change", "--ignore-whitespace", patch])
+    print(f"Applying patch: {patch}")
+    subprocess.run(["git", "apply", "--ignore-space-change", "--ignore-whitespace", patch])
