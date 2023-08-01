9bbe84dca249495faadd8e36f19e9cdf3f8debe2
Tom Ebergen <tom@ebergen.com>
2023-08-01 11:42:37 +0200

    fix slow benchmarks

--- src/optimizer/join_order/relation_manager.cpp
+++ src/optimizer/join_order/relation_manager.cpp
@@ -78,8 +78,6 @@ static bool OperatorNeedsRelation(LogicalOperatorType op_type) {
 	case LogicalOperatorType::LOGICAL_DELIM_GET:
 	case LogicalOperatorType::LOGICAL_AGGREGATE_AND_GROUP_BY:
 	case LogicalOperatorType::LOGICAL_WINDOW:
-	case LogicalOperatorType::LOGICAL_FILTER:
-		return true;
 	default:
 		return false;
 	}
