80ad450571dcaecf5bbdc477b0f99aea9add314e
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-08-03 13:58:58 +0800

    Add files via upload

--- src/include/duckdb/optimizer/cascade/optimizer/COptimizerConfig.h
+++ src/include/duckdb/optimizer/cascade/optimizer/COptimizerConfig.h
@@ -1,25 +1,17 @@
 //---------------------------------------------------------------------------
-//	Greenplum Database
-//	Copyright (C) 2018 Pivotal, Inc.
-//
 //	@filename:
 //		COptimizerConfig.h
 //
 //	@doc:
 //		Configurations used by the optimizer
 //---------------------------------------------------------------------------
-
 #ifndef GPOPT_COptimizerConfig_H
 #define GPOPT_COptimizerConfig_H
 
 #include "duckdb/optimizer/cascade/base.h"
-#include "duckdb/optimizer/cascade/common/CDynamicPtrArray.h"
-#include "duckdb/optimizer/cascade/common/CRefCount.h"
-#include "duckdb/optimizer/cascade/engine/CCTEConfig.h"
 #include "duckdb/optimizer/cascade/engine/CEnumeratorConfig.h"
 #include "duckdb/optimizer/cascade/engine/CHint.h"
 #include "duckdb/optimizer/cascade/engine/CStatisticsConfig.h"
-#include "duckdb/optimizer/cascade/base/CWindowOids.h"
 
 namespace gpopt
 {
@@ -43,83 +35,33 @@ class ICostModel;
 //		<dxl:OptimizerConfig> element
 //
 //---------------------------------------------------------------------------
-class COptimizerConfig : public CRefCount
+class COptimizerConfig
 {
-private:
+public:
 	// plan enumeration configuration
-	CEnumeratorConfig *m_enumerator_cfg;
+	CEnumeratorConfig* m_enumerator_cfg;
 
 	// statistics configuration
-	CStatisticsConfig *m_stats_conf;
-
-	// CTE configuration
-	CCTEConfig *m_cte_conf;
+	CStatisticsConfig* m_stats_conf;
 
 	// cost model configuration
-	ICostModel *m_cost_model;
+	ICostModel* m_cost_model;
 
 	// hint configuration
-	CHint *m_hint;
-
-	// default window oids
-	CWindowOids *m_window_oids;
+	CHint* m_hint;
 
 public:
 	// ctor
-	COptimizerConfig(CEnumeratorConfig *pec, CStatisticsConfig *stats_config, CCTEConfig *pcteconf, ICostModel *pcm, CHint *phint, CWindowOids *pdefoidsGPDB);
+	COptimizerConfig(CEnumeratorConfig* pec, CStatisticsConfig* stats_config, ICostModel* cost_model, CHint* phint);
 
 	// dtor
 	virtual ~COptimizerConfig();
 
-	// plan enumeration configuration
-	CEnumeratorConfig *
-	GetEnumeratorCfg() const
-	{
-		return m_enumerator_cfg;
-	}
-
-	// statistics configuration
-	CStatisticsConfig *
-	GetStatsConf() const
-	{
-		return m_stats_conf;
-	}
-
-	// CTE configuration
-	CCTEConfig *
-	GetCteConf() const
-	{
-		return m_cte_conf;
-	}
-
-	// cost model configuration
-	ICostModel *
-	GetCostModel() const
-	{
-		return m_cost_model;
-	}
-
-	// default window oids
-	CWindowOids *
-	GetWindowOids() const
-	{
-		return m_window_oids;
-	}
-
-	// hint configuration
-	CHint *
-	GetHint() const
-	{
-		return m_hint;
-	}
-
 	// generate default optimizer configurations
-	static COptimizerConfig *PoconfDefault(CMemoryPool *mp);
+	static COptimizerConfig* PoconfDefault();
 
 	// generate default optimizer configurations with the given cost model
-	static COptimizerConfig *PoconfDefault(CMemoryPool *mp, ICostModel *pcm);
+	static COptimizerConfig* PoconfDefault(ICostModel* pcm);
 };	// class COptimizerConfig
-
 }  // namespace gpopt
-
 #endif
