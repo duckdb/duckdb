1632169645cb1d0954bbaf3c2ec562e235d0274f
Sam Ansmink <samansmink@hotmail.com>
2023-08-04 11:00:14 +0200

    update aws and azure

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -20,14 +20,14 @@ endif()
 duckdb_extension_load(aws
         LOAD_TESTS
         GIT_URL https://github.com/duckdblabs/duckdb_aws
-        GIT_TAG 617a4b1456eec1dee3d668f9ce005a1de9ef21c8
+        GIT_TAG b769c8aab638bdbdc46c77fcb3c70412dd715d45
         )
 
 ################# AZURE
 duckdb_extension_load(azure
         LOAD_TESTS
         GIT_URL https://github.com/duckdblabs/duckdb_azure
-        GIT_TAG 7cd5149ee879f3ea9e0a9215e0739643dd75eb6e
+        GIT_TAG 1fe568d3eb3c8842118e395ba8031e2a8566daed
         )
 
 ################# ICEBERG
