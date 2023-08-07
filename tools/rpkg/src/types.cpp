1b6b07b01b8e84fa21e49ebcb8ec5d0dea22b5ce
Kirill Müller <kirill@cynkra.com>
2023-08-07 18:11:34 +0200

    Logical type for STRUCT

--- tools/rpkg/src/types.cpp
+++ tools/rpkg/src/types.cpp
@@ -243,6 +243,17 @@ LogicalType RApiTypes::LogicalTypeFromRType(const RType &rtype, bool experimenta
 		return LogicalType::BLOB;
 	case RTypeId::LIST:
 		return LogicalType::LIST(RApiTypes::LogicalTypeFromRType(rtype.GetListChildType(), experimental));
+	case RTypeId::STRUCT: {
+		child_list_t<LogicalType> children;
+		for (auto child : rtype.GetStructChildTypes()) {
+			children.push_back(
+			    std::make_pair(child.first, RApiTypes::LogicalTypeFromRType(child.second, experimental)));
+		}
+		if (children.size() == 0) {
+			cpp11::stop("rapi_execute: Packed column must have at least one column");
+		}
+		return LogicalType::STRUCT(std::move(children));
+	}
 
 	default:
 		cpp11::stop("rapi_execute: Can't convert R type to logical type");
