1a63874d10546a276181aa31659553c8a5eb67b9
Kirill Müller <kirill@cynkra.com>
2023-08-08 16:33:18 +0200

    Pull out of loop

--- tools/rpkg/src/scan.cpp
+++ tools/rpkg/src/scan.cpp
@@ -86,12 +86,12 @@ void AppendListColumnSegment(const RType &rtype, SEXP *source_data, idx_t sexp_o
 	source_data += sexp_offset;
 	auto &result_mask = FlatVector::Validity(result);
 	auto child_rtype = rtype.GetListChildType();
+	auto result_data = FlatVector::GetData<list_entry_t>(result);
 	for (idx_t i = 0; i < count; i++) {
 		auto val = source_data[i];
 		if (RSexpType::IsNull(val)) {
 			result_mask.SetInvalid(i);
 		} else {
-			auto result_data = FlatVector::GetData<list_entry_t>(result);
 			auto len = RApiTypes::GetVecSize(child_rtype, val);
 			result_data[i].offset = ListVector::GetListSize(result);
 			for (R_len_t i = 0; i < len; i++) {
