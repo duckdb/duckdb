118eb9f54cb191c24195098f719e9816e20b68d5
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-08-09 16:23:12 +0800

    Add files via upload

---
+++ src/optimizer/cascade/xforms/CXformOrderImplementation.cpp
@@ -0,0 +1,66 @@
+//---------------------------------------------------------------------------
+//	@filename:
+//		CXformOrderImplementation.cpp
+//
+//	@doc:
+//		Implementation of transform
+//---------------------------------------------------------------------------
+#include "duckdb/optimizer/cascade/xforms/CXformOrderImplementation.h"
+#include "duckdb/optimizer/cascade/base.h"
+#include "duckdb/planner/operator/logical_order.hpp"
+#include "duckdb/execution/operator/order/physical_order.hpp"
+#include "duckdb/optimizer/cascade/operators/CPatternLeaf.h"
+
+using namespace gpopt;
+
+//---------------------------------------------------------------------------
+//	@function:
+//		CXformOrderImplementation::CXformOrderImplementation
+//
+//	@doc:
+//		Ctor
+//
+//---------------------------------------------------------------------------
+CXformOrderImplementation::CXformOrderImplementation()
+    :CXformImplementation(make_uniq<LogicalOrder>(duckdb::vector<BoundOrderByNode>()))
+{
+}
+
+//---------------------------------------------------------------------------
+//	@function:
+//		CXformGet2TableScan::Exfp
+//
+//	@doc:
+//		Compute promise of xform
+//
+//---------------------------------------------------------------------------
+CXform::EXformPromise CXformOrderImplementation::Exfp(CExpressionHandle &exprhdl) const
+{
+	return CXform::ExfpMedium;
+}
+
+//---------------------------------------------------------------------------
+//	@function:
+//		CXformGet2TableScan::Transform
+//
+//	@doc:
+//		Actual transformation
+//
+//---------------------------------------------------------------------------
+void CXformOrderImplementation::Transform(CXformContext* pxfctxt, CXformResult* pxfres, Operator* pexpr) const
+{
+	LogicalOrder* popOrder = (LogicalOrder*)pexpr;
+    duckdb::vector<BoundOrderByNode> vorders;
+    for(auto &child : popOrder->orders)
+    {
+        vorders.push_back(child.Copy());
+    }
+	// create alternative expression
+	duckdb::unique_ptr<Operator> pexprAlt = make_uniq<PhysicalOrder>(popOrder->types, popOrder->orders, popOrder->projections, popOrder->estimated_cardinality);
+    for(auto &child : pexpr->children)
+    {
+        pexprAlt->AddChild(child->Copy());
+    }
+	// add alternative to transformation result
+	pxfres->Add(std::move(pexprAlt));
+}
