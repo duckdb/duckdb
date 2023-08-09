feff103b8ef584cc347b3d8c32178be7c8ee5b23
zzjjyyy <72371740+zzjjyyy@users.noreply.github.com>
2023-08-09 14:05:19 +0800

    Add files via upload

--- src/include/duckdb/optimizer/cascade/cost/ICostModel.h
+++ src/include/duckdb/optimizer/cascade/cost/ICostModel.h
@@ -9,28 +9,19 @@
 #define GPOPT_ICostModel_H
 
 #include "duckdb/optimizer/cascade/base.h"
-#include "duckdb/optimizer/cascade/common/CRefCount.h"
-#include "duckdb/optimizer/cascade/operators/COperator.h"
-#include "duckdb/optimizer/cascade/md/IMDRelation.h"
-#include "duckdb/optimizer/cascade/statistics/IStatistics.h"
-#include "duckdb/optimizer/cascade/cost/CCost.h"
 #include "duckdb/optimizer/cascade/cost/ICostModelParams.h"
-#include "duckdb/optimizer/cascade/common/CDynamicPtrArray.h"
+#include "duckdb/common/vector.hpp"
 
 // default number of rebinds (number of times a plan is executed due to rebinding to external parameters)
 #define GPOPT_DEFAULT_REBINDS 1
+
+using namespace duckdb;
+using namespace gpos;
 
 namespace gpopt
 {
 // fwd declarations
 class CExpressionHandle;
-
-using namespace gpos;
-using namespace gpmd;
-using namespace gpnaucrates;
-
-// dynamic array of cost model params
-typedef CDynamicPtrArray<ICostModelParams::SCostParam, CleanupDelete> ICostModelParamsArray;
 
 //---------------------------------------------------------------------------
 //	@class:
@@ -40,57 +31,11 @@ typedef CDynamicPtrArray<ICostModelParams::SCostParam, CleanupDelete> ICostModel
 //		Interface for the underlying cost model
 //
 //---------------------------------------------------------------------------
-class ICostModel : public CRefCount
+class ICostModel
 {
 public:
 	enum ECostModelType
-	{
-		EcmtGPDBLegacy = 0,
-		EcmtGPDBCalibrated = 1,
-		EcmtSentinel = 2
-	};
-
-	//---------------------------------------------------------------------------
-	//	@class:
-	//		SCostingStas
-	//
-	//	@doc:
-	//		Stast information used during cost computation
-	//
-	//---------------------------------------------------------------------------
-	class CCostingStats : public CRefCount
-	{
-	private:
-		// stats of the root
-		IStatistics *m_pstats;
-
-	public:
-		// ctor
-		CCostingStats(IStatistics *stats) : m_pstats(stats)
-		{
-			GPOS_ASSERT(NULL != stats);
-		}
-
-		// dtor
-		~CCostingStats()
-		{
-			m_pstats->Release();
-		}
-
-		// the risk of errors in cardinality estimation
-		ULONG
-		StatsEstimationRisk() const
-		{
-			return m_pstats->StatsEstimationRisk();
-		}
-
-		// look up the number of distinct values of a particular column
-		CDouble
-		GetNDVs(const CColRef *colref)
-		{
-			return m_pstats->GetNDVs(colref);
-		}
-	};	// class CCostingStats
+	{ EcmtGPDBLegacy = 0, EcmtGPDBCalibrated = 1, EcmtSentinel = 2 };
 
 	//---------------------------------------------------------------------------
 	//	@class:
@@ -102,236 +47,114 @@ public:
 	//---------------------------------------------------------------------------
 	struct SCostingInfo
 	{
-	private:
+	public:
 		// number of children excluding scalar children
 		ULONG m_ulChildren;
 
-		// stats of the root
-		CCostingStats *m_pcstats;
-
 		// row estimate of root
-		DOUBLE m_rows;
+		double m_rows;
 
 		// width estimate of root
-		DOUBLE m_width;
+		double m_width;
 
 		// number of rebinds of root
-		DOUBLE m_num_rebinds;
+		double m_num_rebinds;
 
 		// row estimates of child operators
-		DOUBLE *m_pdRowsChildren;
+		double* m_pdRowsChildren;
 
 		// width estimates of child operators
-		DOUBLE *m_pdWidthChildren;
+		double* m_pdWidthChildren;
 
 		// number of rebinds of child operators
-		DOUBLE *m_pdRebindsChildren;
+		double* m_pdRebindsChildren;
 
 		// computed cost of child operators
-		DOUBLE *m_pdCostChildren;
-
-		// stats of the children
-		CCostingStats **m_pdrgstatsChildren;
+		double* m_pdCostChildren;
 
 	public:
 		// ctor
-		SCostingInfo(CMemoryPool *mp, ULONG ulChildren, CCostingStats *pcstats)
-			: m_ulChildren(ulChildren),
-			  m_pcstats(pcstats),
-			  m_rows(0),
-			  m_width(0),
-			  m_num_rebinds(GPOPT_DEFAULT_REBINDS),
-			  m_pdRowsChildren(NULL),
-			  m_pdWidthChildren(NULL),
-			  m_pdRebindsChildren(NULL),
-			  m_pdCostChildren(NULL),
-			  m_pdrgstatsChildren(NULL)
+		SCostingInfo(ULONG ulChildren)
+			: m_ulChildren(ulChildren), m_rows(0), m_width(0), m_num_rebinds(GPOPT_DEFAULT_REBINDS), m_pdRowsChildren(NULL), m_pdWidthChildren(NULL), m_pdRebindsChildren(NULL), m_pdCostChildren(NULL)
 		{
-			GPOS_ASSERT(NULL != pcstats);
 			if (0 < ulChildren)
 			{
-				m_pdRowsChildren = GPOS_NEW_ARRAY(mp, DOUBLE, ulChildren);
-				m_pdWidthChildren = GPOS_NEW_ARRAY(mp, DOUBLE, ulChildren);
-				m_pdRebindsChildren = GPOS_NEW_ARRAY(mp, DOUBLE, ulChildren);
-				m_pdCostChildren = GPOS_NEW_ARRAY(mp, DOUBLE, ulChildren);
-				m_pdrgstatsChildren =
-					GPOS_NEW_ARRAY(mp, CCostingStats *, ulChildren);
-
-				for (ULONG ul = 0; ul < m_ulChildren; ul++)
-				{
-					m_pdrgstatsChildren[ul] = NULL;
-				}
+				m_pdRowsChildren = new double[ulChildren];
+				m_pdWidthChildren = new double[ulChildren];
+				m_pdRebindsChildren = new double[ulChildren];
+				m_pdCostChildren = new double[ulChildren];
 			}
 		}
 
 		// dtor
 		~SCostingInfo()
 		{
-			GPOS_DELETE_ARRAY(m_pdRowsChildren);
-			GPOS_DELETE_ARRAY(m_pdWidthChildren);
-			GPOS_DELETE_ARRAY(m_pdRebindsChildren);
-			GPOS_DELETE_ARRAY(m_pdCostChildren);
-
-			for (ULONG ul = 0; ul < m_ulChildren; ul++)
-			{
-				CRefCount::SafeRelease(m_pdrgstatsChildren[ul]);
-			}
-
-			GPOS_DELETE_ARRAY(m_pdrgstatsChildren);
-			m_pcstats->Release();
-		}
-
-		// children accessor
-		ULONG
-		ChildCount() const
-		{
-			return m_ulChildren;
-		}
-
-		// rows accessor
-		DOUBLE
-		Rows() const
-		{
-			return m_rows;
+			delete[] m_pdRowsChildren;
+			delete[] m_pdWidthChildren;
+			delete[] m_pdRebindsChildren;
+			delete[] m_pdCostChildren;
 		}
 
 		// rows setter
-		void
-		SetRows(DOUBLE rows)
+		void SetRows(double rows)
 		{
-			GPOS_ASSERT(0 <= rows);
-
 			m_rows = rows;
 		}
 
-		// width accessor
-		DOUBLE
-		Width() const
-		{
-			return m_width;
-		}
-
 		// width setter
-		void
-		SetWidth(DOUBLE width)
+		void SetWidth(double width)
 		{
-			GPOS_ASSERT(0 <= width);
-
 			m_width = width;
 		}
 
-		// rebinds accessor
-		DOUBLE NumRebinds() const
-		{
-			return m_num_rebinds;
-		}
-
 		// rebinds setter
-		void SetRebinds(DOUBLE num_rebinds)
+		void SetRebinds(double num_rebinds)
 		{
-			GPOS_ASSERT(GPOPT_DEFAULT_REBINDS <= num_rebinds);
-
 			m_num_rebinds = num_rebinds;
 		}
 
-		// children rows accessor
-		DOUBLE* PdRows() const
-		{
-			return m_pdRowsChildren;
-		}
-
 		// child rows setter
-		void SetChildRows(ULONG ulPos, DOUBLE dRowsChild)
+		void SetChildRows(ULONG ulPos, double dRowsChild)
 		{
-			GPOS_ASSERT(0 <= dRowsChild);
-			GPOS_ASSERT(ulPos < m_ulChildren);
-
 			m_pdRowsChildren[ulPos] = dRowsChild;
 		}
 
-		// children width accessor
-		DOUBLE* GetWidth() const
-		{
-			return m_pdWidthChildren;
-		}
-
 		// child width setter
-		void SetChildWidth(ULONG ulPos, DOUBLE dWidthChild)
+		void SetChildWidth(ULONG ulPos, double dWidthChild)
 		{
-			GPOS_ASSERT(0 <= dWidthChild);
-			GPOS_ASSERT(ulPos < m_ulChildren);
 			m_pdWidthChildren[ulPos] = dWidthChild;
 		}
 
-		// children rebinds accessor
-		DOUBLE* PdRebinds() const
-		{
-			return m_pdRebindsChildren;
-		}
-
 		// child rebinds setter
-		void SetChildRebinds(ULONG ulPos, DOUBLE dRebindsChild)
+		void SetChildRebinds(ULONG ulPos, double dRebindsChild)
 		{
-			GPOS_ASSERT(GPOPT_DEFAULT_REBINDS <= dRebindsChild);
-			GPOS_ASSERT(ulPos < m_ulChildren);
 			m_pdRebindsChildren[ulPos] = dRebindsChild;
 		}
 
-		// children cost accessor
-		DOUBLE* PdCost() const
-		{
-			return m_pdCostChildren;
-		}
-
 		// child cost setter
-		void SetChildCost(ULONG ulPos, DOUBLE dCostChild)
+		void SetChildCost(ULONG ulPos, double dCostChild)
 		{
-			GPOS_ASSERT(0 <= dCostChild);
-			GPOS_ASSERT(ulPos < m_ulChildren);
 			m_pdCostChildren[ulPos] = dCostChild;
 		}
-
-		// child stats setter
-		void SetChildStats(ULONG ulPos, CCostingStats *child_stats)
-		{
-			m_pdrgstatsChildren[ulPos] = child_stats;
-		}
-
-		// return additional cost statistics
-		CCostingStats* Pcstats() const
-		{
-			return m_pcstats;
-		}
-
-		// return additional child statistics
-		CCostingStats* Pcstats(ULONG child_index) const
-		{
-			return m_pdrgstatsChildren[child_index];
-		}
-
 	};	// struct SCostingInfo
 
 	// return number of hosts (nodes) that store data
 	virtual ULONG UlHosts() const = 0;
 
-	// return number of rows per host
-	virtual CDouble DRowsPerHost(CDouble dRowsTotal) const = 0;
-
 	// return cost model parameters
-	virtual ICostModelParams *GetCostModelParams() const = 0;
+	virtual ICostModelParams* GetCostModelParams() const = 0;
 
 	// main driver for cost computation
-	virtual CCost Cost(CExpressionHandle &exprhdl, const SCostingInfo *pci) const = 0;
+	virtual double Cost(CExpressionHandle &exprhdl, const SCostingInfo* pci) const = 0;
 
 	// cost model type
 	virtual ECostModelType Ecmt() const = 0;
 
 	// set cost model params
-	void SetParams(ICostModelParamsArray *pdrgpcp);
+	void SetParams(duckdb::vector<ICostModelParams::SCostParam*> pdrgpcp);
 
 	// create a default cost model instance
-	static ICostModel *PcmDefault(CMemoryPool *mp);
+	static ICostModel* PcmDefault();
 };
 }  // namespace gpopt
-
 #endif
--- src/include/duckdb/optimizer/cascade/cost/ICostModelParams.h
+++ src/include/duckdb/optimizer/cascade/cost/ICostModelParams.h
@@ -9,17 +9,12 @@
 #define GPOPT_ICostModelParams_H
 
 #include "duckdb/optimizer/cascade/base.h"
-#include "duckdb/optimizer/cascade/common/CRefCount.h"
 
-#include "duckdb/optimizer/cascade/md/IMDRelation.h"
-
-#include "CCost.h"
+using namespace gpos;
+using namespace std;
 
 namespace gpopt
 {
-using namespace gpos;
-using namespace gpmd;
-
 //---------------------------------------------------------------------------
 //	@class:
 //		ICostModelParams
@@ -28,7 +23,7 @@ using namespace gpmd;
 //		Interface for the parameters of the underlying cost model
 //
 //---------------------------------------------------------------------------
-class ICostModelParams : public CRefCount
+class ICostModelParams
 {
 public:
 	//---------------------------------------------------------------------------
@@ -46,86 +41,69 @@ public:
 		ULONG m_id;
 
 		// param value
-		CDouble m_value;
+		double m_value;
 
 		// param lower bound
-		CDouble m_lower_bound_val;
+		double m_lower_bound_val;
 
 		// param upper bound
-		CDouble m_upper_bound_val;
+		double m_upper_bound_val;
 
 	public:
 		// ctor
-		SCostParam(ULONG id, CDouble dVal, CDouble dLowerBound,
-				   CDouble dUpperBound)
-			: m_id(id),
-			  m_value(dVal),
-			  m_lower_bound_val(dLowerBound),
-			  m_upper_bound_val(dUpperBound)
+		SCostParam(ULONG id, double dVal, double dLowerBound, double dUpperBound)
+			: m_id(id), m_value(dVal), m_lower_bound_val(dLowerBound), m_upper_bound_val(dUpperBound)
 		{
-			GPOS_ASSERT(dVal >= dLowerBound);
-			GPOS_ASSERT(dVal <= dUpperBound);
 		}
 
 		// dtor
 		virtual ~SCostParam(){};
 
 		// return param identifier
-		ULONG
-		Id() const
+		ULONG Id() const
 		{
 			return m_id;
 		}
 
 		// return value
-		CDouble
-		Get() const
+		double Get() const
 		{
 			return m_value;
 		}
 
 		// return lower bound value
-		CDouble
-		GetLowerBoundVal() const
+		double GetLowerBoundVal() const
 		{
 			return m_lower_bound_val;
 		}
 
 		// return upper bound value
-		CDouble
-		GetUpperBoundVal() const
+		double GetUpperBoundVal() const
 		{
 			return m_upper_bound_val;
 		}
 
-		BOOL
-		Equals(SCostParam *pcm) const
+		bool Equals(shared_ptr<SCostParam> pcm) const
 		{
-			return Id() == pcm->Id() && Get() == pcm->Get() &&
-				   GetLowerBoundVal() == pcm->GetLowerBoundVal() &&
-				   GetUpperBoundVal() == pcm->GetUpperBoundVal();
+			return Id() == pcm->Id() && Get() == pcm->Get() && GetLowerBoundVal() == pcm->GetLowerBoundVal() && GetUpperBoundVal() == pcm->GetUpperBoundVal();
 		}
-
 	};	// struct SCostParam
 
 	// lookup param by id
-	virtual SCostParam *PcpLookup(ULONG id) const = 0;
+	virtual shared_ptr<SCostParam> PcpLookup(ULONG id) const = 0;
 
 	// lookup param by name
-	virtual SCostParam *PcpLookup(const CHAR *szName) const = 0;
+	virtual shared_ptr<SCostParam> PcpLookup(const CHAR* szName) const = 0;
 
 	// set param by id
-	virtual void SetParam(ULONG id, CDouble dVal, CDouble dLowerBound,
-						  CDouble dUpperBound) = 0;
+	virtual void SetParam(ULONG id, double dVal, double dLowerBound, double dUpperBound) = 0;
 
 	// set param by name
-	virtual void SetParam(const CHAR *szName, CDouble dVal, CDouble dLowerBound,
-						  CDouble dUpperBound) = 0;
+	virtual void SetParam(const CHAR* szName, double dVal, double dLowerBound, double dUpperBound) = 0;
 
-	virtual BOOL Equals(ICostModelParams *pcm) const = 0;
+	virtual bool Equals(shared_ptr<ICostModelParams> pcm) const = 0;
 
-	virtual const CHAR *SzNameLookup(ULONG id) const = 0;
+	virtual const CHAR* SzNameLookup(ULONG id) const = 0;
 };
 }  // namespace gpopt
-
 #endif
