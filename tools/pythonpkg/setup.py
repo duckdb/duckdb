3d65632c2d680b3fc6375713be60257f62f3a3a6
Tishj <t_b@live.nl>
2023-08-10 15:34:06 +0200

    use 'duckdb_build' instead, add it to clean script

--- tools/pythonpkg/setup.py
+++ tools/pythonpkg/setup.py
@@ -200,7 +200,7 @@ if len(existing_duckdb_dir) == 0:
         import package_build
 
         (source_list, include_list, original_sources) = package_build.build_package(
-            os.path.join(script_path, "build"), extensions, False, unity_build, "build"
+            os.path.join(script_path, "duckdb_build"), extensions, False, unity_build, "duckdb_build"
         )
 
         duckdb_sources = [
@@ -208,10 +208,10 @@ if len(existing_duckdb_dir) == 0:
         ]
         duckdb_sources.sort()
 
-        original_sources = [os.path.join("build", x) for x in original_sources]
+        original_sources = [os.path.join("duckdb_build", x) for x in original_sources]
 
-        duckdb_includes = [os.path.join("build", x) for x in include_list]
-        duckdb_includes += ["build"]
+        duckdb_includes = [os.path.join("duckdb_build", x) for x in include_list]
+        duckdb_includes += ["duckdb_build"]
 
         # gather the include files
         import amalgamation
