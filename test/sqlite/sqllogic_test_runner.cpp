b56e4f787ed5d804b3ecc766a5940bb5a39ab35f
Sam Ansmink <samansmink@hotmail.com>
2023-08-14 10:43:45 +0200

    ci fixes

--- test/sqlite/sqllogic_test_runner.cpp
+++ test/sqlite/sqllogic_test_runner.cpp
@@ -14,9 +14,10 @@ namespace duckdb {
 
 SQLLogicTestRunner::SQLLogicTestRunner(string dbpath) : dbpath(std::move(dbpath)), finished_processing_file(false) {
 	config = GetTestConfig();
-	config->options.load_extensions = false;
+
 	auto env_var = std::getenv("LOCAL_EXTENSION_REPO");
 	if (!env_var) {
+		config->options.load_extensions = false;
 		config->options.autoload_known_extensions = false;
 	}
 }
