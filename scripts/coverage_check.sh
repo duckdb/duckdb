5c007311f6cf6871288bb6f7b531b1546bb6a174
Rob <86313040+robert-manchester@users.noreply.github.com>
2023-08-18 23:13:34 -0700

    remove baseline collection as it is a no-op given the counters get zeroed.

--- scripts/coverage_check.sh
+++ scripts/coverage_check.sh
@@ -4,7 +4,6 @@ set -e
 
 # prepare coverage file
 lcov --config-file .github/workflows/lcovrc --zerocounters --directory .
-lcov --config-file .github/workflows/lcovrc --capture --initial --directory . --base-directory . --no-external --output-file base_coverage.info
 
 # run tests
 build/coverage/test/unittest
@@ -16,9 +15,8 @@ build/coverage/test/unittest test/sql/tpch/tpch_sf01.test_slow
 python3 tools/shell/shell-test.py build/coverage/duckdb
 
 # finalize coverage file
-lcov --config-file .github/workflows/lcovrc --directory . --base-directory . --no-external --capture  --output-file test_coverage.info
-lcov -a base_coverage.info -a test_coverage.info -o total_coverage.info
-lcov --config-file .github/workflows/lcovrc --remove total_coverage.info $(< .github/workflows/lcov_exclude) -o lcov.info
+lcov --config-file .github/workflows/lcovrc --directory . --base-directory . --no-external --capture  --output-file coverage.info
+lcov --config-file .github/workflows/lcovrc --remove coverage.info $(< .github/workflows/lcov_exclude) -o lcov.info
 
 # generate coverage html
 genhtml -o coverage_html lcov.info
