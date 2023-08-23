fa040f566f26f7b42c9ddcd465e2b4baa7ed4880
Pedro Holanda <pedroholanda@gmail.com>
2023-08-23 15:34:49 +0200

    Simplify state machine initalization

--- src/execution/operator/csv_scanner/csv_state_machine_cache.cpp
+++ src/execution/operator/csv_scanner/csv_state_machine_cache.cpp
@@ -3,23 +3,41 @@
 
 namespace duckdb{
 
+void InitializeTransitionArray(unsigned char* transition_array, const uint8_t state){
+	for (uint32_t i = 0; i < NUM_TRANSITIONS; i++) {
+		transition_array[i] = state;
+	}
+}
+
 void CSVStateMachineCache::Insert(char delimiter, char quote, char escape) {
 	// Initialize transition array with default values to the Standard option
 	auto &transition_array = state_machine_cache[{delimiter, quote, escape}];
-	for (uint32_t i = 0; i < 4; i++) {
-		for (uint32_t j = 0; j < NUM_TRANSITIONS; j++) {
-			transition_array[i][j] = static_cast<uint8_t>(CSVState::STANDARD);
+	const uint8_t standard_state = static_cast<uint8_t>(CSVState::STANDARD);
+	const uint8_t field_separator_state = static_cast<uint8_t>(CSVState::DELIMITER);
+	const uint8_t record_separator_state = static_cast<uint8_t>(CSVState::RECORD_SEPARATOR);
+	const uint8_t carriage_return_state = static_cast<uint8_t>(CSVState::CARRIAGE_RETURN);
+	const uint8_t quoted_state = static_cast<uint8_t>(CSVState::QUOTED);
+	const uint8_t unquoted_state = static_cast<uint8_t>(CSVState::UNQUOTED);
+	const uint8_t escape_state = static_cast<uint8_t>(CSVState::ESCAPE);
+	const uint8_t empty_line_state = static_cast<uint8_t>(CSVState::EMPTY_LINE);
+	const uint8_t invalid_state = static_cast<uint8_t>(CSVState::INVALID);
+
+	for (uint32_t i = 0; i < NUM_STATES; i++) {
+		switch (i) {
+		case quoted_state:
+			InitializeTransitionArray(transition_array[i],quoted_state);
+			break;
+		case unquoted_state:
+			InitializeTransitionArray(transition_array[i],invalid_state);
+			break;
+		case escape_state:
+			InitializeTransitionArray(transition_array[i],invalid_state);
+			break;
+		default:
+			InitializeTransitionArray(transition_array[i],standard_state);
+			break;
 		}
 	}
-	uint8_t standard_state = static_cast<uint8_t>(CSVState::STANDARD);
-	uint8_t field_separator_state = static_cast<uint8_t>(CSVState::DELIMITER);
-	uint8_t record_separator_state = static_cast<uint8_t>(CSVState::RECORD_SEPARATOR);
-	uint8_t carriage_return_state = static_cast<uint8_t>(CSVState::CARRIAGE_RETURN);
-	uint8_t quoted_state = static_cast<uint8_t>(CSVState::QUOTED);
-	uint8_t unquoted_state = static_cast<uint8_t>(CSVState::UNQUOTED);
-	uint8_t escape_state = static_cast<uint8_t>(CSVState::ESCAPE);
-	uint8_t empty_line_state = static_cast<uint8_t>(CSVState::EMPTY_LINE);
-	uint8_t invalid_state = static_cast<uint8_t>(CSVState::INVALID);
 
 	// Now set values depending on configuration
 	// 1) Standard State
@@ -42,18 +60,11 @@ void CSVStateMachineCache::Insert(char delimiter, char quote, char escape) {
 	transition_array[carriage_return_state][static_cast<uint8_t>('\r')] = empty_line_state;
 	transition_array[carriage_return_state][static_cast<uint8_t>(escape)] = escape_state;
 	// 5) Quoted State
-	for (uint32_t j = 0; j < NUM_TRANSITIONS; j++) {
-		transition_array[quoted_state][j] = quoted_state;
-	}
 	transition_array[quoted_state][static_cast<uint8_t>(quote)] = unquoted_state;
-
 	if (quote != escape) {
 		transition_array[quoted_state][static_cast<uint8_t>(escape)] = escape_state;
 	}
 	// 6) Unquoted State
-	for (uint32_t j = 0; j < NUM_TRANSITIONS; j++) {
-		transition_array[unquoted_state][j] = invalid_state;
-	}
 	transition_array[unquoted_state][static_cast<uint8_t>('\n')] = record_separator_state;
 	transition_array[unquoted_state][static_cast<uint8_t>('\r')] = carriage_return_state;
 	transition_array[unquoted_state][static_cast<uint8_t>(delimiter)] = field_separator_state;
@@ -61,17 +72,9 @@ void CSVStateMachineCache::Insert(char delimiter, char quote, char escape) {
 		transition_array[unquoted_state][static_cast<uint8_t>(escape)] = quoted_state;
 	}
 	// 7) Escaped State
-	for (uint32_t j = 0; j < NUM_TRANSITIONS; j++) {
-		// Escape is always invalid if not proceeded by another escape or quoted char
-		transition_array[escape_state][j] = invalid_state;
-	}
 	transition_array[escape_state][static_cast<uint8_t>(quote)] = quoted_state;
 	transition_array[escape_state][static_cast<uint8_t>(escape)] = quoted_state;
 	// 8) Empty Line State
-	for (uint32_t j = 0; j < NUM_TRANSITIONS; j++) {
-		// Escape is always invalid if not proceeded by another escape or quoted char
-		transition_array[empty_line_state][j] = standard_state;
-	}
 	transition_array[empty_line_state][static_cast<uint8_t>('\r')] = empty_line_state;
 	transition_array[empty_line_state][static_cast<uint8_t>('\n')] = empty_line_state;
 }
