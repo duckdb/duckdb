67e2249b022eff012b67ade5c6b89b30624934ca
lkuffo <lkuffo@espol.edu.ec>
2023-08-25 17:49:25 +0200

    [Compression] ALP as a type of compression

--- src/common/enums/compression_type.cpp
+++ src/common/enums/compression_type.cpp
@@ -34,6 +34,8 @@ CompressionType CompressionTypeFromString(const string &str) {
 		return CompressionType::COMPRESSION_CHIMP;
 	} else if (compression == "patas") {
 		return CompressionType::COMPRESSION_PATAS;
+	} else if (compression == "alp") {
+		return CompressionType::COMPRESSION_ALP;
 	} else {
 		return CompressionType::COMPRESSION_AUTO;
 	}
@@ -61,6 +63,8 @@ string CompressionTypeToString(CompressionType type) {
 		return "Chimp";
 	case CompressionType::COMPRESSION_PATAS:
 		return "Patas";
+	case CompressionType::COMPRESSION_ALP:
+		return "ALP";
 	default:
 		throw InternalException("Unrecognized compression type!");
 	}
