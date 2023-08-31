476026931eafc6d7c350fe3c74137f03f18afb6c
Tishj <t_b@live.nl>
2023-08-31 17:47:54 +0200

    remove duplicate include

--- src/catalog/dependency_list.cpp
+++ src/catalog/dependency_list.cpp
@@ -1,7 +1,6 @@
 #include "duckdb/catalog/dependency_list.hpp"
 #include "duckdb/catalog/catalog_entry.hpp"
 #include "duckdb/catalog/catalog.hpp"
-#include "duckdb/catalog/catalog_entry/schema_catalog_entry.hpp"
 #include "duckdb/common/serializer/format_deserializer.hpp"
 #include "duckdb/common/serializer/format_serializer.hpp"
 #include "duckdb/catalog/catalog_entry/schema_catalog_entry.hpp"
