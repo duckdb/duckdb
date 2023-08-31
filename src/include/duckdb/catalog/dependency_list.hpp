35ff944ae0c70deedb983e78c3e8b552cefc4d94
Tishj <t_b@live.nl>
2023-08-31 17:51:36 +0200

    tidy fix

--- src/include/duckdb/catalog/dependency_list.hpp
+++ src/include/duckdb/catalog/dependency_list.hpp
@@ -68,7 +68,7 @@ class LogicalDependencyList {
 	using create_info_set_t = unordered_set<LogicalDependency, CreateInfoHashFunction, CreateInfoEquality>;
 
 public:
-	DUCKDB_API void AddDependency(LogicalDependency entry);
+	DUCKDB_API void AddDependency(const LogicalDependency &entry);
 	DUCKDB_API void AddDependency(CatalogEntry &entry);
 	DUCKDB_API bool Contains(LogicalDependency &entry);
 	DUCKDB_API bool Contains(CatalogEntry &entry);
