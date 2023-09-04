67aaf6ea0a518a8471ec8d14a231493f318820be
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-09-04 15:21:21 +0200

    Extensions: add description string for substrait

--- src/main/extension/extension_helper.cpp
+++ src/main/extension/extension_helper.cpp
@@ -116,6 +116,7 @@ static DefaultExtension internal_extensions[] = {
     {"postgres_scanner", "Adds support for reading from a Postgres database", false},
     {"inet", "Adds support for IP-related data types and functions", false},
     {"spatial", "Geospatial extension that adds support for working with spatial data and functions", false},
+    {"substrait", "Adds support for the Substrait integration", false},
     {nullptr, nullptr, false}};
 
 idx_t ExtensionHelper::DefaultExtensionCount() {
