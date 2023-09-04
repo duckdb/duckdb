6c1094c2c8d07f72763c9710166f5ac37a4cd376
Elliana May <me@mause.me>
2023-09-04 13:30:10 +0800

    fix: format

--- tools/jdbc/src/test/java/org/duckdb/Util.java
+++ tools/jdbc/src/test/java/org/duckdb/Util.java
@@ -7,8 +7,7 @@ import java.io.IOException;
  * this class exists only as bridge between the test and main packages
  */
 public class Util {
-    public  static byte[] readAllBytes(InputStream e) throws IOException {
+    public static byte[] readAllBytes(InputStream e) throws IOException {
         return JdbcUtils.readAllBytes(e);
     }
 }
-
