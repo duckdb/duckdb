0ccd11008502470ddcfdf50bd755204eeba7444f
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-09-05 11:22:08 +0200

    More verification and actually use pypi username

--- scripts/pypi_cleanup.py
+++ scripts/pypi_cleanup.py
@@ -6,17 +6,19 @@ import subprocess
 # how many days to retain dev releases - all dev releases older than 10 days are deleted
 retain_days = 10
 
-pypi_username = os.environ['PYPI_CLEANUP_USERNAME']
+pypi_username = os.getenv('PYPI_CLEANUP_USERNAME', "")
 pypi_password = os.getenv("PYPI_CLEANUP_PASSWORD", "")
 pypi_otp = os.getenv("PYPI_CLEANUP_OTP", "")
+if pypi_username == "":
+    print(f'need username in PYPI_CLEANUP_USERNAME env variable')
+    exit(1)
 if pypi_password == "":
-    print(f'need {pypi_username}\' PyPI password in PYPI_PASSWORD env variable')
+    print(f'need {pypi_username}\' PyPI password in PYPI_CLEANUP_PASSWORD env variable')
     exit(1)
 if pypi_otp == "":
-    print(f'need {pypi_username}\' PyPI otp in PYPI_OTP env variable')
+    print(f'need {pypi_username}\' PyPI OTP secret in PYPI_CLEANUP_OTP env variable')
     exit(1)
 
-os.environ['PYPI_CLEANUP_PASSWORD'] = pypi_password
-proc = subprocess.Popen(['pypi-cleanup', '-u', 'Mytherin', '-p', 'duckdb', '-d', str(retain_days), '--do-it'], stdin=subprocess.PIPE)
+proc = subprocess.Popen(['pypi-cleanup', '-u', pypi_username, '-p', 'duckdb', '-d', str(retain_days), '--do-it'], stdin=subprocess.PIPE)
 # insert otp
 proc.communicate(input=(pyotp.TOTP(pypi_otp).now() + '\n').encode('utf8'))
