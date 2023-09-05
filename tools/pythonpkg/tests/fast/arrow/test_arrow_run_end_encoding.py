18e4d3903544ec41965dd23e16b4f44235a3b063
Tishj <t_b@live.nl>
2023-09-05 11:25:21 +0200

    add test for run-end-encoding

---
+++ tools/pythonpkg/tests/fast/arrow/test_arrow_run_end_encoding.py
@@ -0,0 +1,20 @@
+import duckdb
+import pytest
+import pandas as pd
+import duckdb
+
+pa = pytest.importorskip("pyarrow", '14.0.0.dev')
+import pyarrow.compute as pc
+
+
+class TestArrowREE(object):
+    def test_arrow_run_end_encoding(self):
+        con = duckdb.connect()
+        data = [1, 1, 1, 2, 2, 3, 3, 3, 3, 3]
+        array = pa.array(data, type=pa.int32())
+        encoded_array = pc.run_end_encode(array)
+
+        schema = pa.schema([("ree", encoded_array.type)])
+        tbl = pa.Table.from_arrays([encoded_array], schema=schema)
+        res = duckdb.sql("select * from tbl").fetchall()
+        print(res)
