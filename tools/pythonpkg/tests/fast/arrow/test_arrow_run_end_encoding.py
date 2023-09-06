1835c1de88c886cce2e66257c7cff821aaa9d572
Tishj <t_b@live.nl>
2023-09-06 14:29:51 +0200

    test with bigger than STANDARD_VECTOR_SIZE runs

--- tools/pythonpkg/tests/fast/arrow/test_arrow_run_end_encoding.py
+++ tools/pythonpkg/tests/fast/arrow/test_arrow_run_end_encoding.py
@@ -25,3 +25,19 @@ class TestArrowREE(object):
         tbl = pa.Table.from_arrays([encoded_array], schema=schema)
         res = duckdb.sql("select * from tbl").fetchall()
         assert res == expected
+
+    def test_run_bigger_than_vector(self):
+        con = duckdb.connect()
+
+        rel = con.sql("""
+            select (i // 2392) as ree from range(100000) t(i);
+        """)
+        array = rel.arrow()['ree']
+        expected = rel.fetchall()
+
+        encoded_array = pc.run_end_encode(array)
+
+        schema = pa.schema([("ree", encoded_array.type)])
+        tbl = pa.Table.from_arrays([encoded_array], schema=schema)
+        res = duckdb.sql("select * from tbl").fetchall()
+        assert res == expected
