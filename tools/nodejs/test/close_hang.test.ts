65aecd5364112135581b7ef0bc546b008a20a4d7
Elliana May <me@mause.me>
2023-09-08 12:55:00 +0800

    rewrite close_hang.test.ts to actually fail on failure
    and to be skipped on windows

--- tools/nodejs/test/close_hang.test.ts
+++ tools/nodejs/test/close_hang.test.ts
@@ -1,13 +1,16 @@
-import { exec, ExecException } from "child_process";
+import 'chai-as-promised';
+import {exec as _exec} from "child_process";
 import fs from "fs-extra";
 import path from "path";
-import { Database, OPEN_READWRITE } from "..";
+import {Database, OPEN_READWRITE} from "..";
+import {promisify} from "util";
+import {expect} from "chai";
 
-async function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const exec = promisify(_exec);
 
 it("close hang", async function main() {
+  if (process.platform == 'win32') this.skip();
+
   const databasePath = path.join(__dirname, "tmp", "close_hang.db");
   const pathExists = await fs.pathExists(databasePath);
   if (pathExists) {
@@ -17,34 +20,20 @@ it("close hang", async function main() {
 
   const db = await new Promise<Database>((resolve, reject) => {
     let db: Database = new Database(
-      databasePath,
-      OPEN_READWRITE,
-      (err: unknown) => (err ? reject(err) : resolve(db)),
+        databasePath,
+        OPEN_READWRITE,
+        (err: unknown) => (err ? reject(err) : resolve(db)),
     );
   });
-  console.info("Database.create(%s)", databasePath);
   try {
     await new Promise((resolve) =>
-      db.exec("create table foo(bar int)", resolve),
+        db.exec("create table foo(bar int)", resolve),
     );
   } finally {
     await new Promise((resolve) => db.close(resolve));
-    console.info("Database.close(%s)", databasePath);
   }
 
-  let fileOpen = true;
-  do {
-    exec(
-      `lsof ${databasePath}`,
-      (err: ExecException | null, stdout: string): void => {
-        if (err?.code === 1) {
-          fileOpen = false;
-          console.info("Database file closed");
-        } else {
-          console.info("Database file still open. Details:\n%s", stdout);
-        }
-      },
-    );
-    await sleep(1_000);
-  } while (fileOpen);
+  // exit code 1 and stdout blank means no open handles
+  await expect(exec(
+      `lsof ${databasePath}`)).to.eventually.be.rejected.and.to.include({ 'code': 1 ,'stdout': ''});
 });
