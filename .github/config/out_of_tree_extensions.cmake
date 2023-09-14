90dd3fa4611ea8b4f99558656e17864a4bbe0bd3
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-09-14 17:10:17 +0200

    [Dev] Bump duckdb_spatial

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -55,7 +55,7 @@ duckdb_extension_load(postgres_scanner
 duckdb_extension_load(spatial
         DONT_LINK LOAD_TESTS
         GIT_URL https://github.com/duckdblabs/duckdb_spatial.git
-        GIT_TAG 2f55d5d64bad9b5fc7ce67e4bcf52617ee31b865
+        GIT_TAG 44c9e49bdf48d39c936c500b632ad3bd74825b04
         INCLUDE_DIR spatial/include
         TEST_DIR test/sql
         )
