d3327d8db427121c31b9c89963a46cf53c8e5155
Elliana May <me@mause.me>
2023-09-16 08:19:02 +0800

    sanity check file abi

--- scripts/node_build.sh
+++ scripts/node_build.sh
@@ -10,10 +10,13 @@ cd tools/nodejs
 make clean
 ./configure
 
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 if [[ "$TARGET_ARCH" == "arm64" ]] ; then
+  # TODO: double check ABI
   apt-get install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf --yes
   export CC=arm-linux-gnueabihf-gcc
   export CXX=arm-linux-gnueabihf-g++
+fi
 fi
 
 npm install --build-from-source --target_arch="$TARGET_ARCH"
@@ -22,6 +25,12 @@ npm install --build-from-source --target_arch="$TARGET_ARCH"
 
 if [[ "$TARGET_ARCH" != "arm64" ]] ; then
   npm test
+else
+  ARCH=$(file lib/binding/duckdb.node)
+  if [[ "$ARCH" != *"ARM"* ]] ; then
+    echo no arch $ARCH
+    exit -1
+  fi
 fi
 
 export PATH=$(npm bin):$PATH
