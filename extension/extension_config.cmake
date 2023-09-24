b1d1168267bb6ebf89444809ec4d870cf935adcf
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-09-24 20:47:05 +0200

    Extensions selection: JSON also builtin by default in all configurations

--- extension/extension_config.cmake
+++ extension/extension_config.cmake
@@ -7,8 +7,10 @@
 # development, create `extension/extension_config_local.cmake` and specify extensions there.
 # The local file is also loaded by the DuckDB CMake build but ignored by version control.
 
-# Parquet is loaded by default on every build as its a essential part of DuckDB
+# Parquet is built-in (and loaded at startup) by default, on every build as its a essential part of DuckDB
 duckdb_extension_load(parquet)
+# JSON is built-in (and loaded at startup) by default, on every build as its a essential part of DuckDB
+duckdb_extension_load(json)
 
 # Jemalloc is enabled by default for linux. MacOS malloc is already good enough and Jemalloc on windows has issues.
 if(NOT WASM_LOADABLE_EXTENSIONS AND NOT CLANG_TIDY AND OS_NAME STREQUAL "linux" AND NOT ANDROID AND NOT ZOS)
