65d6ef674fe575a776b0ae372c026d9fbb216d6d
Tishj <t_b@live.nl>
2023-09-28 11:43:17 +0200

    remove assertions

--- tools/pythonpkg/tests/fast/pandas/test_pandas_df_none.py
+++ tools/pythonpkg/tests/fast/pandas/test_pandas_df_none.py
@@ -6,14 +6,10 @@ import gc
 
 
 class TestPandasDFNone(object):
+    # This used to decrease the ref count of None
     def test_none_deref(self):
         con = duckdb.connect()
         gc.collect()
-        none_refcount = sys.getrefcount(None)
         for _ in range(1000):
             df = con.sql("select NULL::VARCHAR as a").df()
-
         gc.collect()
-        after = sys.getrefcount(None)
-        # Verify that we don't deref None when creating a Pandas DataFrame
-        assert after >= none_refcount
