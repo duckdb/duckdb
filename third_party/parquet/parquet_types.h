ea1e4f3118dd9cab443a224c418a1db4813384f3
Adam Reeve <adreeve@gmail.com>
2023-10-05 15:05:44 +1300

    Implement byte stream split decoder

--- third_party/parquet/parquet_types.h
+++ third_party/parquet/parquet_types.h
@@ -92,7 +92,8 @@ struct Encoding {
     DELTA_BINARY_PACKED = 5,
     DELTA_LENGTH_BYTE_ARRAY = 6,
     DELTA_BYTE_ARRAY = 7,
-    RLE_DICTIONARY = 8
+    RLE_DICTIONARY = 8,
+    BYTE_STREAM_SPLIT = 9,
   };
 };
 
