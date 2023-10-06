f58386514533167ddfc41ebb0d94a83a302521b4
Tishj <t_b@live.nl>
2023-10-06 15:21:52 +0200

    produce ranges of iterators, so we can create separate scan states that each contain different ranges of batches

--- src/include/duckdb/common/types/batched_data_collection.hpp
+++ src/include/duckdb/common/types/batched_data_collection.hpp
@@ -15,8 +15,16 @@ namespace duckdb {
 class BufferManager;
 class ClientContext;
 
+using batch_map_t = map<idx_t, unique_ptr<ColumnDataCollection>>;
+using batch_iterator_t = typename batch_map_t::iterator;
+
+struct BatchedChunkIteratorRange {
+	batch_iterator_t begin;
+	batch_iterator_t end;
+};
+
 struct BatchedChunkScanState {
-	map<idx_t, unique_ptr<ColumnDataCollection>>::iterator iterator;
+	BatchedChunkIteratorRange range;
 	ColumnDataScanState scan_state;
 };
 
@@ -25,12 +33,17 @@ struct BatchedChunkScanState {
 class BatchedDataCollection {
 public:
 	DUCKDB_API BatchedDataCollection(ClientContext &context, vector<LogicalType> types, bool buffer_managed = false);
+	DUCKDB_API BatchedDataCollection(ClientContext &context, vector<LogicalType> types, batch_map_t batches,
+	                                 bool buffer_managed = false);
 
 	//! Appends a datachunk with the given batch index to the batched collection
 	DUCKDB_API void Append(DataChunk &input, idx_t batch_index);
 
 	//! Merge the other batched chunk collection into this batched collection
 	DUCKDB_API void Merge(BatchedDataCollection &other);
+
+	// Initialize a scan over a range of batches of the batched chunk collection
+	void InitializeScan(BatchedChunkScanState &state, const BatchedChunkIteratorRange &range);
 
 	//! Initialize a scan over the batched chunk collection
 	DUCKDB_API void InitializeScan(BatchedChunkScanState &state);
@@ -40,6 +53,27 @@ public:
 
 	//! Fetch a column data collection from the batched data collection - this consumes all of the data stored within
 	DUCKDB_API unique_ptr<ColumnDataCollection> FetchCollection();
+
+	//! Inspect how many tuples this batched data collection contains
+	DUCKDB_API idx_t Count() const;
+
+	//! Inspect the types of the collection
+	DUCKDB_API const vector<LogicalType> &Types() const;
+
+	//! Inspect how many batches this collection contains
+	DUCKDB_API idx_t BatchCount() const;
+
+	//! Retrieve the batch index of the nth batch in the collection
+	DUCKDB_API idx_t IndexToBatchIndex(idx_t index) const;
+
+	//! Inspect how big a given batch is
+	DUCKDB_API idx_t BatchSize(idx_t batch_index) const;
+
+	//! Inspect a given batch through a const reference
+	const ColumnDataCollection &Batch(idx_t batch_index) const;
+
+	//! Create an iterator range from the provided indices
+	BatchedChunkIteratorRange BatchRange(idx_t begin = 0, idx_t end = DConstants::INVALID_INDEX);
 
 	DUCKDB_API string ToString() const;
 	DUCKDB_API void Print() const;
@@ -59,4 +93,5 @@ private:
 	//! The last batch collection that was inserted into
 	CachedCollection last_collection;
 };
+
 } // namespace duckdb
