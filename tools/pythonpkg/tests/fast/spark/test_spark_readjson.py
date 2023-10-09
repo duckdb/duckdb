f169284ea29d64edbdb9e8517f281d17ee8dc36b
Tishj <t_b@live.nl>
2023-10-09 16:08:44 +0200

    add basic functionality + tests for read.parquet and read.json

--- tools/pythonpkg/tests/fast/spark/test_spark_readjson.py
+++ tools/pythonpkg/tests/fast/spark/test_spark_readjson.py
@@ -7,11 +7,11 @@ import textwrap
 import duckdb
 
 
-class TestSparkReadParquet(object):
-    def test_read_parquet(self, spark, tmp_path):
+class TestSparkReadJson(object):
+    def test_read_json(self, spark, tmp_path):
         file_path = tmp_path / 'basic.parquet'
         file_path = file_path.as_posix()
-        duckdb.execute(f"COPY (select 42 a, true b, 'this is a long string' c) to '{file_path}' (FORMAT PARQUET)")
-        df = spark.read.parquet(file_path)
+        duckdb.execute(f"COPY (select 42 a, true b, 'this is a long string' c) to '{file_path}' (FORMAT JSON)")
+        df = spark.read.json(file_path)
         res = df.collect()
         assert res == [Row(a=42, b=True, c='this is a long string')]
