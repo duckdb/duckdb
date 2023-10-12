33e7d791fe4b15421da8b2aee919bbac6ecbd1f0
Rob <86313040+robert-manchester@users.noreply.github.com>
2023-10-12 05:59:20 -0700

    merge subtract, add operations into add

--- extension/icu/icu-timebucket.cpp
+++ extension/icu/icu-timebucket.cpp
@@ -86,11 +86,7 @@ struct ICUTimeBucket : public ICUDateFunc {
 		timestamp_t bucket = Add(calendar, origin, interval_t {0, static_cast<int32_t>(result_days), 0});
 		if (ts < bucket) {
 			D_ASSERT(ts < origin);
-			// correct bucket is the closest one less than the ts
-			// calculate: bucket -= bucket_width_days * Interval::MICROS_PER_DAY;
-			result_days =
-			    SubtractOperatorOverflowCheck::Operation<int32_t, int32_t, int32_t>(result_days, bucket_width_days);
-			bucket = Add(calendar, origin, interval_t {0, static_cast<int32_t>(result_days), 0});
+			bucket = Add(calendar, bucket, interval_t {0, -bucket_width_days, 0});
 			D_ASSERT(ts > bucket);
 		}
 
