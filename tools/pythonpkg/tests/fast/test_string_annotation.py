8ce3f48b06327dec40f1ad9a77782af8e03ab4d6
Tishj <t_b@live.nl>
2023-10-16 12:59:43 +0200

    we cant make use of __future__ annotations feature in generated functions - just use strings instead

--- tools/pythonpkg/tests/fast/test_string_annotation.py
+++ tools/pythonpkg/tests/fast/test_string_annotation.py
@@ -1,27 +1,53 @@
 import duckdb
-import os
-import pandas as pd
 import pytest
+import sys
 from typing import Union
 
-# This is used to opt in to PEP 563 (string annotations)
-from __future__ import annotations
 
-
-def make_annotated_function(type):
-    # Create a function that returns its input
-    def test_base(x):
-        return x
+def make_annotated_function(type: str):
+    def test_base():
+        return None
 
     import types
 
     test_function = types.FunctionType(
         test_base.__code__, test_base.__globals__, test_base.__name__, test_base.__defaults__, test_base.__closure__
     )
-    # Add annotations for the return type and 'x'
-    test_function.__annotations__ = {'return': type, 'x': type}
+    # Add the 'type' string as return_annotation
+    test_function.__annotations__ = {'return': type}
     return test_function
 
+
+def python_version_lower_than_3_10():
+    import sys
+
+    if sys.version_info[0] < 3:
+        return True
+    if sys.version_info[1] < 10:
+        return True
+    return False
+
+
 class TestStringAnnotation(object):
-    def test_string_annotations(self):
-        pass
+    @pytest.mark.skipif(
+        python_version_lower_than_3_10(), reason="inspect.signature(eval_str=True) only supported since 3.10 and higher"
+    )
+    @pytest.mark.parametrize(
+        ['input', 'expected'],
+        [
+            ('str', 'VARCHAR'),
+            ('list[str]', 'VARCHAR[]'),
+            ('dict[str, str]', 'MAP(VARCHAR, VARCHAR)'),
+            ('dict[Union[str, bool], str]', 'MAP(UNION(u1 VARCHAR, u2 BOOLEAN), VARCHAR)'),
+        ],
+    )
+    def test_string_annotations(self, duckdb_cursor, input, expected):
+        from inspect import signature
+
+        func = make_annotated_function(input)
+        sig = signature(func)
+        assert sig.return_annotation.__class__ == str
+
+        duckdb_cursor.create_function("foo", func)
+        rel = duckdb_cursor.sql("select foo()")
+        assert rel.types == [expected]
