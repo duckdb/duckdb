2d11a282d134bc4cdeb5887345f88a74d9c36ff3
Christina Sioula <chrisiou.myk@gmail.com>
2023-10-17 20:45:31 +0200

    Support gcs:// and r2:// URLs to read data from GCS and R2

--- extension/httpfs/s3fs.cpp
+++ extension/httpfs/s3fs.cpp
@@ -193,7 +193,13 @@ S3AuthParams S3AuthParams::ReadFrom(FileOpener *opener, FileOpenerInfo &info) {
 	if (FileOpener::TryGetCurrentSetting(opener, "s3_endpoint", value, info)) {
 		endpoint = value.ToString();
 	} else {
-		endpoint = "s3.amazonaws.com";
+		if (StringUtil::StartsWith(info.file_path, "gcs://")) {
+			endpoint = "storage.googleapis.com";
+		} else if (StringUtil::StartsWith(info.file_path, "r2://")) {
+			endpoint = "r2.cloudflarestorage.com";
+		} else {
+			endpoint = "s3.amazonaws.com";
+		}
 	}
 
 	if (FileOpener::TryGetCurrentSetting(opener, "s3_url_style", value, info)) {
@@ -556,17 +562,27 @@ void S3FileSystem::ReadQueryParams(const string &url_query_param, S3AuthParams &
 	}
 }
 
-ParsedS3Url S3FileSystem::S3UrlParse(string url, S3AuthParams &params) {
-	string http_proto, host, bucket, path, query_param, trimmed_s3_url;
-
-	if (url.rfind("s3://", 0) != 0) {
-		throw IOException("URL needs to start with s3://");
+static string GetPrefix(string url) {
+	const string prefixes[] = {"s3://", "gcs://", "r2://"};
+	for (auto &prefix : prefixes) {
+		if (StringUtil::StartsWith(url, prefix)) {
+			return prefix;
+		}
 	}
-	auto slash_pos = url.find('/', 5);
+	throw IOException("URL needs to start with s3://, gcs:// or r2://");
+	return string();
+}
+
+ParsedS3Url S3FileSystem::S3UrlParse(string url, S3AuthParams &params) {
+	string http_proto, prefix, host, bucket, path, query_param, trimmed_s3_url;
+
+	prefix = GetPrefix(url);
+	auto prefix_end_pos = url.find("//") + 2;
+	auto slash_pos = url.find('/', prefix_end_pos);
 	if (slash_pos == string::npos) {
 		throw IOException("URL needs to contain a '/' after the host");
 	}
-	bucket = url.substr(5, slash_pos - 5);
+	bucket = url.substr(prefix_end_pos, slash_pos - prefix_end_pos);
 	if (bucket.empty()) {
 		throw IOException("URL needs to contain a bucket name");
 	}
@@ -611,7 +627,7 @@ ParsedS3Url S3FileSystem::S3UrlParse(string url, S3AuthParams &params) {
 
 	http_proto = params.use_ssl ? "https://" : "http://";
 
-	return {http_proto, host, bucket, path, query_param, trimmed_s3_url};
+	return {http_proto, prefix, host, bucket, path, query_param, trimmed_s3_url};
 }
 
 string S3FileSystem::GetPayloadHash(char *buffer, idx_t buffer_len) {
@@ -812,7 +828,7 @@ void S3FileHandle::Initialize(FileOpener *opener) {
 }
 
 bool S3FileSystem::CanHandleFile(const string &fpath) {
-	return fpath.rfind("s3://", 0) == 0;
+	return this->IsRemoteFile(fpath);
 }
 
 void S3FileSystem::FileSync(FileHandle &handle) {
@@ -927,7 +943,7 @@ vector<string> S3FileSystem::Glob(const string &glob_pattern, FileOpener *opener
 		// Repeat requests until the keys of all common prefixes are parsed.
 		auto common_prefixes = AWSListObjectV2::ParseCommonPrefix(response_str);
 		while (!common_prefixes.empty()) {
-			auto prefix_path = "s3://" + parsed_s3_url.bucket + '/' + common_prefixes.back();
+			auto prefix_path = parsed_s3_url.prefix + parsed_s3_url.bucket + '/' + common_prefixes.back();
 			common_prefixes.pop_back();
 
 			// TODO we could optimize here by doing a match on the prefix, if it doesn't match we can skip this prefix
@@ -960,7 +976,7 @@ vector<string> S3FileSystem::Glob(const string &glob_pattern, FileOpener *opener
 		bool is_match = Match(key_splits.begin(), key_splits.end(), pattern_splits.begin(), pattern_splits.end());
 
 		if (is_match) {
-			auto result_full_url = "s3://" + parsed_s3_url.bucket + "/" + s3_key;
+			auto result_full_url = parsed_s3_url.prefix + parsed_s3_url.bucket + "/" + s3_key;
 			// if a ? char was present, we re-add it here as the url parsing will have trimmed it.
 			if (!parsed_s3_url.query_param.empty()) {
 				result_full_url += '?' + parsed_s3_url.query_param;
