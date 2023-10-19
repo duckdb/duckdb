0164db55ff0cee4a4768c186c54c358beac76e10
Tishj <t_b@live.nl>
2023-10-19 18:27:48 +0200

    add patch for sqlite_scanner and substrait, and set APPLY_PATCHES for the corresponding 'duckdb_extension_load' calls

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -72,6 +72,7 @@ duckdb_extension_load(sqlite_scanner
         ${STATIC_LINK_SQLITE} LOAD_TESTS
         GIT_URL https://github.com/duckdblabs/sqlite_scanner
         GIT_TAG 3443b2999ae1e68a108568fd32145705237a5760
+        APPLY_PATCHES
         )
 
 ################# SUBSTRAIT
@@ -80,5 +81,6 @@ if (NOT WIN32)
             LOAD_TESTS DONT_LINK
             GIT_URL https://github.com/duckdblabs/substrait
             GIT_TAG 5d621b1d7d16fe86f8b1930870c8e6bf05bcb92a
+            APPLY_PATCHES
             )
 endif()
