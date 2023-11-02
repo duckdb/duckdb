c28a2e98d9c30e794cb237fa1522da4f5cca1fc2
Tishj <t_b@live.nl>
2023-11-02 20:29:17 +0100

    producing a map with offsets bigger than int32

--- tools/pythonpkg/tests/fast/arrow/test_large_offsets.py
+++ tools/pythonpkg/tests/fast/arrow/test_large_offsets.py
@@ -26,3 +26,18 @@ class TestArrowLargeOffsets(object):
         duckdb_cursor.sql("set arrow_large_buffer_size = true")
         res2 = duckdb_cursor.sql("SELECT col FROM tbl2").arrow()
         res2.validate()
+
+    @pytest.mark.skip(reason="CI does not have enough memory to validate this")
+    def test_large_maps(self, duckdb_cursor):
+        ary = pa.array([np.arange(start=3000 * j, stop=3000 * (j + 1), dtype=np.uint64) for j in range(1_000_000)])
+        tbl = pa.Table.from_pydict(dict(col=ary))
+
+        with pytest.raises(
+            duckdb.InvalidInputException,
+            match='Arrow Appender: The maximum combined list offset for regular list buffers is 2147483647 but the offset of 2147481000 exceeds this.',
+        ):
+            arrow_map = duckdb_cursor.sql("select map(col, col) from tbl").arrow()
+
+        duckdb_cursor.sql("set arrow_large_buffer_size = true")
+        arrow_map_large = duckdb_cursor.sql("select map(col, col) from tbl").arrow()
+        arrow_map_large.validate()
