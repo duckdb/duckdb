4e2935a9fe154a0a731c5c11b0f4041001915f55
Pedro Holanda <pedroholanda@gmail.com>
2023-11-02 08:32:25 -0300

    check result and remove final

--- tools/pythonpkg/tests/fast/pandas/test_raise_warning.py
+++ tools/pythonpkg/tests/fast/pandas/test_raise_warning.py
@@ -1,6 +1,5 @@
 import duckdb
 import pandas as pd
-from typing import final
 import pytest
 
 
@@ -9,7 +8,7 @@ def test_9182():
 
     _con = duckdb.connect(database=':memory:')
 
-    EU: final = {
+    EU = {
         'BELGIUM',
         'BULGARIA',
         'DENMARK',
@@ -23,9 +22,9 @@ def test_9182():
         'SWITZERLAND',
     }
 
-    NAM: final = {'UNITED STATES', 'CANADA'}
-    LATAM: final = {'ARGENTINA', 'BRAZIL', 'COLOMBIA', 'CHILE', 'MEXICO', 'URUGUAY'}
-    AMERICAS: final = NAM | LATAM
+    NAM = {'UNITED STATES', 'CANADA'}
+    LATAM = {'ARGENTINA', 'BRAZIL', 'COLOMBIA', 'CHILE', 'MEXICO', 'URUGUAY'}
+    AMERICAS = NAM | LATAM
 
     dfs = {
         'EUROPE': pd.DataFrame({'EUROPE': list(EU)}),
@@ -36,3 +35,27 @@ def test_9182():
         for name, df in dfs.items():
             _con.register(name, df)
         assert len(warning_record) == 0
+
+    assert _con.execute("FROM EUROPE ORDER BY ALL").fetchall() == [
+        ('BELGIUM',),
+        ('BULGARIA',),
+        ('DENMARK',),
+        ('FINLAND',),
+        ('GERMANY',),
+        ('GREECE',),
+        ('HUNGARY',),
+        ('IRELAND',),
+        ('SPAIN',),
+        ('SWEDEN',),
+        ('SWITZERLAND',),
+    ]
+    assert _con.execute("FROM AMERICAS ORDER BY ALL").fetchall() == [
+        ('ARGENTINA',),
+        ('BRAZIL',),
+        ('CANADA',),
+        ('CHILE',),
+        ('COLOMBIA',),
+        ('MEXICO',),
+        ('UNITED STATES',),
+        ('URUGUAY',),
+    ]
