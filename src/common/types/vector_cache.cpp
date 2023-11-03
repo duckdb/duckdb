60d2368d86321c9ec1dbc77e9247127e7905e50e
Max Gabrielsson <max@gabrielsson.com>
2023-11-03 16:14:20 +0100

    actually resize after reset instead

--- src/common/types/vector_cache.cpp
+++ src/common/types/vector_cache.cpp
@@ -77,6 +77,9 @@ public:
 			auto &child_cache = child_caches[0]->Cast<VectorCacheBuffer>();
 			auto &array_child = result.auxiliary->Cast<VectorArrayBuffer>().GetChild();
 			child_cache.ResetFromCache(array_child, child_caches[0]);
+
+			// Ensure the child validity is (will be) large enough, even if its not initialized.
+			array_child.validity.Resize(array_child.validity.TargetCount(), child_cache.capacity);
 			break;
 		}
 		case PhysicalType::STRUCT: {
