8c4c3eec640be40481f9869d5322083af8ffd0a5
Tom Ebergen <tom@ebergen.com>
2023-11-10 13:53:59 +0100

    convert intersect and except to logical comparison joins

--- src/optimizer/operation_converter.cpp
+++ src/optimizer/operation_converter.cpp
@@ -8,6 +8,36 @@ namespace duckdb {
 
 
 unique_ptr<LogicalOperator> OperationConverter::Optimize(unique_ptr<LogicalOperator> op) {
+	for (auto &child : op->children) {
+		child = Optimize(std::move(child));
+	}
+	switch (op->type) {
+	case LogicalOperatorType::LOGICAL_INTERSECT:
+	case LogicalOperatorType::LOGICAL_EXCEPT: {
+		auto left = std::move(op->children[0]);
+		auto right = std::move(op->children[1]);
+		D_ASSERT(left->expressions.size() == right->expressions.size());
+		vector<JoinCondition> conditions;
+		// create equality condition for all columns
+		for (idx_t i = 0; i < left->expressions.size(); i++) {
+			JoinCondition cond;
+			cond.left = left->expressions[i]->Copy();
+			cond.right = right->expressions[i]->Copy();
+			cond.comparison = ExpressionType::COMPARE_NOT_DISTINCT_FROM;
+			conditions.push_back(std::move(cond));
+		}
+		JoinType join_type = op->type == LogicalOperatorType::LOGICAL_EXCEPT ? JoinType::ANTI : JoinType::SEMI;
+
+		auto join_op = make_uniq<LogicalComparisonJoin>(join_type);
+		join_op->children.push_back(std::move(left));
+		join_op->children.push_back(std::move(right));
+		join_op->conditions = std::move(conditions);
+
+		op = std::move(join_op);
+	}
+	default:
+		break;
+	}
 	return std::move(op);
 }
 
