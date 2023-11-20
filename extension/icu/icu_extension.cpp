bf4b7d01c318dee42cf609e94fb6e000f8f2116a
Tom Ebergen <tom@ebergen.com>
2023-11-20 17:04:43 +0100

    remove unused code

--- extension/icu/icu_extension.cpp
+++ extension/icu/icu_extension.cpp
@@ -32,7 +32,6 @@
 #include "unicode/ucol.h"
 
 #include <cassert>
-#include "iostream"
 
 namespace duckdb {
 
@@ -102,7 +101,7 @@ static void ICUCollateFunction(DataChunk &args, ExpressionState &state, Vector &
 			str_data[i * 2 + 1] = HEX_TABLE[byte % 16];
 		}
 		str_result.Finalize();
-//		printf("%s: %s\n", input.GetString().c_str(), str_result.GetString().c_str());
+		// printf("%s: %s\n", input.GetString().c_str(), str_result.GetString().c_str());
 		return str_result;
 	});
 }
