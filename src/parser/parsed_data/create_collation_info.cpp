76079fdb8f5bd32f56663bf6eebed8a5a67f415d
Tom Ebergen <tom@ebergen.com>
2023-11-22 13:37:40 +0100

    remove unnecessary collation set

--- src/parser/parsed_data/create_collation_info.cpp
+++ src/parser/parsed_data/create_collation_info.cpp
@@ -2,8 +2,6 @@
 
 namespace duckdb {
 
-static unordered_set<string> const equality_requires_collation = {"da"};
-
 CreateCollationInfo::CreateCollationInfo(string name_p, ScalarFunction function_p, bool combinable_p,
                                          bool not_required_for_equality_p)
     : CreateInfo(CatalogType::COLLATION_ENTRY), function(std::move(function_p)), combinable(combinable_p),
