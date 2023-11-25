b81b0c657ef441e2e15b26878c83cd8fab881b18
Giorgi <giorgi@giorgi.dev>
2023-11-25 19:34:43 +0400

    Try to fix formatting

--- src/main/capi/prepared-c.cpp
+++ src/main/capi/prepared-c.cpp
@@ -320,7 +320,8 @@ duckdb_state duckdb_execute_prepared(duckdb_prepared_statement prepared_statemen
 	return duckdb_translate_result(std::move(result), out_result);
 }
 
-duckdb_state duckdb_execute_prepared_streaming(duckdb_prepared_statement prepared_statement, duckdb_result *out_result) {
+duckdb_state duckdb_execute_prepared_streaming(duckdb_prepared_statement prepared_statement,
+                                               duckdb_result *out_result) {
 	auto wrapper = reinterpret_cast<PreparedStatementWrapper *>(prepared_statement);
 	if (!wrapper || !wrapper->statement || wrapper->statement->HasError()) {
 		return DuckDBError;
