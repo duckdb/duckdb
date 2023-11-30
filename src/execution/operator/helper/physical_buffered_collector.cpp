e12c5ed8ff32771d72484953504d0626329c026c
Tishj <t_b@live.nl>
2023-11-30 14:31:56 +0100

    create the QueryResult before the pipeline is complete so we can keep it blocked and unblock it later

---
+++ src/execution/operator/helper/physical_buffered_collector.cpp
@@ -0,0 +1,72 @@
+#include "duckdb/execution/operator/helper/physical_buffered_collector.hpp"
+#include "duckdb/common/types/chunk_collection.hpp"
+#include "duckdb/main/buffered_query_result.hpp"
+#include "duckdb/main/client_context.hpp"
+
+namespace duckdb {
+
+PhysicalBufferedCollector::PhysicalBufferedCollector(PreparedStatementData &data, bool parallel)
+    : PhysicalResultCollector(data), parallel(parallel) {
+}
+
+//===--------------------------------------------------------------------===//
+// Sink
+//===--------------------------------------------------------------------===//
+class BufferedCollectorGlobalState : public GlobalSinkState {
+public:
+	mutex glock;
+	shared_ptr<ClientContext> context;
+	shared_ptr<BufferedData> buffered_data;
+};
+
+class BufferedCollectorLocalState : public LocalSinkState {
+public:
+	unique_ptr<ColumnDataCollection> collection;
+	ColumnDataAppendState append_state;
+};
+
+SinkResultType PhysicalBufferedCollector::Sink(ExecutionContext &context, DataChunk &chunk,
+                                                   OperatorSinkInput &input) const {
+	auto &gstate = input.global_state.Cast<BufferedCollectorGlobalState>();
+
+	lock_guard<mutex> l(gstate.glock);
+	auto callback_state = input.interrupt_state;
+	gstate.buffered_data->AddToBacklog(callback_state);
+
+	return SinkResultType::BLOCKED;
+}
+
+SinkCombineResultType PhysicalBufferedCollector::Combine(ExecutionContext &context, OperatorSinkCombineInput &input) const {
+	return SinkCombineResultType::FINISHED;
+}
+
+unique_ptr<GlobalSinkState> PhysicalBufferedCollector::GetGlobalSinkState(ClientContext &context) const {
+	auto state = make_uniq<BufferedCollectorGlobalState>();
+	state->context = context.shared_from_this();
+	state->buffered_data = make_shared<BufferedData>();
+	return std::move(state);
+}
+
+unique_ptr<LocalSinkState> PhysicalBufferedCollector::GetLocalSinkState(ExecutionContext &context) const {
+	auto state = make_uniq<BufferedCollectorLocalState>();
+	state->collection = make_uniq<ColumnDataCollection>(Allocator::DefaultAllocator(), types);
+	state->collection->InitializeAppend(state->append_state);
+	return std::move(state);
+}
+
+unique_ptr<QueryResult> PhysicalBufferedCollector::GetResult(GlobalSinkState &state) {
+	auto &gstate = state.Cast<BufferedCollectorGlobalState>();
+	lock_guard<mutex> l(gstate.glock);
+	auto result = make_uniq<BufferedQueryResult>(statement_type, properties, types, names, gstate.context->GetClientProperties(), gstate.buffered_data);
+	return std::move(result);
+}
+
+bool PhysicalBufferedCollector::ParallelSink() const {
+	return parallel;
+}
+
+bool PhysicalBufferedCollector::SinkOrderDependent() const {
+	return true;
+}
+
+} // namespace duckdb
---
+++ src/main/buffered_query_result.cpp
@@ -0,0 +1,116 @@
+#include "duckdb/main/buffered_query_result.hpp"
+
+#include "duckdb/main/client_context.hpp"
+#include "duckdb/main/materialized_query_result.hpp"
+#include "duckdb/common/box_renderer.hpp"
+
+namespace duckdb {
+
+void BufferedData::AddToBacklog(InterruptState state) {
+	lock_guard<mutex> lock(glock);
+	blocked_sinks.push(state);
+}
+
+// --------- BUFFERED QUERY RESULT ----------
+
+BufferedQueryResult::BufferedQueryResult(StatementType statement_type, StatementProperties properties,
+	                                   vector<LogicalType> types, vector<string> names, ClientProperties client_properties, shared_ptr<BufferedData> buffered_data)
+    : QueryResult(QueryResultType::BUFFERED_RESULT, statement_type, std::move(properties), std::move(types),
+                  std::move(names), std::move(client_properties)), buffered_data(buffered_data) {
+}
+
+BufferedQueryResult::~BufferedQueryResult() {
+}
+
+string BufferedQueryResult::ToString() {
+	string result;
+	if (success) {
+		result = HeaderToString();
+		result += "[[BUFFERED RESULT]]";
+	} else {
+		result = GetError() + "\n";
+	}
+	return result;
+}
+
+//unique_ptr<ClientContextLock> BufferedQueryResult::LockContext() {
+//	if (!context) {
+//		string error_str = "Attempting to execute an unsuccessful or closed pending query result";
+//		if (HasError()) {
+//			error_str += StringUtil::Format("\nError: %s", GetError());
+//		}
+//		throw InvalidInputException(error_str);
+//	}
+//	return context->LockContext();
+//}
+
+//void BufferedQueryResult::CheckExecutableInternal(ClientContextLock &lock) {
+//	if (!IsOpenInternal(lock)) {
+//		string error_str = "Attempting to execute an unsuccessful or closed pending query result";
+//		if (HasError()) {
+//			error_str += StringUtil::Format("\nError: %s", GetError());
+//		}
+//		throw InvalidInputException(error_str);
+//	}
+//}
+
+unique_ptr<DataChunk> BufferedQueryResult::FetchRaw() {
+	//unique_ptr<DataChunk> chunk;
+	//{
+	//	auto lock = LockContext();
+	//	CheckExecutableInternal(*lock);
+	//	chunk = context->Fetch(*lock, *this);
+	//}
+	//if (!chunk || chunk->ColumnCount() == 0 || chunk->size() == 0) {
+	//	Close();
+	//	return nullptr;
+	//}
+	//return chunk;
+	return nullptr;
+}
+
+unique_ptr<MaterializedQueryResult> BufferedQueryResult::Materialize() {
+	//if (HasError() || !context) {
+	//	return make_uniq<MaterializedQueryResult>(GetErrorObject());
+	//}
+	//auto collection = make_uniq<ColumnDataCollection>(Allocator::DefaultAllocator(), types);
+
+	//ColumnDataAppendState append_state;
+	//collection->InitializeAppend(append_state);
+	//while (true) {
+	//	auto chunk = Fetch();
+	//	if (!chunk || chunk->size() == 0) {
+	//		break;
+	//	}
+	//	collection->Append(append_state, *chunk);
+	//}
+	//auto result =
+	//    make_uniq<MaterializedQueryResult>(statement_type, properties, names, std::move(collection), client_properties);
+	//if (HasError()) {
+	//	return make_uniq<MaterializedQueryResult>(GetErrorObject());
+	//}
+	//return result;
+	return nullptr;
+}
+
+//bool BufferedQueryResult::IsOpenInternal(ClientContextLock &lock) {
+//	bool invalidated = !success || !context;
+//	if (!invalidated) {
+//		invalidated = !context->IsActiveResult(lock, this);
+//	}
+//	return !invalidated;
+//}
+
+//bool BufferedQueryResult::IsOpen() {
+//	if (!success || !context) {
+//		return false;
+//	}
+//	auto lock = LockContext();
+//	return IsOpenInternal(*lock);
+//}
+
+//void BufferedQueryResult::Close() {
+//	context.reset();
+//}
+
+} // namespace duckdb
