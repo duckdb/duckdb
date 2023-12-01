5e58fa0bbd7831bae112b02349eb8841db1e8922
Tom Ebergen <tom@ebergen.com>
2023-12-01 16:22:36 +0100

    make format fix and other improvements

--- src/include/duckdb/common/enums/join_type.hpp
+++ src/include/duckdb/common/enums/join_type.hpp
@@ -16,17 +16,17 @@ namespace duckdb {
 // Join Types
 //===--------------------------------------------------------------------===//
 enum class JoinType : uint8_t {
-	INVALID = 0,   // invalid join type
-	LEFT = 1,      // left
-	RIGHT = 2,     // right
-	INNER = 3,     // inner
-	OUTER = 4,     // outer
-	SEMI = 5, // LEFT SEMI join returns left side row ONLY if it has a join partner, no duplicates.
-	ANTI = 6, // LEFT ANTI join returns left side row ONLY if it has NO join partner, no duplicates
-	MARK = 7,   // MARK join returns marker indicating whether or not there is a join partner (true), there is no join
-	            // partner (false)
-	SINGLE = 8, // SINGLE join is like LEFT OUTER JOIN, BUT returns at most one join partner per entry on the LEFT side
-	            // (and NULL if no partner is found)
+	INVALID = 0, // invalid join type
+	LEFT = 1,    // left
+	RIGHT = 2,   // right
+	INNER = 3,   // inner
+	OUTER = 4,   // outer
+	SEMI = 5,    // LEFT SEMI join returns left side row ONLY if it has a join partner, no duplicates.
+	ANTI = 6,    // LEFT ANTI join returns left side row ONLY if it has NO join partner, no duplicates
+	MARK = 7,    // MARK join returns marker indicating whether or not there is a join partner (true), there is no join
+	             // partner (false)
+	SINGLE = 8,  // SINGLE join is like LEFT OUTER JOIN, BUT returns at most one join partner per entry on the LEFT side
+	             // (and NULL if no partner is found)
 	RIGHT_SEMI = 9,
 	RIGHT_ANTI = 10
 };
