f211323609fb77d929fb3a2c7e8b18cbe8ca7b29
Pedro Holanda <pedroholanda@gmail.com>
2023-12-05 16:00:45 +0100

    parse_chunk fix

--- src/include/duckdb/execution/operator/scan/csv/parse_chunk.hpp
+++ src/include/duckdb/execution/operator/scan/csv/parse_chunk.hpp
@@ -13,10 +13,7 @@
 namespace duckdb {
 struct ParseChunk {
 	inline static void Initialize(CSVScanner &scanner) {
-		scanner.state = CSVState::STANDARD;
-		scanner.previous_state = CSVState::STANDARD;
-		scanner.pre_previous_state = CSVState::STANDARD;
-
+		scanner.states.Initialize(CSVState::EMPTY_LINE);
 		scanner.cur_rows = 0;
 		scanner.column_count = 0;
 		scanner.value = "";
@@ -24,32 +21,38 @@ struct ParseChunk {
 
 	inline static bool Process(CSVScanner &scanner, DataChunk &parse_chunk, char current_char, idx_t current_pos) {
 		auto &sniffing_state_machine = scanner.GetStateMachineSniff();
-		scanner.pre_previous_state = scanner.previous_state;
-		scanner.previous_state = scanner.state;
-		scanner.state = static_cast<CSVState>(
-		    sniffing_state_machine
-		        .transition_array[static_cast<uint8_t>(scanner.state)][static_cast<uint8_t>(current_char)]);
+		auto& states = scanner.states;
 
-		bool carriage_return = scanner.previous_state == CSVState::CARRIAGE_RETURN;
-		if (scanner.previous_state == CSVState::DELIMITER ||
-		    (scanner.previous_state == CSVState::RECORD_SEPARATOR && scanner.state != CSVState::EMPTY_LINE) ||
-		    (scanner.state != CSVState::RECORD_SEPARATOR && carriage_return)) {
+		sniffing_state_machine.Transition(states,current_char);
+
+		bool carriage_return = states.previous_state == CSVState::CARRIAGE_RETURN;
+		if (states.previous_state == CSVState::DELIMITER || (states.previous_state == CSVState::RECORD_SEPARATOR) ||
+		    (states.current_state != CSVState::RECORD_SEPARATOR && carriage_return)) {
 			// Started a new value
 			// Check if it's UTF-8 (Or not?)
 			scanner.VerifyUTF8();
+			if (scanner.column_count >= parse_chunk.ColumnCount() && sniffing_state_machine.options.ignore_errors) {
+				return false;
+			}
+			if (scanner.column_count >= parse_chunk.ColumnCount()) {
+				throw InvalidInputException("Error in file \"%s\": CSV options could not be auto-detected. Consider "
+				                            "setting parser options manually.",
+				                            sniffing_state_machine.options.file_path);
+			}
 			auto &v = parse_chunk.data[scanner.column_count++];
 			auto parse_data = FlatVector::GetData<string_t>(v);
-			auto &validity_mask = FlatVector::Validity(v);
 			if (scanner.value.empty()) {
+				auto &validity_mask = FlatVector::Validity(v);
 				validity_mask.SetInvalid(scanner.cur_rows);
 			} else {
 				parse_data[scanner.cur_rows] = StringVector::AddStringOrBlob(v, string_t(scanner.value));
 			}
 			scanner.value = "";
 		}
-		if (((scanner.previous_state == CSVState::RECORD_SEPARATOR && scanner.state != CSVState::EMPTY_LINE) ||
-		     (scanner.state != CSVState::RECORD_SEPARATOR && carriage_return)) &&
-		    sniffing_state_machine.options.null_padding && scanner.column_count < parse_chunk.ColumnCount()) {
+		if (((states.previous_state == CSVState::RECORD_SEPARATOR && states.current_state != CSVState::EMPTY_LINE) ||
+		     (states.current_state != CSVState::RECORD_SEPARATOR && carriage_return)) &&
+		    (sniffing_state_machine.options.null_padding || sniffing_state_machine.options.ignore_errors) &&
+		    scanner.column_count < parse_chunk.ColumnCount()) {
 			// It's a new row, check if we need to pad stuff
 			while (scanner.column_count < parse_chunk.ColumnCount()) {
 				auto &v = parse_chunk.data[scanner.column_count++];
@@ -57,41 +60,46 @@ struct ParseChunk {
 				validity_mask.SetInvalid(scanner.cur_rows);
 			}
 		}
-		if (scanner.state == CSVState::STANDARD ||
-		    (scanner.state == CSVState::QUOTED && scanner.previous_state == CSVState::QUOTED)) {
+		if (states.current_state == CSVState::STANDARD ||
+		    (states.current_state == CSVState::QUOTED && states.previous_state == CSVState::QUOTED)) {
 			scanner.value += current_char;
 		}
-		scanner.cur_rows +=
-		    scanner.previous_state == CSVState::RECORD_SEPARATOR && scanner.state != CSVState::EMPTY_LINE;
-		scanner.column_count -= scanner.column_count * (scanner.previous_state == CSVState::RECORD_SEPARATOR);
+		scanner.cur_rows += states.previous_state == CSVState::RECORD_SEPARATOR && scanner.column_count > 0;
+		scanner.column_count -= scanner.column_count * (states.previous_state == CSVState::RECORD_SEPARATOR);
 
 		// It means our carriage return is actually a record separator
-		scanner.cur_rows += scanner.state != CSVState::RECORD_SEPARATOR && carriage_return;
-		scanner.column_count -= scanner.column_count * (scanner.state != CSVState::RECORD_SEPARATOR && carriage_return);
+		scanner.cur_rows += states.current_state != CSVState::RECORD_SEPARATOR && carriage_return && scanner.column_count > 0;
+		scanner.column_count -= scanner.column_count * (states.current_state != CSVState::RECORD_SEPARATOR && carriage_return);
 
 		if (scanner.cur_rows >= STANDARD_VECTOR_SIZE) {
-			// We sniffed enough rows
+			// We parsed enough rows
 			return true;
 		}
 		return false;
 	}
 
 	inline static void Finalize(CSVScanner &scanner, DataChunk &parse_chunk) {
-		if (scanner.cur_rows < STANDARD_VECTOR_SIZE && scanner.state != CSVState::EMPTY_LINE) {
+		auto &sniffing_state_machine = scanner.GetStateMachineSniff();
+		auto& states = scanner.states;
+
+		if (scanner.cur_rows < STANDARD_VECTOR_SIZE && states.current_state != CSVState::EMPTY_LINE) {
 			scanner.VerifyUTF8();
-			auto &v = parse_chunk.data[scanner.column_count++];
-			auto parse_data = FlatVector::GetData<string_t>(v);
-			if (scanner.value.empty()) {
-				auto &validity_mask = FlatVector::Validity(v);
-				validity_mask.SetInvalid(scanner.cur_rows);
-			} else {
-				parse_data[scanner.cur_rows] = StringVector::AddStringOrBlob(v, string_t(scanner.value));
+			if (scanner.column_count < parse_chunk.ColumnCount() || !sniffing_state_machine.options.ignore_errors) {
+				auto &v = parse_chunk.data[scanner.column_count++];
+				auto parse_data = FlatVector::GetData<string_t>(v);
+				if (scanner.value.empty()) {
+					auto &validity_mask = FlatVector::Validity(v);
+					validity_mask.SetInvalid(scanner.cur_rows);
+				} else {
+					parse_data[scanner.cur_rows] = StringVector::AddStringOrBlob(v, string_t(scanner.value));
+				}
+				while (scanner.column_count < parse_chunk.ColumnCount()) {
+					auto &v_pad = parse_chunk.data[scanner.column_count++];
+					auto &validity_mask = FlatVector::Validity(v_pad);
+					validity_mask.SetInvalid(scanner.cur_rows);
+				}
 			}
-			while (scanner.column_count < parse_chunk.ColumnCount()) {
-				auto &v_pad = parse_chunk.data[scanner.column_count++];
-				auto &validity_mask = FlatVector::Validity(v_pad);
-				validity_mask.SetInvalid(scanner.cur_rows);
-			}
+
 			scanner.cur_rows++;
 		}
 		parse_chunk.SetCardinality(scanner.cur_rows);
