9bab5a88061ebfdf0c2924ab567c593153a6839b
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-12-08 10:02:30 +0100

    Fix for UseDeletedVersion - always use when id is NOT_DELETED_ID

--- src/common/constants.cpp
+++ src/common/constants.cpp
@@ -12,10 +12,10 @@ const column_t COLUMN_IDENTIFIER_ROW_ID = (column_t)-1;
 const sel_t ZERO_VECTOR[STANDARD_VECTOR_SIZE] = {0};
 const double PI = 3.141592653589793;
 
-const transaction_t TRANSACTION_ID_START = 4611686018427388000ULL;                    // 2^62
-const transaction_t MAX_TRANSACTION_ID = NumericLimits<transaction_t>::Maximum() - 1; // 2^64 - 2
-const transaction_t NOT_DELETED_ID = NumericLimits<transaction_t>::Maximum();         // 2^64 - 1
-const transaction_t MAXIMUM_QUERY_ID = NumericLimits<transaction_t>::Maximum();       // 2^64
+const transaction_t TRANSACTION_ID_START = 4611686018427388000ULL;                // 2^62
+const transaction_t MAX_TRANSACTION_ID = NumericLimits<transaction_t>::Maximum(); // 2^63 - 2
+const transaction_t NOT_DELETED_ID = NumericLimits<transaction_t>::Maximum() - 1; // 2^63 - 1
+const transaction_t MAXIMUM_QUERY_ID = NumericLimits<transaction_t>::Maximum();   // 2^64
 
 bool IsPowerOfTwo(uint64_t v) {
 	return (v & (v - 1)) == 0;
