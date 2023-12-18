68e3a5e5bf78e4cc2c0188e6fd4a78d53a282407
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2023-12-18 11:11:46 +0100

    disabling more tests for smaller block sizes, changing nightly test to run all tests, and with different vector sizes

--- scripts/test_block_sizes.py
+++ scripts/test_block_sizes.py
@@ -1,7 +1,9 @@
-import os, sys, re
+import os, re
 from python_helpers import open_utf8
 
+# we want to test different block sizes with different vector sizes
 block_sizes = [16384]
+vector_sizes = [2048, 512]
 
 current_dir = os.getcwd()
 build_dir = os.path.join(os.getcwd(), 'build', 'release')
@@ -30,6 +32,15 @@ for block_size in block_sizes:
         r'constexpr static idx_t BLOCK_ALLOC_SIZE = \d+',
         'constexpr static idx_t BLOCK_ALLOC_SIZE = %d' % (block_size,),
     )
-    execute_system_command('rm -rf build')
-    execute_system_command('make relassert')
-    execute_system_command('python3 scripts/run_tests_one_by_one.py build/relassert/test/unittest')
+
+    for vector_size in vector_sizes:
+        print("TESTING STANDARD_VECTOR_SIZE=%d" % (vector_size,))
+        replace_in_file(
+            'src/include/duckdb/common/vector_size.hpp',
+            r'#define STANDARD_VECTOR_SIZE \d+',
+            '#define STANDARD_VECTOR_SIZE %d' % (vector_size,),
+            )
+
+        execute_system_command('rm -rf build')
+        execute_system_command('make relassert')
+        execute_system_command('build/release/test/unittest "*"')
