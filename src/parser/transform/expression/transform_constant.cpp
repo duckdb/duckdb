1f065ef873f2dd4bd927fbf07a4487e9cce5d2b5
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-12-27 12:21:05 +0100

    This needs to be SQLNULL

--- src/parser/transform/expression/transform_constant.cpp
+++ src/parser/transform/expression/transform_constant.cpp
@@ -112,7 +112,7 @@ bool Transformer::ConstructConstantFromExpression(const ParsedExpression &expr, 
 			}
 
 			// figure out child type
-			LogicalType child_type(LogicalTypeId::INTEGER);
+			LogicalType child_type(LogicalTypeId::SQLNULL);
 			for (auto &child_value : values) {
 				child_type = LogicalType::MaxLogicalType(child_type, child_value.type());
 			}
