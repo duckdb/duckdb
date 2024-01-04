8db51d9c210756a379253f30e958f916fbcaf048
Tmonster <tom@ebergen.com>
2024-01-04 10:18:12 -0800

    add more headers to get CI to pass

--- src/include/duckdb/execution/reservoir_sample.hpp
+++ src/include/duckdb/execution/reservoir_sample.hpp
@@ -8,8 +8,10 @@
 
 #pragma once
 
+#include "duckdb/common/allocator.hpp"
 #include "duckdb/common/common.hpp"
 #include "duckdb/common/random_engine.hpp"
+#include "duckdb/common/types/data_chunk.hpp"
 
 #include "duckdb/common/queue.hpp"
 
