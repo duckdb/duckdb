032cfef27bdff38116af46e5305f5702ca3c47b3
Sam Ansmink <samansmink@hotmail.com>
2024-01-17 11:49:46 +0100

    more fine-grained fetch-row-verification

--- scripts/run_tests_one_by_one.py
+++ scripts/run_tests_one_by_one.py
@@ -29,7 +29,7 @@ for i in range(len(sys.argv)):
 
 if len(sys.argv) < 2:
     print(
-        "Expected usage: python3 scripts/run_tests_one_by_one.py build/debug/test/unittest [--no-exit] [--profile] [--no-assertions] [--force-storage] [--add-timing]"
+        "Expected usage: python3 scripts/run_tests_one_by_one.py build/debug/test/unittest [--no-exit] [--profile] [--no-assertions] [--force-storage]"
     )
     exit(1)
 unittest_program = sys.argv[1]
