910231fd19a1e3f2c1070ebc096747a9bc6c69ee
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-01-23 21:15:05 +0100

    Cleanup, and lists WIP

--- src/core_functions/scalar/blob/create_sort_key.cpp
+++ src/core_functions/scalar/blob/create_sort_key.cpp
@@ -83,6 +83,10 @@ template<class T>
 struct SortKeyConstantOperator {
 	using TYPE = T;
 
+	static idx_t GetEncodeLength(TYPE input) {
+		return sizeof(T);
+	}
+
 	static idx_t Encode(data_ptr_t result, TYPE input) {
 		Radix::EncodeData<T>(result, input);
 		return sizeof(T);
@@ -110,54 +114,138 @@ struct SortKeyVarcharOperator {
 //===--------------------------------------------------------------------===//
 // Get Sort Key Length
 //===--------------------------------------------------------------------===//
-template<class OP>
-void TemplatedGetSortKeyLength(UnifiedVectorFormat &udata, idx_t size, unsafe_vector<idx_t> &variable_lengths) {
-	auto data = UnifiedVectorFormat::GetData<typename OP::TYPE>(udata);
-	for(idx_t r = 0; r < size; r++) {
-		auto idx = udata.sel->get_index(r);
-		if (!udata.validity.RowIsValid(idx)) {
+struct SortKeyLengthInfo {
+	explicit SortKeyLengthInfo(idx_t size) : constant_length(0) {
+		variable_lengths.resize(size, 0);
+	}
+
+	idx_t constant_length;
+	unsafe_vector<idx_t> variable_lengths;
+};
+
+struct SortKeyVectorData {
+	SortKeyVectorData(Vector &input, idx_t size) :
+		vec(input) {
+		input.ToUnifiedFormat(size, format);
+		this->size = size;
+
+		switch(input.GetType().InternalType()) {
+		case PhysicalType::LIST: {
+			auto &child_entry = ListVector::GetEntry(input);
+			auto child_size = ListVector::GetListSize(input);
+			child_data.emplace_back(child_entry, child_size);
 			break;
 		}
-		variable_lengths[r] += OP::GetEncodeLength(data[idx]);
+		default:
+			break;
+		}
+	}
+
+	PhysicalType GetPhysicalType() {
+		return vec.GetType().InternalType();
+	}
+
+	Vector &vec;
+	idx_t size;
+	UnifiedVectorFormat format;
+	vector<SortKeyVectorData> child_data;
+};
+
+
+static void GetSortKeyLength(SortKeyVectorData &vector_data, const SelectionVector &result_vector, SortKeyLengthInfo &result);
+
+template<class OP>
+void TemplatedGetSortKeyLength(SortKeyVectorData &vector_data, const SelectionVector &result_vector, SortKeyLengthInfo &result) {
+	auto &format = vector_data.format;
+	auto data = UnifiedVectorFormat::GetData<typename OP::TYPE>(vector_data.format);
+	for(idx_t r = 0; r < vector_data.size; r++) {
+		auto result_index = result_vector.get_index(r);
+		result.variable_lengths[result_index]++; // every value is prefixed by a validity byte
+
+		auto idx = format.sel->get_index(r);
+		if (!format.validity.RowIsValid(idx)) {
+			continue;
+		}
+		result.variable_lengths[result_index] += OP::GetEncodeLength(data[idx]);
 	}
 }
 
-static void GetSortKeyLength(Vector &vec, UnifiedVectorFormat &udata, idx_t size, idx_t &constant_length, unsafe_vector<idx_t> &variable_lengths) {
-	auto physical_type = vec.GetType().InternalType();
-	// every row is prefixed by a validity byte
-	constant_length += 1;
-	if (TypeIsConstantSize(physical_type)) {
-		constant_length += GetTypeIdSize(physical_type);
-		return;
+void GetSortKeyLengthList(SortKeyVectorData &vector_data, const SelectionVector &result_vector, SortKeyLengthInfo &result) {
+	auto data = UnifiedVectorFormat::GetData<list_entry_t>(vector_data.format);
+	auto &child_data = vector_data.child_data[0];
+
+
+	// construct a selection vector that points, for each of the child elements, to which sort key in the result they belong
+	SelectionVector child_result_sel;
+	child_result_sel.Initialize(child_data.size);
+	idx_t entry = 0;
+	for (idx_t r = 0; r < vector_data.size; r++) {
+		auto result_idx = result_vector.get_index(r);
+		result.variable_lengths[result_idx]++; // every list is prefixed by a validity byte
+
+		auto idx = vector_data.format.sel->get_index(r);
+		if (!vector_data.format.validity.RowIsValid(idx)) {
+			continue;
+		}
+		auto list_entry = data[idx];
+		// for each list we have an "end of list" delimiter
+		result.variable_lengths[result_idx]++;
+		// set up the selection vector for the child
+		for (idx_t k = 0; k < list_entry.length; k++) {
+			child_result_sel.set_index(entry++, result_idx);
+		}
 	}
+	// now recursively call GetSortKeyLength on the child element
+	GetSortKeyLength(child_data, child_result_sel, result);
+}
+
+static void GetSortKeyLength(SortKeyVectorData &vector_data, const SelectionVector &result_vector, SortKeyLengthInfo &result) {
+	auto physical_type = vector_data.GetPhysicalType();
 	// handle variable lengths
 	switch(physical_type) {
+	case PhysicalType::INT32:
+		TemplatedGetSortKeyLength<SortKeyConstantOperator<int32_t>>(vector_data, result_vector, result);
+		break;
 	case PhysicalType::VARCHAR:
-		if (vec.GetType().id() == LogicalTypeId::VARCHAR) {
-			TemplatedGetSortKeyLength<SortKeyVarcharOperator>(udata, size, variable_lengths);
+		if (vector_data.vec.GetType().id() == LogicalTypeId::VARCHAR) {
+			TemplatedGetSortKeyLength<SortKeyVarcharOperator>(vector_data, result_vector, result);
 		} else {
-			throw InternalException("FIXME: ConstructSortKey blob");
+			throw NotImplementedException("FIXME: ConstructSortKey blob");
 		}
 		break;
+	case PhysicalType::LIST:
+		GetSortKeyLengthList(vector_data, result_vector, result);
+		break;
 	default:
-		throw InternalException("Unsupported physical type in GetSortKeyLength", physical_type);
+		throw NotImplementedException("Unsupported physical type %s in GetSortKeyLength", physical_type);
 	}
 }
 
+static void GetSortKeyLength(SortKeyVectorData &vector_data, SortKeyLengthInfo &result) {
+	// top-level method
+	auto physical_type = vector_data.GetPhysicalType();
+	if (TypeIsConstantSize(physical_type)) {
+		// every row is prefixed by a validity byte
+		result.constant_length += 1;
+		result.constant_length += GetTypeIdSize(physical_type);
+		return;
+	}
+	GetSortKeyLength(vector_data,  *FlatVector::IncrementalSelectionVector(), result);
+}
 
 //===--------------------------------------------------------------------===//
 // Construct Sort Key
 //===--------------------------------------------------------------------===//
 template<class OP>
-void TemplatedConstructSortKey(UnifiedVectorFormat &udata, idx_t size, OrderModifiers modifiers, unsafe_vector<idx_t> &offsets, string_t *result_data) {
-	auto data = UnifiedVectorFormat::GetData<typename OP::TYPE>(udata);
+void TemplatedConstructSortKey(SortKeyVectorData &vector_data, OrderModifiers modifiers, unsafe_vector<idx_t> &offsets, string_t *result_data) {
+	auto data = UnifiedVectorFormat::GetData<typename OP::TYPE>(vector_data.format);
 	data_t null_byte = modifiers.null_type == OrderByNullType::NULLS_LAST ? 1 : 0;
 	data_t valid_byte = 1 - null_byte;
 	bool flip_bytes = modifiers.order_type == OrderType::DESCENDING;
-	for(idx_t r = 0; r < size; r++) {
+	for(idx_t r = 0; r < vector_data.size; r++) {
 		auto result_ptr = data_ptr_cast(result_data[r].GetDataWriteable());
-		auto idx = udata.sel->get_index(r);
-		if (!udata.validity.RowIsValid(idx)) {
+		auto idx = vector_data.format.sel->get_index(r);
+		if (!vector_data.format.validity.RowIsValid(idx)) {
 			// NULL value - write the null byte and skip
 			result_ptr[offsets[r]] = null_byte;
 			offsets[r]++;
@@ -177,51 +265,53 @@ void TemplatedConstructSortKey(UnifiedVectorFormat &udata, idx_t size, OrderModi
 	}
 }
 
-static void ConstructSortKey(Vector &vec, UnifiedVectorFormat &udata, idx_t size, OrderModifiers modifiers, unsafe_vector<idx_t> &offsets, string_t *result_data) {
-	switch(vec.GetType().InternalType()) {
+static void ConstructSortKey(SortKeyVectorData &vector_data, OrderModifiers modifiers, unsafe_vector<idx_t> &offsets, string_t *result_data) {
+	switch(vector_data.GetPhysicalType()) {
 	case PhysicalType::INT32:
-		TemplatedConstructSortKey<SortKeyConstantOperator<int32_t>>(udata, size, modifiers, offsets, result_data);
+		TemplatedConstructSortKey<SortKeyConstantOperator<int32_t>>(vector_data, modifiers, offsets, result_data);
 		break;
 	case PhysicalType::VARCHAR:
-		if (vec.GetType().id() == LogicalTypeId::VARCHAR) {
-			TemplatedConstructSortKey<SortKeyVarcharOperator>(udata, size, modifiers, offsets, result_data);
+		if (vector_data.vec.GetType().id() == LogicalTypeId::VARCHAR) {
+			TemplatedConstructSortKey<SortKeyVarcharOperator>(vector_data, modifiers, offsets, result_data);
 		} else {
-			throw InternalException("FIXME: ConstructSortKey blob");
+			throw NotImplementedException("FIXME: ConstructSortKey blob");
 		}
 		break;
 	default:
-		throw InternalException("Unsupported physical type in ConstructSortKey");
+		throw NotImplementedException("Unsupported type %s in ConstructSortKey", vector_data.vec.GetType());
 	}
 }
 
 static void CreateSortKeyFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto &bind_data = state.expr.Cast<BoundFunctionExpression>().bind_info->Cast<CreateSortKeyBindData>();
-	idx_t constant_length = 0;
 
-	unsafe_vector<idx_t> variable_lengths;
-	variable_lengths.resize(args.size(), 0);
-
-	auto unified_formats = args.ToUnifiedFormat();
+	// prepare the sort key data
+	vector<SortKeyVectorData> sort_key_data;
+	for(idx_t c = 0; c < args.ColumnCount(); c += 2) {
+		sort_key_data.emplace_back(args.data[c], args.size());
+	}
 
 	// two phases
 	// a) get the length of the final sorted key
 	// b) allocate the sorted key and construct
 	// we do all of this in a vectorized manner
-	for(idx_t c = 0; c < args.ColumnCount(); c += 2) {
-		GetSortKeyLength(args.data[c], unified_formats[c], args.size(), constant_length, variable_lengths);
+	SortKeyLengthInfo key_lengths(args.size());
+	for(auto &vector_data : sort_key_data) {
+		GetSortKeyLength(vector_data, key_lengths);
 	}
 	// allocate the empty sort keys
 	auto result_data = FlatVector::GetData<string_t>(result);
 	for(idx_t r = 0; r < args.size(); r++) {
-		result_data[r] = StringVector::EmptyString(result, variable_lengths[r] + constant_length);
+		result_data[r] = StringVector::EmptyString(result, key_lengths.variable_lengths[r] + key_lengths.constant_length);
 	}
 
 	unsafe_vector<idx_t> offsets;
 	offsets.resize(args.size(), 0);
 	// now construct the sort keys
-	for(idx_t c = 0; c < args.ColumnCount(); c += 2) {
-		ConstructSortKey(args.data[c], unified_formats[c], args.size(), bind_data.modifiers[c / 2], offsets, result_data);
+	for(idx_t c = 0; c < sort_key_data.size(); c++) {
+		ConstructSortKey(sort_key_data[c], bind_data.modifiers[c], offsets, result_data);
 	}
+	// call Finalize on the result
 	for(idx_t r = 0; r < args.size(); r++) {
 		result_data[r].Finalize();
 	}
