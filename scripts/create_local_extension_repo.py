988139051e76781c96f67a200224070ec09ad47a
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-01-25 13:31:32 +0100

    Bring back python script, and make python3 compulsory when LOCAL_EXTENSION_REPO is set

---
+++ scripts/create_local_extension_repo.py
@@ -0,0 +1,45 @@
+###
+# This script copies all extensions in a build folder from their cmake-produced structure into the extension repository
+# structure of ./<duckdb_version>/<build_archictecture>/<extension_name>.duckdb_extension
+# Note that it requires duckdb_platofrom_out file to be populated with the platform
+
+import os
+import sys
+import subprocess
+import glob
+import shutil
+
+if len(sys.argv) != 5:
+    print(
+        "Usage: scripts/create_local_extension_repo.py <duckdb_version> <duckdb_platform_out> <path/to/duckdb/build> <path/to/local_repo>"
+    )
+    exit(1)
+
+duckdb_version = sys.argv[1]
+duckdb_platform_out = sys.argv[2]
+extension_path = sys.argv[3]
+dst_path = sys.argv[4]
+
+with open(duckdb_platform_out, 'r') as f:
+    duckdb_platform = [line.split(None, 1)[0] for line in f][0]
+
+if os.name == 'nt':
+    duckdb_path = duckdb_path.replace("/", "\\")
+    extension_path = extension_path.replace("/", "\\")
+    dst_path = dst_path.replace("/", "\\")
+
+print(f"Paths as received: {extension_path}, {dst_path} + '/{duckdb_version}/{duckdb_platform}/")
+
+# Create destination path
+dest_path = os.path.join(dst_path, duckdb_version, duckdb_platform)
+if not os.path.exists(dest_path):
+    os.makedirs(dest_path)
+
+# Now copy over the extensions to the correct path
+glob_string = os.path.join(extension_path, 'extension', '*', '*.duckdb_extension')
+
+print("Copying extensions into repository:")
+for file in glob.glob(glob_string):
+    dest_file = os.path.join(dest_path, os.path.basename(file))
+    print(f" > {file} -> {dest_file}")
+    shutil.copy(file, dest_file)
