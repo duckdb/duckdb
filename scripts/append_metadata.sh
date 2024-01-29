368fb1802d91895963dbee6fb8a83754fd9a9148
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-01-29 16:28:50 +0100

    Fix scritps/append_metadata to compute number of fields anonymously

--- scripts/append_metadata.sh
+++ scripts/append_metadata.sh
@@ -2,7 +2,7 @@
 
 # Usage: ./script/append_metadata.sh <extension_file_to_append_to> <parameters...>
 # Currently hardcoded to host up to 8 fields
-# Example: ./scripts/append_metadata.sh file.duckdb_extension 0 git_hash_duckdb git_hash_extension platfrom
+# Example: ./scripts/append_metadata.sh file.duckdb_extension git_hash_duckdb_file git_hash_extension_file platfrom_file
 
 if (($# >= 9)); then
   echo "Too many parameters provided, current script can handle at maxium 8 fields"
@@ -26,18 +26,26 @@ echo -n -e '\x80\x04' >> "$1"
 dd if=/dev/zero of="$1.empty_32" bs=32 count=1 &> /dev/null
 dd if=/dev/zero of="$1.empty_256" bs=256 count=1 &> /dev/null
 
-for ((i=$#; i<9; i++))
+# Write empty fields
+for ((i=$#; i<8; i++))
 do
   cat "$1.empty_32" >> "$1"
 done
 
+# Write provided fiedls (backwards)
 for ((i=$#; i>=2; i--))
 do
-  echo -n "${!i}" > "$1.add"
+  cat "${!i}" > "$1.add"
   cat "$1.empty_32" >> "$1.add"
   dd if="$1.add" of="$1.add_trunc" bs=32 count=1 &> /dev/null
   cat "$1.add_trunc" >> "$1"
 done
+
+# Write how many fields have been written during previous step
+  echo -n "$(($# - 1))" > "$1.add"
+  cat "$1.empty_32" >> "$1.add"
+  dd if="$1.add" of="$1.add_trunc" bs=32 count=1 &> /dev/null
+  cat "$1.add_trunc" >> "$1"
 
 cat "$1.empty_256" >> "$1"
 
