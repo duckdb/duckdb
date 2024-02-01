b2ad75910ab1a2f9706efea94ecb960320696030
Tishj <t_b@live.nl>
2024-02-01 11:51:32 +0100

    add back lines that were accidentally lost in the merge

--- src/main/stream_query_result.cpp
+++ src/main/stream_query_result.cpp
@@ -69,6 +69,8 @@ unique_ptr<DataChunk> StreamQueryResult::FetchInternal(ClientContextLock &lock) 
 				ValidChecker::Invalidate(db_instance, error.RawMessage());
 			}
 		}
+		ProcessError(error, active_query->query);
+		SetError(std::move(error));
 	} catch (...) { // LCOV_EXCL_START
 		SetError(ErrorData("Unhandled exception in FetchInternal"));
 	} // LCOV_EXCL_STOP
