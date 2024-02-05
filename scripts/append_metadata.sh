fc6136a6e2a430aa724f3da406c4bf677986d4b6
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-02-05 09:20:54 +0100

    Remove debugging logging

--- scripts/append_metadata.sh
+++ scripts/append_metadata.sh
@@ -9,8 +9,6 @@ if (($# >= 9)); then
   exit 1
 fi
 
-echo "$1"
-
 # 0 for custom section
 # 213 in hex = 531 in decimal, total lenght of what follows (1 + 16 + 2 + 8x32 + 256)
 # [1(continuation) + 0010011(payload) = \x93, 0(continuation) + 10(payload) = \x04]
@@ -37,7 +35,6 @@ done
 # Write provided fiedls (backwards)
 for ((i=$#; i>=2; i--))
 do
-  echo "${!i}"
   cat "${!i}" > "$1.add"
   cat "$1.empty_32" >> "$1.add"
   dd if="$1.add" of="$1.add_trunc" bs=32 count=1 &> /dev/null
@@ -45,7 +42,6 @@ do
 done
 
 # Write how many fields have been written during previous step
-  echo "$#"
   echo -n "$#" > "$1.add"
   cat "$1.empty_32" >> "$1.add"
   dd if="$1.add" of="$1.add_trunc" bs=32 count=1 &> /dev/null
@@ -54,4 +50,3 @@ done
 cat "$1.empty_256" >> "$1"
 
 rm -f "$1.*"
-echo "$1"
