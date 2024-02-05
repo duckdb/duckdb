2c5c2cd3ef1793370bbf864bc013a83b2a0f0f78
Pedro Holanda <pedroholanda@gmail.com>
2024-02-05 16:38:08 +0100

    null padding test change

--- src/execution/operator/csv_scanner/table_function/csv_file_scanner.cpp
+++ src/execution/operator/csv_scanner/table_function/csv_file_scanner.cpp
@@ -152,11 +152,6 @@ CSVFileScan::CSVFileScan(ClientContext &context, const string &file_name, CSVRea
 }
 
 void CSVFileScan::InitializeFileNamesTypes() {
-	if (options.null_padding) {
-		// If we are null padding we do not yet support projection pushdown
-		file_types = types;
-		return;
-	}
 	if (reader_data.empty_columns && reader_data.column_ids.empty()) {
 		// This means that the columns from this file are irrelevant.
 		// just read the first column
@@ -171,6 +166,9 @@ void CSVFileScan::InitializeFileNamesTypes() {
 		file_types.emplace_back(types[result_idx]);
 		projected_columns.insert(result_idx);
 		projection_ids.emplace_back(result_idx, i);
+		if (options.null_padding){
+			return;
+		}
 	}
 
 	if (!projected_columns.empty()) {
