2a15e316a5bf228ef2f6eed753932c4618b11394
Tom Ebergen <tom@ebergen.com>
2024-02-09 15:50:33 +0100

    dont forget to run the python script

---
+++ test/parquet/cardinality_estimates/create_mismatch_parquet_sizes.py
@@ -0,0 +1,42 @@
+
+import duckdb
+
+
+con = duckdb.connect('delete_me.duckdb')
+
+con.sql(f"CREATE table all_data as select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range(0,0)");
+
+row_num = 0
+for i in range(4):
+    extra_rows = 0
+    con.sql(f"CREATE OR REPLACE table table_{i} as select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range({row_num}, {row_num+extra_rows})");
+    con.sql(f"INSERT INTO all_data (select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range({row_num}, {row_num+extra_rows}))");
+    con.sql(f"copy table_{i} to 'table_{i}.parquet' (FORMAT PARQUET)")
+    con.sql(f"DROP TABLE table_{i}")
+    row_num += extra_rows
+
+for i in range(4,10):
+    extra_rows = 1000000
+    if i in [5, 6, 7, 8, 9]:
+        extra_rows = 100000
+    con.sql(f"CREATE OR REPLACE table table_{i} as select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range({row_num}, {row_num+extra_rows})");
+    con.sql(f"INSERT INTO all_data (select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range({row_num}, {row_num+extra_rows}))");
+    con.sql(f"copy table_{i} to 'table_{i}.parquet' (FORMAT PARQUET)")
+    con.sql(f"DROP TABLE table_{i}")
+    row_num += extra_rows
+
+for i in range(10,24):
+    extra_rows = 0
+    con.sql(f"CREATE OR REPLACE table table_{i} as select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range({row_num}, {row_num+extra_rows})");
+    con.sql(f"INSERT INTO all_data (select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range({row_num}, {row_num+extra_rows}))");
+    con.sql(f"copy table_{i} to 'table_{i}.parquet' (FORMAT PARQUET)")
+    con.sql(f"DROP TABLE table_{i}")
+    row_num += extra_rows
+
+
+for i in range(10,14):
+    extra_rows = 1000000
+    con.sql(f"INSERT INTO all_data (select range a, range*2 b, range::VARCHAR c, range::VARCHAR || \'sdkfjssdfs\' d from range({row_num}, {row_num+extra_rows}))");
+    row_num += extra_rows
+
+con.sql("COPY all_data to 'all_data.parquet' (FORMAT PARQUET)")
