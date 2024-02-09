5de2cc44070a2bb4fa556cf13009072a5660b40f
Tishj <t_b@live.nl>
2024-02-09 23:50:52 +0100

    add test making sure we don't crash

---
+++ tools/pythonpkg/tests/fast/arrow/test_9443.py
@@ -0,0 +1,30 @@
+import duckdb
+import pytest
+
+pq = pytest.importorskip("pyarrow.parquet")
+pa = pytest.importorskip("pyarrow")
+
+from datetime import time
+from tempfile import NamedTemporaryFile
+
+
+class Test9443(object):
+    def test_9443(self, tmp_path, duckdb_cursor):
+        arrow_table = pa.Table.from_pylist(
+            [
+                {"col1": time(1, 2, 3)},
+            ]
+        )  # col1: time64[us]
+
+        print(arrow_table)
+
+        temp_file = tmp_path / "temp_file.parquet"
+        # Perform operations using temp_file
+        with temp_file.open("wb") as file:
+            pq.write_table(arrow_table, temp_file)
+
+            sql = f'SELECT * FROM "{temp_file}"'
+
+            rel = duckdb_cursor.query(sql)
+            with pytest.raises(duckdb.NotImplementedException, match='Unsupported Arrow type TIME WITH TIME ZONE'):
+                res = rel.arrow()
