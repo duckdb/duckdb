fbcef69a6ca53c5fc4ab8dcbba08e60133c71972
Tishj <t_b@live.nl>
2024-02-10 11:52:15 +0100

    fix failing test

--- tools/pythonpkg/tests/fast/arrow/test_9443.py
+++ tools/pythonpkg/tests/fast/arrow/test_9443.py
@@ -5,7 +5,7 @@ pq = pytest.importorskip("pyarrow.parquet")
 pa = pytest.importorskip("pyarrow")
 
 from datetime import time
-from tempfile import NamedTemporaryFile
+from pathlib import PurePosixPath
 
 
 class Test9443(object):
@@ -18,13 +18,11 @@ class Test9443(object):
 
         print(arrow_table)
 
-        temp_file = tmp_path / "test9443.parquet"
-        # Perform operations using temp_file
-        with temp_file.open("wb") as file:
-            pq.write_table(arrow_table, temp_file)
+        temp_file = str(PurePosixPath(tmp_path.as_posix()) / "test9443.parquet")
+        pq.write_table(arrow_table, temp_file)
 
-            sql = f'SELECT * FROM "{temp_file}"'
+        sql = f'SELECT * FROM "{temp_file}"'
 
-            duckdb_cursor.execute(sql)
-            with pytest.raises(Exception, match='Unsupported Arrow type TIME WITH TIME ZONE'):
-                duckdb_cursor.fetch_record_batch()
+        duckdb_cursor.execute(sql)
+        with pytest.raises(Exception, match='Unsupported Arrow type TIME WITH TIME ZONE'):
+            duckdb_cursor.fetch_record_batch()
