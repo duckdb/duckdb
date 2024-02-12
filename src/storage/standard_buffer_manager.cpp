54e652e619ce3f34c16c2bdcdb6bb1d1976aefa8
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-02-12 18:05:02 +0100

    Zero-initialize array of atomics

--- src/storage/standard_buffer_manager.cpp
+++ src/storage/standard_buffer_manager.cpp
@@ -62,7 +62,7 @@ StandardBufferManager::StandardBufferManager(DatabaseInstance &db, string tmp)
       temporary_id(MAXIMUM_BLOCK), buffer_allocator(BufferAllocatorAllocate, BufferAllocatorFree,
                                                     BufferAllocatorRealloc, make_uniq<BufferAllocatorData>(*this)) {
 	temp_block_manager = make_uniq<InMemoryBlockManager>(*this);
-	for(idx_t i = 0; i < MEMORY_TAG_COUNT; i++) {
+	for (idx_t i = 0; i < MEMORY_TAG_COUNT; i++) {
 		evicted_data_per_tag[i] = 0;
 	}
 }
