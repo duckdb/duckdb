872c19f2c03d772daf107575be5d52a04fe8f0bd
Tom Ebergen <tom@ebergen.com>
2024-02-14 16:31:10 +0100

    optimizer works. lets see if if regresses

--- src/optimizer/build_probe_side_optimizer.cpp
+++ src/optimizer/build_probe_side_optimizer.cpp
@@ -28,17 +28,65 @@ static void FlipChildren(LogicalOperator &op) {
 	}
 }
 
-void BuildProbeSideOptimizer::TryFlipChildren(LogicalOperator &op, idx_t cardinality_ratio) {
+BuildSize BuildProbeSideOptimizer::GetBuildSide(LogicalOperator &op) {
+	BuildSize ret;
+	switch (op.type) {
+	case LogicalOperatorType::LOGICAL_COMPARISON_JOIN:
+	case LogicalOperatorType::LOGICAL_CROSS_PRODUCT:
+	case LogicalOperatorType::LOGICAL_ANY_JOIN:
+	case LogicalOperatorType::LOGICAL_DELIM_JOIN: {
+		auto &left_child = op.children[0];
+		auto &right_child = op.children[1];
+
+		auto left_column_count = left_child->GetColumnBindings();
+		auto right_column_count = right_child->GetColumnBindings();
+
+		// resolve operator types to determine how big the build side is going to be
+		op.ResolveOperatorTypes();
+		auto left_column_types = left_child->types;
+		auto right_column_types = right_child->types;
+
+		idx_t left_build_side = 0;
+		for (auto &type : left_column_types) {
+			left_build_side += GetTypeIdSize(type.InternalType());
+		}
+		idx_t right_build_side = 0;
+		for (auto &type : right_column_types) {
+			right_build_side += GetTypeIdSize(type.InternalType());
+		}
+		// Don't multiply by cardinalities, the only important metric is the size of the row
+		// in the hash table
+		ret.left_side = left_build_side;
+		ret.right_side = right_build_side;
+		return ret;
+	}
+	default:
+		break;
+	}
+	return ret;
+}
+
+void BuildProbeSideOptimizer::TryFlipJoinChildren(LogicalOperator &op, idx_t cardinality_ratio) {
 	auto &left_child = op.children[0];
 	auto &right_child = op.children[1];
 	auto lhs_cardinality = left_child->has_estimated_cardinality ? left_child->estimated_cardinality
 	                                                             : left_child->EstimateCardinality(context);
 	auto rhs_cardinality = right_child->has_estimated_cardinality ? right_child->estimated_cardinality
 	                                                              : right_child->EstimateCardinality(context);
-	if (rhs_cardinality < lhs_cardinality * cardinality_ratio) {
-		return;
+
+	auto build_sizes = GetBuildSide(op);
+
+	auto flip_bc_cardinality = rhs_cardinality > (lhs_cardinality * cardinality_ratio);
+
+	// special math.
+	auto left_side_metric = lhs_cardinality * cardinality_ratio * build_sizes.left_side;
+	auto right_side_metric = rhs_cardinality * build_sizes.right_side;
+	// if the right side has a larger possible build size, build on the left.
+	if (right_side_metric > left_side_metric) {
+		FlipChildren(op);
 	}
-	FlipChildren(op);
+	// last option is both stats say don't flip. So we return.
+	return;
 }
 
 void BuildProbeSideOptimizer::VisitOperator(LogicalOperator &op) {
@@ -49,12 +97,12 @@ void BuildProbeSideOptimizer::VisitOperator(LogicalOperator &op) {
 		switch (join.join_type) {
 		case JoinType::INNER:
 		case JoinType::OUTER:
-			TryFlipChildren(join);
+			TryFlipJoinChildren(join);
 			break;
 		case JoinType::LEFT:
 		case JoinType::RIGHT:
 			if (join.right_projection_map.empty()) {
-				TryFlipChildren(join, 2);
+				TryFlipJoinChildren(join, 2);
 			}
 			break;
 		case JoinType::SEMI:
@@ -65,7 +113,7 @@ void BuildProbeSideOptimizer::VisitOperator(LogicalOperator &op) {
 				// There is no physical join operator (yet) that can do a right_semi/anti join.
 				break;
 			}
-			TryFlipChildren(join, 2);
+			TryFlipJoinChildren(join, 2);
 			break;
 		}
 		default:
@@ -74,16 +122,15 @@ void BuildProbeSideOptimizer::VisitOperator(LogicalOperator &op) {
 		break;
 	}
 	case LogicalOperatorType::LOGICAL_CROSS_PRODUCT: {
-		// cross product not a comparison join so JoinType::INNER will get ignored
-		TryFlipChildren(op, 1);
+		TryFlipJoinChildren(op, 1);
 		break;
 	}
 	case LogicalOperatorType::LOGICAL_ANY_JOIN: {
 		auto &join = op.Cast<LogicalAnyJoin>();
 		if (join.join_type == JoinType::LEFT && join.right_projection_map.empty()) {
-			TryFlipChildren(join, 2);
+			TryFlipJoinChildren(join, 2);
 		} else if (join.join_type == JoinType::INNER) {
-			TryFlipChildren(join, 1);
+			TryFlipJoinChildren(join, 1);
 		}
 		break;
 	}
