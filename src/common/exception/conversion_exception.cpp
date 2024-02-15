ca1cfac212d5725b0def7a71b9bcfc112544f106
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-02-15 17:17:54 +0100

    Format

--- src/common/exception/conversion_exception.cpp
+++ src/common/exception/conversion_exception.cpp
@@ -3,19 +3,20 @@
 namespace duckdb {
 
 ConversionException::ConversionException(const PhysicalType orig_type, const PhysicalType new_type)
-        : Exception(ExceptionType::CONVERSION,
-                    "Type " + TypeIdToString(orig_type) + " can't be cast as " + TypeIdToString(new_type)) {
+    : Exception(ExceptionType::CONVERSION,
+                "Type " + TypeIdToString(orig_type) + " can't be cast as " + TypeIdToString(new_type)) {
 }
 
 ConversionException::ConversionException(const LogicalType &orig_type, const LogicalType &new_type)
-        : Exception(ExceptionType::CONVERSION,
-                    "Type " + orig_type.ToString() + " can't be cast as " + new_type.ToString()) {
+    : Exception(ExceptionType::CONVERSION,
+                "Type " + orig_type.ToString() + " can't be cast as " + new_type.ToString()) {
 }
 
 ConversionException::ConversionException(const string &msg) : Exception(ExceptionType::CONVERSION, msg) {
 }
 
-ConversionException::ConversionException(optional_idx error_location, const string &msg) :
-        Exception(ExceptionType::CONVERSION, msg, Exception::InitializeExtraInfo(error_location)) {}
-
+ConversionException::ConversionException(optional_idx error_location, const string &msg)
+    : Exception(ExceptionType::CONVERSION, msg, Exception::InitializeExtraInfo(error_location)) {
 }
+
+} // namespace duckdb
