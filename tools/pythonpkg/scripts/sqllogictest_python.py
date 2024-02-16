70edee94277939a02a6f3b0a5e3b9bc920d4a6fa
Tishj <t_b@live.nl>
2024-02-16 21:41:32 +0100

    start of python interpreter

--- tools/pythonpkg/scripts/sqllogictest_python.py
+++ tools/pythonpkg/scripts/sqllogictest_python.py
@@ -3,6 +3,7 @@ import os
 import glob
 import json
 from typing import Optional, List
+import duckdb
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(script_path, '..', '..', '..', 'scripts'))
@@ -28,6 +29,7 @@ from sqllogictest import (
     Sleep,
     Skip,
     Unskip,
+    ExpectedResult,
 )
 
 from enum import Enum, auto
@@ -47,10 +49,19 @@ class SQLLogicTestExecutor:
     def reset(self):
         self.skipped = False
         self.error: Optional[str] = None
+        self.conn: Optional[duckdb.DuckDBPyConnection] = None
+        self.cursors = {}
 
     def __init__(self):
         self.reset()
-        self.STATEMENTS = {Statement: self.execute_statement, RequireEnv: self.execute_require_env}
+        self.STATEMENTS = {
+            Statement: self.execute_statement,
+            RequireEnv: self.execute_require_env,
+            Query: self.execute_query,
+        }
+
+    def fail(self, message):
+        raise Exception(message)
 
     def get_unsupported_statements(self, test: SQLLogicTest) -> List[BaseStatement]:
         unsupported_statements = [
@@ -58,8 +69,65 @@ class SQLLogicTestExecutor:
         ]
         return unsupported_statements
 
-    def execute_statement(self, statement: BaseStatement):
-        print('yes')
+    def get_connection(self, name: Optional[str] = None) -> duckdb.DuckDBPyConnection:
+        if not self.conn:
+            self.conn = duckdb.connect(':memory:__named__')
+        if not name:
+            return self.conn
+        if name not in self.cursors:
+            self.cursors[name] = self.conn.cursor()
+        return self.cursors[name]
+
+    def execute_query(self, query: Query):
+        assert isinstance(query, Query)
+        conn = self.get_connection(query.connection_name)
+        sql_query = '\n'.join(query.lines)
+
+        expected_result = query.expected_result
+        assert expected_result.type == ExpectedResult.Type.SUCCES
+        try:
+            conn.execute(sql_query)
+            result = conn.fetchall()
+            if expected_result.lines == None:
+                return
+            actual = []
+            for row in result:
+                converted_row = tuple([str(x) for x in list(row)])
+                actual.append(converted_row)
+            actual = str(actual)
+
+            expected = []
+            for line in expected_result.lines:
+                tuples = tuple(line.split('\t'))
+                expected.append(tuples)
+            expected = str(expected)
+            if expected != actual:
+                self.fail(f"Query result mismatch!")
+        except duckdb.Error as e:
+            self.fail(f"Query unexpectedly failed: {str(e)}")
+
+    def execute_statement(self, statement: Statement):
+        assert isinstance(statement, Statement)
+        conn = self.get_connection(statement.connection_name)
+        query = '\n'.join(statement.lines)
+
+        expected_result = statement.expected_result
+        try:
+            conn.execute(query)
+            result = conn.fetchall()
+            if expected_result.type == ExpectedResult.Type.ERROR:
+                self.fail(f"Query unexpectedly succeeded")
+            assert expected_result.lines == None
+        except duckdb.Error as e:
+            if expected_result.type == ExpectedResult.Type.SUCCES:
+                self.fail(f"Query unexpectedly failed: {str(e)}")
+            if expected_result.lines == None:
+                return
+            expected = '\n'.join(expected_result.lines)
+            if expected not in str(e):
+                self.fail(
+                    f"Query failed, but did not produce the right error: {expected}\nInstead it produced: {str(e)}"
+                )
 
     def execute_require_env(self, statement: BaseStatement):
         self.skipped = True
@@ -83,20 +151,31 @@ class SQLLogicTestExecutor:
         return ExecuteResult(ExecuteResult.Type.SUCCES)
 
 
+import argparse
+
+
 def main():
-    parser = SQLLogicParser()
+    sql_parser = SQLLogicParser()
     executor = SQLLogicTestExecutor()
-    test_directory = os.path.join(script_path, '..', '..', '..', 'test')
-    for filename in glob.iglob(test_directory + '/**/*.test', recursive=True):
-        print(filename)
-        test = parser.parse(filename)
+
+    arg_parser = argparse.ArgumentParser(description='Execute SQL logic tests.')
+    arg_parser.add_argument('--file-path', '-f', type=str, help='Path to the test file')
+    args = arg_parser.parse_args()
+
+    if args.file_path:
+        file_paths = [args.file_path]
+    else:
+        test_directory = os.path.join(script_path, '..', '..', '..', 'test')
+        file_paths = glob.iglob(test_directory + '/**/*.test', recursive=True)
+
+    for file_path in file_paths:
+        print(file_path)
+        test = sql_parser.parse(file_path)
 
         result = executor.execute_test(test)
         if result.type == ExecuteResult.Type.SKIPPED:
             continue
         exit()
-    print('test')
-    pass
 
 
 if __name__ == '__main__':
