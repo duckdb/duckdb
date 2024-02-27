a09a3083d6bb19ac5f16fd49ec7d49eee99fa5ba
Hannes Mühleisen <hannes@duckdblabs.com>
2024-02-27 14:08:11 +0100

    we meet again make_date

--- src/core_functions/scalar/date/make_date.cpp
+++ src/core_functions/scalar/date/make_date.cpp
@@ -30,6 +30,16 @@ static void ExecuteMakeDate(DataChunk &input, ExpressionState &state, Vector &re
 }
 
 template <typename T>
+static date_t FromDateCast(T year, T month, T day) {
+	date_t result;
+	if (!Date::TryFromDate(UnsafeNumericCast<int32_t>(year), UnsafeNumericCast<int32_t>(month),
+	                       UnsafeNumericCast<int32_t>(day), result)) {
+		throw ConversionException("Date out of range: %d-%d-%d", year, month, day);
+	}
+	return result;
+}
+
+template <typename T>
 static void ExecuteStructMakeDate(DataChunk &input, ExpressionState &state, Vector &result) {
 	// this should be guaranteed by the binder
 	D_ASSERT(input.ColumnCount() == 1);
@@ -41,20 +51,22 @@ static void ExecuteStructMakeDate(DataChunk &input, ExpressionState &state, Vect
 	auto &mm = *children[1];
 	auto &dd = *children[2];
 
-	TernaryExecutor::Execute<T, T, T, date_t>(yyyy, mm, dd, result, input.size(), Date::FromDate);
+	TernaryExecutor::Execute<T, T, T, date_t>(yyyy, mm, dd, result, input.size(), FromDateCast<T>);
 }
 
 struct MakeTimeOperator {
 	template <typename HH, typename MM, typename SS, typename RESULT_TYPE>
 	static RESULT_TYPE Operation(HH hh, MM mm, SS ss) {
-		int64_t secs = ss;
-		int64_t micros = std::round((ss - secs) * Interval::MICROS_PER_SEC);
-		if (!Time::IsValidTime(UnsafeNumericCast<int32_t>(hh), UnsafeNumericCast<int32_t>(mm),
-		                       UnsafeNumericCast<int32_t>(secs), UnsafeNumericCast<int32_t>(micros))) {
-			throw ConversionException("Time out of range: %d:%d:%d.%d", hh, mm, secs, micros);
+
+		auto hh_32 = UnsafeNumericCast<int32_t>(hh);
+		auto mm_32 = UnsafeNumericCast<int32_t>(mm);
+		auto ss_32 = UnsafeNumericCast<int32_t>(ss);
+		auto micros = UnsafeNumericCast<int32_t>(std::round((ss - ss_32) * Interval::MICROS_PER_SEC));
+
+		if (!Time::IsValidTime(hh_32, mm_32, ss_32, micros)) {
+			throw ConversionException("Time out of range: %d:%d:%d.%d", hh_32, mm_32, ss_32, micros);
 		}
-		return Time::FromTime(UnsafeNumericCast<int32_t>(hh), UnsafeNumericCast<int32_t>(mm),
-		                      UnsafeNumericCast<int32_t>(secs), UnsafeNumericCast<int32_t>(micros));
+		return Time::FromTime(hh_32, mm_32, ss_32, micros);
 	}
 };
 
@@ -105,7 +117,7 @@ ScalarFunctionSet MakeDateFun::GetFunctions() {
 	child_list_t<LogicalType> make_date_children {
 	    {"year", LogicalType::BIGINT}, {"month", LogicalType::BIGINT}, {"day", LogicalType::BIGINT}};
 	make_date.AddFunction(
-	    ScalarFunction({LogicalType::STRUCT(make_date_children)}, LogicalType::DATE, ExecuteStructMakeDate<int32_t>));
+	    ScalarFunction({LogicalType::STRUCT(make_date_children)}, LogicalType::DATE, ExecuteStructMakeDate<int64_t>));
 	return make_date;
 }
 
