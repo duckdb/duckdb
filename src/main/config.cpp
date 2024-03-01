53ff92bb675c91195ea216f30a594b90bbf61a7e
Tom Ebergen <tom@ebergen.com>
2024-03-01 17:58:01 +0100

    when you add the relation, make sure you call gettableIndexes() on the right relation object

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -15,7 +15,7 @@
 namespace duckdb {
 
 #ifdef DEBUG
-bool DBConfigOptions::debug_print_bindings = false;
+bool DBConfigOptions::debug_print_bindings = true;
 #endif
 
 #define DUCKDB_GLOBAL(_PARAM)                                                                                          \
