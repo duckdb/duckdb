bc454bde67a71e95373c53269e7344b594b8c66f
Alexander Phi. Goetz <alexander.goetz@student.uni-tuebingen.de>
2024-03-04 18:52:22 +0100

    Update JSON array append function

--- extension/json/json_functions/json_array_append.cpp
+++ extension/json/json_functions/json_array_append.cpp
@@ -2,70 +2,90 @@
 
 namespace duckdb {
 
+//! Append String or JSON value to an array
+yyjson_mut_val *ArrayAppendStringOrJSON(yyjson_mut_val *arr, yyjson_mut_doc *doc, string_t element, yyjson_alc *alc,
+                                        Vector &result) {
+	if (!yyjson_mut_is_arr(arr)) {
+		throw InvalidInputException("JSON input not an JSON Array");
+	}
+
+	auto edoc = JSONCommon::ReadDocument(element, JSONCommon::READ_FLAG, alc);
+	auto mut_edoc = yyjson_doc_mut_copy(edoc, alc);
+
+	yyjson_mut_arr_append(arr, mut_edoc->root);
+	return arr;
+}
+
+//! Append boolean value to an array
+yyjson_mut_val *ArrayAppendBoolean(yyjson_mut_val *arr, yyjson_mut_doc *doc, bool element, yyjson_alc *alc,
+                                   Vector &result) {
+	if (!yyjson_mut_is_arr(arr)) {
+		throw InvalidInputException("JSON input not a JSON Array");
+	}
+
+	yyjson_mut_arr_add_bool(doc, arr, element);
+	return arr;
+}
+
+//! Append unsigned Integers to an array
+yyjson_mut_val *ArrayAppendUnsignedIntegers(yyjson_mut_val *arr, yyjson_mut_doc *doc, uint64_t element, yyjson_alc *alc,
+                                            Vector &result) {
+	if (!yyjson_mut_is_arr(arr)) {
+		throw InvalidInputException("JSON input not a JSON Array");
+	}
+
+	yyjson_mut_arr_add_uint(doc, arr, element);
+	return arr;
+}
+
+//! Append signed Integers to an array
+yyjson_mut_val *ArrayAppendSignedIntegers(yyjson_mut_val *arr, yyjson_mut_doc *doc, int64_t element, yyjson_alc *alc,
+                                          Vector &result) {
+	if (!yyjson_mut_is_arr(arr)) {
+		throw InvalidInputException("JSON input not a JSON Array");
+	}
+
+	yyjson_mut_arr_add_int(doc, arr, element);
+	return arr;
+}
+
+//! Append floating values to an array
+yyjson_mut_val *ArrayAppendFloating(yyjson_mut_val *arr, yyjson_mut_doc *doc, double element, yyjson_alc *alc,
+                                    Vector &result) {
+	if (!yyjson_mut_is_arr(arr)) {
+		throw InvalidInputException("JSON input not a JSON Array");
+	}
+
+	yyjson_mut_arr_add_real(doc, arr, element);
+	return arr;
+}
+
+//! Append function wrapper
 static void ArrayAppendFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto left_type = args.data[0].GetType();
 	D_ASSERT(left_type == LogicalType::VARCHAR || left_type == JSONCommon::JSONType());
 
 	auto right_type = args.data[1].GetType();
 
-	// String or JSON value
-	if (right_type == LogicalType::VARCHAR || right_type == JSONCommon::JSONType()) {
-		JSONExecutors::BinaryMutExecute<string_t>(
-		    args, state, result,
-		    [](yyjson_mut_val *arr, yyjson_mut_doc *doc, string_t element, yyjson_alc *alc, Vector &result) {
-			    if (!yyjson_mut_is_arr(arr)) {
-				    throw InvalidInputException("JSON input not an JSON Array");
-			    }
-
-			    auto edoc = JSONCommon::ReadDocument(element, JSONCommon::READ_FLAG, alc);
-			    auto mut_edoc = yyjson_doc_mut_copy(edoc, alc);
-
-			    yyjson_mut_arr_append(arr, mut_edoc->root);
-
-			    return arr;
-		    });
-		// Boolean
-	} else if (right_type == LogicalType::BOOLEAN) {
-		JSONExecutors::BinaryMutExecute<bool>(
-		    args, state, result,
-		    [&](yyjson_mut_val *arr, yyjson_mut_doc *doc, bool element, yyjson_alc *alc, Vector &result) {
-			    if (!yyjson_mut_is_arr(arr)) {
-				    throw InvalidInputException("JSON input not a JSON Array");
-			    }
-
-			    yyjson_mut_arr_add_bool(doc, arr, element);
-			    return arr;
-		    });
-		// Integer value
-	} else if (right_type == LogicalType::BIGINT || right_type == LogicalType::UTINYINT ||
-	           right_type == LogicalType::USMALLINT || right_type == LogicalType::UINTEGER ||
-	           right_type == LogicalType::UBIGINT) {
-		JSONExecutors::BinaryMutExecute<int64_t>(
-		    args, state, result,
-		    [&](yyjson_mut_val *arr, yyjson_mut_doc *doc, int64_t el, yyjson_alc *alc, Vector &result) {
-			    if (!yyjson_mut_is_arr(arr)) {
-				    throw InvalidInputException("JSON input not a JSON Array");
-			    }
-
-			    int64_t element = static_cast<int64_t>(el);
-
-			    yyjson_mut_arr_add_int(doc, arr, element);
-			    return arr;
-		    });
-		// Floating value
-	} else if (right_type == LogicalType::FLOAT || right_type == LogicalType::DOUBLE) {
-		JSONExecutors::BinaryMutExecute<double>(
-		    args, state, result,
-		    [&](yyjson_mut_val *arr, yyjson_mut_doc *doc, double el, yyjson_alc *alc, Vector &result) {
-			    if (!yyjson_mut_is_arr(arr)) {
-				    throw InvalidInputException("JSON input not a JSON Array");
-			    }
-
-			    double element = el;
-
-			    yyjson_mut_arr_add_real(doc, arr, element);
-			    return arr;
-		    });
+	switch (right_type.id()) {
+	case LogicalType::VARCHAR:
+		JSONExecutors::BinaryMutExecute<string_t>(args, state, result, ArrayAppendStringOrJSON);
+		break;
+	case LogicalType::BOOLEAN:
+		JSONExecutors::BinaryMutExecute<bool>(args, state, result, ArrayAppendBoolean);
+		break;
+	case LogicalType::UBIGINT:
+		JSONExecutors::BinaryMutExecute<uint64_t>(args, state, result, ArrayAppendUnsignedIntegers);
+		break;
+	case LogicalType::BIGINT:
+		JSONExecutors::BinaryMutExecute<int64_t>(args, state, result, ArrayAppendSignedIntegers);
+		break;
+	case LogicalType::DOUBLE:
+		JSONExecutors::BinaryMutExecute<double>(args, state, result, ArrayAppendFloating);
+		break;
+	default:
+		// Shouldn't be thrown except implicit casting changes
+		throw InvalidInputException("Not a valid input type");
 	}
 }
 
@@ -79,25 +99,23 @@ ScalarFunctionSet JSONFunctions::GetArrayAppendFunction() {
 
 	// Use different executor for these
 	// Allows booleans directly
-	GetArrayAppendFunctionInternal(set, LogicalType::VARCHAR, LogicalType::BOOLEAN);
 	GetArrayAppendFunctionInternal(set, JSONCommon::JSONType(), LogicalType::BOOLEAN);
 
 	// Allows for Integer types
-	// TINYINT, SMALLINT, INTEGER, UTINYINT, USMALLINT, UINTEGER are captured by BIGINT
+	// TINYINT, SMALLINT, INTEGER, UTINYINT, USMALLINT, UINTEGER are captured by UBIGINT and BIGINT	respecively
 	// relies on consistant casting strategy upfront
-	GetArrayAppendFunctionInternal(set, LogicalType::VARCHAR, LogicalType::BIGINT);
-	GetArrayAppendFunctionInternal(set, JSONCommon::JSONType(), LogicalType::BIGINT);
-	GetArrayAppendFunctionInternal(set, LogicalType::VARCHAR, LogicalType::UBIGINT);
+
+	// unsigned
 	GetArrayAppendFunctionInternal(set, JSONCommon::JSONType(), LogicalType::UBIGINT);
+
+	// signed
+	GetArrayAppendFunctionInternal(set, JSONCommon::JSONType(), LogicalType::BIGINT);
 
 	// Allows for floating types
 	// FLOAT is covered by automatic upfront casting to double
-	GetArrayAppendFunctionInternal(set, LogicalType::VARCHAR, LogicalType::DOUBLE);
 	GetArrayAppendFunctionInternal(set, JSONCommon::JSONType(), LogicalType::DOUBLE);
 
 	// Allows for json and string values
-	GetArrayAppendFunctionInternal(set, LogicalType::VARCHAR, LogicalType::VARCHAR);
-	GetArrayAppendFunctionInternal(set, LogicalType::VARCHAR, JSONCommon::JSONType());
 	GetArrayAppendFunctionInternal(set, JSONCommon::JSONType(), JSONCommon::JSONType());
 	GetArrayAppendFunctionInternal(set, JSONCommon::JSONType(), LogicalType::VARCHAR);
 
