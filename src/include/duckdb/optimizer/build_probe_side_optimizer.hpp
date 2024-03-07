1024ae7a89b91fb15ce6667aba27249b7e8272b1
Tmonster <tom@ebergen.com>
2024-03-07 16:56:29 +0100

    sort of a fix, but is not yet robust

--- src/include/duckdb/optimizer/build_probe_side_optimizer.hpp
+++ src/include/duckdb/optimizer/build_probe_side_optimizer.hpp
@@ -15,29 +15,18 @@
 
 namespace duckdb {
 
-struct BuildSize {
-	idx_t left_side;
-	idx_t right_side;
-
-	// Initialize with 1 so the build side is just the cardinality if types aren't
-	// known.
-	BuildSize() : left_side(1), right_side(1) {
-	}
-};
-
 class BuildProbeSideOptimizer : LogicalOperatorVisitor {
 public:
-	explicit BuildProbeSideOptimizer(ClientContext &context);
+	explicit BuildProbeSideOptimizer(ClientContext &context, vector<ColumnBinding> preferred_on_probe_side);
 
 	void VisitOperator(LogicalOperator &op) override;
 	void VisitExpression(unique_ptr<Expression> *expression) override {};
 
 	void TryFlipJoinChildren(LogicalOperator &op, idx_t cardinality_ratio = 1);
 
-	BuildSize GetBuildSide(LogicalOperator &op);
-
 private:
 	ClientContext &context;
+	vector<ColumnBinding> preferred_on_probe_side;
 };
 
 } // namespace duckdb
