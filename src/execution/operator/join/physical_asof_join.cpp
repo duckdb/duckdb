10a4757fc536e25f50114bf7ca13515137cb5588
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-03-12 10:28:23 +0100

    More fixes

--- src/execution/operator/join/physical_asof_join.cpp
+++ src/execution/operator/join/physical_asof_join.cpp
@@ -238,6 +238,7 @@ bool AsOfLocalState::Sink(DataChunk &input) {
 	//	Compute the join keys
 	lhs_keys.Reset();
 	lhs_executor.Execute(input, lhs_keys);
+	lhs_keys.Flatten();
 
 	//	Combine the NULLs
 	const auto count = input.size();
