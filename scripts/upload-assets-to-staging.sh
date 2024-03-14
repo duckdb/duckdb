eef07e5faf791a5fbd841529be77a083df2dc9f8
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-03-14 08:25:56 +0100

    upload-assets-to-staging: add more logging on why dryrun is added

--- scripts/upload-assets-to-staging.sh
+++ scripts/upload-assets-to-staging.sh
@@ -11,15 +11,18 @@ DRY_RUN_PARAM=""
 
 # dryrun if AWS key is not set
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "No access key available"
   DRY_RUN_PARAM="--dryrun"
 fi
 
 # dryrun if repo is not duckdb/duckdb
 if [ "$GITHUB_REPOSITORY" != "duckdb/duckdb" ]; then
+  echo "Repository is $GITHUB_REPOSITORY (not duckdb/duckdb)"
   DRY_RUN_PARAM="--dryrun"
 fi
 # dryrun if we are not in main
 if [ "$GITHUB_REF" != "refs/heads/main" ]; then
+  echo "git ref is $GITHUB_REF (not refs/heads/main)"
   DRY_RUN_PARAM="--dryrun"
 fi
 
