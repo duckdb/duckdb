b10e9f3b8f9b15d3bbf2d03ca059c99efd9c73aa
Max Gabrielsson <max@gabrielsson.com>
2024-03-14 18:45:24 +0100

    dont round down

--- src/common/types/row/tuple_data_collection.cpp
+++ src/common/types/row/tuple_data_collection.cpp
@@ -264,8 +264,9 @@ static inline void ToUnifiedFormatInternal(TupleDataVectorFormat &format, Vector
 		auto array_size = ArrayType::GetSize(vector.GetType());
 
 		// How many list_entry_t's do we need to cover the whole child array?
+		// Make sure we round up so its all covered
 		auto child_array_total_size = ArrayVector::GetTotalSize(vector);
-		auto list_entry_t_count = (child_array_total_size + array_size - 1) / array_size;
+		auto list_entry_t_count = (child_array_total_size + array_size) / array_size;
 
 		// Create list entries!
 		format.array_list_entries = make_uniq_array<list_entry_t>(list_entry_t_count);
