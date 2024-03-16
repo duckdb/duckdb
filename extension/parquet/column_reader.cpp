cf70e989c70e028449fcf8a8a4bb99d0219babca
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-03-16 10:29:24 +0100

    read metadata -> combine schema

--- extension/parquet/column_reader.cpp
+++ extension/parquet/column_reader.cpp
@@ -1013,7 +1013,7 @@ idx_t CastColumnReader::Read(uint64_t num_values, parquet_filter_t &filter, data
 		                       reader.file_name, schema.name, intermediate_vector.GetType(), result.GetType());
 		extended_error += "\nThis can happen when reading multiple Parquet files. The schema information is taken from "
 		                  "the first Parquet file by default. Possible solutions:\n";
-		extended_error += "* Enable the union_by_name=True option to read the metadata of all Parquet files "
+		extended_error += "* Enable the union_by_name=True option to combine the schema of all Parquet files "
 		                  "(duckdb.org/docs/data/multiple_files/combining_schemas)\n";
 		extended_error += "* Use a COPY statement to automatically derive types from an existing table.";
 		throw ConversionException(
