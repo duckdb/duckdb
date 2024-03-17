6d0039c1d04a02de233d1d83a1034d1b1849a8ec
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-03-17 11:39:29 +0100

    [Dev] catch only subprocess.CalledProcessError, with @Tishj

--- tools/pythonpkg/setup.py
+++ tools/pythonpkg/setup.py
@@ -375,7 +375,7 @@ def get_git_describe():
     if len(override_git_describe) == 0:
         try:
             return subprocess.check_output(['git', 'describe', '--tags', '--long']).strip().decode('utf8')
-        except:
+        except subprocess.CalledProcessError:
             return "v0.0.0-0-deadbeeff"
     if len(override_git_describe.split('-')) == 2:
         return override_git_describe
@@ -388,7 +388,7 @@ def get_git_describe():
             + "-"
             + subprocess.check_output(['git', 'log', '-1', '--format=%h']).strip().decode('utf8')
         )
-    except:
+    except subprocess.CalledProcessError:
         return override_git_describe + "-" + "deadbeeff"
 
 
