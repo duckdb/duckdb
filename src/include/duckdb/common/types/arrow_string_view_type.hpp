03c451990ffa3d13eca12d557fd3f0dd5e3ad991
Tishj <t_b@live.nl>
2024-03-18 14:02:21 +0100

    symbol name collisions

--- src/include/duckdb/common/types/arrow_string_view_type.hpp
+++ src/include/duckdb/common/types/arrow_string_view_type.hpp
@@ -12,13 +12,13 @@
 
 namespace duckdb {
 
-struct ArrowStringView {
+struct ArrowStringViewConstants {
 public:
 	static constexpr uint8_t MAX_INLINED_BYTES = 12 * sizeof(char);
 	static constexpr uint8_t PREFIX_BYTES = 4 * sizeof(char);
 
 public:
-	ArrowStringView() = delete;
+	ArrowStringViewConstants() = delete;
 };
 
 union arrow_string_view_t {
@@ -27,12 +27,12 @@ union arrow_string_view_t {
 
 	//! Constructor for inlined arrow string views
 	arrow_string_view_t(int32_t length, const char *data) {
-		D_ASSERT(length <= ArrowStringView::MAX_INLINED_BYTES);
+		D_ASSERT(length <= ArrowStringViewConstants::MAX_INLINED_BYTES);
 		inlined.length = length;
 		memcpy(inlined.data, data, length);
-		if (length < ArrowStringView::MAX_INLINED_BYTES) {
+		if (length < ArrowStringViewConstants::MAX_INLINED_BYTES) {
 			// have to 0 pad
-			uint8_t remaining_bytes = ArrowStringView::MAX_INLINED_BYTES - NumericCast<uint8_t>(length);
+			uint8_t remaining_bytes = ArrowStringViewConstants::MAX_INLINED_BYTES - NumericCast<uint8_t>(length);
 
 			memset(&inlined.data[length], '0', remaining_bytes);
 		}
@@ -40,9 +40,9 @@ union arrow_string_view_t {
 
 	//! Constructor for non-inlined arrow string views
 	arrow_string_view_t(int32_t length, const char *data, int32_t buffer_idx, int32_t offset) {
-		D_ASSERT(length > ArrowStringView::MAX_INLINED_BYTES);
+		D_ASSERT(length > ArrowStringViewConstants::MAX_INLINED_BYTES);
 		ref.length = length;
-		memcpy(ref.prefix, data, ArrowStringView::PREFIX_BYTES);
+		memcpy(ref.prefix, data, ArrowStringViewConstants::PREFIX_BYTES);
 		ref.buffer_index = buffer_idx;
 		ref.offset = offset;
 	}
@@ -50,13 +50,13 @@ union arrow_string_view_t {
 	//! Representation of inlined arrow string views
 	struct {
 		int32_t length;
-		char data[ArrowStringView::MAX_INLINED_BYTES];
+		char data[ArrowStringViewConstants::MAX_INLINED_BYTES];
 	} inlined;
 
 	//! Representation of non-inlined arrow string views
 	struct {
 		int32_t length;
-		char prefix[ArrowStringView::MAX_INLINED_BYTES];
+		char prefix[ArrowStringViewConstants::MAX_INLINED_BYTES];
 		int32_t buffer_index, offset;
 	} ref;
 
@@ -64,7 +64,7 @@ union arrow_string_view_t {
 		return inlined.length;
 	}
 	bool IsInline() const {
-		return Length() <= ArrowStringView::MAX_INLINED_BYTES;
+		return Length() <= ArrowStringViewConstants::MAX_INLINED_BYTES;
 	}
 
 	const char *GetInlineData() const {
