dab48b45946efd988ef433d0c1c23cebf8add84d
Denis Hirn <denis.hirn@uni-tuebingen.de>
2024-03-21 13:48:30 +0100

    Add TopN optimization in physical plan mapping
    Fixes #11260

--- src/execution/physical_plan/plan_limit.cpp
+++ src/execution/physical_plan/plan_limit.cpp
@@ -1,9 +1,11 @@
 #include "duckdb/execution/operator/helper/physical_limit.hpp"
-#include "duckdb/execution/operator/helper/physical_streaming_limit.hpp"
 #include "duckdb/execution/operator/helper/physical_limit_percent.hpp"
+#include "duckdb/execution/operator/helper/physical_streaming_limit.hpp"
+#include "duckdb/execution/operator/order/physical_top_n.hpp"
 #include "duckdb/execution/physical_plan_generator.hpp"
 #include "duckdb/main/config.hpp"
 #include "duckdb/planner/operator/logical_limit.hpp"
+#include "duckdb/planner/operator/logical_order.hpp"
 
 namespace duckdb {
 
@@ -33,6 +35,22 @@ unique_ptr<PhysicalOperator> PhysicalPlanGenerator::CreatePlan(LogicalLimit &op)
 	D_ASSERT(op.children.size() == 1);
 
 	auto plan = CreatePlan(*op.children[0]);
+
+	if (plan->type == PhysicalOperatorType::ORDER_BY && op.limit_val.Type() == LimitNodeType::CONSTANT_VALUE &&
+	    op.offset_val.Type() != LimitNodeType::EXPRESSION_VALUE) {
+		auto &order_by = plan->Cast<PhysicalOrder>();
+		// Can not use TopN operator if PhysicalOrder uses projections
+		if (order_by.projections.empty()) {
+			idx_t offset_val = 0;
+			if (op.offset_val.Type() == LimitNodeType::CONSTANT_VALUE) {
+				offset_val = op.offset_val.GetConstantValue();
+			}
+			auto top_n = make_uniq<PhysicalTopN>(op.types, std::move(order_by.orders), op.limit_val.GetConstantValue(),
+			                                     offset_val, op.estimated_cardinality);
+			top_n->children.push_back(std::move(order_by.children[0]));
+			return std::move(top_n);
+		}
+	}
 
 	unique_ptr<PhysicalOperator> limit;
 	switch (op.limit_val.Type()) {
