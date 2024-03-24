b18daffd3cd62dcc77b9a9844449296405248262
Tishj <t_b@live.nl>
2024-03-24 09:27:46 +0100

    all tests passing now

--- src/main/attached_database.cpp
+++ src/main/attached_database.cpp
@@ -126,6 +126,10 @@ Catalog &AttachedDatabase::ParentCatalog() {
 	return *parent_catalog;
 }
 
+const Catalog &AttachedDatabase::ParentCatalog() const {
+	return *parent_catalog;
+}
+
 bool AttachedDatabase::IsInitialDatabase() const {
 	return is_initial_database;
 }
