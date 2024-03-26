574e4026d0857490b16ee2a9a794e3eef43baa3e
Jeewon Heo <jeewon.heo@motherduck.com>
2024-03-26 16:27:51 +0100

    format fix

--- src/include/duckdb/optimizer/limit_pushdown.hpp
+++ src/include/duckdb/optimizer/limit_pushdown.hpp
@@ -11,15 +11,15 @@
 #include "duckdb/common/constants.hpp"
 
 namespace duckdb {
-    class LogicalOperator;
-    class Optimizer;
+class LogicalOperator;
+class Optimizer;
 
-    class LimitPushdown {
-    public:
-        //! Optimize PROJECTION + LIMIT to LIMIT + Projection
-        unique_ptr<LogicalOperator> Optimize(unique_ptr<LogicalOperator> op);
-        //! Whether we can perform the optimization on this operator
-        static bool CanOptimize(LogicalOperator &op);
-    };
+class LimitPushdown {
+public:
+	//! Optimize PROJECTION + LIMIT to LIMIT + Projection
+	unique_ptr<LogicalOperator> Optimize(unique_ptr<LogicalOperator> op);
+	//! Whether we can perform the optimization on this operator
+	static bool CanOptimize(LogicalOperator &op);
+};
 
 } // namespace duckdb
