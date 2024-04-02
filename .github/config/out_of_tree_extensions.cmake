01a4e7f6200785c5d9a9c58d0f5fa4202d1c32bb
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-04-02 18:43:42 +0200

    Bump to post-portfile change for duckdb_azure

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -38,7 +38,7 @@ if (NOT MINGW)
     duckdb_extension_load(azure
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb_azure
-            GIT_TAG 86f39d76157de970d16d6d6537bc90c0ee1c7d35
+            GIT_TAG 6620a32454c1eb2e455104d87262061d2464aad0
             APPLY_PATCHES
             )
 endif()
