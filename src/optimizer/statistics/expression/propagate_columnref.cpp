bea48309b5a5e4832e1e5d3a879382bc3b940f26
Laurens Kuiper <laurens.kuiper@cwi.nl>
2024-04-03 16:25:07 +0200

    refactor compressed materialization to be inside of statistics propagation

--- src/optimizer/statistics/expression/propagate_columnref.cpp
+++ src/optimizer/statistics/expression/propagate_columnref.cpp
@@ -4,7 +4,7 @@
 namespace duckdb {
 
 unique_ptr<BaseStatistics> StatisticsPropagator::PropagateExpression(BoundColumnRefExpression &colref,
-                                                                     unique_ptr<Expression> *expr_ptr) {
+                                                                     unique_ptr<Expression> &expr_ptr) {
 	auto stats = statistics_map.find(colref.binding);
 	if (stats == statistics_map.end()) {
 		return nullptr;
