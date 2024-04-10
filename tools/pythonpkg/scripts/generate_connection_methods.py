bbf679e0dc92729042c42c3dce9655fb8c2bea6f
Tishj <t_b@live.nl>
2024-04-10 16:34:44 +0200

    generate the c++ (pybind) definition code

--- tools/pythonpkg/scripts/generate_connection_methods.py
+++ tools/pythonpkg/scripts/generate_connection_methods.py
@@ -29,16 +29,61 @@ if start_index == -1 or end_index == -1:
 
 start_section = source_code[: start_index + 1]
 end_section = source_code[end_index:]
+# ---- Generate the definition code from the json ----
 
-# Generate the definition code from the json
 # Read the JSON file
 with open(JSON_PATH, 'r') as json_file:
     connection_methods = json.load(json_file)
 
-regenerated_method = []
-regenerated_method.extend(['', ''])
+body = []
+body.extend(['', ''])
 
-with_newlines = [x + '\n' for x in regenerated_method]
+DEFAULT_ARGUMENT_MAP = {'True': 'true', 'False': 'false', 'None': 'py::none()'}
+
+
+def map_default(val):
+    if val in DEFAULT_ARGUMENT_MAP:
+        return DEFAULT_ARGUMENT_MAP[val]
+    return val
+
+
+for conn in connection_methods:
+    definition = f"m.def(\"{conn['name']}\""
+    definition += ", "
+    definition += f"""&DuckDBPyConnection::{conn['function']}"""
+    definition += ", "
+    definition += f"\"{conn['docs']}\""
+    if 'args' in conn:
+        definition += ", "
+        arguments = []
+        for arg in conn['args']:
+            argument = f"py::arg(\"{arg['name']}\")"
+            # Add the default argument if present
+            if 'default' in arg:
+                default = map_default(arg['default'])
+                argument += f" = {default}"
+            arguments.append(argument)
+        definition += ', '.join(arguments)
+    if 'kwargs' in conn:
+        definition += ", "
+        definition += "py::kw_only(), "
+        keyword_arguments = []
+        for kwarg in conn['kwargs']:
+            keyword_argument = f"py::arg(\"{kwarg['name']}\")"
+            # Add the default argument if present
+            if 'default' in arg:
+                default = map_default(arg['default'])
+                keyword_argument += f" = {default}"
+            keyword_arguments.append(keyword_argument)
+        definition += ', '.join(keyword_arguments)
+    definition += ");"
+    body.append(definition)
+
+# ---- End of generation code ----
+
+with_newlines = [x + '\n' for x in body]
+print(''.join(with_newlines))
+exit()
 # Recreate the file content by concatenating all the pieces together
 
 new_content = start_section + with_newlines + end_section
