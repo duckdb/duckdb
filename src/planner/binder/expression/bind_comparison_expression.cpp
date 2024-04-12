f1b63beae65bf69faf60adb96214076990dc4381
Laurens Kuiper <laurens.kuiper@cwi.nl>
2024-04-12 10:46:48 +0200

    add missing include

--- src/planner/binder/expression/bind_comparison_expression.cpp
+++ src/planner/binder/expression/bind_comparison_expression.cpp
@@ -7,6 +7,7 @@
 #include "duckdb/main/config.hpp"
 #include "duckdb/parser/expression/comparison_expression.hpp"
 #include "duckdb/parser/expression/constant_expression.hpp"
+#include "duckdb/parser/expression/function_expression.hpp"
 #include "duckdb/planner/expression/bound_cast_expression.hpp"
 #include "duckdb/planner/expression/bound_comparison_expression.hpp"
 #include "duckdb/planner/expression/bound_constant_expression.hpp"
