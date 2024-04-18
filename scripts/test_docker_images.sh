3ed2ae9372c57e7bb34213f39e36ff306228dbf0
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-04-18 22:29:38 +0200

    Add test also on ubuntu:devel

--- scripts/test_docker_images.sh
+++ scripts/test_docker_images.sh
@@ -7,4 +7,5 @@ docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb alpine:latest <<< "apk ad
 docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb alpine:latest <<< "apk add g++ git make cmake ninja python3 && GEN=ninja make && make clean" 2>&1
 docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb alpine:latest <<< "apk add g++ git make cmake ninja && CXX_STANDARD=23 GEN=ninja make && make clean" 2>&1
 docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb ubuntu:20.04 <<< "apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install g++ git make cmake ninja-build -y && GEN=ninja make && make clean" 2>&1
+docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb ubuntu:devel <<< "apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install g++ git make cmake ninja-build -y && GEN=ninja make && make clean" 2>&1
 
