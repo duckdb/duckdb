c5249bbe91df35b0189b7d8d24af1907d610d2f2
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-04-18 10:55:05 +0200

    Add CI check on capability to build duckdb in docker

--- scripts/test_docker_images.sh
+++ scripts/test_docker_images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 make clean
-docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb alpine:latest <<< "apk add g++ git make cmake ninja && GEN=ninja make" 2>&1
-echo "alpine:latest completed"
+docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb alpine:latest <<< "apk add g++ git make cmake ninja python3 && GEN=ninja make && make clean" 2>&1
+docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb ubuntu:20.04 <<< "apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install g++ git make cmake ninja-build python3 -y && GEN=ninja make && make clean" 2>&1
