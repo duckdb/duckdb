e33dae3871979f1382e34bf7e4f6cb2e4e620c02
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-04-18 20:26:43 +0200

    Remove python3 build dependency from docker tests

--- scripts/test_docker_images.sh
+++ scripts/test_docker_images.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 make clean
+docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb alpine:latest <<< "apk add g++ git make cmake ninja && GEN=ninja make && make clean" 2>&1
 docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb alpine:latest <<< "apk add g++ git make cmake ninja python3 && GEN=ninja make && make clean" 2>&1
-docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb ubuntu:20.04 <<< "apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install g++ git make cmake ninja-build python3 -y && GEN=ninja make && make clean" 2>&1
+docker run -i --rm -v $(pwd):/duckdb --workdir /duckdb ubuntu:20.04 <<< "apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install g++ git make cmake ninja-build -y && GEN=ninja make && make clean" 2>&1
