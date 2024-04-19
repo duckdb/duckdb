25d6d851616379b9520b2c7ac510c68c2a37a8e3
Tom Ebergen <tom@ebergen.com>
2024-04-19 15:40:08 +0200

    allow the reconstructor to look into the DP table. Also a big refactor to remove lots of unused code

--- src/include/duckdb/optimizer/join_order/estimated_properties.hpp
+++ src/include/duckdb/optimizer/join_order/estimated_properties.hpp
@@ -18,40 +18,40 @@
 
 namespace duckdb {
 
-class EstimatedProperties {
-public:
-	EstimatedProperties(double cardinality, double cost) : cardinality(cardinality), cost(cost) {};
-	EstimatedProperties() : cardinality(0), cost(0) {};
-
-	template <class T>
-	T GetCardinality() const {
-		throw NotImplementedException("Unsupported type for GetCardinality");
-	}
-	template <class T>
-	T GetCost() const {
-		throw NotImplementedException("Unsupported type for GetCost");
-	}
-	void SetCost(double new_cost);
-	void SetCardinality(double cardinality);
-
-private:
-	double cardinality;
-	double cost;
-
-public:
-	unique_ptr<EstimatedProperties> Copy();
-};
-
-template <>
-double EstimatedProperties::GetCardinality() const;
-
-template <>
-idx_t EstimatedProperties::GetCardinality() const;
-
-template <>
-double EstimatedProperties::GetCost() const;
-
-template <>
-idx_t EstimatedProperties::GetCost() const;
+// class EstimatedProperties {
+// public:
+//	EstimatedProperties(double cardinality, double cost) : cardinality(cardinality), cost(cost) {};
+//	EstimatedProperties() : cardinality(0), cost(0) {};
+//
+//	template <class T>
+//	T GetCardinality() const {
+//		throw NotImplementedException("Unsupported type for GetCardinality");
+//	}
+//	template <class T>
+//	T GetCost() const {
+//		throw NotImplementedException("Unsupported type for GetCost");
+//	}
+//	void SetCost(double new_cost);
+//	void SetCardinality(double cardinality);
+//
+// private:
+//	double cardinality;
+//	double cost;
+//
+// public:
+//	unique_ptr<EstimatedProperties> Copy();
+//};
+//
+// template <>
+// double EstimatedProperties::GetCardinality() const;
+//
+// template <>
+// idx_t EstimatedProperties::GetCardinality() const;
+//
+// template <>
+// double EstimatedProperties::GetCost() const;
+//
+// template <>
+// idx_t EstimatedProperties::GetCost() const;
 
 } // namespace duckdb
--- src/optimizer/join_order/estimated_properties.cpp
+++ src/optimizer/join_order/estimated_properties.cpp
@@ -3,34 +3,34 @@
 
 namespace duckdb {
 
-template <>
-double EstimatedProperties::GetCardinality() const {
-	return cardinality;
-}
-
-template <>
-idx_t EstimatedProperties::GetCardinality() const {
-	auto max_idx_t = NumericLimits<idx_t>::Maximum() - 10000;
-	return MinValue<double>(cardinality, max_idx_t);
-}
-
-template <>
-double EstimatedProperties::GetCost() const {
-	return cost;
-}
-
-template <>
-idx_t EstimatedProperties::GetCost() const {
-	auto max_idx_t = NumericLimits<idx_t>::Maximum() - 10000;
-	return MinValue<double>(cost, max_idx_t);
-}
-
-void EstimatedProperties::SetCardinality(double new_card) {
-	cardinality = new_card;
-}
-
-void EstimatedProperties::SetCost(double new_cost) {
-	cost = new_cost;
-}
+// template <>
+// double EstimatedProperties::GetCardinality() const {
+//	return cardinality;
+//}
+//
+// template <>
+// idx_t EstimatedProperties::GetCardinality() const {
+//	auto max_idx_t = NumericLimits<idx_t>::Maximum() - 10000;
+//	return MinValue<double>(cardinality, max_idx_t);
+//}
+//
+// template <>
+// double EstimatedProperties::GetCost() const {
+//	return cost;
+//}
+//
+// template <>
+// idx_t EstimatedProperties::GetCost() const {
+//	auto max_idx_t = NumericLimits<idx_t>::Maximum() - 10000;
+//	return MinValue<double>(cost, max_idx_t);
+//}
+//
+// void EstimatedProperties::SetCardinality(double new_card) {
+//	cardinality = new_card;
+//}
+//
+// void EstimatedProperties::SetCost(double new_cost) {
+//	cost = new_cost;
+//}
 
 } // namespace duckdb
