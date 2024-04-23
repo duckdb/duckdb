f86de1b6eeea8d2216242fe6c26a2b6bd9b5ebec
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-04-23 12:15:28 +0200

    Add test for strings + extra info

--- test/api/capi/capi_scalar_functions.cpp
+++ test/api/capi/capi_scalar_functions.cpp
@@ -37,7 +37,7 @@ void AddNumbersTogether(duckdb_function_info info, duckdb_data_chunk input, duck
 	}
 }
 
-static void capi_register_scalar_function(duckdb_connection connection, const char *name) {
+static void CAPIRegisterAddition(duckdb_connection connection, const char *name) {
 	duckdb_state status;
 
 	// create a scalar function
@@ -79,7 +79,7 @@ TEST_CASE("Test Scalar Functions C API", "[capi]") {
 	duckdb::unique_ptr<CAPIResult> result;
 
 	REQUIRE(tester.OpenDatabase(nullptr));
-	capi_register_scalar_function(tester.connection, "my_addition");
+	CAPIRegisterAddition(tester.connection, "my_addition");
 
 	// now call it
 	result = tester.Query("SELECT my_addition(40, 2)");
@@ -100,4 +100,82 @@ TEST_CASE("Test Scalar Functions C API", "[capi]") {
 	for(idx_t row = 0; row < 10000; row++) {
 		REQUIRE(result->Fetch<int64_t>(0, row) == static_cast<int64_t>(1000000 + row));
 	}
+}
+
+void ReturnStringInfo(duckdb_function_info info, duckdb_data_chunk input, duckdb_vector output) {
+	auto extra_info = string((const char *) info);
+	// get the total number of rows in this chunk
+	idx_t input_size = duckdb_data_chunk_get_size(input);
+	// extract the two input vectors
+	duckdb_vector input_vector = duckdb_data_chunk_get_vector(input, 0);
+	// get the data pointers for the input vectors (both int64 as specified by the parameter types)
+	auto input_data = (duckdb_string_t *) duckdb_vector_get_data(input_vector);
+	// get the validity vectors
+	auto input_validity = duckdb_vector_get_validity(input_vector);
+	duckdb_vector_ensure_validity_writable(output);
+	auto result_validity = duckdb_vector_get_validity(output);
+	for(idx_t row = 0; row < input_size; row++) {
+		if (duckdb_validity_row_is_valid(input_validity, row)) {
+			// not null - do the operation
+			auto input_string = input_data[row];
+			string result = extra_info + "_";
+			if (duckdb_string_is_inlined(input_string)) {
+				result += string(input_string.value.inlined.inlined, input_string.value.inlined.length);
+			} else {
+				result += input_string.value.pointer.ptr;
+			}
+			duckdb_vector_assign_string_element_len(output, row, result.c_str(), result.size());
+		} else {
+			// either a or b is NULL - set the result row to NULL
+			duckdb_validity_set_row_invalid(result_validity, row);
+		}
+	}
+}
+
+static void CAPIRegisterStringInfo(duckdb_connection connection, const char *name, duckdb_function_info info, duckdb_delete_callback_t destroy_func) {
+	duckdb_state status;
+
+	// create a scalar function
+	auto function = duckdb_create_scalar_function();
+	duckdb_scalar_function_set_name(function, name);
+
+	// add a single varchar parameter
+	duckdb_logical_type type = duckdb_create_logical_type(DUCKDB_TYPE_VARCHAR);
+	duckdb_table_function_add_parameter(function, type);
+
+	// set the return type to varchar
+	duckdb_scalar_function_set_return_type(function, type);
+	duckdb_destroy_logical_type(&type);
+
+	// set up the function
+	duckdb_scalar_function_set_function(function, ReturnStringInfo);
+
+	// set the extra info
+	duckdb_scalar_function_set_extra_info(function, info, destroy_func);
+
+	// register and cleanup
+	status = duckdb_register_scalar_function(connection, function);
+	REQUIRE(status == DuckDBSuccess);
+
+	duckdb_destroy_scalar_function(&function);
+}
+
+TEST_CASE("Test Scalar Functions - strings & extra_info", "[capi]") {
+	CAPITester tester;
+	duckdb::unique_ptr<CAPIResult> result;
+
+	auto string_data = (char *) malloc(100);
+	strcpy(string_data, "my_prefix");
+
+	REQUIRE(tester.OpenDatabase(nullptr));
+	CAPIRegisterStringInfo(tester.connection, "my_prefix", (duckdb_function_info) string_data, free);
+
+	// now call it
+	result = tester.Query("SELECT my_prefix('hello_world')");
+	REQUIRE_NO_FAIL(*result);
+	REQUIRE(result->Fetch<string>(0, 0) == "my_prefix_hello_world");
+
+	result = tester.Query("SELECT my_prefix(NULL)");
+	REQUIRE_NO_FAIL(*result);
+	REQUIRE(result->IsNull(0, 0));
 }
