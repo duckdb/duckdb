80dbc32aa0534dc4faeac7dfce25be371ec6c01a
Tom Ebergen <tom@ebergen.com>
2024-04-23 14:38:43 +0200

    cast to long because you cannot add unsigned types to iterators

--- src/optimizer/join_order/plan_enumerator.cpp
+++ src/optimizer/join_order/plan_enumerator.cpp
@@ -416,8 +416,8 @@ void PlanEnumerator::SolveJoinOrderApproximately() {
 		auto &new_set = query_graph_manager.set_manager.Union(join_relations.at(best_left).get(),
 		                                                      join_relations.at(best_right).get());
 		D_ASSERT(best_right > best_left);
-		join_relations.erase(join_relations.begin() + best_right);
-		join_relations.erase(join_relations.begin() + best_left);
+		join_relations.erase(join_relations.begin() + (long)best_right);
+		join_relations.erase(join_relations.begin() + (long)best_left);
 		join_relations.push_back(new_set);
 	}
 }
