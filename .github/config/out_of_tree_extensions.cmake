eeeac12aab1a3f8ddc7b082a351e127850c0c1a3
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-13 14:17:52 +0200

    Bump to latest vss

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -113,7 +113,7 @@ endif()
 duckdb_extension_load(vss
         LOAD_TESTS
         GIT_URL https://github.com/duckdb/duckdb_vss
-        GIT_TAG 8145f41d97178e82bed3376215eb8d02bcf1eec5
+        GIT_TAG dbf5b74298384a9dc8e78353e628259b020cd4eb
         TEST_DIR test/sql
         APPLY_PATCHES
     )
