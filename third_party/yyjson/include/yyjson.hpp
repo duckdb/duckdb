818a9519a768e6df4435b1b095b6aebded3e2870
Max Gabrielsson <max@gabrielsson.com>
2024-05-13 17:24:42 +0200

    add pragmas to pass amalgamation

--- third_party/yyjson/include/yyjson.hpp
+++ third_party/yyjson/include/yyjson.hpp
@@ -564,6 +564,8 @@ typedef struct yyjson_mut_val yyjson_mut_val;
  * JSON Reader API
  *============================================================================*/
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-const-variable"
 /** Run-time options for JSON reader. */
 typedef uint32_t yyjson_read_flag;
 
@@ -672,7 +674,7 @@ typedef struct yyjson_read_err {
     size_t pos;
 } yyjson_read_err;
 
-
+#pragma clang diagnostic pop
 
 /**
  Read JSON with options.
@@ -870,6 +872,9 @@ yyjson_api_inline const char *yyjson_mut_read_number(const char *dat,
 /** Run-time options for JSON writer. */
 typedef uint32_t yyjson_write_flag;
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-const-variable"
+
 /** Default option:
     - Write JSON minify.
     - Report error on inf or nan number.
@@ -941,7 +946,7 @@ typedef struct yyjson_write_err {
     const char *msg;
 } yyjson_write_err;
 
-
+#pragma clang diagnostic pop
 
 /*==============================================================================
  * JSON Document Writer API
