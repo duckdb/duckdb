df7c860a7fd3cbddd5ab03f7edc6f3dee3dfe3e3
Sam Ansmink <samansmink@hotmail.com>
2024-05-13 10:14:16 +0200

    add accidentally removed cast

--- src/main/extension.cpp
+++ src/main/extension.cpp
@@ -14,7 +14,7 @@ static string PrettyPrintString(const string &s) {
 		    c == '.') {
 			res += c;
 		} else {
-			uint8_t value = c;
+			auto value = UnsafeNumericCast<uint8_t>(c);
 			res += "\\x";
 			uint8_t first = value / 16;
 			if (first < 10) {
