983db804c04827909ba9e7e214613e02c369e769
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2024-05-13 10:46:36 +0200

    tidy check

--- src/storage/compression/validity_uncompressed.cpp
+++ src/storage/compression/validity_uncompressed.cpp
@@ -4,6 +4,7 @@
 #include "duckdb/storage/buffer_manager.hpp"
 #include "duckdb/storage/segment/uncompressed.hpp"
 #include "duckdb/storage/table/append_state.hpp"
+#include "duckdb/storage/table/column_data.hpp"
 #include "duckdb/storage/table/column_segment.hpp"
 #include "duckdb/storage/table/scan_state.hpp"
 
