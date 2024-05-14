b32a2cdef47d280fea5c9ae1fd29eefed7c39302
Tom Ebergen <tom@ebergen.com>
2024-05-14 17:47:34 +0200

    have the rewriting for timestamps working for now. need to check on some other things first

--- src/include/duckdb/optimizer/rule/timestamp_comparison.hpp
+++ src/include/duckdb/optimizer/rule/timestamp_comparison.hpp
@@ -15,13 +15,16 @@ namespace duckdb {
 
 class TimeStampComparison : public Rule {
 public:
-	explicit TimeStampComparison(ExpressionRewriter &rewriter);
+	explicit TimeStampComparison(ClientContext &context, ExpressionRewriter &rewriter);
 
 	unique_ptr<Expression> Apply(LogicalOperator &op, vector<reference<Expression>> &bindings, bool &changes_made,
 	                             bool is_root) override;
 
 	unique_ptr<Expression> ApplyRule(BoundFunctionExpression *expr, ScalarFunction function, string pattern,
 	                                 bool is_not_like);
+
+private:
+	ClientContext &context;
 };
 
 } // namespace duckdb
--- src/optimizer/rule/timestamp_comparison.cpp
+++ src/optimizer/rule/timestamp_comparison.cpp
@@ -1,18 +1,22 @@
 
 #include "duckdb/optimizer/rule/timestamp_comparison.hpp"
+#include "duckdb/common/constants.hpp"
 
 #include "duckdb/execution/expression_executor.hpp"
-#include "duckdb/planner/expression/bound_comparison_expression.hpp"
 #include "duckdb/planner/expression/bound_cast_expression.hpp"
 #include "duckdb/optimizer/matcher/type_matcher_id.hpp"
 #include "duckdb/optimizer/expression_rewriter.hpp"
+#include "duckdb/common/types/interval.hpp"
+#include "duckdb/parser/expression/constant_expression.hpp"
 #include "duckdb/common/types.hpp"
 
 namespace duckdb {
 
-TimeStampComparison::TimeStampComparison(ExpressionRewriter &rewriter) : Rule(rewriter) {
+TimeStampComparison::TimeStampComparison(ClientContext &context, ExpressionRewriter &rewriter)
+    : Rule(rewriter), context(context) {
 	// match on a ComparisonExpression that is an Equality and has a VARCHAR and ENUM as its children
 	auto op = make_uniq<ComparisonExpressionMatcher>();
+	op->policy = SetMatcher::Policy::UNORDERED;
 	// Enum requires expression to be root
 	op->expr_type = make_uniq<SpecificExpressionTypeMatcher>(ExpressionType::COMPARE_EQUAL);
 
@@ -33,13 +37,41 @@ TimeStampComparison::TimeStampComparison(ExpressionRewriter &rewriter) : Rule(re
 
 unique_ptr<Expression> TimeStampComparison::Apply(LogicalOperator &op, vector<reference<Expression>> &bindings,
                                                   bool &changes_made, bool is_root) {
+	auto len = bindings.size();
+	auto &cast_constant = bindings[len - 1].get();
+	auto &cast_columnref = bindings[len - 2].get();
+	auto new_expr = make_uniq<BoundConjunctionExpression>(ExpressionType::CONJUNCTION_AND);
 
-	const Expression &ts_to_date = bindings[0].get();
-	// rewrite it to ts >= date && ts < date + 1 day.
+	Value result;
+	if (ExpressionExecutor::TryEvaluateScalar(context, cast_constant, result)) {
+		D_ASSERT(result.type() == LogicalType::DATE);
+		auto original_val = result.GetValue<duckdb::date_t>();
+		auto no_seconds = dtime_t(0);
 
-	const Expression &date = bindings[1].get();
+		// original date with no timestamp info
+		auto original_val_ts = Value::TIMESTAMP(original_val, no_seconds);
+		auto original_val_for_comparison = make_uniq<BoundConstantExpression>(original_val_ts);
+
+		// add one day
+		auto date_t_copy = result.GetValue<duckdb::date_t>();
+		date_t_copy.days += 1;
+		auto original_val_plus_on_date_ts = Value::TIMESTAMP(date_t_copy, dtime_t(0));
+
+		auto val_for_comparison = make_uniq<BoundConstantExpression>(original_val_plus_on_date_ts);
+
+		auto left_copy = cast_columnref.Copy();
+		auto right_copy = cast_columnref.Copy();
+		auto lt_eq_expr = make_uniq<BoundComparisonExpression>(ExpressionType::COMPARE_LESSTHAN, std::move(right_copy),
+		                                                       std::move(val_for_comparison));
+		auto gt_eq_expr = make_uniq<BoundComparisonExpression>(
+		    ExpressionType::COMPARE_GREATERTHANOREQUALTO, std::move(left_copy), std::move(original_val_for_comparison));
+		new_expr->children.push_back(std::move(gt_eq_expr));
+		new_expr->children.push_back(std::move(lt_eq_expr));
+		return std::move(new_expr);
+	}
+
 	// ok so here now I need to figure out the other stuff.
 	return nullptr;
 }
 
-}
+} // namespace duckdb
