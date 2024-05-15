0259f300c14f7db4303852422588a065c1ef8fbe
Tom Ebergen <tom@ebergen.com>
2024-05-15 14:28:33 +0200

    add one more header

--- src/optimizer/rule/timestamp_comparison.cpp
+++ src/optimizer/rule/timestamp_comparison.cpp
@@ -6,6 +6,7 @@
 #include "duckdb/execution/expression_executor.hpp"
 #include "duckdb/planner/expression/bound_cast_expression.hpp"
 #include "duckdb/planner/expression/bound_conjunction_expression.hpp"
+#include "duckdb/planner/expression/bound_comparison_expression.hpp"
 #include "duckdb/planner/expression/bound_constant_expression.hpp"
 #include "duckdb/optimizer/matcher/type_matcher_id.hpp"
 #include "duckdb/optimizer/expression_rewriter.hpp"
