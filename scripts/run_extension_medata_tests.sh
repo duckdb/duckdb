b3c471f41060a11ebe50fb4265c27f3eff79854f
Sam Ansmink <samansmink@hotmail.com>
2024-05-16 20:53:17 +0200

    add http tests as well

--- scripts/run_extension_medata_tests.sh
+++ scripts/run_extension_medata_tests.sh
@@ -152,6 +152,12 @@ EOL
   cp -R $TEST_DIR $TEST_DIR_COPY
 fi
 
+###########################
+### Populate the minio repositories
+###########################
+#AWS_DEFAULT_REGION=eu-west-1 AWS_ACCESS_KEY_ID=minio_duckdb_user AWS_SECRET_ACCESS_KEY=minio_duckdb_user_password aws --endpoint-url http://duckdb-minio.com:9000 s3 cp $LOCAL_EXTENSION_REPO_UPDATED s3://test-bucket-public/ci-test-repo
+export REMOTE_EXTENSION_REPO_UPDATED=http://duckdb-minio.com:9000/test-bucket-public/ci-test-repo
+
 ################
 ### Run test
 ################
