9b6aa35216c50fae4caa4143c38b627588acbd3c
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-05-16 16:58:57 +0200

    Enable abort_on_wal_failure by default for the test config

--- src/include/duckdb/main/config.hpp
+++ src/include/duckdb/main/config.hpp
@@ -201,12 +201,8 @@ struct DBConfigOptions {
 	string custom_user_agent;
 	//! Use old implicit casting style (i.e. allow everything to be implicitly casted to VARCHAR)
 	bool old_implicit_casting = false;
-	//!  Whether or not to abort if a serialization exception is thrown during WAL playback
-#ifdef DEBUG
-	bool abort_on_wal_failure = true;
-#else
+	//!  Whether or not to abort if a serialization exception is thrown during WAL playback (when reading truncated WAL)
 	bool abort_on_wal_failure = false;
-#endif
 
 	bool operator==(const DBConfigOptions &other) const;
 };
