ea9a7025d601f24bce9567e6abee05ed09272f93
Tishj <t_b@live.nl>
2024-05-18 14:44:41 +0200

    missing include

--- src/planner/binder/statement/bind_insert.cpp
+++ src/planner/binder/statement/bind_insert.cpp
@@ -1,5 +1,6 @@
 #include "duckdb/catalog/catalog.hpp"
 #include "duckdb/parser/expression/constant_expression.hpp"
+#include "duckdb/parser/expression/function_expression.hpp"
 #include "duckdb/parser/statement/insert_statement.hpp"
 #include "duckdb/parser/query_node/select_node.hpp"
 #include "duckdb/parser/tableref/expressionlistref.hpp"
