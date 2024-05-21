3550708a8456e193e5d8ccc21a3dd6563e8a0c21
Pedro Holanda <pedroholanda@gmail.com>
2024-05-21 18:04:27 +0200

    make format

--- src/execution/operator/csv_scanner/util/csv_reader_options.cpp
+++ src/execution/operator/csv_scanner/util/csv_reader_options.cpp
@@ -94,7 +94,7 @@ int64_t CSVReaderOptions::GetSkipRows() const {
 }
 
 void CSVReaderOptions::SetSkipRows(int64_t skip_rows) {
-	if (skip_rows < 0){
+	if (skip_rows < 0) {
 		throw InvalidInputException("skip_rows option from read_csv scanner, must be equal or higher than 0");
 	}
 	dialect_options.skip_rows.Set(NumericCast<idx_t>(skip_rows));
