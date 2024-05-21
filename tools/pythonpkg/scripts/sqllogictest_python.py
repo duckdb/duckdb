48811042042e6242c6f098fb6d47c7da863d46ec
Tishj <t_b@live.nl>
2024-05-21 09:40:21 +0200

    remove the temp directory after every test, avoiding side-effects of one test causing issues in another (created files with same files)

--- tools/pythonpkg/scripts/sqllogictest_python.py
+++ tools/pythonpkg/scripts/sqllogictest_python.py
@@ -90,6 +90,10 @@ class SQLLogicTestExecutor(SQLLogicRunner):
         self.test = test
         self.original_sqlite_test = self.test.is_sqlite_test()
 
+        test_directory = TEST_DIRECTORY_PATH
+        if os.path.exists(test_directory):
+            shutil.rmtree(test_directory, ignore_errors=True)
+
         # Top level keywords
         keywords = {'__TEST_DIR__': self.get_test_directory(), '__WORKING_DIRECTORY__': os.getcwd()}
 
