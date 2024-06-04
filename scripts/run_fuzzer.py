68171c655edf1f98edd4e57213ccd21119690cac
Tmonster <tom@ebergen.com>
2024-06-04 14:33:35 +0200

    Revert "fix max_queries count"
    This reverts commit 39589aab0a8043949babe5560701fd8e8e826bf9.

--- scripts/run_fuzzer.py
+++ scripts/run_fuzzer.py
@@ -103,7 +103,7 @@ if dry:
 else:
     current_errors = fuzzer_helper.extract_github_issues(shell, perform_checks)
 
-max_queries = 2000
+max_queries = 10
 last_query_log_file = 'sqlsmith.log'
 complete_log_file = 'sqlsmith.complete.log'
 
