bdb20475f9d62d5662881c41369323c6bb31aca2
Tishj <t_b@live.nl>
2024-06-07 20:40:54 +0200

    add back test

--- tools/pythonpkg/tests/fast/test_relation.py
+++ tools/pythonpkg/tests/fast/test_relation.py
@@ -519,3 +519,15 @@ class TestRelation(object):
         intersect_rel = unioned_rel.intersect(materialized_one).order('range')
         res = intersect_rel.fetchall()
         assert res == [('0',), ('1',), ('2',), ('3',), ('4',), ('5',), ('6',), ('7',), ('8',), ('9',)]
+
+    def test_materialized_relation_view(self, duckdb_cursor):
+        with pytest.raises(
+            duckdb.NotImplementedException, match='Creating a VIEW from a MaterializedRelation is not supported'
+        ):
+            duckdb_cursor.sql(
+                """
+                create table tbl(a varchar);
+                insert into tbl values ('test') returning *
+            """
+            ).to_view('vw')
+            res = duckdb_cursor.sql("select * from vw").fetchone()
