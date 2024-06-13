3a98472dfa44d7bcce599c9581ca66b5c40ecb99
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-06-13 12:53:31 +0200

    Run formatter also on src/include/duckdb/core_functions/...

--- src/include/duckdb/core_functions/aggregate/algebraic/stddev.hpp
+++ src/include/duckdb/core_functions/aggregate/algebraic/stddev.hpp
@@ -40,7 +40,6 @@ struct STDDevBaseOperation {
 
 		state.mean = new_mean;
 		state.dsquared = new_dsquared;
-		
 	}
 
 	template <class INPUT_TYPE, class STATE, class OP>
@@ -49,7 +48,8 @@ struct STDDevBaseOperation {
 	}
 
 	template <class INPUT_TYPE, class STATE, class OP>
-	static void ConstantOperation(STATE &state, const INPUT_TYPE &input, AggregateUnaryInput &unary_input, idx_t count) {
+	static void ConstantOperation(STATE &state, const INPUT_TYPE &input, AggregateUnaryInput &unary_input,
+	                              idx_t count) {
 		for (idx_t i = 0; i < count; i++) {
 			Operation<INPUT_TYPE, STATE, OP>(state, input, unary_input);
 		}
@@ -65,7 +65,8 @@ struct STDDevBaseOperation {
 			const auto count = target_count + source_count;
 			const auto mean = (source_count * source.mean + target_count * target.mean) / count;
 			const auto delta = source.mean - target.mean;
-			target.dsquared = source.dsquared + target.dsquared + delta * delta * static_cast<double>(source.count * target.count) / count;
+			target.dsquared = source.dsquared + target.dsquared +
+			                  delta * delta * static_cast<double>(source.count * target.count) / count;
 			target.mean = mean;
 			target.count = static_cast<decltype(target.count)>(count);
 		}
