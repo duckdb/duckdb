267896da8cc8988aa548826ff9a1631d26e011c4
Sam Ansmink <samansmink@hotmail.com>
2024-06-23 16:07:13 +0200

    revert earlier changes to capi header

--- src/include/duckdb.h
+++ src/include/duckdb.h
@@ -521,8 +521,6 @@ typedef struct _duckdb_arrow_array {
 	void *__arra;
 } * duckdb_arrow_array;
 
-#ifndef DUCKB_NO_CAPI_FUNCTIONS
-
 //===--------------------------------------------------------------------===//
 // Functions
 //===--------------------------------------------------------------------===//
@@ -3231,30 +3229,6 @@ It is not known beforehand how many chunks will be returned by this result.
 * returns: The resulting data chunk. Returns `NULL` if the result has an error.
 */
 DUCKDB_API duckdb_data_chunk duckdb_fetch_chunk(duckdb_result result);
-
-#endif
-
-// This is a demo of how a function-pointer + header file based API would allow C API extensions
-// Note: obviously we would want some codegen like way to create this
-typedef struct {
-	// Example subset of CAPI to support a basic scalar function example
-	idx_t (*duckdb_data_chunk_get_size)(duckdb_data_chunk chunk);
-	duckdb_vector (*duckdb_data_chunk_get_vector)(duckdb_data_chunk chunk, idx_t col_idx);
-	void *(*duckdb_vector_get_data)(duckdb_vector vector);
-	uint64_t *(*duckdb_vector_get_validity)(duckdb_vector vector);
-	void (*duckdb_vector_ensure_validity_writable)(duckdb_vector vector);
-	bool (*duckdb_validity_row_is_valid)(uint64_t *validity, idx_t row);
-	void (*duckdb_validity_set_row_invalid)(uint64_t *validity, idx_t row);
-	duckdb_scalar_function (*duckdb_create_scalar_function)();
-	void (*duckdb_scalar_function_set_name)(duckdb_scalar_function scalar_function, const char *name);
-	duckdb_logical_type (*duckdb_create_logical_type)(duckdb_type type);
-	void (*duckdb_scalar_function_add_parameter)(duckdb_scalar_function scalar_function, duckdb_logical_type type);
-	void (*duckdb_scalar_function_set_return_type)(duckdb_scalar_function scalar_function, duckdb_logical_type type);
-	void (*duckdb_destroy_logical_type)(duckdb_logical_type *type);
-	void (*duckdb_scalar_function_set_function)(duckdb_scalar_function scalar_function, duckdb_scalar_function_t function);
-	duckdb_state (*duckdb_register_scalar_function)(duckdb_connection con, duckdb_scalar_function scalar_function);
-	void (*duckdb_destroy_scalar_function)(duckdb_scalar_function *scalar_function);
-} duckdb_ext_api_v1;
 
 // We could have v2 APIs here as well where we can allow full changes to the struct
 
