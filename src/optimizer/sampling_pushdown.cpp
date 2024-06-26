f3b3ebfaaf983ef1229e56c7946d335bb0c80128
yuxuan <yuxuanzh@umich.edu>
2024-06-26 12:44:05 -0400

    fix invalid sampling method and size

--- src/optimizer/sampling_pushdown.cpp
+++ src/optimizer/sampling_pushdown.cpp
@@ -6,7 +6,8 @@ namespace duckdb {
 
 unique_ptr<LogicalOperator> SamplingPushdown::Optimize(unique_ptr<LogicalOperator> op) {
 	if (op->type == LogicalOperatorType::LOGICAL_SAMPLE &&
-	    op->Cast<LogicalSample>().sample_options->method == SampleMethod::SYSTEM_SAMPLE && !op->children.empty() &&
+	    op->Cast<LogicalSample>().sample_options->method == SampleMethod::SYSTEM_SAMPLE &&
+	    op->Cast<LogicalSample>().sample_options->is_percentage && !op->children.empty() &&
 	    op->children[0]->type == LogicalOperatorType::LOGICAL_GET &&
 	    op->children[0]->Cast<LogicalGet>().function.name == "seq_scan" && op->children[0]->children.empty()) {
 		auto &get = op->children[0]->Cast<LogicalGet>();
