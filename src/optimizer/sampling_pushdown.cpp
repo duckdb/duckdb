054ad619a96a96c2ae1d391a7c120fbcf0cf6fb6
yuxuan <yuxuanzh@umich.edu>
2024-06-26 10:50:23 -0400

    fix

--- src/optimizer/sampling_pushdown.cpp
+++ src/optimizer/sampling_pushdown.cpp
@@ -2,17 +2,13 @@
 #include "duckdb/planner/operator/logical_get.hpp"
 #include "duckdb/planner/operator/logical_sample.hpp"
 #include "duckdb/common/types/value.hpp"
-#include <iostream>
 namespace duckdb {
 
 unique_ptr<LogicalOperator> SamplingPushdown::Optimize(unique_ptr<LogicalOperator> op) {
 	if (op->type == LogicalOperatorType::LOGICAL_SAMPLE &&
 	    op->Cast<LogicalSample>().sample_options->method == SampleMethod::SYSTEM_SAMPLE &&
-	    op->Cast<LogicalSample>().sample_options->is_percentage &&
-		!op->children.empty() &&
-	    op->children[0]->Cast<LogicalGet>().function.name == "seq_scan" &&
-	    op->children[0]->type == LogicalOperatorType::LOGICAL_GET && op->children[0]->children.empty()) {
-		std::cout << "Optimizing sampling pushdown" << std::endl;
+	    !op->children.empty() && op->children[0]->type == LogicalOperatorType::LOGICAL_GET &&
+	    op->children[0]->Cast<LogicalGet>().function.name == "seq_scan" && op->children[0]->children.empty()) {
 		auto &get = op->children[0]->Cast<LogicalGet>();
 		// set sampling pushdown options
 		get.function.sampling_pushdown = true;
