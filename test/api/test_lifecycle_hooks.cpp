667bf306fdc1df615e80fc3256138e8a0df78d57
Niclas Haderer <niclas.haderer.dev@gmail.com>
2024-07-01 15:03:20 +0200

    Improve error collection

--- test/api/test_lifecycle_hooks.cpp
+++ test/api/test_lifecycle_hooks.cpp
@@ -10,6 +10,10 @@ using namespace Catch::Matchers;
 struct TestClientContextState : ClientContextState {
 	vector<string> query_errors;
 	vector<string> transaction_errors;
+
+	TestClientContextState() = default;
+	TestClientContextState(const TestClientContextState &) = delete;
+
 	void QueryEnd(optional_ptr<ErrorData> error) override {
 		if (error && error->HasError()) {
 			query_errors.push_back(error->Message());
@@ -24,92 +28,128 @@ struct TestClientContextState : ClientContextState {
 	}
 };
 
+shared_ptr<TestClientContextState> WithLifecycleState(const Connection &conn) {
+	auto &register_state = conn.context->registered_state;
+	auto state = make_shared_ptr<TestClientContextState>();
+	register_state["test_state"] = state;
+	return state;
+}
+
 TEST_CASE("Test ClientContextState", "[api]") {
 	DuckDB db(nullptr);
 	Connection conn(db);
 	conn.Query("CREATE TABLE my_table(i INT)");
-	auto &register_state = conn.context->registered_state;
-	register_state["test_state"] = make_uniq<TestClientContextState>();
-	auto &state = static_cast<TestClientContextState &>(*register_state["test_state"]);
-
-	table_function_bind_t bind = [](ClientContext &, TableFunctionBindInput &input, vector<LogicalType> &return_types,
-	                                vector<string> &names) -> unique_ptr<FunctionData> {
-		return_types.push_back(LogicalType::VARCHAR);
-		names.push_back("message");
-		return nullptr;
-	};
+	auto state = WithLifecycleState(conn);
 
 	const TableFunction table_fun(
 	    "raise_exception_tf", {},
 	    [](ClientContext &, TableFunctionInput &, DataChunk &) {
 		    throw std::runtime_error("This is a test exception.");
 	    },
-	    bind);
+	    [](ClientContext &, TableFunctionBindInput &, vector<LogicalType> &return_types,
+	       vector<string> &names) -> unique_ptr<FunctionData> {
+		    return_types.push_back(LogicalType::VARCHAR);
+		    names.push_back("message");
+		    return nullptr;
+	    });
 
 	ExtensionUtil::RegisterFunction(*db.instance, table_fun);
 
-	SECTION("No error and no explicit transaction") {
+	SECTION("No error, No explicit transaction") {
 		REQUIRE_NO_FAIL(conn.Query("SELECT * FROM my_table"));
-		REQUIRE(state.query_errors.empty());
-		REQUIRE(state.transaction_errors.empty());
+		REQUIRE(state->query_errors.empty());
+		REQUIRE(state->transaction_errors.empty());
 	}
 
-	SECTION("Error and no explicit transaction") {
+	SECTION("Error, No explicit transaction") {
 		REQUIRE_FAIL(conn.Query("SELECT * FROM this_table_does_not_exist"));
-		REQUIRE((state.query_errors.size() == 1));
-		REQUIRE_THAT(state.query_errors.at(0), Contains("Table with name this_table_does_not_exist does not exist!"));
-		REQUIRE((state.transaction_errors.size() == 1));
-		REQUIRE_THAT(state.transaction_errors.at(0),
+		REQUIRE((state->query_errors.size() == 1));
+		REQUIRE_THAT(state->query_errors.at(0), Contains("Table with name this_table_does_not_exist does not exist!"));
+		REQUIRE((state->transaction_errors.size() == 1));
+		REQUIRE_THAT(state->transaction_errors.at(0),
 		             Contains("Table with name this_table_does_not_exist does not exist!"));
 	}
 
-	SECTION("No error and explicit transaction") {
+	SECTION("No error, Explicit commit") {
 		conn.BeginTransaction();
 		REQUIRE_NO_FAIL(conn.Query("SELECT * FROM my_table"));
 		conn.Commit();
-		REQUIRE(state.query_errors.empty());
-		REQUIRE(state.transaction_errors.empty());
+		REQUIRE(state->query_errors.empty());
+		REQUIRE(state->transaction_errors.empty());
 	}
 
-	SECTION("No error and explicit rollback") {
+	SECTION("No error, Explicit rollback") {
 		conn.BeginTransaction();
 		REQUIRE_NO_FAIL(conn.Query("SELECT * FROM my_table"));
 		conn.Rollback();
-		REQUIRE(state.query_errors.empty());
-		REQUIRE(state.transaction_errors.empty());
+		REQUIRE(state->query_errors.empty());
+		REQUIRE(state->transaction_errors.empty());
 	}
 
-	SECTION("No error and explicit error rollback") {
-		// Multi statement transactions cannot have errors associated with them
-		// -> no error here
+	SECTION("Binding error, Explicit rollback") {
+		// These errors do not invalidate the transaction...
 		conn.BeginTransaction();
 		REQUIRE_FAIL(conn.Query("SELECT * FROM this_table_does_not_exist_1"));
 		REQUIRE_FAIL(conn.Query("SELECT * FROM this_table_does_not_exist_2"));
+		REQUIRE_NO_FAIL(conn.Query("SELECT * FROM my_table"));
 		conn.Rollback();
-		REQUIRE((state.query_errors.size() == 2));
-		REQUIRE_THAT(state.query_errors.at(0), Contains("Table with name this_table_does_not_exist_1 does not exist!"));
-		REQUIRE_THAT(state.query_errors.at(1), Contains("Table with name this_table_does_not_exist_2 does not exist!"));
-		REQUIRE((state.transaction_errors.empty()));
+		REQUIRE((state->query_errors.size() == 2));
+		REQUIRE_THAT(state->query_errors.at(0),
+		             Contains("Table with name this_table_does_not_exist_1 does not exist!"));
+		REQUIRE_THAT(state->query_errors.at(1),
+		             Contains("Table with name this_table_does_not_exist_2 does not exist!"));
+		REQUIRE((state->transaction_errors.empty()));
 	}
 
-	SECTION("No error and explicit commit") {
-		// Multi statement transactions cannot have errors associated with them, as they have to be deliberate
-		// -> no error here
+	SECTION("Binding error, Explicit commit") {
+		// These errors do not invalidate the transaction...
+		conn.BeginTransaction();
+		REQUIRE_FAIL(conn.Query("SELECT * FROM this_table_does_not_exist_1"));
+		REQUIRE_FAIL(conn.Query("SELECT * FROM this_table_does_not_exist_2"));
+		REQUIRE_NO_FAIL(conn.Query("SELECT * FROM my_table"));
+		conn.Commit();
+		REQUIRE((state->query_errors.size() == 2));
+		REQUIRE_THAT(state->query_errors.at(0),
+		             Contains("Table with name this_table_does_not_exist_1 does not exist!"));
+		REQUIRE_THAT(state->query_errors.at(1),
+		             Contains("Table with name this_table_does_not_exist_2 does not exist!"));
+		REQUIRE((state->transaction_errors.empty()));
+	}
+
+	SECTION("Runtime error, Explicit commit") {
 		conn.BeginTransaction();
 		REQUIRE_NO_FAIL(conn.Query("INSERT INTO my_table VALUES (1)"));
 		REQUIRE_FAIL(conn.Query("SELECT * FROM raise_exception_tf()"));
+		REQUIRE_FAIL(conn.Query("CREATE TABLE my_table2(i INT)"));
 		conn.Commit();
-		REQUIRE((state.query_errors.size() == 1));
-		REQUIRE_THAT(state.query_errors.at(0), Contains("This is a test exception."));
-		REQUIRE((state.transaction_errors.empty()));
+		REQUIRE((state->query_errors.size() == 2));
+		REQUIRE_THAT(state->query_errors.at(0), Contains("This is a test exception."));
+		REQUIRE_THAT(state->query_errors.at(1), Contains("Current transaction is aborted"));
+		REQUIRE((state->transaction_errors.size() == 1));
+		REQUIRE_THAT(state->transaction_errors.at(0), Contains("Failed to commit"));
+		REQUIRE_FAIL(conn.Query("SELECT * FROM my_table2"));
 	}
 
-	SECTION("Error during runtime") {
+	SECTION("Runtime error, No explicit transaction") {
 		REQUIRE_FAIL(conn.Query("SELECT * FROM raise_exception_tf()"));
-		REQUIRE((state.query_errors.size() == 1));
-		REQUIRE_THAT(state.query_errors.at(0), Contains("This is a test exception."));
-		REQUIRE((state.transaction_errors.size() == 1));
-		REQUIRE_THAT(state.transaction_errors.at(0), Contains("This is a test exception."));
+		REQUIRE((state->query_errors.size() == 1));
+		REQUIRE_THAT(state->query_errors.at(0), Contains("This is a test exception."));
+		REQUIRE((state->transaction_errors.size() == 1));
+		REQUIRE_THAT(state->transaction_errors.at(0), Contains("This is a test exception."));
+	}
+
+	SECTION("Manually invalidated transaction, Explicit commit") {
+		conn.BeginTransaction();
+		REQUIRE_NO_FAIL(conn.Query("CREATE TABLE my_table2(i INT)"));
+		auto &transaction = conn.context->ActiveTransaction();
+		ValidChecker::Invalidate(transaction, "42");
+		try {
+			conn.Commit();
+		} catch (...) {
+			// Ignore
+		}
+		REQUIRE((state->transaction_errors.size() == 1));
+		REQUIRE_FAIL(conn.Query("SELECT * FROM my_table2"));
 	}
 }
 // ClientContextState
