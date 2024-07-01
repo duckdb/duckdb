2f9a0bd0b9e32804583839bde41061312c05841a
Yiyuan Liu <yiyuanliu1997@gmail.com>
2024-07-01 21:40:41 +0800

    apply suggestion from code review (#12751)

--- src/storage/standard_buffer_manager.cpp
+++ src/storage/standard_buffer_manager.cpp
@@ -418,7 +418,7 @@ vector<MemoryInformation> StandardBufferManager::GetMemoryUsageInfo() const {
 	for (idx_t k = 0; k < MEMORY_TAG_COUNT; k++) {
 		MemoryInformation info;
 		info.tag = MemoryTag(k);
-		info.size = buffer_pool.memory_usage.GetUsedMemory(MemoryTag(k));
+		info.size = buffer_pool.memory_usage.GetUsedMemory(MemoryTag(k), true);
 		info.evicted_data = evicted_data_per_tag[k].load();
 		result.push_back(info);
 	}
