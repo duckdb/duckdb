32c551372985a8b540e3733a39e3c7c2a772043a
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-07-02 12:08:33 +0200

    Add missing include

--- src/transaction/undo_buffer.cpp
+++ src/transaction/undo_buffer.cpp
@@ -12,6 +12,7 @@
 #include "duckdb/transaction/rollback_state.hpp"
 #include "duckdb/execution/index/bound_index.hpp"
 #include "duckdb/transaction/wal_write_state.hpp"
+#include "duckdb/transaction/delete_info.hpp"
 
 namespace duckdb {
 constexpr uint32_t UNDO_ENTRY_HEADER_SIZE = sizeof(UndoFlags) + sizeof(uint32_t);
