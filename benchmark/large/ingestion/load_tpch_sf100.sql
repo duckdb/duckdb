affc3cf2cea2be2fd527da6cfec62442f47d099e
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-07-03 14:19:44 +0200

    Fix paths in load_tpcds_sf100.sql

--- benchmark/large/ingestion/load_tpch_sf100.sql
+++ benchmark/large/ingestion/load_tpch_sf100.sql
@@ -2,10 +2,10 @@ CALL dbgen(sf=100);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_parquet_sf100' (FORMAT PARQUET);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_csv_sf100' (FORMAT CSV);
 
-create view lineitem_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv/lineitem.csv');
-create view lineitem_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet/lineitem.parquet');
+create view lineitem_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/lineitem.csv');
+create view lineitem_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/lineitem.parquet');
 create view lineitem_native as select * from lineitem;
 
-create view orders_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv/orders.csv');
-create view orders_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet/orders.parquet');
+create view orders_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/orders.csv');
+create view orders_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/orders.parquet');
 create view orders_native as select * from orders;
