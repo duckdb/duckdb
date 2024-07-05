c4886d46aeefdca83f1df33c7ddfee1a0914757f
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-07-05 17:40:57 +0200

    change tpch benchmark orders instead of lineitem

--- benchmark/large/ingestion/tpch/load_tpch_sf100.sql
+++ benchmark/large/ingestion/tpch/load_tpch_sf100.sql
@@ -2,9 +2,9 @@ CALL dbgen(sf=100);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_parquet_sf100' (FORMAT PARQUET);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_csv_sf100' (FORMAT CSV);
 
-create view lineitem_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/lineitem.csv');
-create view lineitem_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/lineitem.parquet');
-create view lineitem_native as select * from lineitem;
+create view customer as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/customer.csv');
+create view customer as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/customer.parquet');
+create view customer as select * from customer;
 
 create view orders_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/orders.csv');
 create view orders_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/orders.parquet');
