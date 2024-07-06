e6fc5978dfffecc7d1c48937d958225afcf99e6d
flashmouse <flashmouse@DESKTOP-FM>
2024-07-06 16:50:06 +0800

    revert include sort

--- src/function/scalar/list/list_zip.cpp
+++ src/function/scalar/list/list_zip.cpp
@@ -1,11 +1,10 @@
-#include "duckdb/common/to_string.hpp"
-#include "duckdb/common/types.hpp"
 #include "duckdb/common/types/data_chunk.hpp"
 #include "duckdb/function/scalar/nested_functions.hpp"
 #include "duckdb/planner/expression/bound_cast_expression.hpp"
+#include "duckdb/planner/expression_binder.hpp"
 #include "duckdb/planner/expression/bound_function_expression.hpp"
 #include "duckdb/planner/expression/bound_parameter_expression.hpp"
-#include "duckdb/planner/expression_binder.hpp"
+#include "duckdb/common/to_string.hpp"
 
 namespace duckdb {
 
