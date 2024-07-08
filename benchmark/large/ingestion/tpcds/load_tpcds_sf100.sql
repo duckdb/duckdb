03eed9c37785cf3561e2743deff4369d981f477d
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-07-08 10:33:16 +0200

    replace benchmarking tables: with catalog_sales and store_returns

--- benchmark/large/ingestion/tpcds/load_tpcds_sf100.sql
+++ benchmark/large/ingestion/tpcds/load_tpcds_sf100.sql
@@ -2,10 +2,10 @@ CALL dsdgen(sf=100);
 EXPORT DATABASE 'duckdb_benchmark_data/tpcds_parquet_sf100' (FORMAT PARQUET);
 EXPORT DATABASE 'duckdb_benchmark_data/tpcds_csv_sf100' (FORMAT CSV);
 
-create or replace view store_sales_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf100/store_sales.csv');
-create or replace view store_sales_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf100/store_sales.parquet');
-create or replace view store_sales_native as select * from store_sales;
+create or replace view catalog_sales_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf100/catalog_sales.csv');
+create or replace view catalog_sales_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf100/catalog_sales.parquet');
+create or replace view catalog_sales_native as select * from catalog_sales;
 
-create or replace view inventory_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf100/inventory.csv');
-create or replace view inventory_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf100/inventory.parquet');
-create or replace view inventory_native as select * from inventory;
+create or replace view store_returns_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf100/store_returns.csv');
+create or replace view store_returns_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf100/store_returns.parquet');
+create or replace view store_returns_native as select * from store_returns;
