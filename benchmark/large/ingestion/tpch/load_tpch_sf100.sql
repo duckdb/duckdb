fb9a30aa75f4202e0cc1532556d67ab03379a7bb
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-07-08 10:32:12 +0200

    replace benchmarking tables: customer replaced with partsupp

--- benchmark/large/ingestion/tpch/load_tpch_sf100.sql
+++ benchmark/large/ingestion/tpch/load_tpch_sf100.sql
@@ -2,9 +2,9 @@ CALL dbgen(sf=100);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_parquet_sf100' (FORMAT PARQUET);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_csv_sf100' (FORMAT CSV);
 
-create view customer as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/customer.csv');
-create view customer as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/customer.parquet');
-create view customer as select * from customer;
+create view partsupp as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/partsupp.csv');
+create view partsupp as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/partsupp.parquet');
+create view partsupp as select * from partsupp;
 
 create view orders_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf100/orders.csv');
 create view orders_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf100/orders.parquet');
