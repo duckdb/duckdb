c0daa79669a2cc5a5e850e386a88ec8f728c7944
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-07-09 15:07:29 +0200

    change table names in sql files back to lineitem, order, store_sales, inventory

--- benchmark/large/ingestion/tpcds/load_tpcds_sf10.sql
+++ benchmark/large/ingestion/tpcds/load_tpcds_sf10.sql
@@ -2,10 +2,10 @@ CALL dsdgen(sf=10);
 EXPORT DATABASE 'duckdb_benchmark_data/tpcds_parquet_sf10' (FORMAT PARQUET);
 EXPORT DATABASE 'duckdb_benchmark_data/tpcds_csv_sf10' (FORMAT CSV);
 
-create or replace view catalog_sales_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf10/catalog_sales.csv');
-create or replace view catalog_sales_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf10/catalog_sales.parquet');
-create or replace view catalog_sales_native as select * from catalog_sales;
+create or replace view store_sales_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf10/store_sales.csv');
+create or replace view store_sales_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf10/store_sales.parquet');
+create or replace view store_sales_native as select * from store_sales;
 
-create or replace view store_returns_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf10/store_returns.csv');
-create or replace view store_returns_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf10/store_returns.parquet');
-create or replace view store_returns_native as select * from store_returns;
+create or replace view inventory_csv as select * from read_csv('duckdb_benchmark_data/tpcds_csv_sf10/inventory.csv');
+create or replace view inventory_parquet as select * from read_parquet('duckdb_benchmark_data/tpcds_parquet_sf10/inventory.parquet');
+create or replace view inventory_native as select * from inventory;
--- benchmark/large/ingestion/tpch/load_tpch_sf10.sql
+++ benchmark/large/ingestion/tpch/load_tpch_sf10.sql
@@ -2,10 +2,10 @@ CALL dbgen(sf=10);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_parquet_sf10' (FORMAT PARQUET);
 EXPORT DATABASE 'duckdb_benchmark_data/tpch_csv_sf10' (FORMAT CSV);
 
-create view partsupp_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf10/partsupp.csv');
-create view partsupp_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf10/partsupp.parquet');
-create view partsupp_native as select * from partsupp;
+create view lineitem_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf10/lineitem.csv');
+create view lineitem_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf10/lineitem.parquet');
+create view lineitem_native as select * from lineitem;
 
-create view part_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf10/part.csv');
-create view part_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf10/part.parquet');
-create view part_native as select * from part;
+create view order_csv as select * from read_csv('duckdb_benchmark_data/tpch_csv_sf10/order.csv');
+create view order_parquet as select * from read_parquet('duckdb_benchmark_data/tpch_parquet_sf10/order.parquet');
+create view order_native as select * from order;
