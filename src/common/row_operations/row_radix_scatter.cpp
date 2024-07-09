979f73e14adfc7ff9cf2abb07b11eaa0305b488b
Laurens Kuiper <laurens.kuiper@cwi.nl>
2024-07-09 10:43:49 +0200

    fix array radix serialization

--- src/common/row_operations/row_radix_scatter.cpp
+++ src/common/row_operations/row_radix_scatter.cpp
@@ -146,9 +146,7 @@ void RadixScatterListVector(Vector &v, UnifiedVectorFormat &vdata, const Selecti
 				memset(key_location, '\0', width - 1);
 				key_location += width - 1;
 			}
-#ifdef DEBUG
 			D_ASSERT(key_location == key_location_start + width);
-#endif
 		}
 	} else {
 		for (idx_t i = 0; i < add_count; i++) {
@@ -172,14 +170,11 @@ void RadixScatterListVector(Vector &v, UnifiedVectorFormat &vdata, const Selecti
 			}
 			// invert bits if desc
 			if (desc) {
-				// skip over validity byte, handled by nulls first/last
-				for (key_location = key_location_start + 1; key_location < key_location_start + width; key_location++) {
+				for (key_location = key_location_start; key_location < key_location_start + width; key_location++) {
 					*key_location = ~*key_location;
 				}
 			}
-#ifdef DEBUG
 			D_ASSERT(key_location == key_location_start + width);
-#endif
 		}
 	}
 }
@@ -187,7 +182,9 @@ void RadixScatterListVector(Vector &v, UnifiedVectorFormat &vdata, const Selecti
 void RadixScatterArrayVector(Vector &v, UnifiedVectorFormat &vdata, idx_t vcount, const SelectionVector &sel,
                              idx_t add_count, data_ptr_t *key_locations, const bool desc, const bool has_null,
                              const bool nulls_first, const idx_t prefix_len, idx_t width, const idx_t offset) {
-	// serialize null values
+	auto &child_vector = ArrayVector::GetEntry(v);
+	auto array_size = ArrayType::GetSize(v.GetType());
+
 	if (has_null) {
 		auto &validity = vdata.validity;
 		const data_t valid = nulls_first ? 1 : 0;
@@ -196,33 +193,48 @@ void RadixScatterArrayVector(Vector &v, UnifiedVectorFormat &vdata, idx_t vcount
 		for (idx_t i = 0; i < add_count; i++) {
 			auto idx = sel.get_index(i);
 			auto source_idx = vdata.sel->get_index(idx) + offset;
-			// write validity and according value
+			data_ptr_t &key_location = key_locations[i];
+			const data_ptr_t key_location_start = key_location;
+
 			if (validity.RowIsValid(source_idx)) {
-				key_locations[i][0] = valid;
+				*key_location++ = valid;
+
+				auto array_offset = source_idx * array_size;
+				RowOperations::RadixScatter(child_vector, array_size, *FlatVector::IncrementalSelectionVector(), 1,
+				                            key_locations + i, false, true, false, prefix_len, width - 1, array_offset);
+
+				// invert bits if desc
+				if (desc) {
+					// skip over validity byte, handled by nulls first/last
+					for (key_location = key_location_start + 1; key_location < key_location_start + width;
+					     key_location++) {
+						*key_location = ~*key_location;
+					}
+				}
 			} else {
-				key_locations[i][0] = invalid;
+				*key_location++ = invalid;
+				memset(key_location, '\0', width - 1);
+				key_location += width - 1;
 			}
-			key_locations[i]++;
+			D_ASSERT(key_location == key_location_start + width);
 		}
-		width--;
-	}
+	} else {
+		for (idx_t i = 0; i < add_count; i++) {
+			auto idx = sel.get_index(i);
+			auto source_idx = vdata.sel->get_index(idx) + offset;
+			data_ptr_t &key_location = key_locations[i];
+			const data_ptr_t key_location_start = key_location;
 
-	// serialize the inner child
-	auto &child_vector = ArrayVector::GetEntry(v);
-	auto array_size = ArrayType::GetSize(v.GetType());
-	for (idx_t i = 0; i < add_count; i++) {
-		auto idx = sel.get_index(i);
-		auto source_idx = vdata.sel->get_index(idx) + offset;
-		auto array_offset = source_idx * array_size;
-		data_ptr_t key_location = key_locations[i];
-
-		RowOperations::RadixScatter(child_vector, array_size, *FlatVector::IncrementalSelectionVector(), 1,
-		                            key_locations + i, false, true, false, prefix_len, width - 1, array_offset);
-		// invert bits if desc
-		if (desc) {
-			for (idx_t s = 0; s < width; s++) {
-				*(key_location + s) = ~*(key_location + s);
+			auto array_offset = source_idx * array_size;
+			RowOperations::RadixScatter(child_vector, array_size, *FlatVector::IncrementalSelectionVector(), 1,
+			                            key_locations + i, false, true, false, prefix_len, width, array_offset);
+			// invert bits if desc
+			if (desc) {
+				for (key_location = key_location_start; key_location < key_location_start + width; key_location++) {
+					*key_location = ~*key_location;
+				}
 			}
+			D_ASSERT(key_location == key_location_start + width);
 		}
 	}
 }
@@ -266,6 +278,14 @@ void RadixScatterStructVector(Vector &v, UnifiedVectorFormat &vdata, idx_t vcoun
 void RowOperations::RadixScatter(Vector &v, idx_t vcount, const SelectionVector &sel, idx_t ser_count,
                                  data_ptr_t *key_locations, bool desc, bool has_null, bool nulls_first,
                                  idx_t prefix_len, idx_t width, idx_t offset) {
+#ifdef DEBUG
+	// initialize to verify written width later
+	auto key_locations_copy = make_uniq_array<data_ptr_t>(ser_count);
+	for (idx_t i = 0; i < ser_count; i++) {
+		key_locations_copy[i] = key_locations[i];
+	}
+#endif
+
 	UnifiedVectorFormat vdata;
 	v.ToUnifiedFormat(vcount, vdata);
 	switch (v.GetType().InternalType()) {
@@ -327,6 +347,12 @@ void RowOperations::RadixScatter(Vector &v, idx_t vcount, const SelectionVector 
 	default:
 		throw NotImplementedException("Cannot ORDER BY column with type %s", v.GetType().ToString());
 	}
+
+#ifdef DEBUG
+	for (idx_t i = 0; i < ser_count; i++) {
+		D_ASSERT(key_locations[i] == key_locations_copy[i] + width);
+	}
+#endif
 }
 
 } // namespace duckdb
