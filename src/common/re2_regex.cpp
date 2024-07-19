b0fb83f0663a633f8b45e86f420815b245ac95bc
Christina Sioula <chrisiou.myk@gmail.com>
2024-07-19 17:56:48 +0200

    fix RegexFindAll for empty string regexp and prevent segfault in RE2::Match()

--- src/common/re2_regex.cpp
+++ src/common/re2_regex.cpp
@@ -13,50 +13,57 @@ Regex::Regex(const std::string &pattern, RegexOptions options) {
 	regex = duckdb::make_shared_ptr<duckdb_re2::RE2>(StringPiece(pattern), o);
 }
 
-bool RegexSearchInternal(const char *input, Match &match, const Regex &r, RE2::Anchor anchor, size_t start,
-                         size_t end) {
-	auto &regex = r.GetRegex();
+bool RegexSearchInternal(const char *input_data, size_t input_size, Match &match, const RE2 &regex, RE2::Anchor anchor,
+                         size_t start, size_t end) {
 	duckdb::vector<StringPiece> target_groups;
 	auto group_count = duckdb::UnsafeNumericCast<size_t>(regex.NumberOfCapturingGroups() + 1);
 	target_groups.resize(group_count);
 	match.groups.clear();
-	if (!regex.Match(StringPiece(input), start, end, anchor, target_groups.data(),
+	if (!regex.Match(StringPiece(input_data, input_size), start, end, anchor, target_groups.data(),
 	                 duckdb::UnsafeNumericCast<int>(group_count))) {
 		return false;
 	}
 	for (auto &group : target_groups) {
 		GroupMatch group_match;
 		group_match.text = group.ToString();
-		group_match.position = group.data() != nullptr ? duckdb::NumericCast<uint32_t>(group.data() - input) : 0;
+		group_match.position = group.data() != nullptr ? duckdb::NumericCast<uint32_t>(group.data() - input_data) : 0;
 		match.groups.emplace_back(group_match);
 	}
 	return true;
 }
 
 bool RegexSearch(const std::string &input, Match &match, const Regex &regex) {
-	return RegexSearchInternal(input.c_str(), match, regex, RE2::UNANCHORED, 0, input.size());
+	auto input_sz = input.size();
+	return RegexSearchInternal(input.c_str(), input_sz, match, regex.GetRegex(), RE2::UNANCHORED, 0, input_sz);
 }
 
 bool RegexMatch(const std::string &input, Match &match, const Regex &regex) {
-	return RegexSearchInternal(input.c_str(), match, regex, RE2::ANCHOR_BOTH, 0, input.size());
+	auto input_sz = input.size();
+	return RegexSearchInternal(input.c_str(), input_sz, match, regex.GetRegex(), RE2::ANCHOR_BOTH, 0, input_sz);
 }
 
 bool RegexMatch(const char *start, const char *end, Match &match, const Regex &regex) {
-	return RegexSearchInternal(start, match, regex, RE2::ANCHOR_BOTH, 0,
-	                           duckdb::UnsafeNumericCast<size_t>(end - start));
+	auto sz = duckdb::UnsafeNumericCast<size_t>(end - start);
+	return RegexSearchInternal(start, sz, match, regex.GetRegex(), RE2::ANCHOR_BOTH, 0, sz);
 }
 
 bool RegexMatch(const std::string &input, const Regex &regex) {
 	Match nop_match;
-	return RegexSearchInternal(input.c_str(), nop_match, regex, RE2::ANCHOR_BOTH, 0, input.size());
+	auto input_sz = input.size();
+	return RegexSearchInternal(input.c_str(), input_sz, nop_match, regex.GetRegex(), RE2::ANCHOR_BOTH, 0, input_sz);
 }
 
 duckdb::vector<Match> RegexFindAll(const std::string &input, const Regex &regex) {
+	return RegexFindAll(input.c_str(), input.size(), regex.GetRegex());
+}
+
+duckdb::vector<Match> RegexFindAll(const char *input_data, size_t input_size, const RE2 &regex) {
 	duckdb::vector<Match> matches;
 	size_t position = 0;
 	Match match;
-	while (RegexSearchInternal(input.c_str(), match, regex, RE2::UNANCHORED, position, input.size())) {
-		position += match.position(0) + match.length(0);
+	while (RegexSearchInternal(input_data, input_size, match, regex, RE2::UNANCHORED, position, input_size)) {
+		auto match_length = (match.length(0)) : match.length(0) ? 1;
+		position = match.position(0) + match_length;
 		matches.emplace_back(match);
 	}
 	return matches;
