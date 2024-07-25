f9df59df38ddba6bc931c18e7d29dc858ea71ac6
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-07-25 08:48:30 +0200

    Apply format fix on clang-tidy-fix.py

--- scripts/clang-tidy-diff.py
+++ scripts/clang-tidy-diff.py
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
 # ===-----------------------------------------------------------------------===#
- 
+
 r"""
 ClangTidy Diff Checker
 ======================
@@ -22,7 +22,7 @@ Example usage for git/svn users:
       clang-tidy-diff.py -fix -checks=-*,modernize-use-override
  
 """
- 
+
 import argparse
 import glob
 import json
@@ -35,33 +35,31 @@ import sys
 import tempfile
 import threading
 import traceback
- 
+
 try:
     import yaml
 except ImportError:
     yaml = None
- 
+
 is_py2 = sys.version[0] == "2"
- 
+
 if is_py2:
     import Queue as queue
 else:
     import queue as queue
- 
- 
+
+
 def run_tidy(task_queue, lock, timeout, failed_files):
     watchdog = None
     while True:
         command = task_queue.get()
         try:
-            proc = subprocess.Popen(
-                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-            )
- 
+            proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
             if timeout is not None:
                 watchdog = threading.Timer(timeout, proc.kill)
                 watchdog.start()
- 
+
             stdout, stderr = proc.communicate()
             if proc.returncode != 0:
                 if proc.returncode < 0:
@@ -71,7 +69,7 @@ def run_tidy(task_queue, lock, timeout, failed_files):
                     )
                     stderr += msg.encode("utf-8")
                 failed_files.append(command)
- 
+
             with lock:
                 sys.stdout.write(stdout.decode("utf-8") + "\n")
                 sys.stdout.flush()
@@ -85,20 +83,18 @@ def run_tidy(task_queue, lock, timeout, failed_files):
             with lock:
                 if not (timeout is None or watchdog is None):
                     if not watchdog.is_alive():
-                        sys.stderr.write(
-                            "Terminated by timeout: " + " ".join(command) + "\n"
-                        )
+                        sys.stderr.write("Terminated by timeout: " + " ".join(command) + "\n")
                     watchdog.cancel()
             task_queue.task_done()
- 
- 
+
+
 def start_workers(max_tasks, tidy_caller, arguments):
     for _ in range(max_tasks):
         t = threading.Thread(target=tidy_caller, args=arguments)
         t.daemon = True
         t.start()
- 
- 
+
+
 def merge_replacement_files(tmpdir, mergefile):
     """Merge all replacement files in a directory into a single file"""
     # The fixes suggested by clang-tidy >= 4.0.0 are given under
@@ -110,7 +106,7 @@ def merge_replacement_files(tmpdir, mergefile):
         if not content:
             continue  # Skip empty files.
         merged.extend(content.get(mergekey, []))
- 
+
     if merged:
         # MainSourceFile: The key is required by the definition inside
         # include/clang/Tooling/ReplacementsYaml.h, but the value
@@ -122,13 +118,11 @@ def merge_replacement_files(tmpdir, mergefile):
     else:
         # Empty the file:
         open(mergefile, "w").close()
- 
- 
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description="Run clang-tidy against changed files, and "
-        "output diagnostics only for modified "
-        "lines."
+        description="Run clang-tidy against changed files, and " "output diagnostics only for modified " "lines."
     )
     parser.add_argument(
         "-clang-tidy-binary",
@@ -146,15 +140,13 @@ def main():
         "-regex",
         metavar="PATTERN",
         default=None,
-        help="custom pattern selecting file paths to check "
-        "(case sensitive, overrides -iregex)",
+        help="custom pattern selecting file paths to check " "(case sensitive, overrides -iregex)",
     )
     parser.add_argument(
         "-iregex",
         metavar="PATTERN",
         default=r".*\.(cpp|cc|c\+\+|cxx|c|cl|h|hpp|m|mm|inc)",
-        help="custom pattern selecting file paths to check "
-        "(case insensitive, overridden by -regex)",
+        help="custom pattern selecting file paths to check " "(case insensitive, overridden by -regex)",
     )
     parser.add_argument(
         "-j",
@@ -162,12 +154,8 @@ def main():
         default=1,
         help="number of tidy instances to be run in parallel.",
     )
-    parser.add_argument(
-        "-timeout", type=int, default=None, help="timeout per each file in seconds."
-    )
-    parser.add_argument(
-        "-fix", action="store_true", default=False, help="apply suggested fixes"
-    )
+    parser.add_argument("-timeout", type=int, default=None, help="timeout per each file in seconds.")
+    parser.add_argument("-fix", action="store_true", default=False, help="apply suggested fixes")
     parser.add_argument(
         "-checks",
         help="checks filter, when not specified, use clang-tidy " "default",
@@ -180,9 +168,7 @@ def main():
         default="",
     )
     parser.add_argument("-use-color", action="store_true", help="Use colors in output")
-    parser.add_argument(
-        "-path", dest="build_path", help="Path used to read a compile command database."
-    )
+    parser.add_argument("-path", dest="build_path", help="Path used to read a compile command database.")
     if yaml:
         parser.add_argument(
             "-export-fixes",
@@ -234,15 +220,15 @@ def main():
         action="store_true",
         help="Allow empty enabled checks.",
     )
- 
+
     clang_tidy_args = []
     argv = sys.argv[1:]
     if "--" in argv:
         clang_tidy_args.extend(argv[argv.index("--") :])
         argv = argv[: argv.index("--")]
- 
+
     args = parser.parse_args(argv)
- 
+
     # Extract changed lines for each file.
     filename = None
     lines_by_file = {}
@@ -252,14 +238,14 @@ def main():
             filename = match.group(2)
         if filename is None:
             continue
- 
+
         if args.regex is not None:
             if not re.match("^%s$" % args.regex, filename):
                 continue
         else:
             if not re.match("^%s$" % args.iregex, filename, re.IGNORECASE):
                 continue
- 
+
         match = re.search(r"^@@.*\+(\d+)(,(\d+))?", line)
         if match:
             start_line = int(match.group(1))
@@ -270,54 +256,50 @@ def main():
                 continue
             end_line = start_line + line_count - 1
             lines_by_file.setdefault(filename, []).append([start_line, end_line])
- 
+
     if not any(lines_by_file):
         print("No relevant changes found.")
         sys.exit(0)
- 
+
     max_task_count = args.j
     if max_task_count == 0:
         max_task_count = multiprocessing.cpu_count()
     max_task_count = min(len(lines_by_file), max_task_count)
- 
+
     combine_fixes = False
     export_fixes_dir = None
     delete_fixes_dir = False
     if args.export_fixes is not None:
         # if a directory is given, create it if it does not exist
-        if args.export_fixes.endswith(os.path.sep) and not os.path.isdir(
-            args.export_fixes
-        ):
+        if args.export_fixes.endswith(os.path.sep) and not os.path.isdir(args.export_fixes):
             os.makedirs(args.export_fixes)
- 
+
         if not os.path.isdir(args.export_fixes):
             if not yaml:
                 raise RuntimeError(
                     "Cannot combine fixes in one yaml file. Either install PyYAML or specify an output directory."
                 )
- 
+
             combine_fixes = True
- 
+
         if os.path.isdir(args.export_fixes):
             export_fixes_dir = args.export_fixes
- 
+
     if combine_fixes:
         export_fixes_dir = tempfile.mkdtemp()
         delete_fixes_dir = True
- 
+
     # Tasks for clang-tidy.
     task_queue = queue.Queue(max_task_count)
     # A lock for console output.
     lock = threading.Lock()
- 
+
     # List of files with a non-zero return code.
     failed_files = []
- 
+
     # Run a pool of clang-tidy workers.
-    start_workers(
-        max_task_count, run_tidy, (task_queue, lock, args.timeout, failed_files)
-    )
- 
+    start_workers(max_task_count, run_tidy, (task_queue, lock, args.timeout, failed_files))
+
     # Form the common args list.
     common_clang_tidy_args = []
     if args.fix:
@@ -340,12 +322,10 @@ def main():
         common_clang_tidy_args.append("-extra-arg-before=%s" % arg)
     for plugin in args.plugins:
         common_clang_tidy_args.append("-load=%s" % plugin)
- 
+
     for name in lines_by_file:
-        line_filter_json = json.dumps(
-            [{"name": name, "lines": lines_by_file[name]}], separators=(",", ":")
-        )
- 
+        line_filter_json = json.dumps([{"name": name, "lines": lines_by_file[name]}], separators=(",", ":"))
+
         # Run clang-tidy on files containing changes.
         command = [args.clang_tidy_binary]
         command.append("-line-filter=" + line_filter_json)
@@ -358,19 +338,19 @@ def main():
         command.extend(common_clang_tidy_args)
         command.append(name)
         command.extend(clang_tidy_args)
- 
+
         task_queue.put(command)
- 
+
     # Application return code
     return_code = 0
- 
+
     # Wait for all threads to be done.
     task_queue.join()
     # Application return code
     return_code = 0
     if failed_files:
         return_code = 1
- 
+
     if combine_fixes:
         print("Writing fixes to " + args.export_fixes + " ...")
         try:
@@ -379,11 +359,11 @@ def main():
             sys.stderr.write("Error exporting fixes.\n")
             traceback.print_exc()
             return_code = 1
- 
+
     if delete_fixes_dir:
         shutil.rmtree(export_fixes_dir)
     sys.exit(return_code)
- 
- 
+
+
 if __name__ == "__main__":
     main()
