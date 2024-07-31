8eb762e25cf1f824f5a73affb0993105089caf4f
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-07-31 13:16:17 +0200

    fix query and answer tpcds 22

--- extension/tpcds/dsdgen/queries/22.sql
+++ extension/tpcds/dsdgen/queries/22.sql
@@ -10,9 +10,5 @@ WHERE inv_date_sk=d_date_sk
   AND inv_item_sk=i_item_sk
   AND d_month_seq BETWEEN 1200 AND 1200 + 11
 GROUP BY rollup(i_product_name ,i_brand ,i_class ,i_category)
-ORDER BY qoh NULLS FIRST,
-         i_product_name NULLS FIRST,
-         i_brand NULLS FIRST,
-         i_class NULLS FIRST,
-         i_category NULLS FIRST
+ORDER BY ALL
 LIMIT 100;
