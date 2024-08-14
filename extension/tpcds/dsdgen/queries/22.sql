208deb49105c43abd5062a0f6f430e5992d4726a
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-08-14 10:44:26 +0200

    Revert "fix query and answer tpcds 22"
    This reverts commit 8eb762e25cf1f824f5a73affb0993105089caf4f.

--- extension/tpcds/dsdgen/queries/22.sql
+++ extension/tpcds/dsdgen/queries/22.sql
@@ -10,5 +10,9 @@ WHERE inv_date_sk=d_date_sk
   AND inv_item_sk=i_item_sk
   AND d_month_seq BETWEEN 1200 AND 1200 + 11
 GROUP BY rollup(i_product_name ,i_brand ,i_class ,i_category)
-ORDER BY ALL
+ORDER BY qoh NULLS FIRST,
+         i_product_name NULLS FIRST,
+         i_brand NULLS FIRST,
+         i_class NULLS FIRST,
+         i_category NULLS FIRST
 LIMIT 100;
