b0ca81a00954616a3ed7a178cfc92121ba3da7cd
Pedro Holanda <pedroholanda@gmail.com>
2024-08-16 15:54:41 +0200

    More tests

--- test/arrow/arrow_roundtrip.cpp
+++ test/arrow/arrow_roundtrip.cpp
@@ -91,7 +91,7 @@ TEST_CASE("Test Arrow Extension Types", "[arrow][.]") {
 	TestArrowRoundtrip("SELECT '2d89ebe6-1e13-47e5-803a-b81c87660b66'::UUID str FROM range(5) tbl(i)");
 
 	// JSON
-	TestArrowRoundtrip("SELECT '{\"name\":\"John\", \"age\":28, \"car\":\"VW Fox\"}'::JSON str FROM range(5) tbl(i)");
+	TestArrowRoundtrip("SELECT '{\"name\":\"Pedro\", \"age\":28, \"car\":\"VW Fox\"}'::JSON str FROM range(5) tbl(i)");
 }
 
 TEST_CASE("Test Arrow String View", "[arrow][.]") {
--- tools/pythonpkg/tests/fast/arrow/test_canonical_extensions.py
+++ tools/pythonpkg/tests/fast/arrow/test_canonical_extensions.py
@@ -31,7 +31,35 @@ class TestCanonicalExtensionTypes(object):
 
         arrow_table = pa.Table.from_arrays([storage_array], names=['uuid_col'])
 
-        con = duckdb.connect()
-        duck_arrow = con.execute('FROM arrow_table').arrow()
+        duck_arrow = duckdb_cursor.execute('FROM arrow_table').arrow()
 
         assert duck_arrow.equals(arrow_table)
+
+        pa.unregister_extension_type("arrow.uuid")
+
+    def test_uuid_exception(self, duckdb_cursor):
+        class UuidTypeWrong(pa.ExtensionType):
+            def __init__(self):
+                pa.ExtensionType.__init__(self, pa.binary(4), "arrow.uuid")
+
+            def __arrow_ext_serialize__(self):
+                # since we don't have a parameterized type, we don't need extra
+                # metadata to be deserialized
+                return b''
+
+            @classmethod
+            def __arrow_ext_deserialize__(self, storage_type, serialized):
+                # return an instance of this subclass given the serialized
+                # metadata.
+                return UuidTypeWrong()
+
+        pa.register_extension_type(UuidTypeWrong())
+
+        storage_array = pa.array(['aaaa'], pa.binary(4))
+        uuid_type = UuidTypeWrong()
+        storage_array = uuid_type.wrap_array(storage_array)
+
+        arrow_table = pa.Table.from_arrays([storage_array], names=['uuid_col'])
+
+        with pytest.raises(duckdb.InvalidInputException, match="arrow.uuid must be a fixed-size binary of 16 bytes"):
+            duck_arrow = duckdb_cursor.execute('FROM arrow_table').arrow()
