7172377296c43fc84092cad70bacfef54f03124c
Tishj <t_b@live.nl>
2024-08-17 20:59:21 +0200

    check that the stream result is open when fetching

--- tools/pythonpkg/src/pyresult.cpp
+++ tools/pythonpkg/src/pyresult.cpp
@@ -22,8 +22,19 @@
 
 namespace duckdb {
 
-DuckDBPyResult::DuckDBPyResult(unique_ptr<QueryResult> result_p) : result(std::move(result_p)) {
+static bool ResultIsOpen(optional_ptr<QueryResult> result) {
 	if (!result) {
+		return false;
+	}
+	if (result->type != QueryResultType::STREAM_RESULT) {
+		return true;
+	}
+	auto &stream_result = result->Cast<StreamQueryResult>();
+	return stream_result.IsOpen();
+}
+
+DuckDBPyResult::DuckDBPyResult(unique_ptr<QueryResult> result_p) : result(std::move(result_p)) {
+	if (!ResultIsOpen(result.get())) {
 		throw InternalException("PyResult created without a result object");
 	}
 }
@@ -38,21 +49,21 @@ DuckDBPyResult::~DuckDBPyResult() {
 }
 
 const vector<string> &DuckDBPyResult::GetNames() {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InternalException("Calling GetNames without a result object");
 	}
 	return result->names;
 }
 
 const vector<LogicalType> &DuckDBPyResult::GetTypes() {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InternalException("Calling GetTypes without a result object");
 	}
 	return result->types;
 }
 
 unique_ptr<DataChunk> DuckDBPyResult::FetchChunk() {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InternalException("FetchChunk called without a result object");
 	}
 	return FetchNext(*result);
@@ -109,7 +120,7 @@ unique_ptr<DataChunk> DuckDBPyResult::FetchNextRaw(QueryResult &query_result) {
 Optional<py::tuple> DuckDBPyResult::Fetchone() {
 	{
 		py::gil_scoped_release release;
-		if (!result) {
+		if (!ResultIsOpen(result.get())) {
 			throw InvalidInputException("result closed");
 		}
 		if (!current_chunk || chunk_offset >= current_chunk->size()) {
@@ -200,7 +211,7 @@ void InsertCategory(QueryResult &result, unordered_map<idx_t, py::list> &categor
 }
 
 unique_ptr<NumpyResultConversion> DuckDBPyResult::InitializeNumpyConversion(bool pandas) {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InvalidInputException("result closed");
 	}
 
@@ -218,7 +229,7 @@ unique_ptr<NumpyResultConversion> DuckDBPyResult::InitializeNumpyConversion(bool
 
 py::dict DuckDBPyResult::FetchNumpyInternal(bool stream, idx_t vectors_per_chunk,
                                             unique_ptr<NumpyResultConversion> conversion_p) {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InvalidInputException("result closed");
 	}
 	if (!conversion_p) {
@@ -355,7 +366,7 @@ bool DuckDBPyResult::FetchArrowChunk(ChunkScanState &scan_state, py::list &batch
 }
 
 py::list DuckDBPyResult::FetchAllArrowChunks(idx_t rows_per_batch, bool to_polars) {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InvalidInputException("result closed");
 	}
 	auto pyarrow_lib_module = py::module::import("pyarrow").attr("lib");
@@ -368,7 +379,7 @@ py::list DuckDBPyResult::FetchAllArrowChunks(idx_t rows_per_batch, bool to_polar
 }
 
 duckdb::pyarrow::Table DuckDBPyResult::FetchArrowTable(idx_t rows_per_batch, bool to_polars) {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InvalidInputException("There is no query result");
 	}
 	auto names = result->names;
@@ -380,7 +391,7 @@ duckdb::pyarrow::Table DuckDBPyResult::FetchArrowTable(idx_t rows_per_batch, boo
 }
 
 ArrowArrayStream DuckDBPyResult::FetchArrowArrayStream(idx_t rows_per_batch) {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InvalidInputException("There is no query result");
 	}
 	ResultArrowArrayStreamWrapper *result_stream = new ResultArrowArrayStreamWrapper(std::move(result), rows_per_batch);
@@ -390,7 +401,7 @@ ArrowArrayStream DuckDBPyResult::FetchArrowArrayStream(idx_t rows_per_batch) {
 }
 
 duckdb::pyarrow::RecordBatchReader DuckDBPyResult::FetchRecordBatchReader(idx_t rows_per_batch) {
-	if (!result) {
+	if (!ResultIsOpen(result.get())) {
 		throw InvalidInputException("There is no query result");
 	}
 	py::gil_scoped_acquire acquire;
---
+++ tools/pythonpkg/tests/fast/udf/test_transactionality.py
@@ -0,0 +1,17 @@
+import duckdb
+import pytest
+
+
+class TestUDFTransactionality(object):
+    def test_type_coverage(self, duckdb_cursor):
+        rel = duckdb_cursor.sql("select * from range(4096)")
+        res = rel.fetchone()
+        assert res == (0,)
+
+        def my_func(x: str) -> int:
+            return int(x)
+
+        duckdb_cursor.create_function('test', my_func)
+
+        with pytest.raises(duckdb.InvalidInputException, match='result closed'):
+            res = rel.fetchone()
