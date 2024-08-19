aef9dd1a84a3af7bdb9763fd6214a44aeb8c3226
Khalid Mammadov <xmamedov@hotmail.com>
2024-08-19 19:56:14 +0000

    draft

--- tools/pythonpkg/src/pyrelation.cpp
+++ tools/pythonpkg/src/pyrelation.cpp
@@ -1043,7 +1043,8 @@ struct SupportedPythonJoinType {
 static const SupportedPythonJoinType *GetSupportedJoinTypes(idx_t &length) {
 	static const SupportedPythonJoinType SUPPORTED_TYPES[] = {{"left", JoinType::LEFT},   {"right", JoinType::RIGHT},
 	                                                          {"outer", JoinType::OUTER}, {"semi", JoinType::SEMI},
-	                                                          {"inner", JoinType::INNER}, {"anti", JoinType::ANTI}};
+	                                                          {"inner", JoinType::INNER}, {"anti", JoinType::ANTI},
+															  {"cross", JoinType::CROSS}};
 	static const auto SUPPORTED_TYPES_COUNT = sizeof(SUPPORTED_TYPES) / sizeof(SupportedPythonJoinType);
 	length = SUPPORTED_TYPES_COUNT;
 	return reinterpret_cast<const SupportedPythonJoinType *>(SUPPORTED_TYPES);
