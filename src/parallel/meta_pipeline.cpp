c8efabb8584fec85865211ec3f2453aedb7c95c9
Laurens Kuiper <laurens.kuiper@cwi.nl>
2024-08-21 11:17:44 +0200

    nope, have to use thread count, not cardinality

--- src/parallel/meta_pipeline.cpp
+++ src/parallel/meta_pipeline.cpp
@@ -145,14 +145,12 @@ vector<shared_ptr<Pipeline>> MetaPipeline::AddDependenciesFrom(Pipeline &dependa
 	return created_pipelines;
 }
 
-static bool PipelinesExceedCardinalityThreshold(Pipeline &dependant, Pipeline &dependancy,
-                                                const idx_t cardinality_threshold) {
+static bool PipelineExceedsThreadCount(Pipeline &pipeline, const idx_t thread_count) {
 #ifdef DEBUG
 	// we always add the dependency in debug mode so that this is well-tested
 	return true;
 #else
-	return dependant.GetSource()->estimated_cardinality > cardinality_threshold &&
-	       dependancy.GetSource()->estimated_cardinality > cardinality_threshold;
+	return pipeline.GetSource()->EstimatedThreadCount() > thread_count;
 #endif
 }
 
@@ -176,15 +174,18 @@ void MetaPipeline::AddRecursiveDependencies(const vector<shared_ptr<Pipeline>> &
 
 	// we try to limit the performance impact of these dependencies on smaller workloads,
 	// by only adding the dependencies if the source operator can likely keep all threads busy
-	const auto num_threads = NumericCast<idx_t>(TaskScheduler::GetScheduler(executor.context).NumberOfThreads());
-	const auto cardinality_threshold = num_threads * Storage::ROW_GROUP_SIZE;
+	const auto thread_count = NumericCast<idx_t>(TaskScheduler::GetScheduler(executor.context).NumberOfThreads());
 	for (; it != child_meta_pipelines.end(); it++) {
 		for (auto &pipeline : it->get()->pipelines) {
+			if (!PipelineExceedsThreadCount(*pipeline, thread_count)) {
+				continue;
+			}
 			auto &pipeline_deps = pipeline_dependencies[*pipeline];
 			for (auto &new_dependency : new_dependencies) {
-				if (PipelinesExceedCardinalityThreshold(*pipeline, *new_dependency, cardinality_threshold)) {
-					pipeline_deps.push_back(*new_dependency);
+				if (!PipelineExceedsThreadCount(*new_dependency, thread_count)) {
+					continue;
 				}
+				pipeline_deps.push_back(*new_dependency);
 			}
 		}
 	}
