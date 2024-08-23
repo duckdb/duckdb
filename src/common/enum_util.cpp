b66b47b9606a7be5f82ac17fcd96a866e409c508
khalidmammadov <khalidmammadov9@gmail.com>
2024-08-23 09:03:07 +0000

    Fix enum gen

--- src/common/enum_util.cpp
+++ src/common/enum_util.cpp
@@ -3468,6 +3468,8 @@ const char* EnumUtil::ToChars<JoinType>(JoinType value) {
 		return "RIGHT_SEMI";
 	case JoinType::RIGHT_ANTI:
 		return "RIGHT_ANTI";
+	case JoinType::CROSS:
+		return "CROSS";
 	default:
 		throw NotImplementedException(StringUtil::Format("Enum value: '%d' not implemented", value));
 	}
@@ -3507,6 +3509,9 @@ JoinType EnumUtil::FromString<JoinType>(const char *value) {
 	}
 	if (StringUtil::Equals(value, "RIGHT_ANTI")) {
 		return JoinType::RIGHT_ANTI;
+	}
+	if (StringUtil::Equals(value, "CROSS")) {
+		return JoinType::CROSS;
 	}
 	throw NotImplementedException(StringUtil::Format("Enum value: '%s' not implemented", value));
 }
