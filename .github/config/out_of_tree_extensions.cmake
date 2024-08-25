4e4ba5c57e952f23b63fe6386a25e521d249782a
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-08-25 12:10:37 +0200

    Bump substrait to latest (with no APPLY_PATCHES)

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -124,8 +124,7 @@ if (NOT WIN32)
     duckdb_extension_load(substrait
             LOAD_TESTS DONT_LINK
             GIT_URL https://github.com/duckdb/substrait
-            GIT_TAG 237931391ebc7e6aee7aa81052fa1411f6c4128e
-            APPLY_PATCHES
+            GIT_TAG 55922a3e77756054abbe3e04dae17ccf4203ad6f
             )
 endif()
 
