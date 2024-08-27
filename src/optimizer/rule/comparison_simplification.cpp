9aa93a4a0cfa3983b7c29b084cb459ffe503885c
Tmonster <tom@ebergen.com>
2024-08-27 09:26:37 +0200

    add debugging statements. I think problem lies in how we cast with between statements

--- src/optimizer/rule/comparison_simplification.cpp
+++ src/optimizer/rule/comparison_simplification.cpp
@@ -16,7 +16,10 @@ ComparisonSimplificationRule::ComparisonSimplificationRule(ExpressionRewriter &r
 
 unique_ptr<Expression> ComparisonSimplificationRule::Apply(LogicalOperator &op, vector<reference<Expression>> &bindings,
                                                            bool &changes_made, bool is_root) {
+
 	auto &expr = bindings[0].get().Cast<BoundComparisonExpression>();
+	Printer::Print("before");
+	expr.Print();
 	auto &constant_expr = bindings[1].get();
 	bool column_ref_left = expr.left.get() != &constant_expr;
 	auto column_ref_expr = !column_ref_left ? expr.right.get() : expr.left.get();
@@ -72,6 +75,8 @@ unique_ptr<Expression> ComparisonSimplificationRule::Apply(LogicalOperator &op, 
 			expr.right = std::move(child_expression);
 		}
 	}
+	Printer::Print("after");
+	expr.Print();
 	return nullptr;
 }
 
