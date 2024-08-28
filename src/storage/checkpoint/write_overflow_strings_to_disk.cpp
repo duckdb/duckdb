6f74ffafee1c95c7737755a3dada1bf106790e49
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-08-28 18:43:21 +0200

    Add missing include, and enable previously disabled test

--- src/storage/checkpoint/write_overflow_strings_to_disk.cpp
+++ src/storage/checkpoint/write_overflow_strings_to_disk.cpp
@@ -1,6 +1,7 @@
 #include "duckdb/storage/checkpoint/write_overflow_strings_to_disk.hpp"
 #include "duckdb/storage/block_manager.hpp"
 #include "duckdb/storage/buffer_manager.hpp"
+#include "duckdb/storage/partial_block_manager.hpp"
 
 namespace duckdb {
 
