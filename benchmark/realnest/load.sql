6d392ea9ed78a016206b95c8f4ef7f07b460fd88
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-08-28 13:05:14 +0200

    replace VIEWS with TABLES

--- benchmark/realnest/load.sql
+++ benchmark/realnest/load.sql
@@ -5,19 +5,19 @@ create table run2012B_singleMu as select * from read_json('https://github.com/cw
 create table gh_issue as select * from read_json('https://github.com/cwida/RealNest/raw/cdaa85652bf187226706b321c29597673b2b7d86/sample-data/100mib/gharchive-IssuesEvent/data.jsonl');
 create table gh_pull as select * from read_json('https://github.com/cwida/RealNest/raw/cdaa85652bf187226706b321c29597673b2b7d86/sample-data/100mib/gharchive-PullRequestEvent/data.jsonl');
 
-CREATE VIEW single_mu_lists AS SELECT * REPLACE(list_resize(Jet, 10, NULL) as Jet, list_resize(Muon, 10, NULL) as Muon, list_resize(Photon, 10, NULL) as Photon, list_resize(Tau, 10, NULL) as Tau) 
+CREATE TABLE single_mu_lists AS SELECT * REPLACE(list_resize(Jet, 10, NULL) as Jet, list_resize(Muon, 10, NULL) as Muon, list_resize(Photon, 10, NULL) as Photon, list_resize(Tau, 10, NULL) as Tau) 
 FROM run2012B_singleMu;
-CREATE VIEW singleMu as SELECT list_transform("Tau", x -> x.pt) AS tau_pt, list_transform("Tau", x -> x.eta) AS tau_eta,
+CREATE TABLE singleMu as SELECT list_transform("Tau", x -> x.pt) AS tau_pt, list_transform("Tau", x -> x.eta) AS tau_eta,
     list_transform("Jet", x -> x.pt) AS jet_pt, list_transform("Jet", x -> x.eta) AS jet_eta, 
     list_transform("Muon", x -> x.pt) AS muon_pt, list_transform("Muon", x -> x.eta) AS muon_eta, 
     list_transform("Electron", x -> x.pt) AS el_pt, list_transform("Electron", x -> x.eta) AS el_eta, 
     list_transform("Photon", x -> x.pt) AS ph_pt, list_transform("Photon", x -> x.eta) AS ph_eta
 FROM run2012B_singleMu; 
-CREATE VIEW unnested_hlt AS (SELECT rowid, UNNEST(HLT) AS hlt FROM run2012B_singleMu);
-CREATE VIEW unnested_pv AS (SELECT rowid, UNNEST(PV) AS pv FROM run2012B_singleMu);
-CREATE VIEW unnested_met AS (SELECT rowid, UNNEST(MET) AS met FROM run2012B_singleMu);
-CREATE VIEW unnested_muon AS (SELECT rowid, UNNEST(Muon, recursive:=true) AS muon FROM run2012B_singleMu);
-CREATE VIEW unnested_electron AS (SELECT rowid, UNNEST(Electron, recursive:=true) AS electron FROM run2012B_singleMu);
-CREATE VIEW unnested_tau AS (SELECT rowid, UNNEST(Tau, recursive:=true) AS tau FROM run2012B_singleMu);
-CREATE VIEW unnested_photon AS (SELECT rowid, UNNEST(Photon, recursive:=true) AS photon FROM run2012B_singleMu);
-CREATE VIEW unnested_jet AS (SELECT rowid, UNNEST(Jet, recursive:=true) AS jet FROM run2012B_singleMu);
+CREATE TABLE unnested_hlt AS (SELECT rowid, UNNEST(HLT) AS hlt FROM run2012B_singleMu);
+CREATE TABLE unnested_pv AS (SELECT rowid, UNNEST(PV) AS pv FROM run2012B_singleMu);
+CREATE TABLE unnested_met AS (SELECT rowid, UNNEST(MET) AS met FROM run2012B_singleMu);
+CREATE TABLE unnested_muon AS (SELECT rowid, UNNEST(Muon, recursive:=true) AS muon FROM run2012B_singleMu);
+CREATE TABLE unnested_electron AS (SELECT rowid, UNNEST(Electron, recursive:=true) AS electron FROM run2012B_singleMu);
+CREATE TABLE unnested_tau AS (SELECT rowid, UNNEST(Tau, recursive:=true) AS tau FROM run2012B_singleMu);
+CREATE TABLE unnested_photon AS (SELECT rowid, UNNEST(Photon, recursive:=true) AS photon FROM run2012B_singleMu);
+CREATE TABLE unnested_jet AS (SELECT rowid, UNNEST(Jet, recursive:=true) AS jet FROM run2012B_singleMu);
