103bd1ca2d52099dfc9d09c7f600f2c21dc5ee95
Tmonster <tom@ebergen.com>
2024-08-29 10:38:34 +0200

    fixup logic to check projection cast expressions

--- src/include/duckdb/optimizer/filter_pushdown.hpp
+++ src/include/duckdb/optimizer/filter_pushdown.hpp
@@ -26,7 +26,6 @@ public:
 	ClientContext &GetContext();
 
 	void CheckMarkToSemi(LogicalOperator &op, unordered_set<idx_t> &table_bindings);
-	static bool FilterInputsChangeTypes(unique_ptr<Expression> filter, vector<unique_ptr<Expression>> &expressions);
 
 	struct Filter {
 		unordered_set<idx_t> bindings;
