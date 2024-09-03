8964c09524e70c5f259097835442193fe73a25aa
Richard Wesley <13156216+hawkfish@users.noreply.github.com>
2024-09-03 09:33:13 -0700

    Internal #2907: Validate Time Zones
    Fix Python test now that we are giving DuckDB a valid TZ.
    
    fixes: duckdblabs/duckdb-internal#2907

--- tools/pythonpkg/tests/fast/pandas/test_datetime_timestamp.py
+++ tools/pythonpkg/tests/fast/pandas/test_datetime_timestamp.py
@@ -115,7 +115,7 @@ class TestDateTimeTimeStamp(object):
         df_in = pandas.DataFrame(
             {
                 0: pandas.Series(
-                    data=[datetime.datetime(year=2022, month=1, day=1, hour=13, tzinfo=timezone)], dtype='object'
+                    data=[datetime.datetime(year=2021, month=12, day=31, hour=23, tzinfo=timezone)], dtype='object'
                 )
             }
         )
