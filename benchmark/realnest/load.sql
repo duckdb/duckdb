077055a685838c77d459a8071fb3556ecb35652b
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-09-06 13:50:18 +0200

    larger data sets for open street map, github and twitter

--- benchmark/realnest/load.sql
+++ benchmark/realnest/load.sql
@@ -1,10 +1,13 @@
 create table cord as select * from read_json('https://github.com/cwida/RealNest/raw/cdaa85652bf187226706b321c29597673b2b7d86/sample-data/100mib/cord-19-document_parses/data.jsonl');
-create table open_street_map as select * from read_json('https://github.com/cwida/RealNest/raw/cdaa85652bf187226706b321c29597673b2b7d86/sample-data/100mib/daylight-openstreetmap-osm_features/data.jsonl');
-create table gh_issue as select * from read_json('https://github.com/cwida/RealNest/raw/cdaa85652bf187226706b321c29597673b2b7d86/sample-data/100mib/gharchive-IssuesEvent/data.jsonl');
-create table gh_pull as select * from read_json('https://github.com/cwida/RealNest/raw/cdaa85652bf187226706b321c29597673b2b7d86/sample-data/100mib/gharchive-PullRequestEvent/data.jsonl');
 
+attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/open_street_map_524k.duckdb' as osm (READ_ONLY);
+create table open_street_map as select * from osm;
+attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/pull_131k.duckdb' as gh_pull (READ_ONLY);
+create table gh_pull as select * from gh_pull.gh_pull;
+attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/issue_131k.duckdb' as gh_issue (READ_ONLY);
+create table gh_issue as select * from gh_issue.gh_issue;
 attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/twitter_131k.duckdb' as tw (READ_ONLY);
-create table twitter as select * from tw.twitter;
+create table twitter as select * from tw.twitter limit 5000;
 attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/singleMu_524k.duckdb' as rn_singleMu (READ_ONLY);
 create table run2012B_singleMu as select * from rn_singleMu.run2012B_singleMu;
 CREATE TABLE single_mu_lists AS SELECT * REPLACE(list_resize(Jet, 10, NULL) as Jet, list_resize(Muon, 10, NULL) as Muon, list_resize(Photon, 10, NULL) as Photon, list_resize(Tau, 10, NULL) as Tau) 
@@ -13,4 +16,8 @@ CREATE or replace TABLE singleMu as SELECT list_distinct(list_transform("Tau", x
     list_distinct(list_transform("Jet", x -> x.pt)) AS jet_pt, list_distinct(list_transform("Jet", x -> x.eta)) AS jet_eta, 
     list_distinct(list_transform("Muon", x -> x.pt)) AS muon_pt, list_distinct(list_transform("Muon", x -> x.eta)) AS muon_eta, 
     list_distinct(list_transform("Photon", x -> x.pt)) AS ph_pt, list_distinct(list_transform("Photon", x -> x.eta)) AS ph_eta
-FROM rn_singleMu.run2012B_singleMu order by all desc limit 100000; 
+FROM rn_singleMu.run2012B_singleMu order by all desc;
+UPDATE singleMu set jet_eta = list_resize(jet_eta, len(jet_pt));
+UPDATE singleMu set muon_eta = list_resize(muon_eta, len(muon_pt));
+UPDATE singleMu set ph_eta = list_resize(ph_eta, len(ph_pt));
+UPDATE singleMu set tau_eta = list_resize(tau_eta, len(tau_pt));
