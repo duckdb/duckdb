410943ba144a99e0a0dca3b9b2a5e955fd40895f
yuxuan <yuxuanzh@umich.edu>
2024-09-10 11:01:43 -0500

    format fix

--- src/function/table_function.cpp
+++ src/function/table_function.cpp
@@ -19,8 +19,8 @@ TableFunction::TableFunction(string name, vector<LogicalType> arguments, table_f
       in_out_function_final(nullptr), statistics(nullptr), dependency(nullptr), cardinality(nullptr),
       pushdown_complex_filter(nullptr), to_string(nullptr), table_scan_progress(nullptr), get_batch_index(nullptr),
       get_bind_info(nullptr), type_pushdown(nullptr), get_multi_file_reader(nullptr), supports_pushdown_type(nullptr),
-      serialize(nullptr), deserialize(nullptr), projection_pushdown(false), filter_pushdown(false),
-      filter_prune(false), sampling_pushdown(false) {
+      serialize(nullptr), deserialize(nullptr), projection_pushdown(false), filter_pushdown(false), filter_prune(false),
+      sampling_pushdown(false) {
 }
 
 TableFunction::TableFunction(const vector<LogicalType> &arguments, table_function_t function,
