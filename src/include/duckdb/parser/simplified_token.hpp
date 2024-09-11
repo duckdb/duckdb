9f182204d16778882736e8e1c921140372f8b4e4
Pedro Holanda <pedroholanda@gmail.com>
2024-09-11 14:32:16 +0200

    remove trash

--- src/include/duckdb/parser/simplified_token.hpp
+++ src/include/duckdb/parser/simplified_token.hpp
@@ -28,13 +28,7 @@ struct SimplifiedToken {
 	idx_t start;
 };
 
-enum class KeywordCategory : uint8_t {
-	KEYWORD_RESERVED,
-	KEYWORD_UNRESERVED,
-	KEYWORD_TYPE_FUNC,
-	KEYWORD_COL_NAME,
-	UNSPECIFIED
-};
+enum class KeywordCategory : uint8_t { KEYWORD_RESERVED, KEYWORD_UNRESERVED, KEYWORD_TYPE_FUNC, KEYWORD_COL_NAME };
 
 struct ParserKeyword {
 	string name;
