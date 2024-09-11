b424c1bdcfcf58d5bc9c35cb0940287af7693f62
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-09-11 14:28:19 +0200

    correct path to the data set

--- benchmark/realnest/load.sql
+++ benchmark/realnest/load.sql
@@ -1,15 +1,15 @@
-attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/cord_10k.duckdb' as cord (READ_ONLY);
-create table cord as select * from cord.cord;
-attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/open_street_map_524k.duckdb' as osm (READ_ONLY);
-create table open_street_map as select * from osm.open_street_map;
-attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/pull_131k.duckdb' as gh_pull (READ_ONLY);
-create table gh_pull as select * from gh_pull.gh_pull;
-attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/issue_131k.duckdb' as gh_issue (READ_ONLY);
-create table gh_issue as select * from gh_issue.gh_issue;
-attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/twitter_131k.duckdb' as tw (READ_ONLY);
-create table twitter as select * from tw.twitter;
-attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/singleMu_524k.duckdb' as rn_singleMu (READ_ONLY);
-create table run2012B_singleMu as select * from rn_singleMu.run2012B_singleMu;
+attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/realnest.duckdb' as real_nest (READ_ONLY);
+create table cord as select * from real_nest.cord;
+-- attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/open_street_map_524k.duckdb' as osm (READ_ONLY);
+create table open_street_map as select * from real_nest.open_street_map;
+-- attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/pull_131k.duckdb' as gh_pull (READ_ONLY);
+create table gh_pull as select * from real_nest.gh_pull;
+-- attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/issue_131k.duckdb' as gh_issue (READ_ONLY);
+create table gh_issue as select * from real_nest.gh_issue;
+-- attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/twitter_131k.duckdb' as tw (READ_ONLY);
+create table twitter as select * from real_nest.twitter;
+-- attach 'https://duckdb-blobs.s3.amazonaws.com/data/realnest/singleMu_524k.duckdb' as rn_singleMu (READ_ONLY);
+create table run2012B_singleMu as select * from real_nest.run2012B_singleMu;
 CREATE TABLE single_mu_lists AS SELECT * REPLACE(
     list_resize(Jet, 10, NULL) as Jet, list_resize(Muon, 10, NULL) as Muon, 
     list_resize(Photon, 10, NULL) as Photon, list_resize(Tau, 10, NULL) as Tau) 
