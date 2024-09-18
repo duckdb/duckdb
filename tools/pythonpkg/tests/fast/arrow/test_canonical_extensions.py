438c50fa718536274063ec7991a552e8db98b5db
Pedro Holanda <pedroholanda@gmail.com>
2024-09-18 15:23:25 +0200

    [Arrow] Make unknown Arrow extensions throw at scan instead of bind

--- tools/pythonpkg/tests/fast/arrow/test_canonical_extensions.py
+++ tools/pythonpkg/tests/fast/arrow/test_canonical_extensions.py
@@ -228,10 +228,16 @@ class TestCanonicalExtensionTypes(object):
         my_type = MyType()
         storage_array = my_type.wrap_array(storage_array)
 
-        arrow_table = pa.Table.from_arrays([storage_array], names=['pedro_pedro_pedro'])
+        age_array = pa.array([29], pa.int32())
+
+        arrow_table = pa.Table.from_arrays([storage_array, age_array], names=['pedro_pedro_pedro', 'age'])
 
         with pytest.raises(duckdb.NotImplementedException, match=" Arrow Type with extension name: pedro.binary"):
             duck_arrow = duckdb_cursor.execute('FROM arrow_table').arrow()
+
+        duck_res = duckdb_cursor.execute('SELECT age FROM arrow_table').fetchall()
+        # This works because we project ze unknown extension array
+        assert duck_res == [(29,)]
 
     def test_hugeint(self):
         duckdb_cursor = duckdb.connect()
