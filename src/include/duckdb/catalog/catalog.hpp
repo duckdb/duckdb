7ea1f14c4c7cf015e074fead4da631705e35dc10
damon <wangmengdamon@gmail.com>
2024-09-20 18:32:45 +0800

    revert include order
    Signed-off-by: damon <wangmengdamon@gmail.com>

--- src/include/duckdb/catalog/catalog.hpp
+++ src/include/duckdb/catalog/catalog.hpp
@@ -9,18 +9,17 @@
 #pragma once
 
 #include "duckdb/catalog/catalog_entry.hpp"
+#include "duckdb/common/mutex.hpp"
+#include "duckdb/parser/query_error_context.hpp"
 #include "duckdb/catalog/catalog_transaction.hpp"
+#include "duckdb/common/reference_map.hpp"
 #include "duckdb/common/atomic.hpp"
-#include "duckdb/common/enums/catalog_lookup_behavior.hpp"
+#include "duckdb/common/map.hpp"
+#include "duckdb/common/optional_ptr.hpp"
 #include "duckdb/common/enums/on_entry_not_found.hpp"
 #include "duckdb/common/error_data.hpp"
 #include "duckdb/common/exception/catalog_exception.hpp"
-#include "duckdb/common/map.hpp"
-#include "duckdb/common/mutex.hpp"
-#include "duckdb/common/optional_ptr.hpp"
-#include "duckdb/common/reference_map.hpp"
-#include "duckdb/parser/query_error_context.hpp"
-
+#include "duckdb/common/enums/catalog_lookup_behavior.hpp"
 #include <functional>
 
 namespace duckdb {
