83b222885cd798e559cb35dd58ba320adc60dcb1
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-09-24 16:41:06 +0200

    WIP - move core functions to an extension

--- src/function/aggregate/distributive/minmax.cpp
+++ src/function/aggregate/distributive/minmax.cpp
@@ -338,6 +338,9 @@ unique_ptr<FunctionData> BindMinMax(ClientContext &context, AggregateFunction &f
 			QueryErrorContext error_context;
 			auto func = Catalog::GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, "", "", function_name,
 			                              OnEntryNotFound::RETURN_NULL, error_context);
+			if (!func) {
+				throw NotImplementedException("arg_min/arg_max do not exist in the catalog - is the core_functions extension loaded");
+			}
 
 			auto &func_entry = func->Cast<AggregateFunctionCatalogEntry>();
 
--- src/main/extension/extension_helper.cpp
+++ src/main/extension/extension_helper.cpp
@@ -403,7 +403,7 @@ void ExtensionHelper::LoadAllExtensions(DuckDB &db) {
 	// The in-tree extensions that we check. Non-cmake builds are currently limited to these for static linking
 	// TODO: rewrite package_build.py to allow also loading out-of-tree extensions in non-cmake builds, after that
 	//		 these can be removed
-	unordered_set<string> extensions {"parquet", "icu",   "tpch", "tpcds",    "fts",         "httpfs",
+	vector<string> extensions {"core_functions", "parquet", "icu",   "tpch", "tpcds",    "fts",         "httpfs",
 	                                  "json",    "excel", "inet", "jemalloc", "autocomplete"};
 	for (auto &ext : extensions) {
 		LoadExtensionInternal(db, ext, true);
