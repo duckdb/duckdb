452534c9365a200d59ea128dd75ed61c586409e4
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-09-25 18:19:15 +0200

    Revert "Revert spatial bump in feature"
    This reverts commit 287f3d174812eaedfde0d6410cba5b51c94b22ee.

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -102,7 +102,7 @@ endif()
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb_spatial.git
-    GIT_TAG dbb9971c900c5888e3e3598af91de3b9b884aca6
+    GIT_TAG bb9c829693965f029eb5a312aefed4c538fad781
     INCLUDE_DIR spatial/include
     TEST_DIR test/sql
     APPLY_PATCHES
