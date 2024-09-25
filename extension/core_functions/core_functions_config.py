b090612980f482b46669ff932efda576380fdcb5
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-09-25 10:00:36 +0200

    **/*

--- extension/core_functions/core_functions_config.py
+++ extension/core_functions/core_functions_config.py
@@ -3,4 +3,4 @@ import glob, os
 prefix = os.path.join('extension', 'core_functions')
 
 include_directories = [os.path.join(prefix, x) for x in ['include']]
-source_files = [os.path.join(prefix, x) for x in glob.glob("**.cpp", root_dir=prefix, recursive=True)]
+source_files = [os.path.join(prefix, x) for x in glob.glob("**/*.cpp", root_dir=prefix, recursive=True)]
