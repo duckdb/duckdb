2bbb34b203eee622c280134bfa88e44c7cc122d9
Tishj <t_b@live.nl>
2024-09-30 13:10:21 +0200

    don't make GetEntryDetailed a pure-virtual method, add a basic implementation (might be better to throw perhaps?)

--- src/include/duckdb/catalog/catalog_entry/schema_catalog_entry.hpp
+++ src/include/duckdb/catalog/catalog_entry/schema_catalog_entry.hpp
@@ -86,7 +86,7 @@ public:
 	DUCKDB_API virtual optional_ptr<CatalogEntry> GetEntry(CatalogTransaction transaction, CatalogType type,
 	                                                       const string &name) = 0;
 	DUCKDB_API virtual CatalogSet::EntryLookup GetEntryDetailed(CatalogTransaction transaction, CatalogType type,
-	                                                            const string &name) = 0;
+	                                                            const string &name);
 	DUCKDB_API virtual SimilarCatalogEntry GetSimilarEntry(CatalogTransaction transaction, CatalogType type,
 	                                                       const string &name);
 
