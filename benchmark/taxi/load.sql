31f15a7b219283dba0a7ab8e46b1b70b8c0133a6
pdet <pedroholanda@gmail.com>
2024-10-02 13:34:47 +0200

    update loader

--- benchmark/taxi/load.sql
+++ benchmark/taxi/load.sql
@@ -51,4 +51,4 @@ CREATE TABLE trips (
     dropoff_ntaname         VARCHAR,
     dropoff_puma            VARCHAR);
 
-COPY trips FROM 'data/trips_*.csv.gz' (header 0);
+COPY trips FROM 'benchmark/taxi/data/trips_*.csv.gz' (header 0);
