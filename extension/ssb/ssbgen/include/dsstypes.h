263aa4e073d86f2c9bdd8dacf333921731efa478
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-10-14 21:46:20 +0200

    Add pragma such that it is only included once per binary

--- extension/ssb/ssbgen/include/dsstypes.h
+++ extension/ssb/ssbgen/include/dsstypes.h
@@ -6,23 +6,24 @@
  * Modified for SSBM prototype
  */
 
+#pragma once
+
 /*
  * typedefs
  */
-typedef struct
-{
-    long custkey;
-    char name[C_NAME_LEN + 1];
-    int nlen;
-    char address[C_ADDR_MAX + 1];
-    int alen;
-    char city[CITY_FIX + 1];
-    int nation_key;
-    char nation_name[C_NATION_NAME_LEN + 1];
-    int region_key;
-    char region_name[C_REGION_NAME_LEN + 1];
-    char phone[PHONE_LEN + 1];
-    char mktsegment[MAXAGG_LEN + 1];
+typedef struct {
+	long custkey;
+	char name[C_NAME_LEN + 1];
+	int nlen;
+	char address[C_ADDR_MAX + 1];
+	int alen;
+	char city[CITY_FIX + 1];
+	int nation_key;
+	char nation_name[C_NATION_NAME_LEN + 1];
+	int region_key;
+	char region_name[C_REGION_NAME_LEN + 1];
+	char phone[PHONE_LEN + 1];
+	char mktsegment[MAXAGG_LEN + 1];
 } customer_t;
 
 /* customers.c */
@@ -30,38 +31,36 @@ long mk_cust PROTO((long n_cust, customer_t *c));
 int pr_cust PROTO((customer_t * c, int mode));
 int ld_cust PROTO((customer_t * c, int mode));
 
-typedef struct
-{
-    DSS_HUGE *okey; /*for clustering line items*/
-    int linenumber; /*integer, constrain to max of 7*/
-    long custkey;
-    long partkey;
-    long suppkey;
-    char orderdate[DATE_LEN];
-    char opriority[MAXAGG_LEN + 1];
-    long ship_priority;
-    long quantity;
-    long extended_price;
-    long order_totalprice;
-    long discount;
-    long revenue;
-    long supp_cost;
-    long tax;
-    char commit_date[DATE_LEN];
-    char shipmode[O_SHIP_MODE_LEN + 1];
+typedef struct {
+	DSS_HUGE *okey; /*for clustering line items*/
+	int linenumber; /*integer, constrain to max of 7*/
+	long custkey;
+	long partkey;
+	long suppkey;
+	char orderdate[DATE_LEN];
+	char opriority[MAXAGG_LEN + 1];
+	long ship_priority;
+	long quantity;
+	long extended_price;
+	long order_totalprice;
+	long discount;
+	long revenue;
+	long supp_cost;
+	long tax;
+	char commit_date[DATE_LEN];
+	char shipmode[O_SHIP_MODE_LEN + 1];
 } lineorder_t;
 
-typedef struct
-{
-    DSS_HUGE *okey;
-    long custkey;
-    int totalprice;
-    char odate[DATE_LEN];
-    char opriority[MAXAGG_LEN + 1];
-    char clerk[O_CLRK_LEN + 1];
-    int spriority;
-    long lines;
-    lineorder_t lineorders[O_LCNT_MAX];
+typedef struct {
+	DSS_HUGE *okey;
+	long custkey;
+	int totalprice;
+	char odate[DATE_LEN];
+	char opriority[MAXAGG_LEN + 1];
+	char clerk[O_CLRK_LEN + 1];
+	int spriority;
+	long lines;
+	lineorder_t lineorders[O_LCNT_MAX];
 } order_t;
 
 /* order.c */
@@ -73,20 +72,19 @@ void ez_sparse PROTO((long index, DSS_HUGE *ok, long seq));
 void hd_sparse PROTO((long index, DSS_HUGE *ok, long seq));
 #endif
 
-typedef struct
-{
-    long partkey;
-    char name[P_NAME_LEN + 1];
-    int nlen;
-    char mfgr[P_MFG_LEN + 1];
-    char category[P_CAT_LEN + 1];
-    char brand[P_BRND_LEN + 1];
-    char color[P_COLOR_MAX + 1];
-    int clen;
-    char type[P_TYPE_MAX + 1];
-    int tlen;
-    long size;
-    char container[P_CNTR_LEN + 1];
+typedef struct {
+	long partkey;
+	char name[P_NAME_LEN + 1];
+	int nlen;
+	char mfgr[P_MFG_LEN + 1];
+	char category[P_CAT_LEN + 1];
+	char brand[P_BRND_LEN + 1];
+	char color[P_COLOR_MAX + 1];
+	int clen;
+	char type[P_TYPE_MAX + 1];
+	int tlen;
+	long size;
+	char container[P_CNTR_LEN + 1];
 } part_t;
 
 /* parts.c */
@@ -94,18 +92,17 @@ long mk_part PROTO((long index, part_t *p));
 int pr_part PROTO((part_t * part, int mode));
 int ld_part PROTO((part_t * part, int mode));
 
-typedef struct
-{
-    long suppkey;
-    char name[S_NAME_LEN + 1];
-    char address[S_ADDR_MAX + 1];
-    int alen;
-    char city[CITY_FIX + 1];
-    int nation_key;
-    char nation_name[S_NATION_NAME_LEN + 1];
-    int region_key;
-    char region_name[S_REGION_NAME_LEN + 1];
-    char phone[PHONE_LEN + 1];
+typedef struct {
+	long suppkey;
+	char name[S_NAME_LEN + 1];
+	char address[S_ADDR_MAX + 1];
+	int alen;
+	char city[CITY_FIX + 1];
+	int nation_key;
+	char nation_name[S_NATION_NAME_LEN + 1];
+	int region_key;
+	char region_name[S_REGION_NAME_LEN + 1];
+	char phone[PHONE_LEN + 1];
 } supplier_t;
 
 /* supplier.c */
@@ -115,26 +112,25 @@ int ld_supp PROTO((supplier_t * supp, int mode));
 
 /*todo: add new date table*/
 
-typedef struct
-{
-    char datekey[DATE_LEN];
-    char date[D_DATE_LEN + 1];
-    char dayofweek[D_DAYWEEK_LEN + 1];
-    char month[D_MONTH_LEN + 1];
-    int year;
-    int yearmonthnum;
-    char yearmonth[D_YEARMONTH_LEN + 1];
-    int daynuminweek;
-    int daynuminmonth;
-    int daynuminyear;
-    int monthnuminyear;
-    int weeknuminyear;
-    char sellingseason[D_SEASON_LEN + 1];
-    int slen;
-    char lastdayinweekfl[2];
-    char lastdayinmonthfl[2];
-    char holidayfl[2];
-    char weekdayfl[2];
+typedef struct {
+	char datekey[DATE_LEN];
+	char date[D_DATE_LEN + 1];
+	char dayofweek[D_DAYWEEK_LEN + 1];
+	char month[D_MONTH_LEN + 1];
+	int year;
+	int yearmonthnum;
+	char yearmonth[D_YEARMONTH_LEN + 1];
+	int daynuminweek;
+	int daynuminmonth;
+	int daynuminyear;
+	int monthnuminyear;
+	int weeknuminyear;
+	char sellingseason[D_SEASON_LEN + 1];
+	int slen;
+	char lastdayinweekfl[2];
+	char lastdayinmonthfl[2];
+	char holidayfl[2];
+	char weekdayfl[2];
 } date_t;
 
 /* date.c */
@@ -143,14 +139,13 @@ long mk_date PROTO((long index, date_t *d));
 int pr_date PROTO((date_t * date, int mode));
 int ld_date PROTO((date_t * date, int mode));
 
-typedef struct
-{
-    long timekey;
-    char alpha[DATE_LEN];
-    long year;
-    long month;
-    long week;
-    long day;
+typedef struct {
+	long timekey;
+	char alpha[DATE_LEN];
+	long year;
+	long month;
+	long week;
+	long day;
 } dss_time_t;
 
 /* time.c */
@@ -159,13 +154,12 @@ long mk_time PROTO((long index, dss_time_t *t));
 /*
  * this assumes that N_CMNT_LEN >= R_CMNT_LEN
  */
-typedef struct
-{
-    long code;
-    char *text;
-    long join;
-    char comment[N_CMNT_MAX + 1];
-    int clen;
+typedef struct {
+	long code;
+	char *text;
+	long join;
+	char comment[N_CMNT_MAX + 1];
+	int clen;
 } code_t;
 
 /* code table */
