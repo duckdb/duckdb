0ae4c10fed920c01c3d782e2571a7fda1de91347
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-10-14 21:47:27 +0200

    Add appender to relevant functions

--- extension/ssb/ssbgen/driver.c
+++ extension/ssb/ssbgen/driver.c
@@ -1,30 +1,33 @@
 /* @(#)driver.c	2.1.8.4 */
 /* main driver for dss banchmark */
 
-#define DECLARER				   /* EXTERN references get defined here */
-#define NO_FUNC (int (*)()) NULL   /* to clean up tdefs */
+#define DECLARER                   /* EXTERN references get defined here */
+#define NO_FUNC  (int (*)()) NULL  /* to clean up tdefs */
 #define NO_LFUNC (long (*)()) NULL /* to clean up tdefs */
 
+#include "include/driver.h"
+
 #include "include/config.h"
+
 #include <stdlib.h>
 #if (defined(_POSIX_) || !defined(WIN32)) /* Change for Windows NT */
 #ifndef DOS
 #ifdef MAC
 #define _POSIX_C_SOURCE 200809L
 #endif
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #endif
 
-#endif			   /* WIN32 */
-#include <stdio.h> /* */
+#endif /* WIN32 */
+#include <ctype.h>
+#include <errno.h>
 #include <limits.h>
 #include <math.h>
-#include <ctype.h>
 #include <signal.h>
+#include <stdio.h> /* */
 #include <string.h>
-#include <errno.h>
 #ifdef HP
 #include <strings.h>
 #endif
@@ -56,9 +59,9 @@
 #pragma warning(default : 4214)
 #endif
 
+#include "include/bcd2.h"
 #include "include/dss.h"
 #include "include/dsstypes.h"
-#include "include/bcd2.h"
 
 /*
  * Function prototypes
@@ -67,11 +70,11 @@ void usage(void);
 int prep_direct(char *);
 int close_direct(void);
 void kill_load(void);
-int pload(int tbl);
-void gen_tbl(int tnum, long start, long count, long upd_num);
+int pload(int tbl, ssb_appender *appender);
+void gen_tbl(int tnum, long start, long count, long upd_num, ssb_appender *appender);
 int pr_drange(int tbl, long min, long cnt, long num);
 int set_files(int t, int pload);
-int partial(int, int);
+int partial(int, int, ssb_appender *);
 
 extern int optind, opterr;
 extern char *optarg;
@@ -153,22 +156,53 @@ int vrf_date(date_t, int mode);
 
 typedef int (*func_ptr)();
 
-tdef tdefs[] =
-	{
+tdef tdefs[] = {
 
-		{"part.tbl", "part table", 200000, hd_part, {(func_ptr)pr_part, (func_ptr)ld_part}, sd_part, (func_ptr)vrf_part, PSUPP, 0},
-		{0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
-		{"supplier.tbl", "suppliers table", 2000, hd_supp, {(func_ptr)pr_supp, (func_ptr)ld_supp}, sd_supp, (func_ptr)vrf_supp, NONE, 0},
+    {"part.tbl",
+     "part table",
+     200000,
+     hd_part,
+     {(func_ptr)pr_part, (func_ptr)ld_part},
+     sd_part,
+     (func_ptr)vrf_part,
+     PSUPP,
+     0},
+    {0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
+    {"supplier.tbl",
+     "suppliers table",
+     2000,
+     hd_supp,
+     {(func_ptr)pr_supp, (func_ptr)ld_supp},
+     sd_supp,
+     (func_ptr)vrf_supp,
+     NONE,
+     0},
 
-		{"customer.tbl", "customers table", 30000, hd_cust, {(func_ptr)pr_cust, (func_ptr)ld_cust}, sd_cust, (func_ptr)vrf_cust, NONE, 0},
-		{"date.tbl", "date table", 2556, 0, {(func_ptr)pr_date, (func_ptr)ld_date}, 0, (func_ptr)vrf_date, NONE, 0},
-		/*line order is SF*1,500,000, however due to the implementation
-		  the base here is 150,000 instead if 1500,000*/
-		{"lineorder.tbl", "lineorder table", 150000, hd_line, {(func_ptr)pr_line, (func_ptr)ld_line}, sd_line, (func_ptr)vrf_line, NONE, 0},
-		{0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
-		{0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
-		{0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
-		{0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
+    {"customer.tbl",
+     "customers table",
+     30000,
+     hd_cust,
+     {(func_ptr)pr_cust, (func_ptr)ld_cust},
+     sd_cust,
+     (func_ptr)vrf_cust,
+     NONE,
+     0},
+    {"date.tbl", "date table", 2556, 0, {(func_ptr)pr_date, (func_ptr)ld_date}, 0, (func_ptr)vrf_date, NONE, 0},
+    /*line order is SF*1,500,000, however due to the implementation
+      the base here is 150,000 instead if 1500,000*/
+    {"lineorder.tbl",
+     "lineorder table",
+     150000,
+     hd_line,
+     {(func_ptr)pr_line, (func_ptr)ld_line},
+     sd_line,
+     (func_ptr)vrf_line,
+     NONE,
+     0},
+    {0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
+    {0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
+    {0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
+    {0, 0, 0, 0, {0, 0}, 0, 0, 0, 0},
 };
 int *pids;
 
@@ -176,13 +210,11 @@ int *pids;
  * routines to handle the graceful cleanup of multi-process loads
  */
 
-void stop_proc(int signum)
-{
+void stop_proc(int signum) {
 	exit(0);
 }
 
-void kill_load(void)
-{
+void kill_load(void) {
 	int i;
 
 #if !defined(U2200) && !defined(DOS)
@@ -196,13 +228,11 @@ void kill_load(void)
 /*
  * re-set default output file names
  */
-int set_files(int i, int pload)
-{
+int set_files(int i, int pload) {
 	char line[80], *new_name;
 
 	if (table & (1 << i))
-	child_table:
-	{
+	child_table: {
 		if (pload != -1)
 			sprintf(line, "%s.%d", tdefs[i].name, pload);
 		else
@@ -212,8 +242,7 @@ int set_files(int i, int pload)
 		MALLOC_CHECK(new_name);
 		strcpy(new_name, line);
 		tdefs[i].name = new_name;
-		if (tdefs[i].child != NONE)
-		{
+		if (tdefs[i].child != NONE) {
 			i = tdefs[i].child;
 			tdefs[i].child = NONE;
 			goto child_table;
@@ -226,20 +255,16 @@ int set_files(int i, int pload)
 /*
  * read the distributions needed in the benchamrk
  */
-void load_dists(void)
-{
+void load_dists(void) {
 	read_dist("p_cntr", &p_cntr_set);
 	read_dist("colors", &colors);
 	read_dist("p_types", &p_types_set);
 	read_dist("nations", &nations);
 	read_dist("regions", &regions);
-	read_dist("o_oprio",
-			  &o_priority_set);
-	read_dist("instruct",
-			  &l_instruct_set);
+	read_dist("o_oprio", &o_priority_set);
+	read_dist("instruct", &l_instruct_set);
 	read_dist("smode", &l_smode_set);
-	read_dist("category",
-			  &l_category_set);
+	read_dist("category", &l_category_set);
 	read_dist("rflag", &l_rflag_set);
 	read_dist("msegmnt", &c_mseg_set);
 
@@ -260,8 +285,7 @@ void load_dists(void)
 /*
  * generate a particular table
  */
-void gen_tbl(int tnum, long start, long count, long upd_num)
-{
+void gen_tbl(int tnum, long start, long count, long upd_num, ssb_appender *appender) {
 	static order_t o;
 	supplier_t supp;
 	customer_t cust;
@@ -275,43 +299,34 @@ void gen_tbl(int tnum, long start, long count, long upd_num)
 	int rows_this_segment = -1;
 	int residual_rows = 0;
 
-	if (insert_segments)
-	{
+	if (insert_segments) {
 		rows_per_segment = count / insert_segments;
 		residual_rows = count - (rows_per_segment * insert_segments);
 	}
 
-	if (init == 0)
-	{
+	if (init == 0) {
 		INIT_HUGE(o.okey);
 		for (i = 0; i < O_LCNT_MAX; i++)
 			INIT_HUGE(o.lineorders[i].okey);
 		init = 1;
 	}
 
-	for (i = start; count; count--, i++)
-	{
+	for (i = start; count; count--, i++) {
 		LIFENOISE(1000, i);
 		row_start(tnum);
 
-		switch (tnum)
-		{
+		switch (tnum) {
 		case LINE:
 			mk_order(i, &o, upd_num % 10000);
 
 			if (insert_segments && (upd_num > 0))
-				if ((upd_num / 10000) < residual_rows)
-				{
-					if ((++rows_this_segment) > rows_per_segment)
-					{
+				if ((upd_num / 10000) < residual_rows) {
+					if ((++rows_this_segment) > rows_per_segment) {
 						rows_this_segment = 0;
 						upd_num += 10000;
 					}
-				}
-				else
-				{
-					if ((++rows_this_segment) >= rows_per_segment)
-					{
+				} else {
+					if ((++rows_this_segment) >= rows_per_segment) {
 						rows_this_segment = 0;
 						upd_num += 10000;
 					}
@@ -321,7 +336,7 @@ void gen_tbl(int tnum, long start, long count, long upd_num)
 				if (validate)
 					((int (*)(order_t *, int))tdefs[tnum].verify)(&o, 0);
 				else
-					((int (*)(order_t *, int))tdefs[tnum].loader[direct])(&o, upd_num);
+					appender->pr_line(&o, upd_num);
 			break;
 		case SUPP:
 			mk_supp(i, &supp);
@@ -329,7 +344,7 @@ void gen_tbl(int tnum, long start, long count, long upd_num)
 				if (validate)
 					((int (*)(supplier_t *, int))tdefs[tnum].verify)(&supp, 0);
 				else
-					((int (*)(supplier_t *, int))tdefs[tnum].loader[direct])(&supp, upd_num);
+					appender->pr_supp(&supp, upd_num);
 			break;
 		case CUST:
 			mk_cust(i, &cust);
@@ -337,7 +352,7 @@ void gen_tbl(int tnum, long start, long count, long upd_num)
 				if (validate)
 					((int (*)(customer_t *, int))tdefs[tnum].verify)(&cust, 0);
 				else
-					((int (*)(customer_t *, int))tdefs[tnum].loader[direct])(&cust, upd_num);
+					appender->pr_cust(&cust, upd_num);
 			break;
 		case PART:
 			mk_part(i, &part);
@@ -345,7 +360,7 @@ void gen_tbl(int tnum, long start, long count, long upd_num)
 				if (validate)
 					((int (*)(part_t *, int))tdefs[tnum].verify)(&part, 0);
 				else
-					((int (*)(part_t *, int))tdefs[tnum].loader[direct])(&part, upd_num);
+					appender->pr_part(&part, upd_num);
 			break;
 		case DATE:
 			mk_date(i, &dt);
@@ -353,12 +368,11 @@ void gen_tbl(int tnum, long start, long count, long upd_num)
 				if (validate)
 					((int (*)(date_t *, int))tdefs[tnum].verify)(&dt, 0);
 				else
-					((int (*)(date_t *, int))tdefs[tnum].loader[direct])(&dt, 0);
+					appender->pr_date(&dt, 0);
 			break;
 		}
 		row_stop(tnum);
-		if (set_seeds && (i % tdefs[tnum].base) < 2)
-		{
+		if (set_seeds && (i % tdefs[tnum].base) < 2) {
 			printf("\nSeeds for %s at rowcount %ld\n", tdefs[tnum].comment, i);
 			dump_seeds(tnum);
 		}
@@ -366,14 +380,10 @@ void gen_tbl(int tnum, long start, long count, long upd_num)
 	completed |= 1 << tnum;
 }
 
-void usage(void)
-{
-	fprintf(stderr, "%s\n%s\n\t%s\n%s %s\n\n",
-			"USAGE:",
-			"dbgen [-{vfFD}] [-O {fhmsv}][-T {pcsdla}]",
-			"[-s <scale>][-C <procs>][-S <step>]",
-			"dbgen [-v] [-O {dfhmr}] [-s <scale>]",
-			"[-U <updates>] [-r <percent>]");
+void usage(void) {
+	fprintf(stderr, "%s\n%s\n\t%s\n%s %s\n\n", "USAGE:", "dbgen [-{vfFD}] [-O {fhmsv}][-T {pcsdla}]",
+	        "[-s <scale>][-C <procs>][-S <step>]", "dbgen [-v] [-O {dfhmr}] [-s <scale>]",
+	        "[-U <updates>] [-r <percent>]");
 
 	fprintf(stderr, "-b <s> -- load distributions for <s>\n");
 	fprintf(stderr, "-C <n> -- use <n> processes to generate data\n");
@@ -405,8 +415,7 @@ void usage(void)
 
 	fprintf(stderr, "-U <s> -- generate <s> update sets\n");
 	fprintf(stderr, "-v     -- enable VERBOSE mode\n");
-	fprintf(stderr,
-			"\nTo generate the SF=1 (1GB), validation database population, use:\n");
+	fprintf(stderr, "\nTo generate the SF=1 (1GB), validation database population, use:\n");
 	fprintf(stderr, "\tdbgen -vfF -s 1\n");
 	fprintf(stderr, "\nTo generate updates for a SF=1 (1GB), use:\n");
 	fprintf(stderr, "\tdbgen -v -U 1 -s 1\n");
@@ -418,15 +427,12 @@ void usage(void)
 /*
  * int partial(int tbl, int s) -- generate the s-th part of the named tables data
  */
-int partial(int tbl, int s)
-{
+int partial(int tbl, int s, ssb_appender *appender) {
 	long rowcnt;
 	long extra;
 
-	if (verbose > 0)
-	{
-		fprintf(stderr, "\tStarting to load stage %d of %d for %s...",
-				s, children, tdefs[tbl].comment);
+	if (verbose > 0) {
+		fprintf(stderr, "\tStarting to load stage %d of %d for %s...", s, children, tdefs[tbl].comment);
 	}
 
 	if (direct == 0)
@@ -435,9 +441,9 @@ int partial(int tbl, int s)
 	rowcnt = set_state(tbl, scale, children, s, &extra);
 
 	if (s == children)
-		gen_tbl(tbl, rowcnt * (s - 1) + 1, rowcnt + extra, upd_num);
+		gen_tbl(tbl, rowcnt * (s - 1) + 1, rowcnt + extra, upd_num, appender);
 	else
-		gen_tbl(tbl, rowcnt * (s - 1) + 1, rowcnt, upd_num);
+		gen_tbl(tbl, rowcnt * (s - 1) + 1, rowcnt, upd_num, appender);
 
 	if (verbose > 0)
 		fprintf(stderr, "done.\n");
@@ -447,32 +453,25 @@ int partial(int tbl, int s)
 
 #ifndef DOS
 
-int pload(int tbl)
-{
+int pload(int tbl, ssb_appender *appender) {
 	int c = 0, i, status;
 
-	if (verbose > 0)
-	{
-		fprintf(stderr, "Starting %d children to load %s",
-				children, tdefs[tbl].comment);
+	if (verbose > 0) {
+		fprintf(stderr, "Starting %d children to load %s", children, tdefs[tbl].comment);
 	}
-	for (c = 0; c < children; c++)
-	{
+	for (c = 0; c < children; c++) {
 		pids[c] = SPAWN();
-		if (pids[c] == -1)
-		{
+		if (pids[c] == -1) {
 			perror("Child loader not created");
 			kill_load();
 			exit(-1);
-		}
-		else if (pids[c] == 0) /* CHILD */
+		} else if (pids[c] == 0) /* CHILD */
 		{
 			SET_HANDLER(stop_proc);
 			verbose = 0;
-			partial(tbl, c + 1);
+			partial(tbl, c + 1, appender);
 			exit(0);
-		}
-		else if (verbose > 0) /* PARENT */
+		} else if (verbose > 0) /* PARENT */
 			fprintf(stderr, ".");
 	}
 
@@ -480,11 +479,9 @@ int pload(int tbl)
 		fprintf(stderr, "waiting...");
 
 	c = children;
-	while (c)
-	{
+	while (c) {
 		i = WAIT(&status, pids[c - 1]);
-		if (i == -1 && children)
-		{
+		if (i == -1 && children) {
 			if (errno == ECHILD)
 				fprintf(stderr, "\nCould not wait on pid %d\n", pids[c - 1]);
 			else if (errno == EINTR)
@@ -492,18 +489,12 @@ int pload(int tbl)
 			else if (errno == EINVAL)
 				fprintf(stderr, "\nProgram bug\n");
 		}
-		if (!WIFEXITED(status))
-		{
+		if (!WIFEXITED(status)) {
 			(void)fprintf(stderr, "\nProcess %d: ", i);
-			if (WIFSIGNALED(status))
-			{
-				(void)fprintf(stderr, "rcvd signal %d\n",
-							  WTERMSIG(status));
-			}
-			else if (WIFSTOPPED(status))
-			{
-				(void)fprintf(stderr, "stopped, signal %d\n",
-							  WSTOPSIG(status));
+			if (WIFSIGNALED(status)) {
+				(void)fprintf(stderr, "rcvd signal %d\n", WTERMSIG(status));
+			} else if (WIFSTOPPED(status)) {
+				(void)fprintf(stderr, "stopped, signal %d\n", WSTOPSIG(status));
 			}
 		}
 		c--;
@@ -515,8 +506,7 @@ int pload(int tbl)
 }
 #endif
 
-void process_options(double scale_f)
-{
+void process_options(double scale_f) {
 	int option;
 
 	table = 1 << CUST;
@@ -526,33 +516,24 @@ void process_options(double scale_f)
 	table |= 1 << LINE;
 
 	flt_scale = scale_f;
-	if (flt_scale < MIN_SCALE)
-	{
+	if (flt_scale < MIN_SCALE) {
 		int i;
 
 		scale = 1;
-		for (i = PART; i < REGION; i++)
-		{
+		for (i = PART; i < REGION; i++) {
 			tdefs[i].base *= flt_scale;
 			if (tdefs[i].base < 1)
 				tdefs[i].base = 1;
 		}
-	}
-	else
+	} else
 		scale = (long)flt_scale;
-	if (scale > MAX_SCALE)
-	{
-		fprintf(stderr, "%s %5.0f %s\n\t%s\n\n",
-				"NOTE: Data generation for scale factors >",
-				MAX_SCALE,
-				"GB is still in development,",
-				"and is not yet supported.\n");
-		fprintf(stderr,
-				"Your resulting data set MAY NOT BE COMPLIANT!\n");
+	if (scale > MAX_SCALE) {
+		fprintf(stderr, "%s %5.0f %s\n\t%s\n\n", "NOTE: Data generation for scale factors >", MAX_SCALE,
+		        "GB is still in development,", "and is not yet supported.\n");
+		fprintf(stderr, "Your resulting data set MAY NOT BE COMPLIANT!\n");
 	}
 
-	if (children != 1 && step == -1)
-	{
+	if (children != 1 && step == -1) {
 		pids = (int *)malloc(children * sizeof(pid_t));
 		MALLOC_CHECK(pids)
 	}
@@ -566,16 +547,10 @@ void process_options(double scale_f)
  * assumes the existance of getopt() to clean up the command
  * line handling
  */
-int gen_main(double scale_f)
-{
+int gen_main(double scale_f, ssb_appender *appender) {
 	int i;
 
-	table = (1 << CUST) |
-			(1 << SUPP) |
-			(1 << NATION) |
-			(1 << REGION) |
-			(1 << PART_PSUPP) |
-			(1 << ORDER_LINE);
+	table = (1 << CUST) | (1 << SUPP) | (1 << NATION) | (1 << REGION) | (1 << PART_PSUPP) | (1 << ORDER_LINE);
 	force = 0;
 	insert_segments = 0;
 	delete_segments = 0;
@@ -592,8 +567,7 @@ int gen_main(double scale_f)
 	updates = 0;
 	refresh = UPD_PCT;
 	step = -1;
-	tdefs[LINE].base *=
-		ORDERS_PER_CUST; /* have to do this after init */
+	tdefs[LINE].base *= ORDERS_PER_CUST; /* have to do this after init */
 	fnames = 0;
 	db_name = NULL;
 	gen_sql = 0;
@@ -606,8 +580,7 @@ int gen_main(double scale_f)
 #endif /* NO_SUPPORT */
 	process_options(scale_f);
 #if (defined(WIN32) && !defined(_POSIX_))
-	for (i = 0; i < ac; i++)
-	{
+	for (i = 0; i < ac; i++) {
 		spawn_args[i] = malloc((strlen(av[i]) + 1) * sizeof(char));
 		MALLOC_CHECK(spawn_args[i]);
 		strcpy(spawn_args[i], av[i]);
@@ -615,11 +588,8 @@ int gen_main(double scale_f)
 	spawn_args[ac] = NULL;
 #endif
 
-	if (verbose >= 0)
-	{
-		fprintf(stderr,
-				"%s Population Generator (Version %d.%d.%d%s)\n",
-				NAME, VERSION, RELEASE, MODIFICATION, PATCH);
+	if (verbose >= 0) {
+		fprintf(stderr, "%s Population Generator (Version %d.%d.%d%s)\n", NAME, VERSION, RELEASE, MODIFICATION, PATCH);
 		fprintf(stderr, "Copyright %s %s\n", TPC, C_DATES);
 	}
 
@@ -634,11 +604,9 @@ int gen_main(double scale_f)
 	/*
 	 * open database connection or set all the file names, as appropriate
 	 */
-	for (i = PART; i <= REGION; i++)
-	{
+	for (i = PART; i <= REGION; i++) {
 		if (table & (1 << i))
-			if (set_files(i, -1))
-			{
+			if (set_files(i, -1)) {
 				fprintf(stderr, "Load aborted!\n");
 				exit(1);
 			}
@@ -648,52 +616,40 @@ int gen_main(double scale_f)
 	 * traverse the tables, invoking the appropriate data generation routine for any to be built
 	 */
 	for (i = PART; i <= REGION; i++)
-		if (table & (1 << i))
-		{
+		if (table & (1 << i)) {
 			if (children > 1 && i < NATION)
-				if (step >= 0)
-				{
-					if (validate)
-					{
+				if (step >= 0) {
+					if (validate) {
 						INTERNAL_ERROR("Cannot validate parallel data generation");
-					}
-					else
-						partial(i, step);
-				}
-				else
-				{
-					if (validate)
-					{
+					} else
+						partial(i, step, appender);
+				} else {
+					if (validate) {
 						INTERNAL_ERROR("Cannot validate parallel data generation");
-					}
-					else
-						pload(i);
+					} else
+						pload(i, appender);
 				}
-			else
-			{
+			else {
 				minrow = 1;
 				if (i < NATION)
 					rowcnt = tdefs[i].base * scale;
 				else
 					rowcnt = tdefs[i].base;
-				if (i == PART)
-				{
+				if (i == PART) {
 					rowcnt = tdefs[i].base * (floor(1 + log((double)(scale)) / (log(2))));
 				}
-				if (i == DATE)
-				{
+				if (i == DATE) {
 					rowcnt = tdefs[i].base;
 				}
 				if (verbose > 0)
-					fprintf(stderr, "%s data for %s [pid: %ld]",
-							(validate) ? "Validating" : "Generating", tdefs[i].comment, DSS_PROC);
-				gen_tbl(i, minrow, rowcnt, upd_num);
+					fprintf(stderr, "%s data for %s [pid: %ld]", (validate) ? "Validating" : "Generating",
+					        tdefs[i].comment, DSS_PROC);
+				gen_tbl(i, minrow, rowcnt, upd_num, appender);
 				if (verbose > 0)
 					fprintf(stderr, "done.\n");
 			}
 			if (validate)
-				printf("Validation checksum for %s at %d GB: %0x\n",
-					   tdefs[i].name, scale, tdefs[i].vtotal);
+				printf("Validation checksum for %s at %d GB: %0x\n", tdefs[i].name, scale, tdefs[i].vtotal);
 		}
 
 	if (direct)
