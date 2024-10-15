5f18bb0122866063206a4e5a8554002ddb89336f
Tishj <t_b@live.nl>
2024-10-15 10:31:57 +0200

    wip wip wip

--- third_party/zstd/decompress/zstd_decompress.cpp
+++ third_party/zstd/decompress/zstd_decompress.cpp
@@ -82,7 +82,7 @@
 #define DDICT_HASHSET_MAX_LOAD_FACTOR_COUNT_MULT 4
 #define DDICT_HASHSET_MAX_LOAD_FACTOR_SIZE_MULT 3  /* These two constants represent SIZE_MULT/COUNT_MULT load factor without using a float. */
                                                    /* Currently, that means a 0.75 load factor. */
-                                                   /* So, if count * COUNT_MULT / size * SIZE_MULT != 0, then we've exceeded *.
+                                                   /* So, if count * COUNT_MULT / size * SIZE_MULT != 0, then we've exceeded */
                                                    /* the load factor of the ddict hash set. */
 
 #define DDICT_HASHSET_TABLE_BASE_SIZE 64
