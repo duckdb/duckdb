ffc59f3558cae97641f49d3103e58d0e7177a5a6
Stefan Binder <binder_stefan@outlook.com>
2024-10-21 04:46:14 +0000

    Add DataFrame.drop_duplicates method

--- tools/pythonpkg/duckdb/experimental/spark/sql/dataframe.py
+++ tools/pythonpkg/duckdb/experimental/spark/sql/dataframe.py
@@ -1159,6 +1159,8 @@ class DataFrame:
 
         return self.distinct()
 
+    drop_duplicates = dropDuplicates
+
 
     def distinct(self) -> "DataFrame":
         """Returns a new :class:`DataFrame` containing the distinct rows in this :class:`DataFrame`.
