a2207c01253bd501096a1d0f950b753d98b2439f
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-10-21 16:47:15 +0200

    Windows fixes

--- tools/shell/shell.cpp
+++ tools/shell/shell.cpp
@@ -725,7 +725,7 @@ static char *local_getline(char *zLine, FILE *in){
     if( zTrans ){
       int nTrans = strlen30(zTrans)+1;
       if( nTrans>nLine ){
-        zLine = realloc(zLine, nTrans);
+        zLine = (char*) realloc(zLine, nTrans);
         if( zLine==0 ) shell_out_of_memory();
       }
       memcpy(zLine, zTrans, nTrans);
@@ -19452,7 +19452,7 @@ static char *find_home_dir(int clearFlag){
     zPath = getenv("HOMEPATH");
     if( zDrive && zPath ){
       n = strlen30(zDrive) + strlen30(zPath) + 1;
-      home_dir = malloc( n );
+      home_dir = (char*) malloc( n );
       if( home_dir==0 ) return 0;
       sqlite3_snprintf(n, home_dir, "%s%s", zDrive, zPath);
       return home_dir;
@@ -19731,7 +19731,7 @@ int SQLITE_CDECL wmain(int argc, wchar_t **wargv){
   */
 #if !SQLITE_SHELL_IS_UTF8
   sqlite3_initialize();
-  argvToFree = malloc(sizeof(argv[0])*argc*2);
+  argvToFree = (char**) malloc(sizeof(argv[0])*argc*2);
   argcToFree = argc;
   argv = argvToFree + argc;
   if( argv==0 ) shell_out_of_memory();
@@ -19740,7 +19740,7 @@ int SQLITE_CDECL wmain(int argc, wchar_t **wargv){
     int n;
     if( z==0 ) shell_out_of_memory();
     n = (int)strlen(z);
-    argv[i] = malloc( n+1 );
+    argv[i] = (char*) malloc( n+1 );
     if( argv[i]==0 ) shell_out_of_memory();
     memcpy(argv[i], z, n+1);
     argvToFree[i] = argv[i];
