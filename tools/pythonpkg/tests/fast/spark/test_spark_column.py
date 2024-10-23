1ca4f622c5287281817f0da993aa0d1f66f13cd8
Stefan Binder <binder_stefan@outlook.com>
2024-10-23 10:46:53 +0000

    Get all tests in test_spark_column to pass with Spark

--- tools/pythonpkg/tests/fast/spark/test_spark_column.py
+++ tools/pythonpkg/tests/fast/spark/test_spark_column.py
@@ -2,6 +2,7 @@ import pytest
 
 _ = pytest.importorskip("duckdb.experimental.spark")
 
+from ...spark_namespace import USE_ACTUAL_SPARK
 from ...spark_namespace.sql.column import Column
 from ...spark_namespace.sql.functions import struct
 from ...spark_namespace.sql.types import Row
@@ -15,11 +16,19 @@ class TestSparkColumn(object):
     def test_struct_column(self, spark):
         df = spark.createDataFrame([Row(a=1, b=2, c=3, d=4)])
 
-        df = df.withColumn('struct', struct(df.col0, df.col1))
-        assert 'struct' in df
-        new_col = df.schema['struct']
-        assert 'col0' in new_col.dataType
-        assert 'col1' in new_col.dataType
+        if USE_ACTUAL_SPARK:
+            df = df.withColumn('struct', struct(df.a, df.b))
+        else:
+            df = df.withColumn('struct', struct(df.col0, df.col1))
+            assert 'struct' in df
+            new_col = df.schema['struct']
+
+        if USE_ACTUAL_SPARK:
+            assert 'a' in df.schema['struct'].dataType.fieldNames()
+            assert 'b' in df.schema['struct'].dataType.fieldNames()
+        else:
+            assert 'col0' in new_col.dataType
+            assert 'col1' in new_col.dataType
 
         with pytest.raises(
             PySparkTypeError, match=re.escape("[NOT_COLUMN] Argument `col` should be a Column, got str.")
