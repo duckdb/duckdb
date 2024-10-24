f6f6f861602b49775ee3ed847a1a60b0676174e4
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-10-24 13:21:29 +0200

    Add test external httpfs extension

--- src/main/extension/extension_helper.cpp
+++ src/main/extension/extension_helper.cpp
@@ -86,10 +86,6 @@
 #include "fts_extension.hpp"
 #endif
 
-#if DUCKDB_EXTENSION_HTTPFS_LINKED
-#include "httpfs_extension.hpp"
-#endif
-
 #if DUCKDB_EXTENSION_JSON_LINKED
 #include "json_extension.hpp"
 #endif
