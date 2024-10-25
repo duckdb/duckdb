f85308f2cdf2b2503a78376f81a4372e6afdde72
223104714 <murali.vemulapati@ge.com>
2024-10-24 18:42:49 -0700

    support for snapshot removal

--- extension/extension_config.cmake
+++ extension/extension_config.cmake
@@ -9,7 +9,7 @@
 
 # Parquet is loaded by default on every build as its a essential part of DuckDB
 duckdb_extension_load(parquet)
-#duckdb_extension_load(kafkaredo)
+duckdb_extension_load(kafkaredo)
 
 # The Linux allocator has issues so we use jemalloc, but only on x86 because page sizes are fixed at 4KB.
 # If page sizes vary for an architecture (e.g., arm64), we cannot create a portable binary due to jemalloc config
