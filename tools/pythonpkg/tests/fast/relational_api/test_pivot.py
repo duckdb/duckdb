06a3e2991bb20d382561bb5a04aa4260e2ba4a89
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-10-29 22:16:49 +0100

    Fix #14601: avoid exporting entries in the temp or system schema

--- tools/pythonpkg/tests/fast/relational_api/test_pivot.py
+++ tools/pythonpkg/tests/fast/relational_api/test_pivot.py
@@ -1,6 +1,7 @@
 import duckdb
 import pytest
-from duckdb import ColumnExpression
+import os
+import tempfile
 
 
 class TestPivot(object):
@@ -16,3 +17,14 @@ class TestPivot(object):
         pivot_2_tbl = duckdb_cursor.table("pivot_2")
         assert set(pivot_1.columns) == set(pivot_1_tbl.columns)
         assert set(pivot_2.columns) == set(pivot_2_tbl.columns)
+
+    def test_pivot_issue_14601(self, duckdb_cursor):
+        duckdb_cursor.sql(
+            "create table input_data as select unnest(['u','v','w']) as a, unnest(['x','y','z']) as b, unnest([1,2,3]) as c;"
+        )
+        pivot_1 = duckdb_cursor.query("pivot input_data on a using max(c) group by b;")
+        pivot_1.create("pivot_1")
+        export_dir = tempfile.mkdtemp()
+        duckdb_cursor.query(f"EXPORT DATABASE '{export_dir}'")
+        with open(os.path.join(export_dir, "schema.sql"), "r") as f:
+            assert 'CREATE TYPE' not in f.read()
