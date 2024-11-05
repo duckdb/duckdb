364c215817108a3d6ce481e7c4e9f4f78b033a74
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-11-05 12:52:02 +0100

    Add missing include

--- src/optimizer/rule/comparison_simplification.cpp
+++ src/optimizer/rule/comparison_simplification.cpp
@@ -3,6 +3,7 @@
 
 #include "duckdb/execution/expression_executor.hpp"
 #include "duckdb/planner/expression/bound_constant_expression.hpp"
+#include "duckdb/optimizer/expression_rewriter.hpp"
 
 namespace duckdb {
 
