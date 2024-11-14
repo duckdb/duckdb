feaf4f9cae6070f799772eff01f884d13c3286f6
Tishj <t_b@live.nl>
2024-11-14 12:03:35 +0100

    implement non-inverted run scan logic

--- src/storage/compression/roaring.cpp
+++ src/storage/compression/roaring.cpp
@@ -680,7 +680,32 @@ public:
 			} while (false);
 			scanned_count += to_scan;
 		} else {
-			throw NotImplementedException("TODO");
+			do {
+				idx_t i = 0;
+				while (i < to_scan) {
+					// Determine the next valid position within the scan range, if available
+					idx_t valid_pos = (run_index < count) ? runs[run_index].start : scanned_count + to_scan;
+					idx_t valid_start = valid_pos - scanned_count;
+
+					if (i < valid_start) {
+						// FIXME: optimize this to group the SetInvalid calls
+						// These bits are all set to 0
+						idx_t invalid_end = MinValue<idx_t>(valid_start, to_scan);
+						for (idx_t j = i; j < invalid_end; j++) {
+							result_mask.SetInvalid(result_offset + j);
+						}
+						i = invalid_end;
+					}
+
+					if (i == valid_start && i < to_scan && run_index < count) {
+						idx_t valid_end = MinValue<idx_t>(i + 1 + runs[run_index].length, to_scan);
+						// These bits are already set, no action required
+						i = valid_end;
+						run_index++;
+					}
+				}
+			} while (false);
+			scanned_count += to_scan;
 		}
 	}
 
