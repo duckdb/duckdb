631edff2ae620a75f0b0e923aae6002cc8d132c8
Tmonster <tom@ebergen.com>
2024-11-20 17:16:12 +0100

    ff

--- src/optimizer/empty_result_pullup.cpp
+++ src/optimizer/empty_result_pullup.cpp
@@ -9,8 +9,7 @@ namespace duckdb {
 unique_ptr<LogicalOperator> EmptyResultPullup::PullUpEmptyJoinChildren(unique_ptr<LogicalOperator> op) {
 	JoinType join_type = JoinType::INVALID;
 	D_ASSERT(op->type == LogicalOperatorType::LOGICAL_COMPARISON_JOIN ||
-	         op->type == LogicalOperatorType::LOGICAL_ANY_JOIN ||
-	         op->type == LogicalOperatorType::LOGICAL_DELIM_JOIN ||
+	         op->type == LogicalOperatorType::LOGICAL_ANY_JOIN || op->type == LogicalOperatorType::LOGICAL_DELIM_JOIN ||
 	         op->type == LogicalOperatorType::LOGICAL_EXCEPT);
 	switch (op->type) {
 	case LogicalOperatorType::LOGICAL_DELIM_JOIN:
