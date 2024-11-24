9c5a682f5684a690da4b0569c9868fd2ac3f3e2f
Mario Taddeucci <mariotaddeucci@gmx.com>
2024-11-24 15:21:33 -0300

    Fix unit test

--- tools/pythonpkg/tests/fast/spark/test_spark_dataframe_cache.py
+++ tools/pythonpkg/tests/fast/spark/test_spark_dataframe_cache.py
@@ -8,8 +8,8 @@ import spark_namespace.sql.functions as F
 class TestDataFrameCache(object):
 
     def test_cache(self, spark):
-        df_base = spark.range(1, 10).select(F.rand().alias("rand"))
+        df_base = spark.range(0, 10).select(F.rand().alias("rand"))
         df_cached = df_base.cache()
 
-        assert df_base.union(df_base).distinct().count() == 20
-        assert df_cached.union(df_cached).distinct().count() == 10
+        assert df_base.union(df_base).union(df_base).distinct().count() > 10
+        assert df_cached.union(df_cached).union(df_cached).distinct().count() == 10
