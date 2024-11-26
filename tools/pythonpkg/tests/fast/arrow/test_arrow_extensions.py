dd341d7b30441a5a22f336ea7c00d4c4ca9e5dbd
pdet <pedroholanda@gmail.com>
2024-11-26 11:29:08 +0100

    Adding basic test

--- tools/pythonpkg/tests/fast/arrow/test_arrow_extensions.py
+++ tools/pythonpkg/tests/fast/arrow/test_arrow_extensions.py
@@ -250,3 +250,13 @@ class TestCanonicalExtensionTypes(object):
                 '179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368',
             )
         ]
+
+    def test_nested_types_with_extensions(self):
+        duckdb_cursor = duckdb.connect()
+        duckdb_cursor.execute("SET arrow_lossless_conversion = true")
+
+        arrow_table = duckdb_cursor.execute("select map {uuid(): 1::uhugeint, uuid(): 2::uhugeint} as li").arrow()
+
+        assert arrow_table.schema[0].type.key_type.extension_name == "arrow.uuid"
+        assert arrow_table.schema[0].type.item_type.extension_name == "arrow.opaque"
+
