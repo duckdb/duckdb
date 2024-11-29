8f63445fe846cf11b270e62b545cad6ebf703cfc
Sam Ansmink <samansmink@hotmail.com>
2024-11-29 10:11:51 +0100

    missing file

---
+++ src/function/table/system/duckdb_log.cpp
@@ -0,0 +1,59 @@
+#include "duckdb/function/table/system_functions.hpp"
+
+#include "duckdb/catalog/catalog.hpp"
+#include "duckdb/catalog/catalog_entry/schema_catalog_entry.hpp"
+#include "duckdb/common/exception.hpp"
+#include "duckdb/main/client_context.hpp"
+#include "duckdb/main/client_data.hpp"
+#include "duckdb/logging/logger.hpp"
+
+namespace duckdb {
+
+struct DuckDBLogData : public GlobalTableFunctionState {
+	DuckDBLogData(ColumnDataCollection &collection_p) : collection(collection_p), state() {
+		collection.InitializeScan(state);
+	}
+
+	ColumnDataCollection &collection;
+	ColumnDataScanState state;
+};
+
+static unique_ptr<FunctionData> DuckDBLogBind(ClientContext &context, TableFunctionBindInput &input,
+                                                  vector<LogicalType> &return_types, vector<string> &names) {
+	names.emplace_back("log_level");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	names.emplace_back("type");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+	names.emplace_back("message");
+	return_types.emplace_back(LogicalType::VARCHAR);
+
+
+	const char* format_string = "formatted string: '%s'";
+	Logger::Info(context, "my first log message");
+	Logger::Warn(context, format_string, "my first formatted log message");
+	Logger::Debug(*context.db, format_string, "using the global logger");
+	Logger::Info("custom_log_type", *context.db, format_string, "this has a custom log type, fancy!");
+	Logger::Info("using_callback", context, [&](){
+		return StringUtil::Format(format_string, "using explicit callback");
+	});
+
+	return nullptr;
+}
+
+unique_ptr<GlobalTableFunctionState> DuckDBLogInit(ClientContext &context, TableFunctionInitInput &input) {
+	auto result = make_uniq<DuckDBLogData>(*context.db->GetLogManager().log_storage->log_entries);
+	return std::move(result);
+}
+
+void DuckDBLogFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
+	auto &data = data_p.global_state->Cast<DuckDBLogData>();
+	data.collection.Scan(data.state, output);
+}
+
+void DuckDBLogFun::RegisterFunction(BuiltinFunctions &set) {
+	set.AddFunction(TableFunction("duckdb_logs", {}, DuckDBLogFunction, DuckDBLogBind, DuckDBLogInit));
+}
+
+} // namespace duckdb
