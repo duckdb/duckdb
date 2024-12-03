8fc4dffaf31cc54ef44eaa328c7e0bfefe098848
Leandro Costantino <lcostant@cisco.com>
2024-12-03 09:36:11 -0300

    Remove count

--- third_party/httplib/httplib.hpp
+++ third_party/httplib/httplib.hpp
@@ -2515,7 +2515,7 @@ inline std::string decode_url(const std::string &s,
         auto val = 0;
         if (from_hex_to_i(s, i + 1, 2, val)) {
           const auto converted = static_cast<char>(val);
-          if (exclude.empty() || exclude.count(converted) == 0) {
+          if (exclude.count(converted) == 0) {
             result += converted;
           } else {
             result.append(s, i, 3);
