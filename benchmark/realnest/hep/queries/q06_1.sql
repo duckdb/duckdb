964a4d4f331c81a57629d9b1076a0f1574f15d2a
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-12-04 09:56:57 +0100

    add helper functions to the load.sql, add working (thanks to @Tmonster) q06 queries

--- benchmark/realnest/hep/queries/q06_1.sql
+++ benchmark/realnest/hep/queries/q06_1.sql
@@ -1,69 +1,19 @@
-WITH xyze_jets AS (
-  SELECT
-    rowid,
-    list_transform(JET,
-              j -> CAST(ROW(j.pt,
-                            j.pt * cos(j.phi),
-                            j.pt * sin(j.phi),
-                            j.pt * ( ( exp(j.eta) - exp(-j.eta) ) / 2.0 ),
-                            sqrt(j.pt * cosh(j.eta) * j.pt * cosh(j.eta) + j.mASs * j.mASs)) AS
-                        ROW (pt REAL, x REAL, y REAL, z REAL, e REAL))) AS Jet
-  FROM hep_singleMu
-),
-tri_jets AS (
-    WITH m AS (SELECT unnest(Jet) AS m FROM hep_singleMu)
-    SELECT m1, m2, m3, idx1, idx2, idx3
-    FROM (
-         SELECT row_number() OVER (PARTITION BY rowid) idx1, m1
-         FROM (SELECT rowid, unnest(jet) AS m1 FROM xyze_jets)
-    ) AS _m1
-    CROSS JOIN (
-        SELECT row_number() OVER (PARTITION BY rowid) idx2, m2
-        FROM (SELECT rowid, unnest(jet) AS m2 FROM xyze_jets)
-    ) AS _m2
-    CROSS JOIN (
-        SELECT row_number() OVER (PARTITION BY rowid) idx3, m3
-        FROM (SELECT rowid, unnest(jet) AS m3 FROM xyze_jets)
-    ) AS _m3
-    WHERE idx1 < idx2 AND idx2 < idx3
-),
-condensed_tri_jet AS (
-  SELECT
-    m1, m2, m3, idx1, idx2, idx3,
-    m1.x + m2.x + m3.x AS x,
-    m1.y + m2.y + m3.y AS y,
-    m1.z + m2.z + m3.z AS z,
-    m1.e + m2.e + m3.e AS e,
-    (m1.x + m2.x + m3.x) * (m1.x + m2.x + m3.x) AS x2,
-    (m1.y + m2.y + m3.y) * (m1.y + m2.y + m3.y) AS y2,
-    (m1.z + m2.z + m3.z) * (m1.z + m2.z + m3.z) AS z2,
-    (m1.e + m2.e + m3.e) * (m1.e + m2.e + m3.e) AS e2
-  FROM tri_jets
-),
-singular_system AS (
-  SELECT
-    idx1, idx2, idx3,
-    min_by(
-      sqrt(x2 + y2),
-      abs(172.5 - sqrt(e2 - x2 - y2 - z2))
-    ) AS tri_jet_pt
-  FROM condensed_tri_jet
-  GROUP BY idx1, idx2, idx3
-)
-SELECT
-  FLOOR((
-    CASE
-      WHEN tri_jet_pt < 15 THEN 14.99
-      WHEN tri_jet_pt > 40 THEN 40.01
-      ELSE tri_jet_pt
-    END) / 0.25) * 0.25 + 0.125 AS x,
-  COUNT(*) AS y
-FROM singular_system
-GROUP BY FLOOR((
-    CASE
-      WHEN tri_jet_pt < 15 THEN 14.99
-      WHEN tri_jet_pt > 40 THEN 40.01
-      ELSE tri_jet_pt
-    END) / 0.25) * 0.25 + 0.125
+WITH trijets_invariant_mass AS materialized (SELECT
+    row_id_i,
+    AddPtEtaPhiM3({'pt':j1.pt, 'eta':j1.eta, 'phi':j1.phi, 'mass':j1.mass},
+        {'pt':j2.pt, 'eta':j2.eta, 'phi':j2.phi, 'mass':j2.mass},
+        {'pt':j3.pt, 'eta':j3.eta, 'phi':j3.phi, 'mass':j3.mass}) AS triJet,
+    abs(triJet['mass'] - 172.5) AS invariant_mass
+FROM (SELECT j1, row_number() OVER(PARTITION BY row_id) i, row_id row_id_i FROM (SELECT UNNEST(Jet) j1, rowid row_id FROM hep_singleMu WHERE array_length(jet) >= 3)),
+   (SELECT j2, row_number() OVER(PARTITION BY row_id) j, row_id row_id_j FROM (SELECT UNNEST(Jet) j2, rowid row_id FROM hep_singleMu WHERE array_length(jet) >= 3)),
+   (SELECT j3, row_number() OVER(PARTITION BY row_id) k, row_id row_id_k FROM (SELECT UNNEST(Jet) j3, rowid row_id FROM hep_singleMu WHERE array_length(jet) >= 3))
+WHERE i < j AND j < k AND row_id_i = row_id_j AND row_id_j = row_id_k
+ORDER BY invariant_mass ASC)
+SELECT 
+    HistogramBin(trijets_invariant_mass.triJet['pt'], 15, 40, 100) AS x, 
+    count(*) AS y
+FROM trijets_invariant_mass
+WHERE 
+    invariant_mass IN (SELECT min(tim_2.invariant_mass) FROM trijets_invariant_mass tim_2 GROUP BY tim_2.row_id_i)
+GROUP BY x
 ORDER BY x;
-
--- benchmark/realnest/hep/queries/q06_2.sql
+++ benchmark/realnest/hep/queries/q06_2.sql
@@ -1,68 +1,20 @@
-WITH xyze_jets AS (
-  SELECT
-    rowid,
-    list_transform(JET,
-              j -> CAST(ROW(j.btag,
-                            j.pt * cos(j.phi),
-                            j.pt * sin(j.phi),
-                            j.pt * ( ( exp(j.eta) - exp(-j.eta) ) / 2.0 ),
-                            sqrt(j.pt * cosh(j.eta) * j.pt * cosh(j.eta) + j.mASs * j.mASs)) AS
-                        ROW (btag REAL, x REAL, y REAL, z REAL, e REAL))) AS Jet
-  FROM hep_singleMu
-),
-tri_jets AS (
-  WITH m AS (SELECT unnest(Jet) AS m FROM hep_singleMu)
-  SELECT m1, m2, m3, idx1, idx2, idx3
-  FROM (
-    SELECT row_number() OVER (Partition BY rowid) idx1, m1
-    FROM (SELECT rowid, unnest(jet) AS m1 FROM xyze_jets)
-  ) AS _m1
-  CROSS JOIN (
-    SELECT row_number() OVER (Partition BY rowid) idx2, m2
-    FROM (SELECT rowid, unnest(jet) AS m2 FROM xyze_jets)
-  ) AS _m2
-  CROSS JOIN (
-    SELECT row_number() OVER (Partition BY rowid) idx3, m3
-    FROM (SELECT rowid, unnest(jet) AS m3 FROM xyze_jets)
-  ) AS _m3
-  WHERE idx1 < idx2 AND idx2 < idx3
-),
-condensed_tri_jet AS (
-  SELECT
-    idx1, idx2, idx3, m1, m2, m3,
-    m1.x + m2.x + m3.x AS x,
-    m1.y + m2.y + m3.y AS y,
-    m1.z + m2.z + m3.z AS z,
-    m1.e + m2.e + m3.e AS e,
-    (m1.x + m2.x + m3.x) * (m1.x + m2.x + m3.x) AS x2,
-    (m1.y + m2.y + m3.y) * (m1.y + m2.y + m3.y) AS y2,
-    (m1.z + m2.z + m3.z) * (m1.z + m2.z + m3.z) AS z2,
-    (m1.e + m2.e + m3.e) * (m1.e + m2.e + m3.e) AS e2
-  FROM tri_jets
-),
-singular_system AS (
-  SELECT
-    idx1, idx2, idx3,
-    min_by(
-        list_max([m1.btag, m2.btag, m3.btag]),
-        abs(172.5 - sqrt(e2 - x2 - y2 - z2))
-    ) AS btag
-  FROM condensed_tri_jet
-  GROUP BY idx1, idx2, idx3
-)
-SELECT
-    CAST((
-     CASE
-         WHEN btag < 0 THEN -0.005
-         WHEN btag > 1 THEN 1.005
-         ELSE btag
-         END - 0.005) / 0.01 AS BIGINT) * 0.01 + 0.005 AS x,
-    COUNT(*) AS y
-FROM singular_system
-GROUP BY CAST((
-  CASE
-      WHEN btag < 0 THEN -0.005
-      WHEN btag > 1 THEN 1.005
-      ELSE btag
-      END - 0.005) / 0.01 AS BIGINT) * 0.01 + 0.005
+WITH trijets_b_tag_dval AS materialized (SELECT
+    row_id_i,
+    list_max(list_transform([j1, j2, j3], x -> x.btag)) AS b_tag_dval,
+    AddPtEtaPhiM3({'pt':j1.pt, 'eta':j1.eta, 'phi':j1.phi, 'mass':j1.mass},
+        {'pt':j2.pt, 'eta':j2.eta, 'phi':j2.phi, 'mass':j2.mass},
+        {'pt':j3.pt, 'eta':j3.eta, 'phi':j3.phi, 'mass':j3.mass}) AS triJet,
+    abs(triJet['mass'] - 172.5) AS invariant_mass
+FROM (SELECT j1, row_number() OVER(Partition BY row_id) i, row_id row_id_i FROM (SELECT UNNEST(Jet) j1, rowid row_id FROM hep_singleMu WHERE array_length(jet) >= 3)),
+   (SELECT j2, row_number() OVER(Partition BY row_id) j, row_id row_id_j FROM (SELECT UNNEST(Jet) j2, rowid row_id FROM hep_singleMu WHERE array_length(jet) >= 3)),
+   (SELECT j3, row_number() OVER(Partition BY row_id) k, row_id row_id_k FROM (SELECT UNNEST(Jet) j3, rowid row_id FROM hep_singleMu WHERE array_length(jet) >= 3))
+WHERE i < j AND j < k AND row_id_i = row_id_j AND row_id_j = row_id_k
+ORDER BY invariant_mass asc)
+SELECT 
+    round(HistogramBin(trijets_b_tag_dval.b_tag_dval, 0, 1, 100),3) AS x, 
+    count(*) AS y
+FROM trijets_b_tag_dval
+WHERE 
+    invariant_mass in (SELECT min(tim_2.invariant_mass) FROM trijets_b_tag_dval tim_2 GROUP BY tim_2.row_id_i)
+GROUP BY x
 ORDER BY x;
