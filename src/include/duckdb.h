854a13cea94cd8d97f36906976615e8b47dd7a0e
Giorgi Dalakishvili <giorgi@giorgi.dev>
2024-12-05 01:23:06 +0400

    Fix header generation

--- src/include/duckdb.h
+++ src/include/duckdb.h
@@ -3809,7 +3809,8 @@ DUCKDB_API duckdb_state duckdb_append_default(duckdb_appender appender);
 
 /*!
 Append a DEFAULT value (NULL if DEFAULT not available for column) to the appender.
-The default value of the column must be a constant value. Non-deterministic expressions like nextval('seq') or random() are not supported.
+The default value of the column must be a constant value. Non-deterministic expressions like nextval('seq') or random()
+are not supported.
 
 * @param appender The appender to get the default value from.
 * @param chunk The data chunk to append the default value to.
@@ -3817,7 +3818,8 @@ The default value of the column must be a constant value. Non-deterministic expr
 * @param column The chunk column index to append the default value to.
 * @return `DuckDBSuccess` on success or `DuckDBError` on failure.
 */
-DUCKDB_API duckdb_state duckdb_append_default_to_chunk(duckdb_appender appender, duckdb_data_chunk chunk, idx_t row, idx_t column);
+DUCKDB_API duckdb_state duckdb_append_default_to_chunk(duckdb_appender appender, duckdb_data_chunk chunk, idx_t row,
+                                                       idx_t column);
 
 /*!
 Append a bool value to the appender.
