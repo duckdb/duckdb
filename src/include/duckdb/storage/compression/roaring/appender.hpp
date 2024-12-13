561adb4a25d7ec06cead4a1ddd7921999a9df5e8
Tishj <t_b@live.nl>
2024-12-13 14:01:08 +0100

    append small vectors

--- src/include/duckdb/storage/compression/roaring/appender.hpp
+++ src/include/duckdb/storage/compression/roaring/appender.hpp
@@ -69,10 +69,45 @@ public:
 				idx_t to_append =
 				    MinValue<idx_t>(ROARING_CONTAINER_SIZE - STATE_TYPE::Count(state), input_size - appended);
 
+				// Deal with a ragged start, when 'appended' isn't a multiple of ValidityMask::BITS_PER_VALUE
+				// then we need to process the remainder of the entry
+				idx_t entry_offset = appended / ValidityMask::BITS_PER_VALUE;
+				auto previous_entry_remainder = appended % ValidityMask::BITS_PER_VALUE;
+				if (DUCKDB_UNLIKELY(previous_entry_remainder != 0)) {
+					auto validity_entry = validity.GetValidityEntry(entry_offset);
+
+					idx_t to_process = ValidityMask::BITS_PER_VALUE - previous_entry_remainder;
+					validity_t mask;
+					if (to_append < to_process) {
+						// Limit the amount of bits to set
+						auto left_bits = to_process - to_append;
+						to_process = to_append;
+						mask = ValidityUncompressed::UPPER_MASKS[to_process] >> left_bits;
+					} else {
+						mask = ValidityUncompressed::UPPER_MASKS[to_process];
+					}
+
+					auto masked_entry = (validity_entry & mask) >> previous_entry_remainder;
+					if (masked_entry == ValidityUncompressed::LOWER_MASKS[to_process]) {
+						// All bits are set
+						STATE_TYPE::HandleAllValid(state, to_process);
+					} else if (masked_entry == 0) {
+						// None of the bits are set
+						STATE_TYPE::HandleNoneValid(state, to_process);
+					} else {
+						// Mixed set/unset bits
+						AppendBytes(state, masked_entry, to_process);
+					}
+
+					to_append -= to_process;
+					appended += to_process;
+					entry_offset++;
+				}
+
 				auto entry_count = to_append / ValidityMask::BITS_PER_VALUE;
 				for (idx_t entry_index = 0; entry_index < entry_count; entry_index++) {
 					// get the validity entry at this index
-					auto validity_entry = validity.GetValidityEntry(entry_index);
+					auto validity_entry = validity.GetValidityEntry(entry_offset + entry_index);
 					if (ValidityMask::AllValid(validity_entry)) {
 						// All bits are set
 						STATE_TYPE::HandleAllValid(state, ValidityMask::BITS_PER_VALUE);
@@ -88,7 +123,7 @@ public:
 				// Deal with a ragged end, when the validity entry isn't entirely used
 				idx_t remainder = to_append % ValidityMask::BITS_PER_VALUE;
 				if (DUCKDB_UNLIKELY(remainder != 0)) {
-					auto validity_entry = validity.GetValidityEntry(entry_count);
+					auto validity_entry = validity.GetValidityEntry(entry_offset + entry_count);
 					auto masked = validity_entry & ValidityUncompressed::LOWER_MASKS[remainder];
 					if (masked == ValidityUncompressed::LOWER_MASKS[remainder]) {
 						// All bits are set
