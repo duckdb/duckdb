6c931db8571704703201b4bfb2bffb173147c5e0
Richard Wesley <13156216+hawkfish@users.noreply.github.com>
2024-12-17 13:34:07 -0800

    Feature #12699: NTILE Secondary Sorts
    * Make WindowNtileExecutor a subclass WindowRowNumberExecutor, not WindowValueExecutor.
    * Don't add the NTILE argument to the collection (it is just per-row)
    * Compute the row number from the sort, if requested.

--- src/include/duckdb/function/window/window_rownumber_function.hpp
+++ src/include/duckdb/function/window/window_rownumber_function.hpp
@@ -20,6 +20,19 @@ public:
 	                                                     const ValidityMask &order_mask) const override;
 	unique_ptr<WindowExecutorLocalState> GetLocalState(const WindowExecutorGlobalState &gstate) const override;
 
+	//! The evaluation index of the NTILE column
+	column_t ntile_idx = DConstants::INVALID_INDEX;
+
+protected:
+	void EvaluateInternal(WindowExecutorGlobalState &gstate, WindowExecutorLocalState &lstate, DataChunk &eval_chunk,
+	                      Vector &result, idx_t count, idx_t row_idx) const override;
+};
+
+// NTILE is just scaled ROW_NUMBER
+class WindowNtileExecutor : public WindowRowNumberExecutor {
+public:
+	WindowNtileExecutor(BoundWindowExpression &wexpr, ClientContext &context, WindowSharedExpressions &shared);
+
 protected:
 	void EvaluateInternal(WindowExecutorGlobalState &gstate, WindowExecutorLocalState &lstate, DataChunk &eval_chunk,
 	                      Vector &result, idx_t count, idx_t row_idx) const override;
--- src/parser/transform/expression/transform_function.cpp
+++ src/parser/transform/expression/transform_function.cpp
@@ -118,13 +118,13 @@ static bool IsOrderableWindowFunction(ExpressionType type) {
 	case ExpressionType::WINDOW_RANK:
 	case ExpressionType::WINDOW_PERCENT_RANK:
 	case ExpressionType::WINDOW_ROW_NUMBER:
+	case ExpressionType::WINDOW_NTILE:
 		return true;
 	case ExpressionType::WINDOW_LEAD:
 	case ExpressionType::WINDOW_LAG:
 	case ExpressionType::WINDOW_AGGREGATE:
 	case ExpressionType::WINDOW_RANK_DENSE:
 	case ExpressionType::WINDOW_CUME_DIST:
-	case ExpressionType::WINDOW_NTILE:
 		return false;
 	default:
 		throw InternalException("Unknown orderable window type %s", ExpressionTypeToString(type).c_str());
