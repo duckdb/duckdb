c5666c6e092b1a9d30d205e7cc168b060276123f
pdet <pedroholanda@gmail.com>
2024-12-19 14:17:33 +0100

    Fix for issue #14648

--- src/execution/operator/csv_scanner/sniffer/csv_sniffer.cpp
+++ src/execution/operator/csv_scanner/sniffer/csv_sniffer.cpp
@@ -6,7 +6,7 @@ namespace duckdb {
 CSVSniffer::CSVSniffer(CSVReaderOptions &options_p, shared_ptr<CSVBufferManager> buffer_manager_p,
                        CSVStateMachineCache &state_machine_cache_p, bool default_null_to_varchar_p)
     : state_machine_cache(state_machine_cache_p), options(options_p), buffer_manager(std::move(buffer_manager_p)),
-      default_null_to_varchar(default_null_to_varchar_p) {
+      lines_sniffed(0), default_null_to_varchar(default_null_to_varchar_p) {
 	// Initialize Format Candidates
 	for (const auto &format_template : format_template_candidates) {
 		auto &logical_type = format_template.first;
