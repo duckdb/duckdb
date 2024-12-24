2c9c5bb304dab23918ed79e278d5fcd3571fde91
pdet <pedroholanda@gmail.com>
2024-12-24 13:15:46 +0100

    Use system threads for parallelism on read_csv if reading from pipe

--- src/execution/operator/csv_scanner/table_function/global_csv_state.cpp
+++ src/execution/operator/csv_scanner/table_function/global_csv_state.cpp
@@ -178,7 +178,7 @@ unique_ptr<StringValueScanner> CSVGlobalState::Next(optional_ptr<StringValueScan
 
 idx_t CSVGlobalState::MaxThreads() const {
 	// We initialize max one thread per our set bytes per thread limit
-	if (single_threaded) {
+	if (single_threaded || !file_scans.front()->on_disk_file) {
 		return system_threads;
 	}
 	idx_t total_threads = file_scans.front()->file_size / CSVIterator::BYTES_PER_THREAD + 1;
