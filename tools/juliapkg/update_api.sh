579960f514c05ef60cca0f4013bc560a316b387f
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2025-01-04 15:54:45 +0100

    Add script to generate api and document usage in readme

---
+++ tools/juliapkg/update_api.sh
@@ -0,0 +1,23 @@
+set -euo pipefail
+
+
+echo "Updating api.jl..."
+
+OLD_API_FILE=tools/juliapkg/src/api_old.jl
+ORIG_DIR=$(pwd)
+GIR_ROOT_DIR=$(git rev-parse --show-toplevel)
+cd "$GIR_ROOT_DIR"
+
+
+
+# Generate the Julia API
+python tools/juliapkg/scripts/generate_c_api_julia.py \
+    --auto-1-index \
+    --base-api tools/juliapkg/src/api_old.jl \
+    --capi-dir src/include/duckdb/main/capi/header_generation \
+    tools/juliapkg/src/api.jl
+
+
+echo "Formatting..."
+cd "$ORIG_DIR"
+./format.sh
