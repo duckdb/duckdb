d5aea4838a058bf54af7f9488fb068b0fd4fa1f7
Tishj <t_b@live.nl>
2025-01-07 17:53:56 +0100

    no need to copy strings to a heap, we have already copied the data to the segment, the map is only ever referenced while the segment is alive, so we can safely reference data from the segment

--- src/include/duckdb/storage/compression/dictionary/compression.hpp
+++ src/include/duckdb/storage/compression/dictionary/compression.hpp
@@ -47,7 +47,6 @@ public:
 	data_ptr_t current_end_ptr;
 
 	// Buffers and map for current segment
-	StringHeap heap;
 	string_map_t<uint32_t> current_string_map;
 	vector<uint32_t> index_buffer;
 	vector<uint32_t> selection_buffer;
