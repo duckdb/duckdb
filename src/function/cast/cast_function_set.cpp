c9caa10d4b5925810d76d78208c17fca978d6841
Max Gabrielsson <max@gabrielsson.com>
2025-01-09 19:53:10 +0100

    remove some debug code

--- src/function/cast/cast_function_set.cpp
+++ src/function/cast/cast_function_set.cpp
@@ -150,10 +150,6 @@ public:
 			}
 		}
 
-		auto leq = target_type_entry->first == target;
-		auto req = target == target_type_entry->first;
-		D_ASSERT(leq || req);
-
 		return &target_type_entry->second;
 	}
 
