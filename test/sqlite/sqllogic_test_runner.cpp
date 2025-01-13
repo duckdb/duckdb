04a6f349ccfd12bf75d31317bacecc961062ec9b
Tishj <t_b@live.nl>
2025-01-13 22:42:56 +0100

    undo test changes

--- test/sqlite/sqllogic_test_runner.cpp
+++ test/sqlite/sqllogic_test_runner.cpp
@@ -232,6 +232,8 @@ bool SQLLogicTestRunner::ForEachTokenReplace(const string &parameter, vector<str
 		result.push_back("uncompressed");
 		result.push_back("rle");
 		result.push_back("bitpacking");
+		result.push_back("dictionary");
+		result.push_back("fsst");
 		result.push_back("dict_fsst");
 		result.push_back("alp");
 		result.push_back("alprd");
