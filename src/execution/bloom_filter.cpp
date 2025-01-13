0b07b9236f471a1f1fa88b7ecdf2139f0249b179
Andreas Zimmerer <andreas.zimmerer@utn.de>
2025-01-13 19:09:51 +0100

    working SIP

--- src/execution/bloom_filter.cpp
+++ src/execution/bloom_filter.cpp
@@ -25,35 +25,40 @@ BloomFilter::BloomFilter(size_t expected_cardinality, double desired_false_posit
 BloomFilter::~BloomFilter() {
 }
 
-inline void BloomFilter::SetBloomBitsForHashes(size_t shift, Vector &hashes, const SelectionVector &rsel, idx_t count) {
+inline size_t HashToIndex(hash_t hash, size_t bloom_filter_size, size_t i) {
+    return (hash >> (i * 2)) % bloom_filter_size;  // TODO: rotation would be a bit nicer because it allows us to generate more values. But C++20 in stdlib.
+}
+
+inline void BloomFilter::SetBloomBitsForHashes(size_t fni, Vector &hashes, const SelectionVector &rsel, idx_t count) {
     D_ASSERT(hashes.GetType().id() == LogicalType::HASH);
 
     const size_t bloom_filter_size = Bit::BitLength(bloom_filter);
 
     if (hashes.GetVectorType() == VectorType::CONSTANT_VECTOR) {
         auto hash = ConstantVector::GetData<hash_t>(hashes);
-		auto bloom_idx = (*hash >> shift) % bloom_filter_size; // TODO: rotation would be a bit nicer because it allows us to generate more values. But C++20 in stdlib.
+		auto bloom_idx = HashToIndex(*hash, bloom_filter_size, fni);
         Bit::SetBit(bloom_filter, bloom_idx, 0x1);
     } else {
-        UnifiedVectorFormat idata;
-		hashes.ToUnifiedFormat(count, idata);
+        UnifiedVectorFormat u_hashes;
+		hashes.ToUnifiedFormat(count, u_hashes);
 
-        if (!idata.validity.AllValid()) {
+        if (!u_hashes.validity.AllValid()) {
             for (idx_t i = 0; i < count; i++) {
                 auto ridx = rsel.get_index(i);
-			    auto idx = idata.sel->get_index(ridx);
-                if (idata.validity.RowIsValid(idx)) {
-                    auto hash = UnifiedVectorFormat::GetData<hash_t>(idata)[idx];
-                    auto bloom_idx = (hash >> shift) % bloom_filter_size;
+			    auto idx = u_hashes.sel->get_index(ridx);
+                if (u_hashes.validity.RowIsValid(idx)) {
+                    auto hash = UnifiedVectorFormat::GetData<hash_t>(u_hashes)[idx];
+                    auto bloom_idx = HashToIndex(hash, bloom_filter_size, fni);
                     Bit::SetBit(bloom_filter, bloom_idx, 0x1);
                 }
             }
         } else {
             for (idx_t i = 0; i < count; i++) {
                 auto ridx = rsel.get_index(i);
-			    auto idx = idata.sel->get_index(ridx);
-                auto hash = UnifiedVectorFormat::GetData<hash_t>(idata)[idx];
-                auto bloom_idx = (hash >> shift) % bloom_filter_size;
+			    auto idx = u_hashes.sel->get_index(ridx);
+                auto* hashes = UnifiedVectorFormat::GetData<hash_t>(u_hashes);
+                auto hash = hashes[idx];
+                auto bloom_idx = HashToIndex(hash, bloom_filter_size, fni);
                 Bit::SetBit(bloom_filter, bloom_idx, 0x1);
             }
         }
@@ -64,7 +69,7 @@ void BloomFilter::BuildWithPrecomputedHashes(Vector &hashes, const SelectionVect
     // Rotate hash by a couple of bits to produce a new "hash value".
     // With this trick, keys have to be hashed only once.
     for (idx_t i = 0; i < num_hash_functions; i++) {
-        auto shift = i * 6;
+        auto shift = i * 1;
         SetBloomBitsForHashes(shift, hashes, rsel, count);
     }
 }
@@ -75,7 +80,7 @@ void BloomFilter::Merge(BloomFilter &other) {
 }
 
 
-inline size_t BloomFilter::ProbeInternal(size_t shift, Vector &hashes, const SelectionVector *&current_sel, idx_t current_sel_count, SelectionVector &sel) {
+inline size_t BloomFilter::ProbeInternal(size_t fni, Vector &hashes, const SelectionVector *&current_sel, idx_t current_sel_count, SelectionVector &sel) {
     D_ASSERT(hashes.GetType().id() == LogicalType::HASH);
     D_ASSERT(current_sel_count > 0); // Should be handled before
 
@@ -85,28 +90,29 @@ inline size_t BloomFilter::ProbeInternal(size_t shift, Vector &hashes, const Sel
 
     if (hashes.GetVectorType() == VectorType::CONSTANT_VECTOR) {
         auto hash = ConstantVector::GetData<hash_t>(hashes);
-		auto bloom_idx = (*hash >> shift) % bloom_filter_size; // TODO: rotation would be a bit nicer because it allows us to generate more values. But C++20 in stdlib.
+		auto bloom_idx = HashToIndex(*hash, bloom_filter_size, fni);
 
         if (Bit::GetBit(bloom_filter, bloom_idx)) {
             // TODO: we need to set the whole 'out' vector to zero.
             return current_sel_count; // ??
         }
     } else {
-        UnifiedVectorFormat idata;
-		hashes.ToUnifiedFormat(current_sel_count, idata);
+        UnifiedVectorFormat u_hashes;
+		hashes.ToUnifiedFormat(current_sel_count, u_hashes);
 
-        if (!idata.validity.AllValid()) {
+        if (!u_hashes.validity.AllValid()) {
             size_t sel_out_idx = 0;
             for (idx_t i = 0; i < current_sel_count; i++) {
                 // TODO: We can skip this row if it was already removed by a previous iteration
-                auto ridx = current_sel->get_index(i);
-			    auto idx = idata.sel->get_index(ridx);
-                if (idata.validity.RowIsValid(idx)) {
-                    auto hash = UnifiedVectorFormat::GetData<hash_t>(idata)[idx];
-                    auto bloom_idx = (hash >> shift) % bloom_filter_size;
+                auto key_idx = current_sel->get_index(i);
+			    auto hash_idx = u_hashes.sel->get_index(key_idx);
+                if (u_hashes.validity.RowIsValid(hash_idx)) {
+                    auto* hashes = UnifiedVectorFormat::GetData<hash_t>(u_hashes);
+                    auto hash = hashes[hash_idx];
+                    auto bloom_idx = HashToIndex(hash, bloom_filter_size, fni);
                     if (Bit::GetBit(bloom_filter, bloom_idx)) {
                         // Bit is set in Bloom-filter. We keep the entry for now.
-                        sel.set_index(sel_out_idx++, ridx);
+                        sel.set_index(sel_out_idx++, key_idx);
                     }
                 }
             }
@@ -115,13 +121,14 @@ inline size_t BloomFilter::ProbeInternal(size_t shift, Vector &hashes, const Sel
         } else {
             size_t sel_out_idx = 0;
             for (idx_t i = 0; i < current_sel_count; i++) {
-                auto ridx = current_sel->get_index(i);
-			    auto idx = idata.sel->get_index(ridx);
-                auto hash = UnifiedVectorFormat::GetData<hash_t>(idata)[idx];
-                auto bloom_idx = (hash >> shift) % bloom_filter_size;
+                auto key_idx = current_sel->get_index(i);
+			    auto hash_idx = u_hashes.sel->get_index(key_idx);
+                auto* hashes = UnifiedVectorFormat::GetData<hash_t>(u_hashes);
+                auto hash = hashes[hash_idx];
+                auto bloom_idx = HashToIndex(hash, bloom_filter_size, fni);
                 if (Bit::GetBit(bloom_filter, bloom_idx)) {
                     // Bit is set in Bloom-filter. We keep the entry for now.
-                    sel.set_index(sel_out_idx++, ridx);
+                    sel.set_index(sel_out_idx++, key_idx);
                 }
             }
             current_sel = &sel;
@@ -139,8 +146,10 @@ size_t BloomFilter::Probe(DataChunk &keys, const SelectionVector *&current_sel, 
     size_t sel_out_count = count;
 
     for (idx_t i = 0; i < num_hash_functions; i++) {
-        auto shift = i * 6;
-        sel_out_count = ProbeInternal(shift, *precomputed_hashes, current_sel, sel_out_count, sel);
+        sel_out_count = ProbeInternal(i, *precomputed_hashes, current_sel, sel_out_count, sel);
+        if (sel_out_count != count ) {
+             return sel_out_count;
+        }
         if (sel_out_count == 0) {
             return 0;
         }
--- src/execution/operator/join/physical_hash_join.cpp
+++ src/execution/operator/join/physical_hash_join.cpp
@@ -886,8 +886,8 @@ OperatorResultType PhysicalHashJoin::ExecuteInternal(ExecutionContext &context, 
 			if (state.scan_structure.count != 0) {
 				Vector hashes(LogicalType::HASH);
 				sink.hash_table->Hash(state.lhs_join_keys, *current_sel, state.scan_structure.count, hashes);
-				size_t sel_rows = sink.bloom_filter->Probe(state.lhs_join_keys, current_sel, state.scan_structure.count, state.scan_structure.sel_vector, hashes);
-				state.scan_structure.count = sel_rows;
+				//size_t sel_rows = sink.bloom_filter->Probe(state.lhs_join_keys, current_sel, state.scan_structure.count, state.scan_structure.sel_vector, hashes);
+				//state.scan_structure.count = sel_rows;
 				if (state.scan_structure.count != 0) {
 					sink.hash_table->Probe(state.scan_structure, state.lhs_join_keys, state.join_key_state, state.probe_state, current_sel, hashes);
 				}
