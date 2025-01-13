7252ebdec11c38c13cc6c1abc530215f0af2a79b
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-01-13 23:51:42 +0100

    Allow storage version 65
    This adds the possibility for DuckDB to read (and modify) files of a new storage version.
    Those files are (at this moment) identical to files version 64, with a significant change
    that they can't be opened by previous DuckDB version.
    
    This would make possible to guard features touching storage that are incompatible with previous
    DuckDB versions, such as improved compression.
    
    The meaning of the data in src/storage/version_map.json changes from being the exact storage
    version produced by a given DuckDB version to the maxium storage version produced.
    Note that this is compatible with previous interpretation, where the LOWER and UPPER
    bounds happened to be the same.
    
    Note that opening files with storage_version = 65 with older duckdb versions will produce
    error messages that point to https://duckdb.org/docs/internals/storage.html, where this is
    documented.

--- src/storage/single_file_block_manager.cpp
+++ src/storage/single_file_block_manager.cpp
@@ -59,27 +59,25 @@ MainHeader MainHeader::Read(ReadStream &source) {
 	}
 	header.version_number = source.Read<uint64_t>();
 	// check the version number
-	if (header.version_number != VERSION_NUMBER) {
+	if (header.version_number < VERSION_NUMBER_LOWER || header.version_number > VERSION_NUMBER_UPPER) {
 		auto version = GetDuckDBVersion(header.version_number);
 		string version_text;
 		if (!version.empty()) {
 			// known version
 			version_text = "DuckDB version " + string(version);
 		} else {
-			version_text = string("an ") + (VERSION_NUMBER > header.version_number ? "older development" : "newer") +
+			version_text = string("an ") +
+			               (VERSION_NUMBER_UPPER > header.version_number ? "older development" : "newer") +
 			               string(" version of DuckDB");
 		}
 		throw IOException(
-		    "Trying to read a database file with version number %lld, but we can only read version %lld.\n"
+		    "Trying to read a database file with version number %lld, but we can only read versions between %lld and "
+		    "%lld.\n"
 		    "The database file was created with %s.\n\n"
-		    "The storage of DuckDB is not yet stable; newer versions of DuckDB cannot read old database files and "
-		    "vice versa.\n"
-		    "The storage will be stabilized when version 1.0 releases.\n\n"
-		    "For now, we recommend that you load the database file in a supported version of DuckDB, and use the "
-		    "EXPORT DATABASE command "
-		    "followed by IMPORT DATABASE on the current version of DuckDB.\n\n"
-		    "See the storage page for more information: https://duckdb.org/internals/storage",
-		    header.version_number, VERSION_NUMBER, version_text);
+		    "Newer DuckDB version might introduce backward incompatible changes (possibly guarded by compatibility "
+		    "settings)"
+		    "See the storage page for migration strategy and more informations: https://duckdb.org/internals/storage",
+		    header.version_number, VERSION_NUMBER_LOWER, VERSION_NUMBER_UPPER, version_text);
 	}
 	// read the flags
 	for (idx_t i = 0; i < FLAG_COUNT; i++) {
@@ -179,6 +177,8 @@ void SingleFileBlockManager::CreateNewDatabase() {
 
 	MainHeader main_header;
 	main_header.version_number = VERSION_NUMBER;
+	D_ASSERT(main_header.version_number >= VERSION_NUMBER_LOWER);
+	D_ASSERT(main_header.version_number <= VERSION_NUMBER_UPPER);
 	memset(main_header.flags, 0, sizeof(uint64_t) * MainHeader::FLAG_COUNT);
 
 	SerializeHeaderStructure<MainHeader>(main_header, header_buffer.buffer);
