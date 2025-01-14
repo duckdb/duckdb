4068b9761ab91532d2bbf3203b078d895df5d257
Mark Raasveldt <mark.raasveldt@gmail.com>
2025-01-14 23:16:59 +0100

    Copy

--- src/planner/binder/statement/bind_execute.cpp
+++ src/planner/binder/statement/bind_execute.cpp
@@ -51,8 +51,8 @@ BoundStatement Binder::Bind(ExecuteStatement &stmt) {
 			parameter_data = BoundParameterData(std::move(constant.value), std::move(return_type));
 		} else {
 			auto value = ExpressionExecutor::EvaluateScalar(context, *bound_expr, true);
-			auto &value_type = value.type();
-			parameter_data = BoundParameterData(std::move(value), value_type);
+			auto value_type = value.type();
+			parameter_data = BoundParameterData(std::move(value), std::move(value_type));
 		}
 		bind_values[pair.first] = std::move(parameter_data);
 	}
