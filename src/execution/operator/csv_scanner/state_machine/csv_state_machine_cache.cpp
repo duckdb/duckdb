4de1d44a4e554d8774e5195dc8fc002035ea5d5f
pdet <pedroholanda@gmail.com>
2025-01-22 16:43:35 +0100

    More restrictness

--- src/execution/operator/csv_scanner/state_machine/csv_state_machine_cache.cpp
+++ src/execution/operator/csv_scanner/state_machine/csv_state_machine_cache.cpp
@@ -42,6 +42,15 @@ void CSVStateMachineCache::Insert(const CSVStateMachineOptions &state_machine_op
 		case CSVState::COMMENT:
 			InitializeTransitionArray(transition_array, cur_state, CSVState::COMMENT);
 			break;
+		case CSVState::CARRIAGE_RETURN:
+			if (state_machine_options.rfc_4180.GetValue()) {
+				// If we have an unquoted state, following rfc 4180, our base state is invalid
+				InitializeTransitionArray(transition_array, cur_state, CSVState::INVALID);
+			} else {
+				// This will allow us to accept unescaped quotes
+				InitializeTransitionArray(transition_array, cur_state, CSVState::STANDARD);
+			}
+			break;
 		default:
 			InitializeTransitionArray(transition_array, cur_state, CSVState::STANDARD);
 			break;
@@ -77,8 +86,7 @@ void CSVStateMachineCache::Insert(const CSVStateMachineOptions &state_machine_op
 				transition_array[static_cast<uint8_t>('\n')][state] = CSVState::STANDARD;
 			} else if (!state_machine_options.rfc_4180.GetValue()) {
 				transition_array[static_cast<uint8_t>('\n')][state] = CSVState::RECORD_SEPARATOR;
-			}
-			else {
+			} else {
 				transition_array[static_cast<uint8_t>('\n')][state] = CSVState::INVALID;
 			}
 		} else if (new_line_id == NewLineIdentifier::SINGLE_N) {
@@ -197,12 +205,34 @@ void CSVStateMachineCache::Insert(const CSVStateMachineOptions &state_machine_op
 	if (enable_unquoted_escape) {
 		transition_array[escape][static_cast<uint8_t>(CSVState::RECORD_SEPARATOR)] = CSVState::UNQUOTED_ESCAPE;
 	}
+	if (state_machine_options.rfc_4180.GetValue()) {
+		// strict rules to error on the new line delimiter if rfc4180 is on
+		switch (new_line_id) {
+		case NewLineIdentifier::CARRY_ON:
+		case NewLineIdentifier::SINGLE_R:
+			transition_array[static_cast<uint8_t>('\n')][static_cast<uint8_t>(CSVState::RECORD_SEPARATOR)] =
+			    CSVState::INVALID;
+			break;
+		case NewLineIdentifier::SINGLE_N:
+			transition_array[static_cast<uint8_t>('\r')][static_cast<uint8_t>(CSVState::RECORD_SEPARATOR)] =
+			    CSVState::INVALID;
+			break;
+		default:
+			break;
+		}
+	}
 
 	// 4) Carriage Return State
 	transition_array[static_cast<uint8_t>('\n')][static_cast<uint8_t>(CSVState::CARRIAGE_RETURN)] =
 	    CSVState::RECORD_SEPARATOR;
-	transition_array[static_cast<uint8_t>('\r')][static_cast<uint8_t>(CSVState::CARRIAGE_RETURN)] =
-	    CSVState::CARRIAGE_RETURN;
+	if (state_machine_options.rfc_4180.GetValue()) {
+		transition_array[static_cast<uint8_t>('\r')][static_cast<uint8_t>(CSVState::CARRIAGE_RETURN)] =
+		    CSVState::INVALID;
+	} else {
+		transition_array[static_cast<uint8_t>('\r')][static_cast<uint8_t>(CSVState::CARRIAGE_RETURN)] =
+		    CSVState::CARRIAGE_RETURN;
+	}
+
 	if (quote != '\0') {
 		transition_array[quote][static_cast<uint8_t>(CSVState::CARRIAGE_RETURN)] = CSVState::QUOTED;
 	}
