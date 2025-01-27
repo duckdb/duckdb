dadbb264ef916fa95941f1a50ad4543839f1cac6
pdet <pedroholanda@gmail.com>
2025-01-27 14:41:52 +0100

    Adjustments

--- src/execution/operator/csv_scanner/state_machine/csv_state_machine_cache.cpp
+++ src/execution/operator/csv_scanner/state_machine/csv_state_machine_cache.cpp
@@ -206,7 +206,7 @@ void CSVStateMachineCache::Insert(const CSVStateMachineOptions &state_machine_op
 		transition_array[escape][static_cast<uint8_t>(CSVState::RECORD_SEPARATOR)] = CSVState::UNQUOTED_ESCAPE;
 	}
 	if (state_machine_options.strict_mode.GetValue()) {
-		// strict rules to error on the new line delimiter if rfc4180 is on
+		// strict rules to error on the new line delimiter
 		switch (new_line_id) {
 		case NewLineIdentifier::CARRY_ON:
 		case NewLineIdentifier::SINGLE_R:
