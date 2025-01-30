cb53c3aba956ab26b8339e365eca90b064c81e6c
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2025-01-30 13:54:10 +0100

    bind index after adding it to the storage

--- src/execution/operator/persistent/physical_copy_database.cpp
+++ src/execution/operator/persistent/physical_copy_database.cpp
@@ -13,6 +13,8 @@
 #include "duckdb/parser/parsed_data/create_index_info.hpp"
 #include "duckdb/execution/index/unbound_index.hpp"
 #include "duckdb/storage/data_table.hpp"
+#include "duckdb/planner/binder.hpp"
+#include "duckdb/planner/expression_binder/index_binder.hpp"
 
 namespace duckdb {
 
@@ -82,7 +84,10 @@ SourceResultType PhysicalCopyDatabase::GetData(ExecutionContext &context, DataCh
 		storage_info.options.emplace("v1_0_0_storage", false);
 		auto unbound_index = make_uniq<UnboundIndex>(create_index_info.Copy(), storage_info,
 		                                             data_table.GetTableIOManager(), catalog.GetAttached());
+
 		data_table.AddIndex(std::move(unbound_index));
+		auto &data_table_info = *data_table.GetDataTableInfo();
+		data_table_info.GetIndexes().InitializeIndexes(context.client, data_table_info);
 	}
 
 	return SourceResultType::FINISHED;
