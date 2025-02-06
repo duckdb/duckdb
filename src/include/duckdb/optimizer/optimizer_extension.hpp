2fa7485b99f053933520525e7fe3e6a0d95b678c
Niclas Haderer <niclas.haderer.dev@gmail.com>
2025-02-06 17:41:57 +0100

    Require optimizer extensions to have names

--- src/include/duckdb/optimizer/optimizer_extension.hpp
+++ src/include/duckdb/optimizer/optimizer_extension.hpp
@@ -8,7 +8,6 @@
 
 #pragma once
 
-#include "duckdb/common/common.hpp"
 #include "duckdb/planner/logical_operator.hpp"
 
 namespace duckdb {
@@ -18,8 +17,9 @@ class ClientContext;
 
 //! The OptimizerExtensionInfo holds static information relevant to the optimizer extension
 struct OptimizerExtensionInfo {
-	virtual ~OptimizerExtensionInfo() {
-	}
+	virtual ~OptimizerExtensionInfo() = default;
+
+	[[nodiscard]] virtual const std::string &GetName() const;
 };
 
 struct OptimizerExtensionInput {
@@ -31,6 +31,13 @@ struct OptimizerExtensionInput {
 typedef void (*optimize_function_t)(OptimizerExtensionInput &input, unique_ptr<LogicalOperator> &plan);
 
 class OptimizerExtension {
+	OptimizerExtension(const optimize_function_t optimize_function_p,
+	                   shared_ptr<OptimizerExtensionInfo> optimizer_info_p)
+	    : optimize_function(optimize_function_p), optimizer_info(move(optimizer_info_p)) {
+		D_ASSERT(optimizer_info);
+		D_ASSERT(!optimizer_info->GetName().empty());
+	}
+
 public:
 	optimize_function_t optimize_function;
 
