b5051e5745fd3856de86342bec5b01fed1bfb6e7
pdet <pedroholanda@gmail.com>
2025-02-10 10:35:39 -0300

    I dont think these tests have been running in our CI

--- test/parallel_csv/test_parallel_csv.cpp
+++ test/parallel_csv/test_parallel_csv.cpp
@@ -25,8 +25,6 @@ const string tbl_zst = "tbl.zst";
 
 const string csv_extensions[5] = {csv, tsv, csv_gz, csv_zst, tbl_zst};
 
-const char *run = std::getenv("DUCKDB_RUN_PARALLEL_CSV_TESTS");
-
 bool RunVariableBuffer(const string &path, idx_t buffer_size, bool set_temp_dir,
                        ColumnDataCollection *ground_truth = nullptr, const string &add_parameters = "") {
 	DuckDB db(nullptr);
@@ -78,9 +76,6 @@ bool RunFull(std::string &path, std::set<std::string> *skip = nullptr, const str
              bool set_temp_dir = false) {
 	DuckDB db(nullptr);
 	Connection conn(db);
-	if (!run) {
-		return true;
-	}
 	// Here we run the csv file first with the full buffer.
 	// Then a combination of multiple buffers.
 	if (skip) {
