437be711802f60f3bb79a13d5baf1800b31cfbf8
Sam Ansmink <samansmink@hotmail.com>
2025-02-18 14:50:21 +0100

    fix tests

--- src/function/table/system/duckdb_log_contexts.cpp
+++ src/function/table/system/duckdb_log_contexts.cpp
@@ -40,7 +40,7 @@ static unique_ptr<FunctionData> DuckDBLogContextBind(ClientContext &context, Tab
 	names.emplace_back("query_id");
 	return_types.emplace_back(LogicalType::UBIGINT);
 
-	names.emplace_back("thread");
+	names.emplace_back("thread_id");
 	return_types.emplace_back(LogicalType::UBIGINT);
 
 	return nullptr;
