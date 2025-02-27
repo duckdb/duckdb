e195439935c7f70d2080e45a5ee1b5930e86c14a
Mytherin <mark.raasveldt@gmail.com>
2025-02-27 19:21:29 +0100

    Add ExpressionFilter class

---
+++ src/include/duckdb/planner/filter/expression_filter.hpp
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/planner/filter/expression_filter.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "duckdb/planner/table_filter.hpp"
+#include "duckdb/planner/expression.hpp"
+
+namespace duckdb {
+
+class ExpressionFilter : public TableFilter {
+public:
+	static constexpr const TableFilterType TYPE = TableFilterType::EXPRESSION_FILTER;
+
+public:
+	ExpressionFilter(unique_ptr<Expression> expr);
+
+	//! The expression to evaluate
+	unique_ptr<Expression> expr;
+
+public:
+	FilterPropagateResult CheckStatistics(BaseStatistics &stats) override;
+	string ToString(const string &column_name) override;
+	bool Equals(const TableFilter &other) const override;
+	unique_ptr<TableFilter> Copy() const override;
+	unique_ptr<Expression> ToExpression(const Expression &column) const override;
+	void Serialize(Serializer &serializer) const override;
+	static unique_ptr<TableFilter> Deserialize(Deserializer &deserializer);
+};
+
+} // namespace duckdb
