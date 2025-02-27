010be09252762491856e16826a4bc9134a6d52a1
Mytherin <mark.raasveldt@gmail.com>
2025-02-27 07:56:19 +0100

    Disable delta for now

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -53,12 +53,12 @@ endif()
 ################# DELTA
 # MinGW build is not available, and our current manylinux ci does not have enough storage space to run the rust build
 # for Delta
-if (NOT MINGW AND NOT "${OS_NAME}" STREQUAL "linux" AND NOT ${WASM_ENABLED})
-    duckdb_extension_load(delta
-            GIT_URL https://github.com/duckdb/duckdb-delta
-            GIT_TAG 846019edcc27000721ff9c4281e85a63d1aa10de
-    )
-endif()
+#if (NOT MINGW AND NOT "${OS_NAME}" STREQUAL "linux" AND NOT ${WASM_ENABLED})
+#    duckdb_extension_load(delta
+#            GIT_URL https://github.com/duckdb/duckdb-delta
+#            GIT_TAG 846019edcc27000721ff9c4281e85a63d1aa10de
+#    )
+#endif()
 
 ################# EXCEL
 duckdb_extension_load(excel
