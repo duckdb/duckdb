b6784bca80c03d94c5bff013a4a92acd04a0a393
root <root@DESKTOP-LGPVEPH>
2025-03-01 19:17:48 -0800

    Fix

--- test_udf.py
+++ test_udf.py
@@ -31,21 +31,30 @@ con.execute("""
 
 # Insert some sample data
 
+def test_python_udf(d1, d2):
+    return d1+d2 + d1+ d2;
+
 con.execute("""
         INSERT INTO sales (id, category, amount) VALUES
-        (1, 'Electronics', 250.50),
-        (2, 'Clothing', 99.99),
-        (3, 'Electronics', 399.99),
-        (4, 'Books', 19.99),
-        (5, 'Clothing', 49.99),
-        (6, 'Books', 10.99)
+        (1, 'Electronics', 7),
+        (2, 'Clothing', 3),
+        (3, 'Electronics', 10),
+        (4, 'Books', 5),
+        (5, 'Clothing', 9),
+        (6, 'Books', 2)
     """
     )
 #con.create_function("random_name", generate_random_name, [], VARCHAR)
 #con.create_aggregate_function("udf_avg", generate_random_name, [], DOUBLE)
-con.create_aggregate_function("udf_sum", generate_random_name, [], DOUBLE)
+con.create_aggregate_function("udf_sum", test_python_udf, [DOUBLE, DOUBLE], DOUBLE)
 result = con.execute("""
         SELECT udf_sum_double(amount) FROM sales
         """
         ).fetchall()
 print(result)
+
+resultFromNormalSum = con.execute("""
+                SELECT SUM(amount) FROM sales
+        """
+        ).fetchall()
+print(resultFromNormalSum)
