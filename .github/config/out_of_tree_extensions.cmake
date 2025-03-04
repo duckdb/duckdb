1c0a11883af208e33d2467e765a6f7d29b509edb
Max Gabrielsson <max@gabrielsson.com>
2025-03-04 14:43:38 +0100

    use commit instead

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -110,7 +110,7 @@ if (NOT MINGW)
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG v1.2.0
+    GIT_TAG 7a266e8c9b09d3a072a7529a5b0168ce1517bc6f
     INCLUDE_DIR spatial/include
     TEST_DIR test/sql
     )
