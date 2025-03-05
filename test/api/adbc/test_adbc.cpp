81bf63cbc01c18e1340b0218b1edfc13eb004856
Curt Hagenlocher <curt@hagenlocher.org>
2025-03-05 06:02:09 -0800

    Fixed formatting bugs

--- test/api/adbc/test_adbc.cpp
+++ test/api/adbc/test_adbc.cpp
@@ -196,8 +196,8 @@ TEST_CASE("ADBC - Test ingestion - Quoted Table and Schema", "[adbc]") {
 	InitializeADBCError(&adbc_error);
 
 	ArrowSchema arrow_schema;
-	REQUIRE(SUCCESS(
-	    AdbcConnectionGetTableSchema(&db.adbc_connection, nullptr, "my schema", "my table", &arrow_schema, &adbc_error)));
+	REQUIRE(SUCCESS(AdbcConnectionGetTableSchema(&db.adbc_connection, nullptr, "my schema", "my table", &arrow_schema,
+	                                             &adbc_error)));
 	REQUIRE((arrow_schema.n_children == 1));
 	arrow_schema.release(&arrow_schema);
 }
@@ -1544,7 +1544,7 @@ TEST_CASE("Test AdbcConnectionGetObjects", "[adbc]") {
 	}
 	//  Now lets test some errors
 	{
-		ADBCTestDatabase db("test_errors");  
+		ADBCTestDatabase db("test_errors");
 		// Create Arrow Result
 		auto input_data = db.QueryArrow("SELECT 42");
 		// Create Table 'my_table' from the Arrow Result
