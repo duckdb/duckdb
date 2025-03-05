e56984d872063e9922596ba6ef3ecc506a8153d8
Laurens Kuiper <laurens.kuiper@cwi.nl>
2025-03-05 12:33:54 +0100

    also rename functions here

--- src/include/duckdb/common/fsst.hpp
+++ src/include/duckdb/common/fsst.hpp
@@ -17,6 +17,7 @@
 #include "duckdb/common/types/vector_buffer.hpp"
 
 namespace duckdb {
+
 class Value;
 class Vector;
 struct string_t;
@@ -36,10 +37,10 @@ public:
 		const auto max_uncompressed_length = compressed_string_len * 8;
 		const auto fsst_decoder = static_cast<duckdb_fsst_decoder_t *>(duckdb_fsst_decoder);
 		const auto compressed_string_ptr = (const unsigned char *)compressed_string; // NOLINT
-		const auto target_ptr = str_buffer.AllocateBuffer(max_uncompressed_length);
+		const auto target_ptr = str_buffer.AllocateShrinkableBuffer(max_uncompressed_length);
 		const auto decompressed_string_size = duckdb_fsst_decompress(
 		    fsst_decoder, compressed_string_len, compressed_string_ptr, max_uncompressed_length, target_ptr);
-		return str_buffer.FinalizeBuffer(target_ptr, max_uncompressed_length, decompressed_string_size);
+		return str_buffer.FinalizeShrinkableBuffer(target_ptr, max_uncompressed_length, decompressed_string_size);
 	}
 	static string_t DecompressInlinedValue(void *duckdb_fsst_decoder, const char *compressed_string,
 	                                       const idx_t compressed_string_len) {
