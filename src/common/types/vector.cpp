a088491e82886a7e1d7a021733d9d235ee79c896
peter <peter@bonczs-MBP.fritz.box>
2025-03-18 00:06:48 +0100

    - change compressed vector serialization keys to 90-92 so they are lower than the other keys

--- src/common/types/vector.cpp
+++ src/common/types/vector.cpp
@@ -25,7 +25,6 @@
 #include "fsst.h"
 
 #include <cstring> // strlen() on Solaris
-
 namespace duckdb {
 
 UnifiedVectorFormat::UnifiedVectorFormat() : sel(nullptr), data(nullptr) {
@@ -1241,20 +1240,20 @@ void Vector::Serialize(Serializer &serializer, idx_t count, bool compressed_seri
 					if (used_count * 2 < count) { // only serialize as a dict vector if that makes things smaller
 						auto sel_data = reinterpret_cast<data_ptr_t>(new_sel.data());
 						dict.Slice(used_sel, used_count);
-						serializer.WriteProperty(200, "vector_type", VectorType::DICTIONARY_VECTOR);
-						serializer.WriteProperty(201, "sel_vector", sel_data, sizeof(sel_t) * count);
-						serializer.WriteProperty(202, "dict_count", used_count);
+						serializer.WriteProperty(90, "vector_type", VectorType::DICTIONARY_VECTOR);
+						serializer.WriteProperty(91, "sel_vector", sel_data, sizeof(sel_t) * count);
+						serializer.WriteProperty(92, "dict_count", used_count);
 						return dict.Serialize(serializer, used_count, false);
 					}
 				}
 			} else if (vtype == VectorType::CONSTANT_VECTOR && count >= 1) {
-				serializer.WriteProperty(200, "vector_type", VectorType::CONSTANT_VECTOR);
+				serializer.WriteProperty(90, "vector_type", VectorType::CONSTANT_VECTOR);
 				return Vector::Serialize(serializer, 1, false); // just serialize one value
 			} else if (vtype == VectorType::SEQUENCE_VECTOR) {
-				serializer.WriteProperty(200, "vector_type", VectorType::SEQUENCE_VECTOR);
+				serializer.WriteProperty(90, "vector_type", VectorType::SEQUENCE_VECTOR);
 				auto data = reinterpret_cast<int64_t *>(buffer->GetData());
-				serializer.WriteProperty(201, "seq_start", data[0]);
-				serializer.WriteProperty(202, "seq_increment", data[1]);
+				serializer.WriteProperty(91, "seq_start", data[0]);
+				serializer.WriteProperty(92, "seq_increment", data[1]);
 				return; // for sequence vectors we do not serialize anything else
 			} else {
 				// TODO: other compressed vector types (FSST)
@@ -1351,7 +1350,7 @@ void Vector::Serialize(Serializer &serializer, idx_t count, bool compressed_seri
 void Vector::Deserialize(Deserializer &deserializer, idx_t count) {
 	auto &logical_type = GetType();
 	const auto vtype = // older versions that only supported flat vectors did not serialize vector_type,
-	    deserializer.ReadPropertyWithExplicitDefault<VectorType>(200, "vector_type", VectorType::FLAT_VECTOR);
+	    deserializer.ReadPropertyWithExplicitDefault<VectorType>(90, "vector_type", VectorType::FLAT_VECTOR);
 
 	// first handle deserialization of compressed vector types
 	if (vtype == VectorType::CONSTANT_VECTOR) {
@@ -1360,14 +1359,14 @@ void Vector::Deserialize(Deserializer &deserializer, idx_t count) {
 		return;
 	} else if (vtype == VectorType::DICTIONARY_VECTOR) {
 		SelectionVector sel(count);
-		deserializer.ReadProperty(201, "sel_vector", reinterpret_cast<data_ptr_t>(sel.data()), sizeof(sel_t) * count);
-		const auto dict_count = deserializer.ReadProperty<idx_t>(202, "dict_count");
+		deserializer.ReadProperty(91, "sel_vector", reinterpret_cast<data_ptr_t>(sel.data()), sizeof(sel_t) * count);
+		const auto dict_count = deserializer.ReadProperty<idx_t>(92, "dict_count");
 		Vector::Deserialize(deserializer, dict_count); // deserialize the dictionary in this vector
 		Vector::Slice(sel, count);                     // will create a dictionary vector
 		return;
 	} else if (vtype == VectorType::SEQUENCE_VECTOR) {
-		const int64_t seq_start = deserializer.ReadProperty<int64_t>(201, "seq_start");
-		const int64_t seq_increment = deserializer.ReadProperty<int64_t>(202, "seq_increment");
+		const int64_t seq_start = deserializer.ReadProperty<int64_t>(91, "seq_start");
+		const int64_t seq_increment = deserializer.ReadProperty<int64_t>(92, "seq_increment");
 		Vector::Sequence(seq_start, seq_increment, count);
 		return;
 	}
