b0938de4fdaafa3a015200d09d127d75fb73b901
Tishj <t_b@live.nl>
2025-03-18 11:44:02 +0100

    operator[] needs to be constant

--- src/include/duckdb/common/base_file_reader.hpp
+++ src/include/duckdb/common/base_file_reader.hpp
@@ -267,7 +267,7 @@ public:
 	void push_back(MultiFileGlobalIndex global_index) { // NOLINT: matching name of std
 		column_mapping.push_back(global_index);
 	}
-	const MultiFileGlobalIndex &operator[](MultiFileLocalIndex local_index) {
+	const MultiFileGlobalIndex &operator[](MultiFileLocalIndex local_index) const {
 		return column_mapping[local_index.index];
 	}
 	idx_t size() const { // NOLINT: matching name of std
