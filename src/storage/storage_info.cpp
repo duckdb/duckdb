b3934d3b0758378fe544ccdb78db5724ee479599
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-03-24 10:01:06 +0100

    Add v1.3.0 to storage versions, preparation for next minor release

--- src/storage/storage_info.cpp
+++ src/storage/storage_info.cpp
@@ -7,7 +7,7 @@ namespace duckdb {
 
 const uint64_t VERSION_NUMBER = 64;
 const uint64_t VERSION_NUMBER_LOWER = 64;
-const uint64_t VERSION_NUMBER_UPPER = 65;
+const uint64_t VERSION_NUMBER_UPPER = 66;
 
 static_assert(VERSION_NUMBER_LOWER <= VERSION_NUMBER, "Check on VERSION_NUMBER lower bound");
 static_assert(VERSION_NUMBER <= VERSION_NUMBER_UPPER, "Check on VERSION_NUMBER upper bound");
@@ -79,13 +79,14 @@ static const StorageVersionInfo storage_version_info[] = {
 	{"v1.2.0", 65},
 	{"v1.2.1", 65},
 	{"v1.2.2", 65},
+	{"v1.3.0", 66},
 	{nullptr, 0}
 };
 // END OF STORAGE VERSION INFO
 static_assert(DEFAULT_STORAGE_VERSION_INFO == VERSION_NUMBER, "Check on VERSION_INFO");
 
 // START OF SERIALIZATION VERSION INFO
-const uint64_t LATEST_SERIALIZATION_VERSION_INFO = 4;
+const uint64_t LATEST_SERIALIZATION_VERSION_INFO = 5;
 const uint64_t DEFAULT_SERIALIZATION_VERSION_INFO = 1;
 static const SerializationVersionInfo serialization_version_info[] = {
 	{"v0.10.0", 1},
@@ -100,7 +101,8 @@ static const SerializationVersionInfo serialization_version_info[] = {
 	{"v1.2.0", 4},
 	{"v1.2.1", 4},
 	{"v1.2.2", 4},
-	{"latest", 4},
+	{"v1.3.0", 5},
+	{"latest", 5},
 	{nullptr, 0}
 };
 // END OF SERIALIZATION VERSION INFO
