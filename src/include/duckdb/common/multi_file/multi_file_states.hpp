f27aa6e95907d0d6c687a5c6025f6686c25430ad
Mytherin <mark.raasveldt@gmail.com>
2025-03-28 09:57:17 +0100

    Add missing include

--- src/include/duckdb/common/multi_file/multi_file_states.hpp
+++ src/include/duckdb/common/multi_file/multi_file_states.hpp
@@ -11,6 +11,7 @@
 #include "duckdb/common/multi_file/multi_file_data.hpp"
 #include "duckdb/common/multi_file/multi_file_options.hpp"
 #include "duckdb/common/multi_file/base_file_reader.hpp"
+#include "duckdb/common/multi_file/multi_file_list.hpp"
 
 namespace duckdb {
 
