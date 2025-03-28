cb7206d57efb9cca90d81db6f215a6f4ec2aa50b
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-03-28 14:57:01 +0100

    add 'Categories' label for string functions

--- extension/core_functions/include/core_functions/scalar/string_functions.hpp
+++ extension/core_functions/include/core_functions/scalar/string_functions.hpp
@@ -20,7 +20,7 @@ struct StartsWithOperatorFun {
 	static constexpr const char *Parameters = "string,search_string";
 	static constexpr const char *Description = "Returns true if string begins with search_string";
 	static constexpr const char *Example = "starts_with('abc','a')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -36,7 +36,7 @@ struct ASCIIFun {
 	static constexpr const char *Parameters = "string";
 	static constexpr const char *Description = "Returns an integer that represents the Unicode code point of the first character of the string";
 	static constexpr const char *Example = "ascii('Ω')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -46,17 +46,17 @@ struct BarFun {
 	static constexpr const char *Parameters = "x,min,max,width";
 	static constexpr const char *Description = "Draws a band whose width is proportional to (x - min) and equal to width characters when x = max. width defaults to 80";
 	static constexpr const char *Example = "bar(5, 0, 20, 10)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
 
 struct BinFun {
 	static constexpr const char *Name = "bin";
-	static constexpr const char *Parameters = "value";
-	static constexpr const char *Description = "Converts the value to binary representation";
-	static constexpr const char *Example = "bin(42)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Parameters = "string::VARCHAR\1value::ANY";
+	static constexpr const char *Description = "Converts the string to binary representation\1Converts the value to binary representation";
+	static constexpr const char *Example = "bin('Aa')\1bin(42)";
+	static constexpr const char *Categories = "string\1numeric";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -72,7 +72,7 @@ struct ChrFun {
 	static constexpr const char *Parameters = "code_point";
 	static constexpr const char *Description = "Returns a character which is corresponding the ASCII code value or Unicode code point";
 	static constexpr const char *Example = "chr(65)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -82,7 +82,7 @@ struct DamerauLevenshteinFun {
 	static constexpr const char *Parameters = "str1,str2";
 	static constexpr const char *Description = "Extension of Levenshtein distance to also include transposition of adjacent characters as an allowed edit operation. In other words, the minimum number of edit operations (insertions, deletions, substitutions or transpositions) required to change one string to another. Different case is considered different";
 	static constexpr const char *Example = "damerau_levenshtein('hello', 'world')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "text_similarity";
 
 	static ScalarFunction GetFunction();
 };
@@ -92,7 +92,7 @@ struct FormatFun {
 	static constexpr const char *Parameters = "format,parameters...";
 	static constexpr const char *Description = "Formats a string using fmt syntax";
 	static constexpr const char *Example = "format('Benchmark \"{}\" took {} seconds', 'CSV', 42)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -102,7 +102,7 @@ struct FormatBytesFun {
 	static constexpr const char *Parameters = "bytes";
 	static constexpr const char *Description = "Converts bytes to a human-readable presentation (e.g. 16000 -> 15.6 KiB)";
 	static constexpr const char *Example = "format_bytes(1000 * 16)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string,numeric";
 
 	static ScalarFunction GetFunction();
 };
@@ -118,7 +118,7 @@ struct FormatreadabledecimalsizeFun {
 	static constexpr const char *Parameters = "bytes";
 	static constexpr const char *Description = "Converts bytes to a human-readable presentation (e.g. 16000 -> 16.0 KB)";
 	static constexpr const char *Example = "format_bytes(1000 * 16)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string,numeric";
 
 	static ScalarFunction GetFunction();
 };
@@ -128,7 +128,7 @@ struct HammingFun {
 	static constexpr const char *Parameters = "str1,str2";
 	static constexpr const char *Description = "The number of positions with different characters for 2 strings of equal length. Different case is considered different";
 	static constexpr const char *Example = "hamming('duck','luck')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "text_similarity";
 
 	static ScalarFunction GetFunction();
 };
@@ -160,7 +160,7 @@ struct InstrFun {
 	static constexpr const char *Parameters = "haystack,needle";
 	static constexpr const char *Description = "Returns location of first occurrence of needle in haystack, counting from 1. Returns 0 if no match found";
 	static constexpr const char *Example = "instr('test test','es')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -182,7 +182,7 @@ struct JaccardFun {
 	static constexpr const char *Parameters = "str1,str2";
 	static constexpr const char *Description = "The Jaccard similarity between two strings. Different case is considered different. Returns a number between 0 and 1";
 	static constexpr const char *Example = "jaccard('duck','luck')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "text_similarity";
 
 	static ScalarFunction GetFunction();
 };
@@ -192,7 +192,7 @@ struct JaroSimilarityFun {
 	static constexpr const char *Parameters = "str1,str2,score_cutoff";
 	static constexpr const char *Description = "The Jaro similarity between two strings. Different case is considered different. Returns a number between 0 and 1";
 	static constexpr const char *Example = "jaro_similarity('duck', 'duckdb', 0.5)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "text_similarity";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -202,7 +202,7 @@ struct JaroWinklerSimilarityFun {
 	static constexpr const char *Parameters = "str1,str2,score_cutoff";
 	static constexpr const char *Description = "The Jaro-Winkler similarity between two strings. Different case is considered different. Returns a number between 0 and 1";
 	static constexpr const char *Example = "jaro_winkler_similarity('duck', 'duckdb', 0.5)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "text_similarity";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -212,7 +212,7 @@ struct LeftFun {
 	static constexpr const char *Parameters = "string,count";
 	static constexpr const char *Description = "Extracts the left-most count characters";
 	static constexpr const char *Example = "left('Hello🦆', 2)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -222,7 +222,7 @@ struct LeftGraphemeFun {
 	static constexpr const char *Parameters = "string,count";
 	static constexpr const char *Description = "Extracts the left-most count grapheme clusters";
 	static constexpr const char *Example = "left_grapheme('🤦🏼‍♂️🤦🏽‍♀️', 1)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -232,7 +232,7 @@ struct LevenshteinFun {
 	static constexpr const char *Parameters = "str1,str2";
 	static constexpr const char *Description = "The minimum number of single-character edits (insertions, deletions or substitutions) required to change one string to the other. Different case is considered different";
 	static constexpr const char *Example = "levenshtein('duck','db')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "text_similarity";
 
 	static ScalarFunction GetFunction();
 };
@@ -248,7 +248,7 @@ struct LpadFun {
 	static constexpr const char *Parameters = "string,count,character";
 	static constexpr const char *Description = "Pads the string with the character from the left until it has count characters";
 	static constexpr const char *Example = "lpad('hello', 10, '>')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -258,7 +258,7 @@ struct LtrimFun {
 	static constexpr const char *Parameters = "string,characters";
 	static constexpr const char *Description = "Removes any occurrences of any of the characters from the left side of the string";
 	static constexpr const char *Example = "ltrim('>>>>test<<', '><')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -268,7 +268,7 @@ struct ParseDirnameFun {
 	static constexpr const char *Parameters = "string,separator";
 	static constexpr const char *Description = "Returns the top-level directory name. separator options: system, both_slash (default), forward_slash, backslash";
 	static constexpr const char *Example = "parse_dirname('path/to/file.csv', 'system')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -278,7 +278,7 @@ struct ParseDirpathFun {
 	static constexpr const char *Parameters = "string,separator";
 	static constexpr const char *Description = "Returns the head of the path similarly to Python's os.path.dirname. separator options: system, both_slash (default), forward_slash, backslash";
 	static constexpr const char *Example = "parse_dirpath('path/to/file.csv', 'system')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -288,7 +288,7 @@ struct ParseFilenameFun {
 	static constexpr const char *Parameters = "string,trim_extension,separator";
 	static constexpr const char *Description = "Returns the last component of the path similarly to Python's os.path.basename. If trim_extension is true, the file extension will be removed (it defaults to false). separator options: system, both_slash (default), forward_slash, backslash";
 	static constexpr const char *Example = "parse_filename('path/to/file.csv', true, 'forward_slash')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -298,7 +298,7 @@ struct ParsePathFun {
 	static constexpr const char *Parameters = "string,separator";
 	static constexpr const char *Description = "Returns a list of the components (directories and filename) in the path similarly to Python's pathlib.PurePath::parts. separator options: system, both_slash (default), forward_slash, backslash";
 	static constexpr const char *Example = "parse_path('path/to/file.csv', 'system')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -308,7 +308,7 @@ struct PrintfFun {
 	static constexpr const char *Parameters = "format,parameters...";
 	static constexpr const char *Description = "Formats a string using printf syntax";
 	static constexpr const char *Example = "printf('Benchmark \"%s\" took %d seconds', 'CSV', 42)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -318,7 +318,7 @@ struct RepeatFun {
 	static constexpr const char *Parameters = "string,count";
 	static constexpr const char *Description = "Repeats the string count number of times";
 	static constexpr const char *Example = "repeat('A', 5)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -328,7 +328,7 @@ struct ReplaceFun {
 	static constexpr const char *Parameters = "string,source,target";
 	static constexpr const char *Description = "Replaces any occurrences of the source with target in string";
 	static constexpr const char *Example = "replace('hello', 'l', '-')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -338,7 +338,7 @@ struct ReverseFun {
 	static constexpr const char *Parameters = "string";
 	static constexpr const char *Description = "Reverses the string";
 	static constexpr const char *Example = "reverse('hello')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -348,7 +348,7 @@ struct RightFun {
 	static constexpr const char *Parameters = "string,count";
 	static constexpr const char *Description = "Extract the right-most count characters";
 	static constexpr const char *Example = "right('Hello🦆', 3)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -358,7 +358,7 @@ struct RightGraphemeFun {
 	static constexpr const char *Parameters = "string,count";
 	static constexpr const char *Description = "Extracts the right-most count grapheme clusters";
 	static constexpr const char *Example = "right_grapheme('🤦🏼‍♂️🤦🏽‍♀️', 1)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -368,7 +368,7 @@ struct RpadFun {
 	static constexpr const char *Parameters = "string,count,character";
 	static constexpr const char *Description = "Pads the string with the character from the right until it has count characters";
 	static constexpr const char *Example = "rpad('hello', 10, '<')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -378,7 +378,7 @@ struct RtrimFun {
 	static constexpr const char *Parameters = "string,characters";
 	static constexpr const char *Description = "Removes any occurrences of any of the characters from the right side of the string";
 	static constexpr const char *Example = "rtrim('>>>>test<<', '><')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -388,7 +388,7 @@ struct TranslateFun {
 	static constexpr const char *Parameters = "string,from,to";
 	static constexpr const char *Description = "Replaces each character in string that matches a character in the from set with the corresponding character in the to set. If from is longer than to, occurrences of the extra characters in from are deleted";
 	static constexpr const char *Example = "translate('12345', '143', 'ax')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -398,7 +398,7 @@ struct TrimFun {
 	static constexpr const char *Parameters = "string::VARCHAR\1string::VARCHAR,characters::VARCHAR";
 	static constexpr const char *Description = "Removes any spaces from either side of the string.\1Removes any occurrences of any of the characters from either side of the string";
 	static constexpr const char *Example = "trim(' test ')\1trim('>>>>test<<', '><')";
-	static constexpr const char *Categories = "\1";
+	static constexpr const char *Categories = "string\1string";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -440,7 +440,7 @@ struct UnicodeFun {
 	static constexpr const char *Parameters = "str";
 	static constexpr const char *Description = "Returns the unicode codepoint of the first character of the string";
 	static constexpr const char *Example = "unicode('ü')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -456,7 +456,7 @@ struct ToBaseFun {
 	static constexpr const char *Parameters = "number,radix,min_length";
 	static constexpr const char *Description = "Converts a value to a string in the given base radix, optionally padding with leading zeros to the minimum length";
 	static constexpr const char *Example = "to_base(42, 16)";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "numeric";
 
 	static ScalarFunctionSet GetFunctions();
 };
@@ -466,7 +466,7 @@ struct UrlEncodeFun {
 	static constexpr const char *Parameters = "input";
 	static constexpr const char *Description = "Escapes the input string by encoding it so that it can be included in a URL query parameter.";
 	static constexpr const char *Example = "url_encode('this string has/ special+ characters>')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
@@ -476,7 +476,7 @@ struct UrlDecodeFun {
 	static constexpr const char *Parameters = "input";
 	static constexpr const char *Description = "Unescapes the URL encoded input.";
 	static constexpr const char *Example = "url_decode('this%20string%20is%2BFencoded')";
-	static constexpr const char *Categories = "";
+	static constexpr const char *Categories = "string";
 
 	static ScalarFunction GetFunction();
 };
