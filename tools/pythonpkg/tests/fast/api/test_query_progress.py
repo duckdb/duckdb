ed5d4026f8f8993326df0d6ce6abf970dc6983b4
Nick Moore <nick@zoic.org>
2025-03-31 14:32:36 +1100

    Fix test for query_progress (now fails)

--- tools/pythonpkg/tests/fast/api/test_query_progress.py
+++ tools/pythonpkg/tests/fast/api/test_query_progress.py
@@ -11,20 +11,23 @@ class TestQueryProgress(object):
         condition=platform.system() == "Emscripten",
         reason="threads not allowed on Emscripten",
     )
-    def test_query_progress(self):
+    def test_query_progress(self, reraise):
         conn = duckdb.connect()
 
         def thread_target():
-            # Wait for query to start running before interrupting
-            time.sleep(0.1)
-            x = conn.query_progress()
-            time.sleep(0.5)
-            y = conn.query_progress()
-            assert 1 > y > x > 0
+            with reraise:
+                # Wait for query to start running before interrupting
+                time.sleep(0.1)
+                x = conn.query_progress()
+                time.sleep(0.1)
+                y = conn.query_progress()
+                assert 1 > y > x > 0
             
         thread = threading.Thread(target=thread_target)
+        print("START")
         thread.start()
-        conn.execute("select count(*) from range(100000000000)").fetchall()
+        conn.execute("select count(*) from range(10000000000)").fetchall()
+        print("JOIN")
         thread.join()
 
     def test_query_progress_closed_connection(self):
