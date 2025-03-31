07024276f16ce00e94ad5f6c56b49aaa4909ee70
Nick Moore <nick@zoic.org>
2025-03-31 15:10:59 +1100

    fix test query to actually work!

--- tools/pythonpkg/tests/fast/api/test_query_progress.py
+++ tools/pythonpkg/tests/fast/api/test_query_progress.py
@@ -13,6 +13,9 @@ class TestQueryProgress(object):
     )
     def test_query_progress(self, reraise):
         conn = duckdb.connect()
+        conn.sql("set enable_progress_bar_print=false")
+        conn.sql("set progress_bar_time=0")
+        conn.sql("create table t as (select range as n from range(10000000))")
 
         def thread_target():
             with reraise:
@@ -21,12 +24,12 @@ class TestQueryProgress(object):
                 x = conn.query_progress()
                 time.sleep(0.1)
                 y = conn.query_progress()
-                assert 1 > y > x > 0
+                assert 100 > y > x > 0
             
         thread = threading.Thread(target=thread_target)
         print("START")
         thread.start()
-        conn.execute("select count(*) from range(10000000000)").fetchall()
+        conn.execute("select max(sha1(n::varchar)) from t").fetchall()
         print("JOIN")
         thread.join()
 
