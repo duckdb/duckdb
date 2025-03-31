30a6d7d1f340260794dfe40c6b10abe45d7c3725
Max Gabrielsson <max@gabrielsson.com>
2025-03-31 16:08:56 +0200

    bump excel

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -73,7 +73,7 @@ endif()
 duckdb_extension_load(excel
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-excel
-    GIT_TAG f14e7c3beaf379c54b47b996aa896a1d814e1be8
+    GIT_TAG b4318627ccb7fff043bb9b1d71e894939f14bbfd
     INCLUDE_DIR src/excel/include
     )
 
