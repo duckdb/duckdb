111daa234e6976f5011b418eca982478f417fe22
Mytherin <mark.raasveldt@gmail.com>
2025-04-02 16:50:56 +0200

    Remove comments

--- src/include/duckdb/common/enums/debug_vector_verification.hpp
+++ src/include/duckdb/common/enums/debug_vector_verification.hpp
@@ -12,17 +12,6 @@
 
 namespace duckdb {
 
-//- vector_type: "dictionary_expression"
-//  test_parameter: '"*"'
-//- vector_type: "dictionary_operator"
-//  test_parameter: '' # skip running all tests here because it is too slow
-//- vector_type: "constant_operator"
-//  test_parameter: '' # skip running all tests here because it is too slow
-//- vector_type: "sequence_operator"
-//  test_parameter: '' # skip running all tests here because it is too slow
-//- vector_type: "nested_shuffle"
-//  test_parameter: '' # skip running all tests here because it is too slow
-
 enum class DebugVectorVerification : uint8_t {
 	NONE,
 	DICTIONARY_EXPRESSION,
