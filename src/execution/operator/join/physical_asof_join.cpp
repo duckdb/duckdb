cb8926f6f74490482526b54e99f7502086fe8d63
Richard Wesley <13156216+hawkfish@users.noreply.github.com>
2025-04-08 21:00:30 +1200

    Issue #17001: AsOf memory Management
    * Free memory right after we are done with it
    
    fixes: duckdb/duckdb#17001
    fixes: duckdblabs/duckdb-internal#4586

--- src/execution/operator/join/physical_asof_join.cpp
+++ src/execution/operator/join/physical_asof_join.cpp
@@ -1,9 +1,6 @@
 #include "duckdb/execution/operator/join/physical_asof_join.hpp"
 
-#include "duckdb/common/fast_mem.hpp"
-#include "duckdb/common/operator/comparison_operators.hpp"
 #include "duckdb/common/row_operations/row_operations.hpp"
-#include "duckdb/common/sort/comparators.hpp"
 #include "duckdb/common/sort/partition_state.hpp"
 #include "duckdb/common/sort/sort.hpp"
 #include "duckdb/common/vector_operations/vector_operations.hpp"
@@ -12,8 +9,6 @@
 #include "duckdb/main/client_context.hpp"
 #include "duckdb/parallel/event.hpp"
 #include "duckdb/parallel/thread_context.hpp"
-
-#include <thread>
 
 namespace duckdb {
 
@@ -348,7 +343,7 @@ public:
 	}
 	void BeginLeftScan(hash_t scan_bin);
 	bool NextLeft();
-	void EndScan();
+	void EndLeftScan();
 
 	// resolve joins that output max N elements (SEMI, ANTI, MARK)
 	void ResolveSimpleJoin(ExecutionContext &context, DataChunk &chunk);
@@ -375,6 +370,7 @@ public:
 	unique_ptr<SBIterator> left_itr;
 	unique_ptr<PayloadScanner> lhs_scanner;
 	DataChunk lhs_payload;
+	idx_t left_group = 0;
 
 	//	RHS scanning
 	optional_ptr<PartitionGlobalHashGroup> right_hash;
@@ -382,6 +378,7 @@ public:
 	unique_ptr<SBIterator> right_itr;
 	unique_ptr<PayloadScanner> rhs_scanner;
 	DataChunk rhs_payload;
+	idx_t right_group = 0;
 
 	idx_t lhs_match_count;
 	bool fetch_next_left;
@@ -407,8 +404,18 @@ AsOfProbeBuffer::AsOfProbeBuffer(ClientContext &context, const PhysicalAsOfJoin 
 
 void AsOfProbeBuffer::BeginLeftScan(hash_t scan_bin) {
 	auto &gsink = op.sink_state->Cast<AsOfGlobalSinkState>();
+
 	auto &lhs_sink = *gsink.lhs_sink;
-	const auto left_group = lhs_sink.bin_groups[scan_bin];
+	left_group = lhs_sink.bin_groups[scan_bin];
+
+	//	Always set right_group too for memory management
+	auto &rhs_sink = gsink.rhs_sink;
+	if (scan_bin < rhs_sink.bin_groups.size()) {
+		right_group = rhs_sink.bin_groups[scan_bin];
+	} else {
+		right_group = rhs_sink.bin_groups.size();
+	}
+
 	if (left_group >= lhs_sink.bin_groups.size()) {
 		return;
 	}
@@ -441,8 +448,6 @@ void AsOfProbeBuffer::BeginLeftScan(hash_t scan_bin) {
 
 	// We are only probing the corresponding right side bin, which may be empty
 	// If they are empty, we leave the iterator as null so we can emit left matches
-	auto &rhs_sink = gsink.rhs_sink;
-	const auto right_group = rhs_sink.bin_groups[scan_bin];
 	if (right_group < rhs_sink.bin_groups.size()) {
 		right_hash = rhs_sink.hash_groups[right_group].get();
 		right_outer = gsink.right_outers.data() + right_group;
@@ -465,15 +470,27 @@ bool AsOfProbeBuffer::NextLeft() {
 	return true;
 }
 
-void AsOfProbeBuffer::EndScan() {
+void AsOfProbeBuffer::EndLeftScan() {
+	auto &gsink = op.sink_state->Cast<AsOfGlobalSinkState>();
+
 	right_hash = nullptr;
 	right_itr.reset();
 	rhs_scanner.reset();
 	right_outer = nullptr;
 
+	auto &rhs_sink = gsink.rhs_sink;
+	if (!gsink.is_outer && right_group < rhs_sink.bin_groups.size()) {
+		rhs_sink.hash_groups[right_group].reset();
+	}
+
 	left_hash = nullptr;
 	left_itr.reset();
 	lhs_scanner.reset();
+
+	auto &lhs_sink = *gsink.lhs_sink;
+	if (left_group < lhs_sink.bin_groups.size()) {
+		lhs_sink.hash_groups[left_group].reset();
+	}
 }
 
 void AsOfProbeBuffer::ResolveJoin(bool *found_match, idx_t *matches) {
@@ -803,7 +820,7 @@ SourceResultType PhysicalAsOfJoin::GetData(ExecutionContext &context, DataChunk 
 			//	Join the next partition
 			continue;
 		} else {
-			lsource.probe_buffer.EndScan();
+			lsource.probe_buffer.EndLeftScan();
 			gsource.flushed++;
 		}
 	}
