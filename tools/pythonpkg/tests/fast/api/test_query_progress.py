eef2221b051cf38cd93bf235c9db70fdd86da446
Nick Moore <nick@zoic.org>
2025-04-09 13:07:58 +1000

    try to fix test up for #16927

--- tools/pythonpkg/tests/fast/api/test_query_progress.py
+++ tools/pythonpkg/tests/fast/api/test_query_progress.py
@@ -18,19 +18,43 @@ class TestQueryProgress(object):
         conn.sql("create table t as (select range as n from range(10000000))")
 
         def thread_target():
+            # run a very slow query which hopefully isn't too memory intensive.
             with reraise:
-                # Wait for query to start running before interrupting
-                time.sleep(0.1)
-                x = conn.query_progress()
-                time.sleep(0.1)
-                y = conn.query_progress()
-                assert 100 > y > x > 0
+                try:
+                    conn.execute("select max(sha1(n::varchar)) from t").fetchall()
+                except duckdb.InterruptException:
+                    pass
 
         thread = threading.Thread(target=thread_target)
-        print("START")
         thread.start()
-        conn.execute("select max(sha1(n::varchar)) from t").fetchall()
-        print("JOIN")
+
+        # monitor the query running in the thread, wait for progress > 0
+        # the 'for/else' is just so the test times out after 5 seconds if the
+        # query never progresses.  This will also fail if the query is too
+        # quick as it will be back at -1 as soon as the query is finished.
+
+        for _ in range(0, 500):
+            assert thread.is_alive(), "query finished too quick"
+            if (qp1 := conn.query_progress()) > 0:
+                break
+            time.sleep(0.01)
+        else:
+            pytest.fail("query start timeout")
+
+        # keep monitoring and wait for the progress to increase
+        for _ in range(0, 500):
+            assert thread.is_alive(), "query finished too quick"
+            if (qp2 := conn.query_progress()) > qp1:
+                break
+            time.sleep(0.01)
+        else:
+            pytest.fail("query progress timeout")
+
+        # check that progress numbers are sensible
+        assert 100 >= qp2 > qp1 > 0
+
+        # kill the query to reduce CPU usage.
+        conn.interrupt()
         thread.join()
 
     def test_query_progress_closed_connection(self):
