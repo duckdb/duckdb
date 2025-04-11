2cf9d57efd47956c415d99475c12513485720062
Joe Isaacs <joe.isaacs@live.co.uk>
2025-04-11 14:19:17 +0100

    rename

--- src/main/capi/vector_buffer-c.cpp
+++ src/main/capi/vector_buffer-c.cpp
@@ -27,9 +27,9 @@ struct CExternalVectorBuffer {
 
 
 duckdb_vector_buffer duckdb_wrap_external_vector_buffer(external_buffer buffer, external_buffer_free free_fn) {
-	auto rbuffer = duckdb::make_shared_ptr<ExternalVectorBuffer>(buffer, free_fn);
-	auto ribuffer = new CExternalVectorBuffer (rbuffer);
-	return reinterpret_cast<duckdb_vector_buffer>(ribuffer);
+	auto external_buffer = duckdb::make_shared_ptr<ExternalVectorBuffer>(buffer, free_fn);
+	auto c_external_buffer = new CExternalVectorBuffer (external_buffer);
+	return reinterpret_cast<duckdb_vector_buffer>(c_external_buffer);
 }
 
 void duckdb_free_vector_buffer(duckdb_vector_buffer *buffer) {
