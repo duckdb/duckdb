a10f6cbca7d2c4ad692e14e5d2103d603bb8fb59
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-04-17 16:52:58 +0200

    format fix

--- scripts/run_tests_one_by_one.py
+++ scripts/run_tests_one_by_one.py
@@ -159,9 +159,7 @@ def launch_test(test, list_of_tests=False):
             env = {'SUMMARIZE_FAILURES': '0', 'NO_DUPLICATING_HEADERS': '1'}
         else:
             env = {'SUMMARIZE_FAILURES': '0'}
-        res = subprocess.run(
-            test_cmd, stdout=unittest_stdout, stderr=unittest_stderr, timeout=timeout, env=env
-        )
+        res = subprocess.run(test_cmd, stdout=unittest_stdout, stderr=unittest_stderr, timeout=timeout, env=env)
     except subprocess.TimeoutExpired as e:
         if list_of_tests:
             print("[TIMED OUT]", flush=True)
