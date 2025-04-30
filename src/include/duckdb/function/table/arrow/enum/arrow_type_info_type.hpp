fe2448bd0446f18bea1a45abe49f14c6c6e0a4d0
pdet <pedroholanda@gmail.com>
2025-04-30 14:29:22 +0200

    Read Decimal32 and 64

--- src/include/duckdb/function/table/arrow/enum/arrow_type_info_type.hpp
+++ src/include/duckdb/function/table/arrow/enum/arrow_type_info_type.hpp
@@ -4,6 +4,6 @@
 
 namespace duckdb {
 
-enum class ArrowTypeInfoType : uint8_t { LIST, STRUCT, DATE_TIME, STRING, ARRAY };
+enum class ArrowTypeInfoType : uint8_t { LIST, STRUCT, DATE_TIME, STRING, ARRAY, DECIMAL };
 
 } // namespace duckdb
