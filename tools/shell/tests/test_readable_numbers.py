ed0801aa3bb363f65fbfc9b44f6af56c5a506f88
Mytherin <mark.raasveldt@gmail.com>
2025-05-08 09:27:14 +0200

    Fix old tests

--- tools/shell/tests/test_readable_numbers.py
+++ tools/shell/tests/test_readable_numbers.py
@@ -10,7 +10,8 @@ import os
 def test_readable_numbers(shell):
     test = (
         ShellTest(shell)
-        .statement("select 59986052 as count, 123456789 as count2, 'hello world' str, 1234 count3, 9999999999 count4, -9999999999 count5;")
+        .statement(".large_number_rendering footer")
+        .statement("select 59986052 as count, 123456789 as count2, 9999999999 count3, -9999999999 count4;")
     )
     result = test.run()
     result.check_stdout("(59.99 million)")
@@ -30,6 +31,7 @@ def test_readable_numbers_exhaustive(shell, test_rounding):
             query += '1' + ('0' * i)
     test = (
         ShellTest(shell)
+        .statement(".large_number_rendering all")
         .statement(".maxwidth 99999")
         .statement(query)
     )
@@ -38,11 +40,12 @@ def test_readable_numbers_exhaustive(shell, test_rounding):
         for number in ['1.00', '10.00', '100.00']:
             if unit == 'quintillion' and number in ['10.00', '100.00']:
                 continue
-            result.check_stdout("(" + number + " " + unit + ")")
+            result.check_stdout(number + " " + unit)
 
 def test_readable_numbers_rounding(shell):
     test = (
         ShellTest(shell)
+        .statement(".large_number_rendering footer")
         .statement(".maxwidth 99999")
         .statement("select 1005000, 1004999, -1005000, -1004999;")
     )
@@ -55,31 +58,34 @@ def test_readable_numbers_rounding(shell):
 def test_readable_rounding_edge_case(shell):
     test = (
         ShellTest(shell)
+        .statement(".large_number_rendering all")
         .statement(".maxwidth 99999")
         .statement("select 994999, 995000")
     )
     result = test.run()
-    result.check_stdout("(1.00 million)")
+    result.check_stdout("1.00 million")
 
 def test_readable_numbers_limit(shell):
     test = (
         ShellTest(shell)
         .statement(".maxwidth 99999")
+        .statement(".large_number_rendering all")
         .statement("select 18446744073709551616, -18446744073709551616, 9999999999999999999, -9999999999999999999;")
     )
     result = test.run()
-    result.check_stdout("(10.00 quintillion)")
-    result.check_stdout("(-10.00 quintillion)")
+    result.check_stdout("10.00 quintillion")
+    result.check_stdout("-10.00 quintillion")
 
 def test_decimal_separator(shell):
     test = (
         ShellTest(shell)
         .statement(".decimal_sep ,")
+        .statement(".large_number_rendering all")
         .statement("select 59986052, 59986052.5, 999999999.123456789, 1e20, 'nan'::double;")
     )
     result = test.run()
-    result.check_stdout("(59,99 million)")
-    result.check_stdout("(1,00 billion)")
+    result.check_stdout("59,99 million")
+    result.check_stdout("1,00 billion")
 
 def test_odd_floating_points(shell):
     test = (
