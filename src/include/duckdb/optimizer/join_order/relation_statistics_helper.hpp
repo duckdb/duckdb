ecc1f070bf124c6114d66787d1346483e65f94d2
Tmonster <tom@ebergen.com>
2025-05-13 15:18:54 +0200

    if distinct count is 0, do not use it for join order optimization

--- src/include/duckdb/optimizer/join_order/relation_statistics_helper.hpp
+++ src/include/duckdb/optimizer/join_order/relation_statistics_helper.hpp
@@ -69,6 +69,7 @@ public:
 	static void CopyRelationStats(RelationStats &to, const RelationStats &from);
 
 private:
+	static idx_t GetDistinctCount(LogicalGet &get, ClientContext &context, idx_t column_id);
 };
 
 } // namespace duckdb
