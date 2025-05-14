d9bfb0680f531d9817d8c74c08ed33d7796eadbd
Max Gabrielsson <max@gabrielsson.com>
2025-05-14 22:18:51 +0200

    also patch vss

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -149,9 +149,8 @@ duckdb_extension_load(vss
         LOAD_TESTS
         DONT_LINK
         GIT_URL https://github.com/duckdb/duckdb-vss
-        GIT_TAG ba199a7215b75e83821ece13f6b921ccfcebd6ac
+        GIT_TAG ccfa7c9c1f1f540fa7f433a93d32bed772aa44f4
         TEST_DIR test/sql
-        APPLY_PATCHES
     )
 
 ################# MYSQL
