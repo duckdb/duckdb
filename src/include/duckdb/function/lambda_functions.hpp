44af400ba994aae4f44c90eaea99f3775d7d161d
Maia <maia@duckdblabs.com>
2025-05-21 13:36:32 +0200

    Refactor lambda binding to use vector of logical types for improved type handling
    Now the lambda function columns match the max logical type of the list child and initial value.

--- src/include/duckdb/function/lambda_functions.hpp
+++ src/include/duckdb/function/lambda_functions.hpp
@@ -53,10 +53,16 @@ public:
 
 class LambdaFunctions {
 public:
+	//! Returns the list child type
+	static LogicalType DetermineListChildType(const vector<LogicalType> &function_child_types);
+
 	//! Returns the parameter type for binary lambdas
-	static LogicalType BindBinaryLambda(const idx_t parameter_idx, const LogicalType &list_child_type);
-	//! Returns the parameter type for ternary lambdas
-	static LogicalType BindTernaryLambda(const idx_t parameter_idx, const LogicalType &list_child_type);
+	static LogicalType BindBinaryLambda(const idx_t parameter_idx, ClientContext &context,
+	                                    const vector<LogicalType> &function_child_types);
+
+	//! Returns the parameter type for ListReduce
+	static LogicalType BindReduce(const idx_t parameter_idx, ClientContext &context,
+	                              const vector<LogicalType> &function_child_types);
 
 	//! Checks for NULL list parameter and prepared statements and adds bound cast expression
 	static unique_ptr<FunctionData> ListLambdaPrepareBind(vector<unique_ptr<Expression>> &arguments,
