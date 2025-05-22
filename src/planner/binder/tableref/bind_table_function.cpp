dc2da4f0de880d42281d25a8cb17f852cef8bead
niykko <niyko010@gmail.com>
2025-05-22 13:49:44 +0200

    format-fix

--- src/planner/binder/tableref/bind_table_function.cpp
+++ src/planner/binder/tableref/bind_table_function.cpp
@@ -241,12 +241,12 @@ unique_ptr<LogicalOperator> Binder::BindTableFunctionInternal(TableFunction &tab
 		}
 		bind_data = table_function.bind(context, bind_input, return_types, return_names);
 		if (table_function.ordinality_data.ordinality_request == Ordinality_request_t::REQUESTED &&
-			table_function.in_out_function) {
+		    table_function.in_out_function) {
 			return_types.emplace_back(LogicalType::BIGINT);
 			return_names.emplace_back("ordinality");
 			D_ASSERT(return_names.size() == return_types.size());
 			table_function.ordinality_data.column_id = return_types.size() - 1;
-			}
+		}
 
 	} else {
 		throw InvalidInputException("Cannot call function \"%s\" directly - it has no bind function",
