bf8c95dbb049837b1391aefd03a8f3018c96e1ea
Max Gabrielsson <max@gabrielsson.com>
2025-06-02 19:18:33 +0200

    patch oot extensions

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -115,7 +115,7 @@ duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
     GIT_TAG 494d94912cc7ebcd8c43c9b6fc173a3e4142740f
-    INCLUDE_DIR spatial/include
+    INCLUDE_DIR src/spatial
     TEST_DIR test/sql
     )
 endif()
