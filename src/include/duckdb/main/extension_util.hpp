0e3b3cf45a23fbf406b9cdaecd95fc1e83373ce9
Max Gabrielsson <max@gabrielsson.com>
2025-06-03 20:24:31 +0200

    add dummy extension util header

---
+++ src/include/duckdb/main/extension_util.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace duckdb {
+
+static_assert(false, "The DuckDB 'ExtensionUtil' class has been removed, see this PR for more details: "
+                     "https://github.com/duckdb/duckdb/pull/17772");
+
+} // namespace duckdb
