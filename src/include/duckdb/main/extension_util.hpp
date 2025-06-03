f9fcc356d52bfc8d2302698d33aa2fd4a5f7532e
Max Gabrielsson <max@gabrielsson.com>
2025-06-03 21:00:06 +0200

    dont run assert in clang tidy

--- src/include/duckdb/main/extension_util.hpp
+++ src/include/duckdb/main/extension_util.hpp
@@ -2,7 +2,11 @@
 
 namespace duckdb {
 
+#ifndef DUCKDB_CLANG_TIDY
+// NOLINTBEGIN
 static_assert(false, "The DuckDB 'ExtensionUtil' class has been removed, see this PR for more details: "
                      "https://github.com/duckdb/duckdb/pull/17772");
+// NOLINTEND
+#endif
 
 } // namespace duckdb
