3b27f81967e0dc1ce2dd288a2fdda2aefe9e17be
dtenwolde <dljtw98@gmail.com>
2025-06-03 09:08:59 +0200

    Lambda changes to parser

--- extension/autocomplete/include/inlined_grammar.hpp
+++ extension/autocomplete/include/inlined_grammar.hpp
@@ -126,7 +126,7 @@ const char INLINED_PEG_GRAMMAR[] = {
 	"CoalesceExpression <- 'COALESCE'i Parens(List(Expression))\n"
 	"ColumnsExpression <- '*'? 'COLUMNS'i Parens(Expression)\n"
 	"ExtractExpression <- 'EXTRACT'i Parens(Expression 'FROM'i Expression)\n"
-	"LambdaExpression <- 'LAMBDA'i Identifier (',' Identifier)? ':' Expression\n"
+	"LambdaExpression <- 'LAMBDA'i Identifier (',' Identifier)* ':' Expression\n"
 	"NullIfExpression <- 'NULLIF'i Parens(Expression ',' Expression)\n"
 	"PositionExpression <- 'POSITION'i Parens(Expression)\n"
 	"RowExpression <- 'ROW'i Parens(List(Expression))\n"
