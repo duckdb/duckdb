71b4ba65891998b6c89cbdc304c0c85853eaae16
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-06-04 14:55:28 +0200

    add ducklake to the OOTE.cmake

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -64,6 +64,14 @@ if (NOT MINGW AND NOT "${OS_NAME}" STREQUAL "linux" AND NOT ${WASM_ENABLED})
 endif()
 endif()
 
+################# DUCKLAKE
+duckdb_extension_load(ducklake
+        APPLY_PATCHES
+        LOAD_TESTS
+        GIT_URL https://github.com/duckdb/ducklake
+        GIT_TAG d4e144737bc3f88aef1d8768cb2ef162b0db9f09
+        )
+
 ################# EXCEL
 duckdb_extension_load(excel
     LOAD_TESTS
