815a221673970e3eafd760e2c6a3ba0da712f3f4
damon <wangmengdamon@gmail.com>
2025-06-04 18:04:00 +0800

    Check page filtered out flag before reading it

--- extension/parquet/column_reader.cpp
+++ extension/parquet/column_reader.cpp
@@ -713,6 +713,12 @@ void ColumnReader::ApplyPendingSkips(data_ptr_t define_out, data_ptr_t repeat_ou
 
 	while (to_skip > 0) {
 		auto skip_now = ReadPageHeaders(to_skip);
+		if (page_is_filtered_out) {
+			// the page has been filtered out entirely - skip
+			page_rows_available -= skip_now;
+			to_skip -= skip_now;
+			continue;
+		}
 		const auto all_valid = PrepareRead(skip_now, define_out, repeat_out, 0);
 
 		const auto define_ptr = all_valid ? nullptr : static_cast<uint8_t *>(define_out);
