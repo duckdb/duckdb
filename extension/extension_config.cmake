31d2ecb317c73619f780a34efae14303d273c24f
ccfelius <felius@cwi.nl>
2025-06-05 18:25:44 +0200

    fix extension config

--- extension/extension_config.cmake
+++ extension/extension_config.cmake
@@ -11,11 +11,6 @@
 duckdb_extension_load(core_functions)
 duckdb_extension_load(parquet)
 
-duckdb_extension_load(httpfs
-        DONT_LINK
-        SOURCE_DIR /Users/lottefeliuscwi/Documents/Projects/duckdb-httpfs
-)
-
 # The Linux allocator has issues so we use jemalloc, but only on x86 because page sizes are fixed at 4KB.
 # Configuring jemalloc properly for 32bit is a hassle, and not worth it so we only enable on 64bit
 if(CMAKE_SIZEOF_VOID_P EQUAL 8 AND NOT FORCE_32_BIT AND OS_NAME STREQUAL "linux" AND NOT WASM_LOADABLE_EXTENSIONS AND NOT CLANG_TIDY AND NOT ANDROID AND NOT ZOS AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
