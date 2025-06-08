c3758f87356a163a0eb9e50812efb406149c26af
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-06-08 12:05:27 +0200

    Add header to allow checking usage of deprecated functions or classes

---
+++ src/include/duckdb/common/shadow_forbidden_functions.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "duckdb/wrapped/std/memory.hpp"
+#include "duckdb/wrapped/std/locale.hpp"
+#include "duckdb/wrapped/std/sstream.hpp"
+#include "duckdb/common/unique_ptr.hpp"
+#include "duckdb/common/shared_ptr.hpp"
+
+namespace std {
+template <class C>
+bool isspace(C c) {
+	static_assert(
+	    sizeof(C) == 0,
+	    "Naked issspace is discontinued, either use StringUtil::CharacterIsSpace or duckdb_wrapped::std::isspace");
+	return false;
+}
+#ifndef DUCKDB_ENABLE_DEPRECATED_API
+template <class T, class... ARGS>
+static std::unique_ptr<T> make_unique(ARGS &&...__args) { // NOLINT: mimic std style
+	static_assert(sizeof(T) == 0, "Use make_uniq instead of make_unique!");
+	return nullptr;
+}
+
+template <class T, class... ARGS>
+static std::shared_ptr<T> make_shared(ARGS &&...__args) { // NOLINT: mimic std style
+	static_assert(sizeof(T) == 0, "Use make_shared_ptr instead of make_shared!");
+	return nullptr;
+}
+#endif // DUCKDB_ENABLE_DEPRECATED_API
+
+} // namespace std
+
+#ifndef DUCKDB_CLANG_TIDY
+using std::isspace;
+using std::make_shared;
+using std::make_unique;
+#endif
