e5c941afe0a673ec946fb3ae33a75372278b2b78
Mytherin <mark.raasveldt@gmail.com>
2025-06-10 14:07:01 +0200

    Add FinalizeLoad callback to catalogs, which can be called after the database is fully instantiated

--- src/catalog/catalog.cpp
+++ src/catalog/catalog.cpp
@@ -1201,6 +1201,9 @@ void Catalog::Initialize(optional_ptr<ClientContext> context, bool load_builtin)
 	Initialize(load_builtin);
 }
 
+void Catalog::FinalizeLoad(optional_ptr<ClientContext> context) {
+}
+
 void Catalog::OnDetach(ClientContext &context) {
 }
 
