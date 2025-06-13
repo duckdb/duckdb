5c8749bf3b7d0769130bbc467c086b2252341c38
Maia <maia@duckdblabs.com>
2025-06-13 10:35:18 +0200

    debug

--- src/function/cast/struct_cast.cpp
+++ src/function/cast/struct_cast.cpp
@@ -3,6 +3,7 @@
 #include "duckdb/function/cast/default_casts.hpp"
 #include "duckdb/function/cast/bound_cast_data.hpp"
 #include "duckdb/function/cast/vector_cast_helpers.hpp"
+#include "duckdb/common/printer.hpp"
 
 namespace duckdb {
 
