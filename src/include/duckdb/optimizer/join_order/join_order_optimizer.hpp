9590c3d2835986931536fe5d01ba880182abf09f
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-06-13 13:04:37 +0200

    Use max_expression_depth to determine the maximum depth

--- src/include/duckdb/optimizer/join_order/join_order_optimizer.hpp
+++ src/include/duckdb/optimizer/join_order/join_order_optimizer.hpp
@@ -61,7 +61,6 @@ private:
 	//! Stats of Delim Scans of the Delim Join that is currently being optimized
 	optional_ptr<RelationStats> delim_scan_stats;
 	idx_t depth;
-	static constexpr idx_t THRESHOLD_AVOID_IN_DEPTH_VISIT = 100;
 };
 
 } // namespace duckdb
