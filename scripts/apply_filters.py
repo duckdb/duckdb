632f68bde3c13149f147d37b828e5ae7ac3d8d09
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-07-03 17:56:14 +0200

    added debug prints

--- scripts/apply_filters.py
+++ scripts/apply_filters.py
@@ -14,28 +14,28 @@ def get_paths_ignore_for(workflow):
 
 # check updated files in PR
 def get_changed_files_pr():
-    pr_number = os.environ["PR_NUMBER"] if "PR_NUMBER" in os.environ else None
-    # gh_pr_diff = ['gh', 'pr', 'diff', '18132', '--name-only']
-    gh_pr_diff = ['gh', 'pr', 'diff', str(pr_number), '--name-only']
+    # pr_number = os.environ["PR_NUMBER"] if "PR_NUMBER" in os.environ else None
+    gh_pr_diff = ['gh', 'pr', 'diff', '18132', '--name-only']
+    # gh_pr_diff = ['gh', 'pr', 'diff', str(pr_number), '--name-only']
     result = subprocess.run(gh_pr_diff, text=True, check=True, capture_output=True)
     files = result.stdout.splitlines()
     return files
 
 def get_changed_files_push():
-    before = os.environ.get('GITHUB_EVENT_BEFORE')
-    after = os.environ.get('GITHUB_SHA')
-    # before = 'fb97862'
-    # after = 'ad80307'
+    # before = os.environ.get('GITHUB_EVENT_BEFORE')
+    # after = os.environ.get('GITHUB_SHA')
+    before = 'fb97862'
+    after = 'ad80307'
 
     files = []
     if before and after:
-        subprocess.run(['git', 'fetch', '--no-tags', '--depth=100', 'origin', os.environ['GITHUB_REF']], check=True)
-        # subprocess.run(['git', 'fetch', '--no-tags', '--depth=100', 'origin', 'refs/heads/main'], check=True)
+        # subprocess.run(['git', 'fetch', '--no-tags', '--depth=100', 'origin', os.environ['GITHUB_REF']], check=True)
+        subprocess.run(['git', 'fetch', '--no-tags', '--depth=100', 'origin', 'refs/heads/main'], check=True)
         result = subprocess.check_output(['git', 'diff', '--name-only', before, after])
         files = result.decode().splitlines()
     return files
 
-
+# returns False when there 
 def should_run_workflow(changed_files, paths_ignore):
     for file in changed_files:
         if not any(fnmatch.fnmatch(file, path) for path in paths_ignore):
@@ -58,6 +58,10 @@ changed_files = get_changed_files()
 for workflow, filters in config['workflows'].items():
     result = should_run_workflow(changed_files, filters.get('paths-ignore', []))
     result_config[workflow] = result
+
+print(changed_files)
+print(filters.get('paths-ignore', []))
+print(result_config)
 
 # write result config to the GH Output
 with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
