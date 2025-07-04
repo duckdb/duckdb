ecb0e444b07b138bbfd2829dde56de16a25776f4
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2025-07-04 15:51:09 +0200

    include fixes

--- src/include/duckdb/common/operator/string_cast.hpp
+++ src/include/duckdb/common/operator/string_cast.hpp
@@ -9,9 +9,11 @@
 #pragma once
 
 #include "duckdb/common/common.hpp"
-#include "duckdb/common/types.hpp"
 #include "duckdb/common/exception.hpp"
+#include "duckdb/common/types.hpp"
+#include "duckdb/common/types/date.hpp"
 #include "duckdb/common/types/string_type.hpp"
+#include "duckdb/common/types/timestamp.hpp"
 
 namespace duckdb {
 
