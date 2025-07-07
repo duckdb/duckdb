3fb764e28614b574d2d11740f5dd10af484edd89
Tishj <t_b@live.nl>
2025-07-07 17:28:00 +0200

    this seems pretty barebones actually

--- extension/parquet/include/reader/variant/variant_binary_decoder.hpp
+++ extension/parquet/include/reader/variant/variant_binary_decoder.hpp
@@ -4,12 +4,115 @@
 
 namespace duckdb {
 
+//! ------------ Metadata ------------
+
+struct VariantMetadataHeader {
+public:
+	static VariantMetadataHeader FromHeaderByte(uint8_t byte);
+
+public:
+	//! The version of the protocol used (only '1' supported for now)
+	uint8_t version;
+	//! Number of bytes per dictionary size and offset field
+	uint8_t offset_size;
+	//! Whether dictionary strings are sorted and unique
+	bool sorted_strings = false;
+};
+
+struct VariantMetadata {
+public:
+	explicit VariantMetadata(const string_t &metadata);
+
+public:
+	const string_t &metadata;
+
+public:
+	VariantMetadataHeader header;
+	const_data_ptr_t offsets;
+	idx_t offsets_length;
+	const_data_ptr_t bytes;
+};
+
+//! ------------ Value ------------
+
+enum class VariantBasicType : uint8_t { PRIMITIVE = 0, SHORT_STRING = 1, OBJECT = 2, ARRAY = 3 };
+
+enum class VariantPrimitiveType : uint8_t {
+	NULL_TYPE = 0,
+	BOOLEAN_TRUE = 1,
+	BOOLEAN_FALSE = 2,
+	INT8 = 3,
+	INT16 = 4,
+	INT32 = 5,
+	INT64 = 6,
+	DOUBLE = 7,
+	DECIMAL4 = 8,
+	DECIMAL8 = 9,
+	DECIMAL16 = 10,
+	DATE = 11,
+	TIMESTAMP_MICROS = 12,
+	TIMESTAMP_NTZ_MICROS = 13,
+	FLOAT = 14,
+	BINARY = 15,
+	STRING = 16,
+	TIME_NTZ = 17,
+	TIMESTAMP_NANOS = 18,
+	TIMESTAMP_NTZ_NANOS = 19,
+	UUID = 20
+};
+
+static VariantBasicType VariantBasicTypeFromByte(uint8_t byte) {
+	switch (byte) {
+	case 0:
+		return VariantBasicType::PRIMITIVE;
+	case 1:
+		return VariantBasicType::SHORT_STRING;
+	case 2:
+		return VariantBasicType::OBJECT;
+	case 3:
+		return VariantBasicType::ARRAY;
+	default:
+		throw NotImplementedException("Variant BasicType (%d) is not supported", byte);
+	}
+}
+
+struct VariantValueMetadata {
+public:
+	VariantValueMetadata() {
+	}
+
+public:
+	static VariantValueMetadata FromHeaderByte(uint8_t byte);
+
+public:
+	VariantBasicType basic_type;
+	uint8_t header;
+};
+
+struct VariantValue {
+public:
+	VariantValue() {
+	}
+
+public:
+};
+
 class VariantBinaryDecoder {
 public:
 	VariantBinaryDecoder();
 
 public:
 	string_t Decode(const string_t &metadata, const string_t &blob);
+
+public:
+	string_t PrimitiveTypeDecode(const VariantMetadata &metadata, const VariantValueMetadata &value_metadata,
+	                             const string_t &blob);
+	string_t ShortStringDecode(const VariantMetadata &metadata, const VariantValueMetadata &value_metadata,
+	                           const string_t &blob);
+	string_t ObjectDecode(const VariantMetadata &metadata, const VariantValueMetadata &value_metadata,
+	                      const string_t &blob);
+	string_t ArrayDecode(const VariantMetadata &metadata, const VariantValueMetadata &value_metadata,
+	                     const string_t &blob);
 };
 
 } // namespace duckdb
--- extension/parquet/reader/variant/variant_binary_decoder.cpp
+++ extension/parquet/reader/variant/variant_binary_decoder.cpp
@@ -2,12 +2,14 @@
 #include "duckdb/common/printer.hpp"
 
 static constexpr uint8_t VERSION_MASK = 0xF;
-
 static constexpr uint8_t SORTED_STRINGS_MASK = 0x1;
 static constexpr uint8_t SORTED_STRINGS_SHIFT = 4;
-
 static constexpr uint8_t OFFSET_SIZE_MINUS_ONE_MASK = 0x3;
 static constexpr uint8_t OFFSET_SIZE_MINUS_ONE_SHIFT = 5;
+
+static constexpr uint8_t BASIC_TYPE_MASK = 0x1;
+static constexpr uint8_t VALUE_HEADER_MASK = 0x2;
+static constexpr uint8_t VALUE_HEADER_SHIFT = 1;
 
 namespace duckdb {
 
@@ -23,64 +25,85 @@ static idx_t ReadVariableLengthLittleEndian(idx_t length_in_bytes, const_data_pt
 	return result;
 }
 
-struct VariantMetadataHeader {
-public:
-	static VariantMetadataHeader FromHeaderByte(uint8_t byte) {
-		VariantMetadataHeader header;
-		header.version = byte & VERSION_MASK;
-		header.sorted_strings = (byte >> SORTED_STRINGS_SHIFT) & SORTED_STRINGS_MASK;
-		header.offset_size = ((byte >> OFFSET_SIZE_MINUS_ONE_SHIFT) & OFFSET_SIZE_MINUS_ONE_MASK) + 1;
-		return header;
-	}
-
-public:
-	//! The version of the protocol used (only '1' supported for now)
-	uint8_t version;
-	//! Number of bytes per dictionary size and offset field
-	uint8_t offset_size;
-	//! Whether dictionary strings are sorted and unique
-	bool sorted_strings = false;
-};
-
-struct VariantMetadata {
-public:
-	explicit VariantMetadata(const string_t &metadata) : metadata(metadata) {
-		auto metadata_length = metadata.GetSize();
-		auto metadata_data = metadata.GetData();
-
-		header = VariantMetadataHeader::FromHeaderByte(metadata_data[0]);
-
-		const_data_ptr_t ptr = reinterpret_cast<const_data_ptr_t>(metadata_data + sizeof(uint8_t));
-		idx_t dictionary_size = ReadVariableLengthLittleEndian(header.offset_size, ptr);
-
-		offsets = ptr;
-		offsets_length = dictionary_size + 1;
-		for (idx_t i = 0; i < offsets_length; i++) {
-			auto offset = ReadVariableLengthLittleEndian(header.offset_size, ptr);
-			Printer::PrintF("Offset[%d] = %d", i, offset);
-		}
-		bytes = ptr;
-	}
-
-public:
-	const string_t &metadata;
-
-public:
-	VariantMetadataHeader header;
-	const_data_ptr_t offsets;
-	idx_t offsets_length;
-	const_data_ptr_t bytes;
-};
-
 } // namespace
 
+VariantMetadataHeader VariantMetadataHeader::FromHeaderByte(uint8_t byte) {
+	VariantMetadataHeader header;
+	header.version = byte & VERSION_MASK;
+	header.sorted_strings = (byte >> SORTED_STRINGS_SHIFT) & SORTED_STRINGS_MASK;
+	header.offset_size = ((byte >> OFFSET_SIZE_MINUS_ONE_SHIFT) & OFFSET_SIZE_MINUS_ONE_MASK) + 1;
+	return header;
+}
+
+VariantMetadata::VariantMetadata(const string_t &metadata) : metadata(metadata) {
+	auto metadata_length = metadata.GetSize();
+	auto metadata_data = metadata.GetData();
+
+	header = VariantMetadataHeader::FromHeaderByte(metadata_data[0]);
+
+	const_data_ptr_t ptr = reinterpret_cast<const_data_ptr_t>(metadata_data + sizeof(uint8_t));
+	idx_t dictionary_size = ReadVariableLengthLittleEndian(header.offset_size, ptr);
+
+	offsets = ptr;
+	offsets_length = dictionary_size + 1;
+	for (idx_t i = 0; i < offsets_length; i++) {
+		auto offset = ReadVariableLengthLittleEndian(header.offset_size, ptr);
+		Printer::PrintF("Offset[%d] = %d", i, offset);
+	}
+	bytes = ptr;
+}
+
+VariantValueMetadata VariantValueMetadata::FromHeaderByte(uint8_t byte) {
+	VariantValueMetadata result;
+	result.basic_type = VariantBasicTypeFromByte(byte & BASIC_TYPE_MASK);
+	result.header = (byte >> VALUE_HEADER_SHIFT) & VALUE_HEADER_MASK;
+	return result;
+}
+
 VariantBinaryDecoder::VariantBinaryDecoder() {
+}
+
+string_t VariantBinaryDecoder::PrimitiveTypeDecode(const VariantMetadata &metadata,
+                                                   const VariantValueMetadata &value_metadata, const string_t &blob) {
+	throw NotImplementedException("VariantBinaryDecoder::PrimitiveTypeDecode");
+}
+
+string_t VariantBinaryDecoder::ShortStringDecode(const VariantMetadata &metadata,
+                                                 const VariantValueMetadata &value_metadata, const string_t &blob) {
+	throw NotImplementedException("VariantBinaryDecoder::ShortStringDecode");
+}
+
+string_t VariantBinaryDecoder::ObjectDecode(const VariantMetadata &metadata, const VariantValueMetadata &value_metadata,
+                                            const string_t &blob) {
+	throw NotImplementedException("VariantBinaryDecoder::ObjectDecode");
+}
+
+string_t VariantBinaryDecoder::ArrayDecode(const VariantMetadata &metadata, const VariantValueMetadata &value_metadata,
+                                           const string_t &blob) {
+	throw NotImplementedException("VariantBinaryDecoder::ArrayDecode");
 }
 
 string_t VariantBinaryDecoder::Decode(const string_t &metadata, const string_t &blob) {
 	VariantMetadata variant_metadata(metadata);
 
-	return string_t();
+	auto value_length = blob.GetSize();
+	auto value_data = blob.GetData();
+	auto value_metadata = VariantValueMetadata::FromHeaderByte(value_data[0]);
+
+	switch (value_metadata.basic_type) {
+	case VariantBasicType::PRIMITIVE: {
+		return PrimitiveTypeDecode(variant_metadata, value_metadata, blob);
+	}
+	case VariantBasicType::SHORT_STRING: {
+		return ShortStringDecode(variant_metadata, value_metadata, blob);
+	}
+	case VariantBasicType::OBJECT: {
+		return ObjectDecode(variant_metadata, value_metadata, blob);
+	}
+	case VariantBasicType::ARRAY: {
+		return ArrayDecode(variant_metadata, value_metadata, blob);
+	}
+	}
 }
 
 } // namespace duckdb
