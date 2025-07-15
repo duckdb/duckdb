1f1f0824a7960f0502815180d3c17dcc017d1470
Mytherin <mark.raasveldt@gmail.com>
2025-07-15 09:20:28 +0200

    Missing include

--- src/include/duckdb/storage/compression/patas/patas_scan.hpp
+++ src/include/duckdb/storage/compression/patas/patas_scan.hpp
@@ -12,6 +12,7 @@
 #include "duckdb/storage/compression/chimp/algorithm/chimp_utils.hpp"
 #include "duckdb/storage/compression/chimp/algorithm/byte_reader.hpp"
 #include "duckdb/storage/compression/patas/shared.hpp"
+#include "duckdb/storage/compression/patas/algorithm/patas.hpp"
 
 #include "duckdb/common/limits.hpp"
 #include "duckdb/common/types/null_value.hpp"
