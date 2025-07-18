919441b87e48a49b0dc0ed47befb3ea4adbd6473
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2025-07-18 16:58:49 +0200

    nit

--- src/include/duckdb/storage/table/append_state.hpp
+++ src/include/duckdb/storage/table/append_state.hpp
@@ -89,7 +89,7 @@ struct LocalAppendState {
 	TableAppendState append_state;
 	LocalTableStorage *storage;
 	unique_ptr<ConstraintState> constraint_state;
-	bool wal_append = false; // TODO: should this be an enum?
+	bool wal_append = false;
 };
 
 } // namespace duckdb
