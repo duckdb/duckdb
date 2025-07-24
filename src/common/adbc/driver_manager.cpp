feee2dfdc45c76b80b5bda6e5a032fe132f1b087
Sheldon Robinson <sheldon.robinson@gmail.com>
2025-07-24 18:42:21 -0400

    Tabwidth 4, 2 Tab indent

--- src/common/adbc/driver_manager.cpp
+++ src/common/adbc/driver_manager.cpp
@@ -52,8 +52,8 @@ void GetWinError(std::string *buffer) {
 	LPVOID message;
 
 	FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                    /*lpSource=*/nullptr, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-                    reinterpret_cast<LPSTR>(&message), /*nSize=*/0, /*Arguments=*/nullptr);
+                /*lpSource=*/nullptr, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                reinterpret_cast<LPSTR>(&message), /*nSize=*/0, /*Arguments=*/nullptr);
 
 	(*buffer) += '(';
 	(*buffer) += std::to_string(rc);
