0c104f6fd403394253cf4044a71236d0dcf77e96
dtenwolde <dljtw98@gmail.com>
2025-07-24 12:02:31 +0200

    Add some validation logic to register keywords

--- src/parser/parser_keyword_manager.cpp
+++ src/parser/parser_keyword_manager.cpp
@@ -7,6 +7,26 @@ void ParserKeywordManager::RegisterKeyword(const string &keyword, KeywordCategor
 	// Lock the mutex to ensure thread-safe modification of the sets
 	std::lock_guard<std::mutex> lock(keyword_mutex);
 
+	if (category == KeywordCategory::KEYWORD_RESERVED) {
+		if (unreserved_keywords.count(keyword) || type_func_keywords.count(keyword) || col_name_keywords.count(keyword)) {
+			throw InvalidInputException(
+				"Cannot register keyword \"%s\" as RESERVED, as it is already registered in a conflicting category.",
+				keyword);
+		}
+	} else if (category == KeywordCategory::KEYWORD_UNRESERVED) {
+		if (reserved_keywords.count(keyword)) {
+			throw InvalidInputException(
+				"Cannot register keyword \"%s\" as UNRESERVED, as it is already registered as RESERVED.", keyword);
+		}
+	} else if (category == KeywordCategory::KEYWORD_TYPE_FUNC || category == KeywordCategory::KEYWORD_COL_NAME) {
+		if (reserved_keywords.count(keyword)) {
+			const char *category_name =
+				(category == KeywordCategory::KEYWORD_TYPE_FUNC) ? "TYPE_FUNC" : "COL_NAME";
+			throw InvalidInputException("Cannot register keyword \"%s\" as %s, as it is already registered as RESERVED.",
+										keyword, category_name);
+		}
+	}
+
 	switch (category) {
 	case KeywordCategory::KEYWORD_RESERVED:
 		reserved_keywords.insert(keyword);
