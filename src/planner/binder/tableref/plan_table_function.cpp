83884873229c22c2be7263ec9a971c9c5ac90d56
niykko <niyko010@gmail.com>
2025-07-30 13:01:05 +0200

    change raw pointer to reference

--- src/planner/binder/tableref/plan_table_function.cpp
+++ src/planner/binder/tableref/plan_table_function.cpp
@@ -7,20 +7,20 @@ unique_ptr<LogicalOperator> Binder::CreatePlan(BoundTableFunction &ref) {
 	if (ref.subquery) {
 		auto child_node = CreatePlan(*ref.subquery);
 
-		LogicalOperator *node = ref.get.get();
+		reference<LogicalOperator> node = *ref.get;
 
-		while (!node->children.empty()) {
-			D_ASSERT(node->children.size() == 1);
-			if (node->children.size() != 1) {
+		while (!node.get().children.empty()) {
+			D_ASSERT(node.get().children.size() == 1);
+			if (node.get().children.size() != 1) {
 				throw InternalException(
 				    "Binder::CreatePlan<BoundTableFunction>: linear path expected, but found node with %d children",
-				    node->children.size());
+				    node.get().children.size());
 			}
-			node = node->children[0].get();
+			node = *node.get().children[0];
 		}
 
-		D_ASSERT(node->type == LogicalOperatorType::LOGICAL_GET);
-		node->children.push_back(std::move(child_node));
+		D_ASSERT(node.get().type == LogicalOperatorType::LOGICAL_GET);
+		node.get().children.push_back(std::move(child_node));
 	}
 	return std::move(ref.get);
 }
