04f3fb4d78ee2b1a0db452212716c040bc934bc7
Mytherin <mark.raasveldt@gmail.com>
2025-08-04 20:24:36 +0200

    In ParseInfo::QualifierToString, also stringify schema when it is the default schema

--- src/parser/parsed_data/parse_info.cpp
+++ src/parser/parsed_data/parse_info.cpp
@@ -37,10 +37,8 @@ string ParseInfo::QualifierToString(const string &catalog, const string &schema,
 	string result;
 	if (!catalog.empty()) {
 		result += KeywordHelper::WriteOptionallyQuoted(catalog) + ".";
-		if (!schema.empty()) {
-			result += KeywordHelper::WriteOptionallyQuoted(schema) + ".";
-		}
-	} else if (!schema.empty() && schema != DEFAULT_SCHEMA) {
+	}
+	if (!schema.empty()) {
 		result += KeywordHelper::WriteOptionallyQuoted(schema) + ".";
 	}
 	result += KeywordHelper::WriteOptionallyQuoted(name);
