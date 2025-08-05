1333204022914bddbe07c07c614574f89793c7ea
Mytherin <mark.raasveldt@gmail.com>
2025-08-05 13:54:32 +0200

    Format fix

--- src/common/local_file_system.cpp
+++ src/common/local_file_system.cpp
@@ -796,8 +796,8 @@ std::string LocalFileSystem::GetLastErrorAsString() {
 
 	LPWSTR messageBuffer = nullptr;
 	idx_t size = FormatMessageW(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
-		errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&messageBuffer, 0, NULL);
+	    FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
+	    errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&messageBuffer, 0, NULL);
 
 	if (size == 0) {
 		return std::string();
