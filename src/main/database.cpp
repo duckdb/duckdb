62725b2ef857d2ae886d16fd42c92287047e0fc2
dtenwolde <dljtw98@gmail.com>
2025-08-06 12:53:26 +0200

    Add a missing if

--- src/main/database.cpp
+++ src/main/database.cpp
@@ -463,7 +463,9 @@ void DatabaseInstance::Configure(DBConfig &new_config, const char *database_path
 	}
 	config.replacement_scans = std::move(new_config.replacement_scans);
 	config.parser_extensions = std::move(new_config.parser_extensions);
-	config.keyword_manager = std::move(new_config.keyword_manager);
+	if (new_config.keyword_manager) {
+		config.keyword_manager = std::move(new_config.keyword_manager);
+	}
 	config.error_manager = std::move(new_config.error_manager);
 	if (!config.error_manager) {
 		config.error_manager = make_uniq<ErrorManager>();
