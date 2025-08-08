5bda8b3b3bcb8995b434dbe4816e5fea5b4aa8b4
Hannes Mühleisen <hannes@muehleisen.org>
2025-08-08 14:31:17 +0200

    fixing compilation with -std=cpp23

--- src/include/duckdb/parser/common_table_expression_info.hpp
+++ src/include/duckdb/parser/common_table_expression_info.hpp
@@ -8,7 +8,6 @@
 
 #pragma once
 
-#include "duckdb/parser/statement/select_statement.hpp"
 #include "duckdb/common/enums/cte_materialize.hpp"
 
 namespace duckdb {
@@ -24,6 +23,8 @@ struct CommonTableExpressionInfo {
 	void Serialize(Serializer &serializer) const;
 	static unique_ptr<CommonTableExpressionInfo> Deserialize(Deserializer &deserializer);
 	unique_ptr<CommonTableExpressionInfo> Copy();
+
+	~CommonTableExpressionInfo();
 };
 
 } // namespace duckdb
