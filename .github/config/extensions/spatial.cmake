d7672d2d2205a7df058155e1266c5b50a82748e5
Max Gabrielsson <max@gabrielsson.com>
2025-08-17 10:40:54 +0200

    bump again

--- .github/config/extensions/spatial.cmake
+++ .github/config/extensions/spatial.cmake
@@ -3,7 +3,7 @@ if (NOT MINGW AND ${BUILD_COMPLETE_EXTENSION_SET})
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG cf29a21952cdbecbe2961ea93d2e46cb74ac3e2b
+    GIT_TAG 35a3635d6e51151134226f38ffe1c4368a5a5292
     INCLUDE_DIR src/spatial
     TEST_DIR test/sql
     )
