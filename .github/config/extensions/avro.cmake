98265eeaf522944f0b3c944655e7b7c778e4bfe3
Alejandro Wainzinger <alejandro.wainzinger@gmail.com>
2025-08-20 12:20:53 -0700

    Bump avro.

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW)
     duckdb_extension_load(avro
             LOAD_TESTS DONT_LINK
             GIT_URL https://github.com/duckdb/duckdb-avro
-            GIT_TAG b75cb5cea43d3e2bee6f5d0f377aa99354dd868a
+            GIT_TAG 3b9a6dc5858b4c21d20201cc6fa416589ceaf24b
     )
 endif()
