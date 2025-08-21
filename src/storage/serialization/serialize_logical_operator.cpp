dd7bc58fc64dd538cb125abc5d0d0694f99f4445
Björn Bamberg <bjoern.bamberg@uni-tuebingen.de>
2025-08-21 16:23:48 +0200

    Fix serializiation

--- src/storage/serialization/serialize_logical_operator.cpp
+++ src/storage/serialization/serialize_logical_operator.cpp
@@ -688,6 +688,8 @@ void LogicalRecursiveCTE::Serialize(Serializer &serializer) const {
 	serializer.WritePropertyWithDefault<vector<unique_ptr<Expression>>>(204, "key_targets", key_targets);
 	serializer.WritePropertyWithDefault<bool>(205, "ref_recurring", ref_recurring);
 	serializer.WritePropertyWithDefault<vector<unique_ptr<Expression>>>(206, "payload_aggregates", payload_aggregates);
+	serializer.WritePropertyWithDefault<vector<LogicalType>>(207, "internal_types", internal_types);
+	serializer.WritePropertyWithDefault<vector<LogicalType>>(208, "result_types", result_types);
 }
 
 unique_ptr<LogicalOperator> LogicalRecursiveCTE::Deserialize(Deserializer &deserializer) {
@@ -699,6 +701,8 @@ unique_ptr<LogicalOperator> LogicalRecursiveCTE::Deserialize(Deserializer &deser
 	deserializer.ReadPropertyWithDefault<vector<unique_ptr<Expression>>>(204, "key_targets", result->key_targets);
 	deserializer.ReadPropertyWithDefault<bool>(205, "ref_recurring", result->ref_recurring);
 	deserializer.ReadPropertyWithDefault<vector<unique_ptr<Expression>>>(206, "payload_aggregates", result->payload_aggregates);
+	deserializer.ReadPropertyWithDefault<vector<LogicalType>>(207, "internal_types", result->internal_types);
+	deserializer.ReadPropertyWithDefault<vector<LogicalType>>(208, "result_types", result->result_types);
 	return std::move(result);
 }
 
