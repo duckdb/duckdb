0b02610ff595891de18360a911106c8e265a29e2
Tmonster <tom@ebergen.com>
2025-09-01 10:04:08 +0200

    also enable loading of tests

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -8,7 +8,7 @@ endif()
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(iceberg
             APPLY_PATCHES
-#            ${LOAD_ICEBERG_TESTS} TODO: re-enable once autoloading test is fixed
+            ${LOAD_ICEBERG_TESTS} 
             GIT_URL https://github.com/duckdb/duckdb-iceberg
             GIT_TAG 213805555a74528f6117fe563d842649982fa415
             )
