c4e83d766f476438d97fd0f9ad9438a5dc1cd8f1
pdet <pedroholanda@gmail.com>
2025-09-02 14:04:54 +0200

    Bump Ducklake

--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(ducklake
     DONT_LINK
     GIT_URL https://github.com/duckdb/ducklake
-    GIT_TAG d2392c36f33151cf5cdd7d006375b0b669bd44ac
+    GIT_TAG c1ebd032eb4c763910551c08f4b61bdb8168f209
     APPLY_PATCHES
 )
