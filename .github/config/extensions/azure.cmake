cab89e43c59bc874b3b2efffa2048fb83c77685a
ben fleis <benfleis@users.noreply.github.com>
2025-09-04 16:35:29 +0200

    remove patch & APPLY_PATCHES flag

--- .github/config/extensions/azure.cmake
+++ .github/config/extensions/azure.cmake
@@ -1,6 +1,5 @@
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(azure
-            APPLY_PATCHES
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-azure
             GIT_TAG 5e458fcc466d2bc421922b11f4316564e3017800
