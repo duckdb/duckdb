1a69957a350c2961bc9bd9b4d9c3faa31b1ab3a0
Tmonster <tom@ebergen.com>
2025-09-04 15:04:45 +0200

    update the hash

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -8,7 +8,7 @@ endif()
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(iceberg
 #            ${LOAD_ICEBERG_TESTS} TODO: re-enable once autoloading test is fixed
-            GIT_URL https://github.com/Tmonster/duckdb-iceberg
-            GIT_TAG c9b9dd2525db13bb3f80f44878e0bdbf6efda969
+            GIT_URL https://github.com/duckdb/duckdb-iceberg
+            GIT_TAG 0522efe2074a1e204c93ffe9dfa4012beb4967c5
             )
 endif()
