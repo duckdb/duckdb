ab2ce5688f13e9a4af0648d6477b094fa151227b
Mytherin <mark.raasveldt@gmail.com>
2025-09-05 18:47:12 +0200

    Keep base data scan state alive in ColumnData::Update call

--- src/storage/table/standard_column_data.cpp
+++ src/storage/table/standard_column_data.cpp
@@ -154,9 +154,10 @@ idx_t StandardColumnData::Fetch(ColumnScanState &state, row_t row_id, Vector &re
 
 void StandardColumnData::Update(TransactionData transaction, idx_t column_index, Vector &update_vector, row_t *row_ids,
                                 idx_t update_count) {
+	ColumnScanState standard_state, validity_state;
 	Vector base_vector(type);
-	auto standard_fetch = FetchUpdateData(row_ids, base_vector);
-	auto validity_fetch = validity.FetchUpdateData(row_ids, base_vector);
+	auto standard_fetch = FetchUpdateData(standard_state, row_ids, base_vector);
+	auto validity_fetch = validity.FetchUpdateData(validity_state, row_ids, base_vector);
 	if (standard_fetch != validity_fetch) {
 		throw InternalException("Unaligned fetch in validity and main column data for update");
 	}
