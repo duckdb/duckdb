ea5625f03d36628a6b5f5cff08ce90f1ea57cf04
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-09-06 09:52:58 +0200

    Bump httpfs to v1.4-andium branch
    A smaller bump is still required, but getting closer

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,7 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG c31f9e922a5cc99c5854fd9529954cb364afd1f1
+    GIT_TAG d39fd75ed0544601f7b745c5de5fbeb916b14c1b
     INCLUDE_DIR extension/httpfs/include
-    APPLY_PATCHES
 )
