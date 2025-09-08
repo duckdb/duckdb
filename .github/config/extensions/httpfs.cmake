8b40379b65874ecedf1f6ac0b2137cbc29bfc9f4
Mark <mark.raasveldt@gmail.com>
2025-09-08 19:55:38 +0200

    Update httpfs.cmake

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG bba3300dac33426fd7b58f1e4eb2f7374c30be4e
+    GIT_TAG dc87eee8851425de259eb9535b121b76fc346cee
     INCLUDE_DIR extension/httpfs/include
 )
