054bf7305201409aa292a1d86d9c1c04b155343d
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-09-08 20:24:48 +0200

    Bump httpfs

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG dc87eee8851425de259eb9535b121b76fc346cee
+    GIT_TAG 7ee09f439c52f706414f271988de93f695ebe721
     INCLUDE_DIR extension/httpfs/include
 )
