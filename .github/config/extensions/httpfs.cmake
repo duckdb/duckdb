b85a5988ac2985688a16d683ade8daed1d8665cd
Sam Ansmink <samansmink@hotmail.com>
2025-09-08 12:27:56 +0200

    bump httpfs again

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 7df8b5c94a35b6a19dc502432874b1cf0f43d73e
+    GIT_TAG 4135c19446bf5c8f60ccb94f21ff3876866e56ac
     INCLUDE_DIR extension/httpfs/include
 )
