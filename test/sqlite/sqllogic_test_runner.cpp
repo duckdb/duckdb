faa23332fdbae19801fc5500fcbdca93834dfcc1
Mytherin <mark.raasveldt@gmail.com>
2025-09-11 14:36:54 +0200

    Unify keyword replacement in TestConfiguration::ProcessPath

--- test/sqlite/sqllogic_test_runner.cpp
+++ test/sqlite/sqllogic_test_runner.cpp
@@ -223,12 +223,10 @@ string SQLLogicTestRunner::ReplaceKeywords(string input) {
 		auto &value = it.second;
 		input = StringUtil::Replace(input, StringUtil::Format("${%s}", name), value);
 	}
-	input = StringUtil::Replace(input, "{UUID}", UUID::ToString(UUID::GenerateRandomUUID()));
-	input = StringUtil::Replace(input, "__TEST_DIR__", TestDirectoryPath());
-	input = StringUtil::Replace(input, "__WORKING_DIRECTORY__", FileSystem::GetWorkingDirectory());
+	auto &test_config = TestConfiguration::Get();
+	test_config.ProcessPath(input, file_name);
 	input = StringUtil::Replace(input, "__BUILD_DIRECTORY__", DUCKDB_BUILD_DIRECTORY);
 
-	auto &test_config = TestConfiguration::Get();
 	string data_location = test_config.DataLocation();
 
 	input = StringUtil::Replace(input, "'data/", string("'") + data_location);
