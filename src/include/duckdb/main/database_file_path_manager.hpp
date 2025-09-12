c5baba5900448ecc78c4e53b572f7ea097f9e22f
Mytherin <mark.raasveldt@gmail.com>
2025-09-12 13:07:02 +0200

    Cleanup duplicate data path handling, and make IF NOT EXISTS no longer abort if we are adding a path with the same name

--- src/include/duckdb/main/database_file_path_manager.hpp
+++ src/include/duckdb/main/database_file_path_manager.hpp
@@ -13,13 +13,17 @@
 #include "duckdb/common/case_insensitive_map.hpp"
 
 namespace duckdb {
+struct AttachInfo;
+struct AttachOptions;
+
+enum class InsertDatabasePathResult { SUCCESS, ALREADY_EXISTS };
 
 //! The DatabaseFilePathManager is used to ensure we only ever open a single database file once
 class DatabaseFilePathManager {
 public:
-	void CheckPathConflict(const string &path, const string &name) const;
 	idx_t ApproxDatabaseCount() const;
-	void InsertDatabasePath(const string &path, const string &name);
+	InsertDatabasePathResult InsertDatabasePath(const string &path, const string &name, OnCreateConflict on_conflict,
+	                                            AttachOptions &options);
 	void EraseDatabasePath(const string &path);
 
 private:
