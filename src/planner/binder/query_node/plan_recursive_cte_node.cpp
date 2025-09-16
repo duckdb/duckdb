8daefbc7a592fb402ef35a4a5700a45cbb2cae0c
Björn Bamberg <bjoern.bamberg@uni-tuebingen.de>
2025-09-16 14:25:20 +0200

    Fix union semantik

--- src/planner/binder/query_node/plan_recursive_cte_node.cpp
+++ src/planner/binder/query_node/plan_recursive_cte_node.cpp
@@ -36,7 +36,7 @@ unique_ptr<LogicalOperator> Binder::CreatePlan(BoundRecursiveCTENode &node) {
 	     *node.right_binder->bind_context.cte_references[node.ctename] == 0) &&
 	    !ref_recurring) {
 		auto root = make_uniq<LogicalSetOperation>(node.setop_index, node.types.size(), std::move(left_node),
-		                                           std::move(right_node), LogicalOperatorType::LOGICAL_UNION, true);
+		                                           std::move(right_node), LogicalOperatorType::LOGICAL_UNION, node.union_all);
 		return VisitQueryNode(node, std::move(root));
 	}
 
