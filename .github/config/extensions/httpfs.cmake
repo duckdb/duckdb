d8a254294d11469e766136501e84880737e4aa17
Tmonster <tom@ebergen.com>
2025-09-22 17:53:33 +0200

    also update httpfs to apply patches

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -3,4 +3,5 @@ duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG 354d3f436a33f80f03a74419e76eb59459e19168
     INCLUDE_DIR extension/httpfs/include
+    APPLY_PATCHES
 )
