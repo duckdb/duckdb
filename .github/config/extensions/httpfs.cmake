5521f16429bb1b5fa0c5cd9c5eb18ee536f8974d
Tmonster <tom@ebergen.com>
2025-09-22 14:13:38 +0200

    minio has 2 redirects for some reason. check for 400 and 301 responses for incorrect regions

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,8 @@
 duckdb_extension_load(httpfs
-    LOAD_TESTS
-    GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 354d3f436a33f80f03a74419e76eb59459e19168
-    INCLUDE_DIR extension/httpfs/include
+        LOAD_TESTS
+        #            GIT_URL https://github.com/duckdb/duckdb-httpfs
+        #            GIT_TAG 354d3f436a33f80f03a74419e76eb59459e19168
+        #            INCLUDE_DIR extension/httpfs/include
+        SOURCE_DIR /Users/tomebergen/git/duckdb-httpfs
+        INCLUDE_DIR /Users/tomebergen/git/duckdb-httpfs/extension/httpfs/include
 )
