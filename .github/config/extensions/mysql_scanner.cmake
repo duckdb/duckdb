90eb648752f7c8b0b7f61d4b4eb8956cc0ff896a
DinosL <konstantinos@duckdblabs.com>
2025-09-25 13:31:31 +0200

    added patch for mysql_scanner

--- .github/config/extensions/mysql_scanner.cmake
+++ .github/config/extensions/mysql_scanner.cmake
@@ -4,5 +4,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-mysql
             GIT_TAG 8a32d4e069438585e80494e296e407653aebfed3
+            APPLY_PATCHES
             )
 endif()
