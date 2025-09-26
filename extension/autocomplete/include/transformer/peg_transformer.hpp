bf8e29b450b472b2e8c203ac2427a156150a4077
dtenwolde <dljtw98@gmail.com>
2025-09-26 12:57:02 +0200

    Initial header files

---
+++ extension/autocomplete/include/transformer/peg_transformer.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include "tokenizer.hpp"
+#include "parse_result.hpp"
+#include "transform_enum_result.hpp"
+#include "transform_result.hpp"
+#include "ast/column_element.hpp"
+#include "ast/create_table_as.hpp"
+#include "ast/extension_repository_info.hpp"
+#include "ast/generic_copy_option.hpp"
+#include "ast/insert_values.hpp"
+#include "ast/join_prefix.hpp"
+#include "ast/join_qualifier.hpp"
+#include "ast/limit_percent_result.hpp"
+#include "ast/on_conflict_expression_target.hpp"
+#include "ast/prepared_parameter.hpp"
+#include "ast/sequence_option.hpp"
+#include "ast/set_info.hpp"
+#include "ast/table_alias.hpp"
+#include "ast/update_set_element.hpp"
+#include "duckdb/function/macro_function.hpp"
+#include "duckdb/parser/expression/case_expression.hpp"
+#include "duckdb/parser/statement/copy_database_statement.hpp"
+#include "duckdb/parser/statement/set_statement.hpp"
+#include "parser/peg_parser.hpp"
+#include "duckdb/storage/arena_allocator.hpp"
+#include "duckdb/parser/statement/drop_statement.hpp"
+
+namespace duckdb {
+
+// Forward declare
+struct QualifiedName;
+struct MatcherToken;
+
+struct PEGTransformerState {
+	explicit PEGTransformerState(const vector<MatcherToken> &tokens_p) : tokens(tokens_p), token_index(0) {
+	}
+
+	const vector<MatcherToken> &tokens;
+	idx_t token_index;
+};
+
+class PEGTransformer {
+public:
+	using AnyTransformFunction =
+	    std::function<unique_ptr<TransformResultValue>(PEGTransformer &, optional_ptr<ParseResult>)>;
+
+	PEGTransformer(ArenaAllocator &allocator, PEGTransformerState &state,
+	               const case_insensitive_map_t<AnyTransformFunction> &transform_functions,
+	               const case_insensitive_map_t<PEGRule> &grammar_rules,
+	               const case_insensitive_map_t<unique_ptr<TransformEnumValue>> &enum_mappings)
+	    : allocator(allocator), state(state), grammar_rules(grammar_rules), transform_functions(transform_functions),
+	      enum_mappings(enum_mappings) {
+	}
+
+public:
+	template <typename T>
+	T Transform(optional_ptr<ParseResult> parse_result);
+
+	template <typename T>
+	T TransformEnum(optional_ptr<ParseResult> parse_result);
+
+	template <typename T>
+	void TransformOptional(ListParseResult &list_pr, idx_t child_idx, T &target);
+
+	// Make overloads return raw pointers, as ownership is handled by the ArenaAllocator.
+	template <class T, typename... Args>
+	T *Make(Args &&...args) {
+		return allocator.Make<T>(std::forward<Args>(args)...);
+	}
+
+	void ClearParameters();
+	static void ParamTypeCheck(PreparedParamType last_type, PreparedParamType new_type);
+	void SetParam(const string &name, idx_t index, PreparedParamType type);
+	bool GetParam(const string &name, idx_t &index, PreparedParamType type);
+
+public:
+	ArenaAllocator &allocator;
+	PEGTransformerState &state;
+	const case_insensitive_map_t<PEGRule> &grammar_rules;
+	const case_insensitive_map_t<AnyTransformFunction> &transform_functions;
+	const case_insensitive_map_t<unique_ptr<TransformEnumValue>> &enum_mappings;
+	case_insensitive_map_t<idx_t> named_parameter_map;
+	idx_t prepared_statement_parameter_index = 0;
+	PreparedParamType last_param_type = PreparedParamType::INVALID;
+
+};
+
+class PEGTransformerFactory {
+public:
+	explicit PEGTransformerFactory();
+	unique_ptr<SQLStatement> Transform(vector<MatcherToken> &tokens, const char *root_rule = "Statement");
+
+private:
+	template <typename T>
+	void RegisterEnum(const string &rule_name, T value) {
+		auto existing_rule = enum_mappings.find(rule_name);
+		if (existing_rule != enum_mappings.end()) {
+			throw InternalException("EnumRule %s already exists", rule_name);
+		}
+		enum_mappings[rule_name] = make_uniq<TypedTransformEnumResult<T>>(value);
+	}
+
+	template <class FUNC>
+	void Register(const string &rule_name, FUNC function) {
+		auto existing_rule = sql_transform_functions.find(rule_name);
+		if (existing_rule != sql_transform_functions.end()) {
+			throw InternalException("Rule %s already exists", rule_name);
+		}
+		sql_transform_functions[rule_name] =
+		    [function](PEGTransformer &transformer,
+		               optional_ptr<ParseResult> parse_result) -> unique_ptr<TransformResultValue> {
+			auto result_value = function(transformer, parse_result);
+			return make_uniq<TypedTransformResult<decltype(result_value)>>(std::move(result_value));
+		};
+	}
+
+	// Register for functions taking one ParseResult child
+	template <class RETURN_TYPE, class A, idx_t INDEX_A, class FUNC>
+	void Register(const string &rule_name, FUNC function) {
+		sql_transform_functions[rule_name] =
+		    [function](PEGTransformer &transformer,
+		               optional_ptr<ParseResult> parse_result) -> unique_ptr<TransformResultValue> {
+			auto &list_pr = parse_result->Cast<ListParseResult>();
+			auto &child_a = list_pr.Child<A>(INDEX_A);
+			RETURN_TYPE result_value = function(transformer, child_a);
+			return make_uniq<TypedTransformResult<RETURN_TYPE>>(std::move(result_value));
+		};
+	}
+
+	static unique_ptr<SQLStatement> TransformStatement(PEGTransformer &, optional_ptr<ParseResult> list);
+
+private:
+	PEGParser parser;
+	case_insensitive_map_t<PEGTransformer::AnyTransformFunction> sql_transform_functions;
+	case_insensitive_map_t<unique_ptr<TransformEnumValue>> enum_mappings;
+};
+
+
+} // namespace duckdb
