9328720fe54cccc12ddd6d2a42e17c193991e67f
Mytherin <mark.raasveldt@gmail.com>
2025-09-29 16:44:40 +0200

    Only keep looping in ATTACH if we are in the process of attaching a database

--- src/include/duckdb/main/database_file_path_manager.hpp
+++ src/include/duckdb/main/database_file_path_manager.hpp
@@ -19,6 +19,14 @@ struct AttachOptions;
 
 enum class InsertDatabasePathResult { SUCCESS, ALREADY_EXISTS };
 
+struct DatabasePathInfo {
+	explicit DatabasePathInfo(string name_p) : name(std::move(name_p)), is_attached(true) {
+	}
+
+	string name;
+	bool is_attached;
+};
+
 //! The DatabaseFilePathManager is used to ensure we only ever open a single database file once
 class DatabaseFilePathManager {
 public:
@@ -26,6 +34,8 @@ public:
 	InsertDatabasePathResult InsertDatabasePath(const string &path, const string &name, OnCreateConflict on_conflict,
 	                                            AttachOptions &options);
 	void EraseDatabasePath(const string &path);
+	//! Called when a database is detached, but before it is fully finished being used
+	void DetachDatabase(const string &path);
 
 private:
 	//! The lock to add entries to the database path map
@@ -33,7 +43,7 @@ private:
 	//! A set containing all attached database path
 	//! This allows to attach many databases efficiently, and to avoid attaching the
 	//! same file path twice
-	case_insensitive_map_t<string> db_paths_to_name;
+	case_insensitive_map_t<DatabasePathInfo> db_paths_to_name;
 };
 
 } // namespace duckdb
--- src/main/database_file_path_manager.cpp
+++ src/main/database_file_path_manager.cpp
@@ -18,14 +18,20 @@ InsertDatabasePathResult DatabaseFilePathManager::InsertDatabasePath(const strin
 	}
 
 	lock_guard<mutex> path_lock(db_paths_lock);
-	auto entry = db_paths_to_name.emplace(path, name);
+	auto entry = db_paths_to_name.emplace(path, DatabasePathInfo(name));
 	if (!entry.second) {
-		if (on_conflict == OnCreateConflict::IGNORE_ON_CONFLICT && entry.first->second == name) {
-			return InsertDatabasePathResult::ALREADY_EXISTS;
+		auto &existing = entry.first->second;
+		if (on_conflict == OnCreateConflict::IGNORE_ON_CONFLICT && existing.name == name) {
+			if (existing.is_attached) {
+				return InsertDatabasePathResult::ALREADY_EXISTS;
+			}
+			throw BinderException("Unique file handle conflict: Cannot attach \"%s\" - the database file \"%s\" is in "
+			                      "the process of being detached",
+			                      name, path);
 		}
 		throw BinderException("Unique file handle conflict: Cannot attach \"%s\" - the database file \"%s\" is already "
 		                      "attached by database \"%s\"",
-		                      name, path, entry.first->second);
+		                      name, path, existing.name);
 	}
 	options.stored_database_path = make_uniq<StoredDatabasePath>(*this, path, name);
 	return InsertDatabasePathResult::SUCCESS;
@@ -37,6 +43,17 @@ void DatabaseFilePathManager::EraseDatabasePath(const string &path) {
 	}
 	lock_guard<mutex> path_lock(db_paths_lock);
 	db_paths_to_name.erase(path);
+}
+
+void DatabaseFilePathManager::DetachDatabase(const string &path) {
+	if (path.empty() || path == IN_MEMORY_PATH) {
+		return;
+	}
+	lock_guard<mutex> path_lock(db_paths_lock);
+	auto entry = db_paths_to_name.find(path);
+	if (entry != db_paths_to_name.end()) {
+		entry->second.is_attached = false;
+	}
 }
 
 } // namespace duckdb
