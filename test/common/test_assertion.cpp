92366ea9f52748f190b53a326a988ea3b9580d93
dentiny <dentinyhao@gmail.com>
2025-09-30 21:42:58 +0000

    Provide force assertion macros

---
+++ test/common/test_assertion.cpp
@@ -0,0 +1,10 @@
+#include "catch.hpp"
+#include "duckdb/common/assert.hpp"
+
+TEST_CASE("Assertion passes", "[assertion]") {
+    DUCKDB_ASSERT(true);
+}
+
+TEST_CASE("Assertion fails", "[assertion]") {
+    REQUIRE_THROWS(DUCKDB_ASSERT(false));
+}
