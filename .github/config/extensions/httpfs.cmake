047cf5c44f46488e460d29b63fbc12091c2c21cc
Tmonster <tom@ebergen.com>
2025-09-30 12:55:13 +0200

    fix extension config for httpfs

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -2,6 +2,6 @@ duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG 0518838dae609ab8e8ae66960ce982b839754075
-    INCLUDE_DIR extension/httpfs/include
+    INCLUDE_DIR src/include
     APPLY_PATCHES
 )
