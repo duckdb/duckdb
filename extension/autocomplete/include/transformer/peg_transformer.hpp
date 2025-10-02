a3f9be32269b2448e26bf4bb969b52de379f4a9b
dtenwolde <dljtw98@gmail.com>
2025-10-02 14:46:50 +0200

    Move implementation of template methods to the header files

--- extension/autocomplete/include/transformer/peg_transformer.hpp
+++ extension/autocomplete/include/transformer/peg_transformer.hpp
@@ -50,13 +50,50 @@ public:
 
 public:
 	template <typename T>
-	T Transform(optional_ptr<ParseResult> parse_result);
+	T Transform(optional_ptr<ParseResult> parse_result) {
+		auto it = transform_functions.find(parse_result->name);
+		if (it == transform_functions.end()) {
+			throw NotImplementedException("No transformer function found for rule '%s'", parse_result->name);
+		}
+		auto &func = it->second;
+
+		unique_ptr<TransformResultValue> base_result = func(*this, parse_result);
+		if (!base_result) {
+			throw InternalException("Transformer for rule '%s' returned a nullptr.", parse_result->name);
+		}
+
+		auto *typed_result_ptr = dynamic_cast<TypedTransformResult<T> *>(base_result.get());
+		if (!typed_result_ptr) {
+			throw InternalException("Transformer for rule '" + parse_result->name + "' returned an unexpected type.");
+		}
+
+		return std::move(typed_result_ptr->value);
+	}
 
 	template <typename T>
-	T TransformEnum(optional_ptr<ParseResult> parse_result);
+	T TransformEnum(optional_ptr<ParseResult> parse_result) {
+		auto enum_rule_name = parse_result->name;
+
+		auto rule_value = enum_mappings.find(enum_rule_name);
+		if (rule_value == enum_mappings.end()) {
+			throw ParserException("Enum transform failed: could not find mapping for '%s'", enum_rule_name);
+		}
+
+		auto *typed_enum_ptr = dynamic_cast<TypedTransformEnumResult<T> *>(rule_value->second.get());
+		if (!typed_enum_ptr) {
+			throw InternalException("Enum mapping for rule '%s' has an unexpected type.", enum_rule_name);
+		}
+
+		return typed_enum_ptr->value;
+	}
 
 	template <typename T>
-	void TransformOptional(ListParseResult &list_pr, idx_t child_idx, T &target);
+	void TransformOptional(ListParseResult &list_pr, idx_t child_idx, T &target) {
+		auto &opt = list_pr.Child<OptionalParseResult>(child_idx);
+		if (opt.HasResult()) {
+			target = Transform<T>(opt.optional_result);
+		}
+	}
 
 	// Make overloads return raw pointers, as ownership is handled by the ArenaAllocator.
 	template <class T, typename... Args>
--- extension/autocomplete/transformer/peg_transformer.cpp
+++ extension/autocomplete/transformer/peg_transformer.cpp
@@ -1,58 +1,9 @@
 #include "transformer/peg_transformer.hpp"
 
 #include "duckdb/parser/statement/set_statement.hpp"
-#include "duckdb/parser/expression/constant_expression.hpp"
-#include "duckdb/common/exception/binder_exception.hpp"
 #include "duckdb/common/string_util.hpp"
-#include "duckdb/common/printer.hpp"
 
 namespace duckdb {
-
-template <typename T>
-T PEGTransformer::Transform(optional_ptr<ParseResult> parse_result) {
-	auto it = transform_functions.find(parse_result->name);
-	if (it == transform_functions.end()) {
-		throw NotImplementedException("No transformer function found for rule '%s'", parse_result->name);
-	}
-	auto &func = it->second;
-
-	unique_ptr<TransformResultValue> base_result = func(*this, parse_result);
-	if (!base_result) {
-		throw InternalException("Transformer for rule '%s' returned a nullptr.", parse_result->name);
-	}
-
-	auto *typed_result_ptr = dynamic_cast<TypedTransformResult<T> *>(base_result.get());
-	if (!typed_result_ptr) {
-		throw InternalException("Transformer for rule '" + parse_result->name + "' returned an unexpected type.");
-	}
-
-	return std::move(typed_result_ptr->value);
-}
-
-template <typename T>
-T PEGTransformer::TransformEnum(optional_ptr<ParseResult> parse_result) {
-	auto enum_rule_name = parse_result->name;
-
-	auto rule_value = enum_mappings.find(enum_rule_name);
-	if (rule_value == enum_mappings.end()) {
-		throw ParserException("Enum transform failed: could not find mapping for '%s'", enum_rule_name);
-	}
-
-	auto *typed_enum_ptr = dynamic_cast<TypedTransformEnumResult<T> *>(rule_value->second.get());
-	if (!typed_enum_ptr) {
-		throw InternalException("Enum mapping for rule '%s' has an unexpected type.", enum_rule_name);
-	}
-
-	return typed_enum_ptr->value;
-}
-
-template <typename T>
-void PEGTransformer::TransformOptional(ListParseResult &list_pr, idx_t child_idx, T &target) {
-	auto &opt = list_pr.Child<OptionalParseResult>(child_idx);
-	if (opt.HasResult()) {
-		target = Transform<T>(opt.optional_result);
-	}
-}
 
 void PEGTransformer::ParamTypeCheck(PreparedParamType last_type, PreparedParamType new_type) {
 	// Mixing positional/auto-increment and named parameters is not supported
