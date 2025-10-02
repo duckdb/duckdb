6b3bae0775beb06c6aba7c0b759b5165e2850c01
Maia <maia@duckdblabs.com>
2025-10-02 19:00:17 +0200

    patch avro

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -3,5 +3,6 @@ if (NOT MINGW)
             LOAD_TESTS DONT_LINK
             GIT_URL https://github.com/duckdb/duckdb-avro
             GIT_TAG 0c97a61781f63f8c5444cf3e0c6881ecbaa9fe13
+            APPLY_PATCHES
     )
 endif()
