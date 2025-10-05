29ef3afb2ada4df42832e3030a93af3367ce3960
Shivam <shivamprasad91@gmail.com>
2025-10-05 14:16:11 -0700

    Fix LENGTH(NULL) with GROUP BY NULL binding issue
    - Set can_contain_nulls=true during GROUP BY expression binding
    - Prevents NULL constants from being converted to INTEGER type
    - Allows LENGTH(NULL) to bind correctly when NULL appears in GROUP BY
    - Add comprehensive test coverage for the fix
    
    Fixes issue where LENGTH(NULL) would fail with 'No function matches
    the given name and argument types length(INTEGER)' when used with
    GROUP BY NULL due to NULL type coercion during binding.

--- src/planner/binder/query_node/bind_select_node.cpp
+++ src/planner/binder/query_node/bind_select_node.cpp
@@ -466,6 +466,9 @@ unique_ptr<BoundQueryNode> Binder::BindSelectNode(SelectNode &statement, unique_
 		// the statement has a GROUP BY clause, bind it
 		unbound_groups.resize(group_expressions.size());
 		GroupBinder group_binder(*this, context, statement, result->group_index, bind_state, info.alias_map);
+		// Allow NULL constants in GROUP BY to maintain their SQLNULL type
+		auto prev_can_contain_nulls = this->can_contain_nulls;
+		this->can_contain_nulls = true;
 		for (idx_t i = 0; i < group_expressions.size(); i++) {
 
 			// we keep a copy of the unbound expression;
@@ -511,6 +514,7 @@ unique_ptr<BoundQueryNode> Binder::BindSelectNode(SelectNode &statement, unique_
 			ExpressionBinder::QualifyColumnNames(*this, unbound_groups[i]);
 			info.map[*unbound_groups[i]] = i;
 		}
+		this->can_contain_nulls = prev_can_contain_nulls;
 	}
 	result->groups.grouping_sets = std::move(statement.groups.grouping_sets);
 
