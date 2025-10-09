a44547d164111d0409446fe724221df2641956c0
Artjom Plaunov <artyemnyc@gmail.com>
2025-10-09 11:18:33 +0200

    Fix scoping for node handlers

--- src/execution/index/art/base_leaf.cpp
+++ src/execution/index/art/base_leaf.cpp
@@ -55,27 +55,36 @@ NodeHandle<BaseLeaf<CAPACITY, TYPE>> BaseLeaf<CAPACITY, TYPE>::DeleteByteInterna
 //===--------------------------------------------------------------------===//
 
 void Node7Leaf::InsertByte(ART &art, Node &node, const uint8_t byte) {
-	// The node is full. Grow to Node15.
-	NodeHandle<Node7Leaf> handle(art, node);
-	auto &n7 = handle.Get();
-	if (n7.count == CAPACITY) {
-		auto node7 = node;
-		Node15Leaf::GrowNode7Leaf(art, node, node7);
-		Node15Leaf::InsertByte(art, node, byte);
-		return;
+	{
+		NodeHandle<Node7Leaf> handle(art, node);
+		auto &n7 = handle.Get();
+
+		if (n7.count != CAPACITY) {
+			InsertByteInternal(n7, byte);
+			return;
+		}
 	}
-	InsertByteInternal(n7, byte);
+	// The node is full. Grow to Node15.
+	auto node7 = node;
+	Node15Leaf::GrowNode7Leaf(art, node, node7);
+	Node15Leaf::InsertByte(art, node, byte);
 }
 
 void Node7Leaf::DeleteByte(ART &art, Node &node, Node &prefix, const uint8_t byte, const ARTKey &row_id) {
-	auto n7_handle = DeleteByteInternal(art, node, byte);
-	auto &n7 = n7_handle.Get();
-	// Compress one-way nodes.
-	if (n7.count == 1) {
+	idx_t remainder;
+	{
+		auto n7_handle = DeleteByteInternal(art, node, byte);
+		auto &n7 = n7_handle.Get();
+
+		if (n7.count != 1) {
+			return;
+		}
+
+		// Compress one-way nodes.
 		D_ASSERT(node.GetGateStatus() == GateStatus::GATE_NOT_SET);
 
 		// Get the remaining row ID.
-		auto remainder = UnsafeNumericCast<idx_t>(row_id.GetRowId()) & AND_LAST_BYTE;
+		remainder = UnsafeNumericCast<idx_t>(row_id.GetRowId()) & AND_LAST_BYTE;
 		remainder |= UnsafeNumericCast<idx_t>(n7.key[0]);
 
 		// Free the prefix (nodes) and inline the remainder.
@@ -84,27 +93,27 @@ void Node7Leaf::DeleteByte(ART &art, Node &node, Node &prefix, const uint8_t byt
 			Leaf::New(prefix, UnsafeNumericCast<row_t>(remainder));
 			return;
 		}
-
-		// Free the Node7Leaf and inline the remainder.
-		Node::FreeNode(art, node);
-		Leaf::New(node, UnsafeNumericCast<row_t>(remainder));
 	}
+	// Free the Node7Leaf and inline the remainder.
+	Node::FreeNode(art, node);
+	Leaf::New(node, UnsafeNumericCast<row_t>(remainder));
 }
 
 void Node7Leaf::ShrinkNode15Leaf(ART &art, Node &node7_leaf, Node &node15_leaf) {
-	auto n7_handle = New(art, node7_leaf);
-	auto &n7 = n7_handle.Get();
+	{
+		auto n7_handle = New(art, node7_leaf);
+		auto &n7 = n7_handle.Get();
 
-	NodeHandle<Node15Leaf> n15_handle(art, node15_leaf);
-	auto &n15 = n15_handle.Get();
+		NodeHandle<Node15Leaf> n15_handle(art, node15_leaf);
+		auto &n15 = n15_handle.Get();
 
-	node7_leaf.SetGateStatus(node15_leaf.GetGateStatus());
+		node7_leaf.SetGateStatus(node15_leaf.GetGateStatus());
 
-	n7.count = n15.count;
-	for (uint8_t i = 0; i < n15.count; i++) {
-		n7.key[i] = n15.key[i];
+		n7.count = n15.count;
+		for (uint8_t i = 0; i < n15.count; i++) {
+			n7.key[i] = n15.key[i];
+		}
 	}
-
 	Node::FreeNode(art, node15_leaf);
 }
 
@@ -113,63 +122,66 @@ void Node7Leaf::ShrinkNode15Leaf(ART &art, Node &node7_leaf, Node &node15_leaf) 
 //===--------------------------------------------------------------------===//
 
 void Node15Leaf::InsertByte(ART &art, Node &node, const uint8_t byte) {
-	// The node is full. Grow to Node256Leaf.
-	NodeHandle<Node15Leaf> n15_handle(art, node);
-	auto &n15 = n15_handle.Get();
-	if (n15.count == CAPACITY) {
-		auto node15 = node;
-		Node256Leaf::GrowNode15Leaf(art, node, node15);
-		Node256Leaf::InsertByte(art, node, byte);
-		return;
+	{
+		NodeHandle<Node15Leaf> n15_handle(art, node);
+		auto &n15 = n15_handle.Get();
+		if (n15.count != CAPACITY) {
+			InsertByteInternal(n15, byte);
+			return;
+		}
 	}
-
-	InsertByteInternal(n15, byte);
+	auto node15 = node;
+	Node256Leaf::GrowNode15Leaf(art, node, node15);
+	Node256Leaf::InsertByte(art, node, byte);
 }
 
 void Node15Leaf::DeleteByte(ART &art, Node &node, const uint8_t byte) {
-	auto n15_handle = DeleteByteInternal(art, node, byte);
-	auto &n15 = n15_handle.Get();
-
-	// Shrink node to Node7.
-	if (n15.count < Node7Leaf::CAPACITY) {
-		auto node15 = node;
-		Node7Leaf::ShrinkNode15Leaf(art, node, node15);
+	{
+		auto n15_handle = DeleteByteInternal(art, node, byte);
+		auto &n15 = n15_handle.Get();
+		if (n15.count >= Node7Leaf::CAPACITY) {
+			return;
+		}
 	}
+	auto node15 = node;
+	Node7Leaf::ShrinkNode15Leaf(art, node, node15);
 }
 
 void Node15Leaf::GrowNode7Leaf(ART &art, Node &node15_leaf, Node &node7_leaf) {
-	NodeHandle<Node7Leaf> n7_handle(art, node7_leaf);
-	auto &n7 = n7_handle.Get();
+	{
+		NodeHandle<Node7Leaf> n7_handle(art, node7_leaf);
+		auto &n7 = n7_handle.Get();
 
-	auto n15_handle = New(art, node15_leaf);
-	auto &n15 = n15_handle.Get();
-	node15_leaf.SetGateStatus(node7_leaf.GetGateStatus());
+		auto n15_handle = New(art, node15_leaf);
+		auto &n15 = n15_handle.Get();
+		node15_leaf.SetGateStatus(node7_leaf.GetGateStatus());
 
-	n15.count = n7.count;
-	for (uint8_t i = 0; i < n7.count; i++) {
-		n15.key[i] = n7.key[i];
+		n15.count = n7.count;
+		for (uint8_t i = 0; i < n7.count; i++) {
+			n15.key[i] = n7.key[i];
+		}
 	}
-
 	Node::FreeNode(art, node7_leaf);
 }
 
 void Node15Leaf::ShrinkNode256Leaf(ART &art, Node &node15_leaf, Node &node256_leaf) {
-	auto n15_handle = New(art, node15_leaf);
-	auto &n15 = n15_handle.Get();
+	{
+		auto n15_handle = New(art, node15_leaf);
+		auto &n15 = n15_handle.Get();
 
-	NodeHandle<Node256Leaf> n256_handle(art, node256_leaf);
-	auto &n256 = n256_handle.Get();
+		NodeHandle<Node256Leaf> n256_handle(art, node256_leaf);
+		auto &n256 = n256_handle.Get();
 
-	node15_leaf.SetGateStatus(node256_leaf.GetGateStatus());
+		node15_leaf.SetGateStatus(node256_leaf.GetGateStatus());
 
-	ValidityMask mask(&n256.mask[0], Node256::CAPACITY);
-	for (uint16_t i = 0; i < Node256::CAPACITY; i++) {
-		if (mask.RowIsValid(i)) {
-			n15.key[n15.count] = UnsafeNumericCast<uint8_t>(i);
-			n15.count++;
+		ValidityMask mask(&n256.mask[0], Node256::CAPACITY);
+		for (uint16_t i = 0; i < Node256::CAPACITY; i++) {
+			if (mask.RowIsValid(i)) {
+				n15.key[n15.count] = UnsafeNumericCast<uint8_t>(i);
+				n15.count++;
+			}
 		}
 	}
-
 	Node::FreeNode(art, node256_leaf);
 }
 
