1a77d4b75d795f7ac9c49f3219b7885f00a2ff04
Tishj <t_b@live.nl>
2025-10-09 13:17:19 +0200

    implement comparison logic for Variants

--- src/common/vector_operations/is_distinct_from.cpp
+++ src/common/vector_operations/is_distinct_from.cpp
@@ -1,6 +1,8 @@
 #include "duckdb/common/uhugeint.hpp"
 #include "duckdb/common/vector_operations/vector_operations.hpp"
 #include "duckdb/common/operator/comparison_operators.hpp"
+#include "duckdb/common/types/variant.hpp"
+#include "duckdb/function/scalar/variant_utils.hpp"
 
 namespace duckdb {
 
@@ -289,6 +291,7 @@ template <class LEFT_TYPE, class RIGHT_TYPE, class OP>
 idx_t DistinctSelect(Vector &left, Vector &right, const SelectionVector *sel, idx_t count, SelectionVector *true_sel,
                      SelectionVector *false_sel, optional_ptr<ValidityMask> null_mask) {
 	if (!sel) {
+		D_ASSERT(count <= STANDARD_VECTOR_SIZE);
 		sel = FlatVector::IncrementalSelectionVector();
 	}
 
@@ -487,7 +490,7 @@ void ExtractNestedMask(const SelectionVector &slice_sel, const idx_t count, cons
 	for (idx_t i = 0; i < count; ++i) {
 		const auto slice_idx = slice_sel.get_index(i);
 		const auto result_idx = sel.get_index(slice_idx);
-		if (child_mask && !child_mask->RowIsValid(slice_idx)) {
+		if (!child_mask->RowIsValid(slice_idx)) {
 			null_mask->SetInvalid(result_idx);
 		}
 	}
@@ -518,10 +521,7 @@ idx_t DistinctSelectStruct(Vector &left, Vector &right, idx_t count, const Selec
 	// In order to reuse the comparators, we have to track what passed and failed internally.
 	// To do that, we need local SVs that we then merge back into the real ones after every pass.
 	const auto vcount = count;
-	SelectionVector slice_sel(count);
-	for (idx_t i = 0; i < count; ++i) {
-		slice_sel.set_index(i, i);
-	}
+	SelectionVector slice_sel(0, count);
 
 	SelectionVector true_sel(count);
 	SelectionVector false_sel(count);
@@ -882,6 +882,39 @@ idx_t DistinctSelectArray(Vector &left, Vector &right, idx_t count, const Select
 }
 
 template <class OP>
+static idx_t DistinctSelectVariant(Vector &left, Vector &right, idx_t count, const SelectionVector &sel,
+                                   OptionalSelection &true_opt, OptionalSelection &false_opt,
+                                   optional_ptr<ValidityMask> null_mask) {
+	if (count == 0) {
+		return count;
+	}
+
+	D_ASSERT(left.GetType().id() == LogicalTypeId::VARIANT && right.GetType().id() == left.GetType().id());
+	RecursiveUnifiedVectorFormat left_variant_format, right_variant_format;
+	Vector::RecursiveToUnifiedFormat(left, count, left_variant_format);
+	Vector::RecursiveToUnifiedFormat(right, count, right_variant_format);
+
+	UnifiedVariantVectorData left_variant(left_variant_format);
+	UnifiedVariantVectorData right_variant(right_variant_format);
+	idx_t true_count = 0, false_count = 0;
+	for (idx_t i = 0; i < count; i++) {
+		auto left_val = VariantUtils::ConvertVariantToValue(left_variant, i, 0);
+		auto right_val = VariantUtils::ConvertVariantToValue(right_variant, i, 0);
+
+		idx_t result_idx = sel.get_index(i);
+		auto res = OP::ValueOperation(left_val, right_val);
+
+		if (!res) {
+			false_opt.Append(false_count, result_idx);
+		} else {
+			true_opt.Append(true_count, result_idx);
+		}
+	}
+
+	return true_count;
+}
+
+template <class OP>
 idx_t DistinctSelectNested(Vector &left, Vector &right, optional_ptr<const SelectionVector> sel, const idx_t count,
                            optional_ptr<SelectionVector> true_sel, optional_ptr<SelectionVector> false_sel,
                            optional_ptr<ValidityMask> null_mask) {
@@ -890,6 +923,7 @@ idx_t DistinctSelectNested(Vector &left, Vector &right, optional_ptr<const Selec
 	// we have to make multiple passes, so we need to keep track of the original input positions
 	// and then scatter the output selections when we are done.
 	if (!sel) {
+		D_ASSERT(count <= STANDARD_VECTOR_SIZE);
 		sel = FlatVector::IncrementalSelectionVector();
 	}
 
@@ -911,14 +945,20 @@ idx_t DistinctSelectNested(Vector &left, Vector &right, optional_ptr<const Selec
 	auto unknown = DistinctSelectNotNull<OP>(l_not_null, r_not_null, count, match_count, *sel, maybe_vec, true_opt,
 	                                         false_opt, null_mask);
 
-	switch (left.GetType().InternalType()) {
+	auto &left_type = left.GetType();
+	switch (left_type.InternalType()) {
 	case PhysicalType::LIST:
 		match_count +=
 		    DistinctSelectList<OP>(l_not_null, r_not_null, unknown, maybe_vec, true_opt, false_opt, null_mask);
 		break;
 	case PhysicalType::STRUCT:
-		match_count +=
-		    DistinctSelectStruct<OP>(l_not_null, r_not_null, unknown, maybe_vec, true_opt, false_opt, null_mask);
+		if (left_type.id() == LogicalTypeId::VARIANT) {
+			match_count +=
+			    DistinctSelectVariant<OP>(l_not_null, r_not_null, unknown, maybe_vec, true_opt, false_opt, null_mask);
+		} else {
+			match_count +=
+			    DistinctSelectStruct<OP>(l_not_null, r_not_null, unknown, maybe_vec, true_opt, false_opt, null_mask);
+		}
 		break;
 	case PhysicalType::ARRAY:
 		match_count +=
