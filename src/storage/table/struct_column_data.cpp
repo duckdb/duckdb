6cf41510a74e36067ea907b96dde58b969cfc301
Tishj <t_b@live.nl>
2025-10-14 10:32:03 +0200

    remove limitations to start

--- src/storage/table/struct_column_data.cpp
+++ src/storage/table/struct_column_data.cpp
@@ -19,9 +19,7 @@ StructColumnData::StructColumnData(BlockManager &block_manager, DataTableInfo &i
 	if (type.id() != LogicalTypeId::UNION && StructType::IsUnnamed(type)) {
 		throw InvalidInputException("A table cannot be created from an unnamed struct");
 	}
-	if (type.id() == LogicalTypeId::VARIANT) {
-		throw NotImplementedException("A table cannot be created from a VARIANT column yet");
-	}
+
 	// the sub column index, starting at 1 (0 is the validity mask)
 	idx_t sub_column_index = 1;
 	for (auto &child_type : child_types) {
