99bac57a77d0c2b9488f81179b249a84fa03d46c
Mytherin <mark.raasveldt@gmail.com>
2025-10-27 16:12:44 +0100

    Rework OutputQuotedEscapedString to emit concat instead

--- tools/shell/shell.cpp
+++ tools/shell/shell.cpp
@@ -986,66 +986,55 @@ void ShellState::OutputQuotedString(const char *z) {
 ** escape mechanism.
 */
 void ShellState::OutputQuotedEscapedString(const char *z) {
-	int i;
-	char c;
-	setBinaryMode(out, 1);
-	for (i = 0; (c = z[i]) != 0 && c != '\'' && c != '\n' && c != '\r'; i++) {
+	bool needs_quoting = false;
+	bool needs_concat = false;
+	for (idx_t i = 0; z[i]; i++) {
+		if (z[i] == '\n' || z[i] == '\r') {
+			needs_quoting = true;
+			needs_concat = true;
+			break;
+		}
+		if (z[i] == '\'') {
+			needs_quoting = true;
+		}
 	}
-	if (c == 0) {
+	if (!needs_quoting) {
 		utf8_printf(out, "'%s'", z);
+		return;
+	}
+	string res;
+	if (needs_concat) {
+		res = "concat('";
 	} else {
-		const char *zNL = 0;
-		const char *zCR = 0;
-		int nNL = 0;
-		int nCR = 0;
-		char zBuf1[20], zBuf2[20];
-		for (i = 0; z[i]; i++) {
-			if (z[i] == '\n')
-				nNL++;
-			if (z[i] == '\r')
-				nCR++;
-		}
-		if (nNL) {
-			raw_printf(out, "replace(");
-			zNL = unused_string(z, "\\n", "\\012", zBuf1);
-		}
-		if (nCR) {
-			raw_printf(out, "replace(");
-			zCR = unused_string(z, "\\r", "\\015", zBuf2);
-		}
-		raw_printf(out, "'");
-		while (*z) {
-			for (i = 0; (c = z[i]) != 0 && c != '\n' && c != '\r' && c != '\''; i++) {
+		res = "'";
+	}
+	for (idx_t i = 0; z[i]; i++) {
+		switch (z[i]) {
+		case '\n':
+		case '\r':
+			// newline - finish the current string literal and write the newline with a chr function
+			res += "', chr(";
+			if (z[i] == '\n') {
+				res += "10";
+			} else {
+				res += "13";
 			}
-			if (c == '\'')
-				i++;
-			if (i) {
-				utf8_printf(out, "%.*s", i, z);
-				z += i;
-			}
-			if (c == '\'') {
-				raw_printf(out, "'");
-				continue;
-			}
-			if (c == 0) {
-				break;
-			}
-			z++;
-			if (c == '\n') {
-				raw_printf(out, "%s", zNL);
-				continue;
-			}
-			raw_printf(out, "%s", zCR);
-		}
-		raw_printf(out, "'");
-		if (nCR) {
-			raw_printf(out, ",'%s',char(13))", zCR);
-		}
-		if (nNL) {
-			raw_printf(out, ",'%s',char(10))", zNL);
+			res += "), '";
+			break;
+		case '\'':
+			// escape the quote
+			res += "''";
+			break;
+		default:
+			res += z[i];
+			break;
 		}
 	}
-	setTextMode(out, 1);
+	res += "'";
+	if (needs_concat) {
+		res += ")";
+	}
+	utf8_printf(out, "%s", res.c_str());
 }
 
 /*
