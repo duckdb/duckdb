295603915b0ab3a1532cbbe6cf9547f9803e3c46
Sam Ansmink <samansmink@hotmail.com>
2025-10-28 10:58:22 +0100

    bump extensions

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -8,6 +8,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(iceberg
 #            ${LOAD_ICEBERG_TESTS} TODO: re-enable once autoloading test is fixed
             GIT_URL https://github.com/duckdb/duckdb-iceberg
-            GIT_TAG 4f3c5499e5feec9fe17a69a8ca74d81aaf472fd2
+            GIT_TAG 30a2c66f1040ffd36e117ace6e22acbdb402e79c
             )
 endif()
