67ec072c0ea6a237213f680709773e1342b11065
Sam Ansmink <samansmink@hotmail.com>
2025-11-04 15:59:04 +0100

    fix: tests

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -3,4 +3,5 @@ duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG 8356a9017444f54018159718c8017ff7db4ea756
     INCLUDE_DIR src/include
+    APPLY_PATCHES
 )
