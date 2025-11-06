d39629fe5a53311a3472e10f7d06a66666661adb
Tishj <t_b@live.nl>
2025-11-06 10:05:51 +0100

    forward declare the overloads of ValueConverter

--- src/include/duckdb/function/variant/variant_value_convert.hpp
+++ src/include/duckdb/function/variant/variant_value_convert.hpp
@@ -116,4 +116,25 @@ struct ValueConverter {
 	}
 };
 
+template <>
+Value ValueConverter::VisitInteger<int8_t>(int8_t val);
+template <>
+Value ValueConverter::VisitInteger<int16_t>(int16_t val);
+template <>
+Value ValueConverter::VisitInteger<int32_t>(int32_t val);
+template <>
+Value ValueConverter::VisitInteger<int64_t>(int64_t val);
+template <>
+Value ValueConverter::VisitInteger<hugeint_t>(hugeint_t val);
+template <>
+Value ValueConverter::VisitInteger<uint8_t>(uint8_t val);
+template <>
+Value ValueConverter::VisitInteger<uint16_t>(uint16_t val);
+template <>
+Value ValueConverter::VisitInteger<uint32_t>(uint32_t val);
+template <>
+Value ValueConverter::VisitInteger<uint64_t>(uint64_t val);
+template <>
+Value ValueConverter::VisitInteger<uhugeint_t>(uhugeint_t val);
+
 } // namespace duckdb
