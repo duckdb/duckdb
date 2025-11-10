4a0f4b0b38b9d5660c8a5c848d8a1c71bc3220de
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-11-10 11:07:58 +0100

    Bump httpfs and resume testing on Windows

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,12 +1,6 @@
-IF (NOT WIN32)
-    set(LOAD_HTTPFS_TESTS "LOAD_TESTS")
-else ()
-    set(LOAD_HTTPFS_TESTS "")
-endif()
 duckdb_extension_load(httpfs
-    # TODO: restore once httpfs is fixed
-    ${LOAD_HTTPFS_TESTS}
+    LOAD TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG b80c680f86dff6061614536d908d4b08c85c9ef4
+    GIT_TAG 041a782b0b33495448a7eaa68973cf8c2174feb6
     INCLUDE_DIR src/include
 )
