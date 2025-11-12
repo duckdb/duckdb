6956da0c3cc799e3ec4ee1df8a0fe3977201a803
Julian Meyers <Julian@MeyersWorld.com>
2025-11-12 16:41:06 -0600

    Upgrade ducklake version

--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -1,6 +1,5 @@
 duckdb_extension_load(ducklake
     DONT_LINK
     GIT_URL https://github.com/duckdb/ducklake
-    GIT_TAG 2554312f71916ba11530c838b5c8c65b4786825c
-    APPLY_PATCHES
+    GIT_TAG 2328fbeac1bb2bca247bc3994f4ed31fe3a49993
 )
