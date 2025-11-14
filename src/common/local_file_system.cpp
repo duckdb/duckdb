3c6329c4ec9949b7b296dc29968ff6f3dd8f8bad
dentiny <dentinyhao@gmail.com>
2025-11-14 11:57:41 +0000

    doc

--- src/common/local_file_system.cpp
+++ src/common/local_file_system.cpp
@@ -996,7 +996,7 @@ unique_ptr<FileHandle> LocalFileSystem::OpenFile(const string &path_p, FileOpenF
 	DWORD desired_access;
 	// For windows platform, by default deletion fails when the file is accessed by other thread/process.
 	// To keep deletion behavior compatible with unix platform, which physically deletes a file when reference count
-	// drops to 0, open files with [`FILE_SHARE_DELETE`].
+	// drops to 0 without interfering with already opened file handles, open files with [`FILE_SHARE_DELETE`].
 	DWORD share_mode = FILE_SHARE_DELETE;
 	DWORD creation_disposition = OPEN_EXISTING;
 	DWORD flags_and_attributes = FILE_ATTRIBUTE_NORMAL;
