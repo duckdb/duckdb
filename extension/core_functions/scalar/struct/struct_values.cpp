6deebf6a9e4a1b960025887a414c30fe28f2f521
Etgar Shmueli <etgarsh@monday.com>
2025-12-02 13:12:15 +0200

    Support prepared statements with `struct_values` and handle UNKNOWN parameter type

--- extension/core_functions/scalar/struct/struct_values.cpp
+++ extension/core_functions/scalar/struct/struct_values.cpp
@@ -53,9 +53,14 @@ static void StructValuesFunction(DataChunk &args, ExpressionState &state, Vector
 // Ensure input is a STRUCT, set return type to an unnamed STRUCT with same child types
 static unique_ptr<FunctionData> StructValuesBind(ClientContext &context, ScalarFunction &bound_function,
                                                  vector<unique_ptr<Expression>> &arguments) {
+	const auto arg_type = arguments[0]->return_type;
+	if (arg_type == LogicalTypeId::UNKNOWN) {
+		throw ParameterNotResolvedException();
+	}
+
 	// Since the type of the argument we declared of in `GetFunction` doesn't contain the inner STRUCT type,
 	// we should take it from the arguments
-	bound_function.arguments[0] = arguments[0]->return_type;
+	bound_function.arguments[0] = arg_type;
 
 	// Build unnamed children list using only types, with empty names
 	child_list_t<LogicalType> unnamed_children;
