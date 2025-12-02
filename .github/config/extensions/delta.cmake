18246c47b9d10c5883b459196d6a131d6438087d
Maia <maia@duckdblabs.com>
2025-12-02 08:44:08 +0100

    bump extensions

--- .github/config/extensions/delta.cmake
+++ .github/config/extensions/delta.cmake
@@ -1,7 +1,7 @@
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(delta
             GIT_URL https://github.com/duckdb/duckdb-delta
-            GIT_TAG 6515bb2560772956f9b74a18a47782f18ed4d827
+            GIT_TAG 77b0f303b7918f1539947833b1ced1eab8e825fc
             SUBMODULES extension-ci-tools
     )
 endif()
--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(ducklake
     DONT_LINK
     GIT_URL https://github.com/duckdb/ducklake
-    GIT_TAG ce40b303c0dbaf474d2949988bcb93eceb215cc8
+    GIT_TAG a75383de129d9a66bb301aa94cb9647352b2a44e
 )
