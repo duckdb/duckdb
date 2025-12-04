b05d3d59b8345813dc39df9c8d854721c23f5993
Max Gabrielsson <max@gabrielsson.com>
2025-12-04 18:22:41 +0100

    initial attempt at geometry POINT shredding

---
+++ src/include/duckdb/storage/table/geo_column_data.hpp
@@ -0,0 +1,88 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/storage/table/variant_column_data.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "duckdb/storage/table/column_data.hpp"
+#include "duckdb/storage/table/validity_column_data.hpp"
+
+namespace duckdb {
+
+class GeoColumnData : public ColumnData {
+public:
+	GeoColumnData(BlockManager &block_manager, DataTableInfo &info, idx_t column_index, LogicalType type,
+	              ColumnDataType data_type, optional_ptr<ColumnData> parent);
+
+	//! The validity column
+	shared_ptr<ValidityColumnData> validity;
+	//! The BLOB fallback column for mixed geometries
+	shared_ptr<ColumnData> blob_column;
+	//! The "shredded" geometry column
+	shared_ptr<ColumnData> geom_column;
+
+public:
+	idx_t GetMaxEntry() override;
+
+	bool IsShredded() const {
+		return geom_column != nullptr;
+	}
+
+	void InitializePrefetch(PrefetchState &prefetch_state, ColumnScanState &scan_state, idx_t rows) override;
+	void InitializeScan(ColumnScanState &state) override;
+	void InitializeScanWithOffset(ColumnScanState &state, idx_t row_idx) override;
+
+	idx_t Scan(TransactionData transaction, idx_t vector_index, ColumnScanState &state, Vector &result,
+	           idx_t scan_count) override;
+	idx_t ScanCommitted(idx_t vector_index, ColumnScanState &state, Vector &result, bool allow_updates,
+	                    idx_t scan_count) override;
+	idx_t ScanCount(ColumnScanState &state, Vector &result, idx_t count, idx_t result_offset = 0) override;
+
+	void Skip(ColumnScanState &state, idx_t count = STANDARD_VECTOR_SIZE) override;
+
+	void InitializeAppend(ColumnAppendState &state) override;
+	void Append(BaseStatistics &stats, ColumnAppendState &state, Vector &vector, idx_t count) override;
+	void RevertAppend(row_t new_count) override;
+	idx_t Fetch(ColumnScanState &state, row_t row_id, Vector &result) override;
+	void FetchRow(TransactionData transaction, ColumnFetchState &state, row_t row_id, Vector &result,
+	              idx_t result_idx) override;
+	void Update(TransactionData transaction, DataTable &data_table, idx_t column_index, Vector &update_vector,
+	            row_t *row_ids, idx_t update_count, idx_t row_group_start) override;
+	void UpdateColumn(TransactionData transaction, DataTable &data_table, const vector<column_t> &column_path,
+	                  Vector &update_vector, row_t *row_ids, idx_t update_count, idx_t depth,
+	                  idx_t row_group_start) override;
+	unique_ptr<BaseStatistics> GetUpdateStatistics() override;
+
+	void CommitDropColumn() override;
+
+	unique_ptr<ColumnCheckpointState> CreateCheckpointState(const RowGroup &row_group,
+	                                                        PartialBlockManager &partial_block_manager) override;
+	unique_ptr<ColumnCheckpointState> Checkpoint(const RowGroup &row_group, ColumnCheckpointInfo &info) override;
+
+	bool IsPersistent() override;
+	bool HasAnyChanges() const override;
+	PersistentColumnData Serialize() override;
+	void InitializeColumn(PersistentColumnData &column_data, BaseStatistics &target_stats) override;
+
+	void GetColumnSegmentInfo(const QueryContext &context, idx_t row_group_index, vector<idx_t> col_path,
+	                          vector<ColumnSegmentInfo> &result) override;
+
+	void Verify(RowGroup &parent) override;
+
+	void SetValidityData(shared_ptr<ValidityColumnData> validity_p);
+	void SetBlobColumnData(shared_ptr<ColumnData> blob_column_p);
+	void SetGeomColumnData(shared_ptr<ColumnData> geom_column_p);
+
+private:
+	void CreateScanStates(ColumnScanState &state);
+	LogicalType GetShreddedType();
+
+	pair<shared_ptr<ColumnData>, shared_ptr<ColumnData>>
+	WriteShreddedData(const RowGroup &row_group, const LogicalType &shredded_type, BaseStatistics &stats);
+};
+
+} // namespace duckdb
