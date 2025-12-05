11cc16ce361cc32648ef80517edfd45307d58f95
Sam Ansmink <samansmink@hotmail.com>
2025-12-05 08:54:57 +0100

    enable windows for uc

--- .github/config/extensions/unity_catalog.cmake
+++ .github/config/extensions/unity_catalog.cmake
@@ -1,4 +1,4 @@
-if (NOT WIN32 AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
+if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
     duckdb_extension_load(unity_catalog
             GIT_URL https://github.com/duckdb/unity_catalog
             GIT_TAG 1ad4f0b1fb12f661ff58fc91c2c9e7022591ea4b
