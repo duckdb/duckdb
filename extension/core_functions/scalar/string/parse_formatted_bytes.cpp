fe886523752edb660d7900e45825f35198073fdf
Etgar Shmueli <etgarsh@monday.com>
2025-12-10 17:47:34 +0200

    Reformat lambda in `parse_formatted_bytes`

--- extension/core_functions/scalar/string/parse_formatted_bytes.cpp
+++ extension/core_functions/scalar/string/parse_formatted_bytes.cpp
@@ -14,14 +14,15 @@ static void PraseFormattedBytesFunction(DataChunk &args, ExpressionState &state,
 static void TryParseFormattedBytesFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto arg0 = args.data[0];
 
-	UnaryExecutor::ExecuteWithNulls<string_t, idx_t>(arg0, result, args.size(), [&](string_t str, ValidityMask &mask, idx_t index) {
-		try {
-			return StringUtil::ParseFormattedBytes(str.GetString());
-		} catch (const Exception &) {
-			mask.SetInvalid(index);
-			return static_cast<idx_t>(0);
-		}
-	});
+	UnaryExecutor::ExecuteWithNulls<string_t, idx_t>(arg0, result, args.size(),
+	                                                 [&](string_t str, ValidityMask &mask, idx_t index) {
+		                                                 try {
+			                                                 return StringUtil::ParseFormattedBytes(str.GetString());
+		                                                 } catch (const Exception &) {
+			                                                 mask.SetInvalid(index);
+			                                                 return static_cast<idx_t>(0);
+		                                                 }
+	                                                 });
 }
 
 ScalarFunction ParseFormattedBytesFun::GetFunction() {
