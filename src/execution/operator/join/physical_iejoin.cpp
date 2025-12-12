7c7c9943b9d7648100e3655b519c9b88c37e4879
Richard Wesley <13156216+hawkfish@users.noreply.github.com>
2025-12-12 18:18:28 +1300

    Internal #6607: IEJoin Task Locking
    * Make stage atomic to avoid race condition.

--- src/execution/operator/join/physical_iejoin.cpp
+++ src/execution/operator/join/physical_iejoin.cpp
@@ -311,7 +311,7 @@ public:
 	IEJoinGlobalState &gsink;
 
 	//! The processing stage
-	IEJoinSourceStage stage = IEJoinSourceStage::INIT;
+	atomic<IEJoinSourceStage> stage;
 	//! The the number of tasks per stage.
 	vector<idx_t> stage_tasks;
 	//! The the first task in the stage.
@@ -1364,7 +1364,7 @@ bool IEJoinGlobalSourceState::TryPrepareNextStage() {
 	//	Inside lock
 	const auto stage_count = GetStageCount(stage);
 	const auto stage_next = GetStageNext(stage).load();
-	switch (stage) {
+	switch (stage.load()) {
 	case IEJoinSourceStage::INIT:
 		stage = IEJoinSourceStage::SINK_L1;
 		return true;
@@ -1479,6 +1479,7 @@ bool IEJoinGlobalSourceState::TryNextTask(TaskPtr &task, Task &task_local) {
 }
 
 bool IEJoinGlobalSourceState::TryNextTask(Task &task) {
+	//	Inside lock
 	if (next_task >= GetTaskCount()) {
 		return false;
 	}
@@ -1496,7 +1497,7 @@ bool IEJoinGlobalSourceState::TryNextTask(Task &task) {
 		return false;
 	}
 
-	switch (stage) {
+	switch (stage.load()) {
 	case IEJoinSourceStage::SINK_L1:
 		task.l_range.first = MinValue(task.thread_idx * per_thread, left_blocks + right_blocks);
 		task.l_range.second = MinValue(task.l_range.first + per_thread, left_blocks + right_blocks);
