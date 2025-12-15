60dfd9fd2a5b2caee9cff649cc0e379561d594bc
Etgar Shmueli <etgarsh@monday.com>
2025-12-15 14:32:16 +0200

    Rename variable `result` to `parsing_result` in `parse_formatted_bytes` for clarity.

--- extension/core_functions/scalar/string/parse_formatted_bytes.cpp
+++ extension/core_functions/scalar/string/parse_formatted_bytes.cpp
@@ -16,13 +16,13 @@ static void TryParseFormattedBytesFunction(DataChunk &args, ExpressionState &sta
 
 	UnaryExecutor::ExecuteWithNulls<string_t, idx_t>(
 	    arg0, result, args.size(), [&](string_t str, ValidityMask &mask, idx_t index) {
-		    idx_t result;
-		    string error = StringUtil::TryParseFormattedBytes(str.GetString(), result);
+		    idx_t parsing_result;
+		    string error = StringUtil::TryParseFormattedBytes(str.GetString(), parsing_result);
 		    if (!error.empty()) {
 			    mask.SetInvalid(index);
 			    return static_cast<idx_t>(0);
 		    }
-		    return result;
+		    return parsing_result;
 	    });
 }
 
