9f0af3c6c27c8e803dc5cd016b2f3ce7ee220195
dtenwolde <dljtw98@gmail.com>
2025-12-15 08:15:04 +0100

    Format fix

--- test/extension/test_splitting_extension_statements.cpp
+++ test/extension/test_splitting_extension_statements.cpp
@@ -11,8 +11,7 @@ TEST_CASE("Parser Extension query splitting", "[parser-extension]") {
 	DuckDB db;
 	Connection conn(*db.instance);
 	ParserExtension parser_extension;
-	parser_extension.parse_function = [](ParserExtensionInfo *,
-										 const std::string &) -> ParserExtensionParseResult {
+	parser_extension.parse_function = [](ParserExtensionInfo *, const std::string &) -> ParserExtensionParseResult {
 		duckdb::unique_ptr<ParserExtensionParseData> empty_data {};
 		return ParserExtensionParseResult {std::move(empty_data)};
 	};
@@ -22,14 +21,13 @@ TEST_CASE("Parser Extension query splitting", "[parser-extension]") {
 	options.extensions = &parser_extensions;
 
 	auto query = GENERATE( // Normal CTAS query
-		"create or syntaxerror table my_object as from (\n	select 1\n	where starts_with('name', "
-		"'test_simple_share') -- ;	\n);",
-		// Extension query, replacing the table with result
-		"create or replace result my_object as from (\n	select 1\n	where starts_with('name', "
-		"'test_simple_share') -- ;	\n);",
-		// Trying other queries
-		"CREATE OR replace result my_object as FROM (SELECT 1); -- ;"
-		);
+	    "create or syntaxerror table my_object as from (\n	select 1\n	where starts_with('name', "
+	    "'test_simple_share') -- ;	\n);",
+	    // Extension query, replacing the table with result
+	    "create or replace result my_object as from (\n	select 1\n	where starts_with('name', "
+	    "'test_simple_share') -- ;	\n);",
+	    // Trying other queries
+	    "CREATE OR replace result my_object as FROM (SELECT 1); -- ;");
 
 	Parser parser {options};
 	REQUIRE_NOTHROW(parser.ParseQuery(query));
@@ -40,8 +38,7 @@ TEST_CASE("Parser Extension multi-query splitting", "[parser-extension]") {
 	DuckDB db;
 	Connection conn(*db.instance);
 	ParserExtension parser_extension;
-	parser_extension.parse_function = [](ParserExtensionInfo *,
-										 const std::string &) -> ParserExtensionParseResult {
+	parser_extension.parse_function = [](ParserExtensionInfo *, const std::string &) -> ParserExtensionParseResult {
 		duckdb::unique_ptr<ParserExtensionParseData> empty_data {};
 		return ParserExtensionParseResult {std::move(empty_data)};
 	};
@@ -51,11 +48,9 @@ TEST_CASE("Parser Extension multi-query splitting", "[parser-extension]") {
 	options.extensions = &parser_extensions;
 
 	auto multi_statement_query = GENERATE( // Normal CTAS query
-		"CREATE OR REPLACE result my_object as FROM (SELECT 1); -- ; \n SELECT 1;"
-		);
+	    "CREATE OR REPLACE result my_object as FROM (SELECT 1); -- ; \n SELECT 1;");
 
 	Parser parser {options};
 	REQUIRE_NOTHROW(parser.ParseQuery(multi_statement_query));
 	REQUIRE(parser.statements.size() == 2);
-
 }
