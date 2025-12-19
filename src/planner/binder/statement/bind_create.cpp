fcf9d620d0dcfcd557e19a026d8abfd2c5027dcb
Tishj <t_b@live.nl>
2025-12-19 11:11:42 +0100

    undo changes

--- src/planner/binder/statement/bind_create.cpp
+++ src/planner/binder/statement/bind_create.cpp
@@ -45,7 +45,6 @@
 #include "duckdb/common/type_visitor.hpp"
 #include "duckdb/function/table_macro_function.hpp"
 #include "duckdb/main/settings.hpp"
-#include "duckdb/parser/query_node.hpp"
 
 namespace duckdb {
 
