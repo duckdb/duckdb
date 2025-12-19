8fddef0a1b1f4ef3196e0205a41efb82fb00befe
Mytherin <mark.raasveldt@gmail.com>
2025-12-19 16:07:06 +0100

    Remove shared lock in scan again

--- src/storage/data_table.cpp
+++ src/storage/data_table.cpp
@@ -234,12 +234,6 @@ TableIOManager &TableIOManager::Get(DataTable &table) {
 //===--------------------------------------------------------------------===//
 void DataTable::InitializeScan(ClientContext &context, DuckTransaction &transaction, TableScanState &state,
                                const vector<StorageIndex> &column_ids, optional_ptr<TableFilterSet> table_filters) {
-	for (auto &id : column_ids) {
-		if (id.IsRowIdColumn()) {
-			state.checkpoint_lock = transaction.SharedLockTable(*info);
-			break;
-		}
-	}
 	auto &local_storage = LocalStorage::Get(transaction);
 	state.Initialize(column_ids, context, table_filters);
 	row_groups->InitializeScan(context, state.table_state, column_ids, table_filters);
@@ -248,12 +242,6 @@ void DataTable::InitializeScan(ClientContext &context, DuckTransaction &transact
 
 void DataTable::InitializeScanWithOffset(DuckTransaction &transaction, TableScanState &state,
                                          const vector<StorageIndex> &column_ids, idx_t start_row, idx_t end_row) {
-	for (auto &id : column_ids) {
-		if (id.IsRowIdColumn()) {
-			state.checkpoint_lock = transaction.SharedLockTable(*info);
-			break;
-		}
-	}
 	state.Initialize(column_ids);
 	row_groups->InitializeScanWithOffset(QueryContext(), state.table_state, column_ids, start_row, end_row);
 }
@@ -282,13 +270,6 @@ idx_t DataTable::MaxThreads(ClientContext &context) const {
 
 void DataTable::InitializeParallelScan(ClientContext &context, ParallelTableScanState &state,
                                        const vector<ColumnIndex> &column_indexes) {
-	for (auto &id : column_indexes) {
-		if (id.IsRowIdColumn()) {
-			auto &transaction = DuckTransaction::Get(context, db);
-			state.checkpoint_lock = transaction.SharedLockTable(*info);
-			break;
-		}
-	}
 	auto &local_storage = LocalStorage::Get(context, db);
 	row_groups->InitializeParallelScan(state.scan_state);
 
