07dfa5744a028bc64bca2780a0938a06f5f43a60
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-12-26 19:37:24 +0100

    Add LOAD_TESTS to more extensions

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(httpfs
-    LOAD TESTS
+    LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG 7bd8c9cb1801dab374b63ac15ade1ba29c501b1e
     INCLUDE_DIR src/include
