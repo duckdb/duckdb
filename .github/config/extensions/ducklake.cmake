32fe112a0934bdcfbf4376748ab992a25c472459
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-12-27 15:46:16 +0100

    Patch ducklake: avoid 2 tests on same DB file

--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -1,6 +1,7 @@
 duckdb_extension_load(ducklake
     DONT_LINK
     LOAD_TESTS
+    APPLY_PATCHES
     GIT_URL https://github.com/duckdb/ducklake
     GIT_TAG de813ff4d052bffe3e9e7ffcdc31d18ca38e5ecd
 )
