e130893a48d2bd4f9835ae19180476fb23f2e0ba
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-12-28 10:33:27 +0100

    Fixup via tests modifications limitations of iceberg and ducklake on autoloading

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -7,6 +7,7 @@ endif()
 if (NOT MINGW)
     duckdb_extension_load(iceberg
             ${LOAD_ICEBERG_TESTS}
+            APPLY_PATCHES
             GIT_URL https://github.com/duckdb/duckdb-iceberg
             GIT_TAG 90c059e2dd876a483e63a2f1b04ef66fa45a0c85
             )
