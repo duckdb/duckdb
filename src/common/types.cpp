ea723cadb928a92e3eee89c015777a146ea2eebe
Max Gabrielsson <max@gabrielsson.com>
2025-12-30 23:44:36 +0100

    remove more user type

--- src/common/types.cpp
+++ src/common/types.cpp
@@ -1484,9 +1484,6 @@ void LogicalType::SetAlias(string alias) {
 }
 
 string LogicalType::GetAlias() const {
-	if (id() == LogicalTypeId::USER) {
-		return UnboundType::GetName(*this);
-	}
 	if (type_info_) {
 		return type_info_->alias;
 	}
@@ -1494,9 +1491,6 @@ string LogicalType::GetAlias() const {
 }
 
 bool LogicalType::HasAlias() const {
-	if (id() == LogicalTypeId::UNBOUND) {
-		return !UnboundType::GetName(*this).empty();
-	}
 	if (type_info_ && !type_info_->alias.empty()) {
 		return true;
 	}
