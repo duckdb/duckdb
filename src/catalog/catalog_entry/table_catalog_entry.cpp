b396d09624adbc20ff02982448b30b4643897d76
Max Gabrielsson <max@gabrielsson.com>
2026-01-01 17:27:37 +0100

    remove strong assertion

--- src/catalog/catalog_entry/table_catalog_entry.cpp
+++ src/catalog/catalog_entry/table_catalog_entry.cpp
@@ -182,7 +182,6 @@ string TableCatalogEntry::ColumnsToSQL(const ColumnList &columns, const vector<u
 				auto &expr = generated_expression.get();
 				D_ASSERT(expr.GetExpressionType() == ExpressionType::OPERATOR_CAST);
 				auto &cast_expr = expr.Cast<CastExpression>();
-				D_ASSERT(cast_expr.cast_type.id() == column_type.id());
 				generated_expression = *cast_expr.child;
 			}
 			ss << " GENERATED ALWAYS AS(" << generated_expression.get().ToString() << ")";
