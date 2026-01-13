c4aca1a8ca0abd7efe7ededdb45bbb75b5946a10
Mytherin <mark.raasveldt@gmail.com>
2026-01-13 13:49:37 +0100

    Add a cache to the Local settings that is refreshed when the settings version is incremented

--- src/include/duckdb/main/user_settings.hpp
+++ src/include/duckdb/main/user_settings.hpp
@@ -26,6 +26,13 @@ private:
 	case_insensitive_map_t<Value> set_variables;
 };
 
+struct CachedGlobalSettings {
+	CachedGlobalSettings(idx_t version, UserSettingsMap settings);
+
+	idx_t version;
+	UserSettingsMap settings;
+};
+
 struct GlobalUserSettings {
 public:
 	GlobalUserSettings();
@@ -41,16 +48,21 @@ public:
 	void AddExtensionOption(const string &name, ExtensionOption extension_option);
 	case_insensitive_map_t<ExtensionOption> GetExtensionSettings() const;
 	bool TryGetExtensionOption(const String &name, ExtensionOption &result) const;
+	shared_ptr<CachedGlobalSettings> GetSettings(shared_ptr<CachedGlobalSettings> &cache) const;
 
 private:
 	mutable mutex lock;
 	UserSettingsMap settings_map;
 	//! Extra parameters that can be SET for loaded extensions
 	case_insensitive_map_t<ExtensionOption> extension_parameters;
+	//! Current version of the settings - incremented when settings are modified
+	atomic<idx_t> settings_version;
 };
 
 struct LocalUserSettings {
 public:
+	~LocalUserSettings();
+
 	void SetUserSetting(const String &name, Value target_value);
 	void ClearSetting(const String &name);
 	bool IsSet(const String &name) const;
@@ -59,5 +71,6 @@ public:
 
 private:
 	UserSettingsMap settings_map;
+	mutable shared_ptr<CachedGlobalSettings> global_settings_cache;
 };
 } // namespace duckdb
