7987e6dfc249811bbe18ae7609e565044d18b95d
Mytherin <mark.raasveldt@gmail.com>
2026-01-13 09:24:42 +0100

    Move setting management to a separate UserSettingsMap variable

--- src/main/user_settings.cpp
+++ src/main/user_settings.cpp
@@ -2,42 +2,58 @@
 
 namespace duckdb {
 
-UserSettings::UserSettings() {
-}
-
-UserSettings::UserSettings(const UserSettings &other)
-    : set_variables(other.set_variables), extension_parameters(other.extension_parameters) {
-}
-
-UserSettings &UserSettings::operator=(const UserSettings &other) {
-	set_variables = other.set_variables;
-	extension_parameters = other.extension_parameters;
-	return *this;
-}
-
-void UserSettings::SetUserSetting(const String &name, Value target_value) {
-	lock_guard<mutex> guard(lock);
+void UserSettingsMap::SetUserSetting(const String &name, Value target_value) {
 	set_variables[name.ToStdString()] = std::move(target_value);
 }
 
-void UserSettings::ClearSetting(const String &name) {
-	lock_guard<mutex> guard(lock);
+void UserSettingsMap::ClearSetting(const String &name) {
 	set_variables.erase(name.ToStdString());
 }
 
-bool UserSettings::IsSet(const String &name) const {
-	lock_guard<mutex> guard(lock);
+bool UserSettingsMap::IsSet(const String &name) const {
 	return set_variables.find(name.ToStdString()) != set_variables.end();
 }
 
-bool UserSettings::TryGetSetting(const String &name, Value &result_value) const {
-	lock_guard<mutex> guard(lock);
+bool UserSettingsMap::TryGetSetting(const String &name, Value &result_value) const {
 	auto entry = set_variables.find(name.ToStdString());
 	if (entry == set_variables.end()) {
 		return false;
 	}
 	result_value = entry->second;
 	return true;
+}
+
+UserSettings::UserSettings() {
+}
+
+UserSettings::UserSettings(const UserSettings &other)
+    : settings_map(other.settings_map), extension_parameters(other.extension_parameters) {
+}
+
+UserSettings &UserSettings::operator=(const UserSettings &other) {
+	settings_map = other.settings_map;
+	extension_parameters = other.extension_parameters;
+	return *this;
+}
+
+void UserSettings::SetUserSetting(const String &name, Value target_value) {
+	lock_guard<mutex> guard(lock);
+	settings_map.SetUserSetting(name, std::move(target_value));
+}
+
+void UserSettings::ClearSetting(const String &name) {
+	lock_guard<mutex> guard(lock);
+	settings_map.ClearSetting(name);
+}
+
+bool UserSettings::IsSet(const String &name) const {
+	lock_guard<mutex> guard(lock);
+	return settings_map.IsSet(name);
+}
+
+bool UserSettings::TryGetSetting(const String &name, Value &result_value) const {
+	lock_guard<mutex> guard(lock);
+	return settings_map.TryGetSetting(name, result_value);
 }
 
 bool UserSettings::HasExtensionOption(const string &name) const {
