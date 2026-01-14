985f8091f10683ff578d7bda2a6fae16b145203c
Maia <maiadegraaf@gmail.com>
2026-01-14 17:30:37 +0100

    Revert GIT_TAG for vss extension

--- .github/config/extensions/vss.cmake
+++ .github/config/extensions/vss.cmake
@@ -2,6 +2,6 @@ duckdb_extension_load(vss
         LOAD_TESTS
         DONT_LINK
         GIT_URL https://github.com/duckdb/duckdb-vss
-        GIT_TAG d796205e45ba38f894fb16bbbf1b89374d3be2e5
+        GIT_TAG c8a4efe05003d8ef6eaad34f5521cf50126c9967
         TEST_DIR test/sql
     )
