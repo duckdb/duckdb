921f003993f7931dc9726e93c423400efb9b5f48
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-01-15 18:12:47 +0100

    Bump ducklake

--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -2,5 +2,5 @@ duckdb_extension_load(ducklake
     DONT_LINK
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/ducklake
-    GIT_TAG 3f1b37258b4b3d123d91180a1f4cc11fe8d33386
+    GIT_TAG f2f2de95a97b4f2da0e898d4f726cc442820fd36
 )
