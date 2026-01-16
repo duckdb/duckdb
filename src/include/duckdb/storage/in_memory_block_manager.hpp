b5a0d335cf3acbac408c77442faacb6e02f870aa
Yan <60644013+yan-alex@users.noreply.github.com>
2026-01-16 14:36:35 +0100

    simple in-memory Write() implementation. Also test that in-memory zstd DOES work.

--- src/include/duckdb/storage/in_memory_block_manager.hpp
+++ src/include/duckdb/storage/in_memory_block_manager.hpp
@@ -11,6 +11,7 @@
 #include "duckdb/common/exception.hpp"
 #include "duckdb/main/client_context.hpp"
 #include "duckdb/storage/block_manager.hpp"
+#include "duckdb/common/checksum.hpp"
 
 namespace duckdb {
 
@@ -63,8 +64,11 @@ public:
 	void Write(FileBuffer &block, block_id_t block_id) override {
 		throw InternalException("Cannot perform IO in in-memory database - Write!");
 	}
+	// We currently do not encrypt in-memory storage
 	void Write(QueryContext context, FileBuffer &block, block_id_t block_id) override {
-		throw InternalException("Cannot perform IO in in-memory database - Write with client context!");
+		D_ASSERT(block_id >= 0);
+		uint64_t checksum = Checksum(block.buffer, block.Size());
+		Store<uint64_t>(checksum, block.InternalBuffer());
 	}
 	void WriteHeader(QueryContext context, DatabaseHeader header) override {
 		throw InternalException("Cannot perform IO in in-memory database - WriteHeader!");
@@ -83,6 +87,7 @@ public:
 	}
 	// LCOV_EXCL_STOP
 private:
-	idx_t max_block = 0;
+	//! The current maximum block id
+	block_id_t max_block = 0;
 };
 } // namespace duckdb
