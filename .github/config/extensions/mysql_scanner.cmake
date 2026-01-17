e9ecab34f96d9665c667cd4c50285d6e7a16ec43
Alex Kasko <alex@staticlibs.net>
2026-01-17 19:25:36 +0000

    Bump MySQL
    This PR updates MySQL scanner extension.
    
    The patch is removed and `extension_entries.hpp` is regenerated using
    `generate_extensions_function.py`.

--- .github/config/extensions/mysql_scanner.cmake
+++ .github/config/extensions/mysql_scanner.cmake
@@ -3,7 +3,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
             DONT_LINK
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-mysql
-            GIT_TAG 7a8c7269dcf452665e7fe05cce0f6e7cab137300
-            APPLY_PATCHES
+            GIT_TAG 934124260a82b2bbb6ca5fb53d7f0a85d2f32250
             )
 endif()
