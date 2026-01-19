3b8e3d43b05c9d390d2da5f9ae7969bb376a81b8
Yan <60644013+yan-alex@users.noreply.github.com>
2026-01-19 15:17:39 +0100

    in-memory Write() / Read() implementation. Also test that in-memory zstd DOES work.

--- src/include/duckdb/storage/in_memory_block_manager.hpp
+++ src/include/duckdb/storage/in_memory_block_manager.hpp
@@ -12,6 +12,8 @@
 #include "duckdb/main/client_context.hpp"
 #include "duckdb/storage/block_manager.hpp"
 #include "duckdb/common/checksum.hpp"
+#include <unordered_map>
+#include <string>
 
 namespace duckdb {
 
@@ -20,12 +22,22 @@ class InMemoryBlockManager : public BlockManager {
 public:
 	using BlockManager::BlockManager;
 
+	static constexpr uint64_t BLOCK_START = Storage::FILE_HEADER_SIZE * 3;
+
 	// LCOV_EXCL_START
 	unique_ptr<Block> ConvertBlock(block_id_t block_id, FileBuffer &source_buffer) override {
-		throw InternalException("Cannot perform IO in in-memory database - ConvertBlock!");
+		D_ASSERT(source_buffer.AllocSize() == GetBlockAllocSize());
+		return make_uniq<Block>(source_buffer, block_id, GetBlockHeaderSize());
 	}
 	unique_ptr<Block> CreateBlock(block_id_t block_id, FileBuffer *source_buffer) override {
-		throw InternalException("Cannot perform IO in in-memory database - CreateBlock!");
+		unique_ptr<Block> result;
+		if (source_buffer) {
+			result = ConvertBlock(block_id, *source_buffer);
+		} else {
+			result = make_uniq<Block>(BlockAllocator::Get(buffer_manager.GetDatabase()), block_id, *this);
+		}
+		result->Initialize(DebugInitialize::DEBUG_ZERO_INITIALIZE);
+		return result;
 	}
 	block_id_t GetFreeBlockId() override {
 		return max_block++;
@@ -54,9 +66,42 @@ public:
 	idx_t GetMetaBlock() override {
 		throw InternalException("Cannot perform IO in in-memory database - GetMetaBlock!");
 	}
+	void CheckChecksum(FileBuffer &block, uint64_t location) const {
+		uint64_t stored_checksum = Load<uint64_t>(block.InternalBuffer());
+		uint64_t computed_checksum = Checksum(block.buffer, block.Size());
+
+		// verify the checksum
+		if (stored_checksum != computed_checksum) {
+			throw IOException(
+			    "Corrupt in-memory block: computed checksum %llu does not match stored checksum %llu in block "
+			    "at location %llu",
+			    computed_checksum, stored_checksum, location);
+		}
+	}
+	void ReadAndChecksum(QueryContext context, Block &block, uint64_t location) const {
+		if (context.GetClientContext() != nullptr) {
+			context.GetClientContext()->client_data->profiler->AddToCounter(MetricType::TOTAL_BYTES_READ,
+			                                                                block.AllocSize());
+		}
+		lock_guard<mutex> guard(lock);
+		auto it = blocks.find(block.id);
+		if (it == blocks.end()) {
+			throw InternalException("Trying to read a block that does not exist in-memory: " + to_string(block.id));
+		}
+
+		// copy from our stored block to the block provided by the buffer manager
+		memcpy(block.InternalBuffer(), it->second.get(), GetBlockAllocSize());
+
+		CheckChecksum(block, location);
+	}
+
+	idx_t GetBlockLocation(block_id_t block_id) const {
+		return BLOCK_START + NumericCast<idx_t>(block_id) * GetBlockAllocSize();
+	}
 
 	void Read(QueryContext context, Block &block) override {
-		throw InternalException("Cannot perform IO in in-memory database - Read!");
+		D_ASSERT(block.id >= 0);
+		ReadAndChecksum(context, block, GetBlockLocation(block.id));
 	}
 	void ReadBlocks(FileBuffer &buffer, block_id_t start_block, idx_t block_count) override {
 		throw InternalException("Cannot perform IO in in-memory database - ReadBlocks!");
@@ -69,6 +114,14 @@ public:
 		D_ASSERT(block_id >= 0);
 		uint64_t checksum = Checksum(block.buffer, block.Size());
 		Store<uint64_t>(checksum, block.InternalBuffer());
+
+		// copy the block to our own storage
+		auto &allocator = BufferAllocator::Get(buffer_manager.GetDatabase());
+		auto copy = allocator.Allocate(GetBlockAllocSize());
+		memcpy(copy.get(), block.InternalBuffer(), GetBlockAllocSize());
+
+		lock_guard<mutex> guard(lock);
+		blocks[block_id] = std::move(copy);
 	}
 	void WriteHeader(QueryContext context, DatabaseHeader header) override {
 		throw InternalException("Cannot perform IO in in-memory database - WriteHeader!");
@@ -87,6 +140,9 @@ public:
 	}
 	// LCOV_EXCL_STOP
 private:
+	mutable mutex lock;
+	mutable unordered_map<block_id_t, AllocatedData> blocks;
+
 	//! The current maximum block id
 	block_id_t max_block = 0;
 };
