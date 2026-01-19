d5a33ec43e1f3745d8dde16b0032c10627da102d
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-01-19 22:15:04 +0100

    FIXME: skip iceberg tests

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -6,7 +6,7 @@ else ()
 endif()
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(iceberg
-            ${LOAD_ICEBERG_TESTS}
+	    #FIXME: restore autoloading tests ${LOAD_ICEBERG_TESTS}
             GIT_URL https://github.com/duckdb/duckdb-iceberg
             GIT_TAG b3e54a56436d6385c6820ec6843d3e927431f565
             APPLY_PATCHES
