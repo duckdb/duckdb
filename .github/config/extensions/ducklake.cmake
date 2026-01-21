f12200655900c11affccc426c7d60121ff0c6b48
Pedro Holanda <pedroholanda@gmail.com>
2026-01-21 20:10:59 +0100

    Remove patched, upgrade ducklake, run tests

--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(ducklake
     DONT_LINK
+    LOAD_TESTS
     GIT_URL https://github.com/duckdb/ducklake
-    GIT_TAG 2328fbeac1bb2bca247bc3994f4ed31fe3a49993
-    APPLY_PATCHES
+    GIT_TAG fa6ebcd05226ed0c36958fe39b54749de885407f
 )
