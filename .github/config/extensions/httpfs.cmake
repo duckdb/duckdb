ab8d5e9639991611baaa74e8d968f0191ae2dfb4
Maia <maia@duckdblabs.com>
2026-01-21 16:41:51 +0100

    bump extensions

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG add35a03c1adfe530bb4ef69133b94fe8ec8ea35
+    GIT_TAG 38c0fafe2257f61f0beca0b61e0aad635a82b28c
     INCLUDE_DIR src/include
 )
--- .github/config/extensions/spatial.cmake
+++ .github/config/extensions/spatial.cmake
@@ -3,9 +3,8 @@ if (${BUILD_COMPLETE_EXTENSION_SET})
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG 2f2668d211c0cf759f460403a108f24eb8b887e3
+    GIT_TAG 783ecbb66554854db1218731566615a30a9f1a0d
     INCLUDE_DIR src/spatial
     TEST_DIR test/sql
-    APPLY_PATCHES
     )
 endif()
