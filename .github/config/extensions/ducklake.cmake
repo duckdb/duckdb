f873df2184809e54f9e927fa9c154a98c07541ac
Pedro Holanda <pedroholanda@gmail.com>
2026-01-22 08:25:16 +0100

    update again

--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -2,5 +2,5 @@ duckdb_extension_load(ducklake
     DONT_LINK
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/ducklake
-    GIT_TAG fa6ebcd05226ed0c36958fe39b54749de885407f
+    GIT_TAG 9a19bd8de89e7af3e3a46deee794c591898fb717
 )
