47e8cda7b7e3b12365ff698f1221d14fe0ec5ca9
dentiny <dentinyhao@gmail.com>
2026-01-22 12:04:49 +0000

    apply patches

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -3,4 +3,5 @@ duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG add35a03c1adfe530bb4ef69133b94fe8ec8ea35
     INCLUDE_DIR src/include
+    APPLY_PATCHES
 )
