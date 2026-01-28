4135288db3f37083b54ded766a9e5705ad266ed9
ben fleis <benfleis@users.noreply.github.com>
2026-01-28 12:42:16 +0100

    revert the alias, leave that in

--- src/main/extension/extension_alias.cpp
+++ src/main/extension/extension_alias.cpp
@@ -10,7 +10,7 @@ static const ExtensionAlias internal_aliases[] = {{"http", "httpfs"}, // httpfs
                                                   {"postgres", "postgres_scanner"}, // postgres
                                                   {"sqlite", "sqlite_scanner"},     // sqlite
                                                   {"sqlite3", "sqlite_scanner"},
-                                                  {"unity_catalog", "unity_catalog"}, // old name for compatibility
+                                                  {"uc_catalog", "unity_catalog"}, // old name for compatibility
                                                   {nullptr, nullptr}};
 
 idx_t ExtensionHelper::ExtensionAliasCount() {
