0dc5d9c49a6fdfeb03c5d8674937f90bf861e16d
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-01-29 10:02:48 +0100

    Bump httpfs AND remove patches

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 64a109592051759ae1605162ed8771caecdd076e
-    APPLY_PATCHES
+    GIT_TAG 7d19b8993c1516da2ae2511b9247b90218e2d178
 )
