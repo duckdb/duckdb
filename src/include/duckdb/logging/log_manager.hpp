6aaac95bfcf97501e04856a4c783338708627486
Christiaan Herrewijn <christiaan@duckdblabs.com>
2026-01-29 18:26:17 +0100

    refactor observer pattern

--- src/include/duckdb/logging/log_manager.hpp
+++ src/include/duckdb/logging/log_manager.hpp
@@ -28,7 +28,7 @@ class LogManager {
 
 public:
 	// Note: two step initialization because Logger needs shared pointer to log manager TODO: can we clean up?
-	explicit LogManager(shared_ptr<DatabaseInstance> db, LogConfig config = LogConfig());
+	explicit LogManager(weak_ptr<DatabaseInstance> db, LogConfig config = LogConfig());
 	~LogManager();
 	void Initialize();
 
@@ -85,14 +85,12 @@ protected:
 
 	void SetConfigInternal(LogConfig config);
 
-	shared_ptr<DatabaseInstance> GetDatabaseInstance();
-
+protected:
 	mutex lock;
 	LogConfig config;
 
 	shared_ptr<Logger> global_logger;
 	weak_ptr<DatabaseInstance> db_instance;
-
 	shared_ptr<LogStorage> log_storage;
 
 	idx_t next_registered_logging_context_index = 0;
--- src/logging/log_manager.cpp
+++ src/logging/log_manager.cpp
@@ -59,19 +59,14 @@ shared_ptr<LogStorage> LogManager::GetLogStorage() {
 	return log_storage;
 }
 
-shared_ptr<DatabaseInstance> LogManager::GetDatabaseInstance() {
-	unique_lock<mutex> lck(lock);
-	return db_instance.lock();
-}
-
 bool LogManager::CanScan(LoggingTargetTable table) {
 	unique_lock<mutex> lck(lock);
 	return log_storage->CanScan(table);
 }
 
-LogManager::LogManager(shared_ptr<DatabaseInstance> db, LogConfig config_p) : config(std::move(config_p)) {
-	log_storage = make_uniq<InMemoryLogStorage>(*db);
-	db_instance = db;
+LogManager::LogManager(weak_ptr<DatabaseInstance> db, LogConfig config_p)
+    : config(std::move(config_p)), db_instance(db) {
+	log_storage = make_uniq<InMemoryLogStorage>(*db_instance.lock());
 }
 
 LogManager::~LogManager() {
@@ -102,7 +97,7 @@ RegisteredLoggingContext LogManager::RegisterLoggingContextInternal(LoggingConte
 
 void LogManager::WriteLogEntry(timestamp_t timestamp, const char *log_type, LogLevel log_level, const char *log_message,
                                const RegisteredLoggingContext &context) {
-	if (log_level == LogLevel::LOG_WARNING && (Settings::Get<WarningsAsErrorsSetting>(*GetDatabaseInstance()))) {
+	if (log_level == LogLevel::LOG_WARNING && (Settings::Get<WarningsAsErrorsSetting>(*db_instance.lock()))) {
 		throw InvalidInputException(log_message);
 	} else {
 		unique_lock<mutex> lck(lock);
