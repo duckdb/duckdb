59296de3f51a106fb75ede2791cf1d0fba191ba5
Etgar Shmueli <legendsarea@gmail.com>
2026-02-02 18:52:24 +0200

    Reverted unrelated changes to the `Verify` functions (will be changed in another PR)

--- src/planner/logical_operator.cpp
+++ src/planner/logical_operator.cpp
@@ -178,15 +178,7 @@ void LogicalOperator::Verify(ClientContext &context) {
 		MemoryStream stream(Allocator::Get(context));
 		// We are serializing a query plan
 		try {
-			auto &config = DBConfig::GetConfig(context);;
-			SerializationOptions options;
-			if (config.options.serialization_compatibility.manually_set) {
-				options.serialization_compatibility = config.options.serialization_compatibility;
-			} else {
-				options.serialization_compatibility = SerializationCompatibility::Latest();
-			}
-
-			BinarySerializer::Serialize(*expressions[expr_idx], stream, options);
+			BinarySerializer::Serialize(*expressions[expr_idx], stream);
 		} catch (NotImplementedException &ex) {
 			// ignore for now (FIXME)
 			continue;
