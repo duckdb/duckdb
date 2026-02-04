38b3f7d820cf6ebb48c5293678d025f59a21349c
Maia <maia@duckdblabs.com>
2026-02-04 15:32:37 +0100

    add mutex lock

--- tools/shell/shell_renderer.cpp
+++ tools/shell/shell_renderer.cpp
@@ -1814,6 +1814,8 @@ unique_ptr<ShellRenderer> ShellState::GetRenderer(RenderMode mode) {
 
 void ShellLogStorage::WriteLogEntry(duckdb::timestamp_t timestamp, duckdb::LogLevel level, const string &log_type,
                                     const string &log_message, const duckdb::RegisteredLoggingContext &context) {
+	lock.lock();
+
 	HighlightElementType element_type;
 	switch (level) {
 	case duckdb::LogLevel::LOG_TRACE:
@@ -1836,9 +1838,18 @@ void ShellLogStorage::WriteLogEntry(duckdb::timestamp_t timestamp, duckdb::LogLe
 		throw std::runtime_error("Unsupported log level for WriteLogEntry");
 	}
 
+	// check if the log has already been printed
+	auto log_id = duckdb::StringUtil::CIHash(log_message);
+	if (printed_logs.find(log_id) != printed_logs.end()) {
+		lock.unlock();
+		return;
+	}
+	printed_logs.emplace(log_id);
+
 	const auto log_level = duckdb::EnumUtil::ToString(level);
 	shell_highlight.PrintText(log_level + ":\n", PrintOutput::STDOUT, element_type);
 	shell_highlight.PrintText(log_message + "\n\n", PrintOutput::STDOUT, element_type);
+	lock.unlock();
 }
 
 } // namespace duckdb_shell
