4d13247e35a6aa58970c850cc512340365cdc9c3
Tim Fischer <me@timfi.dev>
2026-02-06 12:55:08 +0100

    Rerun setting autogeneration again again again

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -201,12 +201,12 @@ static const ConfigurationOption internal_options[] = {
     DUCKDB_SETTING(ZstdMinStringLengthSetting),
     FINAL_SETTING};
 
-static const ConfigurationAlias setting_aliases[] = {DUCKDB_SETTING_ALIAS("memory_limit", 94),
+static const ConfigurationAlias setting_aliases[] = {DUCKDB_SETTING_ALIAS("memory_limit", 95),
                                                      DUCKDB_SETTING_ALIAS("null_order", 39),
-                                                     DUCKDB_SETTING_ALIAS("profiling_output", 113),
-                                                     DUCKDB_SETTING_ALIAS("user", 128),
+                                                     DUCKDB_SETTING_ALIAS("profiling_output", 114),
+                                                     DUCKDB_SETTING_ALIAS("user", 129),
                                                      DUCKDB_SETTING_ALIAS("wal_autocheckpoint", 22),
-                                                     DUCKDB_SETTING_ALIAS("worker_threads", 127),
+                                                     DUCKDB_SETTING_ALIAS("worker_threads", 128),
                                                      FINAL_ALIAS};
 
 vector<ConfigurationOption> DBConfig::GetOptions() {
