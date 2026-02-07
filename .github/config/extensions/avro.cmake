4f5591116655d2ef6e41e843f12a105e1fea43b6
Tishj <t_b@live.nl>
2026-02-07 17:09:06 +0100

    remove patches, merged into duckdb-avro, update hash again

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -2,7 +2,6 @@ if (NOT MINGW)
     duckdb_extension_load(avro
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-avro
-            GIT_TAG 62ed88bc1ea0dd6b09238428d887b4b8689784dd
-            APPLY_PATCHES
+            GIT_TAG e4bd712795ecb31f97106880c6f2843ffea630f1
     )
 endif()
