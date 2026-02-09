414590486fc915cae3922c2f9703981e82ee6db2
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-02-09 16:31:39 +0100

    When 'httpfs' is loaded, use BumpToSecureProtocol to bump url

--- src/main/extension/extension_install.cpp
+++ src/main/extension/extension_install.cpp
@@ -505,7 +505,10 @@ static unique_ptr<ExtensionInstallInfo> InstallFromRepository(DatabaseInstance &
 	string generated_url = ExtensionHelper::ExtensionFinalizeUrlTemplate(url_template, extension_name);
 
 	// Special handling for http repository: avoid using regular filesystem (note: the filesystem is not used here)
-	if (StringUtil::StartsWith(options.repository->path, "http://")) {
+	if (HTTPUtil::IsHTTPProtocol(options.repository->path)) {
+		if (db.ExtensionIsLoaded("httpfs")) {
+			HTTPUtil::BumpToSecureProtocol(generated_url);
+		}
 		return InstallFromHttpUrl(db, generated_url, extension_name, temp_path, local_extension_path, options, context);
 	}
 
