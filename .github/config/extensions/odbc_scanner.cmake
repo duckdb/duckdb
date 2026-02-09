96956cb64d7114a1d48360787c7982ff0605ec2c
Alex Kasko <alex@staticlibs.net>
2026-02-09 00:03:41 +0000

    Bump odbc_scanner
    Update `odbc_scanner` extension to support cross-compiling it on macOS.
    
    Depends on: duckdb/extension-ci-tools#319
    
    Ref: duckdblabs/duckdb-internal#7373

--- .github/config/extensions/odbc_scanner.cmake
+++ .github/config/extensions/odbc_scanner.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(odbc_scanner
             DONT_LINK
             GIT_URL https://github.com/duckdb/odbc-scanner
-            GIT_TAG 673190598d85d556a793006a24691bc7e20acc46
+            GIT_TAG 5140d1d976c7060f7aded4dbe294f97c3d8382ea
             )
 endif()
