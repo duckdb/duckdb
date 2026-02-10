duckdb/duckdb

commit 3c8e32f1b033f52983cdda702554b1f360a01020

shredding option parsing

extension/parquet/parquet_extension.cpp
@@ -12,6 +12,7 @@
 #include "parquet_metadata.hpp"
 #include "parquet_reader.hpp"
 #include "parquet_writer.hpp"
+#include "parquet_shredding.hpp"
 #include "reader/struct_column_reader.hpp"
 #include "zstd_file_system.hpp"
 #include "writer/primitive_column_writer.hpp"
@@ -83,6 +84,7 @@ struct ParquetWriteBindData : public TableFunctionData {
 	optional_idx row_groups_per_file;
 
 	ChildFieldIDs field_ids;
+	ShreddingType shredding_types;
 	//! The compression level, higher value is more
 	int64_t compression_level = ZStdFileSystem::DefaultCompressionLevel();
 
@@ -211,7 +213,47 @@ static unique_ptr<FunctionData> ParquetWriteBind(ClientContext &context, CopyFun
 			    StringUtil::Lower(StringValue::Get(option.second[0])) == "auto") {
 				throw NotImplementedException("The 'auto' option is not yet implemented for 'shredding'");
 			} else {
-				throw NotImplementedException("Shredding");
+				case_insensitive_set_t variant_names;
+				for (idx_t col_idx = 0; col_idx < names.size(); col_idx++) {
+					if (sql_types[col_idx].id() != LogicalTypeId::VARIANT) {
+						continue;
+					}
+					variant_names.emplace(names[col_idx]);
+				}
+				auto &shredding_types_value = option.second[0];
+				if (shredding_types_value.type().id() != LogicalTypeId::STRUCT) {
+					BinderException("SHREDDING value should be a STRUCT of column names to types, i.e: {col1: "
+					                "'INTEGER[]', col2: 'BOOLEAN'}");
+				}
+				const auto &struct_type = shredding_types_value.type();
+				const auto &struct_children = StructValue::GetChildren(shredding_types_value);
+				D_ASSERT(StructType::GetChildTypes(struct_type).size() == struct_children.size());
+				for (idx_t i = 0; i < struct_children.size(); i++) {
+					const auto &col_name = StringUtil::Lower(StructType::GetChildName(struct_type, i));
+					auto it = variant_names.find(col_name);
+					if (it == variant_names.end()) {
+						string names;
+						for (const auto &entry : variant_names) {
+							if (!names.empty()) {
+								names += ", ";
+							}
+							names += entry;
+						}
+						if (names.empty()) {
+							throw BinderException("VARIANT by name \"%s\" specified in SHREDDING not found. There are "
+							                      "no VARIANT columns present.",
+							                      col_name);
+						} else {
+							throw BinderException(
+							    "VARIANT by name \"%s\" specified in SHREDDING not found. Consider using "
+							    "WRITE_PARTITION_COLUMNS if this "
+							    "column is a partition column. Available names of VARIANT columns: [%s]",
+							    col_name, names);
+						}
+					}
+					const auto &child_value = struct_children[i];
+					bind_data->shredding_types.children[col_name] = ShreddingType::GetShreddingTypes(child_value);
+				}
 			}
 		} else if (loption == "kv_metadata") {
 			auto &kv_struct = option.second[0];

extension/parquet/CMakeLists.txt
@@ -25,6 +25,7 @@ set(PARQUET_EXTENSION_FILES
     parquet_statistics.cpp
     parquet_timestamp.cpp
     parquet_writer.cpp
+    parquet_shredding.cpp
     serialize_parquet.cpp
     zstd_file_system.cpp
     geo_parquet.cpp)

extension/parquet/include/parquet_shredding.hpp
@@ -1,0 +1,26 @@
+#pragma once
+
+#include "duckdb/common/serializer/buffered_file_writer.hpp"
+#include "duckdb/common/case_insensitive_map.hpp"
+
+namespace duckdb {
+
+struct ShreddingType {
+public:
+	ShreddingType();
+	explicit ShreddingType(LogicalTypeId shredding_type);
+
+public:
+	void Serialize(Serializer &serializer) const;
+	static ShreddingType Deserialize(Deserializer &source);
+
+public:
+	static ShreddingType GetShreddingTypes(const Value &val);
+
+public:
+	bool set = false;
+	LogicalTypeId shredding_type;
+	case_insensitive_map_t<ShreddingType> children;
+};
+
+} // namespace duckdb

extension/parquet/include/parquet_writer.hpp
@@ -44,19 +44,6 @@ struct PreparedRowGroup {
 	vector<unique_ptr<ColumnWriterState>> states;
 };
 
-struct ShreddingType {
-	static constexpr const auto DUCKDB_SHREDDING = "__duckdb_shredding";
-	ShreddingType();
-	explicit ShreddingType(int32_t shredding_type);
-	ShreddingType Copy() const;
-	bool set;
-	LogicalTypeId shredding_type;
-	case_insensitive_map_t<ShreddingType> child_shredding_types;
-
-	void Serialize(Serializer &serializer) const;
-	static ShreddingType Deserialize(Deserializer &source);
-};
-
 struct ParquetBloomFilterEntry {
 	unique_ptr<ParquetBloomFilter> bloom_filter;
 	idx_t row_group_idx;

extension/parquet/parquet_field_id.cpp
@@ -92,7 +92,6 @@ void FieldID::GenerateFieldIDs(ChildFieldIDs &field_ids, idx_t &field_id, const 
 		vector<string> child_names;
 		vector<LogicalType> child_types;
 		GetChildNamesAndTypes(col_type, child_names, child_types);
-
 		GenerateFieldIDs(inserted.first->second.child_field_ids, field_id, child_names, child_types);
 	}
 }

extension/parquet/parquet_shredding.cpp
@@ -1,0 +1,52 @@
+#include "parquet_shredding.hpp"
+#include "duckdb/common/exception/binder_exception.hpp"
+#include "duckdb/common/type_visitor.hpp"
+
+namespace duckdb {
+
+ShreddingType::ShreddingType() : set(false) {
+}
+
+ShreddingType::ShreddingType(LogicalTypeId shredding_type) : set(true), shredding_type(shredding_type) {
+}
+
+static ShreddingType ConvertShreddingTypeRecursive(const LogicalType &type) {
+	if (type.id() == LogicalTypeId::ANY) {
+		return ShreddingType(type.id());
+	}
+	if (!type.IsNested()) {
+		return ShreddingType(type.id());
+	}
+
+	switch (type.id()) {
+	case LogicalTypeId::STRUCT: {
+		ShreddingType res(LogicalTypeId::STRUCT);
+		auto &children = StructType::GetChildTypes(type);
+		for (auto &entry : children) {
+			res.children[entry.first] = ConvertShreddingTypeRecursive(entry.second);
+		}
+		return res;
+	}
+	case LogicalTypeId::LIST: {
+		ShreddingType res(LogicalTypeId::STRUCT);
+		const auto &child = ListType::GetChildType(type);
+		res.children["element"] = ConvertShreddingTypeRecursive(child);
+		return res;
+	}
+	default:
+		break;
+	}
+	throw BinderException("VARIANT can only be shredded on LIST/STRUCT/ANY/non-nested type, not %s", type.ToString());
+}
+
+ShreddingType ShreddingType::GetShreddingTypes(const Value &val) {
+	if (val.type().id() != LogicalTypeId::VARCHAR) {
+		throw BinderException("SHREDDING value should be of type VARCHAR, a stringified type to use for the column");
+	}
+	auto type_str = val.GetValue<string>();
+	auto logical_type = TransformStringToLogicalType(type_str);
+
+	return ConvertShreddingTypeRecursive(logical_type);
+}
+
+} // namespace duckdb

commit f2558de01ad690aa9a57f637ace64fc7e5fe37dd

Add missed throw The object was created but it is not being used. The 'throw' keyword could be missing: throw BinderException(FOO);
Found by PostgresPro with PVS-Studio static analyzer.
Fixes: 3c8e32f1b0 ("shredding option parsing")
Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

extension/parquet/parquet_extension.cpp
@@ -218,8 +218,8 @@ static unique_ptr<FunctionData> ParquetWriteBind(ClientContext &context, CopyFun
 				}
 				auto &shredding_types_value = option.second[0];
 				if (shredding_types_value.type().id() != LogicalTypeId::STRUCT) {
-					BinderException("SHREDDING value should be a STRUCT of column names to types, i.e: {col1: "
-					                "'INTEGER[]', col2: 'BOOLEAN'}");
+					throw BinderException("SHREDDING value should be a STRUCT of column names to types, i.e: {col1: "
+					                      "'INTEGER[]', col2: 'BOOLEAN'}");
 				}
 				const auto &struct_type = shredding_types_value.type();
 				const auto &struct_children = StructValue::GetChildren(shredding_types_value);