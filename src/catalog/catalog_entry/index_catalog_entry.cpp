duckdb/duckdb

commit 75d80d66cbbe9186ced6a8d060fa3467d66ca8b4

Avoid moving data from the CreateIndexInfo after all

src/catalog/catalog_entry/index_catalog_entry.cpp
@@ -3,18 +3,19 @@
 namespace duckdb {
 
 IndexCatalogEntry::IndexCatalogEntry(Catalog &catalog, SchemaCatalogEntry &schema, CreateIndexInfo &info)
-    : StandardEntry(CatalogType::INDEX_ENTRY, schema, catalog, std::move(info.index_name)), sql(std::move(info.sql)),
-      options(std::move(info.options)), index_type(info.index_type), index_constraint_type(info.constraint_type),
-      column_ids(std::move(info.column_ids)) {
+    : StandardEntry(CatalogType::INDEX_ENTRY, schema, catalog, info.index_name), sql(info.sql), options(info.options),
+      index_type(info.index_type), index_constraint_type(info.constraint_type), column_ids(info.column_ids) {
 
 	this->temporary = info.temporary;
-	this->dependencies = std::move(info.dependencies);
-	this->comment = std::move(info.comment);
+	this->dependencies = info.dependencies;
+	this->comment = info.comment;
 	for (auto &expr : expressions) {
-		expressions.push_back(std::move(expr));
+		D_ASSERT(expr);
+		expressions.push_back(expr->Copy());
 	}
 	for (auto &parsed_expr : info.parsed_expressions) {
-		parsed_expressions.push_back(std::move(parsed_expr));
+		D_ASSERT(parsed_expr);
+		parsed_expressions.push_back(parsed_expr->Copy());
 	}
 }
 

src/storage/checkpoint_manager.cpp
@@ -401,7 +401,6 @@ void CheckpointWriter::WriteIndex(IndexCatalogEntry &index_catalog_entry, Serial
 }
 
 void CheckpointReader::ReadIndex(CatalogTransaction transaction, Deserializer &deserializer) {
-
 	// we need to keep the tag "index", even though it is slightly misleading.
 	auto create_info = deserializer.ReadProperty<unique_ptr<CreateInfo>>(100, "index");
 	auto &info = create_info->Cast<CreateIndexInfo>();
@@ -425,27 +424,19 @@ void CheckpointReader::ReadIndex(CatalogTransaction transaction, Deserializer &d
 	}
 
 	// now we can look for the index in the catalog and assign the table info
-	auto &index = catalog.CreateIndex(transaction, info)->Cast<DuckIndexEntry>();
-	auto data_table_info = table.GetStorage().GetDataTableInfo();
-	index.info = make_shared_ptr<IndexDataTableInfo>(data_table_info, info.index_name);
-
-	// insert the parsed expressions into the index so that we can (de)serialize them during consecutive checkpoints
-	for (auto &parsed_expr : info.parsed_expressions) {
-		index.parsed_expressions.push_back(parsed_expr->Copy());
-	}
-	D_ASSERT(!info.parsed_expressions.empty());
+	auto &index = schema.CreateIndex(transaction, info, table)->Cast<DuckIndexEntry>();
 	auto &data_table = table.GetStorage();
 
 	IndexStorageInfo index_storage_info;
 	if (root_block_pointer.IsValid()) {
 		// this code path is necessary to read older duckdb files
-		index_storage_info.name = info.index_name;
+		index_storage_info.name = index.name;
 		index_storage_info.root_block_ptr = root_block_pointer;
 
 	} else {
 		// get the matching index storage info
 		for (auto const &elem : data_table.GetDataTableInfo()->GetIndexStorageInfo()) {
-			if (elem.name == info.index_name) {
+			if (elem.name == index.name) {
 				index_storage_info = elem;
 				break;
 			}

src/storage/wal_replay.cpp
@@ -4,12 +4,17 @@
 #include "duckdb/catalog/catalog_entry/table_catalog_entry.hpp"
 #include "duckdb/catalog/catalog_entry/type_catalog_entry.hpp"
 #include "duckdb/catalog/catalog_entry/view_catalog_entry.hpp"
+#include "duckdb/common/checksum.hpp"
 #include "duckdb/common/printer.hpp"
 #include "duckdb/common/serializer/binary_deserializer.hpp"
 #include "duckdb/common/serializer/buffered_file_reader.hpp"
+#include "duckdb/common/serializer/memory_stream.hpp"
 #include "duckdb/common/string_util.hpp"
+#include "duckdb/execution/index/art/art.hpp"
+#include "duckdb/execution/index/index_type_set.hpp"
 #include "duckdb/main/attached_database.hpp"
 #include "duckdb/main/client_context.hpp"
+#include "duckdb/main/config.hpp"
 #include "duckdb/main/connection.hpp"
 #include "duckdb/main/database.hpp"
 #include "duckdb/parser/parsed_data/alter_table_info.hpp"
@@ -20,13 +25,8 @@
 #include "duckdb/planner/expression_binder/index_binder.hpp"
 #include "duckdb/planner/parsed_data/bound_create_table_info.hpp"
 #include "duckdb/storage/storage_manager.hpp"
-#include "duckdb/storage/write_ahead_log.hpp"
-#include "duckdb/common/serializer/memory_stream.hpp"
-#include "duckdb/common/checksum.hpp"
-#include "duckdb/execution/index/index_type_set.hpp"
-#include "duckdb/execution/index/art/art.hpp"
-#include "duckdb/main/config.hpp"
 #include "duckdb/storage/table/delete_state.hpp"
+#include "duckdb/storage/write_ahead_log.hpp"
 #include "duckdb/transaction/meta_transaction.hpp"
 
 namespace duckdb {
@@ -581,20 +581,7 @@ void WriteAheadLogDeserializer::ReplayCreateIndex() {
 
 	// create the index in the catalog
 	auto &table = catalog.GetEntry<TableCatalogEntry>(context, create_info->schema, info.table).Cast<DuckTableEntry>();
-	auto &index = catalog.CreateIndex(context, info)->Cast<DuckIndexEntry>();
-	index.info = make_shared_ptr<IndexDataTableInfo>(table.GetStorage().GetDataTableInfo(), index.name);
-
-	// insert the parsed expressions into the index so that we can (de)serialize them during consecutive checkpoints
-	for (auto &parsed_expr : info.parsed_expressions) {
-		index.parsed_expressions.push_back(parsed_expr->Copy());
-	}
-
-	// obtain the parsed expressions of the ART from the index metadata
-	vector<unique_ptr<ParsedExpression>> parsed_expressions;
-	for (auto &parsed_expr : info.parsed_expressions) {
-		parsed_expressions.push_back(parsed_expr->Copy());
-	}
-	D_ASSERT(!parsed_expressions.empty());
+	auto &index = table.schema.CreateIndex(context, info, table)->Cast<DuckIndexEntry>();
 
 	// add the table to the bind context to bind the parsed expressions
 	auto binder = Binder::CreateBinder(context);
@@ -612,9 +599,10 @@ void WriteAheadLogDeserializer::ReplayCreateIndex() {
 
 	// bind the parsed expressions to create unbound expressions
 	vector<unique_ptr<Expression>> unbound_expressions;
-	unbound_expressions.reserve(parsed_expressions.size());
-	for (auto &expr : parsed_expressions) {
-		unbound_expressions.push_back(idx_binder.Bind(expr));
+	unbound_expressions.reserve(index.parsed_expressions.size());
+	for (auto &expr : index.parsed_expressions) {
+		auto copy = expr->Copy();
+		unbound_expressions.push_back(idx_binder.Bind(copy));
 	}
 
 	auto &data_table = table.GetStorage();

commit 87143fe7749087c80aad1b7a32a477ec69a9d774

IndexCatalogEntry: fix typo
Found by PostgresPro.
Fixes: 75d80d66cb ("Avoid moving data from the CreateIndexInfo after all")
Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

src/catalog/catalog_entry/index_catalog_entry.cpp
@@ -8,7 +8,7 @@ IndexCatalogEntry::IndexCatalogEntry(Catalog &catalog, SchemaCatalogEntry &schem
 	this->temporary = info.temporary;
 	this->dependencies = info.dependencies;
 	this->comment = info.comment;
-	for (auto &expr : expressions) {
+	for (auto &expr : info.expressions) {
 		D_ASSERT(expr);
 		expressions.push_back(expr->Copy());
 	}