duckdb/duckdb

commit c1e3416ad904855c925b0ecc8a16b2f958e9bf77

fix crash in deprecated c api code

src/main/capi/result-c.cpp
@@ -146,7 +146,18 @@ duckdb_state deprecated_duckdb_translate_column(MaterializedQueryResult &result,
 	auto &collection = result.Collection();
 	idx_t row_count = collection.Count();
 	column->__deprecated_nullmask = (bool *)duckdb_malloc(sizeof(bool) * collection.Count());
-	column->__deprecated_data = duckdb_malloc(GetCTypeSize(column->__deprecated_type) * row_count);
+
+	auto type_size = GetCTypeSize(column->__deprecated_type);
+	if (type_size == 0) {
+		for (idx_t row_id = 0; row_id < row_count; row_id++) {
+			column->__deprecated_nullmask[row_id] = false;
+		}
+		// Unsupported type, e.g., a LIST. By returning DuckDBSuccess here,
+		// we allow filling other columns, and return NULL for all unsupported types.
+		return DuckDBSuccess;
+	}
+
+	column->__deprecated_data = duckdb_malloc(type_size * row_count);
 	if (!column->__deprecated_nullmask || !column->__deprecated_data) { // LCOV_EXCL_START
 		// malloc failure
 		return DuckDBError;
@@ -295,7 +306,7 @@ duckdb_state DuckDBTranslateResult(unique_ptr<QueryResult> result_p, duckdb_resu
 		return DuckDBError;
 	}
 	// copy the data
-	// first write the meta data
+	// first write the metadata
 	out->__deprecated_column_count = result.ColumnCount();
 	out->__deprecated_rows_changed = 0;
 	return DuckDBSuccess;
@@ -329,6 +340,7 @@ bool DeprecatedMaterializeResult(duckdb_result *result) {
 		// malloc failure
 		return DuckDBError;
 	} // LCOV_EXCL_STOP
+
 	if (result_data->result->type == QueryResultType::STREAM_RESULT) {
 		// if we are dealing with a stream result, convert it to a materialized result first
 		auto &stream_result = (StreamQueryResult &)*result_data->result;
@@ -344,6 +356,7 @@ bool DeprecatedMaterializeResult(duckdb_result *result) {
 		result->__deprecated_columns[i].__deprecated_type = ConvertCPPTypeToC(result_data->result->types[i]);
 		result->__deprecated_columns[i].__deprecated_name = (char *)result_data->result->names[i].c_str(); // NOLINT
 	}
+
 	result->__deprecated_row_count = materialized.RowCount();
 	if (result->__deprecated_row_count > 0 &&
 	    materialized.properties.return_type == StatementReturnType::CHANGED_ROWS) {
@@ -353,7 +366,8 @@ bool DeprecatedMaterializeResult(duckdb_result *result) {
 			result->__deprecated_rows_changed = NumericCast<idx_t>(row_changes.GetValue<int64_t>());
 		}
 	}
-	// now write the data
+
+	// Now write the data and skip any unsupported columns.
 	for (idx_t col = 0; col < column_count; col++) {
 		auto state = deprecated_duckdb_translate_column(materialized, &result->__deprecated_columns[col], col);
 		if (state != DuckDBSuccess) {

src/include/duckdb.h
@@ -934,7 +934,8 @@ converted. The result must be freed with `duckdb_free`.
 DUCKDB_API char *duckdb_value_varchar(duckdb_result *result, idx_t col, idx_t row);
 
 /*!
- * returns: The string value at the specified location.
+ * returns: The string value at the specified location. Attempts to cast the result values to string.
+ * No support for nested types, and for other complex types.
  * The resulting field "string.data" must be freed with `duckdb_free.`
  */
 DUCKDB_API duckdb_string duckdb_value_string(duckdb_result *result, idx_t col, idx_t row);

src/include/duckdb/main/capi/cast/generic.hpp
@@ -65,8 +65,8 @@ RESULT_TYPE GetInternalCValue(duckdb_result *result, idx_t col, idx_t row) {
 	case DUCKDB_TYPE_BLOB:
 		return TryCastCInternal<duckdb_blob, RESULT_TYPE, FromCBlobCastWrapper>(result, col, row);
 	default: { // LCOV_EXCL_START
-		// invalid type for C to C++ conversion
-		D_ASSERT(0);
+		// Invalid type for C to C++ conversion. Internally, we set the null mask to NULL.
+		// This is a deprecated code path. Use the Vector Interface for nested and complex types.
 		return FetchDefaultValue::Operation<RESULT_TYPE>();
 	} // LCOV_EXCL_STOP
 	}

src/main/capi/helper-c.cpp
@@ -180,9 +180,9 @@ idx_t GetCTypeSize(duckdb_type type) {
 	case DUCKDB_TYPE_DECIMAL:
 		return sizeof(duckdb_hugeint);
 	default: // LCOV_EXCL_START
-		// unsupported type
-		D_ASSERT(0);
-		return sizeof(const char *);
+		// Unsupported nested or complex type. Internally, we set the null mask to NULL.
+		// This is a deprecated code path. Use the Vector Interface for nested and complex types.
+		return 0;
 	} // LCOV_EXCL_STOP
 }
 

test/api/capi/test_capi.cpp
@@ -703,3 +703,29 @@ TEST_CASE("Test custom_user_agent config", "[capi]") {
 		duckdb_close(&db);
 	}
 }
+
+TEST_CASE("Test unsupported types in the deprecated C API", "[capi]") {
+	CAPITester tester;
+	REQUIRE(tester.OpenDatabase(nullptr));
+
+	string query_1 = R"EOF(
+		CREATE TABLE test(
+			id BIGINT,
+			one DECIMAL(18,3)[]
+		);
+	)EOF";
+	string query_2 = "INSERT INTO test VALUES (410, '[]');";
+	string query_3 = "INSERT INTO test VALUES (412, '[]');";
+	string query_4 = "SELECT id, one FROM test;";
+	REQUIRE_NO_FAIL(tester.Query(query_1));
+	REQUIRE_NO_FAIL(tester.Query(query_2));
+	REQUIRE_NO_FAIL(tester.Query(query_3));
+
+	// Passes, but does return invalid data for unsupported types.
+	auto result = tester.Query(query_4);
+	auto &result_c = result->InternalResult();
+	REQUIRE(!string(duckdb_value_string(&result_c, 0, 0).data).compare("410"));
+	REQUIRE(duckdb_value_string(&result_c, 1, 0).data == nullptr);
+	REQUIRE(!string(duckdb_value_string(&result_c, 0, 1).data).compare("412"));
+	REQUIRE(duckdb_value_string(&result_c, 1, 1).data == nullptr);
+}

commit 56a85852d4519de29d2726948c9d92c028c511b0

c-api: avoid NULL dereference Replace alloc check before the pointer usage. Fixes: c1e3416ad9 ("fix crash in deprecated c api code") Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

src/main/capi/result-c.cpp
@@ -147,6 +147,9 @@ duckdb_state deprecated_duckdb_translate_column(MaterializedQueryResult &result,
 	auto &collection = result.Collection();
 	idx_t row_count = collection.Count();
 	column->deprecated_nullmask = (bool *)duckdb_malloc(sizeof(bool) * collection.Count());
+	if (!column->deprecated_nullmask) {
+		return DuckDBError;
+	}
 
 	auto type_size = GetCTypeSize(column->deprecated_type);
 	if (type_size == 0) {
@@ -159,7 +162,7 @@ duckdb_state deprecated_duckdb_translate_column(MaterializedQueryResult &result,
 	}
 
 	column->deprecated_data = duckdb_malloc(type_size * row_count);
-	if (!column->deprecated_nullmask || !column->deprecated_data) { // LCOV_EXCL_START
+	if (!column->deprecated_data) { // LCOV_EXCL_START
 		// malloc failure
 		return DuckDBError;
 	} // LCOV_EXCL_STOP