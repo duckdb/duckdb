73bbec353b75e3e0467c52e2d8f1a3f4403ebb4a
Sam Ansmink <samansmink@hotmail.com>
2026-03-03 13:00:03 +0100

    add submodule

--- .github/config/extensions/lance.cmake
+++ .github/config/extensions/lance.cmake
@@ -2,6 +2,7 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(lance
             GIT_URL https://github.com/lance-format/lance-duckdb
             GIT_TAG 892b224af0f4a5508e5f3c46f1b8292155880258
+            SUBMODULES extension-ci-tools
             LOAD_TESTS
     )
 endif()
