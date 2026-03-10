7807707bf4f8df74d4708959250ac66febc6ac15
Sam Ansmink <samansmink@hotmail.com>
2026-03-10 17:33:28 +0100

    bump lance to fix protoc issue

--- .github/config/extensions/lance.cmake
+++ .github/config/extensions/lance.cmake
@@ -1,7 +1,7 @@
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(lance
             GIT_URL https://github.com/lance-format/lance-duckdb
-            GIT_TAG 3633381331446328924e8a1d75d8bbfec960952e
+            GIT_TAG c62e9322d6847d1e3fca709d91e9286b60519fe2
             SUBMODULES extension-ci-tools
             LOAD_TESTS
     )
