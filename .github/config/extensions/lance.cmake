049d01c620be6ff46e0ee99c5b64e6ff51c75015
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-03-16 17:57:41 +0100

    [lance] Bump to adfa8f5601d1de9fbb85f1167c3859bacc775e4d

--- .github/config/extensions/lance.cmake
+++ .github/config/extensions/lance.cmake
@@ -1,7 +1,7 @@
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(lance
             GIT_URL https://github.com/lance-format/lance-duckdb
-            GIT_TAG c62e9322d6847d1e3fca709d91e9286b60519fe2
+            GIT_TAG adfa8f5601d1de9fbb85f1167c3859bacc775e4d
             SUBMODULES extension-ci-tools
             LOAD_TESTS
             DONT_LINK
