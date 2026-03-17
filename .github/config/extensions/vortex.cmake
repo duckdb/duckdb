8631bc56994b55a1ef69148ef116c259ef6e540c
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-03-17 17:22:00 +0100

    [vortex] Bump to d0b5bae4d907694e776c97b57de6edfb662c6c25

--- .github/config/extensions/vortex.cmake
+++ .github/config/extensions/vortex.cmake
@@ -1,7 +1,7 @@
 if (NOT WIN32 AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
     duckdb_extension_load(vortex
             GIT_URL https://github.com/vortex-data/duckdb-vortex
-            GIT_TAG c5d418c1779f3cb5226cddccd3850502dd6801fc
+            GIT_TAG d0b5bae4d907694e776c97b57de6edfb662c6c25
             SUBMODULES vortex
             APPLY_PATCHES
             LOAD_TESTS
