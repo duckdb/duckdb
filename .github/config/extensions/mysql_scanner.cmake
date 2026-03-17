7e4b8d478b0a88f8ac429e17816b3df1dcc2dbcf
Alex Kasko <alex@staticlibs.net>
2026-03-17 19:05:26 +0000

    Bump Postgres and MySQL

--- .github/config/extensions/mysql_scanner.cmake
+++ .github/config/extensions/mysql_scanner.cmake
@@ -3,6 +3,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
             DONT_LINK
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-mysql
-            GIT_TAG 372a86f96f4735ed3a1c3bb2e50594a2d73bc143
+            GIT_TAG d20d7fa20c5a2c951d1572895bac017aac0b80b0
             )
 endif()
