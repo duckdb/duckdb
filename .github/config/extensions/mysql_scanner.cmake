f0a2485d2c33f48b29bdf71ee392682d7d577838
Alex Kasko <alex@staticlibs.net>
2026-03-18 10:09:42 +0000

    Update MySQL with require fix in tests

--- .github/config/extensions/mysql_scanner.cmake
+++ .github/config/extensions/mysql_scanner.cmake
@@ -3,6 +3,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
             DONT_LINK
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-mysql
-            GIT_TAG d20d7fa20c5a2c951d1572895bac017aac0b80b0
+            GIT_TAG c79812602d1c4d09e57d75dac19266a0a0a4c4f4
             )
 endif()
