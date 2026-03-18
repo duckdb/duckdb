87b67f5f74ba89e31e6320999ee0bc11a45d8c76
Laurens Kuiper <laurens.kuiper@cwi.nl>
2026-03-18 11:28:12 +0100

    disable vortex on main for now

--- .github/config/extensions/vortex.cmake
+++ .github/config/extensions/vortex.cmake
@@ -1,4 +1,5 @@
-if (NOT WIN32 AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
+# FIXME: disabled for now while CopyFunction undergoes heavy changes
+if (FALSE AND NOT WIN32 AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
     duckdb_extension_load(vortex
             GIT_URL https://github.com/vortex-data/duckdb-vortex
             GIT_TAG c5d418c1779f3cb5226cddccd3850502dd6801fc
