41f94a8509a8fefbf38bd68ecd05c04d35923185
Mytherin <mark.raasveldt@gmail.com>
2026-03-20 15:16:28 +0100

    Format patches

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -3,6 +3,6 @@ if (NOT MINGW)
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-avro
             GIT_TAG 4fa0f73f816e9878d5b6a39795e060877766ac1a
-    APPLY_PATCHES
+            APPLY_PATCHES
     )
 endif()
