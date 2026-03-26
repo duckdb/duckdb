700bda0ba5ce178dd3a612dfa7ace6db0134357b
Mytherin <mark.raasveldt@gmail.com>
2026-03-26 17:31:47 +0100

    More fixes

--- src/common/arrow/appender/bool_data.cpp
+++ src/common/arrow/appender/bool_data.cpp
@@ -10,14 +10,12 @@ void ArrowBoolData::Initialize(ArrowAppendData &result, const LogicalType &type,
 
 void ArrowBoolData::Append(ArrowAppendData &append_data, Vector &input, idx_t from, idx_t to, idx_t input_size) {
 	idx_t size = to - from;
-	UnifiedVectorFormat format;
-	input.ToUnifiedFormat(input_size, format);
 	auto &main_buffer = append_data.GetMainBuffer();
 	auto &validity_buffer = append_data.GetValidityBuffer();
 	// we initialize both the validity and the bit set to 1's
 	ArrowAppendData::ResizeValidity(validity_buffer, append_data.row_count + size);
 	ArrowAppendData::ResizeValidity(main_buffer, append_data.row_count + size);
-	auto data = UnifiedVectorFormat::GetData<bool>(format);
+	auto data = input.ScanAllValues<bool>(input_size);
 
 	auto result_data = main_buffer.GetData<uint8_t>();
 	auto validity_data = validity_buffer.GetData<uint8_t>();
@@ -25,11 +23,11 @@ void ArrowBoolData::Append(ArrowAppendData &append_data, Vector &input, idx_t fr
 	idx_t current_byte;
 	ArrowAppendData::GetBitPosition(append_data.row_count, current_byte, current_bit);
 	for (idx_t i = from; i < to; i++) {
-		auto source_idx = format.sel->get_index(i);
+		auto entry = data[i];
 		// append the validity mask
-		if (!format.validity.RowIsValid(source_idx)) {
+		if (!entry.is_valid) {
 			append_data.SetNull(validity_data, current_byte, current_bit);
-		} else if (!data[source_idx]) {
+		} else if (!entry.value) {
 			ArrowAppendData::UnsetBit(result_data, current_byte, current_bit);
 		}
 		ArrowAppendData::NextBit(current_byte, current_bit);
