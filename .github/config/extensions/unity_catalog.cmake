247c9f00702cd4892ebc6edd11bd3ddfc2f0cbbd
ben fleis <benfleis@users.noreply.github.com>
2026-03-26 12:53:46 +0100

    bump delta and unity_catalog ext refs in v1.5-variegata

--- .github/config/extensions/unity_catalog.cmake
+++ .github/config/extensions/unity_catalog.cmake
@@ -1,7 +1,7 @@
-if (NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
+if(NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
   duckdb_extension_load(unity_catalog
             GIT_URL https://github.com/duckdb/unity_catalog
-            GIT_TAG c87dfd2c2ec6a5ab2fb42a8f902124065834ff43
+            GIT_TAG 02024095fe6eeed6f53e78dce43e83c7424abec2
             LOAD_TESTS
   )
 endif()
