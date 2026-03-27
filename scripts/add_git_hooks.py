c2888bd221408e2ea2c40d7a225790c0d9b638ea
Yan <60644013+yan-alex@users.noreply.github.com>
2026-03-27 14:48:05 +0100

    add_git_hooks.py

--- scripts/add_git_hooks.py
+++ scripts/add_git_hooks.py
@@ -6,13 +6,17 @@ import os
 HOOK_MARKER = '# make format-fix hook #'
 HOOK_PATH = '.git/hooks/pre-commit'
 
-HOOK_BODY = """\
+
+FORMAT_SCRIPT = 'duckdb/scripts/format.py' if os.path.isfile('duckdb/scripts/format.py') else 'scripts/format.py' # this is to support extension repos
+FORMAT_CMD = f'python3 {FORMAT_SCRIPT} {{}} --fix --noconfirm'
+
+HOOK_BODY = f"""\
 
 # make format-fix hook #
 # Get list of staged files
 staged_files=$(git diff --cached --name-only)
 # Run formatter on staged files
-echo "$staged_files" | xargs -P 0 -I {} python3 scripts/format.py {} --fix --noconfirm
+echo "$staged_files" | xargs -P 0 -I {{}} {FORMAT_CMD}
 # Re-stage files that were:
 # 1. Originally staged
 # 2. Modified by the formatter
@@ -31,9 +35,7 @@ def main():
         if HOOK_MARKER in content:
             print("Hook already exists, no action needed. exiting.")
         else:
-            print(
-                ".git/hooks/pre-commit file exists but does not contain make `make format-fix` functionality for staged files"
-            )
+            print(".git/hooks/pre-commit file exists but does not contain make `make format-fix` functionality for staged files")
             print("Adding `make format-fix` functionality for staged files")
             with open(HOOK_PATH, 'a') as f:
                 f.write(HOOK_BODY)
@@ -41,7 +43,7 @@ def main():
         print("Creating .git/hooks/pre-commit file with `make format-fix` functionality for staged files")
         with open(HOOK_PATH, 'w') as f:
             f.write('#!/bin/sh\n' + HOOK_BODY)
-        os.chmod(HOOK_PATH, 0o755)  # make it executable
+        os.chmod(HOOK_PATH, 0o755)
     print("Done! ✅")
 
 
