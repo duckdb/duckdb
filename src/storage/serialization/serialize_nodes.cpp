798b11b8976bf5ed92b180905a78290a26efe5cb
Denis Hirn <denis.hirn@uni-tuebingen.de>
2026-03-30 21:01:06 +0200

    Revert generic_filters De/Serialization

--- src/storage/serialization/serialize_nodes.cpp
+++ src/storage/serialization/serialize_nodes.cpp
@@ -724,13 +724,11 @@ TableColumn TableColumn::Deserialize(Deserializer &deserializer) {
 
 void TableFilterSet::Serialize(Serializer &serializer) const {
 	serializer.WritePropertyWithDefault<map<ProjectionIndex, unique_ptr<TableFilter>>>(100, "filters", GetTableFiltersForSerialization(serializer));
-	serializer.WritePropertyWithDefault<vector<unique_ptr<Expression>>>(101, "generic_filters", generic_filters);
 }
 
 TableFilterSet TableFilterSet::Deserialize(Deserializer &deserializer) {
 	TableFilterSet result;
 	deserializer.ReadPropertyWithDefault<map<ProjectionIndex, unique_ptr<TableFilter>>>(100, "filters", result.GetTableFiltersForDeserialization(deserializer));
-	deserializer.ReadPropertyWithDefault<vector<unique_ptr<Expression>>>(101, "generic_filters", result.generic_filters);
 	return result;
 }
 
