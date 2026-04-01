6f33780678dcc09b6e4b8ab3c61fd1f4643eccc2
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-04-01 13:47:46 +0200

    Remove unused catalog and file system includes

--- src/common/file_system.cpp
+++ src/common/file_system.cpp
@@ -1,13 +1,11 @@
 #include "duckdb/common/file_system.hpp"
 
-#include "duckdb/common/checksum.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/file_opener.hpp"
 #include "duckdb/common/helper.hpp"
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/common/multi_file/multi_file_list.hpp"
 #include "duckdb/common/windows.hpp"
-#include "duckdb/function/scalar/string_functions.hpp"
 #include "duckdb/logging/log_type.hpp"
 #include "duckdb/main/client_context.hpp"
 #include "duckdb/main/client_data.hpp"
