e8e9556f538cd003d1a154a8a0d2d5a3b622f41a
dentinyhao <dentinyhao@gmail.com>
2026-04-02 10:22:38 -0700

    use duckdb::mutex

--- src/include/duckdb/common/adbc/wrappers.hpp
+++ src/include/duckdb/common/adbc/wrappers.hpp
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "duckdb.h"
+#include "duckdb/common/mutex.hpp"
 #include "duckdb/common/string.hpp"
 #include "duckdb/common/unordered_map.hpp"
 #include "duckdb/common/vector.hpp"
@@ -22,6 +23,7 @@ namespace duckdb {
 struct DuckDBAdbcConnectionWrapper {
 	duckdb_connection connection;
 	unordered_map<string, string> options;
+	mutex stream_mutex;
 	//! Active stream wrappers on this connection (for materialization on concurrent execute)
 	vector<duckdb_adbc::DuckDBAdbcStreamWrapper *> active_streams;
 };
