73e9703a5270f8194a18155cef1825c1cacaecb4
Mytherin <mark.raasveldt@gmail.com>
2026-04-04 16:08:25 +0200

    Fix order

--- src/common/vector/string_vector.cpp
+++ src/common/vector/string_vector.cpp
@@ -4,8 +4,8 @@
 namespace duckdb {
 
 FlatVector::FlatStringWriter::FlatStringWriter(Vector &vector, idx_t count)
-    : vector(vector), data(FlatVector::GetData<string_t>(vector)), validity(FlatVector::Validity(vector)), count(count),
-      heap(StringVector::GetStringHeap(vector)) {
+    : vector(vector), data(FlatVector::GetData<string_t>(vector)), validity(FlatVector::Validity(vector)),
+      heap(StringVector::GetStringHeap(vector)), count(count), {
 }
 
 VectorStringBuffer::VectorStringBuffer() : StandardVectorBuffer(idx_t(0)) {
