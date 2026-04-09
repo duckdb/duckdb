d3ca0709dc3ff97cc2f7c635f030909ea8bc55ba
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-04-09 11:23:01 +0200

    Bump httpfs, now with implemented httpfs_connection_caching

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 56d1cf901d7bdbce788c132fba4d8976696591e8
+    GIT_TAG 13e18b3c9f3810334f5972b76a3acc247b28e537
 )
