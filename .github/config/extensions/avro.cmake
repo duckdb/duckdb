b334c1b362ac348abbf98a222b82357859f1e76a
Tishj <t_b@live.nl>
2026-04-09 09:43:02 +0200

    bump avro hash

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW)
     duckdb_extension_load(avro
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-avro
-            GIT_TAG fa61c8e2bfa5b173749bf9db4fc853dea78969b6
+            GIT_TAG a73b60d629a92146cfd71c210936144a971783bd
     )
 endif()
