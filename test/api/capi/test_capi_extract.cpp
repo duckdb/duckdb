64c6c9fc4f0646d84e103bdb01c3ef3238212038
Yan <60644013+yan-alex@users.noreply.github.com>
2026-04-13 13:33:53 +0200

    typo

--- test/api/capi/test_capi_extract.cpp
+++ test/api/capi/test_capi_extract.cpp
@@ -86,7 +86,7 @@ TEST_CASE("Test invalid PRAGMA in C API", "[capi]") {
 	duckdb_close(&db);
 }
 
-TEST_CASE("Test extract statements for `ALTER TABLE ... ADD COLUMN... DEFAULT` in C API", "[capi]") {
+TEST_CASE("Test extract statements for `ALTER TABLE ... ADD COLUMN ... DEFAULT` in C API", "[capi]") {
 	CAPITester tester;
 	duckdb_result res;
 	duckdb_extracted_statements stmts = nullptr;
