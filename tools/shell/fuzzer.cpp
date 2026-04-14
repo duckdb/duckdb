08995eee5d18c80bd88077449642f7ca84b15860
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-04-14 16:02:07 +0200

    Fix use of undeclared identifier 'read' for fuzzer on Linux

--- tools/shell/fuzzer.cpp
+++ tools/shell/fuzzer.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#if (!defined(_WIN32) && !defined(WIN32)) || defined(__MINGW32__)
+#include <unistd.h>
+#endif
 
 #include "shell_fuzzer.hpp"
 #include "shell_state.hpp"
