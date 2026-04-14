e930b280b352038b965090fd8d7f9931172737df
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-04-14 10:54:23 +0200

    Revert "Invalid UTF-8 should not crash box rendering"
    This reverts commit fbfd52b7c72a4eda3d143b081cdf32a291656bc1.

--- src/common/box_renderer.cpp
+++ src/common/box_renderer.cpp
@@ -891,8 +891,7 @@ vector<RenderDataCollection> BoxRendererImplementation::PivotCollections(vector<
 	return result;
 }
 
-string BoxRendererImplementation::ConvertRenderValue(const string &raw_input) {
-	auto input = Utf8Proc::RemoveInvalid(raw_input.c_str(), raw_input.size());
+string BoxRendererImplementation::ConvertRenderValue(const string &input) {
 	string result;
 	result.reserve(input.size());
 	for (idx_t c = 0; c < input.size(); c++) {
