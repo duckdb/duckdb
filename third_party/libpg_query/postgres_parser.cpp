63b2cda16f35182ccb87d103046b3eca4bf6a764
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-04-14 12:55:39 +0200

    Move utf8 validation back to Parser::ParseQuery

--- third_party/libpg_query/postgres_parser.cpp
+++ third_party/libpg_query/postgres_parser.cpp
@@ -4,36 +4,13 @@
 #include "parser/parser.hpp"
 #include "parser/scansup.hpp"
 #include "common/keywords.hpp"
-#include "utf8proc_wrapper.hpp"
-
-#include <limits>
 
 namespace duckdb {
 
 PostgresParser::PostgresParser() : success(false), parse_tree(nullptr), error_message(""), error_location(0) {}
 
 void PostgresParser::Parse(const std::string &query) {
-	success = false;
-	parse_tree = nullptr;
-	error_message = "";
-	error_location = 0;
 	duckdb_libpgquery::pg_parser_init();
-
-	UnicodeInvalidReason reason = UnicodeInvalidReason::INVALID_UNICODE;
-	size_t invalid_pos = 0;
-	auto unicode_type = Utf8Proc::Analyze(query.c_str(), query.size(), &reason, &invalid_pos);
-	if (unicode_type == UnicodeType::INVALID) {
-		const char *reason_str = reason == UnicodeInvalidReason::BYTE_MISMATCH ? "byte sequence mismatch"
-		                                                                        : "invalid unicode";
-		error_message = std::string("Invalid UTF-8 in query (") + reason_str + ")";
-		if (invalid_pos >= static_cast<size_t>(std::numeric_limits<int>::max() - 1)) {
-			error_location = std::numeric_limits<int>::max();
-		} else {
-			error_location = static_cast<int>(invalid_pos) + 1;
-		}
-		return;
-	}
-
 	duckdb_libpgquery::parse_result res;
 	pg_parser_parse(query.c_str(), &res);
 	success = res.success;
