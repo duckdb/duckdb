9a5829c97e852bd2aba854cf8503f53977ad9e47
Denis Hirn <denis.hirn@uni-tuebingen.de>
2026-04-14 15:26:18 +0200

    Use PushDownJoin

--- src/planner/subquery/flatten_dependent_join.cpp
+++ src/planner/subquery/flatten_dependent_join.cpp
@@ -1069,6 +1069,11 @@ FlattenDependentJoins::PushDownResult FlattenDependentJoins::PushDownLimit(uniqu
 	case LogicalOperatorType::LOGICAL_CROSS_PRODUCT: {
 		return PushDownCrossProduct(std::move(plan), parent_propagate_null_values, lateral_depth, std::move(state));
 	}
+	case LogicalOperatorType::LOGICAL_ANY_JOIN:
+	case LogicalOperatorType::LOGICAL_ASOF_JOIN:
+	case LogicalOperatorType::LOGICAL_COMPARISON_JOIN: {
+		return PushDownJoin(std::move(plan), parent_propagate_null_values, lateral_depth, std::move(state));
+	}
 	}
 	case LogicalOperatorType::LOGICAL_DELIM_JOIN: {
 		throw BinderException("Nested lateral joins or lateral joins in correlated subqueries are not (yet) supported");
