00fe80f025a0a051eba681706faa1ed5fa21ef44
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-04-17 10:52:24 +0200

    Prefer afl-clang-fast/++ in CI

--- scripts/ci/afl_detect_cxx.sh
+++ scripts/ci/afl_detect_cxx.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-if [[ -n "${CI:-}" ]]; then
-	printf 'AFL_CC=%q\nAFL_CXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "afl-clang-fast" "afl-clang-fast++" ""
+if [[ -n "${CC:-}" && -n "${CXX:-}" ]]; then
+	AFL_LTO_CMAKE_VAR="${AFL_LTO_CMAKE_VAR:-}"
+	if [[ "${CC}" == "afl-clang-lto" && "${CXX}" == "afl-clang-lto++" && -z "${AFL_LTO_CMAKE_VAR}" ]]; then
+		AFL_LTO_CMAKE_VAR="-DCMAKE_LTO=full"
+	fi
+	printf 'CC=%q\nCXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "${CC}" "${CXX}" "${AFL_LTO_CMAKE_VAR}"
 	exit 0
 fi
 
-if [[ -n "${AFL_CC:-}" && -n "${AFL_CXX:-}" ]]; then
-	AFL_LTO_CMAKE_VAR="${AFL_LTO_CMAKE_VAR:-}"
-	if [[ "${AFL_CC}" == "afl-clang-lto" && "${AFL_CXX}" == "afl-clang-lto++" && -z "${AFL_LTO_CMAKE_VAR}" ]]; then
-		AFL_LTO_CMAKE_VAR="-DCMAKE_LTO=full"
-	fi
-	printf 'AFL_CC=%q\nAFL_CXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "${AFL_CC}" "${AFL_CXX}" "${AFL_LTO_CMAKE_VAR}"
+# Prefer afl-clang-fast in CI
+if [[ -n "${CI:-}" ]]; then
+	printf 'CC=%q\nCXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "afl-clang-fast" "afl-clang-fast++" ""
 	exit 0
 fi
 
 if command -v afl-clang-lto >/dev/null 2>&1 && command -v afl-clang-lto++ >/dev/null 2>&1; then
-	printf 'AFL_CC=%q\nAFL_CXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "afl-clang-lto" "afl-clang-lto++" "-DCMAKE_LTO=full"
+	printf 'CC=%q\nCXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "afl-clang-lto" "afl-clang-lto++" "-DCMAKE_LTO=full"
 	exit 0
 fi
 
 if command -v afl-clang-fast >/dev/null 2>&1 && command -v afl-clang-fast++ >/dev/null 2>&1; then
-	printf 'AFL_CC=%q\nAFL_CXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "afl-clang-fast" "afl-clang-fast++" ""
+	printf 'CC=%q\nCXX=%q\nAFL_LTO_CMAKE_VAR=%q\n' "afl-clang-fast" "afl-clang-fast++" ""
 	exit 0
 fi
 
