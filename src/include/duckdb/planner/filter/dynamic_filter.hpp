4c26273c90b1c714bacb5a5e3132b398b1417871
Denis Hirn <denis.hirn@uni-tuebingen.de>
2026-04-23 15:36:05 +0200

    Remove legacy dynamic filter internals

--- src/include/duckdb/planner/filter/dynamic_filter.hpp
+++ src/include/duckdb/planner/filter/dynamic_filter.hpp
@@ -18,6 +18,7 @@
 namespace duckdb {
 class Expression;
 
+//! Runtime dynamic-filter state shared by internal tablefilter functions.
 struct DynamicFilterData {
 public:
 	DynamicFilterData(ExpressionType comparison_type, Value constant);
@@ -31,24 +32,24 @@ public:
 
 	void SetValue(Value val);
 	void Reset();
+	static bool CompareValue(ExpressionType comparison_type, const Value &constant, const Value &value);
+	static FilterPropagateResult CheckStatistics(BaseStatistics &stats, ExpressionType comparison_type,
+	                                             const Value &constant);
 };
 
-class DynamicFilter : public TableFilter {
+//! DEPRECATED - only preserved for backwards-compatible deserialization and expression conversion
+class LegacyDynamicFilter : public TableFilter {
 public:
-	static constexpr const TableFilterType TYPE = TableFilterType::DYNAMIC_FILTER;
+	static constexpr const TableFilterType TYPE = TableFilterType::LEGACY_DYNAMIC_FILTER;
 
 public:
-	DynamicFilter();
-	explicit DynamicFilter(shared_ptr<DynamicFilterData> filter_data);
+	LegacyDynamicFilter();
+	explicit LegacyDynamicFilter(shared_ptr<DynamicFilterData> filter_data);
 
 	//! The shared, dynamic filter data
 	shared_ptr<DynamicFilterData> filter_data;
 
 public:
-	FilterPropagateResult CheckStatistics(BaseStatistics &stats) const override;
-	string ToString(const string &column_name) const override;
-	bool Equals(const TableFilter &other) const override;
-	unique_ptr<TableFilter> Copy() const override;
 	unique_ptr<Expression> ToExpression(const Expression &column) const override;
 	void Serialize(Serializer &serializer) const override;
 	static unique_ptr<TableFilter> Deserialize(Deserializer &deserializer);
--- src/planner/filter/dynamic_filter.cpp
+++ src/planner/filter/dynamic_filter.cpp
@@ -1,17 +1,18 @@
 #include "duckdb/planner/filter/dynamic_filter.hpp"
-#include "duckdb/planner/expression/bound_constant_expression.hpp"
-#include "duckdb/planner/expression/bound_comparison_expression.hpp"
-#include "duckdb/planner/expression/bound_reference_expression.hpp"
-#include "duckdb/planner/filter/expression_filter.hpp"
+#include "duckdb/planner/filter/table_filter_functions.hpp"
+#include "duckdb/common/enum_util.hpp"
+#include "duckdb/common/value_operations/value_operations.hpp"
 #include "duckdb/storage/statistics/base_statistics.hpp"
+#include "duckdb/storage/statistics/numeric_stats.hpp"
+#include "duckdb/storage/statistics/string_stats.hpp"
 
 namespace duckdb {
 
-DynamicFilter::DynamicFilter() : TableFilter(TableFilterType::DYNAMIC_FILTER) {
+LegacyDynamicFilter::LegacyDynamicFilter() : TableFilter(TableFilterType::LEGACY_DYNAMIC_FILTER) {
 }
 
-DynamicFilter::DynamicFilter(shared_ptr<DynamicFilterData> filter_data_p)
-    : TableFilter(TableFilterType::DYNAMIC_FILTER), filter_data(std::move(filter_data_p)) {
+LegacyDynamicFilter::LegacyDynamicFilter(shared_ptr<DynamicFilterData> filter_data_p)
+    : TableFilter(TableFilterType::LEGACY_DYNAMIC_FILTER), filter_data(std::move(filter_data_p)) {
 }
 
 DynamicFilterData::DynamicFilterData(ExpressionType comparison_type_p, Value constant_p)
@@ -23,46 +24,58 @@ unique_ptr<Expression> DynamicFilterData::ToExpression(const Expression &column)
 	                                         make_uniq<BoundConstantExpression>(constant));
 }
 
-FilterPropagateResult DynamicFilter::CheckStatistics(BaseStatistics &stats) const {
-	if (!filter_data) {
-		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
-	}
-	lock_guard<mutex> l(filter_data->lock);
-	if (!filter_data->initialized) {
-		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
-	}
-	auto column = make_uniq<BoundReferenceExpression>(stats.GetType(), 0ULL);
-	auto expression = filter_data->ToExpression(*column);
-	return ExpressionFilter::CheckExpressionStatistics(*expression, stats);
-}
-
-string DynamicFilter::ToString(const string &column_name) const {
-	if (filter_data) {
-		return "Dynamic Filter (" + column_name + ")";
-	} else {
-		return "Empty Dynamic Filter (" + column_name + ")";
+bool DynamicFilterData::CompareValue(ExpressionType comparison_type, const Value &constant, const Value &value) {
+	switch (comparison_type) {
+	case ExpressionType::COMPARE_EQUAL:
+		return ValueOperations::Equals(value, constant);
+	case ExpressionType::COMPARE_NOTEQUAL:
+		return ValueOperations::NotEquals(value, constant);
+	case ExpressionType::COMPARE_GREATERTHAN:
+		return ValueOperations::GreaterThan(value, constant);
+	case ExpressionType::COMPARE_GREATERTHANOREQUALTO:
+		return ValueOperations::GreaterThanEquals(value, constant);
+	case ExpressionType::COMPARE_LESSTHAN:
+		return ValueOperations::LessThan(value, constant);
+	case ExpressionType::COMPARE_LESSTHANOREQUALTO:
+		return ValueOperations::LessThanEquals(value, constant);
+	default:
+		throw InternalException("Unknown comparison type for DynamicFilter: " + EnumUtil::ToString(comparison_type));
 	}
 }
 
-unique_ptr<Expression> DynamicFilter::ToExpression(const Expression &column) const {
+FilterPropagateResult DynamicFilterData::CheckStatistics(BaseStatistics &stats, ExpressionType comparison_type,
+                                                         const Value &constant) {
+	switch (constant.type().InternalType()) {
+	case PhysicalType::UINT8:
+	case PhysicalType::UINT16:
+	case PhysicalType::UINT32:
+	case PhysicalType::UINT64:
+	case PhysicalType::UINT128:
+	case PhysicalType::INT8:
+	case PhysicalType::INT16:
+	case PhysicalType::INT32:
+	case PhysicalType::INT64:
+	case PhysicalType::INT128:
+	case PhysicalType::FLOAT:
+	case PhysicalType::DOUBLE:
+		return NumericStats::CheckZonemap(stats, comparison_type, array_ptr<const Value>(constant));
+	case PhysicalType::VARCHAR:
+		if (stats.GetStatsType() == StatisticsType::STRING_STATS) {
+			return StringStats::CheckZonemap(stats, comparison_type, array_ptr<const Value>(constant));
+		}
+		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
+	default:
+		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
+	}
+}
+
+unique_ptr<Expression> LegacyDynamicFilter::ToExpression(const Expression &column) const {
 	if (!filter_data || !filter_data->initialized) {
 		auto bound_constant = make_uniq<BoundConstantExpression>(Value(true));
 		return std::move(bound_constant);
 	}
 	lock_guard<mutex> l(filter_data->lock);
 	return filter_data->ToExpression(column);
-}
-
-bool DynamicFilter::Equals(const TableFilter &other_p) const {
-	if (!TableFilter::Equals(other_p)) {
-		return false;
-	}
-	auto &other = other_p.Cast<DynamicFilter>();
-	return other.filter_data.get() == filter_data.get();
-}
-
-unique_ptr<TableFilter> DynamicFilter::Copy() const {
-	return make_uniq<DynamicFilter>(filter_data);
 }
 
 void DynamicFilterData::SetValue(Value val) {
