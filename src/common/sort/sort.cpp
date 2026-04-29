e4c8f743c82649a3729f3f4811cade91732e8121
Laurens Kuiper <laurens.kuiper@cwi.nl>
2026-04-29 15:26:29 +0200

    fix name collision and make sure atomic is initialized

--- src/common/sort/sort.cpp
+++ src/common/sort/sort.cpp
@@ -15,12 +15,12 @@
 
 namespace duckdb {
 
-Sort::Sort(ClientContext &context_p, const vector<BoundOrderByNode> &orders, const vector<LogicalType> &input_types,
-           vector<idx_t> projection_map, bool is_index_sort_p)
-    : context(context_p), key_layout(make_shared_ptr<TupleDataLayout>()),
+Sort::Sort(ClientContext &client_context_p, const vector<BoundOrderByNode> &orders,
+           const vector<LogicalType> &input_types, vector<idx_t> projection_map, bool is_index_sort_p)
+    : client_context(client_context_p), key_layout(make_shared_ptr<TupleDataLayout>()),
       payload_layout(make_shared_ptr<TupleDataLayout>()), is_index_sort(is_index_sort_p) {
 	// Convert orders to a single "create_sort_key" expression (and corresponding "decode_sort_key")
-	FunctionBinder binder(context);
+	FunctionBinder binder(client_context);
 	vector<unique_ptr<Expression>> create_children;
 	vector<unique_ptr<Expression>> decode_children;
 	child_list_t<LogicalType> decode_child_list;
@@ -365,9 +365,7 @@ class SortGlobalSourceState : public GlobalSourceState {
 public:
 	SortGlobalSourceState(const Sort &sort, ClientContext &context, SortGlobalSinkState &sink_p)
 	    : sink(sink_p), merger(sort, std::move(sink.sorted_runs), sink.partition_size, sink.external, false),
-	      merger_global_state(merger.total_count == 0 ? nullptr : merger.GetGlobalSourceState(context)) {
-		// TODO: we want to pass "sort.is_index_sort" instead of just "false" here
-		//  so that we can do an approximate sort, but that causes issues in the ART
+	      merger_global_state(merger.total_count == 0 ? nullptr : merger.GetGlobalSourceState(context)), registered(0) {
 	}
 
 public:
