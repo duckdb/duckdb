140666a6a3a019d3ecc5eef9631f9730b7b7ddbe
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-05-01 14:06:14 +0200

    Ignore queue branch for push; only run for merge_group

--- scripts/ci/job_stages.py
+++ scripts/ci/job_stages.py
@@ -97,12 +97,12 @@ def should_save_cache(selection_input: JobSelectionInput) -> bool:
         selection_input.repository != "duckdb/duckdb"
         or selection_input.ref_name == "main"
         or selection_input.ref_name == "v1.5-variegata"
-        or (selection_input.event_name == "push" and selection_input.ref_name.startswith("gh-readonly-queue/"))
+        or selection_input.event_name == "merge_group"
     )
 
 
 def enabled_jobs(selection_input: JobSelectionInput) -> list[str]:
-    if selection_input.event_name == "push" and selection_input.ref_name.startswith("gh-readonly-queue/"):
+    if selection_input.event_name == "merge_group":
         selected_jobs = MERGE_GROUP_JOBS.copy()
     elif selection_input.ref_name == "main":
         selected_jobs = NIGHTLY_JOBS.copy()
