6d95a932d9efc1179a2716c991e3de1633dc6967
Mytherin <mark.raasveldt@gmail.com>
2026-05-04 23:15:53 +0200

    Fix count in writer

--- src/common/types/row/tuple_data_allocator.cpp
+++ src/common/types/row/tuple_data_allocator.cpp
@@ -146,8 +146,7 @@ bool TupleDataAllocator::BuildFastPath(TupleDataSegment &segment, TupleDataPinSt
 	}
 
 	// We can do the fast path append!
-	auto row_locations =
-	    FlatVector::Writer<data_ptr_t>(chunk_state.row_locations, append_offset + append_count, append_offset);
+	auto row_locations = FlatVector::Writer<data_ptr_t>(chunk_state.row_locations, append_count, append_offset);
 	const auto base_row_ptr = GetRowPointer(pin_state, part) + part.count * row_width;
 	for (idx_t i = 0; i < append_count; i++) {
 		row_locations.WriteValue(base_row_ptr + i * row_width);
