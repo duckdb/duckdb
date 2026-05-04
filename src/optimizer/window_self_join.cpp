24225d46c6b349aab86e2fd98ec1a8fd4d377c71
Denis Hirn <denis.hirn@uni-tuebingen.de>
2026-05-04 13:56:54 +0200

    Fix missing case in window self join optimizer

--- src/optimizer/window_self_join.cpp
+++ src/optimizer/window_self_join.cpp
@@ -46,6 +46,14 @@ static unique_ptr<Expression> TranslateAggregate(const BoundWindowExpression &w_
 			auto order_copy = order.Copy();
 			orders.emplace_back(std::move(order_copy));
 		}
+	} else if (!w_expr.orders.empty()) {
+		//	If the frame was ordered, copy the frame ordering to the aggregate function
+		result->order_bys = make_uniq<BoundOrderModifier>();
+		auto &orders = result->order_bys->orders;
+		for (auto &order : w_expr.orders) {
+			auto order_copy = order.Copy();
+			orders.emplace_back(std::move(order_copy));
+		}
 	}
 
 	return std::move(result);
