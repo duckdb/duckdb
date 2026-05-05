3f39471f6a81fe51669ff5ee733068493c2f4c83
Denis Hirn <denis.hirn@uni-tuebingen.de>
2026-05-05 15:05:50 +0200

    Remove unused variable in struct filter

--- src/planner/filter/struct_filter.cpp
+++ src/planner/filter/struct_filter.cpp
@@ -13,7 +13,6 @@ LegacyStructFilter::LegacyStructFilter(idx_t child_idx_p, string child_name_p, u
 }
 
 unique_ptr<Expression> LegacyStructFilter::ToExpression(const Expression &column) const {
-	auto &child_type = StructType::GetChildType(column.GetReturnType(), child_idx);
 	vector<unique_ptr<Expression>> arguments;
 	arguments.push_back(column.Copy());
 	arguments.push_back(make_uniq<BoundConstantExpression>(Value::BIGINT(NumericCast<int64_t>(child_idx + 1))));
