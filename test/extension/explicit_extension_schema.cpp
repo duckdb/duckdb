ece4b32502b8cdc5a901eeb41f5f45d7050a3a6c
Charlotte Felius <lotte@duckdblabs.com>
2026-05-07 08:23:47 +0200

    avoid var to be optimized away

--- test/extension/explicit_extension_schema.cpp
+++ test/extension/explicit_extension_schema.cpp
@@ -25,7 +25,7 @@ static void LoadableExtensionExplicitSchemaFunInit(ExtensionLoader &loader) {
 
 extern "C" {
 DUCKDB_CPP_EXTENSION_ENTRY(explicit_extension_schema, loader) {
-	auto &callback_manager = DBConfig::GetConfig(loader.GetDatabaseInstance()).GetCallbackManager();
+	DBConfig::GetConfig(loader.GetDatabaseInstance()).GetCallbackManager();
 	// set the schema for the extension
 	loader.SetExtensionSchema("explicit_schema");
 	LoadableExtensionExplicitSchemaFunInit(loader);
