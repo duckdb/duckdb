de710782b372e404223942fd55cd004e468ce9e1
Alex Kasko <alex@staticlibs.net>
2026-05-11 23:42:16 +0100

    Bump Postgres, MySQL and ODBC
    This PR updates Postgres, MySQL and ODBC extensions for 1.5.3.
    
    There were no changes to SQLite extension.
    
    Besides the changes to function and settings entries, new `rds` secret
    type is added to Postgres extension. Corresponding change to
    `EXTENSION_SECRET_PROVIDERS` is intended to be added along with the
    `aws` extension bump.
    
    MySQL CMake script update also enables MySQL extension for
    `linux_amd64_musl` bringing in the change from #21162.

--- .github/config/extensions/postgres_scanner.cmake
+++ .github/config/extensions/postgres_scanner.cmake
@@ -4,7 +4,7 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(postgres_scanner
             DONT_LINK
             GIT_URL https://github.com/duckdb/duckdb-postgres
-            GIT_TAG c89234f0b1985f4ee0f52f16e742a1ab2d4ae4f0
+            GIT_TAG 6b2b12cad3afef61e8a4637e714e8a88895fed1a
             SUBMODULES database-connector
             )
 endif()
