41c373105898071dd444e552b66d3ed31c6b2450
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-05-11 10:21:40 +0200

    Bump avro, azure, delta, ducklake, spatial, unity_catalog and vortex

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW)
     duckdb_extension_load(avro
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-avro
-            GIT_TAG a73b60d629a92146cfd71c210936144a971783bd
+            GIT_TAG 0535350850786be8a8bc03b9ffc59e29dd32ecf7
     )
 endif()
--- .github/config/extensions/unity_catalog.cmake
+++ .github/config/extensions/unity_catalog.cmake
@@ -1,7 +1,7 @@
 if(NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
   duckdb_extension_load(unity_catalog
             GIT_URL https://github.com/duckdb/unity_catalog
-            GIT_TAG 02024095fe6eeed6f53e78dce43e83c7424abec2
+            GIT_TAG ad54a347dba6a1da2167c716b2c67fdfb69cd499
             LOAD_TESTS
   )
 endif()
