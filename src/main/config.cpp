e71be1149beffc57419131a2862935767cd7ef9b
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2026-05-12 14:46:13 +0200

    merge conflicts

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -225,12 +225,12 @@ static const ConfigurationOption internal_options[] = {
 
 static const ConfigurationAlias setting_aliases[] = {DUCKDB_SETTING_ALIAS("configure_metrics", 27),
                                                      DUCKDB_SETTING_ALIAS("custom_profiling_settings", 27),
-                                                     DUCKDB_SETTING_ALIAS("memory_limit", 112),
+                                                     DUCKDB_SETTING_ALIAS("memory_limit", 113),
                                                      DUCKDB_SETTING_ALIAS("null_order", 52),
-                                                     DUCKDB_SETTING_ALIAS("profiling_output", 133),
-                                                     DUCKDB_SETTING_ALIAS("user", 148),
+                                                     DUCKDB_SETTING_ALIAS("profiling_output", 134),
+                                                     DUCKDB_SETTING_ALIAS("user", 149),
                                                      DUCKDB_SETTING_ALIAS("wal_autocheckpoint", 26),
-                                                     DUCKDB_SETTING_ALIAS("worker_threads", 147),
+                                                     DUCKDB_SETTING_ALIAS("worker_threads", 148),
                                                      FINAL_ALIAS};
 
 vector<ConfigurationOption> DBConfig::GetOptions() {
