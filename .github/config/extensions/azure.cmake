97f662015405dfa6d19286724ccae727ce833ded
ben fleis <benfleis@users.noreply.github.com>
2026-05-13 06:55:48 +0200

    Bump azure

--- .github/config/extensions/azure.cmake
+++ .github/config/extensions/azure.cmake
@@ -1,7 +1,7 @@
-if (NOT MINGW AND NOT ${WASM_ENABLED})
+if(NOT MINGW AND NOT ${WASM_ENABLED})
   duckdb_extension_load(azure
-        LOAD_TESTS
         GIT_URL https://github.com/duckdb/duckdb-azure
-        GIT_TAG 2ad247d4ca090cd2110f2e35531ab6fcdb80c186
+        GIT_TAG 407270037ae87e7ab29a087b6d367e51e6970cf2
+        LOAD_TESTS
   )
 endif()
