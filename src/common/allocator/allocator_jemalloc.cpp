753e164949cbb0c9fd87ac7dab1983b94e2d0d0a
Niclas Haderer <niclas.haderer.dev@gmail.com>
2026-05-15 15:53:11 +0200

    Fixed includes

--- src/common/allocator/allocator_jemalloc.cpp
+++ src/common/allocator/allocator_jemalloc.cpp
@@ -1,5 +1,6 @@
 #include "duckdb/common/allocator.hpp"
-#include "duckdb/common/operator/numeric_cast.hpp"
+#include "duckdb/common/numeric_utils.hpp"
+#include "duckdb/common/string_util.hpp"
 
 #include <thread>
 #include <cstdint>
