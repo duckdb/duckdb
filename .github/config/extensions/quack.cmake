29cded5a7acf410933ad3cd37ad42c37cf881a21
Mytherin <mark.raasveldt@gmail.com>
2026-05-19 12:41:37 +0200

    Quack + appender fixes

--- .github/config/extensions/quack.cmake
+++ .github/config/extensions/quack.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(quack
     ## LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-quack
-    GIT_TAG 1693647c152b438aa2a6a9ad71764f99c5a561e0
+    GIT_TAG daae4826f57986fbb6cc2116316f89c673814b23
 )
