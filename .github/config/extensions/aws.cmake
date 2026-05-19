7d5789696ba57e7e1c8daded2664187b66576838
Mytherin <mark.raasveldt@gmail.com>
2026-05-19 20:41:15 +0200

    Unbump extensions

--- .github/config/extensions/aws.cmake
+++ .github/config/extensions/aws.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(aws
             LOAD_TESTS APPLY_PATCHES
             GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG 38d4ed30b7f5855168f4b494bf9be611c868c69a
+            GIT_TAG b2649e68341a9ee717588dd23f277904727ce793
             )
 endif()
