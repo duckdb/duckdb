1fbb4552aecfd2300775f5c2a1249bc3004ca5c2
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-05-20 11:19:34 +0200

    Remove patches

--- .github/config/extensions/spatial.cmake
+++ .github/config/extensions/spatial.cmake
@@ -3,7 +3,7 @@ if (${BUILD_COMPLETE_EXTENSION_SET})
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG b68b309d371dba936c5bb362980e559b7756b16d
+    GIT_TAG 6019a321b4e9a91cb0e81e29d8458d41c75ec3ba
     INCLUDE_DIR src/spatial
     TEST_DIR test/sql
     APPLY_PATCHES
