1c481c83f62cfc3ca62b89fb5feba96f5e515892
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-05-20 10:20:58 +0200

    Bump spatial to latest main

--- .github/config/extensions/spatial.cmake
+++ .github/config/extensions/spatial.cmake
@@ -3,7 +3,7 @@ if (${BUILD_COMPLETE_EXTENSION_SET})
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG a415e7476009831b317e08fc4be927558d031204
+    GIT_TAG b68b309d371dba936c5bb362980e559b7756b16d
     INCLUDE_DIR src/spatial
     TEST_DIR test/sql
     APPLY_PATCHES
