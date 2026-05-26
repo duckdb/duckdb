2f314a0f1e5cf0564c1f8388b631aa198f640d30
Sebastiaan Gerritsen <dev@sebastiaan.io>
2026-05-26 10:28:42 +0200

    Update reference to v1.4 branch of duckdb-httpfs

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG b28c9fa91b1e9b3708114ed447a573403cb85f73
+    GIT_TAG 8f21bb4c79bd35733f1d6cb537c9f40611bc00fe
     INCLUDE_DIR src/include
 )
