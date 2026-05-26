7b080b98ba2162e0fe67b272ef59e4c96d612009
Etgar Shmueli <107912097+EtgarDev@users.noreply.github.com>
2026-05-26 14:27:14 +0300

    Remove duplicate NEW TABLE on DELETE check

--- src/planner/binder/statement/bind_create.cpp
+++ src/planner/binder/statement/bind_create.cpp
@@ -523,11 +523,6 @@ SchemaCatalogEntry &Binder::BindCreateTriggerInfo(CreateTriggerInfo &create_trig
 	if (create_trigger_info.for_each == TriggerForEach::ROW) {
 		throw NotImplementedException("FOR EACH ROW triggers are not yet supported");
 	}
-	if (!create_trigger_info.referencing_new_table.empty()) {
-		if (create_trigger_info.event_type == TriggerEventType::DELETE_EVENT) {
-			throw BinderException("REFERENCING NEW TABLE AS is not valid for AFTER DELETE triggers");
-		}
-	}
 	if (!create_trigger_info.referencing_old_table.empty()) {
 		if (create_trigger_info.event_type == TriggerEventType::INSERT_EVENT) {
 			throw BinderException("REFERENCING OLD TABLE AS is not valid for AFTER INSERT triggers");
