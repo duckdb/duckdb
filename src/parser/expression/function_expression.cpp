8142f74705159103218d5b6e9a816c771a95ab8b
Max Gabrielsson <max@gabrielsson.com>
2026-05-28 18:44:56 +0200

    serializer

--- src/parser/expression/function_expression.cpp
+++ src/parser/expression/function_expression.cpp
@@ -4,6 +4,8 @@
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/types/hash.hpp"
+#include "duckdb/common/serializer/serializer.hpp"
+#include "duckdb/common/serializer/deserializer.hpp"
 
 namespace duckdb {
 
