1804d61c076157d78e22cd5d03ee973287933fb1
Tishj <t_b@live.nl>
2026-06-02 15:18:34 +0200

    iceberg hash

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -8,6 +8,6 @@ if (NOT MINGW)
     duckdb_extension_load(iceberg
 	    #FIXME: restore autoloading tests ${LOAD_ICEBERG_TESTS}
             GIT_URL https://github.com/duckdb/duckdb-iceberg
-            GIT_TAG 2c432d9a8adeb31507564a4c26e3a17226f9056c
+            GIT_TAG f25e382e829166487398df1cfbb0a48bdc1873f3
             )
 endif()
