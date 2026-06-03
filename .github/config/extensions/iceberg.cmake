d137bbea251e3747abae27bef40f21ad95f6b0af
Tishj <t_b@live.nl>
2026-06-03 13:09:34 +0200

    dont load tests

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -6,7 +6,7 @@ else ()
 endif()
 if (NOT MINGW)
     duckdb_extension_load(iceberg
-	        ${LOAD_ICEBERG_TESTS}
+	    #FIXME: restore autoloading tests ${LOAD_ICEBERG_TESTS}
             GIT_URL https://github.com/duckdb/duckdb-iceberg
             GIT_TAG f25e382e829166487398df1cfbb0a48bdc1873f3
             APPLY_PATCHES
