f00db67dd9553499eb2c4a8d4ec1f1da51a0857f
Tishj <t_b@live.nl>
2026-06-03 14:19:14 +0200

    remove the struct default changes for now

--- src/planner/binder/statement/bind_insert.cpp
+++ src/planner/binder/statement/bind_insert.cpp
@@ -100,91 +100,6 @@ void Binder::ExpandDefaultInValuesList(InsertQueryNode &node, TableCatalogEntry 
 	}
 }
 
-namespace {
-
-//! Used to determine if the field of a struct is mapped or not
-struct StructFieldMapping {
-	case_insensitive_map_t<StructFieldMapping> child_mapping;
-};
-
-static Value CreateStructMapping(const LogicalType &struct_type, const string &name,
-                                 case_insensitive_map_t<StructFieldMapping> &out_mapping) {
-	child_list_t<Value> field_mapping;
-
-	auto &struct_children = StructType::GetChildTypes(struct_type);
-	for (auto &[field_name, field_type] : struct_children) {
-		auto &child_mapping = out_mapping[field_name];
-		Value mapping;
-		if (field_type.id() == LogicalTypeId::STRUCT) {
-			mapping = CreateStructMapping(field_type, field_name, child_mapping.child_mapping);
-		} else {
-			mapping = Value(field_name);
-		}
-		field_mapping.emplace_back(field_name, mapping);
-	}
-	auto struct_value = Value::STRUCT(field_mapping);
-	if (name.empty()) {
-		//! Root column
-		return struct_value;
-	}
-	return Value::STRUCT({{"", Value(name)}, {"", struct_value}});
-}
-
-static Value CreateStructDefault(const Value &value, const case_insensitive_map_t<StructFieldMapping> &mapping = {}) {
-	child_list_t<Value> field_defaults;
-	auto &field_values = StructValue::GetChildren(value);
-	auto &struct_children = StructType::GetChildTypes(value.type());
-	for (idx_t j = 0; j < field_values.size(); j++) {
-		auto &field_name = struct_children[j].first;
-		auto &field_type = struct_children[j].second;
-		auto &field_value = field_values[j];
-
-		auto it = mapping.find(field_name);
-		const bool is_mapped = it != mapping.end();
-
-		Value field_default;
-		if (field_type.id() == LogicalTypeId::STRUCT) {
-			if (is_mapped) {
-				field_default = CreateStructDefault(field_value, it->second.child_mapping);
-			} else {
-				field_default = CreateStructDefault(field_value);
-			}
-
-			if (field_default.IsNull()) {
-				//! All fields were skipped, no need to include this value
-				continue;
-			}
-		} else {
-			if (is_mapped) {
-				continue;
-			}
-			field_default = field_value;
-		}
-
-		field_defaults.emplace_back(field_name, field_default);
-	}
-	if (field_defaults.empty()) {
-		//! Skipped all fields, signal that the value should be omitted
-		return Value();
-	}
-	return Value::STRUCT(field_defaults);
-}
-
-static Value EvaluateStructDefault(ClientContext &context, const Expression &default_expr, const string &column_name) {
-	if (!default_expr.IsFoldable()) {
-		throw BinderException("Cannot resolve partial STRUCT insert for column \"%s\" with non-constant default value",
-		                      column_name);
-	}
-	Value default_value;
-	if (!ExpressionExecutor::TryEvaluateScalar(context, default_expr, default_value)) {
-		throw BinderException("Cannot resolve partial STRUCT insert for column \"%s\" with non-constant default value",
-		                      column_name);
-	}
-	return default_value;
-}
-
-} // namespace
-
 unique_ptr<LogicalOperator> Binder::ResolveDefaultsProjection(LogicalInsert &insert, unique_ptr<LogicalOperator> root,
                                                               const vector<LogicalType> &source_types) {
 	if (insert.column_index_map.empty()) {
@@ -204,26 +119,7 @@ unique_ptr<LogicalOperator> Binder::ResolveDefaultsProjection(LogicalInsert &ins
 
 		auto &original_type = source_types[mapped_index];
 		auto source_binding = source_bindings[mapped_index];
-		if (col.Type().id() != LogicalTypeId::STRUCT || original_type.id() != LogicalTypeId::STRUCT) {
-			select_list.push_back(make_uniq<BoundColumnRefExpression>(original_type, source_binding));
-			continue;
-		}
-
-		auto default_value = EvaluateStructDefault(context, *insert.bound_defaults[storage_idx], col.Name());
-		if (default_value.IsNull()) {
-			select_list.push_back(make_uniq<BoundColumnRefExpression>(original_type, source_binding));
-			continue;
-		}
-
-		// Column is of type STRUCT, create a remap that fills in omitted fields from the column default.
-		vector<unique_ptr<Expression>> children;
-		children.push_back(make_uniq<BoundColumnRefExpression>(original_type, source_binding));
-		children.push_back(make_uniq<BoundConstantExpression>(Value(col.Type())));
-
-		case_insensitive_map_t<StructFieldMapping> mapping;
-		children.push_back(make_uniq<BoundConstantExpression>(CreateStructMapping(original_type, "", mapping)));
-		children.push_back(make_uniq<BoundConstantExpression>(CreateStructDefault(default_value, mapping)));
-		select_list.push_back(RemapStructFun::GetFunction().Bind(context, std::move(children)));
+		select_list.push_back(make_uniq<BoundColumnRefExpression>(original_type, source_binding));
 	}
 
 	auto projection = make_uniq<LogicalProjection>(GenerateTableIndex(), std::move(select_list));
