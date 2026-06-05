43ea399f305a71adbf1590f5c911be24ff14c9d2
Tishj <t_b@live.nl>
2026-06-05 15:47:07 +0200

    add runtime check to parquet RLE bit-unpack logic to harden against corrupted/malicious bitwidths

--- extension/parquet/include/decode_utils.hpp
+++ extension/parquet/include/decode_utils.hpp
@@ -88,6 +88,12 @@ public:
 	template <class T>
 	static void BitUnpackAlignedInternal(ByteBuffer &src, T *dst, const idx_t count, const bitpacking_width_t width) {
 		D_ASSERT(count % BitpackingPrimitives::BITPACKING_ALGORITHM_GROUP_SIZE == 0);
+		if (width > sizeof(T) * BITPACK_DLEN) {
+			throw InternalException("The width (%d) of the bitpacked data exceeds the maximum width (%d) for "
+			                        "the target type, the file might be corrupted.",
+			                        width, sizeof(T) * BITPACK_DLEN);
+		}
+
 		if (cast_pointer_to_uint64(src.ptr) % sizeof(T) == 0) {
 			// Fast path: aligned
 			BitpackingPrimitives::UnPackBuffer<T>(data_ptr_cast(dst), src.ptr, count, width);
