a05f5eb8079ab6e667bac50716d23ddf81df16bd
Alex Kasko <alex@staticlibs.net>
2026-06-10 12:23:40 +0100

    Re-enable Postgres and MySQL

--- .github/config/extensions/mysql_scanner.cmake
+++ .github/config/extensions/mysql_scanner.cmake
@@ -1,11 +1,10 @@
-# Temporarily disabled
-# if (NOT MINGW AND NOT ${WASM_ENABLED})
-#     duckdb_extension_load(mysql_scanner
-#             DONT_LINK
-#             LOAD_TESTS
-#             GIT_URL https://github.com/duckdb/duckdb-mysql
-#             GIT_TAG 2a59de314c07bece84ae0be4286c9b8964419b95
-#             SUBMODULES database-connector
-#             APPLY_PATCHES
-#             )
-# endif()
+if (NOT MINGW AND NOT ${WASM_ENABLED})
+    duckdb_extension_load(mysql_scanner
+            DONT_LINK
+            LOAD_TESTS
+            GIT_URL https://github.com/duckdb/duckdb-mysql
+            GIT_TAG 2a59de314c07bece84ae0be4286c9b8964419b95
+            SUBMODULES database-connector
+            APPLY_PATCHES
+            )
+endif()
