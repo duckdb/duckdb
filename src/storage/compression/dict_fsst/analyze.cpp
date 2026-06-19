16c0e71107a80132e02653914ecb43fe07d61e9f
Sebastiaan Gerritsen <dev@sebastiaan.io>
2026-06-19 10:26:26 +0200

    Removed binary search in favor of approximate size limit calculation.

--- src/storage/compression/dict_fsst/analyze.cpp
+++ src/storage/compression/dict_fsst/analyze.cpp
@@ -5,51 +5,43 @@ namespace dict_fsst {
 
 //! Determine the size requirements for the worst case, which is when a single string fills an
 //! entire segment on its own.
-static idx_t RequiredSpace(const idx_t str_len, const bool fsst_encoded) {
-	// Dictionary contains NULL and current string.
-	const bitpacking_width_t string_lengths_width = BitpackingPrimitives::MinimumBitWidth(str_len);
+static idx_t GetStringSizeLimit(const idx_t available_space, const bool fsst_encoded) {
+	idx_t max_str_len = DictFSSTCompression::STRING_SIZE_LIMIT - 1;
+	if (fsst_encoded) {
+		// In the worst case FSST may double the string length by prepending every byte with an exception
+		max_str_len *= 2;
+	}
+
+	// Dictionary contains NULL and current string
+	const bitpacking_width_t string_lengths_width = BitpackingPrimitives::MinimumBitWidth(max_str_len);
 	const idx_t string_lengths_space = BitpackingPrimitives::GetRequiredSize(2, string_lengths_width);
 
-	// Dictionary stores only one valid string.
+	// Dictionary stores only one valid string
 	const bitpacking_width_t dict_indices_width = BitpackingPrimitives::MinimumBitWidth(1);
 	const idx_t dict_indices_space = BitpackingPrimitives::GetRequiredSize(1, dict_indices_width);
 
-	idx_t size = 0;
-	size += sizeof(dict_fsst_compression_header_t);
-	size = AlignValue<idx_t>(size);
-	size += str_len;
+	idx_t metadata_size = 0;
+	metadata_size += AlignValue<idx_t>(sizeof(dict_fsst_compression_header_t));
 	if (fsst_encoded) {
 		// As denoted in fsst.h
-		size += 7;
+		metadata_size += 7;
 	}
-	size = AlignValue<idx_t>(size);
+	// Reserve maximum alignment padding for variable length string
+	metadata_size += sizeof(idx_t) - 1;
 	if (fsst_encoded) {
-		size += DictFSSTCompression::FSST_SYMBOL_TABLE_SIZE;
-		size = AlignValue<idx_t>(size);
+		metadata_size += AlignValue<idx_t>(DictFSSTCompression::FSST_SYMBOL_TABLE_SIZE);
 	}
-	size += string_lengths_space;
-	size = AlignValue<idx_t>(size);
-	size += dict_indices_space;
+	metadata_size += AlignValue<idx_t>(string_lengths_space);
+	metadata_size += dict_indices_space;
 
-	return size;
-}
+	D_ASSERT(metadata_size < available_space);
+	idx_t max_string_size = available_space - metadata_size;
 
-//! Perform a binary search to find the effective exclusive string size limit.
-static idx_t GetStringSizeLimit(const idx_t available_space, const bool fsst_encoded) {
-	idx_t lower = 0;
-	idx_t upper = DictFSSTCompression::STRING_SIZE_LIMIT;
-
-	while (lower + 1 < upper) {
-		const auto str_len = lower + (upper - lower) / 2;
-		const auto encoded_len = fsst_encoded ? str_len * 2 : str_len;
-
-		if (RequiredSpace(encoded_len, fsst_encoded) <= available_space) {
-			lower = str_len;
-		} else {
-			upper = str_len;
-		}
+	if (fsst_encoded) {
+		max_string_size = max_string_size / 2;
 	}
-	return upper;
+
+	return MinValue(DictFSSTCompression::STRING_SIZE_LIMIT, max_string_size + 1);
 }
 
 DictFSSTAnalyzeState::DictFSSTAnalyzeState(const CompressionInfo &info) : AnalyzeState(info) {
@@ -76,12 +68,12 @@ bool DictFSSTAnalyzeState::Analyze(Vector &input, idx_t count) {
 				max_string_length = str_len;
 			}
 			if (str_len >= string_size_limit) {
-				//! A segment cannot be spread out over multiple blocks, so if a string cannot fit in an empty segment
-				//! the encoding will fail.
+				// A segment cannot be spread out over multiple blocks, so if a string cannot fit in an empty segment
+				// the encoding will fail
 				return false;
 			}
 			if (str_len >= fsst_string_size_limit) {
-				//! In the worst case FSST may double the string length by prepending every byte with an exception.
+				// FSST strings may be up to two times larger than their plain equivalent
 				disable_fsst = true;
 			}
 		}
