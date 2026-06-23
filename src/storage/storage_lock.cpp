e36ca245998e3afd11de148fee9121404df2a0c8
d-justen <mail@davidjusten.de>
2026-06-23 08:27:52 +0200

    Prevent writer starvation

--- src/storage/storage_lock.cpp
+++ src/storage/storage_lock.cpp
@@ -1,29 +1,44 @@
 #include "duckdb/storage/storage_lock.hpp"
 #include "duckdb/common/common.hpp"
 #include "duckdb/common/assert.hpp"
+#include "duckdb/common/mutex.hpp"
 #include "duckdb/common/thread_annotation/thread_annotation.hpp"
 
+#include <condition_variable>
 #include <shared_mutex>
 
 namespace duckdb {
 
 struct StorageLockInternals : enable_shared_from_this<StorageLockInternals> {
 public:
-	std::shared_mutex lock;
-
-public:
 	unique_ptr<StorageLockKey> GetExclusiveLock() DUCKDB_NO_THREAD_SAFETY_ANALYSIS {
+		WaitForExclusiveAccess();
 		lock.lock();
 		return make_uniq<StorageLockKey>(shared_from_this(), StorageLockType::EXCLUSIVE);
 	}
 
 	unique_ptr<StorageLockKey> GetSharedLock() {
+		{
+			unique_lock<mutex> guard(gate_lock);
+			gate_condition.wait(guard, [&] { return !writer_active && waiting_writers == 0; });
+			active_readers++;
+		}
 		lock.lock_shared();
 		return make_uniq<StorageLockKey>(shared_from_this(), StorageLockType::SHARED);
 	}
 
 	unique_ptr<StorageLockKey> TryGetExclusiveLock() DUCKDB_NO_THREAD_SAFETY_ANALYSIS {
+		{
+			lock_guard<mutex> guard(gate_lock);
+			if (writer_active || waiting_writers != 0 || active_readers != 0) {
+				return nullptr;
+			}
+			writer_active = true;
+		}
 		if (!lock.try_lock()) {
+			lock_guard<mutex> guard(gate_lock);
+			writer_active = false;
+			gate_condition.notify_all();
 			return nullptr;
 		}
 		return make_uniq<StorageLockKey>(shared_from_this(), StorageLockType::EXCLUSIVE);
@@ -31,10 +46,38 @@ public:
 
 	void ReleaseExclusiveLock() DUCKDB_NO_THREAD_SAFETY_ANALYSIS {
 		lock.unlock();
+		lock_guard<mutex> guard(gate_lock);
+		D_ASSERT(writer_active);
+		writer_active = false;
+		gate_condition.notify_all();
 	}
+
 	void ReleaseSharedLock() {
 		lock.unlock_shared();
+		lock_guard<mutex> guard(gate_lock);
+		D_ASSERT(active_readers > 0);
+		active_readers--;
+		if (active_readers == 0) {
+			gate_condition.notify_all();
+		}
 	}
+
+private:
+	void WaitForExclusiveAccess() {
+		unique_lock<mutex> guard(gate_lock);
+		waiting_writers++;
+		gate_condition.wait(guard, [&] { return !writer_active && active_readers == 0; });
+		waiting_writers--;
+		writer_active = true;
+	}
+
+private:
+	std::shared_mutex lock;
+	mutex gate_lock;
+	std::condition_variable gate_condition;
+	idx_t active_readers = 0;
+	idx_t waiting_writers = 0;
+	bool writer_active = false;
 };
 
 StorageLockKey::StorageLockKey(shared_ptr<StorageLockInternals> internals_p, StorageLockType type)
--- test/common/test_storage_lock.cpp
+++ test/common/test_storage_lock.cpp
@@ -2,6 +2,9 @@
 #include "duckdb/storage/storage_lock.hpp"
 #include "duckdb/transaction/checkpoint_lock.hpp"
 
+#include <atomic>
+#include <thread>
+
 using namespace duckdb;
 
 TEST_CASE("StorageLock allows shared ownership and excludes writers", "[storage]") {
@@ -39,4 +42,53 @@ TEST_CASE("CheckpointLockCoordinator promotes the final shared lease", "[storage
 
 	first_shared_lock.reset();
 	REQUIRE(lock.TryGetExclusiveLock());
+}
+
+TEST_CASE("StorageLock blocks later readers behind a waiting writer", "[storage]") {
+	StorageLock lock;
+	auto shared_lock = lock.GetSharedLock();
+
+	std::atomic<bool> writer_waiting = false;
+	std::atomic<bool> writer_acquired = false;
+	std::atomic<bool> reader_acquired = false;
+	std::atomic<bool> release_writer = false;
+
+	std::thread writer([&] {
+		writer_waiting = true;
+		auto exclusive_lock = lock.GetExclusiveLock();
+		writer_acquired = true;
+		while (!release_writer) {
+		}
+	});
+
+	while (!writer_waiting) {
+	}
+	REQUIRE(!lock.TryGetExclusiveLock());
+
+	std::thread reader([&] {
+		auto later_shared_lock = lock.GetSharedLock();
+		reader_acquired = true;
+	});
+
+	for (idx_t i = 0; i < 1000 && !writer_acquired; i++) {
+		REQUIRE(!reader_acquired);
+		std::this_thread::yield();
+	}
+
+	shared_lock.reset();
+
+	for (idx_t i = 0; i < 1000 && !writer_acquired; i++) {
+		std::this_thread::yield();
+	}
+	REQUIRE(writer_acquired);
+	REQUIRE(!reader_acquired);
+
+	release_writer = true;
+	writer.join();
+
+	for (idx_t i = 0; i < 1000 && !reader_acquired; i++) {
+		std::this_thread::yield();
+	}
+	REQUIRE(reader_acquired);
+	reader.join();
 }
