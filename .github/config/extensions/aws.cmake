e33c8f9905948adfaa58f727bec0d75354917b41
Yan <60644013+yan-alex@users.noreply.github.com>
2026-06-29 10:37:15 +0200

    bump aws extension

--- .github/config/extensions/aws.cmake
+++ .github/config/extensions/aws.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(aws
             LOAD_TESTS APPLY_PATCHES
             GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG b2649e68341a9ee717588dd23f277904727ce793
+            GIT_TAG 7d04119ee8d3f8836e278f0e8cbf21827ff5338b
             )
 endif()
