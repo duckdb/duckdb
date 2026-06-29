0007256520f922506586d3f3e0e2dc635c7f9d3f
Yan <60644013+yan-alex@users.noreply.github.com>
2026-06-29 14:07:05 +0200

    remove aws.cmake APPLY_PATCHES

--- .github/config/extensions/aws.cmake
+++ .github/config/extensions/aws.cmake
@@ -1,6 +1,6 @@
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(aws
-            LOAD_TESTS APPLY_PATCHES
+            LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-aws
             GIT_TAG 7d04119ee8d3f8836e278f0e8cbf21827ff5338b
             )
