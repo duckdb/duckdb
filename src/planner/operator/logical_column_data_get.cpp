4a918fa91420d8ae0af1ad0d85828d0bb036fead
Denis Hirn <hirnde@gmail.com>
2026-07-03 14:26:04 +0200

    Refine column data scan batch index support

--- src/planner/operator/logical_column_data_get.cpp
+++ src/planner/operator/logical_column_data_get.cpp
@@ -1,5 +1,6 @@
 #include "duckdb/planner/operator/logical_column_data_get.hpp"
 
+#include "duckdb/common/exception.hpp"
 #include "duckdb/common/serializer/deserializer.hpp"
 #include "duckdb/common/serializer/serializer.hpp"
 #include "duckdb/common/types/data_chunk.hpp"
@@ -21,16 +22,16 @@ LogicalColumnDataGet::LogicalColumnDataGet(TableIndex table_index, vector<Logica
     : LogicalOperator(LogicalOperatorType::LOGICAL_CHUNK_GET), table_index(table_index),
       collection(std::move(collection_p)) {
 	D_ASSERT(!types.empty());
-	column_ids = GenerateColumnDataColumnIds(types.size());
 	chunk_types = std::move(types);
+	SetColumnIds(GenerateColumnDataColumnIds(chunk_types.size()));
 }
 
 LogicalColumnDataGet::LogicalColumnDataGet(TableIndex table_index, vector<LogicalType> types,
                                            ColumnDataCollection &to_scan)
     : LogicalOperator(LogicalOperatorType::LOGICAL_CHUNK_GET), table_index(table_index), collection(to_scan) {
 	D_ASSERT(!types.empty());
-	column_ids = GenerateColumnDataColumnIds(types.size());
 	chunk_types = std::move(types);
+	SetColumnIds(GenerateColumnDataColumnIds(chunk_types.size()));
 }
 
 LogicalColumnDataGet::LogicalColumnDataGet(TableIndex table_index, vector<LogicalType> types,
@@ -38,12 +39,19 @@ LogicalColumnDataGet::LogicalColumnDataGet(TableIndex table_index, vector<Logica
     : LogicalOperator(LogicalOperatorType::LOGICAL_CHUNK_GET), table_index(table_index),
       collection(std::move(collection_p)) {
 	D_ASSERT(!types.empty());
-	column_ids = GenerateColumnDataColumnIds(types.size());
 	chunk_types = std::move(types);
+	SetColumnIds(GenerateColumnDataColumnIds(chunk_types.size()));
 }
 
 void LogicalColumnDataGet::SetColumnIds(vector<column_t> column_ids_p) {
+	for (auto column_id : column_ids_p) {
+		if (column_id >= chunk_types.size()) {
+			throw SerializationException("LogicalColumnDataGet column id %llu is out of bounds for %llu chunk types",
+			                             column_id, chunk_types.size());
+		}
+	}
 	column_ids = std::move(column_ids_p);
+	ResolveTypes();
 }
 
 const vector<column_t> &LogicalColumnDataGet::GetColumnIds() const {
@@ -72,7 +80,8 @@ void LogicalColumnDataGet::Serialize(Serializer &serializer) const {
 	serializer.WritePropertyWithDefault<TableIndex>(200, "table_index", table_index);
 	serializer.WritePropertyWithDefault<vector<LogicalType>>(201, "chunk_types", chunk_types);
 	serializer.WritePropertyWithDefault<optionally_owned_ptr<ColumnDataCollection>>(202, "collection", collection);
-	serializer.WriteProperty(203, "column_ids", column_ids);
+	serializer.WritePropertyWithDefault<vector<column_t>>(203, "column_ids", column_ids,
+	                                                      GenerateColumnDataColumnIds(chunk_types.size()));
 }
 
 unique_ptr<LogicalOperator> LogicalColumnDataGet::Deserialize(Deserializer &deserializer) {
@@ -83,7 +92,9 @@ unique_ptr<LogicalOperator> LogicalColumnDataGet::Deserialize(Deserializer &dese
 	auto result = duckdb::unique_ptr<LogicalColumnDataGet>(
 	    new LogicalColumnDataGet(table_index, std::move(chunk_types), std::move(collection)));
 	if (deserializer.CanDeserializeProperty(203, "column_ids")) {
-		deserializer.ReadProperty(203, "column_ids", result->column_ids);
+		vector<column_t> column_ids;
+		deserializer.ReadProperty(203, "column_ids", column_ids);
+		result->SetColumnIds(std::move(column_ids));
 	}
 	return std::move(result);
 }
