a60aa42275c2f09172df36b0da05212e7261b2f6
Laurens Kuiper <laurens@ducklabs.com>
2026-07-06 16:29:06 +0200

    bump for credential refresh

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 3fb3fc987ca8823979912f35d4acc8b3537b77c7
+    GIT_TAG 644d705b4076cd7f8c31cb02a1242d505930aa6a
 )
