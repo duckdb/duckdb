95364ad1c01f1758e2e64840a290bb94cb1f97d4
Laurens Kuiper <laurens@ducklabs.com>
2026-07-07 08:39:31 +0200

    another patch

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 644d705b4076cd7f8c31cb02a1242d505930aa6a
+    GIT_TAG 772952f082faeb5ec21fec55eaab902b8600e450
 )
