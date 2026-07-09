2a635ef1fd68baaf275043e976bcd204bb58af2b
Laurens Kuiper <laurens@ducklabs.com>
2026-07-09 15:17:57 +0200

    bump iceberg

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -8,6 +8,6 @@ if (NOT MINGW)
   duckdb_extension_load(iceberg
      #FIXME: restore autoloading tests ${LOAD_ICEBERG_TESTS}
             GIT_URL https://github.com/duckdb/duckdb-iceberg
-            GIT_TAG 5edc45f0ccdb308f066a9274449c3cb0c49ed0ea
+            GIT_TAG 45163a28e0ed6a2071a82a1bf1dd432d0216cf9c
             )
 endif()
