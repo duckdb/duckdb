6f2e27664e9e01a9dc2a0f3d777e085c9f1fe179
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-07-10 21:05:39 +0200

    Temporarily disable lance

--- .github/config/extensions/lance.cmake
+++ .github/config/extensions/lance.cmake
@@ -1,4 +1,4 @@
-if (NOT MINGW AND NOT ${WASM_ENABLED})
+if (FALSE AND NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(lance
             GIT_URL https://github.com/lance-format/lance-duckdb
             GIT_TAG cd592bc76dfeac28bc7d6344a5bf85c4d4c31405
