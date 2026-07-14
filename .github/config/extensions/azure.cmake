07777b75644d19fb3871b1d670377f3db06bb6cb
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-07-14 13:50:36 +0200

    More azure bump

--- .github/config/extensions/azure.cmake
+++ .github/config/extensions/azure.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
   duckdb_extension_load(azure
         LOAD_TESTS
         GIT_URL https://github.com/duckdb/duckdb-azure
-        GIT_TAG 334798fc85500be5a5cb88d6afba5746a93d121a
+        GIT_TAG 003214c96d0caa39d5c3e27a9e1976a0692c7d37
   )
 endif()
