f8629fb30f3f5f2badbd4346e59bf5030b79ed80
ben fleis <benfleis@users.noreply.github.com>
2026-07-14 10:02:04 +0000

    Bump azure, unity_catalog extensions to v1.5-variegata HEAD
    - duckdb-azure: 563589b -> 003214c (v1.5-variegata branch)
    - unity_catalog: e37b1b4 -> fd85147 (v1.5-variegata branch)

--- .github/config/extensions/azure.cmake
+++ .github/config/extensions/azure.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
   duckdb_extension_load(azure
         LOAD_TESTS
         GIT_URL https://github.com/duckdb/duckdb-azure
-        GIT_TAG 563589b2f24290a4dcdd4247eaedf2b544f9dbcd
+        GIT_TAG 003214c96d0caa39d5c3e27a9e1976a0692c7d37
   )
 endif()
