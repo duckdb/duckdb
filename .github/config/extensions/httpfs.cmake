7e68b4cd6e158e11ce0b0c86500d25e32da9d03f
Tmonster <tom@ebergen.com>
2026-07-15 09:16:09 +0200

    httpfs must also be latest

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
-    GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 53c5b032f6c368cfcc1a1ac3819118e86d3286a6
-    APPLY_PATCHES
+    GIT_URL https://github.com/Tmonster/duckdb-httpfs
+    GIT_TAG 87b7d58884d0e0520db9114f48d2a12ce9b98da9 
 )
