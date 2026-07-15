9d4e8b024c9d99aaba83d18545758a5e8977df30
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-07-15 06:42:18 +0200

    Bump quack to 8e715ebb

--- .github/config/extensions/quack.cmake
+++ .github/config/extensions/quack.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(quack
     ## LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-quack
-    GIT_TAG 29fc0391dc14bc69914aaad0493a1d31bc67e07f
+    GIT_TAG 8e715ebb6d0fd6f36b1c705e431f5e3f33c45ef3
 )
