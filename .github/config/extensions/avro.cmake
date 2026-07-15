a8da34ff0996692d1df5ff3a380b782f062f175a
Tmonster <tom@ebergen.com>
2026-07-15 08:36:17 +0200

    fix avro

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -1,8 +1,8 @@
 if (NOT MINGW)
     duckdb_extension_load(avro
             LOAD_TESTS
-            GIT_URL https://github.com/duckdb/duckdb-avro
-            GIT_TAG d9dccdafa15c57071817ea13f1588c7b04f61ba6
-            APPLY_PATCHES
+            GIT_URL https://github.com/Tmonster/duckdb-avro
+            GIT_TAG 88240f56cbc2d95c60083170ecac9ba389174356
+	    SUBMODULES "third_party/avro-c"
     )
 endif()
