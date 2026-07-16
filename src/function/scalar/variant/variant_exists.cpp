31672bc587ec8ffd4a32e6c5221cf7e254d809d2
Sebastiaan Gerritsen <sebastiaan@ducklabs.com>
2026-07-16 17:16:41 +0200

    Rewrote traversal generics to support VariantIterator.

--- src/function/scalar/variant/variant_exists.cpp
+++ src/function/scalar/variant/variant_exists.cpp
@@ -1,35 +1,20 @@
 #include "duckdb/common/vector/flat_vector.hpp"
-#include "duckdb/common/types/variant.hpp"
+#include "duckdb/common/types/variant_iterator.hpp"
 #include "duckdb/function/scalar/variant_path_function.hpp"
 #include "duckdb/function/scalar/variant_functions.hpp"
 
 namespace duckdb {
 
-// TODO: Currently collection will always happen on the unshredded variant, introduce a fast path for shredded variants.
-static ValidityMask CollectVariantExistence(const UnifiedVariantVectorData &variant,
-                                            const vector<VariantPathComponent> &components, const idx_t count) {
-	ValidityMask path_validity(count);
-	VariantPathSelection path_selection(count);
-
-	const auto owned_nested_data = make_unsafe_uniq_array_uninitialized<VariantNestedData>(count);
-	const array_ptr nested_data(owned_nested_data.get(), count);
-
-	VariantUtils::TraversePath(variant, components, count, nested_data, path_validity, path_selection);
-
-	return path_validity;
-}
-
-static void WriteExistsResult(const UnifiedVariantVectorData &, VectorWriter<bool> &existence_writer,
-                              const ValidityMask &path_validity, const idx_t row_idx) {
-	if (path_validity.RowIsValid(row_idx)) {
-		existence_writer.WriteValue(true);
-	} else {
+static void WriteExistsResult(const optional<VariantNode> &node, VectorWriter<bool> &existence_writer) {
+	if (!node) {
 		existence_writer.WriteValue(false);
+		return;
 	}
+	existence_writer.WriteValue(true);
 }
 
 static void VariantExistsFunction(DataChunk &input, ExpressionState &state, Vector &result) {
-	VariantPathFunction::Execute<ValidityMask, bool>(input, state, result, CollectVariantExistence, WriteExistsResult);
+	VariantPathFunction::Execute<bool>(input, state, result, WriteExistsResult);
 }
 
 ScalarFunctionSet VariantExistsFun::GetFunctions() {
