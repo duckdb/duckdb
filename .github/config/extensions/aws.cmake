5a057d37e893019845265df06966988e09c4808f
Carlo Piovesan <piovesan.carlo@gmail.com>
2026-07-17 12:55:11 +0200

    Bump aws to efa54a990e16c976576685dd4134d2478cf5a574

--- .github/config/extensions/aws.cmake
+++ .github/config/extensions/aws.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(aws
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG 08ad34f625e4a8e15221e462b96000ff29174447
+            GIT_TAG efa54a990e16c976576685dd4134d2478cf5a574
             )
 endif()
