f6a71a4e11d2a407361b03573b855a7147caba1f
Sebastiaan Gerritsen <sebastiaan@ducklabs.com>
2026-07-20 16:27:08 +0200

    Moved state inside function scope to reduce string and heap churn

--- src/function/scalar/variant/variant_extract_string.cpp
+++ src/function/scalar/variant/variant_extract_string.cpp
@@ -9,101 +9,161 @@
 
 namespace duckdb {
 
-static void AppendJSONString(string &result, const string &value);
-static void AppendJSONString(string &result, const string_t &value);
+struct VariantStringExtractLocalState : public FunctionLocalState {
+	string tmp;
+	StringHeap heap;
+};
+
+class VariantStringSerializer {
+public:
+	explicit VariantStringSerializer(VariantStringExtractLocalState &state) : tmp(state.tmp), heap(state.heap) {
+	}
+
+public:
+	//! Row result writer, modeled as part of `VariantStringSerializer` so we have easy access to function scoped state.
+	void operator()(const optional<VariantNode> &node, VectorWriter<string_t> &string_writer) const;
+
+private:
+	template <class T>
+	void AppendStringCast(T value, bool quote) const;
+	void AppendPrimitiveString(const string &value, bool quote) const;
+	void AppendPrimitiveString(const string_t &value, bool quote) const;
+	void SerializePrimitive(const VariantNode &node, VariantLogicalType type, bool quote) const;
+	static bool PrimitiveNeedsQuotes(VariantLogicalType type);
+	void AppendJSONString(const char *data, idx_t size) const;
+	void SerializeVariant(const VariantNode &node) const;
+	void SerializeTopLevel(const VariantNode &node) const;
+
+private:
+	string &tmp;
+	StringHeap &heap;
+};
+
+void VariantStringSerializer::AppendJSONString(const char *data, const idx_t size) const {
+	tmp += '"';
+	for (idx_t i = 0; i < size; i++) {
+		const auto byte = static_cast<uint8_t>(data[i]);
+		switch (byte) {
+		case '"':
+			tmp += "\\\"";
+			break;
+		case '\\':
+			tmp += "\\\\";
+			break;
+		case '\b':
+			tmp += "\\b";
+			break;
+		case '\f':
+			tmp += "\\f";
+			break;
+		case '\n':
+			tmp += "\\n";
+			break;
+		case '\r':
+			tmp += "\\r";
+			break;
+		case '\t':
+			tmp += "\\t";
+			break;
+		default:
+			if (byte < 0x20) {
+				tmp += duckdb_fmt::sprintf("\\u%04x", static_cast<unsigned int>(byte));
+			} else {
+				tmp += static_cast<char>(byte);
+			}
+		}
+	}
+	tmp += '"';
+}
 
 template <class T>
-static void AppendStringCast(string &result, const T value, StringHeap &heap, const bool add_quotes) {
+void VariantStringSerializer::AppendStringCast(const T value, const bool quote) const {
 	const auto val = StringCast::Operation(value, heap);
-	if (add_quotes) {
-		AppendJSONString(result, val);
+	if (quote) {
+		AppendJSONString(val.GetData(), val.GetSize());
 	} else {
-		result.append(val.GetData(), val.GetSize());
+		tmp.append(val.GetData(), val.GetSize());
 	}
 }
 
-static void AppendPrimitiveString(string &result, const string &value, const bool add_quotes) {
-	if (add_quotes) {
-		AppendJSONString(result, value);
+void VariantStringSerializer::AppendPrimitiveString(const string &value, const bool quote) const {
+	if (quote) {
+		AppendJSONString(value.data(), value.size());
 	} else {
-		result.append(value);
+		tmp.append(value);
 	}
 }
 
-static void AppendPrimitiveString(string &result, const string_t &value, const bool add_quotes) {
-	if (add_quotes) {
-		AppendJSONString(result, value);
+void VariantStringSerializer::AppendPrimitiveString(const string_t &value, const bool quote) const {
+	if (quote) {
+		AppendJSONString(value.GetData(), value.GetSize());
 	} else {
-		result.append(value.GetData(), value.GetSize());
+		tmp.append(value.GetData(), value.GetSize());
 	}
 }
 
-static void SerializePrimitive(string &result, const VariantNode &node, const VariantLogicalType type, StringHeap &heap,
-                               const bool add_quotes) {
+void VariantStringSerializer::SerializePrimitive(const VariantNode &node, const VariantLogicalType type,
+                                                 const bool quote) const {
 	switch (type) {
 	case VariantLogicalType::VARIANT_NULL:
-		result.append("null");
+		tmp.append("null");
 		break;
 	case VariantLogicalType::BOOL_TRUE:
-		result.append("true");
+		tmp.append("true");
 		break;
 	case VariantLogicalType::BOOL_FALSE:
-		result.append("false");
+		tmp.append("false");
 		break;
 	case VariantLogicalType::INT8:
-		AppendStringCast(result, node.GetData<int8_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<int8_t>(), quote);
 		break;
 	case VariantLogicalType::INT16:
-		AppendStringCast(result, node.GetData<int16_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<int16_t>(), quote);
 		break;
 	case VariantLogicalType::INT32:
-		AppendStringCast(result, node.GetData<int32_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<int32_t>(), quote);
 		break;
 	case VariantLogicalType::INT64:
-		AppendStringCast(result, node.GetData<int64_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<int64_t>(), quote);
 		break;
 	case VariantLogicalType::INT128:
-		AppendStringCast(result, node.GetData<hugeint_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<hugeint_t>(), quote);
 		break;
 	case VariantLogicalType::UINT8:
-		AppendStringCast(result, node.GetData<uint8_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<uint8_t>(), quote);
 		break;
 	case VariantLogicalType::UINT16:
-		AppendStringCast(result, node.GetData<uint16_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<uint16_t>(), quote);
 		break;
 	case VariantLogicalType::UINT32:
-		AppendStringCast(result, node.GetData<uint32_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<uint32_t>(), quote);
 		break;
 	case VariantLogicalType::UINT64:
-		AppendStringCast(result, node.GetData<uint64_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<uint64_t>(), quote);
 		break;
 	case VariantLogicalType::UINT128:
-		AppendStringCast(result, node.GetData<uhugeint_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<uhugeint_t>(), quote);
 		break;
 	case VariantLogicalType::FLOAT:
-		AppendStringCast(result, node.GetData<float>(), heap, add_quotes);
+		AppendStringCast(node.GetData<float>(), quote);
 		break;
 	case VariantLogicalType::DOUBLE:
-		AppendStringCast(result, node.GetData<double>(), heap, add_quotes);
+		AppendStringCast(node.GetData<double>(), quote);
 		break;
 	case VariantLogicalType::DECIMAL: {
 		const auto decimal = node.GetDecimal();
 		switch (decimal.GetPhysicalType()) {
 		case PhysicalType::INT16:
-			AppendPrimitiveString(result, Decimal::ToString(node.GetData<int16_t>(), decimal.width, decimal.scale),
-			                      add_quotes);
+			AppendPrimitiveString(Decimal::ToString(node.GetData<int16_t>(), decimal.width, decimal.scale), quote);
 			break;
 		case PhysicalType::INT32:
-			AppendPrimitiveString(result, Decimal::ToString(node.GetData<int32_t>(), decimal.width, decimal.scale),
-			                      add_quotes);
+			AppendPrimitiveString(Decimal::ToString(node.GetData<int32_t>(), decimal.width, decimal.scale), quote);
 			break;
 		case PhysicalType::INT64:
-			AppendPrimitiveString(result, Decimal::ToString(node.GetData<int64_t>(), decimal.width, decimal.scale),
-			                      add_quotes);
+			AppendPrimitiveString(Decimal::ToString(node.GetData<int64_t>(), decimal.width, decimal.scale), quote);
 			break;
 		case PhysicalType::INT128:
-			AppendPrimitiveString(result, Decimal::ToString(node.GetData<hugeint_t>(), decimal.width, decimal.scale),
-			                      add_quotes);
+			AppendPrimitiveString(Decimal::ToString(node.GetData<hugeint_t>(), decimal.width, decimal.scale), quote);
 			break;
 		default:
 			throw InternalException("Unsupported VARIANT decimal physical type");
@@ -112,75 +172,71 @@ static void SerializePrimitive(string &result, const VariantNode &node, const Va
 	}
 	case VariantLogicalType::VARCHAR: {
 		const auto str = node.GetString();
-		AppendPrimitiveString(result, str, add_quotes);
+		AppendPrimitiveString(str, quote);
 		break;
 	}
 	case VariantLogicalType::BLOB: {
 		const auto str = node.GetString();
-		AppendPrimitiveString(result, Value::BLOB(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(),
-		                      add_quotes);
+		AppendPrimitiveString(Value::BLOB(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(), quote);
 		break;
 	}
 	case VariantLogicalType::UUID:
-		AppendPrimitiveString(result, Value::UUID(node.GetData<hugeint_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::UUID(node.GetData<hugeint_t>()).ToString(), quote);
 		break;
 	case VariantLogicalType::DATE:
-		AppendStringCast(result, node.GetData<date_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<date_t>(), quote);
 		break;
 	case VariantLogicalType::TIME_MICROS:
-		AppendStringCast(result, node.GetData<dtime_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<dtime_t>(), quote);
 		break;
 	case VariantLogicalType::TIME_NANOS:
-		AppendPrimitiveString(result, Value::TIME_NS(node.GetData<dtime_ns_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::TIME_NS(node.GetData<dtime_ns_t>()).ToString(), quote);
 		break;
 	case VariantLogicalType::TIMESTAMP_SEC:
-		AppendPrimitiveString(result, Value::TIMESTAMPSEC(node.GetData<timestamp_sec_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::TIMESTAMPSEC(node.GetData<timestamp_sec_t>()).ToString(), quote);
 		break;
 	case VariantLogicalType::TIMESTAMP_MILIS:
-		AppendPrimitiveString(result, Value::TIMESTAMPMS(node.GetData<timestamp_ms_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::TIMESTAMPMS(node.GetData<timestamp_ms_t>()).ToString(), quote);
 		break;
 	case VariantLogicalType::TIMESTAMP_MICROS:
-		AppendStringCast(result, node.GetData<timestamp_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<timestamp_t>(), quote);
 		break;
 	case VariantLogicalType::TIMESTAMP_NANOS:
-		AppendPrimitiveString(result, Value::TIMESTAMPNS(node.GetData<timestamp_ns_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::TIMESTAMPNS(node.GetData<timestamp_ns_t>()).ToString(), quote);
 		break;
 	case VariantLogicalType::TIME_MICROS_TZ:
-		AppendPrimitiveString(result, Value::TIMETZ(node.GetData<dtime_tz_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::TIMETZ(node.GetData<dtime_tz_t>()).ToString(), quote);
 		break;
 	case VariantLogicalType::TIMESTAMP_MICROS_TZ:
-		AppendPrimitiveString(result, Value::TIMESTAMPTZ(node.GetData<timestamp_tz_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::TIMESTAMPTZ(node.GetData<timestamp_tz_t>()).ToString(), quote);
 		break;
 	case VariantLogicalType::INTERVAL:
-		AppendStringCast(result, node.GetData<interval_t>(), heap, add_quotes);
+		AppendStringCast(node.GetData<interval_t>(), quote);
 		break;
 	case VariantLogicalType::BIGNUM: {
 		const auto str = node.GetString();
-		AppendPrimitiveString(result, Value::BIGNUM(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(),
-		                      add_quotes);
+		AppendPrimitiveString(Value::BIGNUM(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(), quote);
 		break;
 	}
 	case VariantLogicalType::BITSTRING: {
 		const auto str = node.GetString();
-		AppendPrimitiveString(result, Value::BIT(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(),
-		                      add_quotes);
+		AppendPrimitiveString(Value::BIT(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(), quote);
 		break;
 	}
 	case VariantLogicalType::GEOMETRY: {
 		const auto str = node.GetString();
-		AppendPrimitiveString(result, Value::GEOMETRY(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(),
-		                      add_quotes);
+		AppendPrimitiveString(Value::GEOMETRY(const_data_ptr_cast(str.GetData()), str.GetSize()).ToString(), quote);
 		break;
 	}
 	case VariantLogicalType::TIMESTAMP_NANOS_TZ:
-		AppendPrimitiveString(result, Value::TIMESTAMPTZNS(node.GetData<timestamp_tz_ns_t>()).ToString(), add_quotes);
+		AppendPrimitiveString(Value::TIMESTAMPTZNS(node.GetData<timestamp_tz_ns_t>()).ToString(), quote);
 		break;
 	default:
 		throw NotImplementedException("Cannot stringify VARIANT type %s", EnumUtil::ToString(type));
 	}
 }
 
-static bool PrimitiveNeedsQuotes(const VariantLogicalType type) {
+bool VariantStringSerializer::PrimitiveNeedsQuotes(const VariantLogicalType type) {
 	switch (type) {
 	case VariantLogicalType::VARCHAR:
 	case VariantLogicalType::BLOB:
@@ -204,52 +260,7 @@ static bool PrimitiveNeedsQuotes(const VariantLogicalType type) {
 	}
 }
 
-static void AppendJSONString(string &result, const char *data, const idx_t size) {
-	result += '"';
-	for (idx_t i = 0; i < size; i++) {
-		const auto byte = static_cast<uint8_t>(data[i]);
-		switch (byte) {
-		case '"':
-			result += "\\\"";
-			break;
-		case '\\':
-			result += "\\\\";
-			break;
-		case '\b':
-			result += "\\b";
-			break;
-		case '\f':
-			result += "\\f";
-			break;
-		case '\n':
-			result += "\\n";
-			break;
-		case '\r':
-			result += "\\r";
-			break;
-		case '\t':
-			result += "\\t";
-			break;
-		default:
-			if (byte < 0x20) {
-				result += duckdb_fmt::sprintf("\\u%04x", static_cast<unsigned int>(byte));
-			} else {
-				result += static_cast<char>(byte);
-			}
-		}
-	}
-	result += '"';
-}
-
-static void AppendJSONString(string &result, const string &value) {
-	AppendJSONString(result, value.data(), value.size());
-}
-
-static void AppendJSONString(string &result, const string_t &value) {
-	AppendJSONString(result, value.GetData(), value.GetSize());
-}
-
-static void SerializeVariant(string &tmp, const VariantNode &node, StringHeap &heap) {
+void VariantStringSerializer::SerializeVariant(const VariantNode &node) const {
 	const auto type = node.GetTypeId();
 	switch (type) {
 	case VariantLogicalType::ARRAY: {
@@ -260,7 +271,7 @@ static void SerializeVariant(string &tmp, const VariantNode &node, StringHeap &h
 				tmp += ',';
 			}
 			first = false;
-			SerializeVariant(tmp, child, heap);
+			SerializeVariant(child);
 		}
 		tmp += ']';
 		break;
@@ -273,48 +284,58 @@ static void SerializeVariant(string &tmp, const VariantNode &node, StringHeap &h
 				tmp += ',';
 			}
 			first = false;
-			AppendJSONString(tmp, key);
+			AppendJSONString(key.GetData(), key.GetSize());
 			tmp += ':';
-			SerializeVariant(tmp, value, heap);
+			SerializeVariant(value);
 		}
 		tmp += '}';
 		break;
 	}
 	default: {
-		SerializePrimitive(tmp, node, type, heap, PrimitiveNeedsQuotes(type));
+		SerializePrimitive(node, type, PrimitiveNeedsQuotes(type));
 		break;
 	}
 	}
 }
 
-static void WriteStringResult(const optional<VariantNode> &node, VectorWriter<string_t> &string_writer) {
+void VariantStringSerializer::SerializeTopLevel(const VariantNode &node) const {
+	const auto type = node.GetTypeId();
+	if (type == VariantLogicalType::ARRAY || type == VariantLogicalType::OBJECT) {
+		SerializeVariant(node);
+	} else {
+		SerializePrimitive(node, type, false);
+	}
+}
+
+void VariantStringSerializer::operator()(const optional<VariantNode> &node,
+                                         VectorWriter<string_t> &string_writer) const {
+	tmp.clear();
+	heap.GetAllocator().Reset();
+
 	if (!node || node->GetTypeId() == VariantLogicalType::VARIANT_NULL) {
 		string_writer.WriteNull();
 		return;
 	}
 
-	string tmp;
-	StringHeap heap;
-	const auto type = node->GetTypeId();
-	switch (type) {
-	case VariantLogicalType::ARRAY:
-	case VariantLogicalType::OBJECT:
-		SerializeVariant(tmp, *node, heap);
-		break;
-	default:
-		SerializePrimitive(tmp, *node, type, heap, false);
-		break;
-	}
+	SerializeTopLevel(*node);
 	string_writer.WriteValue(tmp);
 }
 
+static unique_ptr<FunctionLocalState> VariantExtractStringInit(ExpressionState &, const BoundFunctionExpression &,
+                                                               FunctionData *) {
+	return make_uniq<VariantStringExtractLocalState>();
+}
+
 static void VariantExtractStringFunction(DataChunk &input, ExpressionState &state, Vector &result) {
-	VariantPathFunction::Execute<string_t>(input, state, result, WriteStringResult);
+	auto &local_state = ExecuteFunctionState::GetFunctionState(state)->Cast<VariantStringExtractLocalState>();
+
+	const VariantStringSerializer serializer(local_state);
+	VariantPathFunction::Execute<string_t>(input, state, result, serializer);
 }
 
 ScalarFunctionSet VariantExtractStringFun::GetFunctions() {
 	return VariantPathFunction::CreateFunctionSet("variant_extract_string", VariantExtractStringFunction,
-	                                              LogicalType::VARCHAR, false);
+	                                              LogicalType::VARCHAR, false, VariantExtractStringInit);
 }
 
 } // namespace duckdb
--- src/function/scalar/variant/variant_path_function.cpp
+++ src/function/scalar/variant/variant_path_function.cpp
@@ -3,25 +3,28 @@
 namespace duckdb {
 
 ScalarFunctionSet VariantPathFunction::CreateFunctionSet(const Identifier &name, const scalar_function_t &function,
-                                                         const LogicalType &return_type, const bool path_optional) {
+                                                         const LogicalType &return_type, const bool path_optional,
+                                                         const init_local_state_t init_state) {
 	ScalarFunctionSet fun_set(name);
 
 	if (path_optional) {
 		fun_set.AddFunction(ScalarFunction {
-		    {LogicalType::VARIANT()}, return_type, function, VariantBindUtils::VariantPathBind, nullptr});
+		    {LogicalType::VARIANT()}, return_type, function, VariantBindUtils::VariantPathBind, nullptr, init_state});
 	}
 
 	fun_set.AddFunction(ScalarFunction {{LogicalType::VARIANT(), LogicalType::VARCHAR},
 	                                    return_type,
 	                                    function,
 	                                    VariantBindUtils::VariantPathBind,
-	                                    nullptr});
+	                                    nullptr,
+	                                    init_state});
 
 	fun_set.AddFunction(ScalarFunction {{LogicalType::VARIANT(), LogicalType::LIST(LogicalType::VARCHAR)},
 	                                    LogicalType::LIST(return_type),
 	                                    function,
 	                                    VariantBindUtils::VariantPathBind,
-	                                    nullptr});
+	                                    nullptr,
+	                                    init_state});
 
 	return fun_set;
 }
