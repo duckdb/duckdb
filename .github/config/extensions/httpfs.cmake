95ea636cfa8357f52e47ac08aa85933deca58dde
Laurens Kuiper <laurens@ducklabs.com>
2026-07-21 14:02:07 +0200

    bump httpfs and remove patches

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 53c5b032f6c368cfcc1a1ac3819118e86d3286a6
-    APPLY_PATCHES
+    GIT_TAG fd608da2bed92a87dc8d230b424119b065be4bde
 )
