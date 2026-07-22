094edced5f173961c19e465e4760ac44c8d3ec4d
Laurens Kuiper <laurens@ducklabs.com>
2026-07-22 12:25:37 +0200

    proper hash

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,5 +1,5 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG fd608da2bed92a87dc8d230b424119b065be4bde
+    GIT_TAG df92a34d29eb589591adfadba89fa8df874e54ea
 )
