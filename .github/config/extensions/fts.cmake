379f96b477141937275166f1e4f86aa4c72c665f
Denis Hirn <hirnde@gmail.com>
2026-07-23 21:24:14 +0200

    Remove obsolete FTS patches

--- .github/config/extensions/fts.cmake
+++ .github/config/extensions/fts.cmake
@@ -3,6 +3,5 @@ duckdb_extension_load(fts
         DONT_LINK
         GIT_URL https://github.com/duckdb/duckdb-fts
         GIT_TAG b6149a9e358460295ef4ccd9a450f61bddb1f3f3
-        APPLY_PATCHES
         TEST_DIR test/sql
 )
