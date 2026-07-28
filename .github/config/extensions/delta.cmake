b4d4c9053c23653bcacd3e2947ba7fd4ed7f6154
Tishj <t_b@live.nl>
2026-07-28 12:09:55 +0200

    add patch to delta (add missing APPLY PATCHES to others)

--- .github/config/extensions/delta.cmake
+++ .github/config/extensions/delta.cmake
@@ -3,5 +3,6 @@ if(NOT MINGW AND NOT ${WASM_ENABLED})
             GIT_URL https://github.com/duckdb/duckdb-delta
             GIT_TAG e74cf88ebf64a6638de443025510d8269c31616a
             SUBMODULES extension-ci-tools
+            APPLY_PATCHES
   )
 endif()
--- .github/config/extensions/ducklake.cmake
+++ .github/config/extensions/ducklake.cmake
@@ -2,4 +2,5 @@ duckdb_extension_load(ducklake
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/ducklake
     GIT_TAG eb7b95df82fc3ba0ace777e34ef1c81280477042
+    APPLY_PATCHES
 )
