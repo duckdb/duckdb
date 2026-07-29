252a9cb1b2612e1ae1d9e2cabd8a06c091d7df90
Denis Hirn <hirnde@gmail.com>
2026-07-29 09:51:37 +0200

    Update FTS extension pin

--- .github/config/extensions/fts.cmake
+++ .github/config/extensions/fts.cmake
@@ -2,6 +2,6 @@ duckdb_extension_load(fts
         LOAD_TESTS
         DONT_LINK
         GIT_URL https://github.com/duckdb/duckdb-fts
-        GIT_TAG b6149a9e358460295ef4ccd9a450f61bddb1f3f3
+        GIT_TAG 7f833ba49db334fa58b56d2f3cf66e3ae78ec201
         TEST_DIR test/sql
 )
