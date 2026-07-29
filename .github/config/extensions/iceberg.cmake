881b250d9f4e4024471b832b983e6e08eb0feede
Tmonster <tom@ebergen.com>
2026-07-29 10:41:53 +0200

    bump iceberg

--- .github/config/extensions/iceberg.cmake
+++ .github/config/extensions/iceberg.cmake
@@ -1,14 +1,14 @@
 # Windows tests for iceberg currently not working
 IF (NOT WIN32)
-    set(LOAD_ICEBERG_TESTS "LOAD_TESTS")
+  set(LOAD_ICEBERG_TESTS "LOAD_TESTS")
 else ()
-    set(LOAD_ICEBERG_TESTS "")
+  set(LOAD_ICEBERG_TESTS "")
 endif()
 if (NOT MINGW)
-    duckdb_extension_load(iceberg
-	    #FIXME: restore autoloading tests ${LOAD_ICEBERG_TESTS}
+  duckdb_extension_load(iceberg
+     #FIXME: restore autoloading tests ${LOAD_ICEBERG_TESTS}
             GIT_URL https://github.com/duckdb/duckdb-iceberg
-            GIT_TAG 8e802098f438691bd8873e76dfcb2f55f0b11004
+            GIT_TAG 4b76e1195082689eeb8a0e05984b7a7a32c2c652
             APPLY_PATCHES
             )
 endif()
