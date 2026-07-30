22e6d6c14be5448252e73816744a608618fc8dfb
Maia <maia@duckdblabs.com>
2026-07-30 21:29:44 +0200

    arena allocate

--- extension/core_functions/aggregate/holistic/mode.cpp
+++ extension/core_functions/aggregate/holistic/mode.cpp
@@ -39,10 +39,7 @@ struct ModeStandard {
 		return RESULT_TYPE(input);
 	}
 
-	static void Destroy(T *mode) {
-	}
-
-	static T *Update(T *mode, const T &key) {
+	static T *Update(T *mode, uint32_t &, const T &key, AggregateInputData &) {
 		if (!mode) {
 			mode = new T(key);
 		}
@@ -67,39 +64,35 @@ struct ModeString {
 		return StringVector::AddStringOrBlob(result, input);
 	}
 
-	static void Destroy(string_t *mode) {
-		if (mode && !mode->IsInlined()) {
-			delete[] mode->GetData();
-			mode->SetPointer(nullptr);
-		}
-	}
-
-	static string_t *Update(string_t *mode, const string_t &key) {
+	//! alloc_size tracks the capacity of the arena allocation currently held by "mode" - the string_t's own
+	//! size reflects only the current key's length, which can shrink and grow again as the winning mode changes.
+	static string_t *Update(string_t *mode, uint32_t &alloc_size, const string_t &key,
+	                        AggregateInputData &aggr_input_data) {
 		if (key.IsInlined()) {
-			Destroy(mode);
 			if (!mode) {
 				mode = new string_t(nullptr, 0);
 			}
 			*mode = key;
+			alloc_size = 0;
 			return mode;
 		}
 
 		// non-inlined string, need to allocate space for it somehow
-		const auto len = key.GetSize();
+		const auto len = UnsafeNumericCast<uint32_t>(key.GetSize());
 		char *ptr;
-		if (!mode || mode->GetSize() < len) {
-			// we cannot fit this into the current slot - destroy it and re-allocate
-			Destroy(mode);
+		if (mode && alloc_size >= len) {
+			// this fits into the current arena allocation - reuse it
+			ptr = mode->GetDataWriteable();
+		} else {
+			// round up so repeatedly growing keys don't churn through a new arena allocation every time
+			alloc_size = UnsafeNumericCast<uint32_t>(NextPowerOfTwo(len));
+			ptr = char_ptr_cast(aggr_input_data.allocator.Allocate(alloc_size));
 			if (!mode) {
 				mode = new string_t(nullptr, 0);
 			}
-			ptr = new char[len];
-		} else {
-			// this fits into the current slot - take over the pointer
-			ptr = mode->GetDataWriteable();
 		}
 		memcpy(ptr, key.GetData(), len);
-		*mode = string_t(ptr, UnsafeNumericCast<uint32_t>(len));
+		*mode = string_t(ptr, len);
 		return mode;
 	}
 };
@@ -114,6 +107,8 @@ struct ModeState {
 	SubFrames prevs;
 	Counts *frequency_map = nullptr;
 	KEY_TYPE *mode = nullptr;
+	//! Capacity of the arena allocation backing "mode" for TYPE_OP::Update (unused for non-string KEY_TYPE)
+	uint32_t mode_alloc_size = 0;
 	size_t nonzero = 0;
 	bool valid = false;
 	size_t count = 0;
@@ -133,10 +128,7 @@ struct ModeState {
 		if (frequency_map) {
 			delete frequency_map;
 		}
-		if (mode) {
-			TYPE_OP::Destroy(mode);
-			delete mode;
-		}
+		delete mode;
 		if (scan) {
 			delete scan;
 		}
@@ -193,7 +185,7 @@ struct ModeState {
 		valid = false;
 	}
 
-	void ModeAdd(idx_t row) {
+	void ModeAdd(idx_t row, AggregateInputData &aggr_input_data) {
 		const auto &key = GetCell(row);
 		auto &attr = (*frequency_map)[key];
 		auto new_count = (attr.count += 1);
@@ -206,7 +198,7 @@ struct ModeState {
 		if (new_count > count) {
 			valid = true;
 			count = new_count;
-			Update(key);
+			Update(key, aggr_input_data);
 		}
 	}
 
@@ -222,8 +214,8 @@ struct ModeState {
 		}
 	}
 
-	void Update(const KEY_TYPE &key) {
-		mode = TYPE_OP::Update(mode, key);
+	void Update(const KEY_TYPE &key, AggregateInputData &aggr_input_data) {
+		mode = TYPE_OP::Update(mode, mode_alloc_size, key, aggr_input_data);
 	}
 
 	typename Counts::const_iterator Scan() const {
@@ -336,8 +328,10 @@ struct ModeFunction : TypedModeFunction<TYPE_OP> {
 	struct UpdateWindowState {
 		STATE &state;
 		ModeIncluded<STATE> &included;
+		AggregateInputData &aggr_input_data;
 
-		inline UpdateWindowState(STATE &state, ModeIncluded<STATE> &included) : state(state), included(included) {
+		inline UpdateWindowState(STATE &state, ModeIncluded<STATE> &included, AggregateInputData &aggr_input_data)
+		    : state(state), included(included), aggr_input_data(aggr_input_data) {
 		}
 
 		inline void Neither(idx_t begin, idx_t end) {
@@ -354,7 +348,7 @@ struct ModeFunction : TypedModeFunction<TYPE_OP> {
 		inline void Right(idx_t begin, idx_t end) {
 			for (; begin < end; ++begin) {
 				if (included(begin)) {
-					state.ModeAdd(begin);
+					state.ModeAdd(begin, aggr_input_data);
 				}
 			}
 		}
@@ -382,7 +376,7 @@ struct ModeFunction : TypedModeFunction<TYPE_OP> {
 		ModeIncluded<STATE> included(fmask, state);
 
 		using Updater = UpdateWindowState<STATE, INPUT_TYPE>;
-		Updater updater(state, included);
+		Updater updater(state, included, aggr_input_data);
 
 		if (!state.frequency_map) {
 			state.frequency_map = TYPE_OP::CreateEmpty(Allocator::DefaultAllocator());
@@ -398,7 +392,7 @@ struct ModeFunction : TypedModeFunction<TYPE_OP> {
 				for (const auto &frame : frames) {
 					for (auto i = frame.start; i < frame.end; ++i) {
 						if (included(i)) {
-							state.ModeAdd(i);
+							state.ModeAdd(i, aggr_input_data);
 						}
 					}
 				}
@@ -410,7 +404,7 @@ struct ModeFunction : TypedModeFunction<TYPE_OP> {
 				// Rescan
 				auto highest_frequency = state.Scan();
 				if (highest_frequency != state.frequency_map->end()) {
-					state.Update(highest_frequency->first);
+					state.Update(highest_frequency->first, aggr_input_data);
 					state.count = highest_frequency->second.count;
 					state.valid = (state.count > 0);
 				}
