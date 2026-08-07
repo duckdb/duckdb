8093241c4d962b61cb4274e57d1892d2633711df
dentinyhao <dentinyhao@gmail.com>
2026-08-07 05:41:19 -0700

    httpfs

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG d40d52c602b9b6627433f15ee82ece872bafc5f4
+    GIT_TAG a7b3862d86808ea7a8baa89391c96c5ca2141a53
     APPLY_PATCHES
 )
