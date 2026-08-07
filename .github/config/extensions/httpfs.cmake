bf3b9988d646389a44640b5596a646230e66e72a
dentinyhao <dentinyhao@gmail.com>
2026-08-07 03:37:18 -0700

    consider multiple cache control

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,6 +1,6 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG a7b3862d86808ea7a8baa89391c96c5ca2141a53
+    GIT_TAG d40d52c602b9b6627433f15ee82ece872bafc5f4
     APPLY_PATCHES
 )
