f146a6271328889e3456d5c147bc98a99e414d2d
Sam Ansmink <sam@duckdblabs.com>
2026-08-10 10:45:28 +0200

    Update vortex extension GIT_TAG to new commit

--- .github/config/extensions/vortex.cmake
+++ .github/config/extensions/vortex.cmake
@@ -1,7 +1,7 @@
 if (NOT WIN32 AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
     duckdb_extension_load(vortex
             GIT_URL https://github.com/vortex-data/duckdb-vortex
-            GIT_TAG ecd3884f2c94137481bb57641f7141dc7fbf520b
+            GIT_TAG bbeb8e68c350c3e20af759ebf583d7e29b61e826
             APPLY_PATCHES
             LOAD_TESTS
             DONT_LINK
