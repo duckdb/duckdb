e3ba11ca179d6ace0b5261fabf0fa580fd149385
Tishj <t_b@live.nl>
2026-08-11 12:00:44 +0200

    bump

--- .github/config/extensions/aws.cmake
+++ .github/config/extensions/aws.cmake
@@ -2,6 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(aws
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG 9c33539b1cfd2fe56a186119fdb9f378ed784077
+            GIT_TAG f9849cb7e89c1149e6f5349368696cde9f1d3502
             )
 endif()
