f0c8635b4132739ec2f1a50a8e31d302bcda718b
Tishj <t_b@live.nl>
2026-08-11 10:06:11 +0200

    bump aws and delete patches

--- .github/config/extensions/aws.cmake
+++ .github/config/extensions/aws.cmake
@@ -2,7 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(aws
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG 4e9ae4bdff4476e07cbab81bed795ee524ccf5b0
-            APPLY_PATCHES
+            GIT_TAG 9c33539b1cfd2fe56a186119fdb9f378ed784077
             )
 endif()
