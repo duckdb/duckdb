be5ba98a40a4d8ee382a56d97a2a40d27144c8b3
c113 <idlecyb@163.com>
2026-08-18 01:40:29 +0800

    Fix Parquet interval millisecond overflow

--- extension/parquet/include/parquet_interval.hpp
+++ extension/parquet/include/parquet_interval.hpp
@@ -18,10 +18,11 @@ struct ParquetIntervalUtils {
 	static constexpr const idx_t PARQUET_INTERVAL_SIZE = 12;
 
 	static void Encode(const interval_t &interval, data_ptr_t target) {
+		const auto days = int64_t(interval.days) + interval.micros / Interval::MICROS_PER_DAY;
+		const auto micros = interval.micros % Interval::MICROS_PER_DAY;
 		Store<uint32_t>(static_cast<uint32_t>(interval.months), target);
-		Store<uint32_t>(static_cast<uint32_t>(interval.days), target + sizeof(uint32_t));
-		Store<uint32_t>(static_cast<uint32_t>(interval.micros / Interval::MICROS_PER_MSEC),
-		                target + sizeof(uint32_t) * 2);
+		Store<uint32_t>(static_cast<uint32_t>(days), target + sizeof(uint32_t));
+		Store<uint32_t>(static_cast<uint32_t>(micros / Interval::MICROS_PER_MSEC), target + sizeof(uint32_t) * 2);
 	}
 
 	static uint64_t HashNormalized(const interval_t &interval) {
