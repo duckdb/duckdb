13cb54e3de7257c0f65ba0984664938e3310759e
dentinyhao <dentinyhao@gmail.com>
2026-08-21 02:30:42 -0700

    compare needle and child stats

--- src/function/scalar/list/contains_or_position.cpp
+++ src/function/scalar/list/contains_or_position.cpp
@@ -1,11 +1,9 @@
+#include "duckdb/common/types/value.hpp"
 #include "duckdb/function/scalar/list_functions.hpp"
 #include "duckdb/function/scalar/nested_functions.hpp"
-#include "duckdb/planner/expression/bound_cast_expression.hpp"
-#include "duckdb/planner/expression/bound_constant_expression.hpp"
-#include "duckdb/planner/expression_binder.hpp"
 #include "duckdb/function/scalar/list/contains_or_position.hpp"
+#include "duckdb/optimizer/statistics_propagator.hpp"
 #include "duckdb/storage/statistics/list_stats.hpp"
-#include "duckdb/storage/statistics/numeric_stats.hpp"
 #include "duckdb/storage/statistics/string_stats.hpp"
 
 namespace duckdb {
@@ -31,31 +29,58 @@ static FilterPropagateResult ListContainsFilterPrune(const FunctionStatisticsPru
 		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
 	}
 
-	const Expression *needle_expr = input.function.GetChildren()[1].get();
-	while (BoundCastExpression::IsCast(*needle_expr)) {
-		needle_expr = &BoundCastExpression::Child(needle_expr->Cast<BoundFunctionExpression>());
-	}
-	if (needle_expr->GetExpressionType() != ExpressionType::VALUE_CONSTANT) {
-		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
-	}
-	auto needle = needle_expr->Cast<BoundConstantExpression>().GetValue();
-
 	auto &child = ListStats::GetChildStats(*list_stats);
-	if (!list_stats->CanHaveNoNull() || !child.CanHaveNoNull() || needle.IsNull()) {
-		return FilterPropagateResult::FILTER_ALWAYS_FALSE_OR_NULL;
-	}
-	auto cast = needle.DefaultTryCastAs(child.GetType());
-	if (!cast) {
-		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
+	if (!list_stats->CanHaveNoNull() || !child.CanHaveNoNull()) {
+		return FilterPropagateResult::FILTER_FALSE_OR_NULL;
 	}
 
-	auto zonemap = FilterPropagateResult::NO_PRUNING_POSSIBLE;
-	if (child.GetStatsType() == StatisticsType::NUMERIC_STATS) {
-		zonemap = NumericStats::CheckZonemap(child, ExpressionType::COMPARE_EQUAL, array_ptr<const Value>(&*cast, 1));
-	} else if (child.GetStatsType() == StatisticsType::STRING_STATS) {
-		zonemap = StringStats::CheckZonemap(child, ExpressionType::COMPARE_EQUAL, array_ptr<const Value>(&*cast, 1));
+	auto needle_stats = input.ChildStats(1);
+	if (!needle_stats) {
+		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
 	}
-	return zonemap == FilterPropagateResult::FILTER_ALWAYS_FALSE ? zonemap : FilterPropagateResult::NO_PRUNING_POSSIBLE;
+	if (!needle_stats->CanHaveNoNull()) {
+		return FilterPropagateResult::FILTER_FALSE_OR_NULL;
+	}
+	if (child.GetType() != needle_stats->GetType()) {
+		return FilterPropagateResult::NO_PRUNING_POSSIBLE;
+	}
+	const bool can_have_null = list_stats->CanHaveNull() || needle_stats->CanHaveNull();
+	const auto filter_false =
+	    can_have_null ? FilterPropagateResult::FILTER_FALSE_OR_NULL : FilterPropagateResult::FILTER_ALWAYS_FALSE;
+
+	if (child.GetStatsType() == StatisticsType::NUMERIC_STATS &&
+	    needle_stats->GetStatsType() == StatisticsType::NUMERIC_STATS) {
+		auto zonemap = StatisticsPropagator::PropagateComparison(child, *needle_stats, ExpressionType::COMPARE_EQUAL);
+		if (zonemap == FilterPropagateResult::FILTER_ALWAYS_FALSE ||
+		    zonemap == FilterPropagateResult::FILTER_FALSE_OR_NULL) {
+			return filter_false;
+		}
+	} else if (child.GetStatsType() == StatisticsType::STRING_STATS &&
+	           needle_stats->GetStatsType() == StatisticsType::STRING_STATS) {
+		if (StringStats::HasMin(*needle_stats) &&
+		    StringStats::GetMinType(*needle_stats) == StringStatsType::EXACT_STATS) {
+			auto needle_min = StringStats::Min(*needle_stats);
+			auto needle_value =
+			    child.GetType().id() == LogicalTypeId::BLOB ? Value::BLOB_RAW(needle_min) : Value(needle_min);
+			auto zonemap = StringStats::CheckZonemap(child, ExpressionType::COMPARE_GREATERTHANOREQUALTO,
+			                                         array_ptr<const Value>(&needle_value, 1));
+			if (zonemap == FilterPropagateResult::FILTER_ALWAYS_FALSE) {
+				return filter_false;
+			}
+		}
+		if (StringStats::HasMax(*needle_stats) &&
+		    StringStats::GetMaxType(*needle_stats) == StringStatsType::EXACT_STATS) {
+			auto needle_max = StringStats::Max(*needle_stats);
+			auto needle_value =
+			    child.GetType().id() == LogicalTypeId::BLOB ? Value::BLOB_RAW(needle_max) : Value(needle_max);
+			auto zonemap = StringStats::CheckZonemap(child, ExpressionType::COMPARE_LESSTHANOREQUALTO,
+			                                         array_ptr<const Value>(&needle_value, 1));
+			if (zonemap == FilterPropagateResult::FILTER_ALWAYS_FALSE) {
+				return filter_false;
+			}
+		}
+	}
+	return FilterPropagateResult::NO_PRUNING_POSSIBLE;
 }
 
 ScalarFunction ListContainsFun::GetFunction() {
