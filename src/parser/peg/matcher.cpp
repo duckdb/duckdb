0047ca47b81cef45de0da5e13014f962ad730ab6
dtenwolde <dljtw98@gmail.com>
2026-08-21 09:10:32 +0200

    First start on heap based matcher

--- src/parser/peg/matcher.cpp
+++ src/parser/peg/matcher.cpp
@@ -21,10 +21,30 @@
 namespace duckdb {
 
 MatcherResult Matcher::MatchParseResult(MatchState &state) const {
-	if (state.packrat_cache && IsPackratMemoized()) {
-		return state.packrat_cache->Match(*this, state);
+	if (!state.packrat_cache || !IsPackratMemoized() || !GetPackratId().IsValid()) {
+		return MatchParseResultInternal(state);
 	}
-	return MatchParseResultInternal(state);
+
+	auto token_index = state.token_iterator.Position();
+	auto cached_result = state.packrat_cache->Lookup(*this, token_index);
+	if (cached_result) {
+		state.token_iterator.SetPosition(cached_result->token_index_after);
+		state.max_token_index = MaxValue(state.max_token_index, cached_result->max_token_index_seen);
+		if (cached_result->success) {
+			return MatcherResult::Success(cached_result->result);
+		}
+		return MatcherResult::Failure();
+	}
+
+	auto max_token_index_before = state.GetMaxTokenIndex();
+	auto result = MatchParseResultInternal(state);
+	ParserPackratEntry cache_entry;
+	cache_entry.success = result.IsSuccess();
+	cache_entry.token_index_after = state.token_iterator.Position();
+	cache_entry.max_token_index_seen = MaxValue(max_token_index_before, state.GetMaxTokenIndex());
+	cache_entry.result = result.GetParseResult();
+	state.packrat_cache->Store(*this, token_index, std::move(cache_entry));
+	return result;
 }
 
 SuggestionType Matcher::AddSuggestion(MatchState &state) const {
