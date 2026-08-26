7d6e40eccc9eb329b435c40c2545b8b8a67d8fa4
okruitho <oscar@ducklabs.com>
2026-08-26 16:49:39 +0200

    fixed get_copy_list to now recursively copy files.

--- third_party/mbedtls/inline_mbedtls.py
+++ third_party/mbedtls/inline_mbedtls.py
@@ -1,8 +1,8 @@
 import os
 version = '3.6.4'
 
-# os.system(f'wget https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/mbedtls-{version}.tar.gz')
-# os.system(f'tar xvf mbedtls-{version}.tar.gz')
+# assert(os.system(f'wget https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/mbedtls-{version}.tar.gz') == 0)
+# assert(os.system(f'tar xvf mbedtls-{version}.tar.gz') == 0)
 
 directories = ['include', 'library']
 source_dir = f'mbedtls-mbedtls-{version}'
@@ -17,27 +17,30 @@ class FileToCopy:
 def get_copy_list(source_dir, target_dir):
     result = []
     for file in os.listdir(source_dir):
+        target_file_name = file
+        if target_file_name.endswith('.c'):
+            target_file_name = target_file_name[:-2] + '.cpp'
+
+        source_file = os.path.join(source_dir, file)
+        target_file = os.path.join(target_dir, target_file_name)
+
+        if os.path.isdir(source_file):
+            result += get_copy_list(source_file, target_file)
+        if not os.path.isfile(target_file):
+            continue
+
         is_source_file = False
         for ext in extensions:
             if file.endswith(ext):
                 is_source_file = True
         if not is_source_file:
             continue
-        target_file_name = file
-        if target_file_name.endswith('.c'):
-            target_file_name = target_file_name[:-2] + '.cpp'
-        source_file = os.path.join(source_dir, file)
-        target_file = os.path.join(target_dir, target_file_name)
-        if os.path.isdir(source_file):
-            result += get_copy_list(source_file, target_file)
-        if not os.path.isfile(target_file):
-            continue
+
         # check if this is a dummy file
         with open(target_file, 'r') as f:
             text = f.read()
         if '// dummy file' in text:
             continue
-        print(target_file)
         result.append(FileToCopy(source_file, target_file))
     return result
 
