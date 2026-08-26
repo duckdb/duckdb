f2ac8789979ab44ae38f810e3469efd7b73bcee8
dtenwolde <dljtw98@gmail.com>
2026-08-26 14:56:55 +0200

    RepeatMatcher

--- src/parser/peg/matcher_stack.cpp
+++ src/parser/peg/matcher_stack.cpp
@@ -2,6 +2,7 @@
 #include "duckdb/parser/peg/matcher/choice_matcher.hpp"
 #include "duckdb/parser/peg/matcher/list_matcher.hpp"
 #include "duckdb/parser/peg/matcher/optional_matcher.hpp"
+#include "duckdb/parser/peg/matcher/repeat_matcher.hpp"
 
 namespace duckdb {
 namespace {
@@ -148,6 +149,54 @@ private:
 	optional_idx start_offset;
 };
 
+class RepeatMatchStackFrame : public MatchStackFrame {
+public:
+	RepeatMatchStackFrame(match_frame_index_t frame_index, const RepeatMatcher &matcher, MatchState &state)
+	    : MatchStackFrame(frame_index, matcher, state), repeat_matcher(matcher), repeat_state(state) {
+		if (auto current = repeat_state.token_iterator.Current()) {
+			start_offset = optional_idx(current->offset);
+		}
+	}
+
+	void Execute(MatchStack &stack) override {
+		if (HasChildResult()) {
+			auto child_result = TakeChildResult();
+			if (!child_result.IsSuccess()) {
+				if (!matched_once) {
+					SetResult(MatcherResult::Failure());
+				} else {
+					SetRepeatResult();
+				}
+				return;
+			}
+			matched_once = true;
+			if (child_result.HasParseResult()) {
+				results.push_back(*child_result.GetParseResult());
+			}
+			match_state.token_iterator.SetPosition(repeat_state.token_iterator);
+			auto current = repeat_state.token_iterator.Current();
+			if (current && current->type == TokenType::END_OF_INPUT_AUTOCOMPLETE) {
+				repeat_matcher.GetChildMatcher().AddSuggestion(match_state);
+				SetRepeatResult();
+				return;
+			}
+		}
+		stack.PushChildFrame(*this, repeat_matcher.GetChildMatcher(), repeat_state);
+	}
+
+private:
+	void SetRepeatResult() {
+		SetResult(match_state.AllocateParseResult<RepeatParseResult>(std::move(results), start_offset));
+	}
+
+private:
+	const RepeatMatcher &repeat_matcher;
+	MatchState repeat_state;
+	vector<reference<ParseResult>> results;
+	bool matched_once = false;
+	optional_idx start_offset;
+};
+
 } // namespace
 
 MatchStackFrame::MatchStackFrame(match_frame_index_t frame_index_p, const Matcher &matcher_p, MatchState &state_p)
@@ -209,6 +258,9 @@ match_frame_index_t MatchStack::PushFrame(const Matcher &matcher, MatchState &st
 		break;
 	case MatcherType::LIST:
 		frames.push_back(make_uniq<ListMatchStackFrame>(frame_index, matcher.Cast<ListMatcher>(), state));
+		break;
+	case MatcherType::REPEAT:
+		frames.push_back(make_uniq<RepeatMatchStackFrame>(frame_index, matcher.Cast<RepeatMatcher>(), state));
 		break;
 	default:
 		frames.push_back(make_uniq<MatchStackFrame>(frame_index, matcher, state));
