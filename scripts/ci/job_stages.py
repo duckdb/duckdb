6f16ef14dbaf5edbd5dd29cecc64697c2358b6a5
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-08-28 08:54:48 +0200

    Produce static libs zip in existing osx and windows CI jobs

--- scripts/ci/job_stages.py
+++ scripts/ci/job_stages.py
@@ -33,8 +33,7 @@ PULL_REQUEST_ONLY_JOBS = [
 PULL_REQUEST_JOBS = COMMON_JOBS + PULL_REQUEST_ONLY_JOBS
 
 NIGHTLY_ONLY_JOBS = [
-    "static-libs-osx",
-    "static-libs-windows-mingw",
+    "osx",
     "codecov",
 ]
 
@@ -49,8 +48,6 @@ MERGE_GROUP_JOBS = [
 
 RELEASE_JOBS = [
     "osx",
-    "static-libs-osx",
-    "static-libs-windows-mingw",
     "staged-extension-install",
 ]
 
