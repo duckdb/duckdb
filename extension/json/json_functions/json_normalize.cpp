dfb6454acfc253c89782b3788d6517364843865e
lghavami <leyli@ducklabs.com>
2026-09-01 20:05:12 +0200

    cleanup

--- extension/json/json_functions/json_normalize.cpp
+++ extension/json/json_functions/json_normalize.cpp
@@ -3,65 +3,7 @@
 
 namespace duckdb {
 
-//! Recursively sort all object keys alphabetically (byte-order) in a mutable JSON value
-// static void SortKeys(yyjson_mut_val *val, idx_t depth = 0) {
-// 	if (depth >= JSONCommon::MAX_RECURSION_DEPTH) {
-// 		throw InvalidInputException("json_normalize: JSON exceeds maximum recursion depth of %d",
-// 		                            JSONCommon::MAX_RECURSION_DEPTH);
-// 	}
-
-// 	if (!val) {
-// 		return;
-// 	}
-// 	if (yyjson_mut_is_obj(val)) {
-// 		idx_t size = yyjson_mut_obj_size(val);
-// 		if (size <= 1) {
-// 			if (size == 1) {
-// 				yyjson_mut_obj_iter iter;
-// 				yyjson_mut_obj_iter_init(val, &iter);
-// 				yyjson_mut_val *key = yyjson_mut_obj_iter_next(&iter);
-// 				SortKeys(yyjson_mut_obj_iter_get_val(key), depth + 1);
-// 			}
-// 			return;
-// 		}
-
-// 		// Extract key-value pairs
-// 		vector<std::pair<yyjson_mut_val *, yyjson_mut_val *>> pairs;
-// 		pairs.reserve(size);
-// 		idx_t idx, max;
-// 		yyjson_mut_val *key, *child_val;
-// 		yyjson_mut_obj_foreach(val, idx, max, key, child_val) {
-// 			pairs.emplace_back(key, child_val);
-// 		}
-
-// 		// Sort by key string
-// 		std::sort(pairs.begin(), pairs.end(),
-// 		          [](const std::pair<yyjson_mut_val *, yyjson_mut_val *> &a,
-// 		             const std::pair<yyjson_mut_val *, yyjson_mut_val *> &b) {
-// 			          const auto a_key = string_t(unsafe_yyjson_get_str(a.first), unsafe_yyjson_get_len(a.first));
-// 			          const auto b_key = string_t(unsafe_yyjson_get_str(b.first), unsafe_yyjson_get_len(b.first));
-// 			          return a_key < b_key;
-// 		          });
-
-// 		// Recursively sort nested values
-// 		for (auto &pair : pairs) {
-// 			SortKeys(pair.second, depth + 1);
-// 		}
-
-// 		// Clear and rebuild in sorted order
-// 		yyjson_mut_obj_clear(val);
-// 		for (auto &pair : pairs) {
-// 			yyjson_mut_obj_add(val, pair.first, pair.second);
-// 		}
-// 	} else if (yyjson_mut_is_arr(val)) {
-// 		idx_t idx, max;
-// 		yyjson_mut_val *elem;
-// 		yyjson_mut_arr_foreach(val, idx, max, elem) {
-// 			SortKeys(elem, depth + 1);
-// 		}
-// 	}
-// }
-
+//! Sort all object keys alphabetically (byte-order) in a mutable JSON value
 static void SortKeys(yyjson_mut_val *v) {
 	struct stack_item {
 		yyjson_mut_val *val;
@@ -87,7 +29,7 @@ static void SortKeys(yyjson_mut_val *v) {
 					yyjson_mut_obj_iter iter;
 					yyjson_mut_obj_iter_init(item.val, &iter);
 					yyjson_mut_val *key = yyjson_mut_obj_iter_next(&iter);
-					stack.push_back(stack_item{yyjson_mut_obj_iter_get_val(key)});
+					stack.push_back(stack_item {yyjson_mut_obj_iter_get_val(key)});
 				}
 				continue;
 			}
