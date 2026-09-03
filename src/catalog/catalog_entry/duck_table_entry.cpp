6f796399904c169c1a392dcb0490c58c4a90ef78
dentinyhao <dentinyhao@gmail.com>
2026-09-02 19:23:04 -0700

    test cleanup and only keep necessary

--- src/catalog/catalog_entry/duck_table_entry.cpp
+++ src/catalog/catalog_entry/duck_table_entry.cpp
@@ -1355,11 +1355,17 @@ void DuckTableEntry::Rollback(CatalogEntry &prev_entry) {
 		return;
 	}
 
-	// Remove unique/PK indexes that this rolled-back entry added.
+	// Rolls back any physical index creation.
+	// FIXME: Currently only works for PKs.
+	// FIXME: Should be changed to work for any index-based constraint.
+
 	auto &table = Cast<DuckTableEntry>();
 	auto &prev_table = prev_entry.Cast<DuckTableEntry>();
 	auto &prev_info = prev_table.GetStorage().GetDataTableInfo();
 	auto &prev_indexes = prev_info->GetIndexes();
+
+	// Find all index-based constraints that exist in rollback_table, but not in table.
+	// Then, remove them.
 
 	identifier_set_t names;
 	for (const auto &constraint : prev_table.GetConstraints()) {
@@ -1367,7 +1373,10 @@ void DuckTableEntry::Rollback(CatalogEntry &prev_entry) {
 			continue;
 		}
 		const auto &unique = constraint->Cast<UniqueConstraint>();
-		names.insert(unique.GetName(prev_table.name));
+		if (unique.is_primary_key) {
+			auto index_name = unique.GetName(prev_table.name);
+			names.insert(index_name);
+		}
 	}
 
 	for (const auto &constraint : GetConstraints()) {
@@ -1375,6 +1384,9 @@ void DuckTableEntry::Rollback(CatalogEntry &prev_entry) {
 			continue;
 		}
 		const auto &unique = constraint->Cast<UniqueConstraint>();
+		if (!unique.IsPrimaryKey()) {
+			continue;
+		}
 		auto index_name = unique.GetName(table.name);
 		if (names.find(index_name) == names.end()) {
 			prev_indexes.RemoveIndex(index_name);
