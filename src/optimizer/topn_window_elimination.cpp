8f7952543108e8af2fa6ddb52eac8e3ccc5f4b11
d-justen <mail@davidjusten.de>
2026-09-07 12:52:51 +0200

    Undo resolve

--- src/optimizer/topn_window_elimination.cpp
+++ src/optimizer/topn_window_elimination.cpp
@@ -1032,8 +1032,9 @@ bool TopNWindowElimination::CanUseLateMaterialization(const LogicalWindow &windo
 			// However, we allow replacing references to join columns as they are equal to the other side by condition.
 			column_binding_map_t<ColumnBinding> replaceable_bindings;
 			for (auto &condition : join.conditions) {
+				// A condition that is not a left/right comparison has no bindings to replace
 				if (!condition.IsComparison()) {
-					return false;
+					continue;
 				}
 				if (condition.GetComparisonType() != ExpressionType::COMPARE_EQUAL) {
 					return false;
