0cbd878089f588b453979b7151d64135e4522028
dentinyhao <dentinyhao@gmail.com>
2026-09-08 02:31:13 -0700

    check pk not null on physical index finalization

--- src/execution/operator/schema/physical_create_index.cpp
+++ src/execution/operator/schema/physical_create_index.cpp
@@ -7,9 +7,11 @@
 #include "duckdb/execution/index/bound_index.hpp"
 #include "duckdb/main/client_context.hpp"
 #include "duckdb/main/database_manager.hpp"
+#include "duckdb/planner/constraints/bound_not_null_constraint.hpp"
 #include "duckdb/storage/table/append_state.hpp"
 #include "duckdb/storage/table/data_table_info.hpp"
 #include "duckdb/storage/storage_manager.hpp"
+#include "duckdb/transaction/local_storage.hpp"
 #include "duckdb/execution/index/index_type.hpp"
 
 namespace duckdb {
@@ -136,6 +138,13 @@ SinkFinalizeType PhysicalCreateIndex::Finalize(Pipeline &pipeline, Event &event,
 	if (!storage.IsMainTable()) {
 		throw TransactionException(
 		    "Transaction conflict: cannot add an index to a table that has been altered or dropped");
+	}
+	if (alter_table_info && info->constraint_type == IndexConstraintType::PRIMARY) {
+		auto &local_storage = LocalStorage::Get(context, storage.db);
+		for (const auto &column_id : storage_ids) {
+			BoundNotNullConstraint not_null {PhysicalIndex(column_id)};
+			local_storage.VerifyNewConstraint(storage, not_null);
+		}
 	}
 
 	auto &schema = table.schema;
