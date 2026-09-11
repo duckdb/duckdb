51e4bfbebddf01b7e3055b29620e77e2a2389dbe
Maia <maia@duckdblabs.com>
2026-09-11 15:01:26 +0200

    add SUBMODULES database-connector to sqlite_scanner.cmake

--- .github/config/extensions/sqlite_scanner.cmake
+++ .github/config/extensions/sqlite_scanner.cmake
@@ -8,4 +8,5 @@ duckdb_extension_load(sqlite_scanner
         ${STATIC_LINK_SQLITE} LOAD_TESTS
         GIT_URL https://github.com/duckdb/duckdb-sqlite
         GIT_TAG 982cb44841b847e15d44a927adf0ac17f0657f81
+        SUBMODULES database-connector
         )
