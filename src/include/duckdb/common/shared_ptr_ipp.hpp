eecf447a64a3353d6f8d867cdd38619823973f89
Sander Mathijs van Veen <sander@duckdblabs.com>
2026-09-14 15:24:16 +0200

    Fix some clang-tidy errors

--- src/include/duckdb/common/shared_ptr_ipp.hpp
+++ src/include/duckdb/common/shared_ptr_ipp.hpp
@@ -92,7 +92,7 @@ public:
 	}
 
 	// Construct from std::shared_ptr
-	explicit shared_ptr(std::shared_ptr<T> other) : internal(other) {
+	explicit shared_ptr(std::shared_ptr<T> other) : internal(std::move(other)) {
 		// FIXME: should we __enable_weak_this here?
 		// *our* enable_shared_from_this hasn't initialized yet, so I think so?
 		__enable_weak_this(internal.get(), internal.get());
