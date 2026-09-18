2a40123b20825bf25fad04c2217aa589e3ed0450
taniabogatsch <44262898+taniabogatsch@users.noreply.github.com>
2026-09-18 09:45:59 +0200

    keep alias

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -256,6 +256,7 @@ static const ConfigurationOption internal_options[] = {
     FINAL_SETTING};
 
 static const ConfigurationAlias setting_aliases[] = {
+    DUCKDB_SETTING_ALIAS("__delta_only_variant_encoding_enabled", DebugDeltaOnlyVariantEncodingEnabledSetting),
     DUCKDB_SETTING_ALIAS("enable_caching_operators", EnableCachingOperatorsSetting),
     DUCKDB_SETTING_ALIAS("force_bitpacking_mode", ForceBitpackingModeSetting),
     DUCKDB_SETTING_ALIAS("force_mbedtls_unsafe", ForceMbedtlsUnsafeSetting),
