318dd10ad7312e2733a555729ec02a3b83a79b34
dentinyhao <dentinyhao@gmail.com>
2026-09-18 16:31:06 -0700

    inject fatal error before WAL_FLUSH

--- src/storage/write_ahead_log.cpp
+++ src/storage/write_ahead_log.cpp
@@ -24,6 +24,8 @@
 #include "duckdb/storage/wal_entry.hpp"
 #include "duckdb/main/attached_database.hpp"
 #include "duckdb/main/database.hpp"
+#include "duckdb/main/settings.hpp"
+#include "duckdb/main/valid_checker.hpp"
 
 namespace duckdb {
 
@@ -576,6 +578,15 @@ void WriteAheadLog::WriteAlter(CatalogEntry &entry, const AlterInfo &info) {
 void WriteAheadLog::Flush() {
 	if (!writer) {
 		return;
+	}
+
+	auto abort_mode = Settings::Get<DebugCheckpointAbortSetting>(storage_manager.GetDatabase());
+	if (abort_mode == CheckpointAbort::DEBUG_ABORT_BEFORE_WAL_FLUSH) {
+		writer->Sync();
+		storage_manager.SetWALSize(writer->GetFileSize());
+		ValidChecker::Invalidate(storage_manager.GetDatabase(), "Simulated crash before WAL_FLUSH write");
+		ValidChecker::Invalidate(storage_manager.GetAttached(), "Simulated crash before WAL_FLUSH write");
+		throw FatalException("Simulated crash before WAL_FLUSH write");
 	}
 
 	// write an empty entry
