ff586f68e982b6c41d4b892aba3b7939358bd157
dentinyhao <dentinyhao@gmail.com>
2026-09-20 21:04:26 -0700

    test config

--- src/include/duckdb/main/extension_entries.hpp
+++ src/include/duckdb/main/extension_entries.hpp
@@ -1297,7 +1297,6 @@ static constexpr ExtensionEntry EXTENSION_SETTINGS[] = {
     {"binary_as_string", "parquet"},
     {"ca_cert_file", "httpfs"},
     {"calendar", "icu"},
-    {"disable_parquet_prefetching", "parquet"},
     {"ducklake_default_data_inlining_row_limit", "ducklake"},
     {"ducklake_default_version", "ducklake"},
     {"ducklake_max_retry_count", "ducklake"},
