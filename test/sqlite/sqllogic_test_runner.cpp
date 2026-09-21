c8f6a66790253d326f30ef822ec4e3bd7e9271a4
Tishj <t_b@live.nl>
2026-09-21 14:37:36 +0200

    test for absence of a named variable

--- test/sqlite/sqllogic_test_runner.cpp
+++ test/sqlite/sqllogic_test_runner.cpp
@@ -1194,8 +1194,8 @@ void SQLLogicTestRunner::ExecuteScript(SQLLogicParser &parser, const string &scr
 				parser.Fail("%s cannot be called in a loop", directive);
 			}
 
-			if (token.parameters.empty() || (exclude_values && token.parameters.size() < 2)) {
-				parser.Fail(exclude_values ? "require-env-not requires <env name> <excluded value> [excluded value ...]"
+			if (token.parameters.empty()) {
+				parser.Fail(exclude_values ? "require-env-not requires <env name> [excluded value ...]"
 				                           : "require-env requires <env name> [expected value ...]");
 			}
 			auto skip_reason = directive + " " + StringUtil::Join(token.parameters, " ");
@@ -1219,6 +1219,14 @@ void SQLLogicTestRunner::ExecuteScript(SQLLogicParser &parser, const string &scr
 				// More proper solution is wrapping std::getenv in a duckdb::test_getenv, and having a way to inject env
 				// variables
 				env_actual = default_local_repo.c_str();
+			}
+			if (exclude_values && token.parameters.size() == 1) {
+				if (env_actual != nullptr) {
+					SkipTest(skip_reason);
+					return;
+				}
+				// An absent variable has no value to register for substitution or tagging.
+				continue;
 			}
 			if (env_actual == nullptr) {
 				// Environment variable was not found, this test should not be run
