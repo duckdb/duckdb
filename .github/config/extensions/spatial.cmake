2638c35a81ff62530e5df0210d1e317fb36ebb81
Maia <maia@duckdblabs.com>
2026-09-22 16:44:06 +0200

    bump extensions

--- .github/config/extensions/spatial.cmake
+++ .github/config/extensions/spatial.cmake
@@ -3,9 +3,8 @@ if (${BUILD_COMPLETE_EXTENSION_SET})
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG eb1e57c9d92c0f3f76eb03eaa52c315090f328cc
+    GIT_TAG b88d791484e16b5b16bd948c85eaf9ab4334ed63
     INCLUDE_DIR src/spatial
     TEST_DIR test/sql
-    APPLY_PATCHES
     )
 endif()
