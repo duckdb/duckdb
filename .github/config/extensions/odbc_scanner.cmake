91b6526c1e3f2d49a6f1c375c84f4375910642ec
Maia <maia@duckdblabs.com>
2026-09-22 13:09:34 +0200

    bump extensions

--- .github/config/extensions/odbc_scanner.cmake
+++ .github/config/extensions/odbc_scanner.cmake
@@ -2,7 +2,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(odbc_scanner
             DONT_LINK
             GIT_URL https://github.com/duckdb/odbc-scanner
-            GIT_TAG 274a3307341dcafd62471c09b45c5d858d6c95cc
-            APPLY_PATCHES
+            GIT_TAG 7ce06c95c94b46a6984968439ca31ce968a2f473
             )
 endif()
