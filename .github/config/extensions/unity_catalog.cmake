76b2b854a9cef100621b7f4a6c7d1e0d566347ed
Max Gabrielsson <max@gabrielsson.com>
2026-09-23 14:22:28 +0200

    add patches

--- .github/config/extensions/unity_catalog.cmake
+++ .github/config/extensions/unity_catalog.cmake
@@ -3,5 +3,6 @@ if(NOT MINGW AND NOT ${WASM_ENABLED} AND NOT ${MUSL_ENABLED})
             GIT_URL https://github.com/duckdb/unity_catalog
             GIT_TAG 26387d6c8f5bbbf5fad81c6e0008dd5710a9a714
             LOAD_TESTS
+            APPLY_PATCHES
   )
 endif()
