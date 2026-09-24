26481942f89228384c9b784eeef8bce274fcf39b
dtenwolde <dljtw98@gmail.com>
2026-09-24 15:09:26 +0200

    Add avro patch

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -3,6 +3,7 @@ if (NOT MINGW)
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-avro
             GIT_TAG 35fa8441d52ce6c9c5af87fa050616e8564dacf8
-	    SUBMODULES "third_party/avro-c"
+	    SUBMODULES third_party/avro-c
+	    APPLY_PATCHES
     )
 endif()
