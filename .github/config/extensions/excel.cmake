ae11f56815c096d9188b270e01cb9017d8c5b919
Maia <maia@duckdblabs.com>
2026-09-25 11:09:11 +0200

    bump extensions

--- .github/config/extensions/excel.cmake
+++ .github/config/extensions/excel.cmake
@@ -1,7 +1,7 @@
 duckdb_extension_load(excel
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-excel
-    GIT_TAG 9421a2d75bd7544336caa73e5f9e6063cc7f6992
+    GIT_TAG 9a5e88b3c9330449cbe0618100f68162969d14d4
     INCLUDE_DIR src/excel/include
     APPLY_PATCHES
     )
--- .github/config/extensions/vss.cmake
+++ .github/config/extensions/vss.cmake
@@ -2,7 +2,6 @@ duckdb_extension_load(vss
         LOAD_TESTS
         DONT_LINK
         GIT_URL https://github.com/duckdb/duckdb-vss
-        GIT_TAG 9eba8b3dc41e819eafb693102b4fb4a2845934c1
+        GIT_TAG 2e566dac6e9f0064aea8b6e834357189183d5b0b
         TEST_DIR test/sql
-        APPLY_PATCHES
     )
