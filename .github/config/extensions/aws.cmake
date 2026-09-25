0c78ac2fd32a9576abba1029f2a837f106d08604
Evert Lammerts <evert.lammerts@gmail.com>
2026-09-25 16:26:36 +0200

    Rebase aws patch on new pin

--- .github/config/extensions/aws.cmake
+++ .github/config/extensions/aws.cmake
@@ -3,5 +3,6 @@ if (NOT MINGW AND NOT ${WASM_ENABLED})
             LOAD_TESTS
             GIT_URL https://github.com/duckdb/duckdb-aws
             GIT_TAG 54191de5513dbaa441b4a89015d553bd8f67c684
+            APPLY_PATCHES
             )
 endif()
